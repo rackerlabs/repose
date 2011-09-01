3d70e4775dcde8d31a734b5c7df511802e8ba36f
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2011-09-01 15:48:38 -0500

    Move routing logic back into Power Filter's Chain and update forwarding logic.

--- project-set/core/web-application/src/main/java/com/rackspace/papi/EmptyServlet.java
+++ project-set/core/web-application/src/main/java/com/rackspace/papi/EmptyServlet.java
@@ -1,29 +1,12 @@
 package com.rackspace.papi;
 
-import com.rackspace.papi.commons.util.StringUtilities;
-
 import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 
 public final class EmptyServlet extends HttpServlet {
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        route(req, res);
-    }
-
-    private void route(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
-        final String uriPath = ((HttpServletRequest) servletRequest).getRequestURI();
-
-        if (!StringUtilities.isBlank(uriPath)) {
-            final ServletContext targetContext = getServletContext().getContext(uriPath);
-
-            if (targetContext != null) {
-                final RequestDispatcher dispatcher = targetContext.getRequestDispatcher(uriPath);
-                dispatcher.forward(servletRequest, servletResponse);
-            }
-        }
     }
 }
