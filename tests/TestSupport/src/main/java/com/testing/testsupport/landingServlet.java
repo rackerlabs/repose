3795edc32404caaa40bf9b8349c45f0245009169
Meynard Alconis <meynard.alconis@rackspace.com>
2011-09-23 09:57:34 -0500

    Changed content-normalization config to match code. Added header list to response for landing Servlet

--- tests/TestSupport/src/main/java/com/testing/testsupport/landingServlet.java
+++ tests/TestSupport/src/main/java/com/testing/testsupport/landingServlet.java
@@ -6,6 +6,7 @@ package com.testing.testsupport;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,64 +18,70 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class landingServlet extends HttpServlet {
 
-  /** 
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    try {
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet version</title>");  
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>Servlet version at " + request.getRequestURI() + "</h1>");
-      out.println("</body>");
-      out.println("</html>");
-    }finally {      
-      out.close();
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Enumeration<String> headers = request.getHeaderNames();
+        String headerName;
+        try {
+            out.println("<html>");
+            out.println("\t<head>");
+            out.println("\t\t<title>Servlet version</title>");
+            out.println("\t</head>");
+            out.println("\t<body>");
+            out.println("\t\t<h1>Servlet version at " + request.getRequestURI() + "</h1>");
+            while (headers.hasMoreElements()) {
+                headerName = headers.nextElement();
+                out.println("\t\t<h2>" + headerName + " : " + request.getHeader(headerName) + "</h2>");
+            }
+            out.println("\t</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
     }
-  }
 
-  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-  /** 
-   * Handles the HTTP <code>GET</code> method.
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-  /** 
-   * Handles the HTTP <code>POST</code> method.
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    processRequest(request, response);
-  }
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-  /** 
-   * Returns a short description of the servlet.
-   * @return a String containing servlet description
-   */
-  @Override
-  public String getServletInfo() {
-    return "Short description";
-  }// </editor-fold>
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
