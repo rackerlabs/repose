cbc486196c5ebd50635d74913982d55fc2d889c0
rose.coste <rose.coste@rackspace.com>
2011-09-25 20:56:09 -0500

    update doc publication scripts to include new intro doc

--- documentation/publish2beta.sh
+++ documentation/publish2beta.sh
@@ -1,8 +1,10 @@
 scp    target/docbkx/pdf/papi-authn-deploy.pdf     docs-beta.rackspace.com:/var/www/papi/api/v0.9
+scp    target/docbkx/pdf/papi-intro.pdf     docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp    target/docbkx/pdf/papi-ratelimiting-deploy.pdf    docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp    target/docbkx/pdf/papi-rootwar-deploy.pdf     docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp    target/docbkx/pdf/papi-versioning-deploy.pdf    docs-beta.rackspace.com:/var/www/papi/api/v0.9
-scp -r target/docbkx/webhelp/papi-authn-deploy     docs-beta.rackspace.com:/var/www/papi/api/v0.9
+scp -r target/docbkx/webhelp/papi-authn-deploy     docs-beta.rackspace.com:/var/www/papi/api/v.0.9
+scp -r target/docbkx/webhelp/papi-intro     docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp -r target/docbkx/webhelp/papi-ratelimiting-deploy    docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp -r target/docbkx/webhelp/papi-rootwar-deploy     docs-beta.rackspace.com:/var/www/papi/api/v0.9
 scp -r target/docbkx/webhelp/papi-versioning-deploy    docs-beta.rackspace.com:/var/www/papi/api/v0.9
