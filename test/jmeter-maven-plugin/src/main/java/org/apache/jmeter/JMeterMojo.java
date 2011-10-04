5a9b181bd2ec2a61ef3566aa98b01e179bf5991d
John Hopper <john.hopper@rackspace.com>
2011-10-04 13:23:07 -0500

    adding some example code shamelessly stolen from the rpm-maven plugin

--- test/jmeter-maven-plugin/src/main/java/org/apache/jmeter/JMeterMojo.java
+++ test/jmeter-maven-plugin/src/main/java/org/apache/jmeter/JMeterMojo.java
@@ -521,7 +521,74 @@ public class JMeterMojo extends AbstractMojo {
             throw new MojoExecutionException("Can't execute test", e);
         }
     }
+    
+    /* For Meynard - jacked from RPM buil maven plugin
 
+        final File workarea = mojo.getWorkarea();
+        final File f = new File( workarea, "SPECS" );
+
+        final Commandline cl = new Commandline();
+        cl.setExecutable( "rpmbuild" );
+        cl.setWorkingDirectory( f.getAbsolutePath() );
+        cl.createArg().setValue( "-bb" );
+        cl.createArg().setValue( "--buildroot" );
+        cl.createArg().setValue( mojo.getRPMBuildroot().getAbsolutePath() );
+        cl.createArg().setValue( "--define" );
+        cl.createArg().setValue( "_topdir " + workarea.getAbsolutePath() );
+        cl.createArg().setValue( "--target" );
+        cl.createArg().setValue( mojo.getTargetArch() + '-' + mojo.getTargetVendor() + '-' + mojo.getTargetOS() );
+
+        // maintain passive behavior for keyPassphrase not being present
+        final String keyname = mojo.getKeyname();
+        final Passphrase keyPassphrase = mojo.getKeyPassphrase();
+        if ( keyname != null && keyPassphrase == null )
+        {
+            cl.createArg().setValue( "--define" );
+            cl.createArg().setValue( "_gpg_name " + keyname );
+            cl.createArg().setValue( "--sign" );
+        }
+
+        cl.createArg().setValue( mojo.getName() + ".spec" );
+
+        final Log log = mojo.getLog();
+
+        final StreamConsumer stdout = new LogStreamConsumer( LogStreamConsumer.INFO, log );
+        final StreamConsumer stderr = new LogStreamConsumer( LogStreamConsumer.WARN, log );
+        try
+        {
+            if ( log.isDebugEnabled() )
+            {
+                log.debug( "About to execute \'" + cl.toString() + "\'" );
+            }
+
+            int result = CommandLineUtils.executeCommandLine( cl, stdout, stderr );
+            if ( result != 0 )
+            {
+                throw new MojoExecutionException( "RPM build execution returned: \'" + result + "\' executing \'"
+                    + cl.toString() + "\'" );
+            }
+        }
+        catch ( CommandLineException e )
+        {
+            throw new MojoExecutionException( "Unable to build the RPM", e );
+        }
+
+        // now if the passphrase has been provided and we want to try and sign automatically
+        if ( keyname != null && keyPassphrase != null )
+        {
+            RPMSigner signer = new RPMSigner( keyname, keyPassphrase.getPassphrase(), log );
+
+            try
+            {
+                signer.sign( mojo.getRPMFile() );
+            }
+            catch ( Exception e )
+            {
+                throw new MojoExecutionException( "Unable to sign RPM", e );
+            }
+        } 
+     */
+    
     /**
      * Check JMeter logfile (provided as a BufferedReader) for End message.
      *
