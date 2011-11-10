ede50a84066b4887d3e2c0d1d1491f93d375a362
Meynard Alconis <meynard.alconis@rackspace.com>
2011-11-10 12:58:46 -0600

    Added new mock servlet for keystone

--- project-set/external/testing/test-support/src/main/java/com/rackspace/tests/testSupport/MockUser.java
+++ project-set/external/testing/test-support/src/main/java/com/rackspace/tests/testSupport/MockUser.java
@@ -12,60 +12,82 @@ import java.util.Calendar;
  * @author malconis
  */
 public class MockUser {
-    
+
     protected String name, group, token;
-    protected final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T12:00:00-05:00'");
+    protected SimpleDateFormat format;
     protected final Calendar cal = Calendar.getInstance();
-    
-    
-    public MockUser(String name, String group, String token){
+    protected boolean keystone;
+
+    public MockUser(String name, String group, String token) {
         this.name = name;
         this.group = group;
         this.token = token;
         cal.setLenient(true);
         cal.add(Calendar.DAY_OF_YEAR, 1);
+        keystone = false;
     }
-    
-    public MockUser(String params, String token){
+
+    public MockUser(String params, String token) {
         this.token = token;
-        
+
         this.name = params.split("&")[0].split("=")[1];
         this.group = params.split("&")[1].split("=")[1];
         cal.setLenient(true);
         cal.add(Calendar.DAY_OF_YEAR, 1);
+        keystone = false;
     }
-    
-    
-    public String getName(){
+
+    public MockUser(MockUser user) {
+        this.token = user.getToken();
+        this.name = user.getName();
+        keystone = true;
+
+    }
+
+    public String getName() {
         return name;
     }
-    
-    public String getGroup(){
+
+    public String getGroup() {
         return group;
     }
-    
-    public String getToken(){
+
+    public String getToken() {
         return token;
     }
-    
-    
-    public boolean equals(MockUser user){
-        
-        
-        return user.getName().equals(name)  && user.getGroup().equals(group) && user.getToken().equals(token)? true :false;
+
+    public boolean equals(MockUser user) {
+
+
+        return user.getName().equals(name) && user.getGroup().equals(group) && user.getToken().equals(token) ? true : false;
     }
-    
+
     @Override
-    public String toString(){
-        
-        String responseBody = "<token xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" "
-                + "id=\""+token+"\" "
-                + "userId=\""+name+"\" "
-                + "userURL=\"/users/"+name+"\" "
-                + "created=\"2010-09-14T03:32:15-05:00\" "
-                + "expires=\"" + format.format(cal.getTime()) + "\"/>";
-                //+ "expires=\"2012-09-16T03:32:15-05:00\"/>";
-        
+    public String toString() {
+
+        String responseBody;
+        if (keystone) {
+            format  = new SimpleDateFormat("yyyy-MM-dd'T12:00:00.882246'");
+            responseBody = "{\"access\": {\"token\": {\"expires\": \"" + format.format(cal.getTime()) + "\","
+                    + " \"id\": \"" + token + "\", \"tenant\": "
+                    + "{\"id\": \"1\", \"name\": \"admin\"}}, "
+                    + "\"user\": {\"username\": \"" + name + "\", \"id\": \"4\","
+                    + " \"roles\": [{\"id\": \"1\", \"name\": \"Admin\"},"
+                    + " {\"id\": \"1\", \"name\": \"Admin\"}],"
+                    + " \"tenantId\": \"1\"}}}";
+
+
+        } else {
+            format = new SimpleDateFormat("yyyy-MM-dd'T12:00:00-05:00'");
+            responseBody = "<token xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" "
+                    + "id=\"" + token + "\" "
+                    + "userId=\"" + name + "\" "
+                    + "userURL=\"/users/" + name + "\" "
+                    + "created=\"2010-09-14T03:32:15-05:00\" "
+                    + "expires=\"" + format.format(cal.getTime()) + "\"/>";
+        }
+        //+ "expires=\"2012-09-16T03:32:15-05:00\"/>";
+
         return responseBody;
     }
 }
---
+++ project-set/external/testing/test-support/src/main/java/com/rackspace/tests/testSupport/mockKeyStoneTokenServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rackspace.tests.testSupport;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author malconis
+ */
+public class mockKeyStoneTokenServlet extends HttpServlet {
+
+    // Need to move these somewhere else where they won't be re-initialized everytime we auth
+    MockUser user1 = new MockUser("usertest1", "CLOUD", "/asdasdasd-adsasdads-asdasdasd-adsadsasd");
+    MockUser user2 = new MockUser("usertest2", "CLOUD", "/now-is-the-time");
+    MockUser user3 = new MockUser("usertest3", "CLOUD", "/my-third-test-user");
+    MockUser user4 = new MockUser("usertest4", "CLOUD", "/dkshk-fdjke3-fdfjdk-21342");
+    MockUser[] testUsers = {user1, user2, user3, user4}; // new ArrayList<MockUser>();
+    MockUser passedUser;
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String token = request.getPathInfo();
+        boolean valid = false;
+        try {
+        
+            
+            for (MockUser u : testUsers) {
+                if (u.getToken().equals(token)) {
+                    valid = true;
+                    passedUser = u;
+                }
+            }
+
+            if (valid) {
+                passedUser = new MockUser(passedUser);
+                out.println(passedUser);
+            } else {
+                response.setStatus(400);
+            }
+
+
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
