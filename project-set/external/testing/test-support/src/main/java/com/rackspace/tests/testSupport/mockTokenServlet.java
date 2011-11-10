239af723007a7c52d9be69a9139f462cd87fa6c5
Meynard Alconis <meynard.alconis@rackspace.com>
2011-11-10 15:35:58 -0600

    Added mock for getTenants for keystone. updated deb package script to include empty file in creating the /var/powerapi/logs directory

--- project-set/external/testing/test-support/src/main/java/com/rackspace/tests/testSupport/mockTokenServlet.java
+++ project-set/external/testing/test-support/src/main/java/com/rackspace/tests/testSupport/mockTokenServlet.java
@@ -7,8 +7,7 @@ package com.rackspace.tests.testSupport;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
