79508af1e2010cbfff635a3eaa12a8038825c46f
John Hopper <john.hopper@rackspace.com>
2011-12-09 13:53:14 -0600

    fixing the dist-datastore so that it correctly cleans up after the filter destroy method is called

--- project-set/components/datastore/src/main/java/com/rackspace/papi/components/datastore/DatastoreFilterLogicHandlerFactory.java
+++ project-set/components/datastore/src/main/java/com/rackspace/papi/components/datastore/DatastoreFilterLogicHandlerFactory.java
@@ -1,27 +1,18 @@
 package com.rackspace.papi.components.datastore;
 
 import com.rackspace.papi.commons.config.manager.UpdateListener;
-import com.rackspace.papi.components.datastore.hash.HashRingDatastoreManager;
 import com.rackspace.papi.filter.logic.AbstractConfiguredFilterHandlerFactory;
 import com.rackspace.papi.model.Filter;
 import com.rackspace.papi.model.Host;
 import com.rackspace.papi.model.PowerProxy;
-import com.rackspace.papi.service.datastore.DatastoreOperationException;
-import com.rackspace.papi.service.datastore.DatastoreService;
 import com.rackspace.papi.service.datastore.hash.HashedDatastore;
 import com.rackspace.papi.service.datastore.cluster.MutableClusterView;
-import com.rackspace.papi.service.datastore.cluster.ThreadSafeClusterView;
-import com.rackspace.papi.service.datastore.encoding.UUIDEncodingProvider;
-import com.rackspace.papi.service.datastore.hash.HashProvider;
-import com.rackspace.papi.service.datastore.hash.MD5HashProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.NamingException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,79 +20,64 @@ import java.util.Map;
 
 public class DatastoreFilterLogicHandlerFactory extends AbstractConfiguredFilterHandlerFactory<DatastoreFilterLogicHandler> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DatastoreFilterLogicHandlerFactory.class);
-    private final DatastoreService datastoreService;
-    private final MutableClusterView clusterView;
-    private String lastLocalAddr;
-    private HashRingDatastoreManager hashRingDatastoreManager;
-    private HashedDatastore hashRingDatastore;
+   private static final Logger LOG = LoggerFactory.getLogger(DatastoreFilterLogicHandlerFactory.class);
 
+   private final MutableClusterView clusterView;
+   private final HashedDatastore hashRingDatastore;
+   
+   private String lastLocalAddr;
+
+   public DatastoreFilterLogicHandlerFactory(MutableClusterView clusterView, HashedDatastore hashRingDatastore) {
+      this.clusterView = clusterView;
+      this.hashRingDatastore = hashRingDatastore;
+   }
+   
    @Override
    protected Map<Class, UpdateListener<?>> getListeners() {
-      return new HashMap<Class, UpdateListener<?>>() {
-         {
-            put(PowerProxy.class, new SystemModelUpdateListener());
-         }
-      };
+      final Map<Class, UpdateListener<?>> listeners = new HashMap<Class, UpdateListener<?>>();
+      listeners.put(PowerProxy.class, new SystemModelUpdateListener());
+      
+      return listeners;
    }
 
-    private class SystemModelUpdateListener implements UpdateListener<PowerProxy> {
+   protected void updateClusterMembers(PowerProxy configuration) {
+      try {
+         final List<InetSocketAddress> cacheSiblings = new LinkedList<InetSocketAddress>();
 
-        @Override
-        public void configurationUpdated(PowerProxy configurationObject) {
-            if (configurationObject == null) {
-                LOG.error("Power Proxy configuration was null - please check your configurations and error logs");
-                return;
+         for (Host hostInformation : configuration.getHost()) {
+            if (hostInformation.getFilters() != null) {
+               for (Filter f : hostInformation.getFilters().getFilter()) {
+                  if (f.getName().equals("dist-datastore")) {
+                     final InetAddress hostAddress = InetAddress.getByName(hostInformation.getHostname());
+                     final InetSocketAddress hostSocketAddress = new InetSocketAddress(hostAddress, hostInformation.getServicePort());
+
+                     cacheSiblings.add(hostSocketAddress);
+                  }
+               }
             }
+         }
 
-            try {
-                final List<InetSocketAddress> cacheSiblings = new LinkedList<InetSocketAddress>();
+         clusterView.updateMembers(cacheSiblings.toArray(new InetSocketAddress[cacheSiblings.size()]));
+      } catch (UnknownHostException uhe) {
+         LOG.error(uhe.getMessage(), uhe);
+      }
+   }
 
-                for (Host hostInformation : configurationObject.getHost()) {
-                    if (hostInformation.getFilters() != null) {
-                        for (Filter f : hostInformation.getFilters().getFilter()) {
-                            if (f.getName().equals("dist-datastore")) {
-                                final InetAddress hostAddress = InetAddress.getByName(hostInformation.getHostname());
-                                final InetSocketAddress hostSocketAddress = new InetSocketAddress(hostAddress, hostInformation.getServicePort());
+   private class SystemModelUpdateListener implements UpdateListener<PowerProxy> {
 
-                                cacheSiblings.add(hostSocketAddress);
-                            }
-                        }
-                    }
-                }
+      @Override
+      public void configurationUpdated(PowerProxy configurationObject) {
+         if (configurationObject == null) {
+            LOG.error("Power Proxy configuration was null - please check your configurations and error logs");
+            return;
+         }
 
-                clusterView.updateMembers(cacheSiblings.toArray(new InetSocketAddress[cacheSiblings.size()]));
-            } catch (UnknownHostException uhe) {
-                LOG.error(uhe.getMessage(), uhe);
-            }
+         updateClusterMembers(configurationObject);
+      }
+   }
 
-            if (hashRingDatastoreManager == null) {
-                try {
-                    final HashProvider hashProvider = new MD5HashProvider();
-
-                    hashRingDatastoreManager = new HashRingDatastoreManager("temp-host-key", UUIDEncodingProvider.getInstance(), hashProvider, clusterView, datastoreService.defaultDatastore());
-                    hashRingDatastore = hashRingDatastoreManager.newDatastoreServer("default");
-
-                    datastoreService.registerDatastoreManager(HashRingDatastoreManager.DATASTORE_MANAGER_NAME, hashRingDatastoreManager);
-                } catch (NoSuchAlgorithmException algorithmException) {
-                    LOG.error("Unable to create hash-ring datastore. Hashing algorithm is missing. Reason: " + algorithmException.getMessage(), algorithmException);
-
-                    throw new DatastoreOperationException("Unable to create hash-ring datastore. Hashing algorithm is missing. Reason: " + algorithmException.getMessage(), algorithmException);
-                } catch (NamingException ne) {
-                    LOG.error(ne.getExplanation(), ne);
-                }
-            }
-        }
-    }
-    
    @Override
    protected DatastoreFilterLogicHandler buildHandler() {
-      return new DatastoreFilterLogicHandler(datastoreService, clusterView, lastLocalAddr, hashRingDatastore);
+      return new DatastoreFilterLogicHandler(clusterView, lastLocalAddr, hashRingDatastore);
    }
-    
-
-    public DatastoreFilterLogicHandlerFactory(DatastoreService ds) {
-        datastoreService = ds;
-        clusterView = new ThreadSafeClusterView();
-    }
 }
---
+++ project-set/components/datastore/src/test/java/com/rackspace/papi/components/datastore/DatastoreDatastoreFilterTest.java
@@ -0,0 +1,74 @@
+package com.rackspace.papi.components.datastore;
+
+import com.rackspace.papi.components.datastore.hash.HashRingDatastoreManager;
+import com.rackspace.papi.service.ServiceContext;
+import com.rackspace.papi.service.config.ConfigurationService;
+import com.rackspace.papi.service.config.ConfigurationServiceContext;
+import com.rackspace.papi.service.context.jndi.ServletContextHelper;
+import com.rackspace.papi.service.datastore.DatastoreManager;
+import com.rackspace.papi.service.datastore.DatastoreService;
+import com.rackspace.papi.service.datastore.DatastoreServiceContext;
+import javax.naming.Context;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(Enclosed.class)
+public class DatastoreDatastoreFilterTest {
+
+   @Ignore
+   public static class TestParent {
+
+      protected FilterConfig mockFilterConfig;
+      protected DatastoreService datastoreService;
+      protected DistributedDatastoreFilter filter;
+
+      @Before
+      public final void beforeAll() throws Exception {
+         datastoreService = mock(DatastoreService.class);
+         mockFilterConfig = mock(FilterConfig.class);
+
+         final ServletContext servletContext = mock(ServletContext.class);
+         final Context context = mock(Context.class);
+         final ConfigurationService configurationService = mock(ConfigurationService.class);
+         final ServiceContext<ConfigurationService> configurationServiceContext = mock(ServiceContext.class);
+         final ServiceContext<DatastoreService> datastoreServiceContext = mock(ServiceContext.class);
+
+         when(mockFilterConfig.getServletContext()).thenReturn(servletContext);
+         when(servletContext.getAttribute(ServletContextHelper.SERVLET_CONTEXT_ATTRIBUTE_NAME)).thenReturn(context);
+
+         when(context.lookup(ConfigurationServiceContext.SERVICE_NAME)).thenReturn(configurationServiceContext);
+         when(context.lookup(DatastoreServiceContext.SERVICE_NAME)).thenReturn(datastoreServiceContext);
+         
+         when(configurationServiceContext.getService()).thenReturn(configurationService);
+         when(datastoreServiceContext.getService()).thenReturn(datastoreService);
+         
+         filter = new DistributedDatastoreFilter();
+      }
+   }
+
+   public static class WhenRegisteringDatastore extends TestParent {
+
+      @Test
+      public void shouldRegisterDatastore() throws Exception {
+         filter.init(mockFilterConfig);
+
+         verify(datastoreService).registerDatastoreManager(eq(HashRingDatastoreManager.DATASTORE_MANAGER_NAME), any(DatastoreManager.class));
+      }
+
+      @Test
+      public void shouldUnRegisterDatastore() throws Exception {
+         filter.init(mockFilterConfig);
+         filter.destroy();
+         
+         verify(datastoreService, times(1)).registerDatastoreManager(eq(HashRingDatastoreManager.DATASTORE_MANAGER_NAME), any(DatastoreManager.class));
+         verify(datastoreService, times(1)).unregisterDatastoreManager(HashRingDatastoreManager.DATASTORE_MANAGER_NAME);
+      }
+   }
+}
