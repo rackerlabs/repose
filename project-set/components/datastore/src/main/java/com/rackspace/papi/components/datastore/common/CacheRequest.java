48109c76d439d0b2f425f5adbf1472f829c1268a
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2012-01-03 14:47:25 -0600

    Refactor malformed cache request exception to reduce package cycles.

--- project-set/components/datastore/src/main/java/com/rackspace/papi/components/datastore/common/CacheRequest.java
+++ project-set/components/datastore/src/main/java/com/rackspace/papi/components/datastore/common/CacheRequest.java
@@ -6,7 +6,6 @@ import com.rackspace.papi.commons.util.http.ExtendedHttpHeader;
 import com.rackspace.papi.commons.util.http.HeaderConstant;
 import com.rackspace.papi.commons.util.io.BufferCapacityException;
 import com.rackspace.papi.commons.util.io.RawInputStreamReader;
-import com.rackspace.papi.components.datastore.MalformedCacheRequestException;
 
 import java.net.InetSocketAddress;
 
