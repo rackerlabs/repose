e6cc246c8d425300795e1c6e7df30fb01e6a5ae9
Meynard Alconis <meynard.alconis@rackspace.com>
2012-02-08 12:58:58 -0600

    Created new client ip identity filter. Takes the source ip from the request and adds it to the x-pp-user header

---
+++ project-set/components/client-ip-identity/src/main/java/com/rackspace/papi/components/identity/ip/IpIdentityHandler.java
@@ -0,0 +1,39 @@
+package com.rackspace.papi.components.identity.ip;
+
+import com.rackspace.papi.commons.util.http.PowerApiHeader;
+import com.rackspace.papi.commons.util.servlet.http.ReadableHttpServletResponse;
+import com.rackspace.papi.components.identity.ip.config.IpIdentityConfig;
+import com.rackspace.papi.components.identity.ip.extractor.ClientGroupExtractor;
+import com.rackspace.papi.filter.logic.common.AbstractFilterLogicHandler;
+import com.rackspace.papi.filter.logic.FilterAction;
+import com.rackspace.papi.filter.logic.FilterDirector;
+import com.rackspace.papi.filter.logic.HeaderManager;
+import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import javax.servlet.http.HttpServletRequest;
+
+public class IpIdentityHandler extends AbstractFilterLogicHandler {
+
+    private final IpIdentityConfig config;
+    private final String quality;
+
+    public IpIdentityHandler(IpIdentityConfig config, String quality) {
+        this.config = config;
+        this.quality = quality;
+    }
+
+    @Override
+    public FilterDirector handleRequest(HttpServletRequest request, ReadableHttpServletResponse response) {
+
+        final FilterDirector filterDirector = new FilterDirectorImpl();
+        HeaderManager headerManager = filterDirector.requestHeaderManager();
+        String address = request.getRemoteAddr();
+
+        if (!address.isEmpty()) {
+            headerManager.appendHeader(PowerApiHeader.USER.toString(), address+quality);
+            filterDirector.setFilterAction(FilterAction.PASS);
+            String group = new ClientGroupExtractor(request, config).determineIpGroup(address);
+            headerManager.putHeader(PowerApiHeader.GROUPS.toString(), group);
+        }
+        return filterDirector;
+    }
+}
