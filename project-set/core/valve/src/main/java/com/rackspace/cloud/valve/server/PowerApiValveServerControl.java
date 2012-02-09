d014e39bfebc523474776ab35de7187e656ebb12
John Hopper <john.hopper@rackspace.com>
2012-02-09 14:24:46 -0600

    Relocate Repose Listen Port Assignment
    This commit removes listen port assignment from Valve into the container
    configuration xml. By doing this, the port discovery code need not change to
    support other deployment options (such as ROOT.war).

--- project-set/core/valve/src/main/java/com/rackspace/cloud/valve/server/PowerApiValveServerControl.java
+++ project-set/core/valve/src/main/java/com/rackspace/cloud/valve/server/PowerApiValveServerControl.java
@@ -6,49 +6,77 @@ import java.net.Socket;
 import java.io.OutputStream;
 
 import com.rackspace.cloud.valve.jetty.ValveJettyServerBuilder;
+import com.rackspace.papi.commons.config.ConfigurationResourceException;
+import com.rackspace.papi.commons.config.parser.ConfigurationParserFactory;
+import com.rackspace.papi.commons.config.parser.jaxb.JaxbConfigurationParser;
+import com.rackspace.papi.commons.config.resource.impl.BufferedURLConfigurationResource;
+import com.rackspace.papi.container.config.ContainerConfiguration;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.eclipse.jetty.server.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PowerApiValveServerControl {
-    private static final Logger LOG = LoggerFactory.getLogger(ProxyApp.class);
 
-    private Server serverInstance;
-    private CommandLineArguments commandLineArgs;
-    private final String LOCALHOST_IP = "127.0.0.1";
+   private static final Logger LOG = LoggerFactory.getLogger(ProxyApp.class);
+   private static final String LOCALHOST_IP = "127.0.0.1";
+   
+   private Server serverInstance;
+   private CommandLineArguments commandLineArgs;
 
-    public PowerApiValveServerControl(CommandLineArguments commandLineArgs) {
-        this.commandLineArgs = commandLineArgs;
-    }
+   public PowerApiValveServerControl(CommandLineArguments commandLineArgs) {
+      this.commandLineArgs = commandLineArgs;
+   }
+
+   public int readPortNumberFromConfiguration(String cfgRoot) throws MalformedURLException, ConfigurationResourceException {
+      final URL configurationLocation = new URL("file://" + cfgRoot + "/container.cfg.xml");
+      final JaxbConfigurationParser<ContainerConfiguration> containerConfigParser = ConfigurationParserFactory.getXmlConfigurationParser(ContainerConfiguration.class);
+      final ContainerConfiguration cfg = containerConfigParser.read(new BufferedURLConfigurationResource(configurationLocation));
+
+      if (cfg != null && cfg.getDeploymentConfig() != null && cfg.getDeploymentConfig().getPort() != null) {
+         return cfg.getDeploymentConfig().getPort();
+      }
+
+      throw new ConfigurationResourceException("Container configuration is not valid. Please check your configuration.");
+   }
 
    public void startPowerApiValve() throws Exception {
+      final Thread monitor = new MonitorThread(serverInstance, commandLineArgs.getStopPort(), LOCALHOST_IP);
+      monitor.start();
 
-        try {
-            serverInstance = new ValveJettyServerBuilder(commandLineArgs.getPort(), commandLineArgs.getConfigDirectory()).newServer();
-            serverInstance.setStopAtShutdown(true);
-            serverInstance.start();
-            Thread monitor = new MonitorThread(serverInstance, commandLineArgs.getStopPort(), LOCALHOST_IP);
-            monitor.start();
-            LOG.info("Power API Valve running and listening on port: " + Integer.toString(commandLineArgs.getPort()));
-        } catch (Exception e) {
-            LOG.error("Power API Valve could not be started: " + e.getMessage());
-            
-            // Stopping the service completely as even if the user fixes their configurations the proxy will not work as the serverInstance was never started
-            LOG.error("Power API Valve will now stop");
-            serverInstance.stop(); 
-        }
-    }
+      try {
+         final int startPort = readPortNumberFromConfiguration(commandLineArgs.getConfigDirectory());
 
-    public void stopPowerApiValve() {
-        try {
-            Socket s = new Socket(InetAddress.getByName(LOCALHOST_IP), commandLineArgs.getStopPort());
-            OutputStream out = s.getOutputStream();
-            System.out.println("Sending Power API Valve stop request");
-            out.write(("\r\n").getBytes());
-            out.flush();
-            s.close();
-        } catch (IOException ioex) {
-            LOG.error("An error occured while attempting to stop Power API Valve: " + ioex.getMessage());
-        }
-    }
+         serverInstance = new ValveJettyServerBuilder(startPort, commandLineArgs.getConfigDirectory()).newServer();
+         serverInstance.setStopAtShutdown(true);
+         serverInstance.start();
+
+         LOG.info("Power API Valve running and listening on port: " + startPort);
+      } catch (Exception e) {
+         LOG.error("Power API Valve could not be started. Reason: " + e.getMessage(), e);
+         LOG.error("Power API Valve will now stop.");
+
+         monitor.interrupt();
+         
+         if (serverInstance != null) {
+            serverInstance.stop();
+         }
+      }
+   }
+
+   public void stopPowerApiValve() {
+      try {
+         final Socket s = new Socket(InetAddress.getByName(LOCALHOST_IP), commandLineArgs.getStopPort());
+         final OutputStream out = s.getOutputStream();
+
+         LOG.info("Sending Power API Valve stop request");
+
+         out.write(("\r\n").getBytes());
+         out.flush();
+         s.close();
+      } catch (IOException ioex) {
+         LOG.error("An error occured while attempting to stop Power API Valve. Reason: " + ioex.getMessage());
+      }
+   }
 }
