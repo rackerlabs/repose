1de0c31d90abc946535845ac9714b75bd3302dc8
John Hopper <john.hopper@rackspace.com>
2012-02-23 14:58:21 -0600

    adding a limited read input stream to support hard failing streams that are too large

---
+++ project-set/commons/utilities/src/test/java/com/rackspace/papi/commons/util/io/stream/LimitedReadInputStreamTest.java
@@ -0,0 +1,42 @@
+package com.rackspace.papi.commons.util.io.stream;
+
+import java.io.InputStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author zinic
+ */
+@RunWith(Enclosed.class)
+public class LimitedReadInputStreamTest {
+
+   public static class WhenReading {
+
+      protected InputStream mockedInputStream;
+      
+      @Before
+      public void standUp() throws Exception {
+         mockedInputStream = mock(InputStream.class);
+         when(mockedInputStream.read()).thenReturn(1);
+      }
+
+      @Test
+      public void shouldAllowReading() throws Exception {
+         final LimitedReadInputStream stream = new LimitedReadInputStream(10, mockedInputStream);
+         
+         assertEquals("LimitedReadInputStream must delegate reads to the wrapped InputStream", 1, stream.read());
+      }
+
+      @Test(expected = ReadLimitReachedException.class)
+      public void shouldHaltReadingWhenLimitIsBreached() throws Exception {
+         final LimitedReadInputStream stream = new LimitedReadInputStream(0, mockedInputStream);
+         stream.read();
+      }
+   }
+}
