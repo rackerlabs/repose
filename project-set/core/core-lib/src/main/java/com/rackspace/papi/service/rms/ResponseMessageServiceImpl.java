d9608846dcc9df4b73f2f9c79c6385ae929d8606
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2012-03-13 11:58:42 -0500

    B-16613: Refactor RMS some more.

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/service/rms/ResponseMessageServiceImpl.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/service/rms/ResponseMessageServiceImpl.java
@@ -12,6 +12,7 @@ import com.rackspace.papi.commons.util.logging.apache.HttpLogFormatter;
 import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletResponse;
 import com.rackspace.papi.commons.util.thread.KeyedStackLock;
 
+import com.rackspace.papi.service.rms.config.Message;
 import com.rackspace.papi.service.rms.config.OverwriteType;
 import com.rackspace.papi.service.rms.config.StatusCodeMatcher;
 import org.slf4j.Logger;
@@ -21,24 +22,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 public class ResponseMessageServiceImpl implements ResponseMessageService {
 
    private static final Logger LOG = LoggerFactory.getLogger(ResponseMessageServiceImpl.class);
    private static final HeaderChooser<MediaType> ACCEPT_TYPE_CHOOSER = new QualityFactorHeaderChooser<MediaType>(new MediaType(MimeType.WILDCARD, -1));
 
-   private List<StatusCodeMatcher> statusCodeMatcherList = new LinkedList<StatusCodeMatcher>();
-   private Map<String, Pattern> statusCodeRegexes = new HashMap<String, Pattern>();
-   private Map<String, HttpLogFormatter> formatTemplates = new HashMap<String, HttpLogFormatter>();
-
    private final KeyedStackLock configurationLock = new KeyedStackLock();
-   private final Object readKey = new Object();
    private final Object updateKey = new Object();
+   private final Object readKey = new Object();
+
+   private ImmutableRMSConfiguration immutableRMSConfiguration;
 
    @Override
    public void destroy() {
@@ -51,9 +46,8 @@ public class ResponseMessageServiceImpl implements ResponseMessageService {
       final StatusCodeMatcher matchedCode = getMatchingStatusCode(String.valueOf(response.getStatus()));
 
       if (matchedCode != null) {
-         final HttpLogFormatGenerator formatGenerator = new HttpLogFormatGenerator(configurationLock, updateKey, formatTemplates);
          final MediaType preferredMediaType = ACCEPT_TYPE_CHOOSER.choosePreferredHeaderValue(new MediaRangeParser(request.getHeaders("Accept")).parse());
-         final HttpLogFormatter formatter = formatGenerator.generate(matchedCode, preferredMediaType);
+         final HttpLogFormatter formatter = getHttpLogFormatter(matchedCode, preferredMediaType);
 
          if (formatter != null) {
 
@@ -61,14 +55,7 @@ public class ResponseMessageServiceImpl implements ResponseMessageService {
 
                final String formattedOutput = formatter.format("", request, response).trim();
 
-               // overwrite body
-               response.resetBuffer();
-               response.setContentLength(formattedOutput.length());
-               response.setHeader(CommonHttpHeader.CONTENT_TYPE.toString(), preferredMediaType.getMimeType().toString());
-
-               // TODO:Enhancement - Update formatter logic for streaming
-               // TODO:Enhancement - Update getBytes(...) to use requested content encoding
-               response.getOutputStream().write(formattedOutput.getBytes());
+               overwriteResponseBody(response, formattedOutput, preferredMediaType.getMimeType().toString());
             }
          } else{
             LOG.info("No formatter found for message code.  Skipping Response Message Service formatting for status code regex " + matchedCode.getCodeRegex());
@@ -76,48 +63,63 @@ public class ResponseMessageServiceImpl implements ResponseMessageService {
       }
    }
 
-   public boolean configSetToIfEmpty(StatusCodeMatcher matchedCode) {
-      return StringUtilities.nullSafeEqualsIgnoreCase(matchedCode.getOverwrite().value(), OverwriteType.IF_EMPTY.value());
-   }
-
-   public boolean hasBody(HttpServletResponse response) {
-      return ((MutableHttpServletResponse)response).hasBody();
-   }
-
    public void updateConfiguration(List<StatusCodeMatcher> statusCodeMatchers) {
       configurationLock.lock(updateKey);
 
       try {
-         formatTemplates.clear();
-
-         statusCodeMatcherList.clear();
-         statusCodeMatcherList.addAll(statusCodeMatchers);
-
-         statusCodeRegexes.clear();
-         for (StatusCodeMatcher code : statusCodeMatcherList) {
-            statusCodeRegexes.put(code.getId(), Pattern.compile(code.getCodeRegex()));
-         }
+        immutableRMSConfiguration = ImmutableRMSConfiguration.build(statusCodeMatchers);
       } finally {
          configurationLock.unlock(updateKey);
       }
    }
 
-   public StatusCodeMatcher getMatchingStatusCode(String statusCode) {
+   private HttpLogFormatter getHttpLogFormatter(StatusCodeMatcher matchedCode, MediaType preferredMediaType) {
+      HttpLogFormatter httpLogFormatter = null;
+
+      if (matchedCode != null && preferredMediaType != null) {
+         final Message message = MessageFilter.filterByMediaType(matchedCode.getMessage(), preferredMediaType);
+
+         configurationLock.lock(readKey);
+
+         try {
+            httpLogFormatter = immutableRMSConfiguration.getMatchingLogFormatter(matchedCode.getId(), message.getMediaType());
+         } finally {
+            configurationLock.unlock(readKey);
+         }
+      }
+
+      return httpLogFormatter;
+   }
+
+   private StatusCodeMatcher getMatchingStatusCode(String responseCode) {
       StatusCodeMatcher matchedCode = null;
 
       configurationLock.lock(readKey);
 
       try {
-         for (StatusCodeMatcher code : statusCodeMatcherList) {
-            if (statusCodeRegexes.get(code.getId()).matcher(statusCode).matches()) {
-               matchedCode = code;
-               break;
-            }
-         }
+         matchedCode = immutableRMSConfiguration.getMatchingStatusCode(responseCode);
       } finally {
          configurationLock.unlock(readKey);
       }
 
       return matchedCode;
    }
+
+   private void overwriteResponseBody(HttpServletResponse response, final String formattedOutput, String mediaType) throws IOException {
+      response.resetBuffer();
+      response.setContentLength(formattedOutput.length());
+      response.setHeader(CommonHttpHeader.CONTENT_TYPE.toString(), mediaType);
+
+      // TODO:Enhancement - Update formatter logic for streaming
+      // TODO:Enhancement - Update getBytes(...) to use requested content encoding
+      response.getOutputStream().write(formattedOutput.getBytes());
+   }
+
+   private boolean configSetToIfEmpty(StatusCodeMatcher matchedCode) {
+      return StringUtilities.nullSafeEqualsIgnoreCase(matchedCode.getOverwrite().value(), OverwriteType.IF_EMPTY.value());
+   }
+
+   private boolean hasBody(HttpServletResponse response) {
+      return ((MutableHttpServletResponse)response).hasBody();
+   }      
 }
