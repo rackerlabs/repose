8485e7bc1b5773f73788157bd59ac4b62ca8f1c8
Dan Daley <danial.daley@rackspace.com>
2012-03-29 16:06:53 -0500

    Output server's ip address from mock service

--- project-set/external/testing/test-support/src/main/java/com/rackspace/papi/mocks/providers/MockServiceProvider.java
+++ project-set/external/testing/test-support/src/main/java/com/rackspace/papi/mocks/providers/MockServiceProvider.java
@@ -9,95 +9,103 @@ import java.util.Set;
 import javax.ws.rs.core.*;
 import com.rackspace.papi.components.limits.schema.*;
 import com.rackspace.papi.components.ratelimit.util.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MockServiceProvider {
-    
-    private ObjectFactory factory;
-    private String[] absNames = {"Admin", "Tech", "Demo"};
 
-    public MockServiceProvider() {
-        factory = new ObjectFactory();
-    }
+   private ObjectFactory factory;
+   private String[] absNames = {"Admin", "Tech", "Demo"};
 
-    public Response getEndService(String body, HttpHeaders headers, UriInfo uri) {
+   public MockServiceProvider() {
+      factory = new ObjectFactory();
+   }
+
+   public Response getEndService(String body, HttpHeaders headers, UriInfo uri) {
 
 
-        Set<String> headerPairs = headers.getRequestHeaders().keySet();
-        Set<String> queryParams = uri.getQueryParameters().keySet();
-        String resp = "<html>\n\t<head>\n\t\t<title>Servlet version</title>\n\t</head>\n\t<body>\n\t\t<h1>Servlet version at "
-                + uri.getPath() + "</h1>";
-        List<String> header;
-        if (!headerPairs.isEmpty()) {
-            resp += "\n\t\t<h2>HEADERS</h2>";
-            for (String h : headerPairs) {
-                header = headers.getRequestHeader(h);
-                for (String hh : header) {
-                    resp += "\n\t\t<h3> " + h + " : " + hh + "</h3>";
-                }
+      Set<String> headerPairs = headers.getRequestHeaders().keySet();
+      Set<String> queryParams = uri.getQueryParameters().keySet();
+      StringBuilder resp = new StringBuilder("<html>\n\t<head>\n\t\t<title>Servlet version</title>\n\t</head>\n\t<body>\n\t\t<h1>Servlet version at ");
+      resp.append(uri.getPath()).append("</h1>");
+      try {
+         resp.append("<h3>Server : ").append(InetAddress.getLocalHost().getHostAddress()).append("</h3>");
+      } catch (UnknownHostException ex) {
+         resp.append("<h3>Server : ").append("Unknown").append("</h3>");
+      }
+
+      List<String> header;
+      if (!headerPairs.isEmpty()) {
+         resp.append("\n\t\t<h2>HEADERS</h2>");
+         for (String h : headerPairs) {
+            header = headers.getRequestHeader(h);
+            for (String hh : header) {
+               resp.append("\n\t\t<h3> ").append(h).append(" : ").append(hh).append("</h3>");
             }
-        }
-        if (!queryParams.isEmpty()) {
-            resp += "\n\t\t<h2>Query Parameters</h2>";
-            for (String q : queryParams) {
-                resp += "\n\t\t<h3> " + q + " : " + uri.getQueryParameters().get(q) + "</h3>";
-            }
-        }
-        if (!body.isEmpty()){
-            resp += "\n\t\t<h2>Body</h2>";
-            resp += "\n\t\t\t<h3>"+body+"</h3>";
-            
-        }
-        resp += "\n\t</body>\n</html>";
-        return Response.ok(resp).header("x-request-id", "somevalue").header("Content-Length", resp.length()).build();
-    }
-    
-    public Response getAbsoluteLimitsJSON() {
+         }
+      }
+      if (!queryParams.isEmpty()) {
+         resp.append("\n\t\t<h2>Query Parameters</h2>");
+         for (String q : queryParams) {
+            resp.append("\n\t\t<h3> ").append(q).append(" : ").append(uri.getQueryParameters().get(q)).append("</h3>");
+         }
+      }
+      if (!body.isEmpty()) {
+         resp.append("\n\t\t<h2>Body</h2>");
+         resp.append("\n\t\t\t<h3>").append(body).append("</h3>");
 
-        Limits limits = new Limits();
-        AbsoluteLimitList absList = buildAbsoluteLimits();
-        limits.setAbsolute(absList);
+      }
+      resp.append("\n\t</body>\n</html>");
+      return Response.ok(resp.toString()).header("x-request-id", "somevalue").header("Content-Length", resp.length()).build();
+   }
 
-        LimitsEntityTransformer transformer = new LimitsEntityTransformer();
-        return Response.ok(transformer.entityAsJson(limits)).build();
-    }
-    
-    public Response getAbsoluteLimitsXML() {
+   public Response getAbsoluteLimitsJSON() {
 
-        Limits limits = new Limits();
-        AbsoluteLimitList absList = buildAbsoluteLimits();
-        limits.setAbsolute(absList);
+      Limits limits = new Limits();
+      AbsoluteLimitList absList = buildAbsoluteLimits();
+      limits.setAbsolute(absList);
 
-        return Response.ok(factory.createLimits(limits)).build();
-    }
-    
-    
-    
-    private AbsoluteLimitList buildAbsoluteLimits() {
-        AbsoluteLimitList limitList = new AbsoluteLimitList();
+      LimitsEntityTransformer transformer = new LimitsEntityTransformer();
+      return Response.ok(transformer.entityAsJson(limits)).build();
+   }
+
+   public Response getAbsoluteLimitsXML() {
+
+      Limits limits = new Limits();
+      AbsoluteLimitList absList = buildAbsoluteLimits();
+      limits.setAbsolute(absList);
+
+      return Response.ok(factory.createLimits(limits)).build();
+   }
+
+   private AbsoluteLimitList buildAbsoluteLimits() {
+      AbsoluteLimitList limitList = new AbsoluteLimitList();
 
 
-        AbsoluteLimit abs;
-        int value = 20;
-        for (String name : absNames) {
-            abs = new AbsoluteLimit();
-            abs.setName(name);
-            abs.setValue(value -= 5);
-            limitList.getLimit().add(abs);
-        }
+      AbsoluteLimit abs;
+      int value = 20;
+      for (String name : absNames) {
+         abs = new AbsoluteLimit();
+         abs.setName(name);
+         abs.setValue(value -= 5);
+         limitList.getLimit().add(abs);
+      }
 
-        return limitList;
-    }
-    
-    public Response getStatusCode(String statusCode){
-        
-        int status;
-        try{
-            status = Integer.parseInt(statusCode);
-        }catch (NumberFormatException e){
-            status = 404;
-        }
-        
-        return Response.status(status).build();
-        
-    }
+      return limitList;
+   }
+
+   public Response getStatusCode(String statusCode) {
+
+      int status;
+      try {
+         status = Integer.parseInt(statusCode);
+      } catch (NumberFormatException e) {
+         status = 404;
+      }
+
+      return Response.status(status).build();
+
+   }
 }
