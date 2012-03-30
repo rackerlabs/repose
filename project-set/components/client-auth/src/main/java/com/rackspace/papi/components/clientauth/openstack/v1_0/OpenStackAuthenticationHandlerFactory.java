c2802b288531c02ef642e4c26aabdfa9ab3b1909
Fran Torres-Lopez <fran@randy-ubu.rackspace.corp>
2012-03-30 10:31:34 -0500

    Refactor auth white list.

--- project-set/components/client-auth/src/main/java/com/rackspace/papi/components/clientauth/openstack/v1_0/OpenStackAuthenticationHandlerFactory.java
+++ project-set/components/client-auth/src/main/java/com/rackspace/papi/components/clientauth/openstack/v1_0/OpenStackAuthenticationHandlerFactory.java
@@ -4,22 +4,20 @@ import com.rackspace.auth.openstack.ids.AuthenticationServiceClient;
 import com.rackspace.auth.openstack.ids.OpenStackAuthenticationService;
 import com.rackspace.papi.auth.AuthModule;
 import com.rackspace.papi.commons.util.regex.KeyedRegexExtractor;
+import com.rackspace.papi.components.clientauth.UriMatcher;
 import com.rackspace.papi.components.clientauth.config.ClientAuthConfig;
 import com.rackspace.papi.components.clientauth.openstack.config.OpenStackIdentityService;
 import com.rackspace.papi.components.clientauth.openstack.config.OpenstackAuth;
 import com.rackspace.papi.service.datastore.Datastore;
 
-import java.util.List;
-import java.util.regex.Pattern;
-
 public class OpenStackAuthenticationHandlerFactory {
    
-   public static AuthModule newInstance(ClientAuthConfig config, KeyedRegexExtractor accountRegexExtractor, Datastore datastore, List<Pattern> whiteListRegexPatterns) {
+   public static AuthModule newInstance(ClientAuthConfig config, KeyedRegexExtractor accountRegexExtractor, Datastore datastore, UriMatcher uriMatcher) {
       final OpenStackUserInfoCache cache = new OpenStackUserInfoCache(datastore);
       final OpenstackAuth authConfig = config.getOpenstackAuth();
       final OpenStackIdentityService ids = authConfig.getIdentityService();
       final OpenStackAuthenticationService authService = new AuthenticationServiceClient(ids.getUri(), ids.getUsername(), ids.getPassword());
 
-      return new OpenStackAuthenticationHandler(authConfig, authService, accountRegexExtractor, cache, whiteListRegexPatterns);
+      return new OpenStackAuthenticationHandler(authConfig, authService, accountRegexExtractor, cache, uriMatcher);
    }   
 }
