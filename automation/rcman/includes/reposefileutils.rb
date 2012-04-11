98de318d994225c6508be14ac89841344820e012
Meynard Alconis <meynard.alconis@rackspace.com>
2012-04-11 12:51:26 -0500

    Switched regression environment from chef-server to chef-solo.

---
+++ automation/rcman/includes/reposefileutils.rb
@@ -0,0 +1,22 @@
+#!usr/local/bin/ruby
+
+require File.join(File.dirname(__FILE__), 'InstanceConstants.rb')
+require File.join(File.dirname(__FILE__), 'ReposeInstance.rb')
+
+module ReposeFileUtils
+
+    def buildHostsFile1(host)
+        csvString = ""
+        REPOSE_INSTANCE1.each do |instanceType|
+            reposeInstance = ReposeInstance.new(host, instanceType.port(), instanceType.type())
+            csvString += "#{reposeInstance.to_s()}\n"
+        end
+        return csvString;
+    end
+
+    def uploadChefFiles(server)
+        Net::SCP.start( server.ip , "root", :password => server.password) do |scp|
+          scp.upload!("#{File.expand_path(File.dirname(__FILE__))}/chef-solo", "/root/", :recursive => true)
+        end
+    end
+end
