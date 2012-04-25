e4d6d00b604329e694ce6ec76dfa480f300e6518
Meynard Alconis <meynard.alconis@rackspace.com>
2012-04-25 14:36:30 -0500

    Added delegation logic to reverse-proxy-basic auth filter. removed old service auth filter

--- project-set/components/reverse-proxy-basic-auth/src/main/java/com/rackspace/papi/components/reverseproxy/basicauth/ReverseProxyBasicAuthHandler.java
+++ project-set/components/reverse-proxy-basic-auth/src/main/java/com/rackspace/papi/components/reverseproxy/basicauth/ReverseProxyBasicAuthHandler.java
@@ -1,39 +1,87 @@
 package com.rackspace.papi.components.reverseproxy.basicauth;
 
+import com.rackspace.papi.commons.util.StringUtilities;
+import com.rackspace.papi.commons.util.http.CommonHttpHeader;
+import com.rackspace.papi.commons.util.http.HttpStatusCode;
 import com.rackspace.papi.commons.util.servlet.http.ReadableHttpServletResponse;
 import com.rackspace.papi.filter.logic.common.AbstractFilterLogicHandler;
 import com.rackspace.papi.filter.logic.FilterAction;
 import com.rackspace.papi.filter.logic.FilterDirector;
 import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import java.io.UnsupportedEncodingException;
+import org.slf4j.Logger;
 import org.apache.commons.codec.binary.Base64;
 import javax.servlet.http.HttpServletRequest;
 
 public class ReverseProxyBasicAuthHandler extends AbstractFilterLogicHandler {
 
-   private final ReverseProxyBasicAuthConfig config;
-   public static final String AUTH_HEADER = "Authorization";
+    private final ReverseProxyBasicAuthConfig config;
+    public static final String AUTH_HEADER = "Authorization";
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ReverseProxyBasicAuthHandler.class);
+    public ReverseProxyBasicAuthHandler(ReverseProxyBasicAuthConfig config) {
+        this.config = config;
 
+    }
 
-   public ReverseProxyBasicAuthHandler(ReverseProxyBasicAuthConfig config) {
-      this.config = config;
-     
-   }
+    @Override
+    public FilterDirector handleRequest(HttpServletRequest request, ReadableHttpServletResponse response) {
 
-   @Override
-   public FilterDirector handleRequest(HttpServletRequest request, ReadableHttpServletResponse response) {
+        final FilterDirector filterDirector = new FilterDirectorImpl();
+        filterDirector.setFilterAction(FilterAction.PROCESS_RESPONSE);
+        
+        if(config.getCredentials() == null){
+            LOG.warn("Unable to locate config file for Reverse Proxy Basic Auth Filter");
+            return filterDirector;
+        }
 
-      final FilterDirector filterDirector = new FilterDirectorImpl();
-      filterDirector.setFilterAction(FilterAction.PASS);
-      
-      StringBuilder preHash = new StringBuilder(config.getCredentials().getUsername());
-      StringBuilder postHash = new StringBuilder("Basic ");
-      
-      preHash.append(":").append(config.getCredentials().getPassword());
-      String hash = Base64.encodeBase64String(preHash.toString().getBytes()).trim();
-      postHash.append(hash);
-      
-      filterDirector.requestHeaderManager().putHeader(AUTH_HEADER, postHash.toString());
-      return filterDirector;
-   }
+        StringBuilder preHash = new StringBuilder(config.getCredentials().getUsername());
+        StringBuilder postHash = new StringBuilder("Basic ");
+        preHash.append(":").append(config.getCredentials().getPassword());
+        try{
+            postHash.append(Base64.encodeBase64String(preHash.toString().getBytes("UTF-8")).trim());
+        }catch(UnsupportedEncodingException e){
+            LOG.error("Failed to update basic credentials. Reason: " + e.getMessage(), e);
+        }
+        
+        filterDirector.requestHeaderManager().putHeader(AUTH_HEADER, postHash.toString());
+        return filterDirector;
+    }
 
+    @Override
+    public FilterDirector handleResponse(HttpServletRequest request, ReadableHttpServletResponse response) {
+
+        FilterDirectorImpl myDirector = new FilterDirectorImpl();
+        final String wwwAuthenticateHeader = response.getHeader(CommonHttpHeader.WWW_AUTHENTICATE.toString());
+
+        myDirector.setResponseStatus(HttpStatusCode.fromInt(response.getStatus()));
+        myDirector.setFilterAction(FilterAction.RETURN);
+
+        switch (HttpStatusCode.fromInt(response.getStatus())) {
+            case NOT_IMPLEMENTED:
+                //If the service does not support delegation, return a 500
+                LOG.warn("Origin service has not implemented delegation");
+                myDirector.setResponseStatus(HttpStatusCode.INTERNAL_SERVER_ERROR);
+
+                //Remove the WWW-Authenticate header if present
+                if (!StringUtilities.isBlank(wwwAuthenticateHeader)) {
+                    myDirector.responseHeaderManager().removeHeader(CommonHttpHeader.WWW_AUTHENTICATE.toString());
+                }
+                break;
+
+            case FORBIDDEN:
+                //If the WWW-Authenticate header is not present or it is not set to delegated then relay a 500
+                LOG.warn("Failed to authentication against the Origin Service");
+                if (StringUtilities.isBlank(wwwAuthenticateHeader) || !wwwAuthenticateHeader.equalsIgnoreCase("delegated")) {
+                    myDirector.setResponseStatus(HttpStatusCode.INTERNAL_SERVER_ERROR);
+                } else {
+                    //Remove the header
+                    myDirector.responseHeaderManager().removeHeader(CommonHttpHeader.WWW_AUTHENTICATE.toString());
+
+                }
+
+                break;
+        }
+
+        return myDirector;
+    }
 }
--- project-set/components/reverse-proxy-basic-auth/src/test/java/com/rackspace/papi/components/reverseproxy/basicauth/ReverseProxyBasicAuthHandlerTest.java
+++ project-set/components/reverse-proxy-basic-auth/src/test/java/com/rackspace/papi/components/reverseproxy/basicauth/ReverseProxyBasicAuthHandlerTest.java
@@ -4,12 +4,15 @@
  */
 package com.rackspace.papi.components.reverseproxy.basicauth;
 
+import com.rackspace.papi.commons.util.http.CommonHttpHeader;
+import com.rackspace.papi.commons.util.http.HttpStatusCode;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import com.rackspace.papi.commons.util.servlet.http.ReadableHttpServletResponse;
 import com.rackspace.papi.filter.logic.FilterDirector;
 import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -34,6 +37,7 @@ public class ReverseProxyBasicAuthHandlerTest {
         private final String password = "open sesame";
         private final String authHash = "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==";
         private HttpServletRequest httpServletRequest;
+        private ReadableHttpServletResponse httpServletResponse;
 
         @Before
         public void setUp() {
@@ -48,15 +52,64 @@ public class ReverseProxyBasicAuthHandlerTest {
             handler = new ReverseProxyBasicAuthHandler(basicAuthConfig);
 
             httpServletRequest = mock(HttpServletRequest.class);
+            httpServletResponse = mock(ReadableHttpServletResponse.class);
         }
 
         @Test
         public void shouldSetAuthCredentialHeader() {
             FilterDirector director = new FilterDirectorImpl();
-            
+
             director = handler.handleRequest(httpServletRequest, null);
-            
+
             assertTrue(director.requestHeaderManager().headersToAdd().get("authorization").contains(authHash));
+        }
+
+        @Test
+        public void shouldReturn500On501() {
+
+            FilterDirector director = new FilterDirectorImpl();
+
+            when(httpServletResponse.getStatus()).thenReturn(HttpStatusCode.NOT_IMPLEMENTED.intValue());
+            director = handler.handleResponse(httpServletRequest, httpServletResponse);
+
+            assertEquals(director.getResponseStatus(), HttpStatusCode.INTERNAL_SERVER_ERROR);
+        }
+
+        @Test
+        public void shouldReturn500On403() {
+
+            FilterDirector director = new FilterDirectorImpl();
+
+            when(httpServletResponse.getStatus()).thenReturn(HttpStatusCode.FORBIDDEN.intValue());
+            director = handler.handleResponse(httpServletRequest, httpServletResponse);
+
+            assertEquals(director.getResponseStatus(), HttpStatusCode.INTERNAL_SERVER_ERROR);
+        }
+
+        @Test
+        public void shouldSetAuthenticateHeaderToRemove() {
+            FilterDirector director = new FilterDirectorImpl();
+
+            when(httpServletResponse.getStatus()).thenReturn(HttpStatusCode.FORBIDDEN.intValue());
+            when(httpServletResponse.getHeader(CommonHttpHeader.WWW_AUTHENTICATE.toString())).thenReturn("delegated");
+            director = handler.handleResponse(httpServletRequest, httpServletResponse);
+
+            assertTrue(director.responseHeaderManager().headersToRemove().contains(CommonHttpHeader.WWW_AUTHENTICATE.toString()));
+            assertEquals(director.getResponseStatus(), HttpStatusCode.FORBIDDEN);
+        }
+
+        @Test
+        public void shouldSetAuthenticateHeaderToRemoveOn501() {
+
+            FilterDirector director = new FilterDirectorImpl();
+
+            when(httpServletResponse.getStatus()).thenReturn(HttpStatusCode.NOT_IMPLEMENTED.intValue());
+            when(httpServletResponse.getHeader(CommonHttpHeader.WWW_AUTHENTICATE.toString())).thenReturn("delegated");
+            director = handler.handleResponse(httpServletRequest, httpServletResponse);
+            
+
+            assertTrue(director.responseHeaderManager().headersToRemove().contains(CommonHttpHeader.WWW_AUTHENTICATE.toString()));
+            assertEquals(director.getResponseStatus(), HttpStatusCode.INTERNAL_SERVER_ERROR);
         }
     }
 }
