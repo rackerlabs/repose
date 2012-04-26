3f36491444adc8814fa04a87e5ef8aa364d75c33
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2012-04-26 09:00:20 -0500

    Add xsd's and example configs for the uri and header normalization filters; setup the maven packages for the normalization filters; remove configuration updater package that is not being used.  Add content identity 1.1 unit test to give an idea of what needs to change to call content identity 1.1 complete.

---
+++ project-set/components/header-normalization/src/main/java/com/rackspace/papi/components/hnorm/HeaderNormalizationFilter.java
@@ -0,0 +1,34 @@
+package com.rackspace.papi.components.hnorm;
+
+import com.rackspace.papi.filter.logic.impl.FilterLogicHandlerDelegate;
+import com.rackspace.papi.service.config.ConfigurationService;
+import com.rackspace.papi.service.context.jndi.ServletContextHelper;
+import com.rackspacecloud.api.docs.powerapi.header_normalization.v1.HeaderNormalizationConfig;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class HeaderNormalizationFilter implements Filter {
+
+   private HeaderNormalizationHandlerFactory handlerFactory;
+    private ConfigurationService configurationManager;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        new FilterLogicHandlerDelegate(request, response, chain).doFilter(handlerFactory.newHandler());
+    }
+
+    @Override
+    public void destroy() {
+        configurationManager.unsubscribeFrom("header-normalization.cfg.xml", handlerFactory);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        configurationManager = ServletContextHelper.getPowerApiContext(filterConfig.getServletContext()).configurationService();
+        handlerFactory = new HeaderNormalizationHandlerFactory();
+
+        configurationManager.subscribeTo("header-normalization.cfg.xml", handlerFactory, HeaderNormalizationConfig.class);
+    }
+}
