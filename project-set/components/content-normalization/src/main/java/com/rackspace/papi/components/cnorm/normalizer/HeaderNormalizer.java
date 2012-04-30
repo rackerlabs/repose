5232a1b994ec402d8362a39c1622cd44927de9a5
Dan Daley <danial.daley@rackspace.com>
2012-04-30 09:35:28 -0500

    Add content-normalizer back in for now

---
+++ project-set/components/content-normalization/src/main/java/com/rackspace/papi/components/cnorm/normalizer/HeaderNormalizer.java
@@ -0,0 +1,50 @@
+package com.rackspace.papi.components.cnorm.normalizer;
+
+import com.rackspace.papi.commons.util.StringUtilities;
+import com.rackspace.papi.components.normalization.config.HeaderFilterList;
+import com.rackspace.papi.components.normalization.config.HttpHeader;
+import com.rackspace.papi.filter.logic.FilterDirector;
+import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import java.util.Enumeration;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author jhopper
+ */
+public class HeaderNormalizer {
+
+    private final List<HttpHeader> headers;
+    private final boolean isBlacklist;
+
+    public HeaderNormalizer(HeaderFilterList headerFilterList, boolean isBlacklist) {
+        this.headers = isBlacklist ? headerFilterList.getBlacklist().getHeader() : headerFilterList.getWhitelist().getHeader();
+        this.isBlacklist = isBlacklist;
+    }
+
+    public void normalizeHeaders(HttpServletRequest request, FilterDirector currentDirector) {
+        filterHeaders(request, currentDirector);
+    }
+
+    private void filterHeaders(HttpServletRequest request, FilterDirector myDirector) {
+        final Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            final String requestHeaderName = headerNames.nextElement();
+
+            boolean found = false;
+            
+            for (HttpHeader configuredHeader : headers) {
+                if (configuredHeader.getId().equalsIgnoreCase(requestHeaderName)) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (found && isBlacklist || !found && !isBlacklist) {
+                myDirector.requestHeaderManager().removeHeader(requestHeaderName);
+            }
+        }
+    }
+}
