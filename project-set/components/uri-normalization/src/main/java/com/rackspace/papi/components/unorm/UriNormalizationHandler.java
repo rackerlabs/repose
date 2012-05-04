861e2f329a5ff1175f0c4b5ecca24545ed046faa
John Hopper <john.hopper@rackspace.com>
2012-05-03 23:30:52 -0500

    finishing implementation work on http method scoping. added updated chef configurations

--- project-set/components/uri-normalization/src/main/java/com/rackspace/papi/components/unorm/UriNormalizationHandler.java
+++ project-set/components/uri-normalization/src/main/java/com/rackspace/papi/components/unorm/UriNormalizationHandler.java
@@ -1,14 +1,12 @@
 package com.rackspace.papi.components.unorm;
 
-import com.rackspace.papi.commons.util.http.normal.Normalizer;
-import com.rackspace.papi.commons.util.regex.RegexSelector;
-import com.rackspace.papi.commons.util.regex.SelectorResult;
 import com.rackspace.papi.commons.util.servlet.http.ReadableHttpServletResponse;
 import com.rackspace.papi.components.unorm.normalizer.MediaTypeNormalizer;
 import com.rackspace.papi.filter.logic.common.AbstractFilterLogicHandler;
 import com.rackspace.papi.filter.logic.FilterAction;
 import com.rackspace.papi.filter.logic.FilterDirector;
 import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import java.util.Collection;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -17,10 +15,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class UriNormalizationHandler extends AbstractFilterLogicHandler {
 
-   private final RegexSelector<Normalizer<String>> queryStringNormalizers;
+   private final Collection<QueryParameterNormalizer> queryStringNormalizers;
    private final MediaTypeNormalizer mediaTypeNormalizer;
 
-   public UriNormalizationHandler(RegexSelector<Normalizer<String>> queryStringNormalizers, MediaTypeNormalizer mediaTypeNormalizer) {
+   public UriNormalizationHandler(Collection<QueryParameterNormalizer> queryStringNormalizers, MediaTypeNormalizer mediaTypeNormalizer) {
       this.queryStringNormalizers = queryStringNormalizers;
       this.mediaTypeNormalizer = mediaTypeNormalizer;
    }
@@ -31,19 +29,18 @@ public class UriNormalizationHandler extends AbstractFilterLogicHandler {
       myDirector.setFilterAction(FilterAction.PASS);
 
       mediaTypeNormalizer.normalizeContentMediaType(request, myDirector);
-      
-      // TODO: Refactor this into a object like the above method call.
+
+      // TODO: Refactor this into a object like the above method call?
       normalizeUriQuery(request, myDirector);
 
       return myDirector;
    }
 
    private void normalizeUriQuery(HttpServletRequest request, FilterDirector myDirector) {
-      final SelectorResult<Normalizer<String>> selectedQueryStringNormalizer = queryStringNormalizers.select(request.getRequestURI());
-
-      if (selectedQueryStringNormalizer.hasKey()) {
-         final Normalizer<String> queryStringNormalizer = selectedQueryStringNormalizer.getKey();
-         myDirector.setRequestUriQuery(queryStringNormalizer.normalize(request.getQueryString()));
+      for (QueryParameterNormalizer queryParameterNormalizer : queryStringNormalizers) {
+         if (queryParameterNormalizer.normalize(request, myDirector)) {
+            break;
+         }
       }
    }
 }
