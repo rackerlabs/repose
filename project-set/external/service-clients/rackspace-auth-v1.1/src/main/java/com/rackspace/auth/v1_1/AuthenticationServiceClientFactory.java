dced7bd9ca10bd6f8cf842b2dbe21df8080d6ff2
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2012-05-16 16:32:37 -0500

    TK34792: Start refactoring auth service clients:
    1.  Refactor service client, move it into commons, and have both auth service clients use it.

--- project-set/external/service-clients/rackspace-auth-v1.1/src/main/java/com/rackspace/auth/v1_1/AuthenticationServiceClientFactory.java
+++ project-set/external/service-clients/rackspace-auth-v1.1/src/main/java/com/rackspace/auth/v1_1/AuthenticationServiceClientFactory.java
@@ -1,11 +1,15 @@
 package com.rackspace.auth.v1_1;
 
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.rackspace.papi.commons.util.http.ServiceClient;
+
 /**
  * @author fran
  */
 public class AuthenticationServiceClientFactory {
-    public AuthenticationServiceClient buildAuthServiceClient(String targetHostUri, String username, String password) {
-        
-        return new AuthenticationServiceClient(targetHostUri, new ResponseUnmarshaller(), new ServiceClient(username, password));
-    }
+   public AuthenticationServiceClient buildAuthServiceClient(String targetHostUri, String username, String password) {
+      HTTPBasicAuthFilter authFilter = new HTTPBasicAuthFilter(username, password);
+
+      return new AuthenticationServiceClient(targetHostUri, new ResponseUnmarshaller(), new ServiceClient(authFilter));
+   }
 }
