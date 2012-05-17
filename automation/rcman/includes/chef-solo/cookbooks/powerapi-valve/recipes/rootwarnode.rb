685c04e104d831a30588a53a2aae1530687682ae
Meynard Alconis <meynard.alconis@rackspace.com>
2012-05-17 13:26:37 -0500

    Updated regression node build to go to new path for repose-valve deb packages

--- automation/rcman/includes/chef-solo/cookbooks/powerapi-valve/recipes/rootwarnode.rb
+++ automation/rcman/includes/chef-solo/cookbooks/powerapi-valve/recipes/rootwarnode.rb
@@ -9,7 +9,7 @@ end
 script "getVersionAndBuildNumbers" do
    interpreter "ruby"
    `rm -rf /var/lib/tomcat7/webapps/*`
-   latestUrl = "http://maven.research.rackspacecloud.com/content/repositories/snapshots/com/rackspace/repose/installation/deb/war/repose-valve/maven-metadata.xml"
+   latestUrl = "http://maven.research.rackspacecloud.com/content/repositories/snapshots/com/rackspace/repose/installation/deb/valve/repose-valve/maven-metadata.xml"
    latest = `wget -qO- '#{latestUrl}'`.split(/<\/?version>/).select{|v| v=~ /SNAPSHOT/}.shift
 
 
