ffefdc214dde3fd27d4471fe44173d56356a0cfb
Dan Daley <danial.daley@rackspace.com>
2012-05-31 09:22:06 -0500

    Remove old http client code that is no longer used

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/http/proxy/httpclient/ProcessableRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rackspace.papi.http.proxy.httpclient;
-
-import org.apache.commons.httpclient.HttpMethod;
-
-import java.io.IOException;
-
-public interface ProcessableRequest {
-    HttpMethod process(HttpRequestProcessor processor) throws IOException;
-}
--- project-set/core/core-lib/src/main/java/com/rackspace/papi/http/proxy/httpcomponent/HttpComponentProcessableRequest.java
+++
@@ -1,9 +0,0 @@
-package com.rackspace.papi.http.proxy.httpcomponent;
-
-import org.apache.http.client.methods.HttpRequestBase;
-
-import java.io.IOException;
-
-public interface HttpComponentProcessableRequest {
-    HttpRequestBase process(HttpComponentRequestProcessor processor) throws IOException;
-}
