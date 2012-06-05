2bd2ace91c6f6a073de34703d91a228365cc6835
Meynard Alconis <meynard.alconis@rackspace.com>
2012-06-05 12:01:51 -0500

    Openstack Auth Component: Added handling for case when user is returned with no roles

--- project-set/external/service-clients/auth/src/main/java/com/rackspace/auth/openstack/OpenStackToken.java
+++ project-set/external/service-clients/auth/src/main/java/com/rackspace/auth/openstack/OpenStackToken.java
@@ -1,6 +1,7 @@
 package com.rackspace.auth.openstack;
 
 import com.rackspace.auth.AuthToken;
+import com.rackspace.papi.commons.util.StringUtilities;
 import org.openstack.docs.identity.api.v2.AuthenticateResponse;
 import org.openstack.docs.identity.api.v2.Role;
 
@@ -10,70 +11,72 @@ import java.io.Serializable;
  * @author fran
  */
 public class OpenStackToken extends AuthToken implements Serializable {
-   private final String tenantId;
-   private final long expires;
-   private final String roles;
 
-   private final String tokenId;
-   private final String userId;
-   private final String username;
+    private final String tenantId;
+    private final long expires;
+    private final String roles;
+    private final String tokenId;
+    private final String userId;
+    private final String username;
 
-   public OpenStackToken(String tenantId, AuthenticateResponse response) {
-      if (response == null || response.getToken() == null || response.getToken().getExpires() == null ) {
-         throw new IllegalArgumentException("Invalid token");
-      }
+    public OpenStackToken(String tenantId, AuthenticateResponse response) {
+        if (response == null || response.getToken() == null || response.getToken().getExpires() == null) {
+            throw new IllegalArgumentException("Invalid token");
+        }
 
-      this.tenantId = tenantId;
-      this.expires = response.getToken().getExpires().toGregorianCalendar().getTimeInMillis();
-      this.roles = formatRoles(response);
-      this.tokenId = response.getToken().getId();
-      this.userId = response.getUser().getId();
-      this.username = response.getUser().getName(); 
-   }
+        this.tenantId = tenantId;
+        this.expires = response.getToken().getExpires().toGregorianCalendar().getTimeInMillis();
+        this.roles = formatRoles(response);
+        this.tokenId = response.getToken().getId();
+        this.userId = response.getUser().getId();
+        this.username = response.getUser().getName();
+    }
 
-   @Override
-   public String getTenantId() {
-      return tenantId;
-   }
+    @Override
+    public String getTenantId() {
+        return tenantId;
+    }
 
-   @Override
-   public String getUserId() {
-      return userId;
-   }
+    @Override
+    public String getUserId() {
+        return userId;
+    }
 
-   @Override
-   public String getTokenId() {
-      return tokenId;
-   }
+    @Override
+    public String getTokenId() {
+        return tokenId;
+    }
 
-   @Override
-   public long getExpires() {
-      return expires;
-   }
+    @Override
+    public long getExpires() {
+        return expires;
+    }
 
-   @Override
-   public String getUsername() {
-      return username;
-   }
+    @Override
+    public String getUsername() {
+        return username;
+    }
 
-   @Override
-   public String getRoles() {
-      return roles;
-   }
+    @Override
+    public String getRoles() {
+        return roles;
+    }
 
-   private String formatRoles(AuthenticateResponse response) {
-      String formattedRoles = null;
+    private String formatRoles(AuthenticateResponse response) {
+        String formattedRoles = null;
 
-      if (response.getUser() != null && response.getUser().getRoles() != null) {
-         StringBuilder result = new StringBuilder();
-         for (Role role : response.getUser().getRoles().getRole()) {
-            result.append(role.getName());
-            result.append(",");
-         }
+        if (response.getUser() != null && response.getUser().getRoles() != null) {
+            StringBuilder result = new StringBuilder();
+            for (Role role : response.getUser().getRoles().getRole()) {
+                result.append(role.getName());
+                result.append(",");
+            }
 
-         formattedRoles = result.substring(0, result.length() - 1);
-      }
+            if (!StringUtilities.isBlank(result.toString())) {
+                formattedRoles = result.substring(0, result.length() - 1);
+            }
+        }
 
-      return formattedRoles;
-   }
+        return formattedRoles;
+    }
 }
--- project-set/external/service-clients/auth/src/test/java/com/rackspace/auth/openstack/OpenStackTokenTest.java
+++ project-set/external/service-clients/auth/src/test/java/com/rackspace/auth/openstack/OpenStackTokenTest.java
@@ -19,162 +19,175 @@ import static org.junit.Assert.*;
  */
 @RunWith(Enclosed.class)
 public class OpenStackTokenTest {
-   public static class WhenGettingTokenTtl {
 
-      private DatatypeFactory dataTypeFactory;
-      private UserForAuthenticateResponse user;
-      private AuthenticateResponse response;
-      private Token token;
+    public static class WhenGettingTokenTtl {
 
-      @Before
-      public void setup() throws DatatypeConfigurationException {
-         dataTypeFactory = DatatypeFactory.newInstance();
+        private DatatypeFactory dataTypeFactory;
+        private UserForAuthenticateResponse user;
+        private AuthenticateResponse response;
+        private Token token;
 
-         user = new UserForAuthenticateResponse();
-         user.setId("104772");
-         user.setName("user2");
+        @Before
+        public void setup() throws DatatypeConfigurationException {
+            dataTypeFactory = DatatypeFactory.newInstance();
 
-         token = new Token();
-         response = new AuthenticateResponse();
-         response.setToken(token);
-         response.setUser(user);
-      }
+            user = new UserForAuthenticateResponse();
+            user.setId("104772");
+            user.setName("user2");
 
-      private Calendar getCalendarWithOffset(int millis) {
-         return getCalendarWithOffset(Calendar.MILLISECOND, millis);
-      }
+            token = new Token();
+            response = new AuthenticateResponse();
+            response.setToken(token);
+            response.setUser(user);
+        }
 
-      private Calendar getCalendarWithOffset(int field, int millis) {
-         Calendar cal = GregorianCalendar.getInstance();
+        private Calendar getCalendarWithOffset(int millis) {
+            return getCalendarWithOffset(Calendar.MILLISECOND, millis);
+        }
 
-         cal.add(field, millis);
+        private Calendar getCalendarWithOffset(int field, int millis) {
+            Calendar cal = GregorianCalendar.getInstance();
 
-         return cal;
-      }
+            cal.add(field, millis);
 
-      @Test
-      public void shouldHavePositiveTtlForFutureExpires() {
-         Calendar expires = getCalendarWithOffset(1000);
+            return cal;
+        }
 
-         token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
+        @Test
+        public void shouldHavePositiveTtlForFutureExpires() {
+            Calendar expires = getCalendarWithOffset(1000);
 
-         AuthToken info = new OpenStackToken(null, response);
+            token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
 
-         assertEquals("Expires Calendars should be equivalent", expires.getTimeInMillis(), info.getExpires());
-         assertTrue("Ttl should be positive", info.tokenTtl() > 0);
-      }
+            AuthToken info = new OpenStackToken(null, response);
 
-      @Test
-      public void shouldHaveMaxIntTtlForVeryLargeExpires() {
-         Calendar expires = getCalendarWithOffset(Calendar.MINUTE, Integer.MAX_VALUE);
+            assertEquals("Expires Calendars should be equivalent", expires.getTimeInMillis(), info.getExpires());
+            assertTrue("Ttl should be positive", info.tokenTtl() > 0);
+        }
 
-         token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
+        @Test
+        public void shouldHaveMaxIntTtlForVeryLargeExpires() {
+            Calendar expires = getCalendarWithOffset(Calendar.MINUTE, Integer.MAX_VALUE);
 
-         AuthToken info = new OpenStackToken(null, response);
+            token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
 
-         assertTrue("Raw Ttl should be actual large value", info.tokenTtl() > Integer.MAX_VALUE);
-         assertEquals("Safe Ttl should be max integer", Integer.MAX_VALUE, info.safeTokenTtl());
-      }
+            AuthToken info = new OpenStackToken(null, response);
 
-      @Test (expected=IllegalArgumentException.class)
-      public void shouldHaveZeroTtlForNullExpires() throws DatatypeConfigurationException {
-         token.setExpires(null);
-         Token token = new Token();
-         token.setExpires(null);
-         response.setToken(token);
+            assertTrue("Raw Ttl should be actual large value", info.tokenTtl() > Integer.MAX_VALUE);
+            assertEquals("Safe Ttl should be max integer", Integer.MAX_VALUE, info.safeTokenTtl());
+        }
 
-         AuthToken info = new OpenStackToken(null, response);
-      }
+        @Test(expected = IllegalArgumentException.class)
+        public void shouldHaveZeroTtlForNullExpires() throws DatatypeConfigurationException {
+            token.setExpires(null);
+            Token token = new Token();
+            token.setExpires(null);
+            response.setToken(token);
 
+            AuthToken info = new OpenStackToken(null, response);
+        }
 
-      @Test
-      public void shouldHaveZeroTtlForPastExpires() throws InterruptedException {
-         Calendar expires = getCalendarWithOffset(1);
+        @Test
+        public void shouldHaveZeroTtlForPastExpires() throws InterruptedException {
+            Calendar expires = getCalendarWithOffset(1);
 
-         token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
+            token.setExpires(dataTypeFactory.newXMLGregorianCalendar((GregorianCalendar) expires));
 
-         AuthToken info = new OpenStackToken(null, response);
+            AuthToken info = new OpenStackToken(null, response);
 
-         // Sleep until we have passed the expiration date.
-         Thread.sleep(5);
+            // Sleep until we have passed the expiration date.
+            Thread.sleep(5);
 
-         assertEquals("Ttl should be zero", new Long(0), info.tokenTtl());
-         assertEquals("Safe Ttl should be zero", 0, info.safeTokenTtl());
-      }
+            assertEquals("Ttl should be zero", new Long(0), info.tokenTtl());
+            assertEquals("Safe Ttl should be zero", 0, info.safeTokenTtl());
+        }
+    }
 
+    public static class WhenFormattingRoles {
 
-   }
+        private AuthenticateResponse response;
+        private UserForAuthenticateResponse user;
 
-   public static class WhenFormattingRoles {
+        @Before
+        public void setup() {
+            response = new AuthenticateResponse();
 
-      private AuthenticateResponse response;
-      private UserForAuthenticateResponse user;
+            Token token = new Token();
+            token.setId("518f323d-505a-4475-9cba-bc43cd1790-A");
+            response.setToken(token);
 
-      @Before
-      public void setup() {
-         response = new AuthenticateResponse();
+            user = new UserForAuthenticateResponse();
+            user.setId("104772");
+            user.setName("user2");
+        }
 
-         Token token = new Token();
-         token.setId("518f323d-505a-4475-9cba-bc43cd1790-A");
-         response.setToken(token);
+        @Test
+        public void shouldFormatListWithSingleRole() throws DatatypeConfigurationException {
+            RoleList roleList = new RoleList();
+            Role role = new Role();
+            role.setName("default role 1");
+            roleList.getRole().add(role);
+            user.setRoles(roleList);
+            response.setUser(user);
+            Token token = new Token();
+            token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
+            response.setToken(token);
 
-         user = new UserForAuthenticateResponse();
-         user.setId("104772");
-         user.setName("user2");
-      }
+            AuthToken info = new OpenStackToken(null, response);
 
-      @Test
-      public void shouldFormatListWithSingleRole() throws DatatypeConfigurationException {
-         RoleList roleList = new RoleList();
-         Role role = new Role();
-         role.setName("default role 1");
-         roleList.getRole().add(role);
-         user.setRoles(roleList);
-         response.setUser(user);
-         Token token = new Token();
-         token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
-         response.setToken(token);
+            final String expected = "default role 1";
+            assertEquals(expected, info.getRoles());
+        }
 
-         AuthToken info = new OpenStackToken(null, response);
+        @Test
+        public void shouldFormatListWithMultipleRoles() throws DatatypeConfigurationException {
+            RoleList roleList = new RoleList();
 
-         final String expected = "default role 1";
-         assertEquals(expected, info.getRoles());
-      }
+            Role roleOne = new Role();
+            roleOne.setName("default role 1");
 
-      @Test
-      public void shouldFormatListWithMultipleRoles() throws DatatypeConfigurationException {
-         RoleList roleList = new RoleList();
+            Role roleTwo = new Role();
+            roleTwo.setName("default role 2");
 
-         Role roleOne = new Role();
-         roleOne.setName("default role 1");
+            roleList.getRole().add(roleOne);
+            roleList.getRole().add(roleTwo);
+            user.setRoles(roleList);
+            response.setUser(user);
+            Token token = new Token();
+            token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
+            response.setToken(token);
 
-         Role roleTwo = new Role();
-         roleTwo.setName("default role 2");
+            AuthToken info = new OpenStackToken(null, response);
 
-         roleList.getRole().add(roleOne);
-         roleList.getRole().add(roleTwo);
-         user.setRoles(roleList);
-         response.setUser(user);
-         Token token = new Token();
-         token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
-         response.setToken(token);
+            final String expected = "default role 1,default role 2";
+            assertEquals(expected, info.getRoles());
+        }
 
-         AuthToken info = new OpenStackToken(null, response);
+        @Test
+        public void shouldFormatListWithNullRoles() throws DatatypeConfigurationException {
+            response.setUser(user);
+            Token token = new Token();
+            token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
+            response.setToken(token);
 
-         final String expected = "default role 1,default role 2";
-         assertEquals(expected, info.getRoles());
-      }
+            AuthToken info = new OpenStackToken(null, response);
 
-      @Test
-      public void shouldFormatListWithNoRoles() throws DatatypeConfigurationException {
-         response.setUser(user);
-         Token token = new Token();
-         token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
-         response.setToken(token);
+            assertNull(info.getRoles());
+        }
 
-         AuthToken info = new OpenStackToken(null, response);
+        @Test
+        public void shouldFormatListWithNoRoles() throws DatatypeConfigurationException {
 
-         assertNull(info.getRoles());
-      }
-   }
+            RoleList roleList = new RoleList();
+            user.setRoles(roleList);
+            response.setUser(user);
+            Token token = new Token();
+            token.setExpires(DatatypeFactory.newInstance().newXMLGregorianCalendar());
+            response.setToken(token);
+
+            AuthToken info = new OpenStackToken(null, response);
+
+            assertNull(info.getRoles());
+        }
+    }
 }
