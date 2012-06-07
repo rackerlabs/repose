cd7c85b900eb24f0bebed0f72a44988205734fd1
Fran Torres-Lopez <fran@fran-torres-lopezs-macbook-pro.local>
2012-06-07 16:31:51 -0500

    Inject our own HttpURLConnection so that we can override the getErrorStream method and make sure the response body gets returned to the client correctly on a redirect.

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/service/proxy/jersey/RequestProxyServiceImpl.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/service/proxy/jersey/RequestProxyServiceImpl.java
@@ -8,6 +8,7 @@ import com.rackspace.papi.service.proxy.RequestProxyService;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -16,6 +17,8 @@ import java.net.URL;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sun.jersey.client.urlconnection.URLConnectionClientHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -23,174 +26,175 @@ import org.springframework.stereotype.Component;
 @Component("requestProxyService")
 public class RequestProxyServiceImpl implements RequestProxyService {
 
-    private static final Integer DEFAULT_THREADPOOL_SIZE = 20;
-    private static final Logger LOG = LoggerFactory.getLogger(RequestProxyServiceImpl.class);
-    private ClientWrapper client;
-    private Integer connectionTimeout = Integer.valueOf(0);
-    private Integer readTimeout = Integer.valueOf(0);
-    private Integer proxyThreadPool = Integer.valueOf(DEFAULT_THREADPOOL_SIZE);
-    private final Object clientLock = new Object();
-    private boolean requestLogging;
+   private static final Integer DEFAULT_THREADPOOL_SIZE = 20;
+   private static final Logger LOG = LoggerFactory.getLogger(RequestProxyServiceImpl.class);
+   private ClientWrapper client;
+   private Integer connectionTimeout = Integer.valueOf(0);
+   private Integer readTimeout = Integer.valueOf(0);
+   private Integer proxyThreadPool = Integer.valueOf(DEFAULT_THREADPOOL_SIZE);
+   private final Object clientLock = new Object();
+   private boolean requestLogging;
 
-    public RequestProxyServiceImpl() {
-    }
+   public RequestProxyServiceImpl() {
+   }
 
-    @Override
-    public void setTimeouts(Integer connectionTimeout, Integer readTimeout, Integer proxyThreadPool, boolean requestLogging) {
-        LOG.info("Connection and/or read timeouts changed to: " + connectionTimeout + "/" + readTimeout);
-        this.connectionTimeout = connectionTimeout;
-        this.readTimeout = readTimeout;
-        this.proxyThreadPool = proxyThreadPool;
-        this.requestLogging = requestLogging;
+   @Override
+   public void setTimeouts(Integer connectionTimeout, Integer readTimeout, Integer proxyThreadPool, boolean requestLogging) {
+      LOG.info("Connection and/or read timeouts changed to: " + connectionTimeout + "/" + readTimeout);
+      this.connectionTimeout = connectionTimeout;
+      this.readTimeout = readTimeout;
+      this.proxyThreadPool = proxyThreadPool;
+      this.requestLogging = requestLogging;
 
-        // Invalidate client
-        synchronized (clientLock) {
-            client = null;
-        }
-    }
+      // Invalidate client
+      synchronized (clientLock) {
+         client = null;
+      }
+   }
 
-    private static class TargetHostInfo {
+   private static class TargetHostInfo {
 
-        private final URI proxiedHostUri;
-        private final URL proxiedHostUrl;
+      private final URI proxiedHostUri;
+      private final URL proxiedHostUrl;
 
-        public TargetHostInfo(String targetHost) {
-            URI targetUri = null;
+      public TargetHostInfo(String targetHost) {
+         URI targetUri = null;
 
-            try {
-                targetUri = new URI(targetHost);
-            } catch (URISyntaxException e) {
-                LOG.error("Invalid target host url: " + targetHost, e);
+         try {
+            targetUri = new URI(targetHost);
+         } catch (URISyntaxException e) {
+            LOG.error("Invalid target host url: " + targetHost, e);
+         }
+
+         proxiedHostUri = targetUri;
+         proxiedHostUrl = asUri(proxiedHostUri);
+
+      }
+
+      private URL asUri(URI host) {
+         try {
+            return new URL(host.getScheme(), host.getHost(), host.getPort(), "");
+         } catch (MalformedURLException ex) {
+            LOG.error("Invalid host url: " + host, ex);
+         }
+         return null;
+      }
+
+      public URI getProxiedHostUri() {
+         return proxiedHostUri;
+      }
+
+      public URL getProxiedHostUrl() {
+         return proxiedHostUrl;
+      }
+   }
+
+   private ClientWrapper getClient() {
+      synchronized (clientLock) {
+         if (client == null) {
+            JerseyPropertiesConfigurator jerseyPropertiesConfigurator = new JerseyPropertiesConfigurator(connectionTimeout, readTimeout, proxyThreadPool, true);
+
+            URLConnectionClientHandler urlConnectionClientHandler = new URLConnectionClientHandler(new ReposeHttpUrlConnectionFactory());
+            client = new ClientWrapper(new Client(urlConnectionClientHandler, jerseyPropertiesConfigurator.configure()));
+
+            if (requestLogging) {
+               LOG.warn("Enabling info logging of jersey client requests");
+               client.getClient().addFilter(new LoggingFilter());
+            } else {
+               LOG.warn("**** Jersey client request logging not enabled *****");
             }
+         }
 
-            proxiedHostUri = targetUri;
-            proxiedHostUrl = asUri(proxiedHostUri);
+         return client;
+      }
+   }
 
-        }
+   @Override
+   public int proxyRequest(String targetHost, HttpServletRequest request, HttpServletResponse response) throws IOException {
+      TargetHostInfo host = new TargetHostInfo(targetHost);
+      final String target = host.getProxiedHostUrl().toExternalForm() + request.getRequestURI();
 
-        private URL asUri(URI host) {
-            try {
-                return new URL(host.getScheme(), host.getHost(), host.getPort(), "");
-            } catch (MalformedURLException ex) {
-                LOG.error("Invalid host url: " + host, ex);
-            }
-            return null;
-        }
+      JerseyRequestProcessor processor = new JerseyRequestProcessor(request, host.getProxiedHostUri());
+      WebResource resource = getClient().resource(target);
+      WebResource.Builder builder = processor.process(resource);
+      try {
+         return executeProxyRequest(host, builder, request, response);
+      } catch (HttpException ex) {
+         LOG.error("Error processing request", ex);
+      }
 
-        public URI getProxiedHostUri() {
-            return proxiedHostUri;
-        }
+      return -1;
+   }
 
-        public URL getProxiedHostUrl() {
-            return proxiedHostUrl;
-        }
-    }
-    
-    private ClientWrapper getClient() {
-        synchronized (clientLock) {
-            if (client == null) {
-                JerseyPropertiesConfigurator jerseyPropertiesConfigurator = new JerseyPropertiesConfigurator(connectionTimeout, readTimeout, proxyThreadPool, true);
-                client = new ClientWrapper(Client.create(jerseyPropertiesConfigurator.configure()));
+   private String extractHostPath(HttpServletRequest request) {
+      final StringBuilder myHostName = new StringBuilder(request.getServerName());
 
-                if (requestLogging) {
-                    LOG.warn("Enabling info logging of jersey client requests");
-                    client.getClient().addFilter(new LoggingFilter());
-                } else {
-                    LOG.warn("**** Jersey client request logging not enabled *****");
-                }
+      if (request.getServerPort() != 80) {
+         myHostName.append(":").append(request.getServerPort());
+      }
 
-            }
+      return myHostName.append(request.getContextPath()).toString();
+   }
 
-            return client;
-        }
-    }
+   private int executeProxyRequest(TargetHostInfo host, WebResource.Builder builder, HttpServletRequest sourceRequest, HttpServletResponse sourceResponse) throws IOException, HttpException {
 
-    @Override
-    public int proxyRequest(String targetHost, HttpServletRequest request, HttpServletResponse response) throws IOException {
-        TargetHostInfo host = new TargetHostInfo(targetHost);
-        final String target = host.getProxiedHostUrl().toExternalForm() + request.getRequestURI();
+      ClientResponse response = builder.method(sourceRequest.getMethod(), ClientResponse.class);
 
-        JerseyRequestProcessor processor = new JerseyRequestProcessor(request, host.getProxiedHostUri());
-        WebResource resource = getClient().resource(target);
-        WebResource.Builder builder = processor.process(resource);
-        try {
-            return executeProxyRequest(host, builder, request, response);
-        } catch (HttpException ex) {
-            LOG.error("Error processing request", ex);
-        }
+      HttpResponseCodeProcessor responseCode = new HttpResponseCodeProcessor(response.getStatus());
+      JerseyResponseProcessor responseProcessor = new JerseyResponseProcessor(response, sourceResponse);
 
-        return -1;
-    }
+      if (responseCode.isRedirect()) {
+         responseProcessor.sendTranslatedRedirect(host.getProxiedHostUrl().toExternalForm(), extractHostPath(sourceRequest), response.getStatus());
+      } else {
+         responseProcessor.process();
+      }
 
-    private String extractHostPath(HttpServletRequest request) {
-        final StringBuilder myHostName = new StringBuilder(request.getServerName());
+      return responseCode.getCode();
+   }
 
-        if (request.getServerPort() != 80) {
-            myHostName.append(":").append(request.getServerPort());
-        }
+   private WebResource.Builder setHeaders(WebResource.Builder builder, Map<String, String> headers) {
+      WebResource.Builder newBuilder = builder;
 
-        return myHostName.append(request.getContextPath()).toString();
-    }
+      for (String key : headers.keySet()) {
+         newBuilder = newBuilder.header(key, headers.get(key));
+      }
 
-    private int executeProxyRequest(TargetHostInfo host, WebResource.Builder builder, HttpServletRequest sourceRequest, HttpServletResponse sourceResponse) throws IOException, HttpException {
+      return builder;
+   }
 
-        ClientResponse response = builder.method(sourceRequest.getMethod(), ClientResponse.class);
+   @Override
+   public ServiceClientResponse get(String uri, Map<String, String> headers) {
+      WebResource.Builder requestBuilder = getClient().resource(uri).getRequestBuilder();
+      ClientResponse response = setHeaders(requestBuilder, headers).get(ClientResponse.class);
+      return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
+   }
 
-        HttpResponseCodeProcessor responseCode = new HttpResponseCodeProcessor(response.getStatus());
-        JerseyResponseProcessor responseProcessor = new JerseyResponseProcessor(response, sourceResponse);
+   @Override
+   public ServiceClientResponse get(String baseUri, String path, Map<String, String> headers) {
+      WebResource.Builder requestBuilder = getClient().resource(baseUri, true).path(path).getRequestBuilder();
+      ClientResponse response = setHeaders(requestBuilder, headers).get(ClientResponse.class);
+      return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
+   }
 
-        if (responseCode.isRedirect()) {
-            responseProcessor.sendTranslatedRedirect(host.getProxiedHostUrl().toExternalForm(), extractHostPath(sourceRequest), response.getStatus());
-        } else {
-            responseProcessor.process();
-        }
+   @Override
+   public ServiceClientResponse delete(String baseUri, String path, Map<String, String> headers) {
+      WebResource.Builder requestBuilder = getClient().resource(baseUri, true).path(path).getRequestBuilder();
+      ClientResponse response = setHeaders(requestBuilder, headers).delete(ClientResponse.class);
+      return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
+   }
 
-        return responseCode.getCode();
-    }
+   @Override
+   public ServiceClientResponse put(String uri, Map<String, String> headers, byte[] body) {
+      WebResource resource = getClient().resource(uri);
+      ClientResponse response = setHeaders(resource.getRequestBuilder(), headers).put(ClientResponse.class, body);
+      return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
+   }
 
-    private WebResource.Builder setHeaders(WebResource.Builder builder, Map<String, String> headers) {
-        WebResource.Builder newBuilder = builder;
+   @Override
+   public ServiceClientResponse put(String baseUri, String path, Map<String, String> headers, byte[] body) {
+      WebResource resource = getClient().resource(baseUri, true).path(path);
+      ClientResponse response = setHeaders(resource.getRequestBuilder(), headers).put(ClientResponse.class, body);
 
-        for (String key : headers.keySet()) {
-            newBuilder = newBuilder.header(key, headers.get(key));
-        }
-
-        return builder;
-    }
-
-    @Override
-    public ServiceClientResponse get(String uri, Map<String, String> headers) {
-        WebResource.Builder requestBuilder = getClient().resource(uri).getRequestBuilder();
-        ClientResponse response = setHeaders(requestBuilder, headers).get(ClientResponse.class);
-        return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
-    }
-
-    @Override
-    public ServiceClientResponse get(String baseUri, String path, Map<String, String> headers) {
-        WebResource.Builder requestBuilder = getClient().resource(baseUri, true).path(path).getRequestBuilder();
-        ClientResponse response = setHeaders(requestBuilder, headers).get(ClientResponse.class);
-        return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
-    }
-
-    @Override
-    public ServiceClientResponse delete(String baseUri, String path, Map<String, String> headers) {
-        WebResource.Builder requestBuilder = getClient().resource(baseUri, true).path(path).getRequestBuilder();
-        ClientResponse response = setHeaders(requestBuilder, headers).delete(ClientResponse.class);
-        return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
-    }
-
-    @Override
-    public ServiceClientResponse put(String uri, Map<String, String> headers, byte[] body) {
-        WebResource resource = getClient().resource(uri);
-        ClientResponse response = setHeaders(resource.getRequestBuilder(), headers).put(ClientResponse.class, body);
-        return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
-    }
-
-    @Override
-    public ServiceClientResponse put(String baseUri, String path, Map<String, String> headers, byte[] body) {
-        WebResource resource = getClient().resource(baseUri, true).path(path);
-        ClientResponse response = setHeaders(resource.getRequestBuilder(), headers).put(ClientResponse.class, body);
-
-        return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
-    }
+      return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
+   }
 }
