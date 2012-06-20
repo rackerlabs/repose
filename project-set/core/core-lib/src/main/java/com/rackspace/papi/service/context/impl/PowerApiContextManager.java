20c3b9d8b430a88d7b77d988d60f0889da024163
Dan Daley <danial.daley@rackspace.com>
2012-06-20 11:13:27 -0500

    Only configure ports in PowerApiContextManager if set

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/service/context/impl/PowerApiContextManager.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/service/context/impl/PowerApiContextManager.java
@@ -23,81 +23,85 @@ import org.springframework.core.AliasRegistry;
 
 public class PowerApiContextManager implements ServletContextListener {
 
-   private static final Logger LOG = LoggerFactory.getLogger(PowerApiContextManager.class);
-   private static final String DEFAULT_CONNECTION_FRAMEWORK = "jerseyRequestProxyService";
-   private AnnotationConfigApplicationContext applicationContext;
-   private ServicePorts ports;
+    private static final Logger LOG = LoggerFactory.getLogger(PowerApiContextManager.class);
+    private static final String DEFAULT_CONNECTION_FRAMEWORK = "jerseyRequestProxyService";
+    private AnnotationConfigApplicationContext applicationContext;
+    private ServicePorts ports;
 
-   public PowerApiContextManager() {
-      //applicationContext = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_CONFIG);
-   }
+    public PowerApiContextManager() {
+        //applicationContext = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_CONFIG);
+    }
 
-   public PowerApiContextManager setPorts(ServicePorts ports) {
-      if (applicationContext == null) {
-         this.ports = ports;
-      } else {
-         ServicePorts servicePorts = applicationContext.getBean("servicePorts", ServicePorts.class);
-         servicePorts.clear();
-         servicePorts.addAll(ports);
-      }
-      return this;
-   }
+    public PowerApiContextManager setPorts(ServicePorts ports) {
+        this.ports = ports;
+        configurePorts();
+        return this;
+    }
 
-   private void intializeServices(ServletContextEvent sce) {
-      ServletContextHelper helper = ServletContextHelper.getInstance();
-      ContextAdapter ca = helper.getPowerApiContext(sce.getServletContext());
+    private void configurePorts() {
+        if (ports == null || applicationContext == null) {
+            return;
+        }
+        ServicePorts servicePorts = applicationContext.getBean("servicePorts", ServicePorts.class);
+        servicePorts.clear();
+        servicePorts.addAll(ports);
+    }
 
-      ca.getContext(ThreadingServiceContext.class).contextInitialized(sce);
-      ca.getContext(EventManagerServiceContext.class).contextInitialized(sce);
-      ca.getContext(ConfigurationServiceContext.class).contextInitialized(sce);
-      ca.getContext(ContainerServiceContext.class).contextInitialized(sce);
-      ca.getContext(RoutingServiceContext.class).contextInitialized(sce);
-      ca.getContext(LoggingServiceContext.class).contextInitialized(sce);
-      PapiBanner.print(LOG);
-      ca.getContext(ResponseMessageServiceContext.class).contextInitialized(sce);
-      // TODO:Refactor - This service should be bound to a fitler-chain specific JNDI context
-      ca.getContext(DatastoreServiceContext.class).contextInitialized(sce);
-      ca.getContext(ClassLoaderServiceContext.class).contextInitialized(sce);
-      ca.getContext(ArtifactManagerServiceContext.class).contextInitialized(sce);
-      ca.getContext(FilterChainGCServiceContext.class).contextInitialized(sce);
-      ca.getContext(RequestProxyServiceContext.class).contextInitialized(sce);
+    private void intializeServices(ServletContextEvent sce) {
+        ServletContextHelper helper = ServletContextHelper.getInstance();
+        ContextAdapter ca = helper.getPowerApiContext(sce.getServletContext());
 
-   }
+        ca.getContext(ThreadingServiceContext.class).contextInitialized(sce);
+        ca.getContext(EventManagerServiceContext.class).contextInitialized(sce);
+        ca.getContext(ConfigurationServiceContext.class).contextInitialized(sce);
+        ca.getContext(ContainerServiceContext.class).contextInitialized(sce);
+        ca.getContext(RoutingServiceContext.class).contextInitialized(sce);
+        ca.getContext(LoggingServiceContext.class).contextInitialized(sce);
+        PapiBanner.print(LOG);
+        ca.getContext(ResponseMessageServiceContext.class).contextInitialized(sce);
+        // TODO:Refactor - This service should be bound to a fitler-chain specific JNDI context
+        ca.getContext(DatastoreServiceContext.class).contextInitialized(sce);
+        ca.getContext(ClassLoaderServiceContext.class).contextInitialized(sce);
+        ca.getContext(ArtifactManagerServiceContext.class).contextInitialized(sce);
+        ca.getContext(FilterChainGCServiceContext.class).contextInitialized(sce);
+        ca.getContext(RequestProxyServiceContext.class).contextInitialized(sce);
 
-   @Override
-   public void contextInitialized(ServletContextEvent sce) {
-      final ServletContext servletContext = sce.getServletContext();
-      final String connectionFrameworkProp = InitParameter.CONNECTION_FRAMEWORK.getParameterName();
-      final String connectionFramework = System.getProperty(connectionFrameworkProp, servletContext.getInitParameter(connectionFrameworkProp));
-      final String beanName = StringUtilities.isNotBlank(connectionFramework)? connectionFramework + "RequestProxyService": null;
+    }
 
-      applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-      if (StringUtilities.isNotBlank(beanName) && applicationContext.containsBean(beanName)) {
-         LOG.info("Using connection framework: " + beanName);
-         applicationContext.registerAlias(beanName, "requestProxyService");
-      } else {
-         LOG.info("Using default connection framework: " + DEFAULT_CONNECTION_FRAMEWORK);
-         applicationContext.registerAlias(DEFAULT_CONNECTION_FRAMEWORK, "requestProxyService");
-      }
-      
-      setPorts(ports);
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        final ServletContext servletContext = sce.getServletContext();
+        final String connectionFrameworkProp = InitParameter.CONNECTION_FRAMEWORK.getParameterName();
+        final String connectionFramework = System.getProperty(connectionFrameworkProp, servletContext.getInitParameter(connectionFrameworkProp));
+        final String beanName = StringUtilities.isNotBlank(connectionFramework) ? connectionFramework + "RequestProxyService" : null;
 
-      // Most bootstrap steps require or will try to load some kind of
-      // configuration so we need to set our naming context in the servlet context
-      // first before anything else
-      ServletContextHelper.configureInstance(
-              new SpringContextAdapterProvider(applicationContext),
-              servletContext,
-              applicationContext);
+        applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+        if (StringUtilities.isNotBlank(beanName) && applicationContext.containsBean(beanName)) {
+            LOG.info("Using connection framework: " + beanName);
+            applicationContext.registerAlias(beanName, "requestProxyService");
+        } else {
+            LOG.info("Using default connection framework: " + DEFAULT_CONNECTION_FRAMEWORK);
+            applicationContext.registerAlias(DEFAULT_CONNECTION_FRAMEWORK, "requestProxyService");
+        }
 
-      intializeServices(sce);
-   }
+        configurePorts();
 
-   @Override
-   public void contextDestroyed(ServletContextEvent sce) {
-      ServiceRegistry registry = applicationContext.getBean("serviceRegistry", ServiceRegistry.class);
-      for (ServiceContext ctx : registry.getServices()) {
-         ctx.contextDestroyed(sce);
-      }
-   }
+        // Most bootstrap steps require or will try to load some kind of
+        // configuration so we need to set our naming context in the servlet context
+        // first before anything else
+        ServletContextHelper.configureInstance(
+                new SpringContextAdapterProvider(applicationContext),
+                servletContext,
+                applicationContext);
+
+        intializeServices(sce);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ServiceRegistry registry = applicationContext.getBean("serviceRegistry", ServiceRegistry.class);
+        for (ServiceContext ctx : registry.getServices()) {
+            ctx.contextDestroyed(sce);
+        }
+    }
 }
