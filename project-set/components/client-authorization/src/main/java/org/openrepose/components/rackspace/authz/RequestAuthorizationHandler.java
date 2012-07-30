26405f44c37b669c5b67a20c14ffc541803f2f27
forcewhisperer <fran.torreslopez@rackspace.com>
2012-07-30 14:41:01 -0500

    D-07238: Update Auth z handler to catch exceptions and set HTTP response to 500 instead of 200.

--- project-set/components/client-authorization/src/main/java/org/openrepose/components/rackspace/authz/RequestAuthorizationHandler.java
+++ project-set/components/client-authorization/src/main/java/org/openrepose/components/rackspace/authz/RequestAuthorizationHandler.java
@@ -10,6 +10,7 @@ import com.rackspace.papi.filter.logic.FilterAction;
 import com.rackspace.papi.filter.logic.FilterDirector;
 import com.rackspace.papi.filter.logic.common.AbstractFilterLogicHandler;
 import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import com.sun.jersey.api.client.ClientHandlerException;
 import org.openrepose.components.authz.rackspace.config.ServiceEndpoint;
 import org.openrepose.components.rackspace.authz.cache.CachedEndpoint;
 import org.openrepose.components.rackspace.authz.cache.EndpointListCache;
@@ -24,111 +25,120 @@ import java.util.List;
 
 public class RequestAuthorizationHandler extends AbstractFilterLogicHandler {
 
-   private static final Logger LOG = LoggerFactory.getLogger(RequestAuthorizationHandler.class);
-   private final AuthenticationService authenticationService;
-   private final EndpointListCache endpointListCache;
-   private final ServiceEndpoint myEndpoint;
+    private static final Logger LOG = LoggerFactory.getLogger(RequestAuthorizationHandler.class);
+    private final AuthenticationService authenticationService;
+    private final EndpointListCache endpointListCache;
+    private final ServiceEndpoint myEndpoint;
 
-   public RequestAuthorizationHandler(AuthenticationService authenticationService, EndpointListCache endpointListCache, ServiceEndpoint myEndpoint) {
-      this.authenticationService = authenticationService;
-      this.endpointListCache = endpointListCache;
-      this.myEndpoint = myEndpoint;
-   }
+    public RequestAuthorizationHandler(AuthenticationService authenticationService, EndpointListCache endpointListCache, ServiceEndpoint myEndpoint) {
+        this.authenticationService = authenticationService;
+        this.endpointListCache = endpointListCache;
+        this.myEndpoint = myEndpoint;
+    }
 
-   @Override
-   public FilterDirector handleRequest(HttpServletRequest request, ReadableHttpServletResponse response) {
-      final FilterDirector myDirector = new FilterDirectorImpl();
-      myDirector.setFilterAction(FilterAction.RETURN);
-      myDirector.setResponseStatus(HttpStatusCode.FORBIDDEN);
+    @Override
+    public FilterDirector handleRequest(HttpServletRequest request, ReadableHttpServletResponse response) {
+        final FilterDirector myDirector = new FilterDirectorImpl();
+        myDirector.setFilterAction(FilterAction.RETURN);
+        myDirector.setResponseStatus(HttpStatusCode.FORBIDDEN);
 
-      if (authenticationWasDelegated(request)) {
-         // We do not support delegation
-         LOG.debug("Authentication delegation is not supported by the rackspace authorization filter. Rejecting request.");
-      } else {
-         authorizeRequest(myDirector, request);
-      }
+        if (authenticationWasDelegated(request)) {
+            // We do not support delegation
+            LOG.debug("Authentication delegation is not supported by the rackspace authorization filter. Rejecting request.");
+        } else {
+            authorizeRequest(myDirector, request);
+        }
 
-      return myDirector;
-   }
+        return myDirector;
+    }
 
-   public void authorizeRequest(FilterDirector director, HttpServletRequest request) {
-      final String authenticationToken = request.getHeader(CommonHttpHeader.AUTH_TOKEN.toString());
+    public void authorizeRequest(FilterDirector director, HttpServletRequest request) {
+        final String authenticationToken = request.getHeader(CommonHttpHeader.AUTH_TOKEN.toString());
 
-      if (StringUtilities.isBlank(authenticationToken)) {
-         // Reject if no token
-         LOG.debug("Authentication token not found in X-Auth-Token header. Rejecting request.");
-         director.setResponseStatus(HttpStatusCode.UNAUTHORIZED);
-      } else {
-         checkTenantEndpoints(director, authenticationToken);
-      }
-   }
+        if (StringUtilities.isBlank(authenticationToken)) {
+            // Reject if no token
+            LOG.debug("Authentication token not found in X-Auth-Token header. Rejecting request.");
+            director.setResponseStatus(HttpStatusCode.UNAUTHORIZED);
+        } else {
+            checkTenantEndpoints(director, authenticationToken);
+        }
+    }
 
-   public void checkTenantEndpoints(FilterDirector director, String userToken) {
-      final List<CachedEndpoint> authorizedEndpoints = getEndpointsForToken(userToken);
+    public void checkTenantEndpoints(FilterDirector director, String userToken) {
 
-      if (isEndpointAuthorized(authorizedEndpoints)) {
-         director.setFilterAction(FilterAction.PASS);
-      } else {
-         LOG.info("User token: " + userToken + ": The user's service catalog does not contain an endpoint that matches " +
-                  "the endpoint configured in openstack-authorization.cfg.xml: \"" +
-                  myEndpoint.getHref() + "\".  User not authorized to access service.");
-      }
-   }
+        try {
+            final List<CachedEndpoint> authorizedEndpoints = getEndpointsForToken(userToken);
 
-   private boolean isEndpointAuthorized(final List<CachedEndpoint> authorizedEndpoints){
-      boolean authorized = false;
+            if (isEndpointAuthorized(authorizedEndpoints)) {
+                director.setFilterAction(FilterAction.PASS);
+            } else {
+                LOG.info("User token: " + userToken + ": The user's service catalog does not contain an endpoint that matches " +
+                        "the endpoint configured in openstack-authorization.cfg.xml: \"" +
+                        myEndpoint.getHref() + "\".  User not authorized to access service.");
+            }
+        } catch (ClientHandlerException ex) {
+            LOG.error("Failure communicating with the auth service: " + ex.getMessage(), ex);
+            director.setResponseStatus(HttpStatusCode.INTERNAL_SERVER_ERROR);
+        } catch (Exception ex) {
+            LOG.error("Failure in authorization component: " + ex.getMessage(), ex);
+            director.setResponseStatus(HttpStatusCode.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-      for (CachedEndpoint authorizedEndpoint : authorizedEndpoints) {
+    private boolean isEndpointAuthorized(final List<CachedEndpoint> authorizedEndpoints) {
+        boolean authorized = false;
 
-         if (StringUtilities.isBlank(authorizedEndpoint.getPublicUrl())) {
-            LOG.warn("Endpoint Public URL is null.  This is a violation of the OpenStack Identity Service contract." );
-         }
+        for (CachedEndpoint authorizedEndpoint : authorizedEndpoints) {
 
-         if (StringUtilities.isBlank(authorizedEndpoint.getType())) {
-            LOG.warn("Endpoint Type is null.  This is a violation of the OpenStack Identity Service contract." );
-         }
+            if (StringUtilities.isBlank(authorizedEndpoint.getPublicUrl())) {
+                LOG.warn("Endpoint Public URL is null.  This is a violation of the OpenStack Identity Service contract.");
+            }
 
-         if (StringUtilities.nullSafeStartsWith(authorizedEndpoint.getPublicUrl(), myEndpoint.getHref())) {          
-            authorized = true;
-            break;
-         }
-      }
+            if (StringUtilities.isBlank(authorizedEndpoint.getType())) {
+                LOG.warn("Endpoint Type is null.  This is a violation of the OpenStack Identity Service contract.");
+            }
 
-      return authorized;
-   }
+            if (StringUtilities.nullSafeStartsWith(authorizedEndpoint.getPublicUrl(), myEndpoint.getHref())) {
+                authorized = true;
+                break;
+            }
+        }
 
-   private List<CachedEndpoint> getEndpointsForToken(String userToken) {
-      List<CachedEndpoint> cachedEndpoints = endpointListCache.getCachedEndpointsForToken(userToken);
+        return authorized;
+    }
 
-      if (cachedEndpoints == null) {
-         cachedEndpoints = requestEndpointsForTokenFromAuthService(userToken);
+    private List<CachedEndpoint> getEndpointsForToken(String userToken) {
+        List<CachedEndpoint> cachedEndpoints = endpointListCache.getCachedEndpointsForToken(userToken);
 
-         try {
-            endpointListCache.cacheEndpointsForToken(userToken, cachedEndpoints);
-         } catch (IOException ioe) {
-            LOG.error("Caching failure. Reason: " + ioe.getMessage(), ioe);
-         }
-      }
+        if (cachedEndpoints == null) {
+            cachedEndpoints = requestEndpointsForTokenFromAuthService(userToken);
 
-      return cachedEndpoints;
-   }
+            try {
+                endpointListCache.cacheEndpointsForToken(userToken, cachedEndpoints);
+            } catch (IOException ioe) {
+                LOG.error("Caching failure. Reason: " + ioe.getMessage(), ioe);
+            }
+        }
 
-   private List<CachedEndpoint> requestEndpointsForTokenFromAuthService(String userToken) {
-      final List<Endpoint> authorizedEndpoints = authenticationService.getEndpointsForToken(userToken);
-      final LinkedList<CachedEndpoint> serializable = new LinkedList<CachedEndpoint>();
+        return cachedEndpoints;
+    }
 
-      for (Endpoint ep : authorizedEndpoints) {
-         serializable.add(new CachedEndpoint(ep.getPublicURL(), ep.getRegion(), ep.getName(), ep.getType()));
-      }
+    private List<CachedEndpoint> requestEndpointsForTokenFromAuthService(String userToken) {
+        final List<Endpoint> authorizedEndpoints = authenticationService.getEndpointsForToken(userToken);
+        final LinkedList<CachedEndpoint> serializable = new LinkedList<CachedEndpoint>();
 
-      return serializable;
-   }
+        for (Endpoint ep : authorizedEndpoints) {
+            serializable.add(new CachedEndpoint(ep.getPublicURL(), ep.getRegion(), ep.getName(), ep.getType()));
+        }
 
-   // The X-Identity-Status header gets set if client authentication is in delegated mode.  If the token is valid, the
-   // value of the X-Identity-Status header is "Confirmed".  If the token is not valid, then the X-Identity-Status
-   // header is set to "Indeterminate".  In the future, we may want to allow authorization for delegated requests
-   // that have a "Confirmed" status since we know the token is valid in that case.
-   public boolean authenticationWasDelegated(HttpServletRequest request) {
-      return StringUtilities.isNotBlank(request.getHeader(OpenStackServiceHeader.IDENTITY_STATUS.toString()));
-   }
+        return serializable;
+    }
+
+    // The X-Identity-Status header gets set if client authentication is in delegated mode.  If the token is valid, the
+    // value of the X-Identity-Status header is "Confirmed".  If the token is not valid, then the X-Identity-Status
+    // header is set to "Indeterminate".  In the future, we may want to allow authorization for delegated requests
+    // that have a "Confirmed" status since we know the token is valid in that case.
+    public boolean authenticationWasDelegated(HttpServletRequest request) {
+        return StringUtilities.isNotBlank(request.getHeader(OpenStackServiceHeader.IDENTITY_STATUS.toString()));
+    }
 }
--- project-set/components/client-authorization/src/test/java/org/openrepose/components/rackspace/authz/RequestAuthorizationHandlerTest.java
+++ project-set/components/client-authorization/src/test/java/org/openrepose/components/rackspace/authz/RequestAuthorizationHandlerTest.java
@@ -25,126 +25,135 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 /**
- *
  * @author zinic
  */
 @RunWith(Enclosed.class)
 public class RequestAuthorizationHandlerTest {
 
-   private static final String UNAUTHORIZED_TOKEN = "abcdef-abcdef-abcdef-abcdef", AUTHORIZED_TOKEN = "authorized", CACHED_TOKEN = "cached";
-   private static final String PUBLIC_URL = "http://service.api.f.com/v1.1", REGION = "ORD", NAME = "Nova", TYPE = "compute";
+    private static final String UNAUTHORIZED_TOKEN = "abcdef-abcdef-abcdef-abcdef", AUTHORIZED_TOKEN = "authorized", CACHED_TOKEN = "cached";
+    private static final String PUBLIC_URL = "http://service.api.f.com/v1.1", REGION = "ORD", NAME = "Nova", TYPE = "compute";
 
-   @Ignore
-   public static class TestParent {
+    @Ignore
+    public static class TestParent {
 
-      protected AuthenticationService mockedAuthService;
-      protected RequestAuthorizationHandler handler;
-      protected EndpointListCache mockedCache;
-      protected HttpServletRequest mockedRequest;
+        protected AuthenticationService mockedAuthService;
+        protected RequestAuthorizationHandler handler;
+        protected EndpointListCache mockedCache;
+        protected HttpServletRequest mockedRequest;
 
-      @Before
-      public void beforeAny() {
-         // Caching mocks
-         mockedCache = mock(EndpointListCache.class);
+        @Before
+        public void beforeAny() {
+            // Caching mocks
+            mockedCache = mock(EndpointListCache.class);
 
-         final List<CachedEndpoint> cachedEndpointList = new LinkedList<CachedEndpoint>();
-         cachedEndpointList.add(new CachedEndpoint(PUBLIC_URL, REGION, NAME, TYPE));
+            final List<CachedEndpoint> cachedEndpointList = new LinkedList<CachedEndpoint>();
+            cachedEndpointList.add(new CachedEndpoint(PUBLIC_URL, REGION, NAME, TYPE));
 
-         when(mockedCache.getCachedEndpointsForToken(AUTHORIZED_TOKEN)).thenReturn(null);
-         when(mockedCache.getCachedEndpointsForToken(CACHED_TOKEN)).thenReturn(cachedEndpointList);
+            when(mockedCache.getCachedEndpointsForToken(AUTHORIZED_TOKEN)).thenReturn(null);
+            when(mockedCache.getCachedEndpointsForToken(CACHED_TOKEN)).thenReturn(cachedEndpointList);
 
-         // Auth service mocks
-         final List<Endpoint> endpointList = new LinkedList<Endpoint>();
+            // Auth service mocks
+            final List<Endpoint> endpointList = new LinkedList<Endpoint>();
 
-         Endpoint endpoint = new Endpoint();
-         endpoint.setPublicURL(PUBLIC_URL);
-         endpoint.setRegion(REGION);
-         endpoint.setName(NAME);
-         endpoint.setType(TYPE);
+            Endpoint endpoint = new Endpoint();
+            endpoint.setPublicURL(PUBLIC_URL);
+            endpoint.setRegion(REGION);
+            endpoint.setName(NAME);
+            endpoint.setType(TYPE);
 
-         // Added to test case where endpoint values are null
-         Endpoint endpointb = new Endpoint();
+            // Added to test case where endpoint values are null
+            Endpoint endpointb = new Endpoint();
 
-         endpointList.add(endpointb);
-         endpointList.add(endpoint);
+            endpointList.add(endpointb);
+            endpointList.add(endpoint);
 
-         mockedAuthService = mock(AuthenticationService.class);
-         when(mockedAuthService.getEndpointsForToken(UNAUTHORIZED_TOKEN)).thenReturn(Collections.EMPTY_LIST);
-         when(mockedAuthService.getEndpointsForToken(AUTHORIZED_TOKEN)).thenReturn(endpointList);
-         when(mockedAuthService.getEndpointsForToken(CACHED_TOKEN)).thenReturn(endpointList);
+            mockedAuthService = mock(AuthenticationService.class);
+            when(mockedAuthService.getEndpointsForToken(UNAUTHORIZED_TOKEN)).thenReturn(Collections.EMPTY_LIST);
+            when(mockedAuthService.getEndpointsForToken(AUTHORIZED_TOKEN)).thenReturn(endpointList);
+            when(mockedAuthService.getEndpointsForToken(CACHED_TOKEN)).thenReturn(endpointList);
 
-         final ServiceEndpoint myServiceEndpoint = new ServiceEndpoint();
-         myServiceEndpoint.setHref(PUBLIC_URL);
-         myServiceEndpoint.setRegion(REGION);
-         myServiceEndpoint.setName(NAME);
-         myServiceEndpoint.setType(TYPE);
+            final ServiceEndpoint myServiceEndpoint = new ServiceEndpoint();
+            myServiceEndpoint.setHref(PUBLIC_URL);
+            myServiceEndpoint.setRegion(REGION);
+            myServiceEndpoint.setName(NAME);
+            myServiceEndpoint.setType(TYPE);
 
-         handler = new RequestAuthorizationHandler(mockedAuthService, mockedCache, myServiceEndpoint);
+            handler = new RequestAuthorizationHandler(mockedAuthService, mockedCache, myServiceEndpoint);
 
-         mockedRequest = mock(HttpServletRequest.class);
-      }
-   }
+            mockedRequest = mock(HttpServletRequest.class);
+        }
+    }
 
-   public static class WhenAuthorizingRequests extends TestParent {
+    public static class WhenAuthorizingRequests extends TestParent {
 
-      @Test
-      public void shouldRejectDelegatedAuthentication() {
-         when(mockedRequest.getHeader(OpenStackServiceHeader.IDENTITY_STATUS.toString())).thenReturn("Confirmed");
+        @Test
+        public void shouldRejectDelegatedAuthentication() {
+            when(mockedRequest.getHeader(OpenStackServiceHeader.IDENTITY_STATUS.toString())).thenReturn("Confirmed");
 
-         final FilterDirector director = handler.handleRequest(mockedRequest, null);
+            final FilterDirector director = handler.handleRequest(mockedRequest, null);
 
-         assertEquals("Authorization component must return requests that have had authentication delegated", FilterAction.RETURN, director.getFilterAction());
-         assertEquals("Authorization component must reject delegated authentication with a 403", HttpStatusCode.FORBIDDEN, director.getResponseStatus());
-      }
+            assertEquals("Authorization component must return requests that have had authentication delegated", FilterAction.RETURN, director.getFilterAction());
+            assertEquals("Authorization component must reject delegated authentication with a 403", HttpStatusCode.FORBIDDEN, director.getResponseStatus());
+        }
 
-      @Test
-      public void shouldRejectRequestsWithoutAuthTokens() {
-         final FilterDirector director = handler.handleRequest(mockedRequest, null);
+        @Test
+        public void shouldRejectRequestsWithoutAuthTokens() {
+            final FilterDirector director = handler.handleRequest(mockedRequest, null);
 
-         assertEquals("Authorization component must return requests that do not have auth tokens", FilterAction.RETURN, director.getFilterAction());
-         assertEquals("Authorization component must reject unauthenticated requests with a 401", HttpStatusCode.UNAUTHORIZED, director.getResponseStatus());
-      }
+            assertEquals("Authorization component must return requests that do not have auth tokens", FilterAction.RETURN, director.getFilterAction());
+            assertEquals("Authorization component must reject unauthenticated requests with a 401", HttpStatusCode.UNAUTHORIZED, director.getResponseStatus());
+        }
 
-      @Test
-      public void shouldRejectUnauthorizedRequests() {
-         when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(UNAUTHORIZED_TOKEN);
+        @Test
+        public void shouldRejectUnauthorizedRequests() {
+            when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(UNAUTHORIZED_TOKEN);
 
-         final FilterDirector director = handler.handleRequest(mockedRequest, null);
+            final FilterDirector director = handler.handleRequest(mockedRequest, null);
 
-         assertEquals("Authorization component must return unauthorized requests", FilterAction.RETURN, director.getFilterAction());
-         assertEquals("Authorization component must reject unauthorized requests with a 403", HttpStatusCode.FORBIDDEN, director.getResponseStatus());
-      }
+            assertEquals("Authorization component must return unauthorized requests", FilterAction.RETURN, director.getFilterAction());
+            assertEquals("Authorization component must reject unauthorized requests with a 403", HttpStatusCode.FORBIDDEN, director.getResponseStatus());
+        }
 
-      @Test
-      public void shouldPassAuthorizedRequests() {
-         when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(AUTHORIZED_TOKEN);
+        @Test
+        public void shouldPassAuthorizedRequests() {
+            when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(AUTHORIZED_TOKEN);
 
-         final FilterDirector director = handler.handleRequest(mockedRequest, null);
+            final FilterDirector director = handler.handleRequest(mockedRequest, null);
 
-         assertEquals("Authorization component must pass authorized requests", FilterAction.PASS, director.getFilterAction());
-      }
-   }
+            assertEquals("Authorization component must pass authorized requests", FilterAction.PASS, director.getFilterAction());
+        }
 
-   public static class WhenAuthorizingRequestsAgainstCachedEndpointLists extends TestParent {
+        @Test
+        public void shouldReturn500(){
+            when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(AUTHORIZED_TOKEN);
+            when(mockedAuthService.getEndpointsForToken(AUTHORIZED_TOKEN)).thenThrow(new RuntimeException("Service Exception"));
 
-      @Test
-      public void shouldCacheFreshEndpointLists() throws Exception {
-         when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(AUTHORIZED_TOKEN);
-         handler.handleRequest(mockedRequest, null);
+            final FilterDirector director = handler.handleRequest(mockedRequest, null);
 
-         verify(mockedCache, times(1)).getCachedEndpointsForToken(AUTHORIZED_TOKEN);
-         verify(mockedAuthService, times(1)).getEndpointsForToken(AUTHORIZED_TOKEN);
-         verify(mockedCache, times(1)).cacheEndpointsForToken(eq(AUTHORIZED_TOKEN), any(List.class));
-      }
+            assertEquals("Authorization component must retrun 500 on service exception", HttpStatusCode.INTERNAL_SERVER_ERROR.intValue(), director.getResponseStatus().intValue());
+        }
+    }
 
-      @Test
-      public void shouldUseCacheForCachedEndpoitnLists() throws Exception {
-         when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(CACHED_TOKEN);
-         handler.handleRequest(mockedRequest, null);
+    public static class WhenAuthorizingRequestsAgainstCachedEndpointLists extends TestParent {
 
-         verify(mockedCache, times(1)).getCachedEndpointsForToken(CACHED_TOKEN);
-         verify(mockedAuthService, never()).getEndpointsForToken(CACHED_TOKEN);
-         verify(mockedCache, never()).cacheEndpointsForToken(eq(AUTHORIZED_TOKEN), any(List.class));
-      }
-   }
+        @Test
+        public void shouldCacheFreshEndpointLists() throws Exception {
+            when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(AUTHORIZED_TOKEN);
+            handler.handleRequest(mockedRequest, null);
+
+            verify(mockedCache, times(1)).getCachedEndpointsForToken(AUTHORIZED_TOKEN);
+            verify(mockedAuthService, times(1)).getEndpointsForToken(AUTHORIZED_TOKEN);
+            verify(mockedCache, times(1)).cacheEndpointsForToken(eq(AUTHORIZED_TOKEN), any(List.class));
+        }
+
+        @Test
+        public void shouldUseCacheForCachedEndpoitnLists() throws Exception {
+            when(mockedRequest.getHeader(CommonHttpHeader.AUTH_TOKEN.toString())).thenReturn(CACHED_TOKEN);
+            handler.handleRequest(mockedRequest, null);
+
+            verify(mockedCache, times(1)).getCachedEndpointsForToken(CACHED_TOKEN);
+            verify(mockedAuthService, never()).getEndpointsForToken(CACHED_TOKEN);
+            verify(mockedCache, never()).cacheEndpointsForToken(eq(AUTHORIZED_TOKEN), any(List.class));
+        }
+    }
 }
