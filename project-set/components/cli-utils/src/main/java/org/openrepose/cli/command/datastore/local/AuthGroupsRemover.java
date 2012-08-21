01b1b25bfb62c2ced62512484a2b457072108119
forcewhisperer <fran.torreslopez@rackspace.com>
2012-08-21 12:48:14 -0500

    Fix the build... again!  Include the tools jar as a dependency.

--- project-set/components/cli-utils/src/main/java/org/openrepose/cli/command/datastore/local/AuthGroupsRemover.java
+++ project-set/components/cli-utils/src/main/java/org/openrepose/cli/command/datastore/local/AuthGroupsRemover.java
@@ -3,14 +3,17 @@ package org.openrepose.cli.command.datastore.local;
 import com.rackspace.papi.commons.util.jmx.ReposeMBeanObjectNames;
 import com.rackspace.papi.service.datastore.impl.ehcache.ReposeEHCacheMBean;
 import org.openrepose.cli.command.AbstractCommand;
-import org.openrepose.cli.command.datastore.local.jmx.MBeanServerConnector;
+import org.openrepose.cli.command.datastore.local.jmx.LocalJVMPidRetriever;
+import org.openrepose.cli.command.datastore.local.jmx.ReposeMBeanServerConnectionFinder;
 import org.openrepose.cli.command.results.*;
 
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
+import java.util.List;
 
 public class AuthGroupsRemover extends AbstractCommand {
+    private final ReposeMBeanServerConnectionFinder reposeMBeanServerConnectionFinder = new ReposeMBeanServerConnectionFinder();
 
     @Override
     public String getCommandToken() {
@@ -29,16 +32,26 @@ public class AuthGroupsRemover extends AbstractCommand {
             return new InvalidArguments("The groups remover expects one string argument.");
         }
 
+        final List<String> localJVMPids = LocalJVMPidRetriever.getLocalJVMPids();
         final ObjectName reposeEHCacheObjectName = new ReposeMBeanObjectNames().getReposeEHCache();
-        final MBeanServerConnection reposeConnection = MBeanServerConnector.getMBeanServerConnection(arguments[0]);
-        final ReposeEHCacheMBean reposeEhCacheMBeanProxy = JMX.newMBeanProxy(reposeConnection, reposeEHCacheObjectName, ReposeEHCacheMBean.class, true);
+        final List<MBeanServerConnection> reposeConnections = reposeMBeanServerConnectionFinder.getLocalReposeMBeanServerConnections(localJVMPids, reposeEHCacheObjectName);
 
         CommandResult result;
-        if (reposeEhCacheMBeanProxy.removeGroups(arguments[1])) {
-            result = new MessageResult("Removed auth groups for user " + arguments[1]);
-        } else {
-            result = new CommandFailure(StatusCodes.SYSTEM_PRECONDITION_FAILURE.getStatusCode(),
-                    "Failure to remove auth groups for user " + arguments[1]);
+
+        switch (reposeConnections.size()) {
+            case 0:
+                result = new CommandFailure(StatusCodes.NOTHING_TO_DO.getStatusCode(),
+                        "Unable to connect: No local instances of Repose detected.");
+                break;
+            default:
+                ReposeEHCacheMBean reposeEhCacheMbeanProxy = JMX.newMBeanProxy(reposeConnections.get(0), reposeEHCacheObjectName, ReposeEHCacheMBean.class, true);
+
+                if (reposeEhCacheMbeanProxy.removeGroups(arguments[0])) {
+                    result = new MessageResult("Removed auth groups for user " + arguments[0]);
+                } else {
+                    result = new CommandFailure(StatusCodes.SYSTEM_PRECONDITION_FAILURE.getStatusCode(),
+                            "Failure to remove auth groups for user " + arguments[0]);
+                }
         }
 
         return result;
--- project-set/components/cli-utils/src/main/java/org/openrepose/cli/command/datastore/local/AuthTokenAndRolesRemover.java
+++ project-set/components/cli-utils/src/main/java/org/openrepose/cli/command/datastore/local/AuthTokenAndRolesRemover.java
@@ -2,13 +2,16 @@ package org.openrepose.cli.command.datastore.local;
 
 import com.rackspace.papi.service.datastore.impl.ehcache.ReposeEHCacheMBean;
 import org.openrepose.cli.command.AbstractCommand;
+import org.openrepose.cli.command.datastore.local.jmx.LocalJVMPidRetriever;
 import com.rackspace.papi.commons.util.jmx.ReposeMBeanObjectNames;
-import org.openrepose.cli.command.datastore.local.jmx.MBeanServerConnector;
+import org.openrepose.cli.command.datastore.local.jmx.ReposeMBeanServerConnectionFinder;
 import org.openrepose.cli.command.results.*;
 
 import javax.management.*;
+import java.util.List;
 
 public class AuthTokenAndRolesRemover extends AbstractCommand {
+    private final ReposeMBeanServerConnectionFinder reposeMBeanServerConnectionFinder = new ReposeMBeanServerConnectionFinder();
 
     @Override
     public String getCommandToken() {
@@ -27,16 +30,26 @@ public class AuthTokenAndRolesRemover extends AbstractCommand {
             return new InvalidArguments("The token remover expects one string argument.");
         }
 
+        final List<String> localJVMPids = LocalJVMPidRetriever.getLocalJVMPids();
         final ObjectName reposeEHCacheObjectName = new ReposeMBeanObjectNames().getReposeEHCache();
-        final MBeanServerConnection reposeConnection = MBeanServerConnector.getMBeanServerConnection(arguments[0]);
-        final ReposeEHCacheMBean reposeEhCacheMBeanProxy = JMX.newMBeanProxy(reposeConnection, reposeEHCacheObjectName, ReposeEHCacheMBean.class, true);
+        final List<MBeanServerConnection> reposeConnections = reposeMBeanServerConnectionFinder.getLocalReposeMBeanServerConnections(localJVMPids, reposeEHCacheObjectName);
 
         CommandResult result;
-        if (reposeEhCacheMBeanProxy.removeTokenAndRoles(arguments[1])) {
-            result = new MessageResult("Removed auth token and roles for user " + arguments[1]);
-        } else {
-            result = new CommandFailure(StatusCodes.SYSTEM_PRECONDITION_FAILURE.getStatusCode(),
-                    "Failure to remove auth token and roles for user " + arguments[1]);
+
+        switch (reposeConnections.size()) {
+            case 0:
+                result = new CommandFailure(StatusCodes.NOTHING_TO_DO.getStatusCode(),
+                        "Unable to connect: No local instances of Repose detected.");
+                break;
+            default:
+                ReposeEHCacheMBean reposeEhCacheMbeanProxy = JMX.newMBeanProxy(reposeConnections.get(0), reposeEHCacheObjectName, ReposeEHCacheMBean.class, true);
+
+                if (reposeEhCacheMbeanProxy.removeTokenAndRoles(arguments[0])) {
+                    result = new MessageResult("Removed auth token and roles for user " + arguments[0]);
+                } else {
+                    result = new CommandFailure(StatusCodes.SYSTEM_PRECONDITION_FAILURE.getStatusCode(),
+                            "Failure to remove auth token and roles for user " + arguments[0]);
+                }
         }
 
         return result;
