68d578ace63c2527f789820bf9c20abb7c9c6e74
Dan Daley <danial.daley@rackspace.com>
2012-09-19 07:58:34 -0500

    Add Pre processors for json

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/httpx/processor/TranslationResponsePreProcessor.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/httpx/processor/TranslationResponsePreProcessor.java
@@ -1,45 +1,44 @@
 package com.rackspace.papi.httpx.processor;
 
-import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletRequest;
+import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletResponse;
 import com.rackspace.papi.httpx.processor.cdata.UnknownContentStreamProcessor;
 import com.rackspace.papi.httpx.processor.common.InputStreamProcessor;
 import com.rackspace.papi.httpx.processor.json.JsonxStreamProcessor;
 import com.rackspace.papi.httpx.processor.util.BodyContentMediaType;
 import org.codehaus.jackson.JsonFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class TranslationRequestPreProcessor {
+public class TranslationResponsePreProcessor {
 
    private static final SAXTransformerFactory handlerFactory = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
-   private final HttpServletRequest request;
+   private final MutableHttpServletResponse response;
    private final boolean jsonPreprocessing;
 
-   public TranslationRequestPreProcessor(HttpServletRequest request, boolean jsonPreprocessing) {
-      this.request = request;
+   public TranslationResponsePreProcessor(MutableHttpServletResponse response, boolean jsonPreprocessing) {
+      this.response = response;
       this.jsonPreprocessing = jsonPreprocessing;
    }
 
    public InputStream getBodyStream() throws IOException {
-      final String contentType = request.getContentType();
+      final String contentType = response.getContentType();
       final InputStream result;
 
       switch (BodyContentMediaType.getMediaType(contentType)) {
          case XML:
-            result = request.getInputStream();
+            result = response.getInputStream();
             break;
          case JSON:
             if (jsonPreprocessing) {
-                result = getJsonProcessor().process(request.getInputStream());
+                result = getJsonProcessor().process(response.getInputStream());
             } else {
-                result = request.getInputStream();
+                result = response.getInputStream();
             }
             break;
          default:
-            result = getUnknownContentProcessor().process(request.getInputStream());
+            result = getUnknownContentProcessor().process(response.getInputStream());
       }
 
       return result;
