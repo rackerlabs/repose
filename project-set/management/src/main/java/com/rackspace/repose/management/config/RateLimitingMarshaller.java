4bd85ba8b5f28d691154fb3f41b0e9c27f7fd7fc
forcewhisperer <fran.torreslopez@rackspace.com>
2012-10-30 14:57:01 -0500

    Add initial REST API support for updating some Repose config files.

---
+++ project-set/management/src/main/java/com/rackspace/repose/management/config/RateLimitingMarshaller.java
@@ -0,0 +1,52 @@
+package com.rackspace.repose.management.config;
+
+import com.rackspace.repose.service.ratelimit.config.ObjectFactory;
+import com.rackspace.repose.service.ratelimit.config.RateLimitingConfiguration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fran
+ * Date: Oct 30, 2012
+ * Time: 1:58:43 PM
+ */
+public class RateLimitingMarshaller implements ReposeMarshaller {
+
+   private static final Logger LOG = LoggerFactory.getLogger(RateLimitingMarshaller.class);
+
+   private final JAXBContext jaxbContext = JAXBContext.newInstance(ReposeConfiguration.RATE_LIMITING.getConfigContextPath());
+   private final Marshaller marshaller = jaxbContext.createMarshaller();
+   private final ObjectFactory objectFactory = new ObjectFactory();
+
+   public RateLimitingMarshaller() throws JAXBException {
+      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+   }
+
+   @Override
+   public void marshal(String configurationRoot, Object config) throws FileNotFoundException, JAXBException {
+
+      if (!(config instanceof RateLimitingConfiguration)) {
+         // TODO: Clean up exception handling
+         throw new RuntimeException("The config object passed is not a RateLimitingConfiguration.");
+      }
+
+      marshaller.marshal(objectFactory.createRateLimiting((RateLimitingConfiguration) config), new FileOutputStream(configurationRoot + ReposeConfiguration.RATE_LIMITING.getConfigFilename()));
+
+      LOG.info("Created " + ReposeConfiguration.RATE_LIMITING.getConfigFilename() + " : " + config.toString());
+   }
+
+   @Override
+   public JAXBElement<RateLimitingConfiguration> unmarshal(String configurationRoot) throws JAXBException, FileNotFoundException {
+      return (JAXBElement<RateLimitingConfiguration>) jaxbContext.createUnmarshaller().unmarshal(new File(configurationRoot + ReposeConfiguration.RATE_LIMITING.getConfigFilename()));
+   }
+}
---
+++ project-set/management/src/main/java/com/rackspace/repose/management/config/ResponseMessagingMarshaller.java
@@ -0,0 +1,50 @@
+package com.rackspace.repose.management.config;
+
+import com.rackspace.papi.service.rms.config.ObjectFactory;
+import com.rackspace.papi.service.rms.config.ResponseMessagingConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: fran
+ * Date: Oct 30, 2012
+ * Time: 2:09:42 PM
+ */
+public class ResponseMessagingMarshaller implements ReposeMarshaller {
+
+   private static final Logger LOG = LoggerFactory.getLogger(SystemModelMarshaller.class);
+
+   private final JAXBContext jaxbContext = JAXBContext.newInstance(ReposeConfiguration.RMS.getConfigContextPath());
+   private final Marshaller marshaller = jaxbContext.createMarshaller();
+   private final ObjectFactory objectFactory = new ObjectFactory();
+
+   public ResponseMessagingMarshaller() throws JAXBException {
+      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+   }
+
+   @Override
+   public void marshal(String configurationRoot, Object config) throws FileNotFoundException, JAXBException {
+      if (!(config instanceof ResponseMessagingConfiguration)) {
+         // TODO: Clean up exception handling
+         throw new RuntimeException("The config object passed is not a ResponseMessagingConfiguration.");
+      }
+
+      marshaller.marshal(objectFactory.createResponseMessaging((ResponseMessagingConfiguration) config), new FileOutputStream(configurationRoot + ReposeConfiguration.RMS.getConfigFilename()));
+
+      LOG.info("Created " + ReposeConfiguration.RMS.getConfigFilename() + " : " + config.toString());
+   }
+
+   @Override
+   public JAXBElement<?> unmarshal(String configurationRoot) throws FileNotFoundException, JAXBException {
+      return (JAXBElement<ResponseMessagingConfiguration>) jaxbContext.createUnmarshaller().unmarshal(new File(configurationRoot + ReposeConfiguration.RMS.getConfigFilename()));
+   }
+}
