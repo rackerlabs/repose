e32f8238a7af1c3c4f67b2d60091d44640e48c86
Dan Daley <danial.daley@rackspace.com>
2012-11-07 15:18:52 -0600

    Resolve sonar issues

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/service/context/container/ContainerConfigurationServiceImpl.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/service/context/container/ContainerConfigurationServiceImpl.java
@@ -8,37 +8,38 @@ import java.util.List;
 
 @Component("containerConfigurationService")
 public class ContainerConfigurationServiceImpl implements ContainerConfigurationService {
-   private final List<Port> ports = new ArrayList<Port>();
-   private String viaValue;
-   protected int contentBodyReadLimit;
-   
-   public ContainerConfigurationServiceImpl() {
-   }
-   
-   public ContainerConfigurationServiceImpl(List<Port> ports, String via,int contentBodyReadLimit) {
-      this.ports.addAll(ports);
-      this.viaValue = via;
-      this.contentBodyReadLimit=contentBodyReadLimit;
-   }
-   
-   @Override
-   public List<Port> getPorts() {
-      return ports;
-   }
-   
-   @Override
-   public String getVia(){
-      return viaValue;
-   }
-   
-   @Override
-   public void setVia(String via){
-      this.viaValue = via;
-   }
-  
-   @Override
+
+    private final List<Port> ports = new ArrayList<Port>();
+    private String viaValue;
+    private int contentBodyReadLimit;
+
+    public ContainerConfigurationServiceImpl() {
+    }
+
+    public ContainerConfigurationServiceImpl(List<Port> ports, String via, int contentBodyReadLimit) {
+        this.ports.addAll(ports);
+        this.viaValue = via;
+        this.contentBodyReadLimit = contentBodyReadLimit;
+    }
+
+    @Override
+    public List<Port> getPorts() {
+        return ports;
+    }
+
+    @Override
+    public String getVia() {
+        return viaValue;
+    }
+
+    @Override
+    public void setVia(String via) {
+        this.viaValue = via;
+    }
+
+    @Override
     public int getContentBodyReadLimit() {
-        if (contentBodyReadLimit <=1 ) {
+        if (contentBodyReadLimit <= 1) {
             return 0;
         } else {
             return contentBodyReadLimit;
@@ -49,6 +50,4 @@ public class ContainerConfigurationServiceImpl implements ContainerConfiguration
     public void setContentBodyReadLimit(int value) {
         this.contentBodyReadLimit = value;
     }
-
-   
 }
