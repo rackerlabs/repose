79c3cbeac1fb0335511a69be826e394170aaa806
Dan Daley <danial.daley@rackspace.com>
2012-11-09 16:31:22 -0600

    Don't send request body unless request is PUT or POST

--- project-set/core/core-lib/src/test/java/com/rackspace/papi/service/proxy/jersey/JerseyRequestProcessorTest.java
+++ project-set/core/core-lib/src/test/java/com/rackspace/papi/service/proxy/jersey/JerseyRequestProcessorTest.java
@@ -50,6 +50,7 @@ public class JerseyRequestProcessorTest {
             when(request.getParameterValues(eq("param2"))).thenReturn(params2);
             when(request.getQueryString()).thenReturn(queryString);
             when(request.getInputStream()).thenReturn(input);
+            when(request.getMethod()).thenReturn("POST");
             
             processor = new JerseyRequestProcessor(request);
         }
