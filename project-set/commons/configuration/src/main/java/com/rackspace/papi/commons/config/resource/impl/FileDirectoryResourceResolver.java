33fed2816da467a7c8fb468ba141bd3f48442adf
Dan Daley <danial.daley@rackspace.com>
2012-11-19 08:18:52 -0600

    Resolve merge conflicts

--- project-set/commons/configuration/src/main/java/com/rackspace/papi/commons/config/resource/impl/FileDirectoryResourceResolver.java
+++ project-set/commons/configuration/src/main/java/com/rackspace/papi/commons/config/resource/impl/FileDirectoryResourceResolver.java
@@ -3,39 +3,45 @@ package com.rackspace.papi.commons.config.resource.impl;
 import com.rackspace.papi.commons.config.resource.ConfigurationResource;
 import com.rackspace.papi.commons.config.resource.ConfigurationResourceResolver;
 import com.rackspace.papi.commons.config.resource.ResourceResolutionException;
+import com.rackspace.papi.commons.util.StringUtilities;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  *
  * @author malconis
  */
-public class FileDirectoryResourceResolver implements ConfigurationResourceResolver{
+public class FileDirectoryResourceResolver implements ConfigurationResourceResolver {
 
    private final String configRoot;
-   
+
    public FileDirectoryResourceResolver(String configRoot) {
-      
+
       this.configRoot = configRoot;
    }
-   
-   public String getConfigRoot(){
+
+   public String getConfigRoot() {
       return this.configRoot;
    }
-   
-   
+
    @Override
-   public ConfigurationResource resolve(String resourceName){
-      final File spec = new File(configRoot, resourceName);
-        
-        try {
+   public ConfigurationResource resolve(String resourceName) {
+
+      try {
+
+         if (resourceName.contains("://")) {
+            return new BufferedURLConfigurationResource(new URL(resourceName));
+         } else {
+            final File spec = new File(configRoot, resourceName);
+
             return new BufferedURLConfigurationResource(spec.toURI().toURL());
-        } catch (MalformedURLException murle) {
-            throw new ResourceResolutionException("Unable to build URL for resource. Resource: " 
-                    + spec + ". Reason: " + murle.getMessage(), murle);
-        } catch (IllegalArgumentException ex){
-           throw new ResourceResolutionException("Unable to build URL for resource. Resource: "+ spec + ". Reason: " + ex.getMessage(), ex);
-        }
-   }   
-   
+         }
+      } catch (MalformedURLException murle) {
+         throw new ResourceResolutionException("Unable to build URL for resource. Resource: "
+                 + resourceName + ". Reason: " + murle.getMessage(), murle);
+      } catch (IllegalArgumentException ex) {
+         throw new ResourceResolutionException("Unable to build URL for resource. Resource: " + resourceName + ". Reason: " + ex.getMessage(), ex);
+      }
+   }
 }
