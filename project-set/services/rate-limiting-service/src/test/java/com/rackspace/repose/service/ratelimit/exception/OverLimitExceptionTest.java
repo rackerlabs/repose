c3605ff4ec3b19e6e0381d43234c63380610c2ea
Dan Daley <danial.daley@rackspace.com>
2012-12-07 10:22:43 -0600

    Return clone of date to address sonar issue.  Add unit test

---
+++ project-set/services/rate-limiting-service/src/test/java/com/rackspace/repose/service/ratelimit/exception/OverLimitExceptionTest.java
@@ -0,0 +1,52 @@
+package com.rackspace.repose.service.ratelimit.exception;
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+@RunWith(Enclosed.class)
+public class OverLimitExceptionTest {
+
+    public static class WhenCreatingExceptions {
+        private OverLimitException instance;
+        private static final String message = "message";
+        private static final String user = "user";
+        private static final Date date = new Date();
+        private static final int limit = 10;
+        private static final String configuredLimit = "5";
+        
+
+        @Before
+        public void setUp() {
+            instance = new OverLimitException(message, user, date, limit, configuredLimit);
+        }
+        
+        @Test
+        public void shouldStoreMessage() {
+            assertEquals(message, instance.getMessage());
+        }
+        
+        @Test
+        public void shouldStoreUser() {
+            assertEquals(user, instance.getUser());
+        }
+        
+        @Test
+        public void shouldStoreNextDate() {
+            assertEquals(date, instance.getNextAvailableTime());
+        }
+        
+        @Test
+        public void shouldReturnCloneOfDate() {
+            instance.getNextAvailableTime().setTime(100);
+            assertEquals(date, instance.getNextAvailableTime());
+        }
+    }
+
+}
