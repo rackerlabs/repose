a18b9146f6f7f54f0a8748d2c1ce8eb981b51809
Richard Sartor <richard.sartor@rackspace.com>
2012-12-28 16:39:27 -0600

    Extracted the check_responses function out into a separate 'validator' module.

--- test/qe/api-validator-multimatch.py
+++ test/qe/api-validator-multimatch.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 
-import requests
-import sys
 import argparse
-import re
-import itertools
+import validator
 
 parser = argparse.ArgumentParser()
 parser.add_argument(metavar='target-addr', dest='target_addr', help='Hostname or IP address of the target Repose node')
@@ -14,82 +11,51 @@ parser.add_argument('--print-bad-response', help='Print out the response if it f
 
 args = parser.parse_args()
 
-
-def check_responses(path, roles_and_responses, responses=None):
-    url = '%s://%s:%i/%s' % (args.protocol, args.target_addr, args.target_port, path)
-
-    correct = 0
-    incorrect = 0
-    results = []
-
-    for role, code in sorted(roles_and_responses.items()):
-
-        resp = requests.get(url, headers = { 'X-Roles': role })
-
-        if re.match(str(code), str(resp.status_code)) == None:
-            incorrect += 1
-            c = 'INCORRECT'
-            is_correct = False
-        else:
-            correct += 1
-            c = 'CORRECT'
-            is_correct = True
-
-        print 'Get %s with role "%s": expected %s, got %i -> %s' % (url, role, code, resp.status_code, c)
-
-        if not is_correct and args.print_bad_response:
-            print resp.content
-
-        results.append(is_correct)
-        if responses != None:
-            responses.append(resp)
-
-    return results
-
-def count_true(*iterables):
-    c = 0
-    for x in itertools.ifilter(None, itertools.chain(*iterables)): c += 1
-    return c
-
-def count_false(*iterables):
-    c = 0
-    for x in itertools.ifilterfalse(None, itertools.chain(*iterables)): c += 1
-    return c
+protocol = args.protocol
+host = args.target_addr
+port = args.target_port
+pbr = args.print_bad_response
 
 res = []
-res.append(check_responses('multimatch/sspnn', { 'role-0':403,
-                                                 'role-1':405,
-                                                 'role-2':405,
-                                                 'role-3':200,
-                                                 'role-4':404,
-                                                 'role-5':404,
-                                                 'role-2,role-3': 405,
-                                                 'role-3,role-4': 200 }))
+res.append(validator.check_responses(host, 'multimatch/sspnn', { 'role-0':403,
+                                                                 'role-1':405,
+                                                                 'role-2':405,
+                                                                 'role-3':200,
+                                                                 'role-4':404,
+                                                                 'role-5':404,
+                                                                 'role-2,role-3': 405,
+                                                                 'role-3,role-4': 200 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-res.append(check_responses('multimatch/p', { 'role-0':403, 'role-1':200 }))
+res.append(validator.check_responses(host, 'multimatch/p', { 'role-0':403, 'role-1':200 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-res.append(check_responses('multimatch/f', { 'role-0':403, 'role-1':405 }))
+res.append(validator.check_responses(host, 'multimatch/f', { 'role-0':403, 'role-1':405 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-res.append(check_responses('multimatch/mssfsffpnn', { 'role-0':403,
-                                                      'role-1':405,
-                                                      'role-2':405,
-                                                      'role-3':405,
-                                                      'role-4':405,
-                                                      'role-5':405,
-                                                      'role-6':405,
-                                                      'role-7':200,
-                                                      'role-8':404,
-                                                      'role-9':404,
-                                                      'role-3,role-5,role-6,role-7':200,
-                                                      'role-3,role-5,role-6':405,
-                                                      'role-7,role-8':200 }))
+res.append(validator.check_responses(host, 'multimatch/mssfsffpnn', { 'role-0':403,
+                                                                      'role-1':405,
+                                                                      'role-2':405,
+                                                                      'role-3':405,
+                                                                      'role-4':405,
+                                                                      'role-5':405,
+                                                                      'role-6':405,
+                                                                      'role-7':200,
+                                                                      'role-8':404,
+                                                                      'role-9':404,
+                                                                      'role-3,role-5,role-6,role-7':200,
+                                                                      'role-3,role-5,role-6':405,
+                                                                      'role-7,role-8':200 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-res.append(check_responses('multimatch/mp', { 'role-0':403, 'role-1':200 }))
+res.append(validator.check_responses(host, 'multimatch/mp', { 'role-0':403, 'role-1':200 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-res.append(check_responses('multimatch/mf', { 'role-0':403, 'role-1':405 }))
+res.append(validator.check_responses(host, 'multimatch/mf', { 'role-0':403, 'role-1':405 },
+                                                                 protocol=protocol, port=port, print_bad_responses=pbr))
 
-total_correct = count_true(*res)
-total_incorrect = count_false(*res)
+total_correct = validator.count_true(*res)
+total_incorrect = validator.count_false(*res)
 
 print '%i correct' % total_correct
 print '%i incorrect' % total_incorrect
---
+++ test/qe/validator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+import re
+import itertools
+
+def check_responses(host, path, roles_and_responses, responses=None, protocol='http', port='8080', print_bad_responses=False):
+    url = '%s://%s:%i/%s' % (protocol, host, port, path)
+
+    correct = 0
+    incorrect = 0
+    results = []
+
+    for role, code in sorted(roles_and_responses.items()):
+
+        resp = requests.get(url, headers = { 'X-Roles': role })
+
+        if re.match(str(code), str(resp.status_code)) == None:
+            incorrect += 1
+            c = 'INCORRECT'
+            is_correct = False
+        else:
+            correct += 1
+            c = 'CORRECT'
+            is_correct = True
+
+        print 'Get %s with role "%s": expected %s, got %i -> %s' % (url, role, code, resp.status_code, c)
+
+        if not is_correct and print_bad_responses:
+            print resp.content
+
+        results.append(is_correct)
+        if responses != None:
+            responses.append(resp)
+
+    return results
+
+def count_true(*iterables):
+    c = 0
+    for x in itertools.ifilter(None, itertools.chain(*iterables)): c += 1
+    return c
+
+def count_false(*iterables):
+    c = 0
+    for x in itertools.ifilterfalse(None, itertools.chain(*iterables)): c += 1
+    return c
+
