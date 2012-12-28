e46523de06efee4f057db9518b671835c9091091
Richard Sartor <richard.sartor@rackspace.com>
2012-12-28 17:13:04 -0600

    Push the path literals down to the multimatch module.

--- test/qe/multimatch.py
+++ test/qe/multimatch.py
@@ -2,7 +2,7 @@
 import validator
 
 
-def check_sspnn(protocol, host, port, path, pbr):
+def check_sspnn(protocol, host, port, pbr):
     roles_and_responses = {
         'role-0': 403,
         'role-1': 405,
@@ -13,27 +13,27 @@ def check_sspnn(protocol, host, port, path, pbr):
         'role-2,role-3': 405,
         'role-3,role-4': 200
         }
-    return validator.check_responses(host, path,
+    return validator.check_responses(host, 'multimatch/sspnn',
                                      roles_and_responses,
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
 
 
-def check_p(protocol, host, port, path, pbr):
-    return validator.check_responses(host, path,
+def check_p(protocol, host, port, pbr):
+    return validator.check_responses(host, 'multimatch/p',
                                      {'role-0': 403, 'role-1': 200},
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
 
 
-def check_f(protocol, host, port, path, pbr):
-    return validator.check_responses(host, path,
+def check_f(protocol, host, port, pbr):
+    return validator.check_responses(host, 'multimatch/f',
                                      {'role-0': 403, 'role-1': 405},
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
 
 
-def check_mssfsffpnn(protocol, host, port, path, pbr):
+def check_mssfsffpnn(protocol, host, port, pbr):
     roles_and_responses = {
         'role-0': 403,
         'role-1': 405,
@@ -49,21 +49,21 @@ def check_mssfsffpnn(protocol, host, port, path, pbr):
         'role-3,role-5,role-6': 405,
         'role-7,role-8': 200
         }
-    return validator.check_responses(host, path,
+    return validator.check_responses(host, 'multimatch/mssfsffpnn',
                                      roles_and_responses,
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
 
 
-def check_mp(protocol, host, port, path, pbr):
-    return validator.check_responses(host, path,
+def check_mp(protocol, host, port, pbr):
+    return validator.check_responses(host, 'multimatch/mp',
                                      {'role-0': 403, 'role-1': 200},
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
 
 
-def check_mf(protocol, host, port, path, pbr):
-    return validator.check_responses(host, path,
+def check_mf(protocol, host, port, pbr):
+    return validator.check_responses(host, 'multimatch/mf',
                                      {'role-0': 403, 'role-1': 405},
                                      protocol=protocol, port=port,
                                      print_bad_responses=pbr)
