363c42b3dd1b0b570bccfdbb20bec259628e2c0e
Kushala Kondapally <kushala.kondapally@rackspace.com>
2013-01-15 15:56:50 -0600

    Sonar voilation fixes

--- project-set/components/http-logging/src/main/java/com/rackspace/papi/components/logging/util/FileLogger.java
+++ project-set/components/http-logging/src/main/java/com/rackspace/papi/components/logging/util/FileLogger.java
@@ -13,12 +13,13 @@ import java.nio.charset.Charset;
  *
  * @author jhopper
  */
+
 public class FileLogger implements SimpleLogger {
 
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(FileLogger.class);
-    private static final ByteBuffer NEWLINE = ByteBuffer.wrap("\n".getBytes());
-    static final int BUFFER_LIMIT = 2048;
     private static final Charset CHAR_SET = Charset.forName("UTF-8");
+    private static final ByteBuffer NEWLINE = ByteBuffer.wrap("\n".getBytes(CHAR_SET));
+    static final int BUFFER_LIMIT = 2048;    
     private final ByteBuffer buffer;
     private final File f;
 
