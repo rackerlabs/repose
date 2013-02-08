6956b6b3f53b72082a4f19bc32bdff9cfc3b2ce7
Richard Sartor <richard.sartor@rackspace.com>
2013-02-08 16:19:01 -0600

    Added the TestPortsOnCommandLineNone test case class, to test the case when no port is given on the command line.

--- test/qe/valve-self-configure/test_sysmod_vs_cmdline.py
+++ test/qe/valve-self-configure/test_sysmod_vs_cmdline.py
@@ -143,6 +143,34 @@ class TestPortsOnCommandLineHttpDiff(TestPortsOnCommandLineBase,
                           url)
 
 
+class TestPortsOnCommandLineNone(TestPortsOnCommandLineBase,
+                                 unittest.TestCase):
+    def start_repose(self):
+        return repose.ReposeValve(config_dir=config_dir,
+                                  stop_port=stop_port)
+
+    def init_params(self):
+        self.proto = 'http'
+        self.sysmod_port = 8888
+        self.cmd_line_port = 8889
+
+    def runTest(self):
+        logger.debug('runTest')
+
+        # test port in the system model
+        url = '%s://localhost:%i/' % (self.params['proto'], self.sysmod_port)
+        logger.debug('runTest: sysmod url = %s' % url)
+        status_code = get_status_code_from_url(url)
+        logger.debug('runTest: sysmod status_code = %i' % status_code)
+        self.assertEqual(status_code, 200)
+
+        # test port specified at the command line
+        url = '%s://localhost:%i/' % (self.params['proto'], self.cmd_line_port)
+        logger.debug('runTest: con url = %s' % url)
+        self.assertRaises(requests.ConnectionError, get_status_code_from_url,
+                          url)
+
+
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--print-log', help="Print the log to STDERR.",
@@ -162,6 +190,7 @@ def run():
     suite.addTest(load_tests(TestPortsOnCommandLineHttpSame))
     suite.addTest(load_tests(TestPortsOnCommandLineHttpsSame))
     suite.addTest(load_tests(TestPortsOnCommandLineHttpDiff))
+    suite.addTest(load_tests(TestPortsOnCommandLineNone))
 
     testRunner = _xmlrunner.XMLTestRunner(output='test-reports')
 
