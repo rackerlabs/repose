0883a40017b3835af6359d73b05332d6d1763192
Richard Sartor <richard.sartor@rackspace.com>
2013-02-08 15:38:03 -0600

    Renamed the scripts and config files to be more descriptive.

--- test/qe/valve-self-configure/test_sysmod_vs_cmdline.py
+++ test/qe/valve-self-configure/test_sysmod_vs_cmdline.py
@@ -41,7 +41,7 @@ def apply_config_set(config_set_name, params=None):
                             destination_path=config_dir, params=params)
 
 
-class TestPortsInContainerBase:
+class TestPortsOnCommandLineBase:
     def setUp(self):
         logger.debug('setUp')
 
@@ -53,16 +53,15 @@ class TestPortsInContainerBase:
             'sysmod_port': self.sysmod_port,
             'target_hostname': target_hostname,
             'target_port': target_port,
-            'con_port': self.con_port,
+            'cmd_line_port': self.cmd_line_port,
             'deploy_dir': deploy_dir,
             'artifact_dir': artifact_dir,
             'log_file': log_file
         }
         apply_config_set('valve-self-common', params=self.params)
-        apply_config_set('valve-self-1-common', params=self.params)
-        apply_config_set(self.main_config_set_name, params=self.params)
-        self.repose = repose.ReposeValve(config_dir=config_dir,
-                                         stop_port=stop_port)
+        apply_config_set('single-node', params=self.params)
+        apply_config_set('container-no-port', params=self.params)
+        self.repose = self.start_repose()
         time.sleep(20)
 
     def tearDown(self):
@@ -81,83 +80,41 @@ class TestPortsInContainerBase:
         logger.debug('runTest: sysmod status_code = %i' % status_code)
         self.assertEqual(status_code, 200)
 
-        # test port in the container
-        url = '%s://localhost:%i/' % (self.params['proto'], self.con_port)
+        # test port specified at the command line
+        url = '%s://localhost:%i/' % (self.params['proto'], self.cmd_line_port)
         logger.debug('runTest: con url = %s' % url)
         status_code = get_status_code_from_url(url)
         logger.debug('runTest: con status_code = %i' % status_code)
         self.assertEqual(status_code, 200)
 
 
-class TestPortsInContainerHttpSame(TestPortsInContainerBase,
-                                   unittest.TestCase):
+class TestPortsOnCommandLineHttpSame(TestPortsOnCommandLineBase,
+                                     unittest.TestCase):
+    def start_repose(self):
+        return repose.ReposeValve(config_dir=config_dir,
+                                  port=self.cmd_line_port,
+                                  stop_port=stop_port)
+
     def init_params(self):
         self.proto = 'http'
         self.sysmod_port = 8888
-        self.con_port = 8888
-        self.main_config_set_name = 'valve-self-1-with-con-port'
+        self.cmd_line_port = 8888
 
 
-class TestPortsInContainerHttpsSame(TestPortsInContainerBase,
-                                    unittest.TestCase):
+class TestPortsOnCommandLineHttpsSame(TestPortsOnCommandLineBase,
+                                      unittest.TestCase):
+    def start_repose(self):
+        return repose.ReposeValve(config_dir=config_dir,
+                                  https_port=self.cmd_line_port,
+                                  stop_port=stop_port)
+
     def init_params(self):
         self.proto = 'https'
         self.sysmod_port = 8888
-        self.con_port = 8888
-        self.main_config_set_name = 'valve-self-1-with-con-port'
-
-
-class TestPortsInContainerHttpDiff(TestPortsInContainerBase,
-                                   unittest.TestCase):
-    def init_params(self):
-        self.proto = 'http'
-        self.sysmod_port = 8888
-        self.con_port = 8889
-        self.main_config_set_name = 'valve-self-1-with-con-port'
-
-    def runTest(self):
-        logger.debug('runTest')
-
-        # test port in the system model
-        url = '%s://localhost:%i/' % (self.params['proto'], self.sysmod_port)
-        logger.debug('runTest: sysmod url = %s' % url)
-        status_code = get_status_code_from_url(url)
-        logger.debug('runTest: sysmod status_code = %i' % status_code)
-        self.assertEqual(status_code, 200)
-
-        # test port in the container
-        url = '%s://localhost:%i/' % (self.params['proto'], self.con_port)
-        logger.debug('runTest: con url = %s' % url)
-        self.assertRaises(requests.ConnectionError, get_status_code_from_url,
-                          url)
-
-
-class TestPortsInContainerNone(TestPortsInContainerBase, unittest.TestCase):
-    def init_params(self):
-        self.proto = 'http'
-        self.sysmod_port = 8888
-        self.con_port = 8889
-        self.main_config_set_name = 'valve-self-1-no-con-port'
-
-    def runTest(self):
-        logger.debug('runTest')
-
-        # test port in the system model
-        url = '%s://localhost:%i/' % (self.params['proto'], self.sysmod_port)
-        logger.debug('runTest: sysmod url = %s' % url)
-        status_code = get_status_code_from_url(url)
-        logger.debug('runTest: sysmod status_code = %i' % status_code)
-        self.assertEqual(status_code, 200)
-
-        # test port in the container
-        url = '%s://localhost:%i/' % (self.params['proto'], self.con_port)
-        logger.debug('runTest: con url = %s' % url)
-        self.assertRaises(requests.ConnectionError, get_status_code_from_url,
-                          url)
+        self.cmd_line_port = 8888
 
 
 def run():
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--print-log', help="Print the log to STDERR.",
                         action='store_true')
@@ -173,10 +130,8 @@ def run():
 
     loader = unittest.TestLoader()
     load_tests = loader.loadTestsFromTestCase
-    suite.addTest(load_tests(TestPortsInContainerHttpSame))
-    suite.addTest(load_tests(TestPortsInContainerHttpsSame))
-    suite.addTest(load_tests(TestPortsInContainerHttpDiff))
-    suite.addTest(load_tests(TestPortsInContainerNone))
+    suite.addTest(load_tests(TestPortsOnCommandLineHttpSame))
+    suite.addTest(load_tests(TestPortsOnCommandLineHttpsSame))
 
     testRunner = _xmlrunner.XMLTestRunner(output='test-reports')
 
