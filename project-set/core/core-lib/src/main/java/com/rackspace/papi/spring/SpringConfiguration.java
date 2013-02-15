87dbd674798958e73c09be11c16fa30ec9062976
Dan Daley <danial.daley@rackspace.com>
2013-02-15 15:07:31 -0600

    Make header parameter parsing more lenient

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/spring/SpringConfiguration.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/spring/SpringConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:/spring/repose-core-context.xml")
+@ImportResource({"classpath:/spring/repose-core-context.xml"})
 public class SpringConfiguration {
    
 }
