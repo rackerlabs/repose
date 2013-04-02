676ff3c00b4d38fd712dcef908b9298a8944a74c
sethbrayman <sethbrayman@gmail.com>
2013-04-02 12:53:49 -0500

    Formatting.

--- project-set/core/core-lib/src/main/java/com/rackspace/papi/service/datastore/impl/ehcache/EHCacheDatastore.java
+++ project-set/core/core-lib/src/main/java/com/rackspace/papi/service/datastore/impl/ehcache/EHCacheDatastore.java
@@ -15,49 +15,50 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.TimeUnit;
 
 @Component("ehCacheDatastore")
-@ManagedResource(objectName = "com.rackspace.papi.service.datastore.impl.ehcache.EHCacheDatastore", description = "EHCacheDatastore MBean.")
+@ManagedResource(objectName = "com.rackspace.papi.service.datastore.impl.ehcache.EHCacheDatastore",
+                 description = "EHCacheDatastore MBean.")
 public class EHCacheDatastore implements Datastore, EHCacheDatastoreMBean {
 
-   private final Cache ehCacheInstance;
+    private final Cache ehCacheInstance;
 
     @Autowired
-   public EHCacheDatastore(@Qualifier("ehCacheInstance") Cache ehCacheInstance) {
-      this.ehCacheInstance = ehCacheInstance;
-   }
+    public EHCacheDatastore(@Qualifier("ehCacheInstance") Cache ehCacheInstance) {
+        this.ehCacheInstance = ehCacheInstance;
+    }
 
     @ManagedOperation
     public void removeAllCachedData() {
         ehCacheInstance.removeAll();
     }
 
-   @Override
-   public StoredElement get(String key) throws DatastoreOperationException {
-      final Element element = ehCacheInstance.get(key);
+    @Override
+    public StoredElement get(String key) throws DatastoreOperationException {
+        final Element element = ehCacheInstance.get(key);
 
-      if (element != null) {
-         return new StoredElementImpl(key, (byte[]) element.getValue());
-      }
+        if (element != null) {
+            return new StoredElementImpl(key, (byte[]) element.getValue());
+        }
 
-      return new StoredElementImpl(key, null);
-   }
+        return new StoredElementImpl(key, null);
+    }
 
-   @Override
-   public boolean remove(String key) throws DatastoreOperationException {
-      return ehCacheInstance.remove(key);
-   }
+    @Override
+    public boolean remove(String key) throws DatastoreOperationException {
+        return ehCacheInstance.remove(key);
+    }
 
-   @Override
-   public void put(String key, byte[] value) throws DatastoreOperationException {
-      ehCacheInstance.put(new Element(key, value));
-   }
+    @Override
+    public void put(String key, byte[] value) throws DatastoreOperationException {
+        ehCacheInstance.put(new Element(key, value));
+    }
 
-   @Override
-   public void put(String key, byte[] value, int ttl, TimeUnit timeUnit) throws DatastoreOperationException {
-      final Element putMe = new Element(key, value);
-      putMe.setTimeToLive((int) TimeUnit.SECONDS.convert(ttl, timeUnit));
+    @Override
+    public void put(String key, byte[] value, int ttl, TimeUnit timeUnit) throws DatastoreOperationException {
+        final Element putMe = new Element(key, value);
+        putMe.setTimeToLive((int) TimeUnit.SECONDS.convert(ttl, timeUnit));
 
-      ehCacheInstance.put(putMe);
-   }
+        ehCacheInstance.put(putMe);
+    }
 
     @Override
     public boolean remove(String key, boolean notify) throws DatastoreOperationException {
@@ -70,7 +71,8 @@ public class EHCacheDatastore implements Datastore, EHCacheDatastoreMBean {
     }
 
     @Override
-    public void put(String key, byte[] value, int ttl, TimeUnit timeUnit, boolean notify) throws DatastoreOperationException {
+    public void put(String key, byte[] value, int ttl, TimeUnit timeUnit, boolean notify)
+            throws DatastoreOperationException {
         put(key, value, ttl, timeUnit);
     }
 }
