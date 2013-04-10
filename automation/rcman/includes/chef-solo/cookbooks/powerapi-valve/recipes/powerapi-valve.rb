fe9760a850115dece36e9492ee07b46b3a546712
Kushala Kondapally <kushala.kondapally@rackspace.com>
2013-04-10 14:23:59 -0500

    Changed get groups Jmeter tests to use valuve group2

--- automation/rcman/includes/chef-solo/cookbooks/powerapi-valve/recipes/powerapi-valve.rb
+++ automation/rcman/includes/chef-solo/cookbooks/powerapi-valve/recipes/powerapi-valve.rb
@@ -105,7 +105,7 @@ for valveGroup in 1..3
       end
       #Client Auth for RS Cloud Auth 1.1 And Client Auth for OpenStack Identity
 
-      ["client-auth-keystone.cfg.xml","client-auth-keystone-no-groups.cfg.xml", "client-auth-v1.1.cfg.xml","client-auth-v1.1-no-groups.cfg.xml","dist-datastore.cfg.xml"].each do |config|
+      ["client-auth-keystone.cfg.xml", "client-auth-v1.1.cfg.xml","dist-datastore.cfg.xml"].each do |config|
         cookbook_file "/etc/repose/valveGroup1/#{config}" do
           source "/valveGroup1/#{config}"
           mode 0644
@@ -114,7 +114,7 @@ for valveGroup in 1..3
 
       end
 
-      ["header-normalization.cfg.xml", "uri-normalization.cfg.xml","openstack-authorization.cfg.xml","openstack-authorization-no-groups.cfg.xml"].each do |config|
+      ["header-normalization.cfg.xml", "uri-normalization.cfg.xml","openstack-authorization.cfg.xml"].each do |config|
         cookbook_file "/etc/repose/valveGroup1/#{config}" do
           source "#{config}"
           mode 0644
@@ -137,7 +137,7 @@ for valveGroup in 1..3
       ["uri-identity.cfg.xml", "content-normalization.cfg.xml", "response-messaging.cfg.xml", "ip-identity.cfg.xml",
        "header-identity.cfg.xml", "rate-limiting.cfg.xml", "rate-limiting-2.cfg.xml", "dist-datastore.cfg.xml",
        "responsefor5xx", "content-identity-auth-1-1.cfg.xml", "header-id-mapping.cfg.xml", "default.wadl",
-       "group1.wadl", "group2.wadl", "test.xsd", "validator.cfg.xml", "keystone-auth.cfg.xml"].each do |config|
+       "group1.wadl", "group2.wadl", "test.xsd", "validator.cfg.xml", "keystone-auth.cfg.xml","client-auth-keystone-no-groups.cfg.xml","client-auth-v1.1-no-groups.cfg.xml","openstack-authorization-no-groups.cfg.xml"].each do |config|
         cookbook_file "/etc/repose/valveGroup2/#{config}" do
           source "/valveGroup2/#{config}"
           mode 0644
