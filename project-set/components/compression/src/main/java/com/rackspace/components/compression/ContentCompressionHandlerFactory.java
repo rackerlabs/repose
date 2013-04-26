363f22eb79568c0f538dc4a71be92cc7db6a22ca
Meynard Alconis <meynard.alconis@rackspace.com>
2013-04-26 15:23:58 -0500

    Added xsd checks to compression filter. users not allowed to configure include-content-types and exclude-content-types simultaniously. the same with include-user-agents and exclude-user-agents

--- project-set/components/compression/src/main/java/com/rackspace/components/compression/ContentCompressionHandlerFactory.java
+++ project-set/components/compression/src/main/java/com/rackspace/components/compression/ContentCompressionHandlerFactory.java
@@ -16,42 +16,63 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 public class ContentCompressionHandlerFactory extends AbstractConfiguredFilterHandlerFactory<ContentCompressionHandler> {
-   
+
    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ContentCompressionHandlerFactory.class);
    private CompressionConfigWrapper config;
    private Compression contentCompressionConfig;
    private CompressingFilter filter;
    private ContentCompressionHandler handler;
-   
+
    public ContentCompressionHandlerFactory(FilterConfig config) {
-      
+
       this.config = new CompressionConfigWrapper(config);
       this.config.setInitParameter(CompressionParameters.STATS_ENABLED.getParam(), "true");
       this.config.setInitParameter(CompressionParameters.JAVA_UTIL_LOGGER.getParam(), LOG.getName());
       this.config.setInitParameter(CompressionParameters.DEBUG.getParam(), "true");
       try {
-         
-         
+
+
          filter = new CompressingFilter();
          filter.init(config);
       } catch (ServletException ex) {
          Logger.getLogger(ContentCompressionHandlerFactory.class.getName()).log(Level.SEVERE, null, ex);
       }
    }
-   
+
    private class ContentCompressionConfigurationListener implements UpdateListener<ContentCompressionConfig> {
-      
+
       private boolean isInitialized = false;
-      
+
       @Override
       public void configurationUpdated(ContentCompressionConfig configurationObject) {
          contentCompressionConfig = configurationObject.getCompression();
-         
+
          config.setInitParameter(CompressionParameters.STATS_ENABLED.getParam(), String.valueOf(contentCompressionConfig.isStatsEnabled()));
          config.setInitParameter(CompressionParameters.JAVA_UTIL_LOGGER.getParam(), LOG.getName());
          config.setInitParameter(CompressionParameters.DEBUG.getParam(), String.valueOf(contentCompressionConfig.isDebug()));
+         config.setInitParameter(CompressionParameters.COMPRESSION_THRESHHOLD.getParam(), String.valueOf(contentCompressionConfig.getCompressionThreshold()));
+         if (!contentCompressionConfig.getIncludeContentTypes().isEmpty()) {
+            config.setInitParameter(CompressionParameters.INCLUDE_CONTENT_TYPES.getParam(), StringUtils.collectionToCommaDelimitedString(contentCompressionConfig.getIncludeContentTypes()));
+
+         }
+         if (!contentCompressionConfig.getIncludeContentTypes().isEmpty()) {
+            config.setInitParameter(CompressionParameters.EXCLUDE_CONTENT_TYPES.getParam(), StringUtils.collectionToCommaDelimitedString(contentCompressionConfig.getExcludeContentTypes()));
+
+         }
+         
+         if (!contentCompressionConfig.getIncludeUserAgentPatterns().isEmpty()) {
+            config.setInitParameter(CompressionParameters.INCLUDE_USER_AGENT_PATTERNS.getParam(), StringUtils.collectionToCommaDelimitedString(contentCompressionConfig.getIncludeUserAgentPatterns()));
+
+         }
+         if (!contentCompressionConfig.getIncludeContentTypes().isEmpty()) {
+            config.setInitParameter(CompressionParameters.INCLUDE_USER_AGENT_PATTERNS.getParam(), StringUtils.collectionToCommaDelimitedString(contentCompressionConfig.getExcludeUserAgentPatterns()));
+
+         }
+         
+         
          filter = new CompressingFilter();
          try {
             filter.init(config);
@@ -59,32 +80,32 @@ public class ContentCompressionHandlerFactory extends AbstractConfiguredFilterHa
          } catch (ServletException ex) {
             LOG.error("Unable to initialize content compression filter");
          }
-         
-         
+
+
       }
-      
+
       @Override
       public boolean isInitialized() {
          return isInitialized;
       }
    }
-   
+
    @Override
    protected ContentCompressionHandler buildHandler() {
-      
+
       if (!this.isInitialized()) {
-            return null;
-        }else{
+         return null;
+      } else {
          return new ContentCompressionHandler(filter);
       }
    }
-   
+
    @Override
    protected Map<Class, UpdateListener<?>> getListeners() {
       return new HashMap<Class, UpdateListener<?>>() {
-            {
-                put(ContentCompressionConfig.class, new ContentCompressionConfigurationListener());
-            }
-        };
+         {
+            put(ContentCompressionConfig.class, new ContentCompressionConfigurationListener());
+         }
+      };
    }
 }
