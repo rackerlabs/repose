0ec10b645aa2f89f47d48817dabdcd41be531400
Damien Johnson <damien.johnson@rackspace.com>
2013-07-02 15:41:31 -0500

    B-48285 Initial structure in place, need to mark and fix string identifiers

---
+++ project-set/components/destination-router/src/main/java/org/openrepose/components/routing/servlet/DestinationRouter.java
@@ -0,0 +1,7 @@
+package org.openrepose.components.routing.servlet;
+
+/**
+ * This class is used to register the destination-router-related metrics to JMX.
+ */
+public class DestinationRouter {
+}
--- project-set/components/destination-router/src/main/java/org/openrepose/components/routing/servlet/RoutingTagger.java
+++ project-set/components/destination-router/src/main/java/org/openrepose/components/routing/servlet/RoutingTagger.java
@@ -6,19 +6,34 @@ import com.rackspace.papi.filter.logic.FilterAction;
 import com.rackspace.papi.filter.logic.FilterDirector;
 import com.rackspace.papi.filter.logic.common.AbstractFilterLogicHandler;
 import com.rackspace.papi.filter.logic.impl.FilterDirectorImpl;
+import com.rackspace.papi.service.reporting.metrics.MetricsService;
+import com.rackspace.papi.service.reporting.metrics.impl.MeterByCategorySum;
 import org.slf4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 public class RoutingTagger extends AbstractFilterLogicHandler {
 
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(RoutingTagger.class);
     private String id;
     private double quality;
+    private boolean useMetrics = false;
+    private MetricsService metricsService;
+    private MeterByCategorySum mbcs;
 
-    public RoutingTagger(String id, double quality) {
+    public RoutingTagger(String id, double quality, MetricsService metricsService) {
         this.quality = quality;
         this.id = id;
+        this.metricsService = metricsService;
+
+        // TODO
+        try {
+            mbcs = metricsService.newMeterByCategorySum(DestinationRouter.class, "destination-router", "Routed Response", TimeUnit.SECONDS);
+            useMetrics = true;
+        } catch (Exception e) {
+            LOG.error("blahblahblah", e);
+        }
     }
 
     @Override
