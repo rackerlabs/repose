38d7a39adbaf6a3901989573dc85527d590eec60
Richard Sartor <richard.sartor@rackspace.com>
2013-07-19 15:09:34 -0500

    Removed common base class.

--- test/qe/valve-self-configure/test_valve_self_configure.py
+++ test/qe/valve-self-configure/test_valve_self_configure.py
@@ -385,7 +385,20 @@ class TestStartWithZeroNodes(unittest.TestCase):
         self.make_request_and_assert_status_code(url, 200)
 
 
-class TestPortsOnCommandLineBase:
+
+
+class TestPortsOnCommandLineHttpSame(unittest.TestCase):
+    def start_valve(self):
+        return valve.Valve(config_dir=config_dir,
+                                  port=self.cmd_line_port,
+                                  stop_port=self.stop_port)
+
+    def init_params(self):
+        self.proto = 'http'
+        self.sysmod_port = get_next_open_port()
+        self.cmd_line_port = self.sysmod_port
+        self.stop_port = get_next_open_port()
+
     def setUp(self):
         logger.debug('setUp')
 
@@ -432,22 +445,7 @@ class TestPortsOnCommandLineBase:
         self.assertEqual(status_code, 200)
 
 
-class TestPortsOnCommandLineHttpSame(TestPortsOnCommandLineBase,
-                                     unittest.TestCase):
-    def start_valve(self):
-        return valve.Valve(config_dir=config_dir,
-                                  port=self.cmd_line_port,
-                                  stop_port=self.stop_port)
-
-    def init_params(self):
-        self.proto = 'http'
-        self.sysmod_port = get_next_open_port()
-        self.cmd_line_port = self.sysmod_port
-        self.stop_port = get_next_open_port()
-
-
-class TestPortsOnCommandLineHttpsSame(TestPortsOnCommandLineBase,
-                                      unittest.TestCase):
+class TestPortsOnCommandLineHttpsSame(unittest.TestCase):
     def start_valve(self):
         return valve.Valve(config_dir=config_dir,
                                   https_port=self.cmd_line_port,
@@ -459,9 +457,53 @@ class TestPortsOnCommandLineHttpsSame(TestPortsOnCommandLineBase,
         self.cmd_line_port = self.sysmod_port
         self.stop_port = get_next_open_port()
 
+    def setUp(self):
+        logger.debug('setUp')
 
-class TestPortsOnCommandLineHttpDiff(TestPortsOnCommandLineBase,
-                                     unittest.TestCase):
+        self.init_params()
+
+        pathutil.clear_folder(config_dir)
+        self.params = {
+            'proto': self.proto,
+            'sysmod_port': self.sysmod_port,
+            'target_hostname': target_hostname,
+            'target_port': target_port,
+            'cmd_line_port': self.cmd_line_port,
+            'deploy_dir': deploy_dir,
+            'artifact_dir': artifact_dir,
+            'log_file': log_file
+        }
+        apply_config_set('valve-self-common', params=self.params)
+        apply_config_set('single-node-with-proto', params=self.params)
+        apply_config_set('container-no-port', params=self.params)
+        self.valve = self.start_valve()
+        time.sleep(sleep_duration)
+
+    def tearDown(self):
+        logger.debug('tearDown')
+        if self.valve is not None:
+            self.valve.stop()
+            time.sleep(5)
+
+    def runTest(self):
+        logger.debug('runTest')
+
+        # test port in the system model
+        url = '%s://localhost:%i/' % (self.params['proto'], self.sysmod_port)
+        logger.debug('runTest: sysmod url = %s' % url)
+        status_code = get_status_code_from_url(url)
+        logger.debug('runTest: sysmod status_code = %i' % status_code)
+        self.assertEqual(status_code, 200)
+
+        # test port specified at the command line
+        url = '%s://localhost:%i/' % (self.params['proto'], self.cmd_line_port)
+        logger.debug('runTest: con url = %s' % url)
+        status_code = get_status_code_from_url(url)
+        logger.debug('runTest: con status_code = %i' % status_code)
+        self.assertEqual(status_code, 200)
+
+
+class TestPortsOnCommandLineHttpDiff(unittest.TestCase):
     def start_valve(self):
         return valve.Valve(config_dir=config_dir,
                                   port=self.cmd_line_port,
@@ -489,10 +531,36 @@ class TestPortsOnCommandLineHttpDiff(TestPortsOnCommandLineBase,
         logger.debug('runTest: con url = %s' % url)
         self.assertRaises(requests.ConnectionError, get_status_code_from_url,
                           url)
+    def setUp(self):
+        logger.debug('setUp')
+
+        self.init_params()
+
+        pathutil.clear_folder(config_dir)
+        self.params = {
+            'proto': self.proto,
+            'sysmod_port': self.sysmod_port,
+            'target_hostname': target_hostname,
+            'target_port': target_port,
+            'cmd_line_port': self.cmd_line_port,
+            'deploy_dir': deploy_dir,
+            'artifact_dir': artifact_dir,
+            'log_file': log_file
+        }
+        apply_config_set('valve-self-common', params=self.params)
+        apply_config_set('single-node-with-proto', params=self.params)
+        apply_config_set('container-no-port', params=self.params)
+        self.valve = self.start_valve()
+        time.sleep(sleep_duration)
+
+    def tearDown(self):
+        logger.debug('tearDown')
+        if self.valve is not None:
+            self.valve.stop()
+            time.sleep(5)
 
 
-class TestPortsOnCommandLineNone(TestPortsOnCommandLineBase,
-                                 unittest.TestCase):
+class TestPortsOnCommandLineNone(unittest.TestCase):
     def start_valve(self):
         return valve.Valve(config_dir=config_dir,
                                   stop_port=self.stop_port)
@@ -519,6 +587,33 @@ class TestPortsOnCommandLineNone(TestPortsOnCommandLineBase,
         logger.debug('runTest: con url = %s' % url)
         self.assertRaises(requests.ConnectionError, get_status_code_from_url,
                           url)
+    def setUp(self):
+        logger.debug('setUp')
+
+        self.init_params()
+
+        pathutil.clear_folder(config_dir)
+        self.params = {
+            'proto': self.proto,
+            'sysmod_port': self.sysmod_port,
+            'target_hostname': target_hostname,
+            'target_port': target_port,
+            'cmd_line_port': self.cmd_line_port,
+            'deploy_dir': deploy_dir,
+            'artifact_dir': artifact_dir,
+            'log_file': log_file
+        }
+        apply_config_set('valve-self-common', params=self.params)
+        apply_config_set('single-node-with-proto', params=self.params)
+        apply_config_set('container-no-port', params=self.params)
+        self.valve = self.start_valve()
+        time.sleep(sleep_duration)
+
+    def tearDown(self):
+        logger.debug('tearDown')
+        if self.valve is not None:
+            self.valve.stop()
+            time.sleep(5)
 
 
 class TestPortsInContainerHttpSame(unittest.TestCase):
