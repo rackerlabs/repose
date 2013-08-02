75c096f02aacbfd6e8decddffffb3263edd464b4
malconis <meynard.alconis@rackspace.com>
2013-08-02 09:44:29 -0500

    Converted URI-Identity Jmeter tests to GDeproxy

---
+++ test/spock-functional-test/src/test/groovy/features/filters/uriidentity/UriIdentityTest.groovy
@@ -0,0 +1,40 @@
+package features.filters.uriidentity
+
+import framework.ReposeValveTest
+import org.rackspace.gdeproxy.Deproxy
+import org.rackspace.gdeproxy.Handling
+import org.rackspace.gdeproxy.MessageChain
+import spock.lang.Unroll
+
+class UriIdentityTest extends ReposeValveTest {
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/filters/uriidentity")
+        repose.start()
+        waitUntilReadyToServiceRequests()
+    }
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
+    }
+
+    def "when identifying requests by uri"() {
+
+        when: "Request contains the user within the uri"
+        def messageChain = deproxy.makeRequest([url: reposeEndpoint+"/service/reposeuser1/something"])
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then: "Repose will send x-pp-user with a single value"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").contains("reposeuser1;q=0.5")
+
+    }
+}
