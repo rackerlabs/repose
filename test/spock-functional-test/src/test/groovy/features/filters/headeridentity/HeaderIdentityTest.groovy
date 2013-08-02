b1274a0129b6824c07d8ffd4f3841d14a8e0cd4b
malconis <meynard.alconis@rackspace.com>
2013-08-02 10:46:30 -0500

    enhanced header identity gdeproxy tests

--- test/spock-functional-test/src/test/groovy/features/filters/headeridentity/HeaderIdentityTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/filters/headeridentity/HeaderIdentityTest.groovy
@@ -10,8 +10,8 @@ class HeaderIdentityTest extends ReposeValveTest {
 
     def static String user1 = "reposeuser1"
     def static String user2 = "reposeuser2"
-    def static Map headersIdSingle = ["x-header-user": user1]
-    def static Map headersIdMulti = ["x-header-user": user2+","+user1, "x-rax-groups": "reposegroup1"]
+    def static Map headersIdSingle = ["x-header-user":user1,"some-other-header":"value"]
+    def static Map headersIdMulti = ["x-header-user":user2+","+user1]
 
     def setupSpec() {
         deproxy = new Deproxy()
@@ -32,16 +32,22 @@ class HeaderIdentityTest extends ReposeValveTest {
         }
     }
 
-    @Unroll("Sent headers: #incomingHeaders")
+    @Unroll("When mapping header: #incomingHeaders to x-pp-user Should use first user in the list: #expecteduser")
     def "when identifying requests by header"() {
 
-        when: "Request contains a single value of the target header"
+        when: "Request contains value(s) of the target header"
         def messageChain = deproxy.makeRequest([url: reposeEndpoint, headers: incomingHeaders])
         def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
 
         then: "Repose will send x-pp-user with a single value"
-        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").contains(expecteduser+";q=0.2")
         ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").size() == 1
+
+        and: "Repose will send x-pp-user based on incoming header"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").contains(expecteduser+";q=0.2")
+
+        and: "Repose will send x-pp-groups with the value of the targeted identity header"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-groups").contains(identityHeader+";q=0.2")
+
 
         where:
         incomingHeaders | expecteduser
