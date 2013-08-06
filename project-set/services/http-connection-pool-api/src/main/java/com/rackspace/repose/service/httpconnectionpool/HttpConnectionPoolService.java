63092fa04c23f215ae8be253a7b9999fd9655b5f
Lisa Clark <lisa.clark@rackspace.com>
2013-08-06 11:17:19 -0500

    B-51783 adding pom for api

--- project-set/services/http-connection-pool-api/src/main/java/com/rackspace/repose/service/httpconnectionpool/HttpConnectionPoolService.java
+++ project-set/services/http-connection-pool-api/src/main/java/com/rackspace/repose/service/httpconnectionpool/HttpConnectionPoolService.java
@@ -1,9 +1,20 @@
 package com.rackspace.repose.service.httpconnectionpool;
 
+import sun.net.www.http.HttpClient;
+
+import java.util.Set;
+
 /**
  * HttpConnectionPoolService - proxies HTTP requests using a configurable pool of
  * connections
  *
  */
 public interface HttpConnectionPoolService {
+
+    public HttpClient getDefaultClient();
+
+    public HttpClient getClient(String id);
+
+    public Set<String> getAvailablePools();
+
 }
