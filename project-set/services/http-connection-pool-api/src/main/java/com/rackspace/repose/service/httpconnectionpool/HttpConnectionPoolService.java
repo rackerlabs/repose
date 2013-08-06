68e448330150298a151bbbb6717f9cb666c48c65
Lisa Clark <lisa.clark@rackspace.com>
2013-08-06 09:47:09 -0500

    Adding interface for http connection pool

---
+++ project-set/services/http-connection-pool-api/src/main/java/com/rackspace/repose/service/httpconnectionpool/HttpConnectionPoolService.java
@@ -0,0 +1,9 @@
+package com.rackspace.repose.service.httpconnectionpool;
+
+/**
+ * HttpConnectionPoolService - proxies HTTP requests using a configurable pool of
+ * connections
+ *
+ */
+public interface HttpConnectionPoolService {
+}
