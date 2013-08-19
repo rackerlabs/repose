641c8c21865e0d6ecbf2dd46ff386938cc30bd40
malconis <meynard.alconis@rackspace.com>
2013-08-19 08:56:40 -0500

    added gdeproxy functional tests for uri translation

---
+++ test/spock-functional-test/src/test/groovy/features/filters/uritransformation/UriTransformationTest.groovy
@@ -0,0 +1,60 @@
+package features.filters.uritransformation
+
+import framework.ReposeValveTest
+import org.rackspace.gdeproxy.Deproxy
+import org.rackspace.gdeproxy.Handling
+import org.rackspace.gdeproxy.MessageChain
+import org.rackspace.gdeproxy.Response
+
+class UriTransformationTest extends ReposeValveTest {
+
+    String tenantId = "105620"
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/filters/ipidentity")
+        repose.start()
+        waitUntilReadyToServiceRequests()
+    }
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
+    }
+
+    def "when removing tenant id from request"() {
+
+        when: "Request is sent through repose"
+        def messageChain = deproxy.makeRequest([url: reposeEndpoint + "/v1/${tenantId}/path/to/resource"])
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then: "Repose will send uri without tenant id"
+        !((Handling) sentRequest).request.path.contains(tenantId)
+
+    }
+
+    def "when putting back tenant id to Location Header within the Response"() {
+
+        given:
+        def resp = { request -> return new Response(301, "Moved Permanently", ["Location": "http://${originServiceEndpoint}/v1/path/to/resource"]) }
+
+        when: "Request is sent through repose"
+        def response = deproxy.makeRequest([url: reposeEndpoint + "/v1/${tenantId}/path/to/resource", defaultHandler: resp])
+        def sentRequest = ((MessageChain) response).getHandlings()[0]
+
+        then: "Repose will put back the tenant id in the location header"
+        response.receivedResponse.headers.getFirstValue("Location").contains(tenantId)
+
+        and: "Repose will send uri without tenant id"
+        !((Handling) sentRequest).request.path.contains(tenantId)
+
+
+    }
+}
