e53174b169d7aa8a49c7a9744352f1905f2bb565
Kushala Kondapally <kushala.kondapally@rackspace.com>
2013-08-22 16:31:11 -0500

    [Connection Pooling Impl for AuthN, Auth Z] B-54671  added connection pooling to service client

---
+++ project-set/commons/utilities/src/main/java/com/rackspace/papi/commons/util/http/ServiceClientException.java
@@ -0,0 +1,11 @@
+package com.rackspace.papi.commons.util.http;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kush5342
+ * Date: 8/21/13
+ * Time: 3:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServiceClientException {
+}
