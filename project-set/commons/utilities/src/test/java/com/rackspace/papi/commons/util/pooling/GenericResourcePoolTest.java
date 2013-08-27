a3bef44ddb52df1ef333c7f67d56234afd005e22
Lisa Clark <lisa.clark@rackspace.com>
2013-08-27 15:40:19 -0500

    Tweaking flaky unit test

--- project-set/commons/utilities/src/test/java/com/rackspace/papi/commons/util/pooling/GenericResourcePoolTest.java
+++ project-set/commons/utilities/src/test/java/com/rackspace/papi/commons/util/pooling/GenericResourcePoolTest.java
@@ -135,7 +135,7 @@ public class GenericResourcePoolTest {
                 t.start();
             }
             
-            while (internalThreadCount < 6) {
+            while (internalThreadCount < 7) {
                 Thread.sleep(10);
             }
             
