70db282dc4d66ef493399550c6d1b2b8003cf5f1
Richard Sartor <richard.sartor@rackspace.com>
2013-09-18 17:07:03 -0500

    Split out Accept-Encoding into its own test. Assign headers explicitly to the map to work-around a bug in gdeproxy concerning groovy-strings. Add a note concerninghop-by-hop headers.

--- test/spock-functional-test/src/test/groovy/features/core/headers/SplitHeadersOnCommaTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/core/headers/SplitHeadersOnCommaTest.groovy
@@ -70,62 +70,43 @@ class SplitHeadersOnCommaTest extends ReposeValveTest {
 
 
         where:
-        headerName           | headerValue                                                          | expectedCount
-        "Accept"             | "text/plain"                                                         | 1
-        "Accept"             | "text/plain, *"                                                      | 2
-        "Accept"             | "text/plain;q=0.2, application/xml;q=0.8"                            | 2
-        "Accept"             | "text/plain, application/xml, application/json"                      | 3
-        "Accept-Charset"     | "ISO-8859-1"                                                         | 1
-        "Accept-Charset"     | "ISO-8859-1, *"                                                      | 2
-        "Accept-Charset"     | "US_ASCII, *"                                                        | 2
-        "Accept-Charset"     | "UTF-8, US-ASCII, ISO-8859-1"                                        | 3
-        "Accept-Encoding"    | ""                                                                   | 1
-        "Accept-Encoding"    | "identity"                                                           | 1
-        "Accept-Encoding"    | "*"                                                                  | 1
-        "Accept-Encoding"    | "compress, gzip"                                                     | 2
-        "Accept-Encoding"    | "gzip;q=1.0, identity;q=0.5, *;q=0"                                  | 3
-        "Accept-Language"    | "*"                                                                  | 1
-        "Accept-Language"    | "en-US, *"                                                           | 2
-        "Accept-Language"    | "en-US, en-gb"                                                       | 2
-        "Accept-Language"    | "da, en-gb;q=0.8, en;q=0.7"                                          | 3
-        "Accept-Language"    | "en, en-US, en-cockney, i-cherokee, x-pig-latin"                     | 5
-        "Allow"              | ""                                                                   | 1
-        "Allow"              | "GET"                                                                | 1
-        "Allow"              | "GET, OPTIONS"                                                       | 2
-        "Allow"              | "GET, POST, PUT, DELETE"                                             | 4
-        "Cache-Control"      | "no-cache"                                                           | 1
-        "Cache-Control"      | "max-age = 3600, max-stale"                                          | 2
-        "Cache-Control"      | "max-age = 3600, max-stale = 2500, no-transform"                     | 3
-//        "Connection"         | "keep-alive"                                                         | 1
-//        "Connection"         | "keep-alive, x-header"                                               | 2
-//        "Connection"         | "keep-alive, x-header-1, x-header2"                                  | 3
-        "Content-Encoding"   | "identity"                                                           | 1
-        "Content-Encoding"   | "identity, identity"                                                 | 2
-        "Content-Encoding"   | "identity, identity, identity"                                       | 3
-        "Content-Language"   | "da"                                                                 | 1
-        "Content-Language"   | "mi, en"                                                             | 2
-        "Content-Language"   | "en, en-US, en-cockney, i-cherokee, x-pig-latin"                     | 5
-        "Expect"             | "100-continue"                                                       | 1
-        "Expect"             | "100-continue, 100-continue"                                         | 2
-        "Expect"             | "100-continue, x-expectation-1, x-expectation-2"                     | 3
-        "Pragma"             | "no-cache"                                                           | 2
-        "Pragma"             | "no-cache, x-pragma"                                                 | 2
-        "Pragma"             | "no-cache, x-pragma-1, x-pragma-2"                                   | 2
-//        "Proxy-Authenticate" | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="                                 | 1
-//        "Proxy-Authenticate" | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==, Basic QWxhZGRpbjpwbGVhc2U/Cg==" | 2
-//        "TE"                 | ""                                                                   | 1
-//        "TE"                 | "deflate"                                                            | 1
-//        "TE"                 | "trailers, deflate;q=0.5"                                            | 2
-//        "Trailer"            | "x-header"                                                           | 1
-//        "Trailer"            | "x-header1, x-header2"                                               | 2
-//        "Transfer-Encoding"  | "identity"                                                           | 1
-//        "Transfer-Encoding"  | "identity, identity"                                                 | 2
-//        "Transfer-Encoding"  | "identity, identity, identity"                                       | 3
-//        "Upgrade"            | "HTTP/2.0, SHTTP/1.3, IRC/6.9, RTA/x11"                              | 4
-        "Warning"            | "199 fred \"Misc. warning\""                                         | 1
-        "Warning"            | "199 fred \"Misc. warning\", 199 ethel \"Another warning\""          | 2
-        "WWW-Authenticate"   | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="                                 | 1
-        "WWW-Authenticate"   | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==, Basic QWxhZGRpbjpwbGVhc2U/Cg==" | 2
+        headerName         | headerValue                                                          | expectedCount
+        "Accept"           | "text/plain"                                                         | 1
+        "Accept"           | "text/plain, *"                                                      | 2
+        "Accept"           | "text/plain;q=0.2, application/xml;q=0.8"                            | 2
+        "Accept"           | "text/plain, application/xml, application/json"                      | 3
+        "Accept-Charset"   | "ISO-8859-1"                                                         | 1
+        "Accept-Charset"   | "ISO-8859-1, *"                                                      | 2
+        "Accept-Charset"   | "US_ASCII, *"                                                        | 2
+        "Accept-Charset"   | "UTF-8, US-ASCII, ISO-8859-1"                                        | 3
+        "Accept-Language"  | "*"                                                                  | 1
+        "Accept-Language"  | "en-US, *"                                                           | 2
+        "Accept-Language"  | "en-US, en-gb"                                                       | 2
+        "Accept-Language"  | "da, en-gb;q=0.8, en;q=0.7"                                          | 3
+        "Accept-Language"  | "en, en-US, en-cockney, i-cherokee, x-pig-latin"                     | 5
+        "Allow"            | ""                                                                   | 0
+        "Allow"            | "GET"                                                                | 1
+        "Allow"            | "GET, OPTIONS"                                                       | 2
+        "Allow"            | "GET, POST, PUT, DELETE"                                             | 4
+        "Cache-Control"    | "no-cache"                                                           | 1
+        "Cache-Control"    | "max-age = 3600, max-stale"                                          | 2
+        "Cache-Control"    | "max-age = 3600, max-stale = 2500, no-transform"                     | 3
+        "Content-Encoding" | "identity"                                                           | 1
+        "Content-Encoding" | "identity, identity"                                                 | 2
+        "Content-Encoding" | "identity, identity, identity"                                       | 3
+        "Content-Language" | "da"                                                                 | 1
+        "Content-Language" | "mi, en"                                                             | 2
+        "Content-Language" | "en, en-US, en-cockney, i-cherokee, x-pig-latin"                     | 5
+        "Pragma"           | "no-cache"                                                           | 1
+        "Pragma"           | "no-cache, x-pragma"                                                 | 2
+        "Pragma"           | "no-cache, x-pragma-1, x-pragma-2"                                   | 3
+        "Warning"          | "199 fred \"Misc. warning\""                                         | 1
+        "Warning"          | "199 fred \"Misc. warning\", 199 ethel \"Another warning\""          | 2
+        "WWW-Authenticate" | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="                                 | 1
+        "WWW-Authenticate" | "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==, Basic QWxhZGRpbjpwbGVhc2U/Cg==" | 2
+
+        // Connection, Expect, Proxy-Authenticate, TE, Trailer, Transfer-Encoding, Upgrade are all hop-by-hop headers, and thus wouldn't be forwarded by a proxy
+
     }
 
     @Unroll("Via header with \"#headerValue\" should result in #expectedCount parts")
@@ -145,12 +126,41 @@ class SplitHeadersOnCommaTest extends ReposeValveTest {
 
 
         where:
-        headerName           | headerValue                                                          | expectedCount
-        "Via"                | "HTTP/2.0 pseudonym"                                                 | 2
-        "Via"                | "HTTP/2.0 pseudonym (this, is, a, comment)"                          | 2
-        "Via"                | "1.0 fred, 1.1 nowhere.com (Apache/1.1)"                             | 3
-        "Via"                | "1.0 ricky, 1.1 ethel, 1.1 fred, 1.0 lucy"                           | 5
-        "Via"                | "1.0 ricky, 1.1 mertz, 1.0 lucy"                                     | 4
+        headerName | headerValue                                 | expectedCount
+        "Via"      | "HTTP/2.0 pseudonym"                        | 2
+        "Via"      | "HTTP/2.0 pseudonym (this, is, a, comment)" | 2
+        "Via"      | "1.0 fred, 1.1 nowhere.com (Apache/1.1)"    | 3
+        "Via"      | "1.0 ricky, 1.1 ethel, 1.1 fred, 1.0 lucy"  | 5
+        "Via"      | "1.0 ricky, 1.1 mertz, 1.0 lucy"            | 4
+    }
+
+
+    @Unroll("Accept-Encoding header with \"#headerValue\" should result in #expectedResult parts")
+    def "Accept-Encoding header is special, because Repose will pick values with highest qvalue"() {
+
+        when: "make a request with the given header and value"
+        def headers = [
+                'Content-Length': '0'
+        ]
+        headers[headerName.toString()] = headerValue.toString()
+
+        MessageChain mc = deproxy.makeRequest(url: url, headers: headers)
+
+        then: "the request should make it to the origin service with the header appropriately split"
+        mc.handlings.size() == 1
+        mc.handlings[0].request.headers.getCountByName(headerName) == expectedCount
+        mc.handlings[0].request.headers[headerName] == expectedResult
+
+
+        where:
+        headerName        | headerValue                           | expectedCount | expectedResult
+        "Accept-Encoding" | "identity"                            | 1             | "identity"
+        "Accept-Encoding" | "*"                                   | 1             | "*"
+        "Accept-Encoding" | "compress, gzip"                      | 1             | "compress, gzip"
+        "Accept-Encoding" | "gzip;q=1.0, identity;q=0.5, *;q=0"   | 1             | "gzip;q=1.0"
+        "Accept-Encoding" | "gzip;q=0.9, identity;q=0.5, *;q=0.1" | 1             | "gzip;q=0.9"
+        "Accept-Encoding" | "gzip;q=0.9, identity;q=0.5"          | 1             | "gzip;q=0.9"
+        "Accept-Encoding" | ""                                    | 0             | null
     }
 
     @Unroll("Headers defined by RFC: #headerName with \"#headerValue\" should not be split")
@@ -158,9 +168,9 @@ class SplitHeadersOnCommaTest extends ReposeValveTest {
 
         when: "make a request with the given header and value"
         def headers = [
-                headerName: headerValue,
                 'Content-Length': '0'
         ]
+        headers[headerName.toString()] = headerValue.toString()
 
         def mc = deproxy.makeRequest(url: url, headers: headers)
 
@@ -185,9 +195,9 @@ class SplitHeadersOnCommaTest extends ReposeValveTest {
 
         when: "make a request with the given header and value"
         def headers = [
-                headerName: headerValue,
                 'Content-Length': '0'
         ]
+        headers[headerName.toString()] = headerValue.toString()
 
         def mc = deproxy.makeRequest(url: url, headers: headers)
 
@@ -209,9 +219,9 @@ class SplitHeadersOnCommaTest extends ReposeValveTest {
 
         when: "make a request with the given header and value"
         def headers = [
-                headerName: headerValue,
                 'Content-Length': '0'
         ]
+        headers[headerName.toString()] = headerValue.toString()
 
         def mc = deproxy.makeRequest(url: url, headers: headers)
 
