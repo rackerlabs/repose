02af1aa0632890709d1af49923ec98722b752af9
malconis <meynard.alconis@rackspace.com>
2013-09-20 13:30:48 -0500

    added simple functional tests for chunked encoding and non chunked encoding

---
+++ test/spock-functional-test/src/test/groovy/features/core/proxy/ChunkedTest.groovy
@@ -0,0 +1,45 @@
+package features.core.proxy
+
+import framework.ReposeValveTest
+import org.rackspace.gdeproxy.Deproxy
+import org.rackspace.gdeproxy.Handling
+import org.rackspace.gdeproxy.MessageChain
+
+class ChunkedTest extends ReposeValveTest {
+
+
+    def setupSpec() {
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/core/proxy")
+        repose.start()
+    }
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
+    }
+
+
+    def "When set to send chunked encoding to true. Repose should send requests chunked"() {
+
+        when:
+        MessageChain messageChain = deproxy.makeRequest([url: reposeEndpoint, method: "POST", requestBody: "blah"])
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then:
+        ((Handling) sentRequest).request.getHeaders().findAll("transfer-encoding").size() == 1
+
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("transfer-encoding").equalsIgnoreCase("chunked")
+
+    }
+
+
+}
---
+++ test/spock-functional-test/src/test/groovy/features/core/proxy/ContentLengthTest.groovy
@@ -0,0 +1,48 @@
+package features.core.proxy
+
+import framework.ReposeValveTest
+import org.rackspace.gdeproxy.Deproxy
+import org.rackspace.gdeproxy.Handling
+import org.rackspace.gdeproxy.MessageChain
+
+class ContentLengthTest extends ReposeValveTest {
+
+
+    def setupSpec() {
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/core/proxy")
+        repose.start()
+    }
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
+    }
+
+
+    def "When set to send chunked encoding to false. Repose should not send requests chunked"() {
+
+        when:
+        MessageChain messageChain = deproxy.makeRequest([url: reposeEndpoint, method: "POST", requestBody: "blah"])
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then:
+        !((Handling) sentRequest).request.getHeaders().findAll("transfer-encoding").size() == 1
+
+        !((Handling) sentRequest).request.getHeaders().getFirstValue("transfer-encoding").equalsIgnoreCase("chunked")
+
+        ((Handling) sentRequest).request.getHeaders().findAll("content-length").size() == 1
+
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("content-length").equalsIgnoreCase("4")
+
+
+    }
+}
