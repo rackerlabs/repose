f6018b75172ef7b6a266fa2ab51c5f2fbbc20283
dimtruck <dimalg@yahoo.com>
2013-09-24 15:41:43 -0500

    beefed up chunked encoding tests

---
+++ test/spock-functional-test/src/test/groovy/features/core/proxy/ChunkedTranslationTest.groovy
@@ -0,0 +1,60 @@
+package features.core.proxy
+
+import framework.ReposeValveTest
+import org.rackspace.gdeproxy.Deproxy
+import org.rackspace.gdeproxy.Handling
+import org.rackspace.gdeproxy.MessageChain
+import spock.lang.Unroll
+
+/**
+ * User: dimi5963
+ * Date: 9/24/13
+ * Time: 3:36 PM
+ */
+class ChunkedTranslationTest extends ReposeValveTest {
+
+    def static String xmlPayLoad = "<a><remove-me>test</remove-me>somebody</a>"
+
+    def setupSpec() {
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/filters/translation/common",
+                "features/filters/translation/request"
+                , "features/services/httpconnectionpool/chunkedfalse")
+        repose.start()
+    }
+
+    def cleanupSpec(){
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+        if (repose) {
+            repose.stop()
+        }
+
+    }
+
+    def "When set to send chunked encoding to false. Repose should send content length of translated request"() {
+        def reqHeaders = ["accept": "application/xml", "content-type": "application/xml"]
+
+        when:
+        MessageChain messageChain = deproxy.makeRequest([url: reposeEndpoint, method: "POST", requestBody: xmlPayLoad,
+                headers: reqHeaders])
+
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then: "repose should not sending any transfer encoding header"
+        ((Handling) sentRequest).request.getHeaders().findAll("transfer-encoding").size() == 0
+
+        and: "repose should send a content-length with the request"
+        ((Handling) sentRequest).request.getHeaders().findAll("content-length").size() == 1
+
+        and: "new content length should not match that of the original request"
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("content-length") == "15"
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("content-length") != messageChain.sentRequest.headers.getFirstValue("content-length")
+
+    }
+}
+
