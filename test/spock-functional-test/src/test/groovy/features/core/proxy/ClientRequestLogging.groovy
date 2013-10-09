a101340699654da92b263795888da0976161c8da
dimtruck <dimalg@yahoo.com>
2013-10-09 11:03:03 -0500

    added comment to test

--- test/spock-functional-test/src/test/groovy/features/core/proxy/ClientRequestLogging.groovy
+++ test/spock-functional-test/src/test/groovy/features/core/proxy/ClientRequestLogging.groovy
@@ -10,6 +10,19 @@ import org.rackspace.gdeproxy.MessageChain
  * User: dimi5963
  * Date: 10/8/13
  * Time: 4:45 PM
+ *
+ * D-15731
+ *
+ * Client-request-logging attribute does not filter out logging information.  It is assigned to add request/response logging into log4j logs.  However, it maps to jetty properties.  Since we are not using Jetty but HttpClient, all requests are logged regardless of Client-request-logging attribute status.
+
+ * Current workaround is to set the following properties in log4j.properties to anything but DEBU
+
+ * log4j.logger.org.apache.commons.httpclient=WARN
+ * log4j.logger.org.apache.http.wire=WARN
+
+ * log4j.logger.org.apache.http.headers=WARN
+
+ * Fix would map client-request-logging attribute to HttpClient
  */
 @Category(Bug)
 class ClientRequestLogging extends ReposeValveTest {
