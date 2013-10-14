ed44b20165de4363280a7676bb293f34873f5b81
Damien Johnson <damien.johnson@rackspace.com>
2013-10-14 12:41:15 -0500

    B-58212 Added header splitting test to AuthZ suite, Correction to category documentation

--- test/spock-functional-test/src/test/groovy/features/filters/clientauthz/serviceresponse/ClientAuthZTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/filters/clientauthz/serviceresponse/ClientAuthZTest.groovy
@@ -6,6 +6,7 @@ import framework.category.Slow
 import org.junit.experimental.categories.Category
 import org.rackspace.gdeproxy.Deproxy
 import org.rackspace.gdeproxy.MessageChain
+import org.rackspace.gdeproxy.Response
 
 @Category(Slow.class)
 class ClientAuthZTest extends ReposeValveTest {
@@ -64,6 +65,51 @@ class ClientAuthZTest extends ReposeValveTest {
         foundLogs.size() == 1
         mc.handlings.size() == 0
         mc.receivedResponse.code == "403"
+    }
+
+    def "Should not split request headers according to rfc"() {
+        given:
+        def userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36"
+        def reqHeaders =
+            [
+                    "user-agent": userAgentValue,
+                    "x-pp-user": "usertest1, usertest2, usertest3",
+                    "accept": "application/xml;q=1 , application/json;q=0.5",
+                    'X-Auth-Token': fakeIdentityService.client_token
+            ]
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(reposeEndpoint + "/v1/"+fakeIdentityService.client_token+"/ss", 'GET',
+                reqHeaders)
+        def handling = mc.getHandlings()[0]
+
+        then: "User should receive a 200 response"
+        mc.receivedResponse.code == "200"
+        mc.handlings.size() == 1
+        handling.request.getHeaders().findAll("user-agent").size() == 1
+        handling.request.headers['user-agent'] == userAgentValue
+        handling.request.getHeaders().findAll("x-pp-user").size() == 3
+        handling.request.getHeaders().findAll("accept").size() == 2
+    }
+
+    def "Should not split response headers according to rfc"() {
+        given: "Origin service returns headers "
+        def respHeaders = ["location": "http://somehost.com/blah?a=b,c,d", "via": "application/xml;q=0.3, application/json;q=1"]
+        def xmlResp = { request -> return new Response(201, "Created", respHeaders, "") }
+        Map<String, String> headers = ['X-Auth-Token': fakeIdentityService.client_token]
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/v1/"+fakeIdentityService.client_token+"/ss",
+                method: 'GET', headers: headers, defaultHandler: xmlResp)
+        def handling = mc.getHandlings()[0]
+
+        then:
+        mc.receivedResponse.code == "201"
+        mc.handlings.size() == 1
+        mc.receivedResponse.headers.findAll("location").size() == 1
+        mc.receivedResponse.headers['location'] == "http://somehost.com/blah?a=b,c,d"
+        mc.receivedResponse.headers.findAll("via").size() == 1
     }
 
 }
