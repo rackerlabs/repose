173bf37c5c4f441d3454645eb96aeba487b1afac
Damien Johnson <damien.johnson@rackspace.com>
2013-10-15 12:49:38 -0500

    B-58212 Added IpIdentity test

--- test/spock-functional-test/src/test/groovy/features/filters/ipidentity/IpIdentityTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/filters/ipidentity/IpIdentityTest.groovy
@@ -7,6 +7,7 @@ import org.rackspace.gdeproxy.Deproxy
 import org.rackspace.gdeproxy.Handling
 import org.rackspace.gdeproxy.MessageChain
 import org.rackspace.gdeproxy.PortFinder
+import org.rackspace.gdeproxy.Response
 import spock.lang.Shared
 
 @Category(Slow.class)
@@ -79,5 +80,44 @@ class IpIdentityTest extends ReposeValveTest {
 
         and: "Repose will send x-pp-groups with the value IP_Standard"
         ((Handling) sentRequest).request.headers.getFirstValue("x-pp-groups").equalsIgnoreCase("IP_Standard;q=0.4")
+    }
+
+    def "Should not split request headers according to rfc"() {
+        given:
+        def userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36"
+        def reqHeaders =
+            [
+                    "user-agent": userAgentValue,
+                    "x-pp-user": "usertest1, usertest2, usertest3",
+                    "accept": "application/xml;q=1 , application/json;q=0.5"
+            ]
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(url: url, method: 'GET', headers: reqHeaders)
+        def handling = mc.getHandlings()[0]
+
+        then:
+        handling.request.getHeaders().findAll("user-agent").size() == 1
+        handling.request.headers['user-agent'] == userAgentValue
+        handling.request.getHeaders().findAll("x-pp-user").size() == 4
+        handling.request.getHeaders().findAll("accept").size() == 2
+    }
+
+    def "Should not split response headers according to rfc"() {
+        given: "Origin service returns headers "
+        def respHeaders = ["location": "http://somehost.com/blah?a=b,c,d", "via": "application/xml;q=0.3, application/json;q=1"]
+        def xmlResp = { request -> return new Response(201, "Created", respHeaders, "") }
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(url: url, method: 'GET', defaultHandler: xmlResp)
+        def handling = mc.getHandlings()[0]
+
+        then:
+        mc.receivedResponse.code == "201"
+        mc.handlings.size() == 1
+        mc.receivedResponse.headers.findAll("location").size() == 1
+        mc.receivedResponse.headers['location'] == "http://somehost.com/blah?a=b,c,d"
+        mc.receivedResponse.headers.findAll("via").size() == 1
     }
 }
