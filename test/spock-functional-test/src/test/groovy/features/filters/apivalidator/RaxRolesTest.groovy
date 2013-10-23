fe36b49d79dee4765b40d16c0bb8a116413a0f6d
Damien Johnson <damien.johnson@rackspace.com>
2013-10-23 13:05:55 -0500

    B-55821 Updated tests

--- test/spock-functional-test/src/test/groovy/features/filters/apivalidator/RaxRolesTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/filters/apivalidator/RaxRolesTest.groovy
@@ -31,28 +31,8 @@ class RaxRolesTest extends ReposeValveTest{
             deproxy.shutdown()
     }
 
-    @Unroll
-    def "when a rax-roles is disabled, then a role is validated for an entire wadl"() {
-        given:
-        MessageChain messageChain
-
-        when:
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + "/a", method: method, headers: headers,
-            defaultHandler: okHandler)
-
-        then:
-        messageChain.getReceivedResponse().getCode().equals(responseCode)
-
-        where:
-        method   | headers                   | responseCode
-        "GET"    | ["x-roles": "test_user1"] | "200"
-        "PUT"    | ["x-roles": "test_user1"] | "200"
-        "POST"   | ["x-roles": "test_user1"] | "200"
-        "DELETE" | ["x-roles": "test_user1"] | "200"
-    }
-
-    @Unroll
-    def "when a rax-roles is enabled, then the wadl roles should be used for validation"() {
+    @Unroll("User1:method="+method+",headers="+headers.toString()+",expected response="+responseCode)
+    def "when enable-rax-role is false, user validates the entire wadl"() {
         given:
         MessageChain messageChain
 
@@ -64,18 +44,74 @@ class RaxRolesTest extends ReposeValveTest{
         messageChain.getReceivedResponse().getCode().equals(responseCode)
 
         where:
-        method   | headers                   | responseCode
-        "GET"    | ["x-roles": "test_user2"] | "403"
-        "GET"    | ["x-roles": "a:observer"] | "200"
-        "GET"    | ["x-roles": "a:admin"]    | "403"
-        "PUT"    | ["x-roles": "test_user2"] | "200"
-        "PUT"    | ["x-roles": "a:observer"] | "200"
-        "PUT"    | ["x-roles": "a:admin"]    | "200"
-        "POST"   | ["x-roles": "test_user2"] | "403"
-        "POST"   | ["x-roles": "a:observer"] | "403"
-        "POST"   | ["x-roles": "a:admin"]    | "200"
-        "DELETE" | ["x-roles": "test_user2"] | "403"
-        "DELETE" | ["x-roles": "a:observer"] | "200"
-        "DELETE" | ["x-roles": "a:admin"]    | "200"
+        method   | headers                            | responseCode
+        "GET"    | ["x-roles": "test_user1"]          | "200"
+        "PUT"    | ["x-roles": "test_user1"]          | "200"
+        "POST"   | ["x-roles": "test_user1"]          | "200"
+        "DELETE" | ["x-roles": "test_user1"]          | "200"
+    }
+
+    @Unroll("User2:method="+method+",headers="+headers.toString()+",expected response="+responseCode)
+    def "when enable-rax-roles is true, validate with wadl method level roles"() {
+        given:
+        MessageChain messageChain
+
+        when:
+        messageChain = deproxy.makeRequest(url: reposeEndpoint + "/a", method: method, headers: headers,
+                defaultHandler: okHandler)
+
+        then:
+        messageChain.getReceivedResponse().getCode().equals(responseCode)
+
+        where:
+        method   | headers                            | responseCode
+        "GET"    | ["x-roles": "test_user2"]          | "403"
+        "GET"    | ["x-roles": "a:observer"]          | "200"
+        "GET"    | ["x-roles": "a:observer, a:bar"]   | "200"
+        "GET"    | ["x-roles": "a:observer, a:bar"]   | "403"
+        "GET"    | ["x-roles": "a:bar, a:admin"]      | "403"
+        "GET"    | ["x-roles": "a:admin"]             | "403"
+        "GET"    | null                               | "403"
+        "PUT"    | ["x-roles": "test_user2"]          | "200"
+        "PUT"    | ["x-roles": "a:bar"]               | "200"
+        "PUT"    | ["x-roles": "a:observer, a:bar"]   | "200"
+        "PUT"    | ["x-roles": "a:bar, a:jawsome"]    | "200"
+        "PUT"    | ["x-roles": "a:admin"]             | "200"
+        "PUT"    | null                               | "200"
+        "POST"   | ["x-roles": "test_user2"]          | "403"
+        "POST"   | ["x-roles": "a:observer"]          | "403"
+        "POST"   | ["x-roles": "a:admin"]             | "200"
+        "POST"   | ["x-roles": "a:bar, a:admin"]      | "200"
+        "POST"   | ["x-roles": "a:bar"]               | "403"
+        "POST"   | ["x-roles": "a:bar, a:observer"]   | "403"
+        "POST"   | null                               | "403"
+        "DELETE" | ["x-roles": "test_user2"]          | "403"
+        "DELETE" | ["x-roles": "a:observer, a:bar"]   | "200"
+        "DELETE" | ["x-roles": "a:admin, a:bar"]      | "200"
+        "DELETE" | ["x-roles": "a:bar, a:admin"]      | "200"
+        "DELETE" | ["x-roles": "a:observer, a:admin"] | "200"
+        "DELETE" | ["x-roles": "a:observer"]          | "200"
+        "DELETE" | ["x-roles": "a:admin"]             | "200"
+        "DELETE" | ["x-roles": "a:bar"]               | "403"
+        "DELETE" | ["x-roles": "a:bar, a:jawsome"]    | "403"
+        "DELETE" | ["x-roles": "observer, creator"]   | "403"
+        "DELETE" | null                               | "403"
+    }
+
+    @Unroll("User3:method="+method+",headers="+headers.toString()+",expected response="+responseCode)
+    def "when enable-rax-roles is true, validate with wadl resource level roles"() {
+        given:
+        MessageChain messageChain
+
+        when:
+        messageChain = deproxy.makeRequest(url: reposeEndpoint + "/a", method: method, headers: headers,
+                defaultHandler: okHandler)
+
+        then:
+        messageChain.getReceivedResponse().getCode().equals(responseCode)
+
+        where:
+        method   | headers                            | responseCode
+
     }
 }
