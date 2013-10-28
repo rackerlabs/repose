da415efccbdb8481464ff920eb8a0a50617c0e53
malconis <meynard.alconis@rackspace.com>
2013-10-28 15:46:02 -0500

    added container support utils to parse common repose container arguments

---
+++ project-set/external/testing/container-support/src/main/java/com/rackspace/papi/test/ContainerMonitorThread.java
@@ -0,0 +1,59 @@
+package com.rackspace.papi.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ContainerMonitorThread extends Thread {
+
+    private final ReposeContainer container;
+    private final int stopPort;
+    private static final String MONITOR_NAME = "GlassFish_MONITOR";
+    private static final String LOCAL_ADDRESS = "127.0.0.1";
+    private ServerSocket socket;
+
+    public static class MonitorException extends RuntimeException {
+        public MonitorException(String message) {
+            super(message);
+        }
+
+        public MonitorException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+
+    public ContainerMonitorThread(ReposeContainer container, int stopPort) throws MonitorException {
+        this.container = container;
+        this.stopPort = stopPort;
+        setDaemon(true);
+        setName(MONITOR_NAME);
+        try {
+            socket = new ServerSocket(stopPort, 1, InetAddress.getByName(LOCAL_ADDRESS));
+        } catch (Exception e) {
+            throw new MonitorException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+
+        Socket accept;
+
+        try {
+            accept = socket.accept();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+            reader.readLine();
+            container.stopRepose();
+            accept.close();
+            socket.close();
+        } catch (IOException ex) {
+            System.err.println("Unble to stop glassfish instance " + ex.getMessage());
+        } catch (Exception e) {
+            System.err.println("Unable to stop repose container instance: " + e.getMessage());
+        }
+    }
+}
