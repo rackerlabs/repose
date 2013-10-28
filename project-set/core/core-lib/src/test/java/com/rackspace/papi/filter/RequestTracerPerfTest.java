6bc2d1a51435b9c73420976a763f6b744e6c2edb
Damien Johnson <damien.johnson@rackspace.com>
2013-10-28 12:41:49 -0500

    B-58460 Added performance test for RequestTracer

---
+++ project-set/core/core-lib/src/test/java/com/rackspace/papi/filter/RequestTracerPerfTest.java
@@ -0,0 +1,95 @@
+package com.rackspace.papi.filter;
+
+import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletResponse;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * A jUnit test class for the performance of the RequestTracer class.
+ */
+@Ignore("Should only be run manually")
+public class RequestTracerPerfTest {
+
+    private final long NUM_HITS = 1000L;
+    private final int NUM_THREADS = 100;
+    private final MutableHttpServletResponse response = mock(MutableHttpServletResponse.class);
+
+    @Test
+    public void checkPerformanceUnderLoad() {
+        boolean trace = true;
+        boolean addHeader = true;
+        long totalMillis = 0;
+
+        final RequestTracer rt = new RequestTracer(trace, addHeader);
+
+        // Ramp up, should not be necessary
+        for (int i = 0; i < 100; ++i) {
+            rt.traceEnter();
+            rt.traceExit(response, "myFilter");
+        }
+
+        // Sequential request test
+        for (int i = 0; i < NUM_HITS; ++i) {
+            long startTime = System.currentTimeMillis();
+            rt.traceEnter();
+            rt.traceExit(response, "myFilter");
+            long endTime = System.currentTimeMillis();
+            totalMillis += endTime - startTime;
+        }
+
+        long avg = totalMillis / NUM_HITS;
+    }
+
+    @Test
+    public void checkPerformanceUnderLoadMultiThreaded() {
+        boolean trace = true;
+        boolean addHeader = true;
+        long totalTime = 0;
+
+        final RequestTracer rt = new RequestTracer(trace, addHeader);
+
+        final Thread[] threads = new Thread[NUM_THREADS];
+        final long[] times = new long[NUM_THREADS];
+
+        // Ramp up, should not be necessary
+        for (int i = 0; i < 100; ++i) {
+            rt.traceEnter();
+            rt.traceExit(response, "myFilter");
+        }
+
+        // Threaded request test
+        for (int i = 0; i < NUM_THREADS; ++i) {
+            final int index = i;
+            Thread thread = new Thread() {
+                public void run() {
+                    long totalMillis = 0;
+                    for (int i = 0; i < NUM_HITS; ++i) {
+                        long startTime = System.currentTimeMillis();
+                        rt.traceEnter();
+                        rt.traceExit(response, "myFilter");
+                        long endTime = System.currentTimeMillis();
+                        totalMillis += endTime - startTime;
+                    }
+                    times[index] = totalMillis;
+                }
+            };
+            threads[index] = thread;
+        }
+
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (Exception e) {}
+        }
+        for (long l : times) {
+            totalTime += l;
+        }
+
+        long avg = totalTime / (NUM_HITS * NUM_THREADS);
+    }
+}
