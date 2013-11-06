0a2b885b25bf72b4a5e2e1b1a3994d873e1d66ee
Damien Johnson <damien.johnson@rackspace.com>
2013-11-06 10:32:10 -0600

    D-16104 Added very simple functional test

--- test/spock-functional-test/src/test/groovy/features/core/proxy/RequestQueryParamTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/core/proxy/RequestQueryParamTest.groovy
@@ -17,7 +17,6 @@ class RequestQueryParamTest extends ReposeValveTest {
 
     }
 
-
     @Unroll("When client requests: #method #uriSuffixGiven, repose should normalize to: #uriSuffixExpected")
     def "when given a query param list, Repose should forward a valid query param list"() {
 
@@ -52,6 +51,20 @@ class RequestQueryParamTest extends ReposeValveTest {
         "/path/to/resource/?&a=12345"        | "/path/to/resource/?a=12345"        | "POST"
         "/path/to/resource?&a=12345&b=54321" | "/path/to/resource?a=12345&b=54321" | "GET"
         "/path/to/resource?&a=12345&b=54321" | "/path/to/resource?a=12345&b=54321" | "POST"
+    }
+
+    def "when given an improperly encoded URI character, Repose should pass it through"() {
+
+        when:
+        MessageChain messageChain = deproxy.makeRequest(reposeEndpoint + uriSuffixGiven, method)
+
+        then:
+        messageChain.handlings.size() == 1
+        messageChain.handlings[0].request.path.endsWith(uriSuffixExpected)
+
+        where:
+        uriSuffixGiven                  | uriSuffixExpected               | method
+        "/path/to/resource?key=value@%" | "/path/to/resource?key=value@%" | "GET"
     }
 
     def cleanupSpec() {
