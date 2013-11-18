d71879216d196b9c19c6b0a08ef1962f6d5e27a6
Kushala Kondapally <kushala.kondapally@rackspace.com>
2013-11-18 17:26:41 -0600

    added more tests to spock test

--- test/spock-functional-test/src/test/groovy/features/core/powerfilter/URIEncodingTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/core/powerfilter/URIEncodingTest.groovy
@@ -8,6 +8,9 @@ class URIEncodingTest extends ReposeValveTest {
 
     def setupSpec() {
 
+        repose.applyConfigs( "features/core/powerfilter/URIEncode/noFilters" )
+        repose.start()
+        repose.waitForNon500FromUrl(reposeEndpoint)
 
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
@@ -23,90 +26,122 @@ class URIEncodingTest extends ReposeValveTest {
 
 
 
-   @Unroll("URI's with special character through no filter sent = #URISent")
-   def "URI's with special character through no filter(only destination filter)"() {
-       setup:
-       repose.applyConfigs( "features/core/powerfilter/URIEncode/noFilters" )
-       repose.start()
-       repose.waitForNon500FromUrl(reposeEndpoint)
+   @Unroll("URI's with reserved character through no filter sent = #URISent")
+   def "URI's with reserved character(+) through no filter(only destination filter)"() {
 
        when: "User sends a request through repose"
        def messageChain = deproxy.makeRequest(url: reposeEndpoint, path: URISent)
 
        then: "Repose send the URI parameters without manipulation"
-       messageChain.handlings.size()>0
-       messageChain.handlings.get(0).request.path.equals(URISent)
        messageChain.receivedResponse.code.equals("200")
+       messageChain.handlings.size()>0
+       messageChain.handlings.get(0).request.path.equals(URItoOriginService)
 
-       cleanup:
-       repose.stop()
+
 
        where:
        URISent | URItoOriginService
-       "/messages?ids= locations"  | "/messages?ids= locations"
        "/messages?ids=+locations"  | "/messages?ids=+locations"
-       "/ messages?ids=locations"  | "/ messages?ids=locations"
        "/+messages?ids=locations"  | "/+messages?ids=locations"
        "/messages?ids=locations"   | "/messages?ids=locations"
+
+       "/messages?ids=;locations"  | "/messages?ids=;locations"
+       "/messages?ids=/locations"  | "/messages?ids=/locations"
+       "/messages?ids=?locations"  | "/messages?ids=?locations"
+       "/messages?ids=:locations"  | "/messages?ids=:locations"
+       "/messages?ids=@locations"  | "/messages?ids=@locations"
+       "/messages?ids==locations"  | "/messages?ids==locations"
+       "/messages?ids=,locations"  | "/messages?ids=,locations"
+
+       "/:messages?ids=locations"  | "/:messages?ids=locations"
+       "/@messages?ids=locations"  | "/@messages?ids=locations"
+       "/=messages?ids=locations"  | "/=messages?ids=locations"
+       "/,messages?ids=locations"  | "/,messages?ids=locations"
+       "//messages?ids=locations"  | "//messages?ids=locations"
+       "/;messages?ids=locations"  | "/;messages?ids=locations"
+
+       "/?messages?ids=locations"  | "/?messages%3Fids=locations"
 
 
     }
 
+
+
+
     @Unroll("URI's with special character through API Validator filter sent = #URISent")
     def "URI's with special character through API Validator filter"() {
 
-        setup:
-        repose.applyConfigs( "features/core/powerfilter/URIEncode/withAPIValidator" )
-        repose.start()
+        given:
+        repose.updateConfigs( "features/core/powerfilter/URIEncode/withAPIValidator" )
+        repose.waitForNon500FromUrl(reposeEndpoint)
 
 
         when: "User sends a request through repose"
         def messageChain = deproxy.makeRequest(url: reposeEndpoint, path: URISent ,method: "GET", headers: ["X-Roles" : "role-1"])
 
         then: "Repose send the URI parameters without manipulation"
-        messageChain.receivedResponse.code.equals("200")
+        messageChain.receivedResponse.code.equals("404")
         messageChain.handlings.size()>0
         messageChain.handlings.get(0).request.path.equals(URISent)
 
-        cleanup:
-        repose.stop()
+
 
         where:
         URISent | URItoriginService
-        "/+messages?ids=locations"  | "/+messages?ids=locations"
+        "/messages/+add-nodes"  | "/messages/+add-nodes"
 
 
 
     }
 
 
-    @Unroll("URI's with special character through All filters except API Validator sent = #URISent")
-    def "URI's with special character through All filters except API Validator"() {
+    @Unroll("URI's with special character through Identity filters except API Validator sent = #URISent")
+    def "URI's with special character through Identity filter"() {
 
         setup:
-        repose.applyConfigs( "features/core/powerfilter/multifilters" )
+        repose.applyConfigs( "features/core/powerfilter/URIEncode" )
         repose.start()
+        repose.waitForNon500FromUrl(reposeEndpoint)
 
         when: "User sends a request through repose"
         def messageChain = deproxy.makeRequest(url: reposeEndpoint, path: URISent)
 
         then: "Repose send the URI parameters without manipulation"
-        messageChain.handlings.size()>0
-        messageChain.handlings.get(0).request.path.equals(URISent)
         messageChain.receivedResponse.code.equals("200")
+        messageChain.handlings.size()>0
+        messageChain.handlings.get(0).request.path.equals(URItoOriginService)
 
         cleanup:
         repose.stop()
 
         where:
         URISent | URItoOriginService
-        "/messages?ids= locations"  | "/messages?ids= locations"
+
+        // space in the URI is not valid so returning 400's "/ messages?ids=+locations"  | "/messages?ids=+locations"
         "/messages?ids=+locations"  | "/messages?ids=+locations"
-        "/ messages?ids=locations"  | "/ messages?ids=locations"
         "/+messages?ids=locations"  | "/+messages?ids=locations"
         "/messages?ids=locations"   | "/messages?ids=locations"
 
+        "/messages?ids=;locations"  | "/messages?ids=%3Blocations"
+        "/messages?ids=/locations"  | "/messages?ids=%2Flocations"
+        "/messages?ids=?locations"  | "/messages?ids=%3Flocations"
+        "/messages?ids=:locations"  | "/messages?ids=%3Alocations"
+        "/messages?ids=@locations"  | "/messages?ids=%40locations"
+        "/messages?ids==locations"  | "/messages?ids=%3Dlocations"
+        "/messages?ids=,locations"  | "/messages?ids=%2Clocations"
+
+        "/?messages?ids=locations"  | "/?messages%3Fids=locations"
+        "//messages?ids=locations"  | "/messages?ids=locations"
+
+        "/;messages?ids=locations"  | "/;messages?ids=locations"
+        "/:messages?ids=locations"  | "/:messages?ids=locations"
+        "/@messages?ids=locations"  | "/@messages?ids=locations"
+        "/=messages?ids=locations"  | "/=messages?ids=locations"
+        "/,messages?ids=locations"  | "/,messages?ids=locations"
+
 
     }
+
+
 
 }
