76ae39cf6de883a6a0846195cec26cf5f93fe7fa
Damien Johnson <damien.johnson@rackspace.com>
2013-11-25 10:47:01 -0600

    Fixing comments

--- repose-aggregator/services/metrics/api/src/main/java/com/rackspace/papi/service/metrics/MeterByCategory.java
+++ repose-aggregator/services/metrics/api/src/main/java/com/rackspace/papi/service/metrics/MeterByCategory.java
@@ -2,13 +2,13 @@ package com.rackspace.papi.service.metrics;
 
 /**
  * Interface to allow different MeterByCategory implementations (like
- * {@link com.rackspace.papi.service.reporting.metrics.impl.MeterByCategoryImpl} &
- * {@link com.rackspace.papi.service.reporting.metrics.impl.MeterByCategorySum}) to be used interchangeably.
+ * {@link com.rackspace.papi.service.metrics.impl.MeterByCategoryImpl} &
+ * {@link com.rackspace.papi.service.metrics.impl.MeterByCategorySum}) to be used interchangeably.
  * <p>
  * Any class which implements this interface is expected to be thread-safe.  The individual yammer Meter class are
  * thread-safe.
  * <p>
- * These objects should be created by the {@link com.rackspace.papi.service.reporting.metrics.impl.MetricsServiceImpl}
+ * These objects should be created by the {@link com.rackspace.papi.service.metrics.impl.MetricsServiceImpl}
  * factory class.
  */
 public interface MeterByCategory {
