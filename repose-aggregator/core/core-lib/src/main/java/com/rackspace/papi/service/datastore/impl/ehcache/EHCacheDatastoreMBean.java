88126a44a715a4cc909e8294587cb05eb7ed352a
kushala2012 <kushala@rackspace.com>
2013-12-11 11:26:12 -0600

    B-58092 moved datastore implementation for ehcache into datastore service

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/datastore/impl/ehcache/EHCacheDatastoreMBean.java
+++
@@ -1,7 +0,0 @@
-package com.rackspace.papi.service.datastore.impl.ehcache;
-
-public interface EHCacheDatastoreMBean {
-    String OBJECT_NAME = "com.rackspace.papi.service.datastore.impl.ehcache:type=EHCacheDatastore";
-
-    void removeAllCachedData();
-}
