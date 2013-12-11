b15bcf8e2b9674146e92a35c026df92b20dad0ea
malconis <meynard.alconis@rackspace.com>
2013-12-11 10:10:37 -0600

    added config files for multi-validator with pre-processing functional tests. Filled in basic functional test

--- test/spock-functional-test/src/test/groovy/features/filters/apivalidator/MultipleValidatorsTest.groovy
+++ test/spock-functional-test/src/test/groovy/features/filters/apivalidator/MultipleValidatorsTest.groovy
@@ -6,11 +6,13 @@ import org.rackspace.deproxy.Deproxy
 
 class MultipleValidatorsTest extends ReposeValveTest{
 
+    def body = """"<a blah=\"string\"><remove-me>test</remove-me>Stuff</a>"""
+
     def setupSpec() {
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
 
-        repose.applyConfigs("features/filters/apivalidator/jmeter/")
+        repose.applyConfigs("features/filters/apivalidator/common","features/filters/apivalidator/multiValidatorsPreProcess/")
         repose.start()
         repose.waitForNon500FromUrl(reposeEndpoint)
     }
@@ -23,5 +25,18 @@ class MultipleValidatorsTest extends ReposeValveTest{
     }
 
 
+    def whenRequestGoesThroughMultipleValidatorsWithPreprocessing(){
 
+        when:
+        def messageChain = deproxy.makeRequest(url: reposeEndpoint+"/resource", method: "POST",
+                body: body, headers:["x-roles": "admin", "content-type": "application/xml"] )
+        def sentRequest = messageChain.getHandlings()[0]
+
+        then:
+        messageChain.getReceivedResponse().code == "200"
+
+        and:
+        !sentRequest.getRequest().body.toString().contains("remove-me")
+
+    }
 }
