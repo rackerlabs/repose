163dbbd21cfa007eb213e757bec461aed20c3026
Damien Johnson <damien.johnson@rackspace.com>
2013-12-13 11:24:52 -0600

    D-16424 Added functional test

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/URIEncodingWithApiValidatorTest.groovy
@@ -0,0 +1,44 @@
+package features.core.powerfilter
+
+import framework.ReposeValveTest
+import org.rackspace.deproxy.Deproxy
+import spock.lang.Unroll
+
+class URIEncodingWithApiValidatorTest extends ReposeValveTest {
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.getProperty("target.port").toInteger())
+
+        repose.applyConfigs("features/core/powerfilter/URIEncode/withAPIValidator")
+        repose.start(killOthersBeforeStarting: false, waitOnJmxAfterStarting: false)
+        repose.waitForNon500FromUrl(reposeEndpoint, 120)
+    }
+
+    @Unroll("URI's with special character through API Validator filter sent = #URISent")
+    def "API Validator filter handles '+' in URI path"() {
+
+        when: "User sends a request through repose"
+        def messageChain = deproxy.makeRequest(url: reposeEndpoint, path: URISent, method: "GET", headers: ["X-Roles": "role-1"])
+
+        then: "Repose send the URI parameters without manipulation"
+        messageChain.receivedResponse.code.equals("200")
+        messageChain.handlings.size() == 1
+        messageChain.handlings.get(0).request.path == URItoriginService
+
+        where:
+        URISent                    | URItoriginService
+        "/+messages?ids=locations" | "/+messages?ids=locations"
+        "/messages/+add-nodes"     | "/messages/+add-nodes"
+    }
+
+    def cleanupSpec() {
+        if (repose) {
+            repose.stop()
+        }
+
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+    }
+}
