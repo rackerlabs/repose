295fb5b35659f940f8ff8320ccbae35567b9dc5b
Lisa Clark <lisa.clark@rackspace.com>
2013-12-17 20:54:01 -0600

    B-58092 updated RemoteCommandExecutorTest

--- repose-aggregator/components/datastores/distributed/src/test/java/com/rackspace/papi/components/datastore/impl/distributed/remote/RemoteCommandExecutorTest.java
+++ repose-aggregator/components/datastores/distributed/src/test/java/com/rackspace/papi/components/datastore/impl/distributed/remote/RemoteCommandExecutorTest.java
@@ -1,106 +1,96 @@
 package com.rackspace.papi.components.datastore.impl.distributed.remote;
 
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
+import com.rackspace.papi.commons.util.http.ServiceClientResponse;
+import com.rackspace.papi.commons.util.proxy.ProxyRequestException;
+import com.rackspace.papi.commons.util.proxy.RequestProxyService;
+import com.rackspace.papi.components.datastore.DatastoreOperationException;
+import com.rackspace.papi.components.datastore.distributed.RemoteBehavior;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.junit.Before;
+import org.junit.Test;
 
-@RunWith(Enclosed.class)
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class RemoteCommandExecutorTest {
 
-   public static final String HOST_KEY = "host_key";
+    String HOST_KEY = "host_key";
+    RequestProxyService mockRequestProxyService;
+    RemoteCommandExecutor executor;
 
-    /**
-   public static class WhenExecutingRemoteCommands {
+    @Before
+    public void standUp() throws Exception {
+        final HttpResponse mockedResponse = mock(HttpResponse.class);
+        final HttpClient mockedClient = mock(HttpClient.class);
+        mockRequestProxyService = mock(RequestProxyService.class);
 
-      protected RemoteCommandExecutor executor;
+        when(mockedClient.execute(any(HttpRequestBase.class))).thenReturn(mockedResponse);
 
-      @Before
-      public void standUp() throws Exception {
-         final HttpResponse mockedResponse = mock(HttpResponse.class);
-         final HttpClient mockedClient = mock(HttpClient.class);
+        executor = new RemoteCommandExecutor(mockRequestProxyService, HOST_KEY);
+    }
 
-         when(mockedClient.execute(any(HttpRequestBase.class))).thenReturn(mockedResponse);
+    @Test
+    public void shouldPerformRemoteCommand() {
+        final Object result = executor.execute(new TestableRemoteCommand(), RemoteBehavior.ALLOW_FORWARDING);
+        assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
+    }
 
-         final Pool<HttpClient> httpClientPool = new GenericBlockingResourcePool<HttpClient>(new ConstructionStrategy<HttpClient>() {
+    @Test(expected = DatastoreOperationException.class)
+    public void shouldThrowDatastoreOperationExceptionsForIOExceptions() {
+        final Object result = executor.execute(new TestableRemoteCommand(true), RemoteBehavior.ALLOW_FORWARDING);
+        assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
+    }
 
-            @Override
-            public HttpClient construct() throws ResourceConstructionException {
-               return mockedClient;
+    @Test(expected = RemoteConnectionException.class)
+    public void shouldCatchProxyRequestExceptions() {
+        final Object result = executor.execute(new TestableRemoteCommand(new ProxyRequestException("foo", new IllegalArgumentException())), RemoteBehavior.ALLOW_FORWARDING);
+        assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
+    }
+
+    private class TestableRemoteCommand implements RemoteCommand {
+
+        RuntimeException exception;
+        boolean throwsIOException = false;
+
+        private TestableRemoteCommand() {}
+
+        private TestableRemoteCommand(RuntimeException exception) {
+            this.exception = exception;
+        }
+
+        private TestableRemoteCommand(boolean throwsIOException) {
+            this.throwsIOException = throwsIOException;
+        }
+
+        @Override
+        public ServiceClientResponse execute(RequestProxyService proxyService, RemoteBehavior remoteBehavior) {
+            return mock(ServiceClientResponse.class);
+        }
+
+        @Override
+        public Object handleResponse(ServiceClientResponse httpResponse) throws IOException {
+            if (exception != null) {
+                throw exception;
             }
-         });
-
-         executor = new RemoteCommandExecutor(httpClientPool);
-         executor.setHostKey(HOST_KEY);
-      }
-
-      @Test
-      public void shouldPerformRemoteCommand() {
-         final Object result = executor.execute(new RemoteCommand() {
-
-            @Override
-            public HttpRequestBase buildRequest() {
-               return mock(HttpRequestBase.class);
+            if (throwsIOException) {
+                throw new IOException("foo", new IllegalArgumentException());
             }
+            return Boolean.TRUE;
+        }
 
-            @Override
-            public Object handleResponse(HttpResponse httpResponse) throws IOException, DatastoreOperationException {
-               return Boolean.TRUE;
-            }
-
-            @Override
-            public void setHostKey(String hostKey) {
-               assertEquals("Host key must be set in the remote command", HOST_KEY, hostKey);
-            }
-         });
-
-         assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
-      }
-
-      @Test(expected = DatastoreOperationException.class)
-      public void shouldNotCatchDatastoreOperationExceptions() {
-         final Object result = executor.execute(new RemoteCommand() {
-
-            @Override
-            public HttpRequestBase buildRequest() {
-               return mock(HttpRequestBase.class);
-            }
-
-            @Override
-            public Object handleResponse(HttpResponse httpResponse) throws IOException, DatastoreOperationException {
-               throw new DatastoreOperationException("Failure");
-            }
-
-            @Override
-            public void setHostKey(String hostKey) {
-               assertEquals("Host key must be set in the remote command", HOST_KEY, hostKey);
-            }
-         });
-
-         assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
-      }
-
-      @Test(expected = RemoteConnectionException.class)
-      public void shouldCatchIOExceptions() {
-         final Object result = executor.execute(new RemoteCommand() {
-
-            @Override
-            public HttpRequestBase buildRequest() {
-               return mock(HttpRequestBase.class);
-            }
-
-            @Override
-            public Object handleResponse(HttpResponse httpResponse) throws IOException, DatastoreOperationException {
-               throw new IOException("Failure");
-            }
-
-            @Override
-            public void setHostKey(String hostKey) {
-               assertEquals("Host key must be set in the remote command", HOST_KEY, hostKey);
-            }
-         });
-
-         assertEquals("Executor must return the remote command's response", Boolean.TRUE, result);
-      }
-   }**/
-
+        @Override
+        public void setHostKey(String hostKey) {
+            assertEquals("Host key must be set in the remote command", HOST_KEY, hostKey);
+        }
+    }
 
 }
+
+
+
