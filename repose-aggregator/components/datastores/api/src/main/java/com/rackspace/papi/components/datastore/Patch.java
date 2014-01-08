48582f627086859ad481cc479df3d8a7afe81aed
Adrian <adrian.george@rackspace.com>
2014-01-08 08:39:19 -0600

    TK-116225 Changd the generic type to be more standard

--- repose-aggregator/components/datastores/api/src/main/java/com/rackspace/papi/components/datastore/Patch.java
+++ repose-aggregator/components/datastores/api/src/main/java/com/rackspace/papi/components/datastore/Patch.java
@@ -1,10 +1,10 @@
 package com.rackspace.papi.components.datastore;
 
 /**
- * An interface for a Patch (of TStoredElement).
+ * An interface for a Patch (of T).
  *
- * TStoredElement - this is the abstraction of the data that gets stored in the datastore
+ * T - this is the abstraction of the data that gets stored in the datastore
  */
-public interface Patch<TStoredElement> {
-    public TStoredElement newFromPatch();
+public interface Patch<T> {
+    public T newFromPatch();
 }
