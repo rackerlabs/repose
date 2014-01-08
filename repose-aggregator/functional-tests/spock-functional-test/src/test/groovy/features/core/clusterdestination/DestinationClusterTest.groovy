7622d674e2251c1188a6d7f86b3305da3b3020de
malconis <meynard.alconis@rackspace.com>
2014-01-08 13:16:59 -0600

    migrated root war test which tested repose routing to a destination cluster

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/clusterdestination/DestinationClusterTest.groovy
@@ -0,0 +1,85 @@
+package features.core.clusterdestination
+
+import framework.ReposeConfigurationProvider
+import framework.ReposeContainerLauncher
+import framework.ReposeLauncher
+import framework.TestProperties
+import framework.TestUtils
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+import org.rackspace.deproxy.PortFinder
+import spock.lang.Specification
+
+class DestinationClusterTest extends Specification {
+
+    static ReposeLauncher repose
+    static Deproxy deproxy
+    static String tomcatEndpoint
+
+    def setupSpec() {
+
+        def TestProperties properties = new TestProperties(ClassLoader.getSystemResource("test.properties").openStream())
+        PortFinder pf = new PortFinder(properties.getDynamicPortBase())
+
+        int originServicePort1 = pf.getNextOpenPort()
+        int originServicePort2 = pf.getNextOpenPort()
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(originServicePort1)
+        deproxy.addEndpoint(originServicePort2)
+
+        int reposePort = pf.getNextOpenPort()
+        int shutdownPort = pf.getNextOpenPort()
+        tomcatEndpoint = "http://localhost:${reposePort}"
+
+        def configDirectory = properties.getConfigDirectory()
+        def configSamples = properties.getRawConfigDirectory()
+        def rootWar = properties.getReposeRootWar()
+        def buildDirectory = properties.getReposeHome() + "/.."
+        ReposeConfigurationProvider config = new ReposeConfigurationProvider(configDirectory, configSamples)
+
+        config.applyConfigsRuntime("features/core/proxy/clusterdestination",
+                [
+                        'repose_port': reposePort.toString(),
+                        'dst_port1': originServicePort1.toString(),
+                        'dst_port2': originServicePort2.toString(),
+                        'repose.config.directory': configDirectory,
+                        'repose.cluster.id': "repose1",
+                        'repose.node.id': 'node1',
+                        'target_hostname': 'localhost',
+                ]
+        )
+        config.applyConfigsRuntime("common", ['project.build.directory': buildDirectory])
+
+
+        repose = new ReposeContainerLauncher(config, properties.getTomcatJar(), "repose1", "node1", rootWar, reposePort, shutdownPort)
+        repose.clusterId = "repose"
+        repose.nodeId = "simple-node"
+        repose.start()
+    }
+
+    def cleanupSpec() {
+        if (deproxy)
+            deproxy.shutdown()
+
+        if (repose)
+            repose.stop()
+    }
+
+    def "should switch routing between to nodes within destination cluster"(){
+
+        when: "Requests are sent through Repose/Tomcat"
+        TestUtils.waitUntilReadyToServiceRequests(tomcatEndpoint)
+        MessageChain mc = deproxy.makeRequest(url: tomcatEndpoint + "/cluster", headers: ['x-pp-user': 'usertest1'])
+        MessageChain mc2 = deproxy.makeRequest(url: tomcatEndpoint + "/cluster", headers: ['x-pp-user': 'usertest1'])
+        def sentRequest1 = ((MessageChain) mc).getHandlings()[0]
+        def sentRequest2 = ((MessageChain) mc2).getHandlings()[0]
+
+        then: "Responses should come back successfully"
+        mc.receivedResponse.code == "200"
+        mc2.receivedResponse.code == "200"
+
+        and: "Repose should have routed to two different endpoints"
+        sentRequest1.endpoint != sentRequest2.endpoint
+
+    }
+}
