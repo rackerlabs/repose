f7b72df678ee80c44e87f9f93932c20b973342a6
malconis <meynard.alconis@rackspace.com>
2014-01-17 12:38:30 -0600

    deleted test and automation directories as they are no longer needed for regression tests

--- automation/README.md
+++
@@ -1,4 +0,0 @@
-Repose Automation
-
-Wrapper for our chef-solo to build out the 3 Repose nodes used by our
-JMeter tests
--- automation/rcman/includes/chef-solo/cookbooks/powerapi-valve/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures powerapi-valve"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.12"
-
-depends "java"
