6cab42129de7e03f33b1a509adc9a2c8b0dd3e68
Adrian George <adrian.george@rackspace.com>
2014-01-23 14:31:52 -0600

    Added a todo

--- repose-aggregator/services/httpclient/api/src/main/java/com/rackspace/papi/service/httpclient/HttpClientResponse.java
+++ repose-aggregator/services/httpclient/api/src/main/java/com/rackspace/papi/service/httpclient/HttpClientResponse.java
@@ -2,6 +2,7 @@ package com.rackspace.papi.service.httpclient;
 
 import org.apache.http.client.HttpClient;
 
+//todo: change the name of this using response mixes contexts
 public interface HttpClientResponse {
     HttpClient getHttpClient();
 }
