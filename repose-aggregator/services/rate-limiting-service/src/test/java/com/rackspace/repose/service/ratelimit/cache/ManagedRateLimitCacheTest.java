1d2c94c86547869f84ca25a237ebc997c3821f0e
Adrian <adrian.george@rackspace.com>
2014-01-27 15:12:31 -0600

    Reformatted the test

--- repose-aggregator/services/rate-limiting-service/src/test/java/com/rackspace/repose/service/ratelimit/cache/ManagedRateLimitCacheTest.java
+++ repose-aggregator/services/rate-limiting-service/src/test/java/com/rackspace/repose/service/ratelimit/cache/ManagedRateLimitCacheTest.java
@@ -4,16 +4,19 @@ import com.rackspace.papi.components.datastore.Datastore;
 import com.rackspace.repose.service.limits.schema.HttpMethod;
 import com.rackspace.repose.service.limits.schema.TimeUnit;
 import com.rackspace.repose.service.ratelimit.config.ConfiguredRatelimit;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
 
 import java.io.Serializable;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 // TODO: Still depend on Repose core
 
@@ -21,79 +24,66 @@ import static org.mockito.Mockito.*;
  *
  * @author jhopper
  */
-@RunWith(Enclosed.class)
 public class ManagedRateLimitCacheTest {
 
-    public static final String ACCOUNT = "12345";
-    public static final int datastoreWarnLimit= 1000;
+    private String ACCOUNT = "12345";
+    private int datastoreWarnLimit= 1000;
+    private Datastore datastore;
+    private ManagedRateLimitCache rateLimitCache;
 
-
-    public static class WhenRetrievingAccountLimitKeys {
-
-        @Test
-        public void shouldReturnEmptySetsWhenNoLimitKeysExist() {
-            final Datastore cacheMock = mock(Datastore.class);
-            
-            final ManagedRateLimitCache cache = new ManagedRateLimitCache(cacheMock);
-            assertTrue("Should have an empty map when no limits have been registered for an account", cache.getUserRateLimits("key").isEmpty());
-        }
-
-        @Test
-        public void shouldReturnCachedKeySets() throws Exception {
-            final Datastore cacheMock = mock(Datastore.class);
-            final HashMap<String, CachedRateLimit> limitMap = new HashMap<String, CachedRateLimit>();
-            limitMap.put("12345", new CachedRateLimit(".*"));
-            
-            when(cacheMock.get(ACCOUNT)).thenReturn(limitMap);
-            
-            final ManagedRateLimitCache cache = new ManagedRateLimitCache(cacheMock);
-            assertFalse("Should return a non-empty set", cache.getUserRateLimits(ACCOUNT).isEmpty());
-        }
+    @Before
+    public void setUp() throws Exception {
+        datastore = mock(Datastore.class);
+        rateLimitCache = new ManagedRateLimitCache(datastore);
     }
 
-    public static class WhenUpdatingRateLimits {
+    @Test
+    public void getUserRateLimits_shouldReturnEmptySetsWhenNoLimitKeysExist() {
+        assertTrue("Should have an empty map when no limits have been registered for an account", rateLimitCache.getUserRateLimits("key").isEmpty());
+    }
 
-        private ManagedRateLimitCache cache;
+    @Test
+    public void getUserRateLimits_shouldReturnCachedKeySets() throws Exception {
+        HashMap<String, CachedRateLimit> limitMap = new HashMap<String, CachedRateLimit>();
+        limitMap.put("12345", new CachedRateLimit(".*"));
 
-        @Test
-        public void shouldAddNewLimit() throws Exception {
-            final String user = "12345", key = "variant";
-            final ConfiguredRatelimit rate = new ConfiguredRatelimit();
-            rate.setUriRegex(".*");
-            rate.setUnit(TimeUnit.HOUR);
-            rate.setValue(3);
-            
-            final HashMap<String, CachedRateLimit> limitMap = new HashMap<String, CachedRateLimit>();
-            
-            final Datastore cacheMock = mock(Datastore.class);
-            when(cacheMock.get(user)).thenReturn(limitMap);
-            
-            cache = new ManagedRateLimitCache(cacheMock);
+        when(datastore.get(ACCOUNT)).thenReturn(limitMap);
 
-            cache.updateLimit(HttpMethod.GET, user, key, rate, datastoreWarnLimit);
-            
-            verify(cacheMock).put(eq(user), any(Serializable.class), eq(1), eq(java.util.concurrent.TimeUnit.HOURS));
-        }
+        assertFalse("Should return a non-empty set", rateLimitCache.getUserRateLimits(ACCOUNT).isEmpty());
+    }
 
-        @Test
-        public void shouldReturnValidNextAvailableResponse() throws Exception {
-            final String account = "12345", key = "variant";
-            final ConfiguredRatelimit rate = new ConfiguredRatelimit();
-            rate.setUriRegex(".*");
-            rate.setValue(2);
-            rate.setUnit(TimeUnit.HOUR);
-         
-            final HashMap<String, CachedRateLimit> liveLimitMap = new HashMap<String, CachedRateLimit>();
-            liveLimitMap.put(key, new CachedRateLimit(".*"));
+    @Test
+    public void updateLimit_shouldAddNewLimit() throws Exception {
+        final String user = "12345", key = "variant";
+        final ConfiguredRatelimit rate = new ConfiguredRatelimit();
+        rate.setUriRegex(".*");
+        rate.setUnit(TimeUnit.HOUR);
+        rate.setValue(3);
 
-            final Datastore cacheMock = mock(Datastore.class);
-            when(cacheMock.get(account)).thenReturn(liveLimitMap);
-            
-            cache = new ManagedRateLimitCache(cacheMock);
+        final HashMap<String, CachedRateLimit> limitMap = new HashMap<String, CachedRateLimit>();
 
-            assertTrue(cache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
-            assertTrue(cache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
-            assertFalse(cache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
-        }
+        when(datastore.get(user)).thenReturn(limitMap);
+
+        rateLimitCache.updateLimit(HttpMethod.GET, user, key, rate, datastoreWarnLimit);
+
+        verify(datastore).put(eq(user), any(Serializable.class), eq(1), eq(java.util.concurrent.TimeUnit.HOURS));
+    }
+
+    @Test
+    public void updateLimit_shouldReturnValidNextAvailableResponse() throws Exception {
+        final String account = "12345", key = "variant";
+        final ConfiguredRatelimit rate = new ConfiguredRatelimit();
+        rate.setUriRegex(".*");
+        rate.setValue(2);
+        rate.setUnit(TimeUnit.HOUR);
+
+        final HashMap<String, CachedRateLimit> liveLimitMap = new HashMap<String, CachedRateLimit>();
+        liveLimitMap.put(key, new CachedRateLimit(".*"));
+
+        when(datastore.get(account)).thenReturn(liveLimitMap);
+
+        assertTrue(rateLimitCache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
+        assertTrue(rateLimitCache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
+        assertFalse(rateLimitCache.updateLimit(HttpMethod.GET, account, key, rate, datastoreWarnLimit).hasRequestsRemaining());
     }
 }
