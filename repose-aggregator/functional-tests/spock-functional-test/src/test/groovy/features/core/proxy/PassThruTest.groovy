22e71c50d77024540dccc230a2cfaff05a88c4b3
Richard Sartor <richard.sartor@rackspace.com>
2014-02-04 12:10:48 -0600

    Add test cases for the response as well as the request.

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/proxy/PassThruTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/proxy/PassThruTest.groovy
@@ -4,6 +4,7 @@ import framework.ReposeValveTest
 import framework.category.Bug
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
+import org.rackspace.deproxy.Response
 import spock.lang.Unroll
 import org.junit.experimental.categories.Category
 
@@ -74,8 +75,8 @@ class PassThruTest extends ReposeValveTest {
     }
 
     @Category(Bug.class) // Defect D-11822
-    @Unroll("Should not interfere with semicolons and equals signs: #name - #value")
-    def "Should not interfere with semicolons and equals signs"() {
+    @Unroll("Should not interfere with semicolons and equals signs in request: #name - #value")
+    def "Should not interfere with semicolons and equals signs in request"() {
 
         //This test checks that Repose is not trying to look for qvalues in the
         // field values of headers that don't accept qvalues.
@@ -111,6 +112,90 @@ class PassThruTest extends ReposeValveTest {
         mc.handlings.size() == 1
         mc.handlings[0].request.headers.getCountByName(name) == 1
         mc.handlings[0].request.headers[name] == value
+
+        where:
+        name                  | value
+        'Accept-Ranges'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Age'                 | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Allow'               | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Authorization'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Cache-Control'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Encoding'    | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Language'    | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Length'      | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Location'    | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-MD5'         | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Range'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Content-Type'        | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Date'                | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'ETag'                | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Expires'             | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'From'                | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'If-Match'            | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'If-Modified-Since'   | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'If-None-Match'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'If-Range'            | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'If-Unmodified-Since' | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Last-Modified'       | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Location'            | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Max-Forwards'        | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Pragma'              | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Range'               | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Referer'             | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Retry-After'         | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Server'              | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'User-Agent'          | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Vary'                | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Via'                 | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'Warning'             | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'WWW-Authenticate'    | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+
+        'Extension-Header'    | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+        'X-Extension-Header'  | 'something/1.0; q=0.5, another/0.8 ;q=0.8=0.3 ; a=b=c ; abc'
+    }
+
+    @Category(Bug.class) // Defect D-11822
+    @Unroll("Should not interfere with semicolons and equals signs in response: #name - #value")
+    def "Should not interfere with semicolons and equals signs in response"() {
+
+        //This test checks that Repose is not trying to look for qvalues in the
+        // field values of headers that don't accept qvalues.
+
+        // Substituting test values for the Host and Expect headers generally breaks
+        // the protocol and confuses servers. Therefore, they are not included in
+        // this test.
+
+        // The Accept, Accept-Charset, Accept-Encoding, Accept-Language, and TE
+        // headers accept qvalue parameters. They will be considered as-is, and are
+        // not subject to the requirement.
+
+        // The X-PP-User, X-PP-Groups, and X-Roles headers, while not defined by
+        // RFC 2616, are well known to Repose and utilize qvalues. They are not
+        // subject to the requirement.
+
+        // The Connection, Keep-Alive, Proxy-Authenticate, Proxy-Authorization,
+        // TE, Trailer, Transfer-Encoding, Upgrade, and trailers in the entity
+        // body are "hop-by-hop" headers. These should not be naively forwarded
+        // by a proxy, and are thus not subject to the requirement.
+
+        // All other headers that are defined in RFC 2616 but not mentioned
+        // above are tested, as well as some example extension headers not
+        // defined by RFC 2616.
+
+        given:
+        def headers = [(name): value]
+
+        when:
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint,
+                defaultHandler: { request -> new Response(200, "OK", headers) })
+
+        then:
+        mc.handlings.size() == 1
+        mc.handlings[0].response.code == "200"
+        mc.receivedResponse.code == "200"
+        mc.receivedResponse.headers.getCountByName(name) == 1
+        mc.receivedResponse.headers[name] == value
 
         where:
         name                  | value
