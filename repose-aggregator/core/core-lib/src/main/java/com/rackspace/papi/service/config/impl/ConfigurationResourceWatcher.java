a92c518b51e38790f8b73dd6edd5a8b47185abfe
David Kowis <david.kowis@rackspace.com>
2014-02-27 14:09:24 -0600

    Formatting, restoring normal 4 spaces

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/config/impl/ConfigurationResourceWatcher.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/config/impl/ConfigurationResourceWatcher.java
@@ -19,39 +19,38 @@ import java.util.Map;
  * However, one of the requirements of PAPI is that a user can drop a resource
  * into the resources directory and PAPI will begin processing it on the fly. So
  * these "standard" exceptions will be caught but will allow the normal thread
- * processing to continue.   
- *
+ * processing to continue.
  */
 public class ConfigurationResourceWatcher implements RecurringTask {
 
-   private static final Logger LOG = LoggerFactory.getLogger(ConfigurationResourceWatcher.class);
-   private final Map<String, ConfigurationResource> watchMap;
-   private final EventService eventManager;
-   
-   public ConfigurationResourceWatcher(EventService eventManager) {
-      this.eventManager = eventManager;
-      watchMap = new HashMap<String, ConfigurationResource>();
-   }
+    private static final Logger LOG = LoggerFactory.getLogger(ConfigurationResourceWatcher.class);
+    private final Map<String, ConfigurationResource> watchMap;
+    private final EventService eventManager;
 
-   @Override
-   public synchronized void run() {
-      for (ConfigurationResource resource : watchMap.values()) {
-         try {
-            if (resource.updated()) {
-               eventManager.newEvent(ConfigurationEvent.UPDATE, resource);
-               LOG.info("Updated " + resource.name());
+    public ConfigurationResourceWatcher(EventService eventManager) {
+        this.eventManager = eventManager;
+        watchMap = new HashMap<String, ConfigurationResource>();
+    }
+
+    @Override
+    public synchronized void run() {
+        for (ConfigurationResource resource : watchMap.values()) {
+            try {
+                if (resource.updated()) {
+                    eventManager.newEvent(ConfigurationEvent.UPDATE, resource);
+                    LOG.info("Updated " + resource.name());
+                }
+            } catch (Exception e) {
+                // TODO:Log - Create a logger that is smart enough not to print out errors we don't care about more than once
             }
-         } catch (Exception e) {
-            // TODO:Log - Create a logger that is smart enough not to print out errors we don't care about more than once
-         }
-      }
-   }
+        }
+    }
 
-   public synchronized void watch(ConfigurationResource resource) {
-      watchMap.put(resource.name(), resource);
-   }
+    public synchronized void watch(ConfigurationResource resource) {
+        watchMap.put(resource.name(), resource);
+    }
 
-   public synchronized void stopWatching(String resourceName) {
-      watchMap.remove(resourceName);
-   }
+    public synchronized void stopWatching(String resourceName) {
+        watchMap.remove(resourceName);
+    }
 }
