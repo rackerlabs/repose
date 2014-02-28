58774ccc9b759bfe1a4de8515917a895db217543
malconis <meynard.alconis@rackspace.com>
2014-02-28 08:10:22 -0600

    adding passing of dist-datastore ports to configuration for distdatashutdown test

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/services/datastore/DistDataShutdownTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/services/datastore/DistDataShutdownTest.groovy
@@ -2,22 +2,29 @@ package features.services.datastore
 
 import framework.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.PortFinder
 
 class DistDataShutdownTest extends ReposeValveTest {
 
-    def setupSpec(){
-         deproxy = new Deproxy()
-         deproxy.addEndpoint(properties.targetPort)
-     }
+    def setupSpec() {
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+    }
 
-     def cleanupSpec(){
-         if (deproxy)
-             deproxy.shutdown()
-     }
+    def cleanupSpec() {
+        if (deproxy)
+            deproxy.shutdown()
+    }
 
-    def "when configured with dist datastore as a service should shutdown nicely when asked" () {
+    def "when configured with dist datastore as a service should shutdown nicely when asked"() {
         given: "repose is configured with dist datastore"
         def params = properties.getDefaultTemplateParams()
+        int dataStorePort1 = PortFinder.Singleton.getNextOpenPort()
+        int dataStorePort2 = PortFinder.Singleton.getNextOpenPort()
+        params += [
+                'datastorePort1': dataStorePort1,
+                'datastorePort2': dataStorePort2
+        ]
         repose.configurationProvider.applyConfigs("common", params)
         repose.configurationProvider.applyConfigs("features/services/datastore", params)
         repose.start()
