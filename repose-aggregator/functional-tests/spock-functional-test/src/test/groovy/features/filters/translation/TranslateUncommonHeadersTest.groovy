cecf86ecdeb613ce93773161e345b217a8fe321a
Richard Sartor <richard.sartor@rackspace.com>
2014-03-06 16:25:22 -0600

    Clean up.

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/translation/TranslateUncommonHeadersTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/translation/TranslateUncommonHeadersTest.groovy
@@ -5,84 +5,110 @@ import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.Response
 import spock.lang.Unroll
 
-/**
- * User: dimi5963
- * Date: 3/5/14
- * Time: 9:49 AM
- */
 class TranslateUncommonHeadersTest extends ReposeValveTest {
     def static Map acceptXML = ["accept": "application/xml"]
-    def static Map acceptJSON = ["accept": "application/json"]
     def static Map contentTypeAtomXML = ['content-type': 'application/atom+xml']
     def static Map contentTypeXML = ['content-type': 'application/xml']
-    def static Map contentTypeJSON = ['content-type': 'application/json']
     def static Map weirdContentTypeAtomXML = ['content-type': 'application/atom+xml; type=entry;charset=utf-8']
     def static Map weirdAcceptAtomXML = ['accept': 'application/atom+xml; type=entry;charset=utf-8']
     def static Map weirdContentTypeXML = ['content-type': 'application/xml; type=entry;charset=utf-8']
     def static Map weirdAcceptXML = ['accept': 'application/xml; type=entry;charset=utf-8']
-    def static Map weirdContentTypeJSON = ['content-type': 'application/json; type=entry;charset=utf-8']
-    def static Map weirdAcceptJSON = ['accept': 'application/json; type=entry;charset=utf-8']
-    def static respXMLBody = '<atom:entry xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://wadl.dev.java.net/2009/02" xmlns:db="http://docbook.org/ns/docbook" xmlns:error="http://docs.rackspace.com/core/error" xmlns:wadl="http://wadl.dev.java.net/2009/02" xmlns:json="http://json-schema.org/schema#" xmlns:d317e1="http://wadl.dev.java.net/2009/02" xmlns:usage-summary="http://docs.rackspace.com/core/usage-summary">\n' +
-            '  <atom:title type="text">Slice Action</atom:title>\n' +
-            '  <atom:author>\n' +
-            '    <atom:name>Name</atom:name>\n' +
-            '  </atom:author>\n' +
-            '  <atom:content type="application/xml">\n' +
-            '    <event xmlns="http://docs.rackspace.com/core/event" xmlns:csd="http://docs.rackspace.com/event/servers/slice">\n' +
-            '      <csd:product huddleId="202" rootPassword="12345678" version="1">\n' +
-            '        <csd:sliceMetaData key="key1" value="value1"/>\n' +
-            '        <csd:additionalPublicAddress dns1="1.1.1.1" dns2="1.1.1.1" ip="1.1.1.1"/>\n' +
-            '      </csd:product>\n' +
-            '    </event>\n' +
-            '  </atom:content>\n' +
-            '</atom:entry>'
-
-    def static respJSONBody = '{"id": "remove-me"}'
+    def static sampleAtomEntry =
+        """<atom:entry xmlns:atom="http://www.w3.org/2005/Atom"
+            xmlns="http://wadl.dev.java.net/2009/02" 
+            xmlns:db="http://docbook.org/ns/docbook" 
+            xmlns:error="http://docs.rackspace.com/core/error" 
+            xmlns:wadl="http://wadl.dev.java.net/2009/02" 
+            xmlns:json="http://json-schema.org/schema#" 
+            xmlns:d317e1="http://wadl.dev.java.net/2009/02" 
+            xmlns:usage-summary="http://docs.rackspace.com/core/usage-summary">
+  <atom:title type="text">Slice Action</atom:title>
+  <atom:author>
+    <atom:name>Name</atom:name>
+  </atom:author>
+  <atom:content type="application/xml">
+    <event xmlns="http://docs.rackspace.com/core/event" xmlns:csd="http://docs.rackspace.com/event/servers/slice">
+      <csd:product huddleId="202" rootPassword="12345678" version="1">
+        <csd:sliceMetaData key="key1" value="value1"/>
+        <csd:additionalPublicAddress dns1="1.1.1.1" dns2="1.1.1.1" ip="1.1.1.1"/>
+      </csd:product>
+    </event>
+  </atom:content>
+</atom:entry>"""
 
     //Start repose once for this particular translation test
     def setupSpec() {
 
-        def params = properties.getDefaultTemplateParams()
-        repose.configurationProvider.applyConfigs("common", params)
-        repose.configurationProvider.applyConfigs("features/filters/translation/common", params)
-        repose.configurationProvider.applyConfigs("features/filters/translation/uncommon", params)
-        repose.start()
-
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort)
+
+        def params = properties.getDefaultTemplateParams()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/translation/uncommon", params)
+        repose.start()
     }
 
     def cleanupSpec() {
-        deproxy.shutdown()
-        repose.stop()
+
+        if (repose) {
+            repose.stop()
+        }
+        if (deproxy) {
+            deproxy.shutdown()
+        }
     }
 
-    @Unroll("response: #respHeaders, request: #reqHeaders - #reqHeaders")
-    def "when translating responses with uncommon headers"(){
+    @Unroll("when translating requests with uncommon headers - #reqHeaders")
+    def "when translating requests with uncommon headers"() {
+
+        when: "User sends requests through repose"
+        def mc = deproxy.makeRequest(url: reposeEndpoint, method: method, headers: reqHeaders, requestBody: sampleAtomEntry)
+
+        then: "The requests makes it to the origin service"
+        mc.handlings.size() == 1
+        mc.sentRequest.body.contains("csd:product")
+        mc.handlings[0].request.body.contains("csd:product")
+
+        and: "Repose removes the indicated xml attributes from the request body"
+        mc.sentRequest.body.contains("rootPassword")
+        mc.sentRequest.body.contains("huddleId")
+        !mc.handlings[0].request.body.contains("rootPassword")
+        !mc.handlings[0].request.body.contains("huddleId")
+
+        where:
+        reqHeaders                                   |  method
+        acceptXML + contentTypeAtomXML               |  "POST"
+        weirdAcceptAtomXML + weirdContentTypeAtomXML |  "POST"
+        acceptXML + contentTypeXML                   |  "POST"
+        weirdAcceptXML + weirdContentTypeXML         |  "POST"
+    }
+
+    @Unroll("when translating responses with uncommon headers - #respHeaders")
+    def "when translating responses with uncommon headers"() {
 
         given: "Origin service returns body of type " + respHeaders
-        def xmlResp = { request -> return new Response(200, "OK", respHeaders, respBody) }
+        def handler = { request -> return new Response(200, "OK", respHeaders, sampleAtomEntry) }
 
 
         when: "User sends requests through repose"
-        def resp = deproxy.makeRequest(url:(String) reposeEndpoint, method:method, headers:reqHeaders, requestBody:reqBody, defaultHandler:xmlResp)
+        def mc = deproxy.makeRequest(url: reposeEndpoint, headers: reqHeaders, defaultHandler: handler)
 
-        then: "Response body should contain"
-        !resp.receivedResponse.body.contains(notContains)
-        !resp.handlings[0].request.body.contains(notContains)
+        then: "The requests makes it to the origin service"
+        mc.handlings.size() == 1
+        mc.handlings[0].response.body.contains("csd:product")
+        mc.receivedResponse.body.contains("csd:product")
 
-        and: "Response code should be"
-        resp.receivedResponse.code.equalsIgnoreCase("200")
+        and: "Repose removes the indicated xml attributes from the response body"
+        mc.handlings[0].response.body.contains("rootPassword")
+        mc.handlings[0].response.body.contains("huddleId")
+        !mc.receivedResponse.body.contains("rootPassword")
+        !mc.receivedResponse.body.contains("huddleId")
 
         where:
-        respHeaders                                  | reqHeaders                                    | notContains    | respBody     | reqBody       | method
-        weirdContentTypeAtomXML                      | acceptXML                                     | "rootPassword" | respXMLBody  | ""            | "GET"
-        weirdContentTypeAtomXML + weirdAcceptAtomXML | acceptXML                                     | "rootPassword" | respXMLBody  | ""            | "GET"
-        weirdContentTypeAtomXML + weirdAcceptAtomXML | acceptXML + contentTypeAtomXML                | "rootPassword" | respXMLBody  | respXMLBody   | "POST"
-        weirdContentTypeAtomXML + weirdAcceptAtomXML | weirdAcceptAtomXML + weirdContentTypeAtomXML  | "rootPassword" | respXMLBody  | respXMLBody   | "GET"
-        weirdContentTypeXML                          | acceptXML                                     | "rootPassword" | respXMLBody  | ""            | "GET"
-        weirdContentTypeXML + weirdAcceptXML         | acceptXML                                     | "rootPassword" | respXMLBody  | ""            | "GET"
-        weirdContentTypeXML + weirdAcceptXML         | acceptXML + contentTypeXML                    | "rootPassword" | respXMLBody  | respXMLBody   | "POST"
-        weirdContentTypeXML + weirdAcceptXML         | weirdAcceptXML + weirdContentTypeXML          | "rootPassword" | respXMLBody  | respXMLBody   | "GET"
+        respHeaders                                  | reqHeaders
+        weirdContentTypeAtomXML                      | acceptXML
+        weirdContentTypeAtomXML + weirdAcceptAtomXML | acceptXML
+        weirdContentTypeXML                          | acceptXML
+        weirdContentTypeXML + weirdAcceptXML         | acceptXML
     }
 }
