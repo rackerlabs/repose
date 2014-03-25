7efc1a66018b99cc264b1ace95ff8dbc0dced3cf
David Kowis <david.kowis@rackspace.com>
2014-03-25 14:19:38 -0500

    Reformatted these files to match project standards

--- repose-aggregator/components/filters/client-auth/src/main/java/com/rackspace/papi/components/clientauth/atomfeed/sax/AdminTokenProvider.java
+++ repose-aggregator/components/filters/client-auth/src/main/java/com/rackspace/papi/components/clientauth/atomfeed/sax/AdminTokenProvider.java
@@ -3,15 +3,14 @@ package com.rackspace.papi.components.clientauth.atomfeed.sax;
 // Retrieves admin tokens from os auth for the atom feed reader
 // TODO: this should be refactored into the openstack stuff I think
 // TODO: also I wish JSON instead of sax and xml and marshalling :(
+
 import com.rackspace.auth.ResponseUnmarshaller;
 import com.rackspace.auth.openstack.AdminToken;
 import com.rackspace.papi.commons.util.http.HttpStatusCode;
-import com.rackspace.papi.commons.util.http.ServiceClient;
 import com.rackspace.papi.commons.util.http.ServiceClientResponse;
 import com.rackspace.papi.commons.util.transform.jaxb.JaxbEntityToXml;
 import com.rackspace.papi.service.serviceclient.akka.AkkaServiceClient;
 import org.openstack.docs.identity.api.v2.*;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
@@ -22,77 +21,77 @@ import java.util.HashMap;
 
 public class AdminTokenProvider {
 
-   private String authUrl;
+    private String authUrl;
     private AkkaServiceClient client;
-   private JAXBContext coreJaxbContext;
-   private ResponseUnmarshaller marshaller;
-   private AdminToken curAdminToken;
-   private final String requestBody;
-   private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AdminTokenProvider.class);
+    private JAXBContext coreJaxbContext;
+    private ResponseUnmarshaller marshaller;
+    private AdminToken curAdminToken;
+    private final String requestBody;
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AdminTokenProvider.class);
 
-   public AdminTokenProvider(AkkaServiceClient client, String authUrl, String username, String password) {
-      this.client = client;
-      this.authUrl = authUrl;
-       setJAXBContext();
-      marshaller = new ResponseUnmarshaller(coreJaxbContext);
-      ObjectFactory factory = new ObjectFactory();
+    public AdminTokenProvider(AkkaServiceClient client, String authUrl, String username, String password) {
+        this.client = client;
+        this.authUrl = authUrl;
+        setJAXBContext();
+        marshaller = new ResponseUnmarshaller(coreJaxbContext);
+        ObjectFactory factory = new ObjectFactory();
 
-      PasswordCredentialsRequiredUsername credentials = new PasswordCredentialsRequiredUsername();
-      credentials.setUsername(username);
-      credentials.setPassword(password);
+        PasswordCredentialsRequiredUsername credentials = new PasswordCredentialsRequiredUsername();
+        credentials.setUsername(username);
+        credentials.setPassword(password);
 
-      JAXBElement jaxbCredentials = factory.createPasswordCredentials(credentials);
+        JAXBElement jaxbCredentials = factory.createPasswordCredentials(credentials);
 
-      AuthenticationRequest request = new AuthenticationRequest();
-      request.setCredential(jaxbCredentials);
+        AuthenticationRequest request = new AuthenticationRequest();
+        request.setCredential(jaxbCredentials);
 
-      JaxbEntityToXml jaxbToString = new JaxbEntityToXml(coreJaxbContext);
-      requestBody = jaxbToString.transform(factory.createAuth(request));
-   }
+        JaxbEntityToXml jaxbToString = new JaxbEntityToXml(coreJaxbContext);
+        requestBody = jaxbToString.transform(factory.createAuth(request));
+    }
 
-   private void setJAXBContext() {
-      try {
-         coreJaxbContext = JAXBContext.newInstance(
-                 org.openstack.docs.identity.api.v2.ObjectFactory.class,
-                 com.rackspace.docs.identity.api.ext.rax_auth.v1.ObjectFactory.class);
-      } catch (JAXBException ex) {
-          LOG.trace("Unable to set JAXB Context", ex);
-      }
-   }
-   
-   public String getFreshAdminToken(){
-      
-      curAdminToken = null;
-      return getAdminToken();
-   }
+    private void setJAXBContext() {
+        try {
+            coreJaxbContext = JAXBContext.newInstance(
+                    org.openstack.docs.identity.api.v2.ObjectFactory.class,
+                    com.rackspace.docs.identity.api.ext.rax_auth.v1.ObjectFactory.class);
+        } catch (JAXBException ex) {
+            LOG.trace("Unable to set JAXB Context", ex);
+        }
+    }
 
-   public String getAdminToken() {
+    public String getFreshAdminToken() {
 
-      String adminToken = curAdminToken != null && curAdminToken.isValid() ? curAdminToken.getToken() : null;
+        curAdminToken = null;
+        return getAdminToken();
+    }
 
-      if (adminToken == null) {
-          final ServiceClientResponse serviceResponse = client.post(AkkaServiceClient.ADMIN_TOKEN_KEY,
-                  authUrl + "/tokens",
-                  new HashMap<String, String>(),
-                  requestBody,
-                  MediaType.APPLICATION_XML_TYPE );
+    public String getAdminToken() {
 
-         switch (HttpStatusCode.fromInt(serviceResponse.getStatusCode())) {
-            case OK:
-               final AuthenticateResponse authenticateResponse = marshaller.unmarshall(serviceResponse.getData(), AuthenticateResponse.class);
+        String adminToken = curAdminToken != null && curAdminToken.isValid() ? curAdminToken.getToken() : null;
 
-               Token token = authenticateResponse.getToken();
-               curAdminToken = new AdminToken(token.getId(), token.getExpires().toGregorianCalendar());
-               adminToken = curAdminToken.getToken();
-               break;
+        if (adminToken == null) {
+            final ServiceClientResponse serviceResponse = client.post(AdminToken.CACHE_KEY,
+                    authUrl + "/tokens",
+                    new HashMap<String, String>(),
+                    requestBody,
+                    MediaType.APPLICATION_XML_TYPE);
 
-            default:
-               LOG.error("Unable to get admin token.  Verify admin credentials. " + serviceResponse.getStatusCode());
-               curAdminToken = null;
-               break;
-         }
-      }
+            switch (HttpStatusCode.fromInt(serviceResponse.getStatusCode())) {
+                case OK:
+                    final AuthenticateResponse authenticateResponse = marshaller.unmarshall(serviceResponse.getData(), AuthenticateResponse.class);
 
-      return adminToken;
-   }
+                    Token token = authenticateResponse.getToken();
+                    curAdminToken = new AdminToken(token.getId(), token.getExpires().toGregorianCalendar());
+                    adminToken = curAdminToken.getToken();
+                    break;
+
+                default:
+                    LOG.error("Unable to get admin token.  Verify admin credentials. " + serviceResponse.getStatusCode());
+                    curAdminToken = null;
+                    break;
+            }
+        }
+
+        return adminToken;
+    }
 }
--- repose-aggregator/components/filters/client-auth/src/test/java/com/rackspace/papi/components/clientauth/atomfeed/sax/SaxAuthFeedReaderTest.java
+++ repose-aggregator/components/filters/client-auth/src/test/java/com/rackspace/papi/components/clientauth/atomfeed/sax/SaxAuthFeedReaderTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rackspace.papi.components.clientauth.atomfeed.sax;
 
 import com.rackspace.papi.commons.util.http.ServiceClient;
@@ -25,73 +21,73 @@ import static org.mockito.Mockito.when;
 
 public class SaxAuthFeedReaderTest {
 
-   private ServiceClient client;
+    private ServiceClient client;
     private AkkaServiceClient akkaClient;
-   private ServiceClientResponse resp1, resp2, resp3;
-   private SaxAuthFeedReader reader;
+    private ServiceClientResponse resp1, resp2, resp3;
+    private SaxAuthFeedReader reader;
 
-   @Before
-   public void setUp() throws FileNotFoundException {
+    @Before
+    public void setUp() throws FileNotFoundException {
 
-      AppenderForTesting.clear();
+        AppenderForTesting.clear();
 
-      client = mock(ServiceClient.class);
-       akkaClient = mock(AkkaServiceClient.class);
+        client = mock(ServiceClient.class);
+        akkaClient = mock(AkkaServiceClient.class);
 
-      FilePathReaderImpl fileReader1 = new FilePathReaderImpl(File.separator + "META-INF" + File.separator + "feed.xml");
-      FilePathReaderImpl fileReader2 = new FilePathReaderImpl(File.separator + "META-INF" + File.separator + "empty-feed.xml");
-      resp1 = new ServiceClientResponse(200, fileReader1.getResourceAsStream());
+        FilePathReaderImpl fileReader1 = new FilePathReaderImpl(File.separator + "META-INF" + File.separator + "feed.xml");
+        FilePathReaderImpl fileReader2 = new FilePathReaderImpl(File.separator + "META-INF" + File.separator + "empty-feed.xml");
+        resp1 = new ServiceClientResponse(200, fileReader1.getResourceAsStream());
 
-      resp2 = new ServiceClientResponse(200, fileReader2.getResourceAsStream());
-       when(client.getPoolSize()).thenReturn(100);
+        resp2 = new ServiceClientResponse(200, fileReader2.getResourceAsStream());
+        when(client.getPoolSize()).thenReturn(100);
 
-   }
+    }
 
-   @After
-   public void tearDown() {
-      AppenderForTesting.clear();
-   }
+    @After
+    public void tearDown() {
+        AppenderForTesting.clear();
+    }
 
-   @Test
-   public void shouldRetrieveUserAndTokenKeysFromAtomFeed() {
+    @Test
+    public void shouldRetrieveUserAndTokenKeysFromAtomFeed() {
 
-      when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp1);
-      when(client.get(eq("https://test.feed.atomhopper.rackspace.com/some/identity/feed/?marker=urn:uuid:b23a9c7f-5489-4fd8-bf10-3292032d805f&limit=25&search=&direction=forward"),
-              anyMap())).thenReturn(resp2);
-      reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
-      CacheKeys keys = reader.getCacheKeys();
+        when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp1);
+        when(client.get(eq("https://test.feed.atomhopper.rackspace.com/some/identity/feed/?marker=urn:uuid:b23a9c7f-5489-4fd8-bf10-3292032d805f&limit=25&search=&direction=forward"),
+                anyMap())).thenReturn(resp2);
+        reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
+        CacheKeys keys = reader.getCacheKeys();
 
-      String[] users = {"224277258"}; //User from atom feed
-      String[] tokens = {"834d3be1-c479-11e2-8b8b-0800200c9a66"}; //token from atom feed
+        String[] users = {"224277258"}; //User from atom feed
+        String[] tokens = {"834d3be1-c479-11e2-8b8b-0800200c9a66"}; //token from atom feed
 
-      assertArrayEquals("Retrieved key should have user from atom feed", keys.getUserKeys().toArray(), users);
-      assertArrayEquals("Retrieved keys should have token from atom feed", keys.getTokenKeys().toArray(), tokens);
-   }
+        assertArrayEquals("Retrieved key should have user from atom feed", keys.getUserKeys().toArray(), users);
+        assertArrayEquals("Retrieved keys should have token from atom feed", keys.getTokenKeys().toArray(), tokens);
+    }
 
-   @Test
-   public void shouldLogUnauthorizedFeeds() {
+    @Test
+    public void shouldLogUnauthorizedFeeds() {
 
-      resp3 = new ServiceClientResponse(401, null);
-      when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp3);
-      reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
-      CacheKeys keys = reader.getCacheKeys();
+        resp3 = new ServiceClientResponse(401, null);
+        when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp3);
+        reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
+        CacheKeys keys = reader.getCacheKeys();
 
-      assertEquals("Should log 401 with atom feed configured without auth", "Feed at http://some.junit.test.feed/at/somepath requires Authentication. Please "
-              + "reconfigure Feed atomId with valid credentials and/or configure isAuthed to true", AppenderForTesting.getMessages()[0]);
+        assertEquals("Should log 401 with atom feed configured without auth", "Feed at http://some.junit.test.feed/at/somepath requires Authentication. Please "
+                + "reconfigure Feed atomId with valid credentials and/or configure isAuthed to true", AppenderForTesting.getMessages()[0]);
 
 
-   }
+    }
 
-   @Test
-   public void shouldLogServerErrorFromAtomFeeds() {
+    @Test
+    public void shouldLogServerErrorFromAtomFeeds() {
 
-      resp3 = new ServiceClientResponse(503, null);
-      when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp3);
-      reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
+        resp3 = new ServiceClientResponse(503, null);
+        when(client.get(eq("http://some.junit.test.feed/at/somepath"), anyMap())).thenReturn(resp3);
+        reader = new SaxAuthFeedReader(client, akkaClient, "http://some.junit.test.feed/at/somepath", "atomId");
 
-      CacheKeys keys = reader.getCacheKeys();
+        CacheKeys keys = reader.getCacheKeys();
 
-      assertEquals("Unable to retrieve atom feed from FeedatomId: http://some.junit.test.feed/at/somepath\n"
-              + " Response Code: 503", AppenderForTesting.getMessages()[0]);
-   }
+        assertEquals("Unable to retrieve atom feed from FeedatomId: http://some.junit.test.feed/at/somepath\n"
+                + " Response Code: 503", AppenderForTesting.getMessages()[0]);
+    }
 }
