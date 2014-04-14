67b66c098d43eea4bf2d55b2f1c06a43dd284c6a
jenny.vo <jennyvo@ubuntu.ubuntu-domain>
2014-04-14 17:00:02 -0500

    update...

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/slf4jlogging/Slf4jHttpLoggingTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/slf4jlogging/Slf4jHttpLoggingTest.groovy
@@ -1,7 +1,8 @@
-package features.filters.httplog.slf4jlogging
+package features.filters.slf4jlogging
 
 import framework.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
 
 /**
  * Created by jennyvo on 4/8/14.
@@ -10,10 +11,19 @@ class Slf4jHttpLoggingTest extends ReposeValveTest{
     def setupSpec() {
         def params = properties.defaultTemplateParams
         repose.configurationProvider.applyConfigs("common", params)
-        repose.configurationProvider.applyConfigs("features/filters/httplogging/slf4httplogging", params)
+        repose.configurationProvider.applyConfigs("features/filters/slf4jhttplogging", params)
         repose.start()
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort)
+
+    }
+
+    def "Test check slf4log" () {
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint)
+
+        then:
+        1 == 1
 
     }
 
