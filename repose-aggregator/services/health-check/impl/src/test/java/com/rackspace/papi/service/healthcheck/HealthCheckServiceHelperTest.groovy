0327f8b135a124f59c52a41947646745e9887a88
Damien Johnson <damien.johnson@rackspace.com>
2014-04-15 09:14:40 -0500

    Converted HealthCheckServiceHelper unit test to Spock

--- repose-aggregator/services/health-check/impl/src/test/java/com/rackspace/papi/service/healthcheck/HealthCheckServiceHelperTest.groovy
+++ repose-aggregator/services/health-check/impl/src/test/java/com/rackspace/papi/service/healthcheck/HealthCheckServiceHelperTest.groovy
@@ -1,47 +1,73 @@
 package com.rackspace.papi.service.healthcheck
-import org.junit.Before
-import org.junit.Test
+
 import org.slf4j.Logger
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
 
-import static org.mockito.Matchers.eq
 import static org.mockito.Matchers.any
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.never
-import static org.mockito.Mockito.verify
+import static org.mockito.Matchers.eq
+import static org.mockito.Mockito.*
 
-class HealthCheckServiceHelperTest {
+class HealthCheckServiceHelperTest extends Specification {
+    private static final TEST_UID = "test_uid"
+    private static final TEST_RID = "test_rid"
+
+    @Shared
     private HealthCheckServiceHelper healthCheckServiceHelper
 
+    @Shared
     private HealthCheckService hcs
+
+    @Shared
     private Logger lgr
 
-    @Before
-    void setUp() {
+    void setupSpec() {
         hcs = mock(HealthCheckService.class)
         lgr = mock(Logger.class)
-        String uid = "test_uid"
 
-        healthCheckServiceHelper = new HealthCheckServiceHelper(hcs, lgr, uid)
+        healthCheckServiceHelper = new HealthCheckServiceHelper(hcs, lgr, TEST_UID)
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    void "when passed non-null arguments on construction, should not throw IllegalArgumentException"() {
+        when:
+        new HealthCheckServiceHelper(hcs, lgr, TEST_UID)
+
+        then:
+        notThrown(IllegalArgumentException.class)
+    }
+
+    @Unroll("when HealthCheckServiceHelper(#constructorString) is constructed, should throw exception")
     void "when passed null arguments on construction, should throw IllegalArgumentException"() {
-        new HealthCheckServiceHelper(null, null, null)
+        when:
+        new HealthCheckServiceHelper(healthCheckService, logger, uid)
+
+        then:
+        thrown(IllegalArgumentException.class)
+
+        where:
+        healthCheckService | logger | uid      | constructorString
+        hcs                | lgr    | null     | "hcs, lgr, null"
+        hcs                | null   | TEST_UID | "hcs, null, uid"
+        null               | lgr    | TEST_UID | "null, lgr, uid"
+        null               | null   | null     | "null, null, null"
     }
 
-    @Test
     void "when a valid issue is reported, should report the issue to the health check service"() {
-        healthCheckServiceHelper.reportIssue("test_rid", "some_message", Severity.BROKEN)
+        when:
+        healthCheckServiceHelper.reportIssue(TEST_RID, "some_message", Severity.BROKEN)
 
-        verify(hcs).reportIssue(eq("test_uid"), eq("test_rid"), any(HealthCheckReport.class))
+        then:
+        verify(hcs).reportIssue(eq(TEST_UID), eq(TEST_RID), any(HealthCheckReport.class))
         verify(lgr, never()).error(any(String.class))
     }
 
-    @Test
     void "when a valid issue is resolved, should resolve the issue with the health check service"() {
-        healthCheckServiceHelper.resolveIssue("test_rid")
+        when:
+        healthCheckServiceHelper.resolveIssue(TEST_RID)
 
-        verify(hcs).solveIssue("test_uid", "test_rid")
+        then:
+        verify(hcs).solveIssue(TEST_UID, TEST_RID)
         verify(lgr, never()).error(any(String.class))
     }
 
