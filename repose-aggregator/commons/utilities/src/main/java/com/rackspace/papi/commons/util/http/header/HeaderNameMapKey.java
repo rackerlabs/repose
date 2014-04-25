3f280668d4256cc8fd771e95e139e6d663dead8b
Damien Johnson <damien.johnson@rackspace.com>
2014-04-25 15:28:05 -0500

    Added HeaderNameMapKey wrapper

--- repose-aggregator/commons/utilities/src/main/java/com/rackspace/papi/commons/util/http/header/HeaderNameMapKey.java
+++ repose-aggregator/commons/utilities/src/main/java/com/rackspace/papi/commons/util/http/header/HeaderNameMapKey.java
@@ -1,4 +1,37 @@
 package com.rackspace.papi.commons.util.http.header;
 
 public class HeaderNameMapKey {
+    private String headerName;
+
+    public HeaderNameMapKey(String headerName) {
+        this.headerName = headerName;
+    }
+
+    public String getName() {
+        return headerName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+
+        HeaderNameMapKey that = (HeaderNameMapKey) o;
+
+        if (headerName != null ? !headerName.equalsIgnoreCase(that.headerName) : that.headerName != null) { return false; }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return headerName != null ? headerName.toLowerCase().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "HeaderNameMapKey{" +
+                "headerName='" + headerName + '\'' +
+                '}';
+    }
 }
--- repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/header/HeaderNameMapKeyTest.groovy
+++ repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/header/HeaderNameMapKeyTest.groovy
@@ -1,5 +1,40 @@
 package com.rackspace.papi.commons.util.http.header
+import org.junit.Test
 
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.core.IsNot.not
+import static org.junit.Assert.assertThat
 
 class HeaderNameMapKeyTest {
+    public static final String HEADER_NAME = "SomeName"
+
+    @Test
+    void "getName returns original string"() throws Exception {
+        HeaderNameMapKey headerNameMapKey = new HeaderNameMapKey(HEADER_NAME)
+
+        assertThat(headerNameMapKey.getName(), equalTo(HEADER_NAME))
+    }
+
+    @Test
+    void "getName does not change case"() throws Exception {
+        HeaderNameMapKey headerNameMapKey = new HeaderNameMapKey(HEADER_NAME)
+
+        assertThat(headerNameMapKey.getName(), not(equalTo(HEADER_NAME.toLowerCase())))
+    }
+
+    @Test
+    void "equals performs case insensitive comparison"() throws Exception {
+        HeaderNameMapKey headerNameMapKey = new HeaderNameMapKey(HEADER_NAME)
+        HeaderNameMapKey headerNameMapKeyLower = new HeaderNameMapKey(HEADER_NAME.toLowerCase())
+
+        assertThat(headerNameMapKey, equalTo(headerNameMapKeyLower))
+    }
+
+    @Test
+    void "generated hashCode is case insensitive"() throws Exception {
+        HeaderNameMapKey headerNameMapKey = new HeaderNameMapKey(HEADER_NAME)
+        HeaderNameMapKey headerNameMapKeyLower = new HeaderNameMapKey(HEADER_NAME.toLowerCase())
+
+        assertThat(headerNameMapKey.hashCode(), equalTo(headerNameMapKeyLower.hashCode()))
+    }
 }
