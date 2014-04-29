4664b9408260692be61044c0083fdc4cf620702a
sethbrayman <sethbrayman@gmail.com>
2014-04-29 08:50:53 -0500

    REP-206: test of media parameter stripping

---
+++ repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/media/MediaRangeProcessorTest.groovy
@@ -0,0 +1,25 @@
+package com.rackspace.papi.commons.util.http.media
+
+import com.rackspace.papi.commons.util.http.header.HeaderValue
+import com.rackspace.papi.commons.util.http.header.HeaderValueImpl
+import spock.lang.Specification
+
+
+class MediaRangeProcessorTest extends Specification {
+
+    MediaRangeProcessor mediaRangeProcessor
+    List<? extends HeaderValue> headerValues
+
+    def "media type parameters should be stripped during processing"() {
+        given:
+        headerValues = new ArrayList<>()
+        headerValues.add(new HeaderValueImpl("application/atom+xml; type=entry;charset=utf-8", 0.1))
+        mediaRangeProcessor = new MediaRangeProcessor(headerValues);
+
+        when:
+        List<MediaType> mediaTypeList = mediaRangeProcessor.process()
+
+        then:
+        mediaTypeList.get(0).getMimeType() == MimeType.APPLICATION_ATOM_XML
+    }
+}
