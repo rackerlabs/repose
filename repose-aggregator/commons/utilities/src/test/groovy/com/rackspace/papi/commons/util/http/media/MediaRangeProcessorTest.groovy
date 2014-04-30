d959df5edbb647e3647a8a6f16d50f415c2bb1a0
sethbrayman <sethbrayman@gmail.com>
2014-04-30 10:50:34 -0500

    REP-206: test of unknown media type jic

--- repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/media/MediaRangeProcessorTest.groovy
+++ repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/media/MediaRangeProcessorTest.groovy
@@ -23,7 +23,7 @@ class MediaRangeProcessorTest extends Specification {
         mediaTypeList.get(0).getMimeType() == MimeType.APPLICATION_ATOM_XML
     }
 
-    def "media type parameters stripping should not change behavior when no parameters present"() {
+    def "media type parameter stripping should not change behavior when no parameters present"() {
         given:
         headerValues = new ArrayList<>()
         headerValues.add(new HeaderValueImpl("application/atom+xml", 0.1))
@@ -34,5 +34,18 @@ class MediaRangeProcessorTest extends Specification {
 
         then:
         mediaTypeList.get(0).getMimeType() == MimeType.APPLICATION_ATOM_XML
+    }
+
+    def "unknown media type parameters should still be a valid mime type"() {
+        given:
+        headerValues = new ArrayList<>()
+        headerValues.add(new HeaderValueImpl("thing/what", 0.1))
+        mediaRangeProcessor = new MediaRangeProcessor(headerValues);
+
+        when:
+        List<MediaType> mediaTypeList = mediaRangeProcessor.process()
+
+        then:
+        mediaTypeList.get(0).getMimeType() == MimeType.UNKNOWN
     }
 }
