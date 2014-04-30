21530783e304e253d172991b1bfe4b23e65ecd08
Adrian <adrian.george@rackspace.com>
2014-04-30 10:20:55 -0500

    Some updates to header normalization

--- repose-aggregator/components/filters/header-normalization/src/main/java/com/rackspace/papi/components/hnorm/util/HeaderNormalizer.java
+++ repose-aggregator/components/filters/header-normalization/src/main/java/com/rackspace/papi/components/hnorm/util/HeaderNormalizer.java
@@ -1,5 +1,7 @@
 package com.rackspace.papi.components.hnorm.util;
 
+import com.rackspace.papi.commons.util.http.header.HeaderNameStringWrapper;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 import java.util.HashSet;
@@ -10,16 +12,16 @@ public final class HeaderNormalizer {
     
     private HeaderNormalizer(){}
     
-    public static Set<String> getHeadersToRemove(HttpServletRequest request, CompiledRegexAndList target) {
+    public static Set<HeaderNameStringWrapper> getHeadersToRemove(HttpServletRequest request, CompiledRegexAndList target) {
 
         final Enumeration<String> headerNames = request.getHeaderNames();
-        Set<String> headersToRemove = new HashSet<String>();
-        Set<String> filterList = target.getFilterList();
+        Set<HeaderNameStringWrapper> headersToRemove = new HashSet<HeaderNameStringWrapper>();
+        Set<HeaderNameStringWrapper> filterList = target.getFilterList();
         String header;        
         
         while(headerNames.hasMoreElements()){
             header = headerNames.nextElement();
-            headersToRemove.add(header);
+            headersToRemove.add(new HeaderNameStringWrapper(header));
         }
 
         if(!target.isBlackList()){
