e4c57786b70eec0459d073bb8e64d2ae65320737
Dimitry Ushakov <dimitry.ushakov@RACKSPACE.COM>
2014-05-21 19:46:58 -0500

    clean up test and added more validations

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/IntrafilterLoggingTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/IntrafilterLoggingTest.groovy
@@ -1,7 +1,9 @@
 package features.core.powerfilter
+
 import framework.ReposeLogSearch
 import framework.ReposeValveTest
 import framework.mocks.MockIdentityService
+import groovy.json.JsonSlurper
 import org.codehaus.jettison.json.JSONObject
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
@@ -45,10 +47,11 @@ class IntrafilterLoggingTest extends ReposeValveTest{
             deproxy.shutdown()
     }
 
-    def "Request entry with client-auth and ip-identity filter"() {
-        def logSearch = new ReposeLogSearch(properties.logFile)
-        logSearch.cleanLog()
+    def setup(){
+        reposeLogSearch.cleanLog()
+    }
 
+    def "Request entry with client-auth and ip-identity filter"() {
         when: "User passes a request through repose"
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/servers/server123", method: 'GET',
                 headers: ['X-Auth-Token': fakeIdentityService.client_token])
@@ -59,40 +62,65 @@ class IntrafilterLoggingTest extends ReposeValveTest{
         then: "checking for response code and handlings"
         mc.receivedResponse.code == "200"
         mc.handlings.size() == 1
-        logSearch.searchByString("TRACE intrafilter-logging").size() == 4
-        logSearch.searchByString("Intrafilter Request Log").size() == 2
-        logSearch.searchByString("intrafilter Response Log").size() == 2
+        reposeLogSearch.searchByString("TRACE intrafilter-logging").size() == 4
+        reposeLogSearch.searchByString("Intrafilter Request Log").size() == 2
+        reposeLogSearch.searchByString("Intrafilter Response Log").size() == 2
 
         // This part of test what will be in the TRACE log to expect
-        and: "checking for client-auth - enter"
-        def firstReq = logSearch.searchByString("Intrafilter Request Log").get(0)
-        JSONObject authreqline = new JSONObject(firstReq)
-        authreqline.getString("currentFilter") == "client-auth"
-        authreqline.getString("httpMethod") == "GET"
+        and: "checking for client-auth - request"
 
-        and: "checking for client-auth - exit"
-        def firstResp = logSearch.searchByString("Intrafilter Response Log").get(0)
-        JSONObject authrespline = new JSONObject(firstResp)
-        authrespline.getString("currentFilter") == "client-auth"
-        authrespline.getString("httpMethod") == "GET"
-        def getrespheader = authrespline.getString("headers")
+        JSONObject authreqline1 = convertToJson("Intrafilter Request Log", 0)
+        authreqline1.get("currentFilter") == "client-auth"
+        authreqline1.get("httpMethod") == "GET"
+        authreqline1.get("requestURI") == "/servers/server123"
+        authreqline1.get("requestBody") == ""
+        authreqline1.get("headers").has("X-Auth-Token")
+        authreqline1.get("headers").has("Intrafilter-UUID")
+        def requestId = authreqline1.get("headers").get("Intrafilter-UUID")
 
-        and: "checking for ip-identity - enter"
-        def secondReq = logSearch.searchByString("Intrafilter Request Log").get(1)
-        JSONObject ipidentityreqline = new JSONObject(secondReq)
-        ipidentityreqline.getString("currentFilter") == "ip-identity"
-        ipidentityreqline.getString("httpMethod") == "GET"
-        ipidentityreqline.getString("headers") == getrespheader
+        and: "checking for client-auth - response"
+        //THIS IS 2ND IN THE LIST BECAUSE IT'S A STACK
+        def authrespline1 = convertToJson("Intrafilter Response Log", 1)
+        authrespline1.get("currentFilter") == "client-auth"
+        authrespline1.get("responseBody") == ""
+        authrespline1.get("httpResponseCode") == "200"
+        authrespline1.get("headers").has("Intrafilter-UUID")
+        println requestId
+        println authrespline1.get("headers").get("Intrafilter-UUID")
+        requestId == authrespline1.get("headers").get("Intrafilter-UUID")
 
-        and: "checking for ip-identity - exit"
-        def secondResp = logSearch.searchByString("Intrafilter Response Log").get(1)
-        JSONObject ipidentityrespline = new JSONObject(secondResp)
-        ipidentityrespline.getString("currentFilter") == "ip-identity"
-        ipidentityrespline.getString("httpMethod") == "GET"
+        and: "checking for ip-identity - request"
+        def authreqline2 = convertToJson("Intrafilter Request Log", 1)
+        authreqline2.get("currentFilter") == "ip-identity"
+        authreqline2.get("httpMethod") == "GET"
+        authreqline2.get("requestURI") == "/servers/server123"
+        authreqline2.get("requestBody") == ""
+        authreqline2.get("headers").has("X-Auth-Token")
+        authreqline2.get("headers").has("x-pp-user")
+        authreqline2.get("headers").has("x-pp-groups")
+        authreqline2.get("headers").has("x-tenant-name")
+        authreqline2.get("headers").has("x-user-id")
+        authreqline2.get("headers").has("x-authorization")
+        authreqline2.get("headers").has("Intrafilter-UUID")
+        println authreqline2.get("headers").get("Intrafilter-UUID")
+        requestId == authreqline2.get("headers").get("Intrafilter-UUID")
 
-        and: "additional checking for ip"
-        sentRequest.request.getHeaders().findAll("x-pp-user").size() == 2
-        user == "127.0.0.1;q=0.4" || user == "0:0:0:0:0:0:0:1;q=0.4" | user == "::1;q=0.4"
-        sentRequest.request.headers.findAll("x-pp-groups")[1].equalsIgnoreCase("IP_Standard;q=0.4")
+        and: "checking for ip-identity - response"
+        //THIS IS FIRST ON THE LIST BECAUSE IT'S A STACK
+        def authrespline2 = convertToJson("Intrafilter Response Log", 0)
+        authrespline2.get("currentFilter") == "ip-identity"
+        authrespline2.get("responseBody") == ""
+        authrespline2.get("httpResponseCode") == "200"
+        authrespline2.get("headers").has("Intrafilter-UUID")
+        println authrespline2.get("headers").get("Intrafilter-UUID")
+        requestId == authrespline2.get("headers").get("Intrafilter-UUID")
+    }
+
+    private JSONObject convertToJson(String searchString, int entryNumber ){
+        def reqString = reposeLogSearch.searchByString(searchString).get(entryNumber)
+        def requestJson = reqString.substring(reqString.indexOf("{\"preamble\""))
+        println requestJson
+
+        return new JSONObject(requestJson)
     }
 }
