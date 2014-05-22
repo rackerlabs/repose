5e4a9528be5fd567ed589d6678d026463f8a946d
Dimitry Ushakov <dimitry.ushakov@RACKSPACE.COM>
2014-05-21 20:38:20 -0500

    cleaned up tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/IntrafilterLoggingTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/IntrafilterLoggingTest.groovy
@@ -3,10 +3,11 @@ package features.core.powerfilter
 import framework.ReposeLogSearch
 import framework.ReposeValveTest
 import framework.mocks.MockIdentityService
-import groovy.json.JsonSlurper
 import org.codehaus.jettison.json.JSONObject
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
+import org.rackspace.deproxy.Response
+
 /**
  * Created by jennyvo on 5/19/14.
  * This test checking TRACE log configuration.
@@ -70,49 +71,114 @@ class IntrafilterLoggingTest extends ReposeValveTest{
         and: "checking for client-auth - request"
 
         JSONObject authreqline1 = convertToJson("Intrafilter Request Log", 0)
-        authreqline1.get("currentFilter") == "client-auth"
-        authreqline1.get("httpMethod") == "GET"
-        authreqline1.get("requestURI") == "/servers/server123"
-        authreqline1.get("requestBody") == ""
-        authreqline1.get("headers").has("X-Auth-Token")
-        authreqline1.get("headers").has("Intrafilter-UUID")
+        assertHeadersExists(["X-Auth-Token","Intrafilter-UUID"], authreqline1)
+        assertKeyValueMatch([
+                "currentFilter": "client-auth",
+                "httpMethod": "GET",
+                "requestURI": "/servers/server123",
+                "requestBody": ""
+        ], authreqline1)
         def requestId = authreqline1.get("headers").get("Intrafilter-UUID")
 
         and: "checking for client-auth - response"
         //THIS IS 2ND IN THE LIST BECAUSE IT'S A STACK
         def authrespline1 = convertToJson("Intrafilter Response Log", 1)
-        authrespline1.get("currentFilter") == "client-auth"
-        authrespline1.get("responseBody") == ""
-        authrespline1.get("httpResponseCode") == "200"
-        authrespline1.get("headers").has("Intrafilter-UUID")
-        println requestId
-        println authrespline1.get("headers").get("Intrafilter-UUID")
+        assertHeadersExists(["Intrafilter-UUID"], authrespline1)
+        assertKeyValueMatch([
+                "currentFilter": "client-auth",
+                "responseBody": "",
+                "httpResponseCode": "200"
+        ], authrespline1)
         requestId == authrespline1.get("headers").get("Intrafilter-UUID")
 
         and: "checking for ip-identity - request"
         def authreqline2 = convertToJson("Intrafilter Request Log", 1)
-        authreqline2.get("currentFilter") == "ip-identity"
-        authreqline2.get("httpMethod") == "GET"
-        authreqline2.get("requestURI") == "/servers/server123"
-        authreqline2.get("requestBody") == ""
-        authreqline2.get("headers").has("X-Auth-Token")
-        authreqline2.get("headers").has("x-pp-user")
-        authreqline2.get("headers").has("x-pp-groups")
-        authreqline2.get("headers").has("x-tenant-name")
-        authreqline2.get("headers").has("x-user-id")
-        authreqline2.get("headers").has("x-authorization")
-        authreqline2.get("headers").has("Intrafilter-UUID")
-        println authreqline2.get("headers").get("Intrafilter-UUID")
+        assertHeadersExists(["X-Auth-Token","x-pp-user", "x-pp-groups","x-tenant-name",
+                             "x-user-id", "x-authorization", "Intrafilter-UUID"], authreqline2)
+        assertKeyValueMatch([
+                "currentFilter": "ip-identity",
+                "httpMethod": "GET",
+                "requestURI": "/servers/server123",
+                "requestBody": ""
+        ], authreqline2)
         requestId == authreqline2.get("headers").get("Intrafilter-UUID")
 
         and: "checking for ip-identity - response"
         //THIS IS FIRST ON THE LIST BECAUSE IT'S A STACK
         def authrespline2 = convertToJson("Intrafilter Response Log", 0)
-        authrespline2.get("currentFilter") == "ip-identity"
-        authrespline2.get("responseBody") == ""
-        authrespline2.get("httpResponseCode") == "200"
-        authrespline2.get("headers").has("Intrafilter-UUID")
-        println authrespline2.get("headers").get("Intrafilter-UUID")
+        assertHeadersExists(["Intrafilter-UUID"], authrespline2)
+        assertKeyValueMatch([
+                "currentFilter": "ip-identity",
+                "responseBody": "",
+                "httpResponseCode": "200"
+        ], authrespline2)
+        requestId == authrespline2.get("headers").get("Intrafilter-UUID")
+    }
+
+
+    def "Request entry with client-auth and ip-identity filter with response body"() {
+        given:
+        def responseBody = "{\"response\": \"amazing\""
+        when: "User passes a request through repose"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/servers/server123", method: 'GET',
+                headers: ['X-Auth-Token': fakeIdentityService.client_token],
+                defaultHandler: { request ->
+                    return new Response(200, "OK", ["Content-Type": "application/json"], responseBody) })
+
+        def sentRequest = mc.getHandlings()[0]
+
+        then: "checking for response code and handlings"
+        mc.receivedResponse.code == "200"
+        mc.handlings.size() == 1
+        reposeLogSearch.searchByString("TRACE intrafilter-logging").size() == 4
+        reposeLogSearch.searchByString("Intrafilter Request Log").size() == 2
+        reposeLogSearch.searchByString("Intrafilter Response Log").size() == 2
+
+        // This part of test what will be in the TRACE log to expect
+        and: "checking for client-auth - request"
+
+        JSONObject authreqline1 = convertToJson("Intrafilter Request Log", 0)
+        assertHeadersExists(["X-Auth-Token","Intrafilter-UUID"], authreqline1)
+        assertKeyValueMatch([
+                "currentFilter": "client-auth",
+                "httpMethod": "GET",
+                "requestURI": "/servers/server123",
+                "requestBody": ""
+        ], authreqline1)
+        def requestId = authreqline1.get("headers").get("Intrafilter-UUID")
+
+        and: "checking for client-auth - response"
+        //THIS IS 2ND IN THE LIST BECAUSE IT'S A STACK
+        def authrespline1 = convertToJson("Intrafilter Response Log", 1)
+        assertHeadersExists(["Intrafilter-UUID","Content-Type"], authrespline1)
+        assertKeyValueMatch([
+                "currentFilter": "client-auth",
+                "responseBody": responseBody,
+                "httpResponseCode": "200"
+        ], authrespline1)
+        requestId == authrespline1.get("headers").get("Intrafilter-UUID")
+
+        and: "checking for ip-identity - request"
+        def authreqline2 = convertToJson("Intrafilter Request Log", 1)
+        assertHeadersExists(["X-Auth-Token","x-pp-user", "x-pp-groups","x-tenant-name",
+                             "x-user-id", "x-authorization", "Intrafilter-UUID"], authreqline2)
+        assertKeyValueMatch([
+                "currentFilter": "ip-identity",
+                "httpMethod": "GET",
+                "requestURI": "/servers/server123",
+                "requestBody": ""
+        ], authreqline2)
+        requestId == authreqline2.get("headers").get("Intrafilter-UUID")
+
+        and: "checking for ip-identity - response"
+        //THIS IS FIRST ON THE LIST BECAUSE IT'S A STACK
+        def authrespline2 = convertToJson("Intrafilter Response Log", 0)
+        assertHeadersExists(["Intrafilter-UUID","Content-Type"], authrespline2)
+        assertKeyValueMatch([
+                "currentFilter": "ip-identity",
+                "responseBody": responseBody,
+                "httpResponseCode": "200"
+        ], authrespline2)
         requestId == authrespline2.get("headers").get("Intrafilter-UUID")
     }
 
@@ -122,5 +188,21 @@ class IntrafilterLoggingTest extends ReposeValveTest{
         println requestJson
 
         return new JSONObject(requestJson)
+    }
+
+    private assertHeadersExists(List headers, JSONObject jsonObject){
+        headers.each {
+            headerName ->
+                assert jsonObject.get("headers").has(headerName)
+        }
+    }
+
+    private assertKeyValueMatch(Map headers, JSONObject jsonObject){
+        headers.each {
+            key, value ->
+                assert jsonObject.get(key) == value
+
+        }
+
     }
 }
