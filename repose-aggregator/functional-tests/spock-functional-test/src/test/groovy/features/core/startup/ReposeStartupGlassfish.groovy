6423f59000ef69a6b6787a3d686c6dc4efe52973
Damien Johnson <damien.johnson@rackspace.com>
2014-05-28 11:30:24 -0500

    Added test

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/startup/ReposeStartupGlassfish.groovy
@@ -0,0 +1,64 @@
+package features.core.startup
+import framework.*
+import org.rackspace.deproxy.Deproxy
+import spock.lang.Specification
+
+class ReposeStartupGlassfishTest extends Specification {
+    static def reposeGlassfishEndpoint
+
+    static int reposePort
+    static int shutdownPort
+    static String rootWar
+    static Deproxy deproxy
+    static ReposeLauncher repose
+    static ReposeLogSearch reposeLogSearch
+    static ReposeConfigurationProvider configProvider
+
+    static TestProperties properties = new TestProperties()
+
+    def setupSpec() {
+        def params = properties.getDefaultTemplateParams()
+        def configDirectory = properties.getConfigDirectory()
+        def configTemplates = properties.getRawConfigDirectory()
+        def logFile = properties.logFile
+
+        rootWar = properties.getReposeRootWar()
+        reposePort = properties.reposePort
+        shutdownPort = properties.reposeShutdownPort
+
+        configProvider = new ReposeConfigurationProvider(configDirectory, configTemplates)
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+
+        reposeGlassfishEndpoint = "http://localhost:${reposePort}"
+
+        params += ['reposePort': reposePort]
+
+        configProvider.applyConfigs("features/core/systemprops", params)
+        configProvider.applyConfigs("common", params)
+
+        reposeLogSearch = new ReposeLogSearch(logFile)
+    }
+
+    def setup() {
+        reposeLogSearch.cleanLog()
+    }
+
+    def cleanupSpec() {
+        repose?.stop()
+        deproxy?.shutdown()
+    }
+
+    def "when Repose is started without the repose-cluster-id property, a message should be logged and Repose should stop"() {
+        given:
+        repose = new ReposeContainerLauncher(configProvider, properties.getGlassfishJar(), null, "node", rootWar, reposePort, shutdownPort)
+
+        when:
+        repose.start()
+        sleep(10000)
+
+        then:
+        reposeLogSearch.searchByString("repose-cluster-id not provided -- Repose cannot start").size() == 1
+    }
+}
