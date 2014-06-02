8092ffe475e955d68d0792e580b8a567c0c07cbd
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-06-02 16:15:32 -0500

    Increased unit test coverage for HeaderValueParser for REP-540

--- repose-aggregator/commons/utilities/src/test/java/com/rackspace/papi/commons/util/http/header/HeaderValueParserTest.java
+++ repose-aggregator/commons/utilities/src/test/java/com/rackspace/papi/commons/util/http/header/HeaderValueParserTest.java
@@ -36,5 +36,44 @@ public class HeaderValueParserTest {
          assertEquals("Should parse parameter 'b' correctly", headerValue.getParameters().get("b"), "banana");
          assertEquals("Should parse parameter 'c' correctly", headerValue.getParameters().get("c"), "clementine");
       }
+
+      @Test
+      public void shouldAppendSemicolonIfNoEquals() {
+          final String headerValueString = "the; value";
+          final HeaderValue headerValue = new HeaderValueParser(headerValueString).parse();
+
+          assertEquals("Should parse the acutal header value correctly", "the; value", headerValue.getValue());
+          assertTrue("Parameter map should be empty", headerValue.getParameters().isEmpty());
+      }
+
+      @Test
+      public void concatBecauseOfEqualInValue() {
+          final String headerValueString = "the value; q=0.5; a=apple ; b= banana; c = clementine; z = lemon=lime";
+          final HeaderValue headerValue = new HeaderValueParser(headerValueString).parse();
+
+          assertEquals("Should parse the acutal header value correctly", "the value", headerValue.getValue());
+          assertEquals("Should parse quality factor as a double", Double.valueOf(0.5), Double.valueOf(headerValue.getQualityFactor()));
+          assertEquals("Should parse parameter 'a' correctly", headerValue.getParameters().get("a"), "apple");
+          assertEquals("Should parse parameter 'b' correctly", headerValue.getParameters().get("b"), "banana");
+          assertEquals("Should parse parameter 'c' correctly", headerValue.getParameters().get("c"), "clementine");
+          assertEquals("Should parse parameter 'z' correctly", headerValue.getParameters().get("z"), "lemon=lime");
+
+
+      }
+
+       @Test
+       public void throwMalformedHeaderValExceptionIfNeeded() {
+
+           final String headerValueString = ";=";
+           try {
+               final HeaderValue headerValue = new HeaderValueParser(headerValueString).parse();
+               assertTrue(false);
+           } catch (MalformedHeaderValueException e) {
+               assertEquals("Valid parameter expected for header. Got: =" , e.getMessage());
+               assertTrue(true);
+           }
+
+
+       }
    }
 }
