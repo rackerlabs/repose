f424b319c8046ab2edc2e7ec10a4a5a03e2b19e6
Damien johnson <damien.johnson@rackspace.com>
2014-06-16 15:02:53 -0500

    Totally legit unit test

---
+++ repose-aggregator/core/valve/src/test/groovy/com/rackspace/cloud/valve/server/PowerApiValveServerControlTest.groovy
@@ -0,0 +1,46 @@
+package com.rackspace.cloud.valve.server
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import static org.junit.Assert.assertTrue
+
+public class PowerApiValveServerControlTest {
+    PowerApiValveServerControl powerApiValveServerControl
+    CommandLineArguments commandLineArguments
+
+    @Before
+    public void setUp() throws Exception {
+        AppenderForTesting.clear()
+        commandLineArguments = new CommandLineArguments()
+        commandLineArguments.setConfigDirectory("/")
+        commandLineArguments.setHttpPort(9090)
+        powerApiValveServerControl = new PowerApiValveServerControl(
+                commandLineArguments.getHttpPort(),
+                commandLineArguments.getHttpsPort(),
+                commandLineArguments.getConfigDirectory(),
+                commandLineArguments.getInsecure())
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        AppenderForTesting.clear()
+    }
+
+    @Test
+    public void shouldStartWithValidCLA() throws Exception {
+        powerApiValveServerControl.startPowerApiValve()
+
+        assertTrue(powerApiValveServerControl.serverInstance.isStarted())
+    }
+
+    @Test
+    public void shouldStopAfterStartingAndRunShutdownHook() throws Exception {
+        powerApiValveServerControl.startPowerApiValve()
+        powerApiValveServerControl.stopPowerApiValve()
+
+        assertTrue(powerApiValveServerControl.serverInstance.isStopped())
+        assertTrue(AppenderForTesting.getMessages()[915].startsWith("Repose has been stopped"))
+    }
+}
