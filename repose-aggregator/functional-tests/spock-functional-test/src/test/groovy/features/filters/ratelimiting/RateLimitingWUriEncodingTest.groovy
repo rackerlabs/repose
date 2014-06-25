1fe2c1626cbd4be334e57e7283c02b3271f96c45
jenny.vo <jenny.vo@rackspace.com>
2014-06-25 15:38:47 -0500

    add test for ratelimitting encode url

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/ratelimiting/RateLimitingWUriEncodingTest.groovy
@@ -0,0 +1,105 @@
+package features.filters.ratelimiting
+
+import framework.ReposeConfigurationProvider
+import framework.ReposeValveLauncher
+import framework.TestProperties
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.PortFinder
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Created by jennyvo on 6/25/14.
+ */
+class RateLimitingWUriEncodingTest extends Specification{
+
+    static Deproxy deproxy
+
+    static TestProperties properties
+    static ReposeConfigurationProvider reposeConfigProvider
+    static ReposeValveLauncher repose
+
+    def setupSpec() {
+
+        properties = new TestProperties()
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+
+        properties.reposePort = PortFinder.Singleton.getNextOpenPort()
+
+        reposeConfigProvider = new ReposeConfigurationProvider(properties.getConfigDirectory(), properties.getConfigTemplates())
+
+        def params = properties.getDefaultTemplateParams()
+        reposeConfigProvider.cleanConfigDirectory()
+        reposeConfigProvider.applyConfigs("common", params)
+        reposeConfigProvider.applyConfigs("features/filters/ratelimiting/uriencoding", params)
+        repose = new ReposeValveLauncher(
+                reposeConfigProvider,
+                properties.getReposeJar(),
+                properties.reposeEndpoint,
+                properties.getConfigDirectory(),
+                properties.reposePort,
+                properties.reposeShutdownPort
+        )
+        repose.enableDebug()
+        repose.start(killOthersBeforeStarting: false,
+                waitOnJmxAfterStarting: false)
+        repose.waitForNon500FromUrl(properties.reposeEndpoint)
+    }
+
+    @Unroll("Request with uri #uri should be applied the same limit group #group")
+    def "Requests with encoded url should be decode before applying RL"() {
+
+        given:
+
+        def mc
+        def headers = ['X-PP-User': 'user1', 'X-PP-Groups': group]
+
+
+        when: "we make one request to the url"
+        mc = deproxy.makeRequest(url: "${properties.reposeEndpoint}" + uri, headers: headers)
+        then: "it should rate limit and get correct respcode"
+        mc.receivedResponse.code == respcode
+        if(respcode == "200")
+            mc.handlings.size() == 1
+
+        where:
+        uri                                    | respcode  |group
+        "/servers/abc/instances/123"           | "200"     |"test1"
+        "/servers/abc/instances/%31%32%33"     | "200"     |"test1"
+        "/servers/abc/instances%2F123"         | "200"     |"test1"
+        "/servers/abc/instances/123"           | "413"     |"test1"
+        "/objects/jkl/things/123"              | "200"     |"test2"
+        "/objects/%6a%6b%6c/things/%31%32%33"  | "200"     |"test2"
+        "/objects/%6A%6B%6C/things/%31%32%33"  | "200"     |"test2"
+        "/objects/jkl/things/123"              | "413"     |"test2"
+    }
+
+    @Unroll("Request with uri #uri and method #method should be applied the same limit group #group")
+    def "Requests with encoded url should be decode before applying RL uri and method"() {
+
+        given:
+
+        def mc
+        def headers = ['X-PP-User': 'user1', 'X-PP-Groups': group]
+
+
+        when: "we make one request to the url"
+        mc = deproxy.makeRequest(url: "${properties.reposeEndpoint}" + uri, method: method, headers: headers)
+        then: "it should rate limit and get correct respcode"
+        mc.receivedResponse.code == respcode
+        if(respcode == "200")
+            mc.handlings.size() == 1
+
+        where:
+        uri                                 | respcode  | method     |group
+        "/method/jkl/test/123"              | "200"     |"GET"       |"test3"
+        "/method/%6a%6b%6c/test/%31%32%33"  | "200"     |"GET"       |"test3"
+        "/method/%6A%6B%6C/test/%31%32%33"  | "200"     |"GET"       |"test3"
+        "/method/jkl/test/123"              | "200"     |"PATCH"     |"test3"
+        "/method/%6a%6b%6c/test/%31%32%33"  | "413"     |"GET"       |"test3"
+        "/method/%6A%6B%6C/test/123"        | "200"     |"PATCH"     |"test3"
+    }
+}
+
