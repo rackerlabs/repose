88e59474b05fc5c875e87d4acb4a19042edd3b04
Damien Johnson <damien.johnson@rackspace.com>
2014-07-01 14:55:27 -0500

    Removed timebomb since we are one java 1.7 now!

--- repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/logging/apache/format/converters/DateTimeFormatConverterTest.groovy
+++ repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/logging/apache/format/converters/DateTimeFormatConverterTest.groovy
@@ -39,24 +39,8 @@ class DateTimeFormatConverterTest {
         assert result.equals(testValue)
     }
 
-    /*
-     * The format we're using is incorrect and doesn't do what it says it does,
-     * the following two tests prove it.
-     *
-     * Unfortunately the SimpleDateFormat class in Java 1.6 is broken
-     * The splodeDate is set to July of 2014, which is about when we expect to be switching to JDK 1.7
-     * At this point the tests will execute, and probably fail unless we've switched to JDK 1.7, at which point they
-     * should succeed!
-     *
-     * As long as the assumption proves false, it will ignore the tests. So we're assuming that we're past the timebomb
-     * date, otherwise the test method is ignored
-     */
-    def splodeDate = new Date(2014 - 1900, Calendar.JULY, 1, 9, 0);
-
     @Test
     void "valid input gets converted correctly"() {
-        Assume.assumeTrue(new Date() > splodeDate)
-
         def testValue = "1994-11-05T13:15:30Z"
         String result = converter.convert(testValue, 'ISO_8601', 'RFC_1123')
         assert result.equals('Sat, 05 Nov 1994 13:15:30 GMT')
@@ -64,8 +48,6 @@ class DateTimeFormatConverterTest {
 
     @Test
     void "valid input value with a bad output format defaults to rfc-1123"() {
-        Assume.assumeTrue(new Date() > splodeDate)
-
         def testValue = "1994-11-05T13:15:30Z"
         String result = converter.convert(testValue, 'ISO_8601', 'squirrel noises')
         assert (result.equals('Sat, 05 Nov 1994 13:15:30 GMT'))
