123f0ff5e0eae44a956f53d91965e5cf4134dcd8
David Kowis <david.kowis@rackspace.com>
2014-07-07 16:16:56 -0500

    Cover those failure paths real quick

--- repose-aggregator/services/health-check/impl/src/test/groovy/com/rackspace/papi/service/healthcheck/HealthCheckServiceHelperTest.groovy
+++ repose-aggregator/services/health-check/impl/src/test/groovy/com/rackspace/papi/service/healthcheck/HealthCheckServiceHelperTest.groovy
@@ -22,7 +22,7 @@ class HealthCheckServiceHelperTest extends Specification {
     @Shared
     private Logger lgr
 
-    void setupSpec() {
+    void setup() {
         hcs = mock(HealthCheckService.class)
         lgr = mock(Logger.class)
 
@@ -72,4 +72,39 @@ class HealthCheckServiceHelperTest extends Specification {
     }
 
     //todo: add non-happy path testing
+
+    @Unroll("When a #clazz is thrown reporting an issue, it is swallowed")
+    def "Testing thrown exceptions when reporting"() {
+        given:
+        when(hcs.reportIssue(anyString(), anyString(), any(HealthCheckReport.class))).thenThrow(exception)
+
+        when:
+        healthCheckServiceHelper.reportIssue(TEST_RID, "The time for chilling is past!", Severity.BROKEN)
+
+        then:
+        notThrown(clazz)
+
+        where:
+        exception                             | clazz
+        new InputNullException("OH NOES")     | InputNullException
+        new NotRegisteredException("OH NOES") | NotRegisteredException
+    }
+
+    @Unroll("When a #clazz is thrown resolving an issue, it is swallowed")
+    def "Testing thrown exceptions when resolving"() {
+        given:
+        when(hcs.solveIssue(anyString(), anyString())).thenThrow(exception)
+
+        when:
+        healthCheckServiceHelper.resolveIssue(TEST_RID)
+
+        then:
+        notThrown(clazz)
+
+        where:
+        exception                             | clazz
+        new InputNullException("OH NOES")     | InputNullException
+        new NotRegisteredException("OH NOES") | NotRegisteredException
+    }
+
 }
