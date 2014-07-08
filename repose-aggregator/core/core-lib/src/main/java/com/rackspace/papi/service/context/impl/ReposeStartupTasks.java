a570b11420a2abeea553e532e7c6e0e1e7406775
David Kowis <david.kowis@rackspace.com>
2014-07-08 17:19:50 -0500

    Adding a placeholder for things that need to happen during startup

---
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/context/impl/ReposeStartupTasks.java
@@ -0,0 +1,39 @@
+package com.rackspace.papi.service.context.impl;
+
+import com.rackspace.papi.commons.util.StringUtilities;
+import com.rackspace.papi.commons.util.http.HttpsURLConnectionSslInitializer;
+import com.rackspace.papi.servlet.InitParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
+/**
+ * TODO: I'm worried this could be a race condition
+ * These things happened in the PAPIContextManager, and need to happen early on, but I'm not sure before what
+ * things. They're fired once, and then that's persistent throughout the entire JVM...
+ */
+@Component
+public class ReposeStartupTasks implements ServletContextAware {
+    private ServletContext servletContext;
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        //Determine if stuff has been set insecurely.
+        final String insecureProp = InitParameter.INSECURE.getParameterName();
+        final String insecure = System.getProperty(insecureProp, servletContext.getInitParameter(insecureProp));
+
+        if (StringUtilities.nullSafeEqualsIgnoreCase(insecure, "true")) {
+            new HttpsURLConnectionSslInitializer().allowAllServerCerts();
+        }
+
+        //Allows Repose to set any header to pass to the origin service. Namely the "Via" header
+        System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+    }
+}
