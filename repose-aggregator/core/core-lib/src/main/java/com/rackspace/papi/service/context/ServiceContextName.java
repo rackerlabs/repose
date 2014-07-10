6609bb922253f70100e0d74714a17eb4e68b78e5
Damien Johnson <damien.johnson@rackspace.com>
2014-07-10 13:55:22 -0500

    Removed deleted service contexts from services enum

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/context/ServiceContextName.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/context/ServiceContextName.java
@@ -2,27 +2,16 @@ package com.rackspace.papi.service.context;
 
 public enum ServiceContextName {
 
-    CLASS_LOADER_SERVICE_CONTEXT("classLoaderServiceContext"),
     CONFIGURATION_SERVICE_CONTEXT("configurationServiceContext"),
     CONTAINER_SERVICE_CONTEXT("containerServiceContext"),
-    DATASTORE_SERVICE_CONTEXT("datastoreServiceContext"),
-    EVENT_MANAGER_SERVICE_CONTEXT("eventManagerServiceContext"),
-    FILTER_CHAIN_GC_SERVICE_CONTEXT("filterChainGCServiceContext"),
-    LOGGING_SERVICE_CONTEXT("loggingServiceContext"),
     METRICS_SERVICE_CONTEXT( "metricsServiceContext" ),
-    RESPONSE_MESSAGE_SERVICE_CONTEXT("responseMessageServiceContext"),
-    ROUTING_SERVICE_CONTEXT("routingServiceContext"),
-    THREADING_SERVICE_CONTEXT("threadingServiceContext"),
     HTTP_CONNECTION_POOL_SERVICE_CONTEXT("httpConnectionPoolServiceContext"),
     AKKA_SERVICE_CLIENT_SERVICE_CONTEXT("akkaServiceClientContext"),
     REQUEST_PROXY_SERVICE_CONTEXT("requestProxyServiceContext"),
-    REPORTING_SERVICE_CONTEXT("reportingServiceContext"),
-    REQUEST_HEADER_SERVICE_CONTEXT("requestHeaderServiceContext"),
     RESPONSE_HEADER_SERVICE_CONTEXT("responseHeaderServiceContext"),
     POWER_FILTER_CHAIN_BUILDER("powerFilterChainBuilder"),
     REPOSE_CONFIGURATION_INFORMATION("reposeConfigurationInformation"),
     DISTRIBUTED_DATASTORE_SERVICE_CONTEXT("distributedDatastoreServiceContext"),
-    DISTRIBUTED_DATASTORE_SERVICE_CLUSTER_CONTEXT("distributedDatastoreServiceClusterContext"),
     HEALTH_CHECK_SERVICE_CONTEXT("healthCheckServiceContext");
     private String serviceContextName;
 
