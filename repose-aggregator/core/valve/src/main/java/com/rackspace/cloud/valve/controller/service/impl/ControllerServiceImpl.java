e8fef419575b80c9a85aa19384cced6b787c1b1e
Damien Johnson <damien.johnson@rackspace.com>
2014-07-10 22:51:11 -0500

    Reformatted code

--- repose-aggregator/core/valve/src/main/java/com/rackspace/cloud/valve/controller/service/impl/ControllerServiceImpl.java
+++ repose-aggregator/core/valve/src/main/java/com/rackspace/cloud/valve/controller/service/impl/ControllerServiceImpl.java
@@ -27,161 +27,154 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component("controllerService")
 public class ControllerServiceImpl implements ControllerService {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ControllerServiceImpl.class);
 
-   //TODO: Find a better way than using a ConcurrentHashMap for this.
-   private Map<String, Server> managedServers = new ConcurrentHashMap<String, Server>();
-   private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ControllerServiceImpl.class);
+    //TODO: Find a better way than using a ConcurrentHashMap for this.
+    private Map<String, Server> managedServers = new ConcurrentHashMap<>();
     private static final String REPOSE_NODE = "Repose node ";
-   private String configDir;
-   private boolean isInsecure;
+    private String configDir;
+    private boolean isInsecure;
 
-   @Override
-   public Set<String> getManagedInstances() {
-      return managedServers.keySet();
+    @Override
+    public Set<String> getManagedInstances() {
+        return managedServers.keySet();
 
-   }
+    }
 
-   @Override
-   public synchronized void updateManagedInstances(Map<String, ExtractorResult<Node>> updatedInstances, Set<String> nodesToStop) {
+    @Override
+    public synchronized void updateManagedInstances(Map<String, ExtractorResult<Node>> updatedInstances, Set<String> nodesToStop) {
+        if (!nodesToStop.isEmpty()) {
+            stopServers(nodesToStop);
+        }
+        if (updatedInstances != null && !updatedInstances.isEmpty()) {
+            startValveServers(updatedInstances);
+        }
+    }
 
-      if (!nodesToStop.isEmpty()) {
-         stopServers(nodesToStop);
-      }
-      if (updatedInstances != null && !updatedInstances.isEmpty()) {
-         startValveServers(updatedInstances);
-      }
-   }
+    private void startValveServers(Map<String, ExtractorResult<Node>> updatedInstances) {
+        final Set<Entry<String, ExtractorResult<Node>>> entrySet = updatedInstances.entrySet();
 
-   private void startValveServers(Map<String, ExtractorResult<Node>> updatedInstances) {
+        for (Entry<String, ExtractorResult<Node>> entry : entrySet) {
+            Node curNode = entry.getValue().getKey();
 
-      final Set<Entry<String, ExtractorResult<Node>>> entrySet = updatedInstances.entrySet();
+            List<Port> ports = getNodePorts(curNode);
 
-      for (Entry<String, ExtractorResult<Node>> entry : entrySet) {
-
-         Node curNode = entry.getValue().getKey();
-
-         List<Port> ports = getNodePorts(curNode);
-
-
-         Server serverInstance = new ValveJettyServerBuilder(configDir, ports, validateSsl(curNode), isInsecure, entry.getValue().getResult(), curNode.getId()).newServer();
-         try {
-            serverInstance.start();
-            serverInstance.setStopAtShutdown(true);
-         } catch (Exception e) {
-            LOG.error("Repose Node with Id " + curNode.getId() + " could not be started: " + e.getMessage(), e);
-            if (serverInstance != null) {
-               try {
-                  serverInstance.stop();
-               } catch (Exception ex) {
-                  LOG.error("Error stopping server", ex);
-               }
+            Server serverInstance = new ValveJettyServerBuilder(configDir, ports, validateSsl(curNode), isInsecure, entry.getValue().getResult(), curNode.getId()).newServer();
+            try {
+                serverInstance.start();
+                serverInstance.setStopAtShutdown(true);
+            } catch (Exception e) {
+                LOG.error("Repose Node with Id " + curNode.getId() + " could not be started: " + e.getMessage(), e);
+                if (serverInstance != null) {
+                    try {
+                        serverInstance.stop();
+                    } catch (Exception ex) {
+                        LOG.error("Error stopping server", ex);
+                    }
+                }
             }
-         }
-         logReposeLaunch(ports);
-         managedServers.put(entry.getKey(), serverInstance);
-      }
-   }
 
-   private void stopServers(Set<String> nodesToStop) {
+            logReposeLaunch(ports);
+            managedServers.put(entry.getKey(), serverInstance);
+        }
+    }
 
-      for (String key : nodesToStop) {
-         Server serverInstance = managedServers.get(key);
-         try {
-            serverInstance.stop();
-            managedServers.remove(key);
-         } catch (Exception e) {
-            LOG.error("Unable to shutdown server: " + key + ": " + e.getMessage(), e);
-         }
-      }
-   }
+    private void stopServers(Set<String> nodesToStop) {
+        for (String key : nodesToStop) {
+            Server serverInstance = managedServers.get(key);
 
-   @Override
-   public Boolean reposeInstancesInitialized() {
-      return managedServers.isEmpty();
-   }
+            try {
+                serverInstance.stop();
+                managedServers.remove(key);
+            } catch (Exception e) {
+                LOG.error("Unable to shutdown server: " + key + ": " + e.getMessage(), e);
+            }
+        }
+    }
 
-   @Override
-   public void setConfigDirectory(String directory) {
-      this.configDir = directory;
-   }
+    @Override
+    public Boolean reposeInstancesInitialized() {
+        return managedServers.isEmpty();
+    }
 
-   @Override
-   public String getConfigDirectory() {
-      return this.configDir;
-   }
+    @Override
+    public void setConfigDirectory(String directory) {
+        this.configDir = directory;
+    }
 
-   private List<Port> getNodePorts(Node node) {
+    @Override
+    public String getConfigDirectory() {
+        return this.configDir;
+    }
 
-      List<Port> ports = new LinkedList<Port>();
+    private List<Port> getNodePorts(Node node) {
+        List<Port> ports = new LinkedList<>();
 
-      if (node.getHttpPort() != 0) {
-         ports.add(new Port("Http", node.getHttpPort()));
-      }
-      if (node.getHttpsPort() != 0) {
-         ports.add(new Port("Https", node.getHttpsPort()));
-      }
+        if (node.getHttpPort() != 0) {
+            ports.add(new Port("Http", node.getHttpPort()));
+        }
+        if (node.getHttpsPort() != 0) {
+            ports.add(new Port("Https", node.getHttpsPort()));
+        }
 
-      return ports;
-   }
+        return ports;
+    }
 
-   @Override
-   public void setIsInsecure(boolean isInsecure) {
-      this.isInsecure = isInsecure;
-   }
+    @Override
+    public void setIsInsecure(boolean isInsecure) {
+        this.isInsecure = isInsecure;
+    }
 
-   @Override
-   public Boolean isInsecure() {
-      return isInsecure;
-   }
+    @Override
+    public Boolean isInsecure() {
+        return isInsecure;
+    }
 
-   private SslConfiguration validateSsl(Node node) {
-      SslConfiguration sslConfiguration = null;
+    private SslConfiguration validateSsl(Node node) {
+        SslConfiguration sslConfiguration = null;
 
-      if (node.getHttpsPort() != 0) {
+        if (node.getHttpsPort() != 0) {
+            try {
+                sslConfiguration = readSslConfiguration(configDir);
+            } catch (MalformedURLException e) {
+                LOG.error("Unable to build path to SSL configuration: " + e.getMessage(), e);
+            }
 
-         try{
-         sslConfiguration = readSslConfiguration(configDir);
-         }catch(MalformedURLException e){
-            LOG.error("Unable to build path to SSL configuration: " + e.getMessage(), e);
-         }
+            if (sslConfiguration == null) {
+                throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl configuration is not in container.cfg.xml.");
+            }
 
-         if (sslConfiguration == null) {
-            throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl configuration is not in container.cfg.xml.");
-         }
+            if (sslConfiguration.getKeystoreFilename() == null) {
+                throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl keystore filename is not in container.cfg.xml.");
+            }
 
-         if (sslConfiguration.getKeystoreFilename() == null) {
-            throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl keystore filename is not in container.cfg.xml.");
-         }
+            if (sslConfiguration.getKeystorePassword() == null) {
+                throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl keystore password is not in container.cfg.xml.");
+            }
 
-         if (sslConfiguration.getKeystorePassword() == null) {
-            throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl keystore password is not in container.cfg.xml.");
-         }
+            if (sslConfiguration.getKeyPassword() == null) {
+                throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl key password is not in container.cfg.xml.");
+            }
+        }
 
-         if (sslConfiguration.getKeyPassword() == null) {
-            throw new ConfigurationResourceException(REPOSE_NODE + node.getId() + " is configured to run on https but the ssl key password is not in container.cfg.xml.");
-         }
-      }
+        return sslConfiguration;
+    }
 
-      return sslConfiguration;
-   }
+    private SslConfiguration readSslConfiguration(String cfgRoot) throws MalformedURLException {
+        final URL configurationLocation = new URL("file://" + cfgRoot + File.separator + "container.cfg.xml");
+        final JaxbConfigurationParser<ContainerConfiguration> containerConfigParser = ConfigurationParserFactory.getXmlConfigurationParser(ContainerConfiguration.class, null);
+        final ContainerConfiguration cfg = containerConfigParser.read(new BufferedURLConfigurationResource(configurationLocation));
 
-   private SslConfiguration readSslConfiguration(String cfgRoot) throws MalformedURLException {
-      final URL configurationLocation = new URL("file://" + cfgRoot + File.separator + "container.cfg.xml");
-      final JaxbConfigurationParser<ContainerConfiguration> containerConfigParser = ConfigurationParserFactory.getXmlConfigurationParser(ContainerConfiguration.class, null);
-      final ContainerConfiguration cfg = containerConfigParser.read(new BufferedURLConfigurationResource(configurationLocation));
+        if (cfg != null && cfg.getDeploymentConfig() != null) {
+            return cfg.getDeploymentConfig().getSslConfiguration();
+        }
 
-      if (cfg != null && cfg.getDeploymentConfig() != null) {
-         return cfg.getDeploymentConfig().getSslConfiguration();
-      }
+        throw new ConfigurationResourceException("Container configuration is not valid. Please check your configuration.");
+    }
 
-      throw new ConfigurationResourceException("Container configuration is not valid. Please check your configuration.");
-   }
-   
-   private void logReposeLaunch(List<Port> ports){
-      
-      for(Port port: ports){
-         LOG.info("Repose node listening on " + port.getProtocol() + " on port " + port.getPort());
-      }
-      
-   }
+    private void logReposeLaunch(List<Port> ports) {
+        for (Port port : ports) {
+            LOG.info("Repose node listening on " + port.getProtocol() + " on port " + port.getPort());
+        }
+    }
 }
