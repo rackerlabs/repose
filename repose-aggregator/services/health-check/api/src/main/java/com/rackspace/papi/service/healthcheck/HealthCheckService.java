fb613253a7461bfb39d74e1c34cb72e686b61673
Damien Johnson <damien.johnson@rackspace.com>
2014-07-11 13:22:40 -0500

    Refactored HealthCheckService to not throw exceptions and use a proxy API

--- repose-aggregator/services/health-check/api/src/main/java/com/rackspace/papi/service/healthcheck/HealthCheckService.java
+++ repose-aggregator/services/health-check/api/src/main/java/com/rackspace/papi/service/healthcheck/HealthCheckService.java
@@ -7,61 +7,78 @@ public interface HealthCheckService {
 
     /**
      * Reports whether there is an issue with the current Repose Deployment
-     * @return
+     *
+     * @return boolean indicating whether or not there is an issue with the current Repose deployment
      */
     boolean isHealthy();
 
     /**
      * Retrieves message associated with the latest issue
-     * @param UID Id of class/user which reported the issue
-     * @param id The id of issue
-     * @return HealthReport containing details of issue reported
-     * @throws NotRegisteredException If the UID is not registered
-     * @throws InputNullException If passed null for UID or id
+     *
+     * @param uid Id of class/user which reported the issue. If null, behavior undefined.
+     * @param id  The id of issue. If null, behavior undefined.
+     * @return HealthCheckReport containing details of issue reported
      */
-    HealthCheckReport getDiagnosis(String UID, String id) throws NotRegisteredException, InputNullException;
+    HealthCheckReport getDiagnosis(String uid, String id);
 
     /**
      * Reports an issue to the health service
-     * @param UID Id of class/user which reported the issue
-     * @param RID The id of issue
+     *
+     * @param uid    Id of class/user which reported the issue. If null, behavior undefined.
+     * @param rid    The id of issue. If null, behavior undefined.
      * @param report The HealthCheckReport which contains the details of the report
-     * @throws NotRegisteredException If the UID is not registered
-     * @throws InputNullException If passed null for UID or id
      */
-    void reportIssue(String UID, String RID, HealthCheckReport report) throws NotRegisteredException, InputNullException;
+    void reportIssue(String uid, String rid, HealthCheckReport report);
 
     /**
      * Retrieves the ids of all reported problems for a given UID
-     * @param UID Id of class/user which reported the issue
-     * @return
-     * @throws NotRegisteredException If the UID is not registered
-     * @throws InputNullException If passed null for UID or id
+     *
+     * @param uid Id of class/user which reported the issue. If null, behavior undefined.
+     * @return IDs of all reports corresponding to the UID provided
      */
-     Set<String> getReportIds(String UID) throws NotRegisteredException, InputNullException;
+    Set<String> getReportIds(String uid);
 
     /**
      * Tells the Health Check Service that the issue has been resolved.
-     * @param UID Id of class/user which reported the issue
-     * @param id The id of issue
-     * @throws NotRegisteredException If the UID is not registered
-     * @throws InputNullException If passed null for UID or id
+     *
+     * @param uid Id of class/user which reported the issue. If null, behavior undefined.
+     * @param id  The id of issue. If null, behavior undefined.
      */
-    void solveIssue(String UID, String id) throws NotRegisteredException, InputNullException;
+    void solveIssue(String uid, String id);
 
     /**
      * Register to Health Check Service. This will return a unique ID which the caller will associate with issues
+     *
      * @param T Class registering with the Health Check Service
      * @return UID with which to report issues
      */
-    String register(Class T);
+    HealthCheckServiceProxy register(Class T);
 
     /**
      * Retrieves map of health reports associated with the given UID
-     * @param UID Id of class/user which reported the issue
-     * @return
-     * @throws NotRegisteredException If the UID is not registered
-     * @throws InputNullException If passed null for UID or id
+     *
+     * @param uid Id of class/user which reported the issue. If null, behavior undefined.
+     * @return Map of all reports corresponding to the UID provided
      */
-    Map<String, HealthCheckReport> getReports(String UID) throws NotRegisteredException, InputNullException;
+    Map<String, HealthCheckReport> getReports(String uid);
+
+    /**
+     * An API proxy returned when registering with the HealthCheckService.
+     *
+     * This proxy stores a UID to uniquely identify the caller of the register method and provides a simplified API
+     * by eliminating the need to provide a UID on each request.
+     */
+    interface HealthCheckServiceProxy {
+        boolean isHealthy();
+
+        HealthCheckReport getDiagnosis(String id);
+
+        void reportIssue(String rid, HealthCheckReport report);
+
+        Set<String> getReportIds();
+
+        void solveIssue(String id);
+
+        Map<String, HealthCheckReport> getReports();
+    }
 }
--- repose-aggregator/services/health-check/api/src/main/java/com/rackspace/papi/service/healthcheck/NotRegisteredException.java
+++
@@ -1,9 +0,0 @@
-package com.rackspace.papi.service.healthcheck;
-
-public class NotRegisteredException extends Exception {
-
-    public NotRegisteredException(String UID) {
-        super("ID with " + UID + " not registered with the Health Check Service");
-    }
-
-}
