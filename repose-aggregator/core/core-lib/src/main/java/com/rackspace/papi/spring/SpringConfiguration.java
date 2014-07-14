06c05e13268db7091b5385903d136c8baa1ee92a
David Kowis <david.kowis@rackspace.com>
2014-07-14 17:54:03 -0500

    setting the location for the context

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/spring/SpringConfiguration.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/spring/SpringConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource({"classpath:/application-context.xml"})
+@ImportResource({"classpath:applicationContext.xml"})
 public class SpringConfiguration {
    
 }
