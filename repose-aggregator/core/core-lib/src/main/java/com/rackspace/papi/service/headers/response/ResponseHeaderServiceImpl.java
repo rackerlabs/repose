3047a245daa316a3030eb41e54b28a45c8980b78
sethbrayman <sethbrayman@gmail.com>
2014-07-15 18:06:26 -0500

    Spring refactor. Versions info needs to be obtained in some other manner.

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/headers/response/ResponseHeaderServiceImpl.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/headers/response/ResponseHeaderServiceImpl.java
@@ -1,25 +1,55 @@
 package com.rackspace.papi.service.headers.response;
 
+import com.rackspace.papi.commons.config.manager.UpdateListener;
 import com.rackspace.papi.commons.util.StringUtilities;
 import com.rackspace.papi.commons.util.http.CommonHttpHeader;
 import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletRequest;
 import com.rackspace.papi.commons.util.servlet.http.MutableHttpServletResponse;
 import com.rackspace.papi.commons.util.servlet.http.RouteDestination;
+import com.rackspace.papi.container.config.ContainerConfiguration;
+import com.rackspace.papi.service.config.ConfigurationService;
+import com.rackspace.papi.service.context.ServletContextHelper;
 import com.rackspace.papi.service.headers.common.ViaHeaderBuilder;
-import java.net.MalformedURLException;
-import javax.servlet.http.HttpServletRequest;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
 
 @Named("responseHeaderService")
 public class ResponseHeaderServiceImpl implements ResponseHeaderService {
 
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ResponseHeaderServiceImpl.class);
+    private final ConfigurationService configurationService;
+    private final ContainerConfigurationListener configurationListener;
     private ViaHeaderBuilder viaHeaderBuilder;
     private LocationHeaderBuilder locationHeaderBuilder;
+    private String reposeVersion = "";
+
+    @Inject
+    public ResponseHeaderServiceImpl(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+        this.configurationListener = new ContainerConfigurationListener();
+    }
+
+    @PreDestroy
+    public void destroy() {
+        configurationService.unsubscribeFrom("container.cfg.xml", configurationListener);
+    }
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        reposeVersion = ServletContextHelper.getInstance(servletContextEvent.getServletContext()).getPowerApiContext()
+                                            .getReposeVersion(); //need some other way to get the version!!!
+        configurationService.subscribeTo("container.cfg.xml", configurationListener, ContainerConfiguration.class);
+    }
 
     @Override
-    public synchronized void updateConfig(ViaHeaderBuilder viaHeaderBuilder, LocationHeaderBuilder locationHeaderBuilder) {
+    public synchronized void updateConfig(ViaHeaderBuilder viaHeaderBuilder,
+                                          LocationHeaderBuilder locationHeaderBuilder) {
         this.viaHeaderBuilder = viaHeaderBuilder;
         this.locationHeaderBuilder = locationHeaderBuilder;
     }
@@ -28,20 +58,26 @@ public class ResponseHeaderServiceImpl implements ResponseHeaderService {
     public void setVia(MutableHttpServletRequest request, MutableHttpServletResponse response) {
         final String existingVia = response.getHeader(CommonHttpHeader.VIA.toString());
         final String myVia = viaHeaderBuilder.buildVia(request);
-        final String via = StringUtilities.isBlank(existingVia) ? myVia : existingVia + ", " + myVia ;
+        final String via = StringUtilities.isBlank(existingVia) ? myVia : existingVia + ", " + myVia;
 
         response.setHeader(CommonHttpHeader.VIA.name(), via);
     }
 
     @Override
-    public void fixLocationHeader(HttpServletRequest originalRequest, MutableHttpServletResponse response, RouteDestination destination, String destinationLocationUri, String proxiedRootContext) {
+    public void fixLocationHeader(HttpServletRequest originalRequest, MutableHttpServletResponse response,
+                                  RouteDestination destination, String destinationLocationUri,
+                                  String proxiedRootContext) {
         String destinationUri = cleanPath(destinationLocationUri);
+
         if (!destinationUri.matches("^https?://.*")) {
             // local dispatch
             destinationUri = proxiedRootContext;
         }
+
         try {
-            locationHeaderBuilder.setLocationHeader(originalRequest, response, destinationUri, destination.getContextRemoved(), proxiedRootContext);
+            locationHeaderBuilder.setLocationHeader(originalRequest, response, destinationUri,
+                                                    destination.getContextRemoved(),
+                                                    proxiedRootContext);
         } catch (MalformedURLException ex) {
             LOG.warn("Invalid URL in location header processing", ex);
         }
@@ -49,5 +85,31 @@ public class ResponseHeaderServiceImpl implements ResponseHeaderService {
 
     private String cleanPath(String uri) {
         return uri == null ? "" : uri.split("\\?")[0];
+    }
+
+    /**
+     * Listens for updates to the container.cfg.xml file which holds the via
+     * header receivedBy value.
+     */
+    private class ContainerConfigurationListener implements UpdateListener<ContainerConfiguration> {
+
+        private boolean isInitialized = false;
+
+        @Override
+        public void configurationUpdated(ContainerConfiguration configurationObject) {
+
+            if (configurationObject.getDeploymentConfig() != null) {
+                final String viaReceivedBy = configurationObject.getDeploymentConfig().getVia();
+
+                final ViaResponseHeaderBuilder viaBuilder = new ViaResponseHeaderBuilder(reposeVersion, viaReceivedBy);
+                final LocationHeaderBuilder locationBuilder = new LocationHeaderBuilder();
+            }
+            isInitialized = true;
+        }
+
+        @Override
+        public boolean isInitialized() {
+            return isInitialized;
+        }
     }
 }
