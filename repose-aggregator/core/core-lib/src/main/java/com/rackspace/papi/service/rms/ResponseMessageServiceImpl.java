fc3213a6c62e2f0067255c0441615cc105fc842c
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-07-16 15:33:28 -0500

    Fixing formatting and removed some dead assignments

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/rms/ResponseMessageServiceImpl.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/rms/ResponseMessageServiceImpl.java
@@ -26,144 +26,129 @@ import java.util.List;
 @Component("responseMessagingService")
 public class ResponseMessageServiceImpl implements ResponseMessageService {
 
-   private static final Logger LOG = LoggerFactory.getLogger(ResponseMessageServiceImpl.class);
-   private static final MediaType DEFAULT_TYPE = new MediaType(MimeType.WILDCARD);
+    private static final Logger LOG = LoggerFactory.getLogger(ResponseMessageServiceImpl.class);
+    private static final MediaType DEFAULT_TYPE = new MediaType(MimeType.WILDCARD);
 
-   private final KeyedStackLock configurationLock = new KeyedStackLock();
-   private final Object updateKey = new Object();
-   private final Object readKey = new Object();
-   private boolean initialized=false;
+    private final KeyedStackLock configurationLock = new KeyedStackLock();
+    private final Object updateKey = new Object();
+    private final Object readKey = new Object();
+    private boolean initialized=false;
 
-   private ImmutableStatusCodes immutableStatusCodes;
-   private ImmutableFormatTemplates immutableFormatTemplates;
+    private ImmutableStatusCodes immutableStatusCodes;
+    private ImmutableFormatTemplates immutableFormatTemplates;
 
-   @Override
-   public void destroy() {
-      // Nothing that a good de-referencing can't clean up.
-   }
+    @Override
+    public void destroy() {
+        // Nothing that a good de-referencing can't clean up.
+    }
 
-  @Override
-   public void setInitialized(){
+    @Override
+    public void setInitialized(){
         this.initialized=true;
-   }
+    }
 
 
-   @Override
-   public boolean isInitialized(){
+    @Override
+    public boolean isInitialized(){
         return initialized;
-   }
+    }
 
-   @Override
-   public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-      final StatusCodeMatcher matchedCode = getMatchingStatusCode(String.valueOf(response.getStatus()));
-      final MutableHttpServletRequest mutableRequest = MutableHttpServletRequest.wrap(request);
-      MediaRangeProcessor processor = new MediaRangeProcessor(mutableRequest.getPreferredHeaders("Accept", DEFAULT_TYPE));
+        final StatusCodeMatcher matchedCode = getMatchingStatusCode(String.valueOf(response.getStatus()));
+        final MutableHttpServletRequest mutableRequest = MutableHttpServletRequest.wrap(request);
+        MediaRangeProcessor processor = new MediaRangeProcessor(mutableRequest.getPreferredHeaders("Accept", DEFAULT_TYPE));
 
-      if(!isInitialized()){
-           response.sendError(HttpStatusCode.SERVICE_UNAVAIL.intValue(), "Error creating Response Messaging service.");
-      }else{
+        if(!isInitialized()) {
+            response.sendError(HttpStatusCode.SERVICE_UNAVAIL.intValue(), "Error creating Response Messaging service.");
+        } else {
+            if (matchedCode != null) {
+                HttpLogFormatter formatter;
+                List<MediaType>  mediaTypes=processor.process();
+                Message message = MessageFilter.filterByMediaType(matchedCode.getMessage(), mediaTypes);
 
-
-        if (matchedCode != null) {
-
-           HttpLogFormatter formatter = null;
-           Message message=null;
-           List<MediaType>  mediaTypes=processor.process();
-
-
-              message = MessageFilter.filterByMediaType(matchedCode.getMessage(), mediaTypes);
-
-        if(message!=null){
-              formatter=getHttpLogFormatter(matchedCode, message.getMediaType());
-
-
-
-           if (formatter != null) {
-
-              if (!(configSetToIfEmpty(matchedCode) && hasBody(response))) {
-
-                 final String formattedOutput = formatter.format("", request, response).trim();
-
-                 overwriteResponseBody(response, formattedOutput, message.getContentType());
-              }
-           } else{
-              LOG.info("No formatter found for message code.  Skipping Response Message Service formatting for status code regex " + matchedCode.getCodeRegex());
-           }}else{
-
-            LOG.info("Message for Matched code is empty. Matched Code is :" + matchedCode.getCodeRegex());
+                if(message!=null) {
+                    formatter=getHttpLogFormatter(matchedCode, message.getMediaType());
+                    if (formatter != null) {
+                        if (!(configSetToIfEmpty(matchedCode) && hasBody(response))) {
+                            final String formattedOutput = formatter.format("", request, response).trim();
+                            overwriteResponseBody(response, formattedOutput, message.getContentType());
+                        }
+                    } else {
+                        LOG.info("No formatter found for message code.  Skipping Response Message Service formatting for status code regex " + matchedCode.getCodeRegex());
+                    }
+                } else {
+                    LOG.info("Message for Matched code is empty. Matched Code is :" + matchedCode.getCodeRegex());
+                }
+            }
         }
-       }
-      }
-   }
+    }
 
-   @Override
-   public void updateConfiguration(List<StatusCodeMatcher> statusCodeMatchers) {
-      configurationLock.lock(updateKey);
+    @Override
+    public void updateConfiguration(List<StatusCodeMatcher> statusCodeMatchers) {
+        configurationLock.lock(updateKey);
 
-      try {
-        immutableStatusCodes = ImmutableStatusCodes.build(statusCodeMatchers);
-        immutableFormatTemplates = ImmutableFormatTemplates.build(statusCodeMatchers);
-      } finally {
-         configurationLock.unlock(updateKey);
-      }
-   }
+        try {
+            immutableStatusCodes = ImmutableStatusCodes.build(statusCodeMatchers);
+            immutableFormatTemplates = ImmutableFormatTemplates.build(statusCodeMatchers);
+        } finally {
+            configurationLock.unlock(updateKey);
+        }
+    }
 
-   private HttpLogFormatter getHttpLogFormatter(StatusCodeMatcher matchedCode, String preferredMediaType) {
-      HttpLogFormatter httpLogFormatter = null;
+    private HttpLogFormatter getHttpLogFormatter(StatusCodeMatcher matchedCode, String preferredMediaType) {
+        HttpLogFormatter httpLogFormatter = null;
 
-      if (matchedCode != null && preferredMediaType != null) {
+        if (matchedCode != null && preferredMediaType != null) {
+            configurationLock.lock(readKey);
 
+            try {
+                httpLogFormatter = immutableFormatTemplates.getMatchingLogFormatter(matchedCode.getId(), preferredMediaType);
+            } finally {
+                configurationLock.unlock(readKey);
+            }
+        }
+        return httpLogFormatter;
+    }
 
-         configurationLock.lock(readKey);
+    private StatusCodeMatcher getMatchingStatusCode(String responseCode) {
+        StatusCodeMatcher matchedCode = null;
 
-         try {
-            httpLogFormatter = immutableFormatTemplates.getMatchingLogFormatter(matchedCode.getId(), preferredMediaType);
-         } finally {
+        configurationLock.lock(readKey);
+
+        try {
+            if (immutableStatusCodes != null) {
+                matchedCode = immutableStatusCodes.getMatchingStatusCode(responseCode);
+            }
+        } finally {
             configurationLock.unlock(readKey);
-         }
-      }
+        }
 
-      return httpLogFormatter;
-   }
+        return matchedCode;
+    }
 
-   private StatusCodeMatcher getMatchingStatusCode(String responseCode) {
-      StatusCodeMatcher matchedCode = null;
+    private void overwriteResponseBody(HttpServletResponse response, final String formattedOutput, String contentType) throws IOException {
+        response.resetBuffer();
+        response.setContentLength(formattedOutput.length());
+        response.setHeader(CommonHttpHeader.CONTENT_TYPE.toString(), contentType);
 
-      configurationLock.lock(readKey);
+        // TODO:Enhancement - Update formatter logic for streaming
+        // TODO:Enhancement - Update getBytes(...) to use requested content encoding
+        response.getOutputStream().write(formattedOutput.getBytes(CharacterSets.UTF_8));
+    }
 
-      try {
-         if (immutableStatusCodes != null) {
-            matchedCode = immutableStatusCodes.getMatchingStatusCode(responseCode);
-         }
-      } finally {
-         configurationLock.unlock(readKey);
-      }
+    private boolean configSetToIfEmpty(StatusCodeMatcher matchedCode) {
+        return StringUtilities.nullSafeEqualsIgnoreCase(matchedCode.getOverwrite().value(), OverwriteType.IF_EMPTY.value());
+    }
 
-      return matchedCode;
-   }
-
-   private void overwriteResponseBody(HttpServletResponse response, final String formattedOutput, String contentType) throws IOException {
-      response.resetBuffer();
-      response.setContentLength(formattedOutput.length());
-      response.setHeader(CommonHttpHeader.CONTENT_TYPE.toString(), contentType);
-
-      // TODO:Enhancement - Update formatter logic for streaming
-      // TODO:Enhancement - Update getBytes(...) to use requested content encoding
-      response.getOutputStream().write(formattedOutput.getBytes(CharacterSets.UTF_8));
-   }
-
-   private boolean configSetToIfEmpty(StatusCodeMatcher matchedCode) {
-      return StringUtilities.nullSafeEqualsIgnoreCase(matchedCode.getOverwrite().value(), OverwriteType.IF_EMPTY.value());
-   }
-
-   private boolean hasBody(HttpServletResponse response) {
-      boolean hasBody = false;
-      try{
-         hasBody = ((MutableHttpServletResponse)response).getBufferedOutputAsInputStream().available() > 0;
-      }catch(IOException e){
-         LOG.warn("Unable to retrieve response body input stream",e);
-      }
-      return hasBody;
-   }
+    private boolean hasBody(HttpServletResponse response) {
+        boolean hasBody = false;
+        try {
+            hasBody = ((MutableHttpServletResponse)response).getBufferedOutputAsInputStream().available() > 0;
+        }catch(IOException e){
+            LOG.warn("Unable to retrieve response body input stream",e);
+        }
+        return hasBody;
+    }
 }
