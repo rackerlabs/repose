2371f88dcda2bcd2df8746a84d2ef8ecccf9be3d
Melissa Kam <melissa.kam@rackspace.com>
2014-07-17 22:10:31 -0500

    Cleaning up and adding null tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/config/DefaultDestinationTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/config/DefaultDestinationTest.groovy
@@ -4,7 +4,6 @@ import framework.ReposeConfigurationProvider
 import framework.ReposeLogSearch
 import framework.ReposeValveLauncher
 import framework.TestProperties
-import org.junit.Ignore
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.PortFinder
 
@@ -27,6 +26,8 @@ class DefaultDestinationTest extends Specification {
     Map params = [:]
     Deproxy deproxy
     boolean expectCleanShutdown
+
+    String errorMessage="There should only be one default destination."
 
     def setup() {
 
@@ -51,46 +52,6 @@ class DefaultDestinationTest extends Specification {
 
     }
 
-    def "start with only one default destination endpoint in system model configs, should return 200"() {
-        given:
-        // set the common and good configs
-        reposeConfigProvider.cleanConfigDirectory()
-        reposeConfigProvider.applyConfigs("common", params)
-        reposeConfigProvider.applyConfigs("features/core/config/common", params)
-
-        params += [
-                "default1":default1, "default2":default2, "default2":default3
-
-        ]
-        reposeConfigProvider.applyConfigs("features/core/config/default-dest-good", params)
-        expectCleanShutdown = true
-
-        // start repose
-        repose = new ReposeValveLauncher(
-                reposeConfigProvider,
-                properties.getReposeJar(),
-                url,
-                properties.getConfigDirectory(),
-                reposePort
-        )
-        repose.enableDebug()
-        reposeLogSearch = new ReposeLogSearch(properties.getLogFile())
-        reposeLogSearch.deleteLog()
-
-
-        repose.start(killOthersBeforeStarting: false,
-                waitOnJmxAfterStarting: false)
-        repose.waitForNon500FromUrl(url)
-
-        expect: "starting Repose with good configs should yield 200"
-        deproxy.makeRequest(url: url).receivedResponse.code == "200"
-
-        where:
-        default1 | default2 | default3
-        'true'   | 'false'   | 'false'
-
-    }
-
     @Unroll("when defaults: #default1, #default2, #default3")
     def "start with more or less than one default destination endpoint in system model configs, should log error and fail to connect"() {
         given:
@@ -103,8 +64,7 @@ class DefaultDestinationTest extends Specification {
                 "default1":default1, "default2":default2, "default3":default3
 
         ]
-        reposeConfigProvider.applyConfigs("features/core/config/default-dest-bad", params)
-        // = false
+        reposeConfigProvider.applyConfigs("features/core/config/default-dest", params)
 
         // start repose
         repose = new ReposeValveLauncher(
@@ -137,23 +97,121 @@ class DefaultDestinationTest extends Specification {
 
 
         where:
-        default1 | default2 | default3  | errorMessage
-        null     | null     | null      | "not a valid value for 'boolean'"
-        null     | null     | 'false'   | "not a valid value for 'boolean'"
-        null     | 'true'   | 'true'    | "not a valid value for 'boolean'"
-        null     | 'false'  | null      | "not a valid value for 'boolean'"
-        'false'  | 'false'  | 'false'   | "There should only be one default destination."
-        'true'   | null     | 'true'    | "not a valid value for 'boolean'"
-        'true'   | 'true'   | null      | "not a valid value for 'boolean'"
-        'true'   | 'true'     | 'true'  | "There should only be one default destination."
-//        'true'   | 'true'   | 'false'
-//        'true'   | 'false'  | 'true'
-//        'false'  | null     | null
-//        'false'  | null     | 'false'
-//        'false'  | 'true'   | 'true'
-//        'false'  | 'false'  | null
+        default1 | default2 | default3
+        false    | false    | false
+        true     | true     | true
+        true     | true     | false
+        true     | false    | true
+        false    | true     | true
 
     }
+
+    @Unroll("when defaults: #default1, #default2, #default3")
+    def "start with only one default destination endpoint in system model configs, should return 200"() {
+        given:
+        // set the common and good configs
+        reposeConfigProvider.cleanConfigDirectory()
+        reposeConfigProvider.applyConfigs("common", params)
+        reposeConfigProvider.applyConfigs("features/core/config/common", params)
+
+        params += [
+                "default1":default1, "default2":default2, "default3":default3
+
+        ]
+        reposeConfigProvider.applyConfigs("features/core/config/default-dest", params)
+        expectCleanShutdown = true
+
+        // start repose
+        repose = new ReposeValveLauncher(
+                reposeConfigProvider,
+                properties.getReposeJar(),
+                url,
+                properties.getConfigDirectory(),
+                reposePort
+        )
+        repose.enableDebug()
+        reposeLogSearch = new ReposeLogSearch(properties.getLogFile())
+        reposeLogSearch.deleteLog()
+
+
+        repose.start(killOthersBeforeStarting: false,
+                waitOnJmxAfterStarting: false)
+        repose.waitForNon500FromUrl(url)
+
+        expect: "starting Repose with good configs should yield 200"
+        deproxy.makeRequest(url: url).receivedResponse.code == "200"
+
+        where:
+        default1  | default2  | default3
+        true      | false     | false
+        false     | true      | false
+        false     | false     | true
+
+    }
+
+    @Unroll("when defaults: #default1, #default2, #default3")
+    def "start with more or less than one default destination and null values, should log error and fail to connect"() {
+        given:
+        // set the common and good configs
+        reposeConfigProvider.cleanConfigDirectory()
+        reposeConfigProvider.applyConfigs("common", params)
+        reposeConfigProvider.applyConfigs("features/core/config/common", params)
+
+        params += [
+                "default1":defaultParamWrapper(default1),
+                "default2":defaultParamWrapper(default2),
+                "default3":defaultParamWrapper(default3)
+        ]
+        reposeConfigProvider.applyConfigs("features/core/config/default-dest-null", params)
+
+        // start repose
+        repose = new ReposeValveLauncher(
+                reposeConfigProvider,
+                properties.getReposeJar(),
+                url,
+                properties.getConfigDirectory(),
+                reposePort
+        )
+        repose.enableDebug()
+        reposeLogSearch = new ReposeLogSearch(properties.getLogFile())
+        reposeLogSearch.deleteLog()
+
+
+        when: "starting Repose with more or less than one default destination endpoint"
+        repose.start([waitOnJmxAfterStarting: false])
+
+        then: "error should be logged"
+        waitForCondition(repose.clock, "30s", "2s") {
+            new File(reposeLogSearch.logFileLocation).exists()
+        }
+        waitForCondition(repose.clock, "20s", "2s"){
+            reposeLogSearch.searchByString(errorMessage).size() != 0
+        }
+
+        when: "making a request to repose with and invalid default destination endpoint settings"
+        deproxy.makeRequest(url: url)
+        then: "connection exception should be returned"
+        thrown(ConnectException)
+
+
+        where:
+        default1 | default2 | default3
+        null     | null     | null
+        null     | null     | false
+        null     | false    | false
+        null     | true     | true
+        null     | false    | null
+        true     | null     | true
+        true     | true     | null
+        false    | null     | null
+        false    | null     | false
+        false    | false    | null
+    }
+
+    private def defaultParamWrapper(Object value){
+        return value==null ? '' :  '\" default=\"'+value
+    }
+
 
     def cleanup() {
         if (repose) {
