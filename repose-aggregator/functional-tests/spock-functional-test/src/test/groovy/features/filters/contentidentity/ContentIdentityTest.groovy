1f8e27833972f736fa2be09bd814894dc63020ea
Damien Johnson <damien.johnson@rackspace.com>
2014-07-24 14:05:42 -0500

    Updated content-identity-auth tests for 2.0

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/contentidentity/ContentIdentityTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/contentidentity/ContentIdentityTest.groovy
@@ -9,45 +9,16 @@ import spock.lang.Unroll
 
 class ContentIdentityTest extends ReposeValveTest {
 
-    def static Map contentJson = ["content-type": "application/json"]
+    // NOTE: JSON is not supported by the content identity 2.0 filter
+
+    def static String xmlPasswordCred = "<auth xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://docs.openstack.org/identity/api/v2.0\"><credential xsi:type=\"PasswordCredentialsRequiredUsername\" username=\"test-user\" password=\"testpwd\" /></auth>"
+
     def static Map contentXml = ["content-type": "application/xml"]
-
-    def static String jsonPasswordCred = "{ \"passwordCredentials\": { \"username\": \"test-user\", \"password\": \"testpwd\"}}"
-    def static String jsonKeyCred = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"testpwd\"}}"
-    def static String jsonKeyCredEmptyKey = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"\"}}"
-    def static String jsonMossoIdCred = "{ \"mossoCredentials\": { \"mossoId\": 12345, \"key\": \"testpwd\"}}"
-    def static String jsonMossoNastCred = "{ \"mossoCredentials\": { \"mossoId\": 12345, \"key\": \"testpwd\"}}"
-    def static String jsonMossoNastCredEmptyKey = "{ \"nastCredentials\": { \"nastId\": \"12345\", \"key\": \"\"}}"
-    def static String xmlPasswordCred = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" password=\"testpwd\" />"
-    def static String xmlPasswordCredEmptyKey = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" password=\"\" />"
-    def static String xmlKeyCred = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"testpwd\" />"
-    def static String xmlKeyCredEmptyKey = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"\" />"
-    def static String xmlMossoIdCred = "<mossoCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" mossoId=\"12345\" key=\"testpwd\" />"
-    def static String xmlMossoNastCred = "<mossoCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" mossoId=\"12345\" key=\"testpwd\" />"
-
-    def static String jsonPasswordCredEmptyUser = "{ \"passwordCredentials\": { \"username\": \"\", \"password\": \"testpwd\"}}"
-    def static String jsonPasswordCredEmptyUserField = "{ \"passwordCredentials\": { \"password\": \"testpwd\"}}"
-    def static String jsonPasswordCredEmptyPasswordField = "{ \"passwordCredentials\": { \"username\": \"test-user\" }}"
-    def static String jsonKeyCredEmptyUser = "{ \"credentials\": { \"username\": \"\", \"key\": \"testpwd\"}}"
-    def static String jsonKeyCredEmptyUserField = "{ \"credentials\": { \"key\": \"testpwd\"}}"
-    def static String jsonKeyCredNoKey = "{ \"credentials\": { \"username\": \"test-user\" }}"
-    def static String jsonMossoCredNoId = "{ \"mossoCredentials\": { \"key\": \"testpwd\"}}"
-    def static String jsonMossoCredNoKey = "{ \"mossoCredentials\": { \"mossoId\": 12345 }}"
-    def static String jsonMossoNastEmptyNast = "{ \"nastCredentials\": { \"nastId\": \"\", \"key\": \"testpwd\"}}"
-    def static String jsonMossoNastNoNastId = "{ \"nastCredentials\": { \"key\": \"testpwd\"}}"
-    def static String jsonMossoNastNoKey = "{ \"nastCredentials\": { \"nastId\": \"12345\" }}"
-    def static String xmlPasswordCredEmptyUser = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"\" password=\"testpwd\" />"
-    def static String xmlPasswordCredEmptyUserField = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" password=\"testpwd\" />"
-    def static String xmlKeyCredEmptyUser = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"\" key=\"testpwd\" />"
-    def static String xmlKeyCredEmptyUserField = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" key=\"testpwd\" />"
-    def static String xmlMossoCredInvalidId = "<mossoCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" mossoId=\"a\" key=\"testpwd\" />"
-    def static String xmlMossoNastCredEmptyNastId = "<nastCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" nastId=\"\" key=\"testpwd\" />"
-    def static String xmlMossoNastCredNoEmptyNastField = "<nastCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" key=\"testpwd\" />"
+    def static String xmlPasswordCredEmptyKey = "<auth xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://docs.openstack.org/identity/api/v2.0\"><credential xsi:type=\"PasswordCredentialsRequiredUsername\" username=\"test-user\" password=\"\" /></auth>"
 
     def static String invalidData = "Invalid data"
 
-    def static String jsonOverLimit = "{ \"passwordCredentials\": { \"username\": \"012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\", \"password\": \"testpwd\"}}"
-    def static String xmlOverLimit = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\" password=\"testpwd\" />"
+    def static String xmlOverLimit = "<auth xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://docs.openstack.org/identity/api/v2.0\"><credential xsi:type=\"PasswordCredentialsRequiredUsername\" username=\"012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\" password=\"testpwd\" /></auth>"
 
     def setupSpec() {
         deproxy = new Deproxy()
@@ -72,7 +43,6 @@ class ContentIdentityTest extends ReposeValveTest {
 
 
     @Unroll("When request contains identity in content #testName\n Expected user is #expectedUser")
-
     def "when identifying requests by header"() {
 
         when: "Request body contains user credentials"
@@ -93,23 +63,12 @@ class ContentIdentityTest extends ReposeValveTest {
 
         where:
         requestBody               | contentType | expectedUser  | testName
-        jsonPasswordCred          | contentJson | "test-user"   | "jsonPasswordCred"
-        jsonKeyCred               | contentJson | "test-user"   | "jsonKeyCred"
-        jsonKeyCredEmptyKey       | contentJson | "test-user"   | "jsonKeyCredEmptyKey"
-        jsonMossoIdCred           | contentJson | "12345"       | "jsonMossoIdCred"
-        jsonMossoNastCred         | contentJson | "12345"       | "jsonMossoNastCred"
-        jsonMossoNastCredEmptyKey | contentJson | "12345"       | "jsonMossoNastCredEmptyKey"
         xmlPasswordCred           | contentXml  | "test-user"   | "xmlPasswordCred"
         xmlPasswordCredEmptyKey   | contentXml  | "test-user"   | "xmlPasswordCredEmptyKey"
-        xmlKeyCred                | contentXml  | "test-user"   | "xmlKeyCred"
-        xmlKeyCredEmptyKey        | contentXml  | "test-user"   | "xmlKeyCredEmptyKey"
-        xmlMossoIdCred            | contentXml  | "12345"       | "xmlMossoIdCred"
-        xmlMossoNastCred          | contentXml  | "12345"       | "xmlMossoNastCred"
     }
 
 
     @Unroll("When bad requests pass through repose #testName\n")
-
     def "when attempting to identity user by content and passed bad content"() {
 
         when: "Request body contains user credentials"
@@ -122,36 +81,10 @@ class ContentIdentityTest extends ReposeValveTest {
         and: "Repose will not send x-pp-groups"
         ((Handling) sentRequest).request.getHeaders().findAll("x-pp-groups").size() == 0
 
-
-
         where:
         requestBody                        | contentType | testName
-        jsonPasswordCredEmptyUser          | contentJson | "jsonPasswordCredEmptyUser"
-        jsonPasswordCredEmptyUserField     | contentJson | "jsonPasswordCredEmptyUserField"
-        jsonPasswordCredEmptyPasswordField | contentJson | "jsonPasswordCredEmptyPasswordField"
-        jsonKeyCredEmptyUser               | contentJson | "jsonKeyCredEmptyUser"
-        jsonKeyCredEmptyUserField          | contentJson | "jsonKeyCredEmptyUserField"
-        jsonKeyCredNoKey                   | contentJson | "jsonKeyCredNoKey"
-        jsonMossoCredNoId                  | contentJson | "jsonMossoCredNoId"
-        jsonMossoCredNoKey                 | contentJson | "jsonMossoCredNoKey"
-        jsonMossoNastEmptyNast             | contentJson | "jsonMossoNastEmptyNast"
-        jsonMossoNastNoNastId              | contentJson | "jsonMossoNastNoNastId"
-        jsonMossoNastNoKey                 | contentJson | "jsonMossoNastNoKey"
-
-        xmlPasswordCredEmptyUser           | contentXml  | "xmlPasswordCredEmptyUser"
-        xmlPasswordCredEmptyUserField      | contentXml  | "xmlPasswordCredEmptyUserField"
-        xmlKeyCredEmptyUser                | contentXml  | "xmlKeyCredEmptyUser"
-        xmlKeyCredEmptyUserField           | contentXml  | "xmlKeyCredEmptyUserField"
-        xmlMossoCredInvalidId              | contentXml  | "xmlMossoCredInvalidId"
-        xmlMossoNastCredEmptyNastId        | contentXml  | "xmlMossoNastCredEmptyNastId"
-        xmlMossoNastCredNoEmptyNastField   | contentXml  | "xmlMossoNastCredNoEmptyNastField"
-
-        invalidData                        | contentJson | "invalidData"
         invalidData                        | contentXml  | "invalidData"
-        jsonOverLimit                      | contentJson | "jsonOverLimit"
         xmlOverLimit                       | contentXml  | "xmlOverLimit"
-
-
     }
 
     def "Should not split request headers according to rfc"() {
