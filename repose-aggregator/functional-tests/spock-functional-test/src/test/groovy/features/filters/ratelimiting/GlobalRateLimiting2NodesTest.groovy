ef0a1789e9ac5c031e97b8a9de677930274bbca1
jenny.vo <jenny.vo@rackspace.com>
2014-07-30 14:49:14 -0500

    update and add Global rate limit test with 2 nodes

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/ratelimiting/GlobalRateLimiting2NodesTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/ratelimiting/GlobalRateLimiting2NodesTest.groovy
@@ -1,7 +1,6 @@
 package features.filters.ratelimiting
 
 import framework.ReposeValveTest
-import groovy.json.JsonSlurper
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
 import org.rackspace.deproxy.PortFinder
@@ -15,12 +14,20 @@ import javax.xml.parsers.DocumentBuilderFactory
 /**
  * Created by jennyvo on 7/30/14.
  */
-class GlobalRateLimitingTest extends ReposeValveTest {
+class GlobalRateLimiting2NodesTest extends ReposeValveTest {
     final handler = {return new Response(200, "OK")}
 
     final Map<String, String> userHeaderDefault = ["X-PP-User" : "user"]
     final Map<String, String> groupHeaderDefault = ["X-PP-Groups" : "customer"]
     final Map<String, String> acceptHeaderDefault = ["Accept" : "application/xml"]
+
+    static int reposePort2
+    static int distDatastorePort
+    static int distDatastorePort2
+
+    def getReposeEndpoint2() {
+        return "http://localhost:${reposePort2}"
+    }
 
     static int userCount = 0;
     String getNewUniqueUser() {
@@ -30,13 +37,26 @@ class GlobalRateLimitingTest extends ReposeValveTest {
         return name;
     }
 
+
     def setupSpec() {
+
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort)
 
+        reposePort2 = PortFinder.Singleton.getNextOpenPort()
+        distDatastorePort = PortFinder.Singleton.getNextOpenPort()
+        distDatastorePort2 = PortFinder.Singleton.getNextOpenPort()
+
         def params = properties.getDefaultTemplateParams()
+        params += [
+                reposePort2: reposePort2,
+                distDatastorePort: distDatastorePort,
+                distDatastorePort2: distDatastorePort2
+        ]
+
+
         repose.configurationProvider.applyConfigs("common", params)
-        repose.configurationProvider.applyConfigs("features/filters/ratelimiting/onenodes", params)
+        repose.configurationProvider.applyConfigs("features/filters/ratelimiting/twonodes", params)
         repose.start()
         repose.waitForNon500FromUrl(reposeEndpoint)
     }
@@ -48,36 +68,23 @@ class GlobalRateLimitingTest extends ReposeValveTest {
             deproxy.shutdown()
     }
 
-    def cleanup() {
-        waitForLimitReset()
-    }
+    def "When Repose is configured with multiple nodes, rate-limiting info should be shared"() {
+        given: "load the configs for multiple nodes, and use all remaining requests"
+        useAllRemainingRequests()
 
-    def "When Repose config with Global Rate Limit, user limit should hit first" () {
-        given: "the rate-limit has not been reached"
-        waitForLimitReset()
+        when: "the user sends their request and the rate-limit has not been reached"
+        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint2, method: "GET",
+                headers: userHeaderDefault + groupHeaderDefault, defaultHandler: handler)
 
-        (1..5).each{
-            i ->
-                when: "the user sends their request and the rate-limit has not been reached"
-                MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint + "/service/test", method: "GET",
-                        headers: userHeaderDefault + ['X-PP-Groups': 'all-limits-small'], defaultHandler: handler)
-
-                then: "the request is not rate-limited, and passes to the origin service"
-                messageChain.receivedResponse.code.equals("200")
-                messageChain.handlings.size() == 1
-        }
-
-        when: "the user hit the rate-limit"
-        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint + "/service/test", method: "GET",
-                headers: userHeaderDefault + ['X-PP-Groups': 'all-limits-small'], defaultHandler: handler)
-
-        then: "the request is not rate-limited, and passes to the origin service"
+        then: "the request is rate-limited, and does not pass to the origin service"
         messageChain.receivedResponse.code.equals("413")
+        messageChain.handlings.size() == 0
     }
 
     def "When Run with different users, hit the same resource, global limit share between users" () {
         given:"the rate-limit has not been reached"
-        waitForLimitReset()
+        //waitForLimitReset
+        sleep(60000)
         def user1 = getNewUniqueUser()
         def user2 = getNewUniqueUser()
         def group = "customer"
@@ -98,7 +105,7 @@ class GlobalRateLimitingTest extends ReposeValveTest {
         (1..3).each {
             i ->
                 when: "user2 hit the same resource"
-                MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint + "/service/test", method: "GET",
+                MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint2 + "/service/test", method: "GET",
                         headers: headers2, defaultHandler: handler)
 
                 then: "the request is not rate-limited, and passes to the origin service"
@@ -106,54 +113,21 @@ class GlobalRateLimitingTest extends ReposeValveTest {
                 messageChain.handlings.size() == 1
         }
 
-        when: "user2 hit the same resource, rate limitted"
-        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint + "/service/test", method: "GET",
+        when: "user1 hit the same resource, rate limitted"
+        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint2 + "/service/test", method: "GET",
                 headers: headers1, defaultHandler: handler)
 
         then: "the request is rate-limited, and passes to the origin service"
         messageChain.receivedResponse.code.equals("413")
-
     }
 
     private void useAllRemainingRequests() {
         MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint, method: "GET",
                 headers: userHeaderDefault + groupHeaderDefault, defaultHandler: handler);
 
-        while(!messageChain.receivedResponse.code.equals("413")) {
+        while (!messageChain.receivedResponse.code.equals("413")) {
             messageChain = deproxy.makeRequest(url: reposeEndpoint, method: "GET",
                     headers: userHeaderDefault + groupHeaderDefault, defaultHandler: handler);
         }
-    }
-
-    private void waitForLimitReset(Map group = null) {
-        while (parseRemainingFromXML(getDefaultLimits(group), 0) != parseAbsoluteFromXML(getDefaultLimits(group), 0)) {
-            sleep(1000)
-        }
-    }
-
-    private String getDefaultLimits(Map group = null) {
-        def groupHeader = (group != null) ? group : groupHeaderDefault
-        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint + "/service2/limits", method: "GET",
-                headers: userHeaderDefault + groupHeader + acceptHeaderDefault);
-
-        return messageChain.receivedResponse.body
-    }
-
-    private int parseRemainingFromXML(String s, int limit) {
-        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        Document document = documentBuilder.parse(new InputSource(new StringReader(s)))
-
-        document.getDocumentElement().normalize()
-
-        return Integer.parseInt(document.getElementsByTagName("limit").item(limit).getAttributes().getNamedItem("remaining").getNodeValue())
-    }
-
-    private int parseAbsoluteFromXML(String s, int limit) {
-        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        Document document = documentBuilder.parse(new InputSource(new StringReader(s)))
-
-        document.getDocumentElement().normalize()
-
-        return Integer.parseInt(document.getElementsByTagName("limit").item(limit).getAttributes().getNamedItem("value").getNodeValue())
     }
 }
