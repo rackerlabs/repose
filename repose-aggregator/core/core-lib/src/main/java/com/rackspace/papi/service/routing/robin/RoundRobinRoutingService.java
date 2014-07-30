32ec058343d80f385cb2497d4c8c00f45e324c4a
David Kowis <dkowis@shlrm.org>
2014-07-30 16:17:13 -0500

    Took out all the servicePort stuff from this class
    There's no reason to have it there if we've got it wrapped nicely to get
    service ports for a cluster and a node.

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/routing/robin/RoundRobinRoutingService.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/routing/robin/RoundRobinRoutingService.java
@@ -1,54 +1,42 @@
 package com.rackspace.papi.service.routing.robin;
 
-import com.rackspace.papi.domain.Port;
-import com.rackspace.papi.domain.ReposeInstanceInfo;
 import com.rackspace.papi.model.Node;
-import com.rackspace.papi.model.ReposeCluster;
 import com.rackspace.papi.model.SystemModel;
 import com.rackspace.papi.service.routing.RoutingService;
-import com.rackspace.papi.servlet.InitParameter;
 import org.openrepose.core.service.config.ConfigurationService;
 import org.openrepose.core.service.config.manager.UpdateListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.context.ServletContextAware;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.ServletContext;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * This no longer does weird stuff with ports
+ * It just provides a way to get routable nodes from the SystemModel.
+ */
 @Named
-public class RoundRobinRoutingService implements RoutingService, ServletContextAware {
+public class RoundRobinRoutingService implements RoutingService {
     private static final Logger LOG = LoggerFactory.getLogger(RoundRobinRoutingService.class);
-    private final ReposeInstanceInfo reposeInstanceInfo;
     private Clusters domains;
     private SystemModel config;
     private final ConfigurationService configurationManager;
     private final PowerApiConfigListener configurationListener;
-    private ServletContext servletContext;
 
 
     @Inject
     public RoundRobinRoutingService(
-            ReposeInstanceInfo reposeInstanceInfo,
             ConfigurationService configurationManager) {
         this.configurationManager = configurationManager;
         this.configurationListener = new PowerApiConfigListener();
-        this.reposeInstanceInfo = reposeInstanceInfo;
-    }
-
-    @Override
-    public void setServletContext(ServletContext servletContext) {
-        this.servletContext = servletContext;
     }
 
     @PostConstruct
     public void afterPropertiesSet() {
+        LOG.debug("initializing RoundRobinRoutingService");
         final URL xsdURL = getClass().getResource("/META-INF/schema/system-model/system-model.xsd");
         configurationManager.subscribeTo("system-model.cfg.xml", xsdURL, configurationListener, SystemModel.class);
     }
@@ -77,62 +65,14 @@ public class RoundRobinRoutingService implements RoutingService, ServletContextA
         return rtn;
     }
 
-    //TODO: WHAT IS THIS MADNESS!?!?
-    //WHAT ARE THESE PORTS FOR??!?
     private class PowerApiConfigListener implements UpdateListener<SystemModel> {
         private boolean isInitialized = false;
-
-        //So this collects poerts from all nodes, and just throws them in a list without associating what node they're with?
-        // WHAT MADNESS IS THIS
-        private List<Port> determinePorts(final Node reposeNode) {
-            //This will replace all the ports, and I don't know how this is not a problem.
-            final ArrayList<Port> ports = new ArrayList<>();
-
-            if (reposeNode != null) {
-                if (reposeNode.getHttpPort() != 0) {
-                    ports.add(new Port("http", reposeNode.getHttpPort()));
-                } else {
-                    LOG.error("Http service port not specified for Repose Node " + reposeNode.getId());
-                }
-
-                if (reposeNode.getHttpsPort() != 0) {
-                    ports.add(new Port("https", reposeNode.getHttpsPort()));
-                } else {
-                    LOG.info("Https service port not specified for Repose Node " + reposeNode.getId());
-                }
-            }
-            return ports;
-        }
-
-        private Node determineReposeNode(final String clusterId, final String nodeId) {
-            for (ReposeCluster cluster : config.getReposeCluster()) {
-                if (cluster.getId().equals(clusterId)) {
-                    for (Node node : cluster.getNodes().getNode()) {
-                        if (node.getId().equals(nodeId)) {
-                            return node;
-                        }
-                    }
-                }
-            }
-            return null;
-        }
 
         @Override
         public void configurationUpdated(final SystemModel configurationObject) {
             config = configurationObject;
             setSystemModel(config);
 
-            if (!isInitialized()) {
-                final String clusterIdParam = InitParameter.REPOSE_CLUSTER_ID.getParameterName();
-                final String nodeIdParam = InitParameter.REPOSE_NODE_ID.getParameterName();
-                final String clusterId = System.getProperty(clusterIdParam, servletContext.getInitParameter(clusterIdParam));
-                final String nodeId = System.getProperty(nodeIdParam, servletContext.getInitParameter(nodeIdParam));
-
-                LOG.info("Determining ports for repose under cluster: " + clusterId);
-                //TODO: This is super jank, because it clobbers ports and then adds them all, but it's what it always did
-                reposeInstanceInfo.getPorts().clear();
-                reposeInstanceInfo.getPorts().addAll(determinePorts(determineReposeNode(clusterId, nodeId)));
-            }
             isInitialized = true;
         }
 
