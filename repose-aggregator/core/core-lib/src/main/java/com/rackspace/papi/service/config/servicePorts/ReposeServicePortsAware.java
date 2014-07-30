8038100c13f3230baf50a5c7d3e3b8d9ca349245
David Kowis <dkowis@shlrm.org>
2014-07-30 15:41:41 -0500

    Add something internal services can use to get ahold of servicePorts

---
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/config/servicePorts/ReposeServicePortsAware.java
@@ -0,0 +1,13 @@
+package com.rackspace.papi.service.config.servicePorts;
+
+import com.rackspace.papi.domain.Port;
+
+import java.util.List;
+
+/**
+ * This interface provides the callback to tell things about when their service ports have been updated.
+ */
+public interface ReposeServicePortsAware {
+    public void updatedServicePorts(List<Port> ports);
+    public void destroy();
+}
