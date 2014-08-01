15e62485a4c7a54a544b18903a53d3be6123568e
David Kowis <dkowis@shlrm.org>
2014-08-01 17:14:33 -0500

    Create some tests describing our acceptance criteria

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/ServoTest.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/ServoTest.scala
@@ -1,7 +1,36 @@
 package org.openrepose.servo
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
+@RunWith(classOf[JUnitRunner])
 class ServoTest extends FunSpec with Matchers {
 
+  describe("Servo, the Repose Valve Launcher") {
+    it("executes the command to start a Jetty with the Repose War on the specified port"){
+      pending
+    }
+    it("executes several commands if there are several local instances configured") {
+      pending
+    }
+    it("passes through REPOSE_JVM_OPTS through as JVM_OPTS") {
+      pending
+    }
+    it("warns when JVM_OPTS are set") {
+      pending
+    }
+    describe("watches the system-model.cfg.xml for changes to nodes") {
+      it("starts new valves when they're added") {
+        pending
+      }
+      it("stops orphaned valves when they're removed") {
+        pending
+      }
+      it("does not restart valves if the ordering changed") {
+        pending
+      }
+    }
+
+  }
 }
