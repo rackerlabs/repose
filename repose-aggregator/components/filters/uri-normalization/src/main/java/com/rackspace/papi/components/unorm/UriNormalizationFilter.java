689ebbce035092fc33340e6a7161316144e88914
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-05 16:18:01 -0500

    Springified the UriNormalizationFilter.

--- repose-aggregator/components/filters/uri-normalization/src/main/java/com/rackspace/papi/components/unorm/UriNormalizationFilter.java
+++ repose-aggregator/components/filters/uri-normalization/src/main/java/com/rackspace/papi/components/unorm/UriNormalizationFilter.java
@@ -7,19 +7,30 @@ import org.openrepose.core.service.config.ConfigurationService;
 import com.rackspace.papi.service.context.ServletContextHelper;
 import java.io.IOException;
 import java.net.URL;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.*;
 
 import com.rackspace.papi.service.reporting.metrics.MetricsService;
 import org.slf4j.Logger;
 
+@Named
 public class UriNormalizationFilter implements Filter {
 
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(UriNormalizationFilter.class);
     private static final String DEFAULT_CONFIG = "uri-normalization.cfg.xml";
     private String config;
     private UriNormalizationHandlerFactory handlerFactory;
-    private ConfigurationService configurationManager;
-    private MetricsService metricsService;
+    private final ConfigurationService configurationService;
+    private final MetricsService metricsService;
+
+    @Inject
+    public UriNormalizationFilter(
+            ConfigurationService configurationService,
+            MetricsService metricsService) {
+        this.configurationService = configurationService;
+        this.metricsService = metricsService;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -28,18 +39,15 @@ public class UriNormalizationFilter implements Filter {
 
     @Override
     public void destroy() {
-        configurationManager.unsubscribeFrom(config, handlerFactory);
+        configurationService.unsubscribeFrom(config, handlerFactory);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
-        configurationManager = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
-        metricsService = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext()
-                .metricsService();
         handlerFactory = new UriNormalizationHandlerFactory(metricsService);
         URL xsdURL = getClass().getResource("/META-INF/schema/config/uri-normalization-configuration.xsd");
-        configurationManager.subscribeTo(filterConfig.getFilterName(),config,xsdURL, handlerFactory, UriNormalizationConfig.class);
+        configurationService.subscribeTo(filterConfig.getFilterName(), config, xsdURL, handlerFactory, UriNormalizationConfig.class);
     }
 }
