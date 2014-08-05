607f9867d7273ef8a22bcce1ea69d9d6784ba9f1
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-05 18:51:51 -0500

    Springified the DestinationRouterFilter.

--- repose-aggregator/components/filters/destination-router/src/main/java/org/openrepose/components/routing/servlet/DestinationRouterFilter.java
+++ repose-aggregator/components/filters/destination-router/src/main/java/org/openrepose/components/routing/servlet/DestinationRouterFilter.java
@@ -8,20 +8,30 @@ import com.rackspace.papi.service.context.ServletContextHelper;
 import com.rackspace.papi.service.reporting.metrics.MetricsService;
 import org.openrepose.components.routing.servlet.config.DestinationRouterConfiguration;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.*;
 import java.io.IOException;
 import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Named
 public class DestinationRouterFilter implements Filter {
 
     private static final Logger LOG = LoggerFactory.getLogger(DestinationRouterFilter.class);
     private static final String DEFAULT_CONFIG = "destination-router.cfg.xml";
     private String config;
     private DestinationRouterHandlerFactory handlerFactory;
-    private ConfigurationService manager;
-    private MetricsService metricsService;
+    private final ConfigurationService configurationService;
+    private final MetricsService metricsService;
+
+    @Inject
+    public DestinationRouterFilter(ConfigurationService configurationService,
+                     MetricsService metricsService) {
+        this.configurationService = configurationService;
+        this.metricsService = metricsService;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -30,19 +40,16 @@ public class DestinationRouterFilter implements Filter {
 
     @Override
     public void destroy() {
-        manager.unsubscribeFrom(config, handlerFactory);
+        configurationService.unsubscribeFrom(config, handlerFactory);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {        
-        manager = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
-        metricsService = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext()
-                .metricsService();
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
         handlerFactory = new DestinationRouterHandlerFactory(metricsService);
         URL xsdURL = getClass().getResource("/META-INF/schema/config/destination-router-configuration.xsd");
-        manager.subscribeTo(filterConfig.getFilterName(),"system-model.cfg.xml", handlerFactory, SystemModel.class);
-        manager.subscribeTo(filterConfig.getFilterName(),config,xsdURL, handlerFactory, DestinationRouterConfiguration.class);
+        configurationService.subscribeTo(filterConfig.getFilterName(),"system-model.cfg.xml", handlerFactory, SystemModel.class);
+        configurationService.subscribeTo(filterConfig.getFilterName(), config, xsdURL, handlerFactory, DestinationRouterConfiguration.class);
     }
 }
