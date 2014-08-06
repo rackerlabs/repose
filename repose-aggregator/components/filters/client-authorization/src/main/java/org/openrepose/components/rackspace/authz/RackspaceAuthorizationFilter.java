1f07b4d93f21d8e0304083d5ab30681fe9bad708
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-06 14:12:33 -0500

    Springified the RackspaceAuthorizationFilter.

--- repose-aggregator/components/filters/client-authorization/src/main/java/org/openrepose/components/rackspace/authz/RackspaceAuthorizationFilter.java
+++ repose-aggregator/components/filters/client-authorization/src/main/java/org/openrepose/components/rackspace/authz/RackspaceAuthorizationFilter.java
@@ -2,26 +2,42 @@ package org.openrepose.components.rackspace.authz;
 
 import com.rackspace.papi.filter.FilterConfigHelper;
 import com.rackspace.papi.filter.logic.impl.FilterLogicHandlerDelegate;
-import org.openrepose.core.service.config.ConfigurationService;
-import com.rackspace.papi.service.context.ContextAdapter;
-import com.rackspace.papi.service.context.ServletContextHelper;
-import com.rackspace.papi.components.datastore.Datastore;
 import com.rackspace.papi.service.datastore.DatastoreService;
+import com.rackspace.papi.service.httpclient.HttpClientService;
+import com.rackspace.papi.service.serviceclient.akka.AkkaServiceClient;
 import org.openrepose.components.authz.rackspace.config.RackspaceAuthorization;
+import org.openrepose.core.service.config.ConfigurationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.*;
 import java.io.IOException;
 import java.net.URL;
 
+@Named
 public class RackspaceAuthorizationFilter implements Filter {
 
     private static final Logger LOG = LoggerFactory.getLogger(RackspaceAuthorizationFilter.class);
     private static final String DEFAULT_CONFIG = "openstack-authorization.cfg.xml";
     private String config;
     private RequestAuthorizationHandlerFactory handlerFactory;
-    private ConfigurationService configurationService;
+    private final ConfigurationService configurationService;
+    private final DatastoreService datastoreService;
+    private final HttpClientService httpClientService;
+    private final AkkaServiceClient akkaServiceClient;
+
+    @Inject
+    public RackspaceAuthorizationFilter(ConfigurationService configurationService,
+                                        DatastoreService datastoreService,
+                                        HttpClientService httpClientService,
+                                        AkkaServiceClient akkaServiceClient) {
+        this.configurationService = configurationService;
+        this.datastoreService = datastoreService;
+        this.httpClientService = httpClientService;
+        this.akkaServiceClient = akkaServiceClient;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -35,15 +51,9 @@ public class RackspaceAuthorizationFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        final ContextAdapter ctx = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext();
-        final DatastoreService datastoreService = ctx.datastoreService();
-        final Datastore defaultLocal = datastoreService.getDefaultDatastore();
-
-        configurationService = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
-        handlerFactory = new RequestAuthorizationHandlerFactory(defaultLocal,
-                ctx.httpConnectionPoolService(), ctx.akkaServiceClientService());
+        handlerFactory = new RequestAuthorizationHandlerFactory(datastoreService.getDefaultDatastore(), httpClientService, akkaServiceClient);
         URL xsdURL = getClass().getResource("/META-INF/schema/config/openstack-authorization-configuration.xsd");
         configurationService.subscribeTo(filterConfig.getFilterName(),config,xsdURL, handlerFactory, RackspaceAuthorization.class);
     }
