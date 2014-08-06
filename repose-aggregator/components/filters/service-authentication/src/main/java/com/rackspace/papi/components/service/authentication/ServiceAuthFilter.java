46d232df7534d35f3ac39ef783dec8e34b6b4c13
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-05 19:00:22 -0500

    Springified the ServiceAuthFilter.

--- repose-aggregator/components/filters/service-authentication/src/main/java/com/rackspace/papi/components/service/authentication/ServiceAuthFilter.java
+++ repose-aggregator/components/filters/service-authentication/src/main/java/com/rackspace/papi/components/service/authentication/ServiceAuthFilter.java
@@ -4,19 +4,27 @@ package com.rackspace.papi.components.service.authentication;
 import com.rackspace.papi.filter.FilterConfigHelper;
 import com.rackspace.papi.filter.logic.impl.FilterLogicHandlerDelegate;
 import org.openrepose.core.service.config.ConfigurationService;
-import com.rackspace.papi.service.context.ServletContextHelper;
-import java.io.IOException;
-import java.net.URL;
-import javax.servlet.*;
 import org.slf4j.Logger;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.*;
+import java.io.IOException;
+import java.net.URL;
+
+@Named
 public class ServiceAuthFilter implements Filter {
 
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(ServiceAuthFilter.class);
     private static final String DEFAULT_CONFIG = "service-authentication.cfg.xml";
     private String config;
     private ServiceAuthHandlerFactory handlerFactory;
-    private ConfigurationService configurationManager;
+    private final ConfigurationService configurationService;
+
+    @Inject
+    public ServiceAuthFilter(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -25,16 +33,15 @@ public class ServiceAuthFilter implements Filter {
 
     @Override
     public void destroy() {
-        configurationManager.unsubscribeFrom(config, handlerFactory);
+        configurationService.unsubscribeFrom(config, handlerFactory);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
-        configurationManager = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
         handlerFactory = new ServiceAuthHandlerFactory();
         URL xsdURL = getClass().getResource("/META-INF/schema/config/service-auth-configuration.xsd");
-        configurationManager.subscribeTo(filterConfig.getFilterName(),config,xsdURL, handlerFactory, ServiceAuthenticationConfig.class);
+        configurationService.subscribeTo(filterConfig.getFilterName(), config, xsdURL, handlerFactory, ServiceAuthenticationConfig.class);
     }
 }
