1f0aaffa5304102683bd6505395c1a35eda93ad4
Damien Johnson <damien.johnson@rackspace.com>
2014-08-11 17:07:13 -0500

    Updated tests for the handler and handler factory classes

--- repose-aggregator/components/filters/keystone-v3/src/test/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3FilterTest.scala
+++
@@ -1,7 +0,0 @@
-package com.rackspace.papi.components.keystone.v3
-
-import org.scalatest.FunSuite
-
-class KeystoneV3FilterTest extends FunSuite {
-
-}
--- repose-aggregator/components/filters/keystone-v3/src/test/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3HandlerFactoryTest.scala
+++ repose-aggregator/components/filters/keystone-v3/src/test/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3HandlerFactoryTest.scala
@@ -1,7 +1,40 @@
 package com.rackspace.papi.components.keystone.v3
 
-import org.scalatest.FunSuite
+import com.rackspace.papi.components.keystone.v3.config.{OpenstackKeystoneService, KeystoneV3Config}
+import com.rackspace.papi.service.datastore.DatastoreService
+import com.rackspace.papi.service.httpclient.HttpClientService
+import com.rackspace.papi.service.serviceclient.akka.AkkaServiceClient
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
 
-class KeystoneV3HandlerFactoryTest extends FunSuite {
+@RunWith(classOf[JUnitRunner])
+class KeystoneV3HandlerFactoryTest extends FunSpec with BeforeAndAfter with Matchers with MockitoSugar {
 
+    var handlerFactory: KeystoneV3HandlerFactory = _
+
+    val mockAkkaServiceClient = mock[AkkaServiceClient]
+    val mockDatastoreService = mock[DatastoreService]
+    val mockConnectionPoolService = mock[HttpClientService[_, _]]
+
+    before {
+        handlerFactory = new KeystoneV3HandlerFactory(mockAkkaServiceClient, mockDatastoreService, mockConnectionPoolService)
+    }
+
+    describe("buildHandler") {
+        it("should return a Keystone v3 handler") {
+            handlerFactory.configurationUpdated(new KeystoneV3Config())
+            handlerFactory.buildHandler shouldBe a[KeystoneV3Handler]
+        }
+    }
+
+    describe("getListeners") {
+        it("should return a map of listeners one of which listens to the keystone configuration file") {
+            val listeners = handlerFactory.getListeners
+
+            listeners should have size 1
+            listeners should contain key classOf[KeystoneV3Config]
+        }
+    }
 }
