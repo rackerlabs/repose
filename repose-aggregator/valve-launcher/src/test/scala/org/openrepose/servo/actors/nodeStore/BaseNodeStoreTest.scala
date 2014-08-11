c9f3cfe2676fed7cd538c07a6b979878bb3aa606
David Kowis <david.kowis@rackspace.com>
2014-08-11 13:40:56 -0500

    A pile of refactoring on the tests to clean up cross talk

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/nodeStore/BaseNodeStoreTest.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/nodeStore/BaseNodeStoreTest.scala
@@ -1,5 +1,6 @@
 package org.openrepose.servo.actors.nodeStore
 
+import akka.actor.ActorRef
 import akka.testkit.TestKit
 import org.openrepose.servo.ReposeNode
 import org.openrepose.servo.actors.TestReposeNodeActor
@@ -8,13 +9,15 @@ trait BaseNodeStoreTest {
   this:TestKit =>
 
   lazy val nodeList1 = List(ReposeNode("repose", "repose_node1", "localhost", httpPort = Some(8080), httpsPort = None))
+
   lazy val nodeList2 = List(
     ReposeNode("repose", "repose_node1", "localhost", httpPort = Some(8080), httpsPort = None),
     ReposeNode("repose", "repose_node2", "localhost", httpPort = Some(8081), httpsPort = None)
   )
+
   lazy val nodeList3 = List(ReposeNode("repose", "repose_node2", "localhost", httpPort = Some(8081), httpsPort = None))
 
   //Create a props that the other actor can use to "start stuff"
-  val testStartActorProps = TestReposeNodeActor.props(testActor)
+  def testStartActorProps(probe:ActorRef) = TestReposeNodeActor.props(probe)
 
 }
