9e5a34f7afdcffa7dbfc1f4518f76d63c64f6e15
Damien Johnson <damien.johnson@rackspace.com>
2014-08-11 17:05:29 -0500

    Modified the ServiceClient to allow for variable accept MediaTypes, modified ServiceClientResponse to hold response headers

--- repose-aggregator/services/service-client/impl/src/test/groovy/com/rackspace/papi/service/serviceclient/akka/AkkaServiceClientImplTest.groovy
+++ repose-aggregator/services/service-client/impl/src/test/groovy/com/rackspace/papi/service/serviceclient/akka/AkkaServiceClientImplTest.groovy
@@ -1,4 +1,5 @@
 package com.rackspace.papi.service.serviceclient.akka
+
 import com.rackspace.papi.commons.util.http.ServiceClient
 import com.rackspace.papi.commons.util.http.ServiceClientResponse
 import com.rackspace.papi.service.httpclient.HttpClientResponse
@@ -8,108 +9,107 @@ import org.apache.http.HttpEntity
 import org.apache.http.HttpResponse
 import org.apache.http.StatusLine
 import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpRequestBase
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentCaptor
 
 import javax.ws.rs.core.MediaType
 
+import static org.hamcrest.CoreMatchers.equalTo
+import static org.hamcrest.MatcherAssert.assertThat
 import static org.junit.Assert.assertEquals
 import static org.mockito.Matchers.*
 import static org.mockito.Mockito.*
 
 class AkkaServiceClientImplTest {
 
+    private static final String AUTH_TOKEN_HEADER = "X-Auth-Token"
+    private static final String ACCEPT_HEADER = "Accept"
 
-
-    private AkkaServiceClientImpl akkaServiceClientImpl;
-    private String tenant;
-    private String userToken;
-    private String targetHostUri;
-    ServiceClientResponse<String> serviceClientResponseGet, serviceClientResponsePost;
-    HttpClientService httpClientService;
-    ServiceClient serviceClient;
+    private AkkaServiceClientImpl akkaServiceClientImpl
+    private String userToken
+    private String adminToken
+    private String targetHostUri
+    ArgumentCaptor<HttpRequestBase> requestCaptor
+    HttpClientService httpClientService
     String returnString = "getinput"
     HttpClient httpClient
 
     @Before
     public void setUp() {
+        httpClientService = mock(HttpClientService.class)
 
+        HttpClientResponse httpClientResponse = mock(HttpClientResponse.class)
 
-        serviceClientResponseGet = new ServiceClientResponse(200,new ByteArrayInputStream(returnString.getBytes("UTF-8")));
-        httpClientService  = mock(HttpClientService.class) ;
+        when(httpClientService.getMaxConnections(anyString())).thenReturn(20)
+        when(httpClientService.getClient(anyString())).thenReturn(httpClientResponse)
 
-        HttpClientResponse httpClientResponse =mock(HttpClientResponse.class)
-
-        when(httpClientService.getMaxConnections(anyString())).thenReturn(20);
-        when(httpClientService.getClient(anyString())).thenReturn(httpClientResponse);
-
-        httpClient = mock(HttpClient.class);
-        when(httpClientResponse.getHttpClient()).thenReturn(httpClient);
+        httpClient = mock(HttpClient.class)
+        when(httpClientResponse.getHttpClient()).thenReturn(httpClient)
 
         HttpResponse httpResponse = mock(HttpResponse.class)
-        when(httpClient.execute(anyObject())).thenReturn(httpResponse);
+        requestCaptor = ArgumentCaptor.forClass(HttpRequestBase.class)
+        when(httpClient.execute(requestCaptor.capture())).thenReturn(httpResponse)
 
         HttpEntity entity = mock(HttpEntity.class)
-        when(httpResponse.getEntity()).thenReturn(entity);
-        when(entity.getContent()).thenReturn(new ByteArrayInputStream(returnString.getBytes("UTF-8")));
+        when(httpResponse.getEntity()).thenReturn(entity)
+        when(entity.getContent()).thenReturn(new ByteArrayInputStream(returnString.getBytes("UTF-8")))
 
         StatusLine statusLine = mock(StatusLine.class)
         when(statusLine.getStatusCode()).thenReturn(200)
-        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(httpResponse.getStatusLine()).thenReturn(statusLine)
 
-        serviceClient = mock(ServiceClient.class);
-        when(serviceClient.get(anyString(), any(Map.class)))
-                .thenReturn(serviceClientResponseGet);
-        when(serviceClient.getPoolSize()).thenReturn(100)
-
-        akkaServiceClientImpl = new AkkaServiceClientImpl(httpClientService);
-        userToken = "userToken";
-        targetHostUri = "targetHostUri";
+        akkaServiceClientImpl = new AkkaServiceClientImpl(httpClientService)
+        userToken = "userToken"
+        adminToken = "adminToken"
+        targetHostUri = "targetHostUri"
     }
 
     @Test
     public void testValidateToken() {
-        final String AUTH_TOKEN_HEADER = "X-Auth-Token";
-        final String ACCEPT_HEADER = "Accept";
-        final Map<String, String> headers = new HashMap<String, String>();
-        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML);
-        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token");
-        ServiceClientResponse serviceClientResponse = akkaServiceClientImpl.get(userToken, targetHostUri,  headers );
-        org.junit.Assert.assertEquals("Should retrive service client with response", serviceClientResponse.getStatusCode(), 200);
+        final Map<String, String> headers = new HashMap<String, String>()
+        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML)
+        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token")
+        ServiceClientResponse serviceClientResponse = akkaServiceClientImpl.get(userToken, targetHostUri, headers)
+        org.junit.Assert.assertEquals("Should retrieve service client with response", serviceClientResponse.getStatusCode(), 200)
+    }
+
+    @Test
+    public void testGetAdminToken() {
+        akkaServiceClientImpl.post(adminToken, targetHostUri, new HashMap(), "", MediaType.APPLICATION_JSON_TYPE, MediaType.APPLICATION_JSON_TYPE)
+        assertThat(requestCaptor.value.getFirstHeader("Content-Type").value, equalTo(MediaType.APPLICATION_JSON))
+        assertThat(requestCaptor.value.getFirstHeader("Accept").value, equalTo(MediaType.APPLICATION_JSON))
     }
 
     @Test
     public void shouldExpireItemInFutureMap() {
-        final String AUTH_TOKEN_HEADER = "X-Auth-Token";
-        final String ACCEPT_HEADER = "Accept";
-        final Map<String, String> headers = new HashMap<String, String>();
-        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML);
-        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token");
-        akkaServiceClientImpl.get(userToken, targetHostUri,  headers );
+        final Map<String, String> headers = new HashMap<String, String>()
+        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML)
+        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token")
+        akkaServiceClientImpl.get(userToken, targetHostUri, headers)
 
         Thread.sleep(500)
 
-        akkaServiceClientImpl.get(userToken, targetHostUri,  headers );
+        akkaServiceClientImpl.get(userToken, targetHostUri, headers)
 
         verify(httpClient, times(2)).execute(anyObject())
     }
 
     @Test
     public void testServiceResponseReusable() {
-        final String AUTH_TOKEN_HEADER = "X-Auth-Token";
-        final String ACCEPT_HEADER = "Accept";
-        final Map<String, String> headers = new HashMap<String, String>();
-        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML);
-        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token");
-        ServiceClientResponse serviceClientResponse1 = akkaServiceClientImpl.get(userToken, targetHostUri,  headers );
-        ServiceClientResponse serviceClientResponse2 = akkaServiceClientImpl.get(userToken, targetHostUri,  headers );
+        final Map<String, String> headers = new HashMap<String, String>()
+        ((HashMap<String, String>) headers).put(ACCEPT_HEADER, MediaType.APPLICATION_XML)
+        ((HashMap<String, String>) headers).put(AUTH_TOKEN_HEADER, "admin token")
+        ServiceClientResponse serviceClientResponse1 = akkaServiceClientImpl.get(userToken, targetHostUri, headers)
+        ServiceClientResponse serviceClientResponse2 = akkaServiceClientImpl.get(userToken, targetHostUri, headers)
 
-        StringWriter writer1 = new StringWriter();
-        IOUtils.copy(serviceClientResponse1.data, writer1, "UTF-8");
-        String returnString1 = writer1.toString();
+        StringWriter writer1 = new StringWriter()
+        IOUtils.copy(serviceClientResponse1.data, writer1, "UTF-8")
+        String returnString1 = writer1.toString()
 
         StringWriter writer2 = new StringWriter()
-        IOUtils.copy(serviceClientResponse2.data, writer2, "UTF-8");
+        IOUtils.copy(serviceClientResponse2.data, writer2, "UTF-8")
         String returnString2 = writer2.toString()
 
         assertEquals(returnString1, returnString2)
