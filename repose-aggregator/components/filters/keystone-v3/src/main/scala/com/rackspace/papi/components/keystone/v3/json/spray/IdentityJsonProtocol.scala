cd2408e141d63ac74b9fdf5134cb6bf4d34ae024
Damien Johnson <damien.johnson@rackspace.com>
2014-08-11 17:05:56 -0500

    Added implicit conversions for identity objects

--- repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/json/spray/IdentityJsonProtocol.scala
+++ repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/json/spray/IdentityJsonProtocol.scala
@@ -1,9 +1,27 @@
 package com.rackspace.papi.components.keystone.v3.json.spray
 
-import com.rackspace.papi.components.keystone.v3.objects.{AuthRequest, AuthResponse}
+import com.rackspace.papi.components.keystone.v3.objects._
 import spray.json.DefaultJsonProtocol
 
 object IdentityJsonProtocol extends DefaultJsonProtocol {
-  implicit val authResponseFormat = jsonFormat1(AuthResponse)
-  implicit val authRequestFormat = jsonFormat2(AuthRequest)
+    implicit val endpointTypeFormat = jsonFormat6(EndpointType)
+    implicit val endpointsTypeFormat = jsonFormat1(EndpointsType)
+    implicit val serviceForAuthenticationResponse = jsonFormat3(ServiceForAuthenticationResponse)
+    implicit val catalogTypeFormat = jsonFormat1(CatalogType)
+    implicit val domainScopeFormat = jsonFormat2(DomainScopeType)
+    implicit val domainsForAuthenticateResponse = jsonFormat3(DomainsForAuthenticateResponse)
+    implicit val domainTypeFormat = jsonFormat3(DomainType)
+    implicit val userNamePasswordRequestFormat = jsonFormat4(UserNamePasswordRequest)
+    implicit val passwordCredentialsFormat = jsonFormat1(PasswordCredentials)
+    implicit val projectForAuthenticateResponseFormat = jsonFormat4(ProjectForAuthenticateResponse)
+    implicit val projectScopeTypeFormat = jsonFormat3(ProjectScopeType)
+    implicit val roleFormat = jsonFormat5(Role)
+    implicit val trustScopeTypeFormat = jsonFormat1(TrustScopeType)
+    implicit val scopeTypeFormat = jsonFormat3(ScopeType)
+    implicit val tokenCredentialsFormat = jsonFormat1(TokenCredentials)
+    implicit val userForAuthenticateResponse = jsonFormat5(UserForAuthenticateResponse)
+    implicit val authenticateResponseFormat = jsonFormat8(AuthenticateResponse)
+    implicit val authIdentityRequestFormat = jsonFormat3(AuthIdentityRequest)
+    implicit val authRequestFormat = jsonFormat2(AuthRequest)
+    implicit val authResponseFormat = jsonFormat1(AuthResponse)
 }
