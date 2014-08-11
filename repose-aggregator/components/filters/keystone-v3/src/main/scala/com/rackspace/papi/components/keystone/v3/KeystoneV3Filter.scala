e3e10520d547c613ae26a3f779fd287c44a2dbae
Damien Johnson <damien.johnson@rackspace.com>
2014-08-11 17:07:54 -0500

    Added code for the Keystone filter, handler factory, and handler

--- repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3Filter.scala
+++ repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3Filter.scala
@@ -1,18 +1,42 @@
 package com.rackspace.papi.components.keystone.v3
 
+import java.net.URL
 import javax.servlet._
+
+import com.rackspace.papi.commons.config.manager.UpdateListener
+import com.rackspace.papi.components.keystone.v3.config.KeystoneV3Config
+import com.rackspace.papi.filter.FilterConfigHelper
+import com.rackspace.papi.filter.logic.impl.FilterLogicHandlerDelegate
+import com.rackspace.papi.service.config.ConfigurationService
+import com.rackspace.papi.service.context.ServletContextHelper
+import org.slf4j.LoggerFactory
 
 class KeystoneV3Filter extends Filter {
 
-  override def init(filterConfig: FilterConfig) = {
+    private final val LOG = LoggerFactory.getLogger(classOf[KeystoneV3Filter])
+    private final val DEFAULT_CONFIG = "keystone-v3.cfg.xml"
 
-  }
+    private var config: String = _
+    private var handlerFactory: KeystoneV3HandlerFactory = _
+    private var configurationManager: ConfigurationService = _
 
-  override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain) = {
+    override def init(filterConfig: FilterConfig) {
+        config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
+        LOG.info("Initializing filter using config " + config)
+        val powerApiContext = ServletContextHelper.getInstance(filterConfig.getServletContext).getPowerApiContext
+        configurationManager = powerApiContext.configurationService
+        // TODO: These services are passed in to support asynchronous requests, caching, and connection pooling (in the future!)
+        handlerFactory = new KeystoneV3HandlerFactory(powerApiContext.akkaServiceClientService, powerApiContext.datastoreService, powerApiContext.httpConnectionPoolService)
+        val xsdURL: URL = getClass.getResource("/META-INF/config/schema/keystone-v3.xsd")
+        // TODO: Clean up the asInstanceOf below, if possible?
+        configurationManager.subscribeTo(filterConfig.getFilterName, config, xsdURL, handlerFactory.asInstanceOf[UpdateListener[KeystoneV3Config]], classOf[KeystoneV3Config])
+    }
 
-  }
+    override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain) {
+        new FilterLogicHandlerDelegate(servletRequest, servletResponse, filterChain).doFilter(handlerFactory.newHandler)
+    }
 
-  override def destroy() = {
-
-  }
+    override def destroy() {
+        configurationManager.unsubscribeFrom(config, handlerFactory)
+    }
 }
