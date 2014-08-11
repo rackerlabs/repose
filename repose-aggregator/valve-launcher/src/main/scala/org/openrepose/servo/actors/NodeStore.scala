8df5fdaf3d48884da3a0fbcd12604b4402e3e835
David Kowis <david.kowis@rackspace.com>
2014-08-11 13:51:22 -0500

    Removing println noise

--- repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/actors/NodeStore.scala
+++ repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/actors/NodeStore.scala
@@ -1,10 +1,10 @@
 package org.openrepose.servo.actors
 
+import java.util.UUID
+
 import akka.actor.{PoisonPill, Props, ActorRef, Actor}
 import org.openrepose.servo.ReposeNode
 import org.openrepose.servo.actors.NodeStoreMessages.Initialize
-
-import scala.collection.mutable
 
 //A bit of logic to give me a mutable map
 //List exists as MutableList, don't know why map doesn't
@@ -37,12 +37,12 @@ class NodeStore(startActor: Props) extends Actor {
     case list: List[ReposeNode] => {
       //Figure out what nodes we need to stop
       // Stuff that's not in the sent list should be stopped!
-      val stopList = runningNodes.filterNot(n => list.contains(n)).map(_._2)
+      val stopList = runningNodes.filterNot(n => list.contains(n._2)).map(_._2)
 
       //Figure out what nodes we need to start
       val startList = list.filterNot(n => runningNodes.contains(nodeKey(n)))
 
-      stopList.foreach( n => {
+      stopList.foreach(n => {
         val nk = nodeKey(n)
         //Kill the children!
         childActors(nk) ! PoisonPill
@@ -51,8 +51,9 @@ class NodeStore(startActor: Props) extends Actor {
       })
 
       startList.foreach(n => {
+        val uuid = UUID.randomUUID.toString
         val nk = nodeKey(n)
-        val actor = context.system.actorOf(startActor)
+        val actor = context.actorOf(startActor, s"${n.clusterId}_${n.nodeId}_runner_${uuid}")
         actor ! Initialize(n.clusterId, n.nodeId)
 
         //Persist our jank
