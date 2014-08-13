42ccbd7a63c39b9ff337e3793d3c7344b5343dc9
David Kowis <david.kowis@rackspace.com>
2014-08-13 14:04:38 -0500

    Makin tests, makin tests pass.
    Scala is teh bestest

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/ReposeLauncherTest.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/ReposeLauncherTest.scala
@@ -1,9 +1,10 @@
 package org.openrepose.servo.actors
 
-import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.actor.{Terminated, ActorSystem}
+import akka.testkit.{EventFilter, TestProbe, TestKit}
 import com.typesafe.config.ConfigFactory
 import org.junit.runner.RunWith
+import org.openrepose.servo.actors.NodeStoreMessages.Initialize
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpecLike, Matchers, BeforeAndAfterAll}
 
@@ -25,12 +26,44 @@ with FunSpecLike with Matchers with BeforeAndAfterAll {
 
   describe("The Repose Launcher") {
     it("will execute the command and remain running until the command exits"){
-      pending
-    }
-    it("sets environment variables that are passed to it") {
-      pending
+
+      //Create a test probe to watch for DEATH
+      val probe = TestProbe()
+
+      //create an actor with that command
+      val props = ReposeLauncher.props(List("bash", "-c", "sleep 1"))
+
+      //Won't actually start until the initialize is sent
+      val actor = system.actorOf(props)
+      probe.watch(actor)
+
+      actor ! Initialize("testCluster", "testNode")
+
+      probe.expectMsgPF(3 seconds) {
+        case Terminated(theActor) => {
+          theActor should equal(actor)
+        }
+      }
     }
     it("will log standard out to info") {
+      val probe = TestProbe()
+      val props = ReposeLauncher.props(Seq("bash", "-c", "echo 'lololol'"))
+
+      val actor = system.actorOf(props)
+
+      EventFilter.info(pattern = ".*lololol.*", occurrences = 1) intercept {
+        actor ! Initialize("testCluster", "testNode")
+      }
+    }
+    it("automatically sets the environment variable CONFIG_ROOT") {
+      val probe = TestProbe()
+      val props = ReposeLauncher.props(List("bash", "-c", "echo $CONFIG_ROOT"))
+      pending
+    }
+    it("automatically sets the environment variables NODE_ID and CLUSTER_ID") {
+      pending
+    }
+    it("sets any other environment variables that are passed to it") {
       pending
     }
     it("will log standard error out to warn") {
