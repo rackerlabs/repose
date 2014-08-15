b9fa301f3ee5a74a54b01549ca546d4df272ec98
Damien Johnson <damien.johnson@rackspace.com>
2014-08-15 09:42:28 -0500

    Got Handler to compile, moved logic for createAdminAuthRequest, added tests

--- repose-aggregator/components/filters/keystone-v3/src/test/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3HandlerTest.scala
+++ repose-aggregator/components/filters/keystone-v3/src/test/scala/com/rackspace/papi/components/keystone/v3/KeystoneV3HandlerTest.scala
@@ -1,28 +1,40 @@
 package com.rackspace.papi.components.keystone.v3
 
+import javax.ws.rs.core.MediaType
+
 import com.mockrunner.mock.web.MockHttpServletRequest
-import com.rackspace.papi.commons.util.http.HttpStatusCode
-import com.rackspace.papi.components.keystone.v3.config.{OpenstackKeystoneService, KeystoneV3Config}
+import com.rackspace.papi.commons.util.http.{HttpStatusCode, ServiceClientResponse}
+import com.rackspace.papi.components.keystone.v3.config.{KeystoneV3Config, OpenstackKeystoneService}
+import com.rackspace.papi.components.keystone.v3.utilities.KeystoneAuthException
 import com.rackspace.papi.filter.logic.{FilterAction, FilterDirector}
 import com.rackspace.papi.service.datastore.DatastoreService
-import com.rackspace.papi.service.httpclient.HttpClientService
+import com.rackspace.papi.service.httpclient.{HttpClientResponse, HttpClientService}
 import com.rackspace.papi.service.serviceclient.akka.AkkaServiceClient
+import org.apache.http.Header
 import org.junit.runner.RunWith
+import org.mockito.Matchers.{any, anyMap, anyString}
+import org.mockito.Mockito.when
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{BeforeAndAfter, FunSpec, Matchers, PrivateMethodTester}
+
+import scala.util.{Failure, Success, Try}
 
 @RunWith(classOf[JUnitRunner])
 class KeystoneV3HandlerTest extends FunSpec with BeforeAndAfter with Matchers with PrivateMethodTester with MockitoSugar {
 
     var keystoneV3Handler: KeystoneV3Handler = _
-
-    val mockAkkaServiceClient = mock[AkkaServiceClient]
-    val mockDatastoreService = mock[DatastoreService]
-    val mockConnectionPoolService = mock[HttpClientService[_, _]]
+    var keystoneConfig: KeystoneV3Config = _
+    var mockAkkaServiceClient: AkkaServiceClient = _
+    var mockDatastoreService: DatastoreService = _
+    var mockConnectionPoolService: HttpClientService[_, _ <: HttpClientResponse] = _
 
     before {
-        keystoneV3Handler = new KeystoneV3Handler(new KeystoneV3Config(), mockAkkaServiceClient, mockDatastoreService, mockConnectionPoolService)
+        mockAkkaServiceClient = mock[AkkaServiceClient]
+        mockDatastoreService = mock[DatastoreService]
+        mockConnectionPoolService = mock[HttpClientService[_, _ <: HttpClientResponse]]
+        keystoneConfig = new KeystoneV3Config()
+        keystoneV3Handler = new KeystoneV3Handler(keystoneConfig, mockAkkaServiceClient, mockDatastoreService, mockConnectionPoolService)
     }
 
     describe("handleRequest")(pending)
@@ -60,22 +72,63 @@ class KeystoneV3HandlerTest extends FunSpec with BeforeAndAfter with Matchers wi
     }
 
     describe("fetchAdminToken") {
-        it("should throw an exception when unable to retrieve admin token")(pending)
+        it("should return a Failure when unable to retrieve admin token") {
+            val fetchAdminToken = PrivateMethod[Try[String]]('fetchAdminToken)
+            val mockServiceClientResponse = mock[ServiceClientResponse[Object]]
+
+            keystoneConfig.setKeystoneService(new OpenstackKeystoneService())
+            keystoneConfig.getKeystoneService.setUsername("user")
+            keystoneConfig.getKeystoneService.setPassword("password")
+
+            when(mockServiceClientResponse.getStatusCode).thenReturn(HttpStatusCode.UNAUTHORIZED.intValue)
+            when(mockAkkaServiceClient.post(anyString, anyString, anyMap.asInstanceOf[java.util.Map[String, String]], anyString, any(classOf[MediaType]), any(classOf[MediaType]))).
+                    thenReturn(mockServiceClientResponse, Nil: _*) // Note: Nil was passed to resolve the ambiguity between Mockito's multiple method signatures
+
+            keystoneV3Handler invokePrivate fetchAdminToken() shouldBe a[Failure[_]]
+            keystoneV3Handler.invokePrivate(fetchAdminToken()).failed.get shouldBe a[KeystoneAuthException]
+        }
+
+        it("should return an admin token as a string when the admin API call succeeds") {
+            val fetchAdminToken = PrivateMethod[Try[String]]('fetchAdminToken)
+            val mockServiceClientResponse = mock[ServiceClientResponse[Object]]
+            val mockHeader = mock[Header]
+
+            keystoneConfig.setKeystoneService(new OpenstackKeystoneService())
+            keystoneConfig.getKeystoneService.setUsername("user")
+            keystoneConfig.getKeystoneService.setPassword("password")
+
+            when(mockHeader.getName).thenReturn("X-Subject-Token", Nil: _*)
+            when(mockHeader.getValue).thenReturn("test-admin-token", Nil: _*)
+            when(mockServiceClientResponse.getStatusCode).thenReturn(HttpStatusCode.OK.intValue)
+            when(mockServiceClientResponse.getHeaders).thenReturn(Array(mockHeader), Nil: _*)
+            when(mockAkkaServiceClient.post(anyString, anyString, anyMap.asInstanceOf[java.util.Map[String, String]], anyString, any(classOf[MediaType]), any(classOf[MediaType]))).
+                    thenReturn(mockServiceClientResponse, Nil: _*) // Note: Nil was passed to resolve the ambiguity between Mockito's multiple method signatures
+
+            keystoneV3Handler invokePrivate fetchAdminToken() shouldBe a[Success[_]]
+            keystoneV3Handler.invokePrivate(fetchAdminToken()).get should startWith("test-admin-token")
+        }
     }
 
     describe("createAdminAuthRequest") {
         it("should build a JSON auth token request without a domain ID") {
-//            val createAdminAuthRequest = PrivateMethod[String]('createAdminAuthRequest)
-//            keystoneV3Handler invokePrivate createAdminAuthRequest("user", "password") should equal ("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"user\",\"password\":\"password\"}}}}}")
-            keystoneV3Handler.createAdminAuthRequest("user", "password") should equal ("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"user\",\"password\":\"password\"}}}}}")
-        }
+            val createAdminAuthRequest = PrivateMethod[String]('createAdminAuthRequest)
 
-        it("should build a JSON auth token request with a none domain ID") {
-            keystoneV3Handler.createAdminAuthRequest("user", "password", None) should equal ("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"user\",\"password\":\"password\"}}}}}")
+            keystoneConfig.setKeystoneService(new OpenstackKeystoneService())
+            keystoneConfig.getKeystoneService.setUsername("user")
+            keystoneConfig.getKeystoneService.setPassword("password")
+
+            keystoneV3Handler invokePrivate createAdminAuthRequest() should equal("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"user\",\"password\":\"password\"}}}}}")
         }
 
         it("should build a JSON auth token request with a string domain ID") {
-            keystoneV3Handler.createAdminAuthRequest("user", "password", Option("domainId")) should equal ("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"domain\":{\"id\":\"domainId\"},\"name\":\"user\",\"password\":\"password\"}}}}}")
+            val createAdminAuthRequest = PrivateMethod[String]('createAdminAuthRequest)
+
+            keystoneConfig.setKeystoneService(new OpenstackKeystoneService())
+            keystoneConfig.getKeystoneService.setUsername("user")
+            keystoneConfig.getKeystoneService.setPassword("password")
+            keystoneConfig.getKeystoneService.setDomainId("domainId")
+
+            keystoneV3Handler invokePrivate createAdminAuthRequest() should equal("{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"domain\":{\"id\":\"domainId\"},\"name\":\"user\",\"password\":\"password\"}}}}}")
         }
     }
 }
