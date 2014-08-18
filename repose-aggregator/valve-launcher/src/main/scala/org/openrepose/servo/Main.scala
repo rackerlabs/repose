685b20bbc5dfaee4f8b4a2fe5eb280e23252e1d0
David Kowis <david.kowis@rackspace.com>
2014-08-18 14:46:46 -0500

    Pulled the configuration out a level.
    Main will pass it in, this way I can make a modified config during
    testing as well

--- repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/Main.scala
+++ repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/Main.scala
@@ -1,11 +1,15 @@
 package org.openrepose.servo
 
+import com.typesafe.config.ConfigFactory
+
 /**
  * Just a thin wrapper around the actual Servo. This is the application entry point
  * Servo.scala does all the actual work
  */
 object Main extends App {
-  val exitCode = Servo.execute(args, System.in, System.out, System.err)
+
+  val config = ConfigFactory.load()
+  val exitCode = Servo.execute(args, System.in, System.out, System.err, config)
   //Should only get here if it's done (like that method blocks all this forever)
   sys.exit(exitCode)
 }
