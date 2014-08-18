e876cc683f375648f90b79a914115bdf389bb978
David Kowis <david.kowis@rackspace.com>
2014-08-18 14:47:35 -0500

    Tweaked the ServoTest and got it lined up for a failing test.

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/ServoTest.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/ServoTest.scala
@@ -1,19 +1,23 @@
 package org.openrepose.servo
 
 import java.io.{ByteArrayOutputStream, File, PrintStream}
-import java.nio.file.Files
+import java.nio.charset.StandardCharsets
+import java.nio.file.{StandardOpenOption, Paths, Files}
 
 import com.typesafe.config.ConfigFactory
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
+import scala.concurrent.{ExecutionContext, Future}
+import scala.io.Source
+
 @RunWith(classOf[JUnitRunner])
-class ServoTest extends FunSpec with Matchers {
+class ServoTest extends FunSpec with Matchers with TestUtils {
 
-  val config = ConfigFactory.load()
+  val defaultConfig = ConfigFactory.load()
 
-  val servoVersion = config.getString("version")
+  val servoVersion = defaultConfig.getString("version")
 
   def afterExecution(args: Array[String] = Array.empty[String], callback: (String, String, Int) => Unit) = {
     val stdout = new ByteArrayOutputStream()
@@ -21,7 +25,7 @@ class ServoTest extends FunSpec with Matchers {
     //Servo doesn't yet do anything with stdin, so we can ignore that here
     val stdin = System.in
 
-    val exitStatus = Servo.execute(args, stdin, new PrintStream(stdout), new PrintStream(stderr))
+    val exitStatus = Servo.execute(args, stdin, new PrintStream(stdout), new PrintStream(stderr), defaultConfig)
 
     val error = new String(stderr.toByteArray)
     val output = new String(stdout.toByteArray)
@@ -30,7 +34,7 @@ class ServoTest extends FunSpec with Matchers {
   }
 
   describe("Servo, the Repose Valve Launcher") {
-    it("prints out a usage message when given unknown parameters") {
+    it("prints out a usage message when given --help and exits 1") {
       afterExecution(Array("--help"), (output, error, exitStatus) => {
         output should include("Usage: java -jar servo.jar [options]")
         exitStatus should be(1)
@@ -38,13 +42,75 @@ class ServoTest extends FunSpec with Matchers {
     }
     it(s"prints out the version ($servoVersion) when given --version and exits 1") {
       afterExecution(Array("--version"), (output, error, exitStatus) => {
-        output should include(s"Servo: ${config.getString("version")}")
+        output should include(s"Servo: ${defaultConfig.getString("version")}")
         exitStatus should be(1)
       })
     }
-    it("outputs to stdout the settings it's going to use to start valves") {
-      //Needs a solid start up
-      pending
+
+    describe("For a good single node configuration") {
+      it("executes the command to start a Jetty with the Repose War on the specified port") {
+        //Create the 1-node system model
+        val configRoot = Files.createTempDirectory("servo").toString
+        val systemModel = resourceContent("/servoTesting/system-model-1.cfg.xml")
+        writeSystemModel(configRoot, systemModel)
+
+        //Create a bash script that outputs test data and just runs
+        val fakeRepose = resourceContent("/servoTesting/fakeRepose.sh")
+        val tmpBash = File.createTempFile("fakeRepose", ".sh").toPath
+        val tmpOutput = File.createTempFile("fakeRepose", ".out")
+        Files.write(tmpBash, fakeRepose.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE)
+
+        //Create a config object to merge in
+        val config = ConfigFactory.parseString(
+          s"""
+            |executionCommand = ${tmpBash.toFile.getAbsolutePath} ${tmpOutput.getAbsolutePath}
+          """.stripMargin).withFallback(defaultConfig)
+
+        info(s"Execution is: ${config.getString("executionCommand")}")
+
+        //Have to throw my executable into a thread so I can asynchronously do stuff
+        import ExecutionContext.Implicits.global
+        //Just somewhere to do work
+        val exitValue = Future {
+          //    val exitStatus = Servo.execute(args, stdin, new PrintStream(stdout), new PrintStream(stderr), defaultConfig)
+          Servo.execute(Array("--config-file", configRoot.toString), System.in, System.out, System.err, config)
+        }.onComplete { t =>
+          info("Completion of servo!")
+          t.isSuccess shouldBe true
+          t.get shouldBe 0
+        }
+
+        val lines = Source.fromFile(tmpOutput).getLines().toList
+        lines shouldNot be(empty)
+        //Check the arguments for what we need
+        lines.filter(_.startsWith("ARGS:")).map { l =>
+          //This should be the ARGS line
+          l should include("--port 8080")
+        }
+
+        //Check the environment variables for our cluster ID/node ID
+        lines.filter(_.startsWith("CLUSTER_ID")).map { l =>
+          l should be("CLUSTER_ID=repose")
+        }
+
+        lines.filter(_.startsWith("NODE_ID")).map { l =>
+          l should be("NODE_ID=repose_node1")
+        }
+
+        //tell servo to stop.
+        Servo.shutdown()
+
+
+      }
+      it("outputs to stdout the settings it's going to use to start valves") {
+        pending
+      }
+      it("passes through REPOSE_JVM_OPTS through as JVM_OPTS") {
+        pending
+      }
+      it("warns when JVM_OPTS are set") {
+        pending
+      }
     }
     describe("Failing to find nodes in the config file") {
       it("outputs a failure message and exits 1if it cannot find any local nodes to start") {
@@ -54,29 +120,10 @@ class ServoTest extends FunSpec with Matchers {
         pending
       }
     }
-    it("executes the command to start a Jetty with the Repose War on the specified port") {
-      pending
-    }
-    it("executes several commands if there are several local instances configured") {
-      pending
-    }
-    it("passes through REPOSE_JVM_OPTS through as JVM_OPTS") {
-      pending
-    }
-    it("warns when JVM_OPTS are set") {
-      pending
-    }
-    describe("watches the system-model.cfg.xml for changes to nodes") {
-      it("starts new valves when they're added") {
-        pending
-      }
-      it("stops orphaned valves when they're removed") {
-        pending
-      }
-      it("does not restart valves if the ordering changed") {
+    describe("for a good multi-local-node configuration") {
+      it("starts those multiple nodes") {
         pending
       }
     }
-
   }
 }
