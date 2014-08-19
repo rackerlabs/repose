84e253e35f31778b03b68f9542099b571017510f
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-19 15:48:54 -0500

    Moved the directory from /usr/share/lib/repose back to /usr/share/repose to be more inline with other installer packages. Updated the POM's to use the newly renamed xxx-repose-valve.conf file. Updated some User/Group names.

--- setup-repose.sh
+++ setup-repose.sh
@@ -8,12 +8,12 @@ else
 
   mvn -f "$DIR/pom.xml" -Dmaven.test.skip=true -DskipTests install
 
-  mkdir -p "$1/usr/share/lib/repose/filters"
+  mkdir -p "$1/usr/share/repose/filters"
   mkdir -p "$1/var/repose"
   mkdir -p "$1/var/log/repose"
   mkdir -p "$1/etc/repose"
 
-  for f in "$1"/usr/share/lib/repose/* "$1"/usr/share/lib/repose/filters/*
+  for f in "$1"/usr/share/repose/* "$1"/usr/share/repose/filters/*
   do
     if [ -f $f ]; then
       echo unlink $f
@@ -23,14 +23,14 @@ else
 
   for f in $DIR/repose-aggregator/components/filter-bundle/target/filter-bundle*.ear $DIR/repose-aggregator/extensions/extensions-filter-bundle/target/extensions-filter-bundle*.ear
   do
-    echo ln -s $f "$1/usr/share/lib/repose/filters/"
-    ln -s $f "$1/usr/share/lib/repose/filters/"
+    echo ln -s $f "$1/usr/share/repose/filters/"
+    ln -s $f "$1/usr/share/repose/filters/"
   done
 
   for f in $DIR/repose-aggregator/core/valve/target/repose-valve.jar
   do
-    echo ln -s $f "$1/usr/share/lib/repose/"
-    ln -s $f "$1/usr/share/lib/repose/"
+    echo ln -s $f "$1/usr/share/repose/"
+    ln -s $f "$1/usr/share/repose/"
   done
 
 fi
