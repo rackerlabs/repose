682ef0391d1e272b7da0c5c5c072b8d79f736177
Damien Johnson <damien.johnson@rackspace.com>
2014-08-20 15:14:44 -0500

    Removed Type from objects

--- repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/ProjectScope.scala
+++ repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/ProjectScope.scala
@@ -1,3 +1,3 @@
 package com.rackspace.papi.components.keystone.v3.objects
 
-case class ProjectScope(domain: Option[DomainScopeType] = None, id: String, name: Option[String] = None) extends Serializable
+case class ProjectScope(domain: Option[DomainScope] = None, id: String, name: Option[String] = None) extends Serializable
--- repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/ServiceForAuthenticationResponse.scala
+++ repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/ServiceForAuthenticationResponse.scala
@@ -1,4 +1,4 @@
 package com.rackspace.papi.components.keystone.v3.objects
 
 // TODO: Use jsonFormat to map type => openstackType
-case class ServiceForAuthenticationResponse(endpoints: Option[List[EndpointsType]] = None, openstackType: String, id: Option[String] = None) extends Serializable
+case class ServiceForAuthenticationResponse(endpoints: Option[List[Endpoints]] = None, openstackType: String, id: Option[String] = None) extends Serializable
