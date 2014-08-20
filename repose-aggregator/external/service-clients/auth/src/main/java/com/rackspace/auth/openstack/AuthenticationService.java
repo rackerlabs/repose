ffadf71dead5996de7f1835d0cc6da0ee8ed755f
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-08-20 11:30:55 -0500

    Returning AuthResponse instead of Token so I have access to the endpoint tenant IDs

--- repose-aggregator/external/service-clients/auth/src/main/java/com/rackspace/auth/openstack/AuthenticationService.java
+++ repose-aggregator/external/service-clients/auth/src/main/java/com/rackspace/auth/openstack/AuthenticationService.java
@@ -1,7 +1,7 @@
 package com.rackspace.auth.openstack;
 
 import com.rackspace.auth.AuthGroups;
-import com.rackspace.auth.AuthToken;
+import org.openstack.docs.identity.api.v2.AuthenticateResponse;
 import org.openstack.docs.identity.api.v2.Endpoint;
 
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public interface AuthenticationService {
 
-    AuthToken validateToken(String tenant, String userToken);
+    AuthenticateResponse validateToken(String tenant, String userToken);
     
     List<Endpoint> getEndpointsForToken(String userToken);
 
