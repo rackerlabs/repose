f005fa38c0d7e640fa6d4cc1d174e759bcffc776
Damien Johnson <damien.johnson@rackspace.com>
2014-08-20 15:52:48 -0500

    Trash for you to look at!

--- repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/UserNamePasswordRequest.scala
+++ repose-aggregator/components/filters/keystone-v3/src/main/scala/com/rackspace/papi/components/keystone/v3/objects/UserNamePasswordRequest.scala
@@ -1,3 +1,3 @@
 package com.rackspace.papi.components.keystone.v3.objects
 
-case class UserNamePasswordRequest(domain: Option[Domain] = None, id: Option[String] = None, name: Option[String] = None, password: String) extends Serializable
+case class UserNamePasswordRequest(domain: Option[Domain] = None, id: Option[String] = None, name: Option[String] = None, password: String, scope: Option[Scope] = None) extends Serializable
