62a6d42d7f31550de131d1c892ce75f891ddd43a
David Kowis <david.kowis@rackspace.com>
2014-08-21 15:00:21 -0500

    Adding a command line generator class
    Trying to find a nice way to build my command line

---
+++ repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/CommandGenerator.scala
@@ -0,0 +1,26 @@
+package org.openrepose.servo
+
+class CommandGenerator(configRoot: String, warPath: String, launcherPath: String) {
+
+  def commandLine(node: ReposeNode): Seq[String] = {
+    val systemProps = Seq(
+      s"-Drepose-cluster-id=${node.clusterId}",
+      s"-Drepose-node-id=${node.nodeId}",
+      s"-Dpowerapi-config-directory=$configRoot"
+    )
+
+    val jettyParams: Seq[String] = node match {
+      case ReposeNode(_, _, _, Some(port), None) => {
+        Seq("--port", port.toString)
+      }
+      case ReposeNode(_, _, _, None, Some(httpsPort)) => {
+        ???
+      }
+      case ReposeNode(_, _, _, Some(httpPort), Some(httpsPort)) => {
+        ???
+      }
+    }
+
+    Seq("java") ++ systemProps ++ Seq("-jar", launcherPath) ++ jettyParams ++ Seq(warPath)
+  }
+}
---
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/CommandGeneratorTest.scala
@@ -0,0 +1,29 @@
+package org.openrepose.servo
+
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CommandGeneratorTest extends FunSpec with Matchers with TestUtils {
+
+  describe("Command line generator") {
+    it("generates a proper command line from a node, a configurationRoot, a war path, and a launcher path") {
+      val configurationRoot = "/config/root"
+      val warPath = "/path/to/war"
+      val launcherPath = "/path/to/launcher"
+      val cg = new CommandGenerator(configurationRoot, warPath, launcherPath)
+
+      val node = ReposeNode("clusterId", "nodeId", "hostname", Some(8080), None)
+
+      cg.commandLine(node) shouldBe Seq("java", "-Drepose-cluster-id=clusterId", "-Drepose-node-id=nodeId", "-Dpowerapi-config-directory=/config/root",
+        "-jar", "/path/to/launcher", "--port", "8080", "/path/to/war")
+    }
+    it("generates a proper command line for a node with an HTTPS port") {
+      pending
+    }
+    it("generates a proper command line for a node with both an http port and an https port") {
+      pending
+    }
+  }
+}
