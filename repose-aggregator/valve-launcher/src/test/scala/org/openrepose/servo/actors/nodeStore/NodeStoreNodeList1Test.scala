5eba363579dfffc0c5bec04e3e14576aa51faded
David Kowis <david.kowis@rackspace.com>
2014-08-25 13:59:46 -0500

    Refactored node list 1 tests

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/nodeStore/NodeStoreNodeList1Test.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/actors/nodeStore/NodeStoreNodeList1Test.scala
@@ -5,7 +5,7 @@ import akka.testkit.{TestProbe, ImplicitSender, TestKit}
 import org.junit.runner.RunWith
 import org.openrepose.servo.actors.NodeStoreMessages.{ConfigurationUpdated, Initialize}
 import org.openrepose.servo.actors.NodeStore
-import org.openrepose.servo.{ReposeNode, TestUtils}
+import org.openrepose.servo.{ContainerConfig, ReposeNode, TestUtils}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSpecLike, Matchers}
 
@@ -28,40 +28,71 @@ with FunSpecLike with Matchers with BeforeAndAfter with BeforeAndAfterAll with T
   //Using a standalone test probe works so much better here, because I'm actually creating a new nodestore each time
   var probe: TestProbe = _
 
-  val initial =  ConfigurationUpdated(Some(nodeList1), Some(containerConfig1))
+  val initial = ConfigurationUpdated(Some(nodeList1), Some(containerConfig1))
 
   before {
     probe = TestProbe()
 
     nodeStoreVar = system.actorOf(NodeStore.props(propsFunc(probe.ref)))
     nodeStoreVar ! initial
-    probe.expectMsg(1 second,"Started repose:repose_node1")
+    probe.expectMsg(1 second, "Started repose:repose_node1")
   }
 
   after {
     nodeStoreVar ! PoisonPill
   }
 
+
   describe("The Node Store with nodeList1 and container config 1 running") {
-    it("will do nothing when the same configuration is sent") {
-      nodeStoreVar ! initial
+    def someTests: Option[ContainerConfig] => Unit = { cc =>
+      it("will do nothing when the node list is the same") {
+        nodeStoreVar ! initial
 
-      probe.expectNoMsg(1 second)
+        probe.expectNoMsg(1 second)
+      }
+
+      it("will start a new local node when NodeList2 is sent") {
+        nodeStoreVar ! ConfigurationUpdated(Some(nodeList2), cc)
+        probe.expectMsg(1 second, "Started repose:repose_node2")
+      }
+
+      it("will start node2 and stop repose_node1 when NodeList3 is sent") {
+        nodeStoreVar ! ConfigurationUpdated(Some(nodeList3), cc)
+
+        //Expect all these messages within 1 second, no ordering,
+        // But I'm also getting the shutdown messages?
+        probe.expectMsgAllOf(1 second,
+          "Started repose:repose_node2",
+          "Stopped repose:repose_node1")
+      }
+
     }
 
-    it("will start a new local node when NodeList2 is sent with the same containerConfig") {
-      nodeStoreVar ! ConfigurationUpdated(Some(nodeList2), Some(containerConfig1))
-      probe.expectMsg(1 second, "Started repose:repose_node2")
+    describe("keeping the same configuration sent") {
+      someTests(Some(containerConfig1))
+    }
+    describe("without sending any configuration at all") {
+      someTests(None)
     }
 
-    it("will start node2 and stop repose_node1 when NodeList3 is sent with the same containerConfig") {
-      nodeStoreVar ! ConfigurationUpdated(Some(nodeList3), Some(containerConfig1))
+    describe("with a differing container config") {
+      it("will restart all running nodes, even if none were sent") {
+        nodeStoreVar ! ConfigurationUpdated(None, Some(containerConfig2))
 
-      //Expect all these messages within 1 second, no ordering,
-      // But I'm also getting the shutdown messages?
-      probe.expectMsgAllOf(1 second,
-        "Started repose:repose_node2",
-        "Stopped repose:repose_node1")
+        probe.expectMsgAllOf(1 second,
+          "Stopped repose:repose_node1",
+          "Started repose:repose_node1"
+        )
+      }
+      it("will stop existing nodes, and start the nodes from list 2") {
+        nodeStoreVar ! ConfigurationUpdated(Some(nodeList2), Some(containerConfig2))
+
+        probe.expectMsgAllOf(1 second,
+        "Stopped repose:repose_node1",
+        "Started repose:repose_node1",
+        "Started repose:repose_node2"
+        )
+      }
     }
 
     it("will stop all nodes when told to shut down") {
