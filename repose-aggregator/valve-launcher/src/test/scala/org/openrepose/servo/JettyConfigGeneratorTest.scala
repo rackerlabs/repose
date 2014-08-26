58a4d7feed2baa52d9ac5ed926e53a707b85b6d8
David Kowis <david.kowis@rackspace.com>
2014-08-26 18:00:34 -0500

    Created test for jetty configuration generator.
    Created sample xml files to compare with generated XML.
    
    Read lots of XML config files...

---
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/JettyConfigGeneratorTest.scala
@@ -0,0 +1,34 @@
+package org.openrepose.servo
+
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class JettyConfigGeneratorTest extends FunSpec with Matchers{
+
+  describe("Jetty configuration generator for a ReposeNode and a container.cfg.xml") {
+    it("generates a ssl-config.xml when given a Keystore Config") {
+      pending
+    }
+    it("does not generate a ssl-config.xml when not given a Keystore Config") {
+      pending
+    }
+    describe("with a keystore") {
+      it("generates a Jetty config.xml for HTTP") {
+        pending
+      }
+      it("generates a Jetty config.xml for HTTPS") {
+        pending
+      }
+    }
+    describe("without a keystore") {
+      it("generates a Jetty config.xml for HTTP") {
+        pending
+      }
+      it("Fails to generate a Jetty config.xml for HTTPS") {
+        pending
+      }
+    }
+  }
+}
