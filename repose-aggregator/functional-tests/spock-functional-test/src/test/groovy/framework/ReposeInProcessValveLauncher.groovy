b4570b8ad34fca0cba120532acde276c2cd6a63d
wdschei <Bill.Scheidegger@Rackspace.com>
2014-08-26 12:55:06 -0500

    Initial update of the Spock functional tests to utilize the new Servo launcher.

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/ReposeInProcessValveLauncher.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/ReposeInProcessValveLauncher.groovy
@@ -1,33 +1,61 @@
 package framework
-import com.rackspace.cloud.valve.server.PowerApiValveServerControl
+import com.typesafe.config.Config
+import org.openrepose.servo.Servo
+
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
+import java.util.concurrent.TimeUnit
 
 class ReposeInProcessValveLauncher extends ReposeLauncher {
 
-    ReposeConfigurationProvider configurationProvider
-    String configDir
-    PowerApiValveServerControl valve
-    boolean isUp = false
+    def ReposeConfigurationProvider configurationProvider
 
-    ReposeInProcessValveLauncher(ReposeConfigurationProvider configurationProvider,
-                                 String configDir) {
+    def String[] args
+    def InputStream ins
+    def PrintStream out
+    def PrintStream err
+    def Config config
+    def Servo servo
+    def boolean isUp = false
+    def ExecutorService executor = Executors.newSingleThreadExecutor()
+    def FutureTask<Integer> future = null
+    def int futureRtn
 
-        this.configurationProvider = configurationProvider
-        this.configDir = configDir
+    ReposeInProcessValveLauncher(String[] args,
+                                 InputStream ins,
+                                 PrintStream out,
+                                 PrintStream err,
+                                 Config config) {
 
-        this.valve = new PowerApiValveServerControl(null, null, configDir, null, null)
+        this.args = args
+        this.ins = ins
+        this.out = out
+        this.err = err
+        this.config = config
+
+        this.servo = new Servo()
     }
 
     @Override
     void start() {
-
-        this.valve.startPowerApiValve()
+        future =  new FutureTask<Integer>(
+                new Callable<Integer>() {
+                    public Integer call() {
+                        return servo.execute(args, ins, out, err, config)
+                    }})
+        executor.execute(future)
         this.isUp = true
     }
 
     void stop() {
-
-        this.valve.stopPowerApiValve()
-        this.isUp = false
+        if(this.isUp) {
+            this.servo.shutdown()
+            futureRtn = future.get(10, TimeUnit.SECONDS)
+            this.isUp = false
+        }
     }
 
     @Override
