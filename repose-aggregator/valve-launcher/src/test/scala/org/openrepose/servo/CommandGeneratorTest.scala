111c4ed80b7712fec91bbbcbee4047283ac80abd
David Kowis <dkowis@shlrm.org>
2014-08-26 12:11:27 -0500

    Failing test, and notes about how to do things!

--- repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/CommandGeneratorTest.scala
+++ repose-aggregator/valve-launcher/src/test/scala/org/openrepose/servo/CommandGeneratorTest.scala
@@ -7,22 +7,34 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class CommandGeneratorTest extends FunSpec with Matchers with TestUtils {
 
+  val tempdir = tempDir("commandGenerator").toFile.getAbsolutePath
+
+  val configurationRoot = tempdir
+  val warPath = "/path/to/war"
+  val launcherPath = "/path/to/launcher"
+  val baseCommand = Seq("java")
+  val node = ReposeNode("clusterId", "nodeId", "hostname", Some(8080), None)
+  val keystoreConfig = KeystoreConfig("keystoreFile", "keystorePass", "keyPass")
+
+
   describe("Command line generator") {
     it("generates a proper command line from a base command, a node, a configurationRoot, a war path, and a launcher path") {
-      val configurationRoot = "/config/root"
-      val warPath = "/path/to/war"
-      val launcherPath = "/path/to/launcher"
-      val baseCommand = Seq("java")
       val cg = new CommandGenerator(baseCommand, configurationRoot, launcherPath, warPath)
 
-      val node = ReposeNode("clusterId", "nodeId", "hostname", Some(8080), None)
 
-      cg.commandLine(node) shouldBe Seq("java", "-Drepose-cluster-id=clusterId", "-Drepose-node-id=nodeId", "-Dpowerapi-config-directory=/config/root",
+      cg.commandLine(node) shouldBe Seq("java", "-Drepose-cluster-id=clusterId", "-Drepose-node-id=nodeId", s"-Dpowerapi-config-directory=$tempdir",
         "-jar", "/path/to/launcher", "--port", "8080", "/path/to/war")
     }
     it("generates a proper command line when given a container config with a keystore") {
       //This would include the configuration file generated.
-      pending
+      val cg = new CommandGenerator(baseCommand, configurationRoot, launcherPath, warPath, Some(keystoreConfig))
+
+      //TODO: also need to verify the file that's written? This means that /config/root has to be real
+      //This has information on configuring the xmls for SSL and for a keystore
+      //http://www.eclipse.org/jetty/documentation/current/configuring-ssl.html
+
+      cg.commandLine(node) shouldBe Seq("java", "-Drepose-cluster-id=clusterId", "-Drepose-node-id=nodeId", s"-Dpowerapi-config-directory=$tempdir",
+        "-jar", "/path/to/launcher", "--config", s"$tempdir/clusterId_nodeId_jetty.xml", "--port", "8080", "/path/to/war")
     }
 
     it("generates a proper command line when told to operate insecurely") {
