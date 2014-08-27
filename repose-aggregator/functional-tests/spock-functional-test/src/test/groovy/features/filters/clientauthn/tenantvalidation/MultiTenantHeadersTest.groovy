103953d87eb97f2444e640be28618b98743b6fef
jenny.vo <jenny.vo@rackspace.com>
2014-08-27 13:48:57 -0500

    rename test file

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthn/tenantvalidation/MultiTenantHeadersTest.groovy
@@ -0,0 +1,86 @@
+package features.filters.clientauthn.tenantvalidation
+
+import framework.ReposeValveTest
+import framework.mocks.MockIdentityService
+import org.joda.time.DateTime
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+import spock.lang.Unroll
+
+/**
+ * Created by jennyvo on 8/26/14.
+ * This test verify when user token having multi-tenant client-auth filter will retrieve
+ * all tenants and put in multi x-tenant-id in headers
+ */
+class MultiTenantHeadersTest extends ReposeValveTest{
+
+    def static originEndpoint
+    def static identityEndpoint
+
+    def static MockIdentityService fakeIdentityService
+
+    def setupSpec() {
+
+        deproxy = new Deproxy()
+
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/clientauthn/common", params)
+        repose.configurationProvider.applyConfigs("features/filters/clientauthn/multitenantheader", params)
+        repose.start()
+
+        originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
+        fakeIdentityService = new MockIdentityService(properties.identityPort, properties.targetPort)
+        identityEndpoint = deproxy.addEndpoint(properties.identityPort,
+                'identity service', null, fakeIdentityService.handler)
+
+
+    }
+
+    def cleanupSpec() {
+        deproxy.shutdown()
+
+        repose.stop()
+    }
+
+    def setup(){
+        fakeIdentityService.resetHandlers()
+    }
+
+    @Unroll("#defaultTenant, #secondTenant, #requestTenant ")
+    def "When user token have multi-tenant will retrieve all tenants in the header" () {
+        given:
+        fakeIdentityService.with {
+            client_token = clientToken
+            tokenExpiresAt = (new DateTime()).plusDays(1)
+            client_tenant = defaultTenant
+            client_tenant_file = secondTenant
+            service_admin_role = "not-admin"
+        }
+
+        when: "User passes a request through repose with $requestTenant"
+        MessageChain mc = deproxy.makeRequest(
+                url: "$reposeEndpoint/servers/$requestTenant",
+                method: 'GET',
+                headers: ['content-type': 'application/json', 'X-Auth-Token': fakeIdentityService.client_token])
+
+        then: "Everything gets passed as is to the origin service (no matter the user)"
+        mc.receivedResponse.code == serviceRespCode
+
+        if (serviceRespCode != "200")
+            assert mc.handlings.size() == 0
+        else {
+            assert mc.handlings.size() == 1
+            assert mc.handlings[0].request.headers.findAll("x-tenant-id").size() == numberTenants
+            assert mc.handlings[0].request.headers.findAll("x-tenant-id").contains(defaultTenant)
+            assert mc.handlings[0].request.headers.findAll("x-tenant-id").contains(secondTenant)
+        }
+
+        where:
+        defaultTenant   | secondTenant  |requestTenant  |clientToken        |serviceRespCode    | numberTenants
+        "123456"        | "nast-id"     | "123456"      |UUID.randomUUID()  | "200"             | 2
+        "123456"        | "nast-id"     | "nast-id"     |UUID.randomUUID()  | "200"             | 2
+        "123456"        | "123456"      | "123456"      |UUID.randomUUID()  | "200"             | 1
+        "123456"        | "nast-id"     | "223456"      |UUID.randomUUID()  | "401"             | 0
+    }
+}
