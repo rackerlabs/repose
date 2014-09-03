046a23b59ebc5037b9d1ee1abcce7c6d7ba344ea
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-09-03 16:51:45 -0500

    Added some violations to the bad code test file

--- repose-aggregator/components/filters/client-auth/src/main/java/BadCode.java
+++ repose-aggregator/components/filters/client-auth/src/main/java/BadCode.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
+
 /**
  * Created by eric7500 on 9/2/14.
  */
 public class BadCode {
     int x;
+    String s;
 
-    public int getX() {
+    public int getX ( ) {
         return x;
+    }
+
+    public int getXPlus () {
+        x+=1;
+        return x;
+    }
+
+    public String callToStringS() {
+        return(s .toString());
+    }
+
+    public ArrayList getList ( ) {
+        return new ArrayList < Integer > (     ) ;
     }
 }
