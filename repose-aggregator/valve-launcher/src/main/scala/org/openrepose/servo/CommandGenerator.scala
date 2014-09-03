900e9ca7e6a5480e240a4abb10f302fbe22787c1
David Kowis <dkowis@shlrm.org>
2014-09-03 14:47:37 -0500

    Adding the file permissions to the jetty configs, nearly forgot

--- repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/CommandGenerator.scala
+++ repose-aggregator/valve-launcher/src/main/scala/org/openrepose/servo/CommandGenerator.scala
@@ -1,6 +1,7 @@
 package org.openrepose.servo
 
 import java.io.File
+import java.nio.file.attribute.{PosixFilePermissions, PosixFilePermission}
 import java.nio.file.{StandardOpenOption, OpenOption, Files}
 
 import org.slf4j.LoggerFactory
@@ -35,12 +36,29 @@ class CommandGenerator(baseCommand: Seq[String],
     val sslConfigName = s"${node.clusterId}_${node.nodeId}_jetty_ssl.xml"
 
     val config = new File(configRoot, configFileName)
+    val configPermissions = PosixFilePermissions.fromString("rw-rw----")
+
+    /**
+     * Use the Files stuff to make sure that we set the permissions on the jetty config files properly
+     * This will handle it if the file exists, or if we're creating new ones, without doing unnecessary deletes
+     * @param f
+     */
+    def prepConfigFile(f:File):Unit = {
+      if(!Files.exists(f.toPath)) {
+        Files.createFile(f.toPath, PosixFilePermissions.asFileAttribute(configPermissions))
+      } else {
+        Files.setPosixFilePermissions(f.toPath, configPermissions)
+      }
+    }
+
+    prepConfigFile(config)
     Files.write(config.toPath, jettyConfig.jettyConfig.getBytes, StandardOpenOption.CREATE)
     val jettyConfigParams = Seq("--config", config.getAbsolutePath)
 
     val jettySslParams = jettyConfig.sslConfig match {
       case Some(sslConfig) =>
         val sslConfigFile = new File(configRoot, sslConfigName)
+        prepConfigFile(sslConfigFile)
         Files.write(sslConfigFile.toPath, sslConfig.getBytes, StandardOpenOption.CREATE)
         Seq("--config", sslConfigFile.getAbsolutePath)
       case None =>
