0cb7f9b0437d535d9d6fef9f939b3a228cad75e8
Damien Johnson <damien.johnson@rackspace.com>
2014-09-08 11:09:23 -0500

    Removed sleep and cache reliance & formatted

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/keystonev3/KeystoneV3AuthZTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/keystonev3/KeystoneV3AuthZTest.groovy
@@ -29,12 +29,11 @@ class KeystoneV3AuthZTest extends ReposeValveTest {
         repose.start()
 
         originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
-        fakeKeystoneV3Service = new MockKeystoneV3Service(properties.identityPort,properties.targetPort)
+        fakeKeystoneV3Service = new MockKeystoneV3Service(properties.identityPort, properties.targetPort)
         identityEndpoint = deproxy.addEndpoint(properties.identityPort,
                 'identity service', null, fakeKeystoneV3Service.handler)
         //targetPort = properties.targetPort
     }
-
 
     def cleanupSpec() {
         if (deproxy)
@@ -45,27 +44,26 @@ class KeystoneV3AuthZTest extends ReposeValveTest {
     }
 
     @Unroll("service endpoint #endpointResponse status code #statusCode")
-    def "When user is authorized should forward request to origin service"(){
+    def "When user is authorized should forward request to origin service"() {
         given:
         fakeKeystoneV3Service.with {
             endpointUrl = endpointResponse
         }
-        sleep(500) // TODO: Necessary to circumvent token caching. Is there a better way?
 
         when: "User sends a request through repose"
-        MessageChain mc = deproxy.makeRequest(url:"http://localhost:${properties.reposePort}/v3/${fakeKeystoneV3Service.client_token}/ss", method:'GET', headers:['X-Subject-Token': fakeKeystoneV3Service.client_token])
+        MessageChain mc = deproxy.makeRequest(url: "http://localhost:${properties.reposePort}/v3/${fakeKeystoneV3Service.client_token}/ss", method: 'GET', headers: ['X-Subject-Token': token])
 
         then: "User should receive a #statusCode response"
         mc.receivedResponse.code == statusCode
         mc.handlings.size() == handlings
 
         where:
-        endpointResponse | statusCode | handlings
-        "localhost"      | "200"      | 1
-        "myhost.com"     | "403"      | 0
+        endpointResponse | statusCode | handlings | token
+        "localhost"      | "200"      | 1         | "some-random-token"
+        "myhost.com"     | "403"      | 0         | "some-other-token"
     }
 
-    def "When user is not authorized should receive a 403 FORBIDDEN response"(){
+    def "When user is not authorized should receive a 403 FORBIDDEN response"() {
 
         given: "IdentityService is configured with allowed endpoints that will differ from the user's requested endpoint"
         def token = UUID.randomUUID().toString()
@@ -75,7 +73,7 @@ class KeystoneV3AuthZTest extends ReposeValveTest {
         }
 
         when: "User sends a request through repose"
-        MessageChain mc = deproxy.makeRequest(url:"http://localhost:${properties.reposePort}/v3/${token}/ss", method:'GET', headers:['X-Subject-Token': token])
+        MessageChain mc = deproxy.makeRequest(url: "http://localhost:${properties.reposePort}/v3/${token}/ss", method: 'GET', headers: ['X-Subject-Token': token])
         def foundLogs = reposeLogSearch.searchByString("User token: " + token +
                 ": The user's service catalog does not contain an endpoint that matches the endpoint configured in keystone-v3.cfg.xml")
 
