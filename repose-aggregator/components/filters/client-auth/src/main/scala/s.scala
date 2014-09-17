a62e9d71cb09a1ce2b372c1cb3571fef345739d3
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-09-17 16:35:28 -0500

    Some bad scala code to test checkers

--- repose-aggregator/components/filters/client-auth/src/main/scala/s.scala
+++ repose-aggregator/components/filters/client-auth/src/main/scala/s.scala
@@ -1,3 +1,50 @@
-class s {
+package ThisPackageNeedsABlankLine
+import scala.util.Random;
+class s(xi: Int)  {
+  var x: Int = xi
 
+  def getX ={
+    for (5 <- Range){
+      while(true) {
+        if  (true) {
+
+        } else
+        if (true) {
+
+        } else{
+
+        }
+      }
+      Random r = new Random();
+      try{
+
+      } catch {
+        case e: Exception => println("exception caught: " + e);
+      } finally  {
+
+      }
+
+      do  {
+
+      } while (true)
+
+      if (true){
+
+      } else  {
+
+      }
+      while (true)     {
+
+      }
+      if (true)
+      {
+
+      }
+      import java.util.ArrayList;
+      ArrayList e;
+
+    }
+  }
 }
+
+
