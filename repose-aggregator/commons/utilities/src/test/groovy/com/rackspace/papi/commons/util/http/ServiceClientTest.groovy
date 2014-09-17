81f83e46ff2fe86ff231a4f4d29bab489c60955d
Damien Johnson <damien.johnson@rackspace.com>
2014-09-17 12:45:49 -0500

    Unit test for new ServiceClient functionality

---
+++ repose-aggregator/commons/utilities/src/test/groovy/com/rackspace/papi/commons/util/http/ServiceClientTest.groovy
@@ -0,0 +1,62 @@
+package com.rackspace.papi.commons.util.http
+
+import com.rackspace.papi.service.httpclient.HttpClientResponse
+import com.rackspace.papi.service.httpclient.HttpClientService
+import org.apache.http.HttpResponse
+import org.apache.http.StatusLine
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpRequestBase
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+
+import javax.ws.rs.core.MediaType
+
+import static org.hamcrest.Matchers.equalTo
+import static org.junit.Assert.assertThat
+import static org.junit.Assert.assertTrue
+import static org.mockito.Matchers.any
+import static org.mockito.Matchers.anyString
+import static org.mockito.Mockito.*
+
+class ServiceClientTest {
+
+    private ServiceClient serviceClient
+
+    private HttpClient mockHttpClient
+    private ArgumentCaptor<HttpRequestBase> requestCaptor
+
+    @Before
+    void setup() {
+        HttpClientService mockClientService = mock(HttpClientService)
+        HttpClientResponse mockClientResponse = mock(HttpClientResponse)
+        HttpResponse mockHttpResponse = mock(HttpResponse)
+        StatusLine mockStatusLine = mock(StatusLine)
+        mockHttpClient = mock(HttpClient)
+        requestCaptor = ArgumentCaptor.forClass(HttpRequestBase)
+
+        when(mockClientService.getClient(anyString())).thenReturn(mockClientResponse)
+        when(mockClientResponse.getHttpClient()).thenReturn(mockHttpClient)
+        when(mockHttpClient.execute(requestCaptor.capture())).thenReturn(mockHttpResponse)
+        when(mockHttpResponse.getAllHeaders()).thenReturn(null)
+        when(mockHttpResponse.getStatusLine()).thenReturn(mockStatusLine)
+        when(mockStatusLine.getStatusCode()).thenReturn(200)
+
+        serviceClient = new ServiceClient("test-pool", mockClientService)
+    }
+
+    @Test
+    void "when post is called with headers, then a request should be made with those headers"() {
+        Map<String, String> headers = new HashMap<>()
+        headers.put("x-foo", "foo")
+        headers.put("x-bar", "bar")
+
+        serviceClient.post("http://example.com", headers, "body", MediaType.TEXT_PLAIN_TYPE)
+
+        verify(mockHttpClient).execute(any(HttpRequestBase))
+        assertTrue(requestCaptor.getValue().containsHeader("x-foo"))
+        assertTrue(requestCaptor.getValue().containsHeader("x-bar"))
+        assertThat(requestCaptor.getValue().getFirstHeader("x-foo").getValue(), equalTo("foo"))
+        assertThat(requestCaptor.getValue().getFirstHeader("x-bar").getValue(), equalTo("bar"))
+    }
+}
