8f71b44ea64f334c298195b0a3c851de5e394b41
wdschei <Bill.Scheidegger@Rackspace.com>
2014-09-24 17:34:25 -0500

    Removed the original pseudo-code comments as per code review comments.

--- repose-aggregator/components/filters/rackspace-identity-basic-auth/src/main/scala/com/rackspace/papi/components/rackspace/identity/basicauth/RackspaceIdentityBasicAuthHandler.scala
+++ repose-aggregator/components/filters/rackspace-identity-basic-auth/src/main/scala/com/rackspace/papi/components/rackspace/identity/basicauth/RackspaceIdentityBasicAuthHandler.scala
@@ -32,60 +32,41 @@ class RackspaceIdentityBasicAuthHandler(basicAuthConfig: RackspaceIdentityBasicA
     val filterDirector: FilterDirector = new FilterDirectorImpl()
     // We need to process the Response unless a couple of specific conditions occur.
     filterDirector.setFilterAction(FilterAction.PROCESS_RESPONSE)
-    // IF request already has an X-Auth-Token header,
-    // THEN skip any HTTP Basic authentication headers (Authorization) that may be present.
     val optionXAuthHeaders = Option(httpServletRequest.getHeaders(X_AUTH_TOKEN))
     if (!optionXAuthHeaders.isDefined || !(optionXAuthHeaders.get.hasMoreElements)) {
-      // IF request has a HTTP Basic authentication header (Authorization) with method of Basic, THEN ...
       val optionHeaders = Option(httpServletRequest.getHeaders(HttpHeaders.AUTHORIZATION))
       val authMethodBasicHeaders = BasicAuthUtils.getBasicAuthHeaders(optionHeaders, "Basic")
       if (authMethodBasicHeaders.isDefined && !(authMethodBasicHeaders.get.isEmpty)) {
-        // FOR EACH HTTP Basic authentication header (Authorization) with method Basic...
         var tokenFound = false
         var code = HttpServletResponse.SC_OK
-        // THIS First in wins
         val authHeader = authMethodBasicHeaders.get.next()
-        // OR Loop through until we find a good one.
-        //for (authHeader <- authMethodBasicHeaders.get.next()
-        //if !tokenFound) {
-          val authValue = authHeader.replace("Basic ", "")
-          var token = Option(datastore.get(TOKEN_KEY_PREFIX + authValue))
-          // IF the userName/apiKey is in the cache,
-          // THEN add the token header;
-          // ELSE ...
+        val authValue = authHeader.replace("Basic ", "")
+        var token = Option(datastore.get(TOKEN_KEY_PREFIX + authValue))
+        if (token.isDefined && !(token.isEmpty)) {
+          val tokenStr = token.get.toString
+          if (tokenStr.length > 0) {
+            filterDirector.requestHeaderManager().appendHeader(X_AUTH_TOKEN, token.get.toString)
+            tokenFound = true
+          }
+        } else {
+          // request a token
+          val rtn = getUserToken(authValue)
+          code = rtn._1
+          token = rtn._2
           if (token.isDefined && !(token.isEmpty)) {
             val tokenStr = token.get.toString
             if (tokenStr.length > 0) {
+              if (tokenCacheTtlMillis > 0) {
+                datastore.put(TOKEN_KEY_PREFIX + authValue, token.get.toString, tokenCacheTtlMillis, TimeUnit.MILLISECONDS)
+              }
+              // add the token header
               filterDirector.requestHeaderManager().appendHeader(X_AUTH_TOKEN, token.get.toString)
+              // set the flag
               tokenFound = true
             }
-          } else {
-            // request a token
-            val rtn = getUserToken(authValue)
-            code = rtn._1
-            token = rtn._2
-            // IF a token was received, THEN ...
-            if (token.isDefined && !(token.isEmpty)) {
-              val tokenStr = token.get.toString
-              if (tokenStr.length > 0) {
-                // IF the Cache TTL is enabled,
-                // THEN cache the token with the configured cache timeout.
-                if (tokenCacheTtlMillis > 0) {
-                  datastore.put(TOKEN_KEY_PREFIX + authValue, token.get.toString, tokenCacheTtlMillis, TimeUnit.MILLISECONDS)
-                }
-                // add the token header
-                filterDirector.requestHeaderManager().appendHeader(X_AUTH_TOKEN, token.get.toString)
-                // set the flag
-                tokenFound = true
-              }
-            }
           }
-        //}
-        // IF the Token was not found, THEN ...
+        }
         if (!tokenFound) {
-          // IF the status code from the Identity Service is UNAUTHORIZED (401) or NOT_FOUND (404)
-          // THEN set the response, add the header, and return;
-          // ELSE set the response to INTERNAL_SERVER_ERROR (500) and return.
           code match {
             case HttpServletResponse.SC_UNAUTHORIZED |                // (401)
                  HttpServletResponse.SC_NOT_FOUND =>                  // (404)
@@ -122,13 +103,8 @@ class RackspaceIdentityBasicAuthHandler(basicAuthConfig: RackspaceIdentityBasicA
       createAuthRequest(authValue).toString,
       MediaType.APPLICATION_XML_TYPE))
 
-    // IF the Akka Service Client gives a response, THEN...;
-    // ELSE just return INTERNAL_SERVER_ERROR (500).
     if (authTokenResponse.isDefined) {
       val statusCode = authTokenResponse.get.getStatusCode
-      // IF the response is OK (200),
-      // THEN extract the Token;
-      // ELSE just return the code.
       if (statusCode == HttpServletResponse.SC_OK) {
         def authRespDataRaw = authTokenResponse.get.getData
         def authRespDataStr = Source.fromInputStream(authRespDataRaw).mkString
@@ -147,7 +123,6 @@ class RackspaceIdentityBasicAuthHandler(basicAuthConfig: RackspaceIdentityBasicA
   override def handleResponse(httpServletRequest: HttpServletRequest, httpServletResponse: ReadableHttpServletResponse): FilterDirector = {
     LOG.debug("Handling HTTP Response. Incoming status code: " + httpServletResponse.getStatus())
     val filterDirector: FilterDirector = new FilterDirectorImpl()
-    // IF response Status Code is UNAUTHORIZED (401) OR FORBIDDEN (403), THEN
     if (httpServletResponse.getStatus == HttpServletResponse.SC_UNAUTHORIZED ||
       httpServletResponse.getStatus == HttpServletResponse.SC_FORBIDDEN) {
       handleUnauthorized(filterDirector, httpServletRequest)
@@ -159,8 +134,6 @@ class RackspaceIdentityBasicAuthHandler(basicAuthConfig: RackspaceIdentityBasicA
   private def handleUnauthorized(filterDirector: FilterDirector, httpServletRequest: HttpServletRequest) {
     // add HTTP Basic authentication header (WWW-Authenticate) with the realm of RAX-KEY
     filterDirector.responseHeaderManager().appendHeader(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"RAX-KEY\"")
-    // IF request has a HTTP Basic authentication header (Authorization),
-    // THEN remove the encoded userName/apiKey (Key) & token (Value) cached in the Datastore
     val optionHeaders = Option(httpServletRequest.getHeaders(HttpHeaders.AUTHORIZATION))
     val authMethodBasicHeaders = BasicAuthUtils.getBasicAuthHeaders(optionHeaders, "Basic")
     if (authMethodBasicHeaders.isDefined && !(authMethodBasicHeaders.get.isEmpty)) {
