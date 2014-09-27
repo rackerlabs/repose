e5a1093d84bcb43e37e72e99ed600a5d9c61fe12
David Kowis <dkowis@shlrm.org>
2014-09-27 11:37:36 -0500

    Initial creation of a combined rackspace auth identity filter
    Along the lines of ip-identity, but specifically for rackspace's auth
    token posting logic.

---
+++ repose-aggregator/components/filters/rackspace-auth-identity/src/main/scala/com/rackspace/identity/repose/authIdentity/RackspaceAuthIdentityFilter.scala
@@ -0,0 +1,47 @@
+package com.rackspace.identity.repose.authIdentity
+
+import javax.servlet._
+
+import com.rackspace.papi.commons.config.manager.UpdateListener
+import com.rackspace.papi.filter.FilterConfigHelper
+import com.rackspace.papi.filter.logic.impl.FilterLogicHandlerDelegate
+import com.rackspace.papi.service.config.ConfigurationService
+import com.rackspace.papi.service.context.ServletContextHelper
+import com.typesafe.scalalogging.slf4j.LazyLogging
+
+class RackspaceAuthIdentityFilter extends Filter with LazyLogging {
+
+  private final val DEFAULT_CONFIG = "rackspace-auth-identity.cfg.xml"
+
+  private var config: String = _
+  private var handlerFactory: RackspaceAuthIdentityHandlerFactory = _
+  private var configurationService: ConfigurationService = _
+
+
+  override def init(filterConfig: FilterConfig): Unit = {
+    config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
+    logger.info(s"Initializing RackspaceAuthIdentityFilter using config $config")
+
+    //Ew, nasty spring hax
+    val powerApiContext = ServletContextHelper.getInstance(filterConfig.getServletContext).getPowerApiContext
+    configurationService = powerApiContext.configurationService()
+    handlerFactory = new RackspaceAuthIdentityHandlerFactory()
+
+    val xsdURL = getClass.getResource("/META-INF/config/schema/rackspace-auth-identity-configuration.xsd")
+    configurationService.subscribeTo(filterConfig.getFilterName,
+      config,
+      xsdURL,
+      handlerFactory.asInstanceOf[UpdateListener[RackspaceAuthIdentityConfig]],
+      classOf[RackspaceAuthIdentityConfig])
+
+  }
+
+  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
+    new FilterLogicHandlerDelegate(request, response, chain).doFilter(handlerFactory.newHandler)
+
+  }
+
+  override def destroy(): Unit = {
+    configurationService.unsubscribeFrom(config, handlerFactory)
+  }
+}
---
+++ repose-aggregator/components/filters/rackspace-auth-identity/src/main/scala/com/rackspace/identity/repose/authIdentity/RackspaceAuthIdentityHandlerFactory.scala
@@ -0,0 +1,42 @@
+package com.rackspace.identity.repose.authIdentity
+
+import java.util
+import java.util.concurrent.atomic.{AtomicReference, AtomicBoolean}
+
+import com.rackspace.papi.commons.config.manager.UpdateListener
+import com.rackspace.papi.filter.logic.AbstractConfiguredFilterHandlerFactory
+
+class RackspaceAuthIdentityHandlerFactory extends AbstractConfiguredFilterHandlerFactory[RackspaceAuthIdentityHandler] {
+
+  private val handlerReference = new AtomicReference[RackspaceAuthIdentityHandler]()
+
+  override protected def buildHandler(): RackspaceAuthIdentityHandler = {
+    if(isInitialized) {
+      handlerReference.get
+    } else {
+      null //EW
+    }
+  }
+
+  override protected def getListeners: util.Map[Class[_], UpdateListener[_]] = {
+    val listenerMap = new util.HashMap[Class[_], UpdateListener[_]]()
+
+    listenerMap.put(classOf[RackspaceAuthIdentityConfig], new RackspaceAuthIdentityConfigListener())
+
+    listenerMap
+  }
+
+  private class RackspaceAuthIdentityConfigListener extends UpdateListener[RackspaceAuthIdentityConfig] {
+    private val initialized = new AtomicBoolean()
+
+    override def configurationUpdated(config: RackspaceAuthIdentityConfig): Unit = {
+      val handler = new RackspaceAuthIdentityHandler(config)
+      handlerReference.set(handler)
+      initialized.set(true)
+    }
+
+    override def isInitialized: Boolean = {
+      initialized.get()
+    }
+  }
+}
