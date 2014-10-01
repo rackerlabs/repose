0bf6e2284a3be0db29e2b321922c1ea9583559ad
jenny.vo <jenny.vo@rackspace.com>
2014-10-01 16:45:48 -0500

    update test to include test for v1.1 and v2.0, also rename test to fix for both versions

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/rackspaceAuthIdentity/RackspaceAuthIdentityTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/rackspaceAuthIdentity/RackspaceAuthIdentityTest.groovy
@@ -1,13 +1,13 @@
 package features.filters.rackspaceAuthIdentity
-
 import framework.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.Handling
 import org.rackspace.deproxy.MessageChain
-import org.rackspace.deproxy.Response
 import spock.lang.Unroll
-
-class RackspaceAuthIdentity20Test extends ReposeValveTest {
+/*
+ * rackspace auth identity test include test for both versions v2.0 and v1.1
+ */
+class RackspaceAuthIdentityTest extends ReposeValveTest {
 
     def static Map contentXml = ["content-type": "application/xml"]
     def static Map contentJSON = ["content-type": "application/json"]
@@ -51,6 +51,14 @@ class RackspaceAuthIdentity20Test extends ReposeValveTest {
     }
 }
 """
+    //v1.1
+    def static String jsonPasswordCred11 = "{ \"passwordCredentials\": { \"username\": \"test-user\", \"password\": \"testpwd\"}}"
+    def static String jsonKeyCred11 = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"testpwd\"}}"
+    def static String jsonKeyCredEmptyKey11 = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"\"}}"
+    def static String xmlPasswordCred11 = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" password=\"testpwd\" />"
+    def static String xmlPasswordCredEmptyKey11 = "<passwordCredentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" password=\"\" />"
+    def static String xmlKeyCred11 = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"testpwd\" />"
+    def static String xmlKeyCredEmptyKey11 = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"\" />"
 
     def setupSpec() {
         deproxy = new Deproxy()
@@ -119,6 +127,36 @@ class RackspaceAuthIdentity20Test extends ReposeValveTest {
         requestBody  | contentType | testName
         invalidData  | contentXml  | "invalidData"
         xmlOverLimit | contentXml  | "xmlOverLimit"
+    }
+
+    @Unroll("When request contains identity 1.1 in content #testName Expected user is #expectedUser")
+    def "when using identity1.1 identifying requests by header"() {
+
+        when: "Request body contains user credentials"
+        def messageChain = deproxy.makeRequest([url: reposeEndpoint, requestBody: requestBody, headers: contentType])
+        def sentRequest = ((MessageChain) messageChain).getHandlings()[0]
+
+        then: "Repose will send x-pp-user with a single value"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").size() == 1
+
+        and: "Repose will send user from Request body"
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("x-pp-user").equals(expectedUser + ";q=0.75")
+
+        and: "Repose will send a single value for x-pp-groups"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-groups").size() == 1
+
+        and: "Repose will send 'My Group' for x-pp-groups"
+        ((Handling) sentRequest).request.getHeaders().getFirstValue("x-pp-groups").equals("1_1 Group;q=0.75")
+
+        where:
+        requestBody               | contentType | expectedUser  | testName
+        xmlPasswordCred11         | contentXml  | "test-user"   | "xmlPasswordCred11"
+        xmlPasswordCredEmptyKey11 | contentXml  | "test-user"   | "xmlPasswordCredEmptyKey11"
+        xmlKeyCred11              | contentXml  | "test-user"   | "xmlKeyCred11"
+        xmlKeyCredEmptyKey11      | contentXml  | "test-user"   | "xmlKeyCredEmptyKey11"
+        jsonPasswordCred11        | contentJSON | "test-user"   | "jsonPasswordCred11"
+        jsonKeyCred11             | contentJSON | "test-user"   | "jsonKeyCred11"
+        jsonKeyCredEmptyKey11     | contentJSON | "test-user"   | "jsonKeyCredEmptyKey11"
     }
 
 }
