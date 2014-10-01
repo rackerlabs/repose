8de76c6814341b099336cb27f84146a79f29a25a
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-10-01 16:42:10 -0500

    Using regexes because some of the rules don't work

--- repose-aggregator/components/filters/client-auth/src/test/groovy/GroovClass.groovy
+++ repose-aggregator/components/filters/client-auth/src/test/groovy/GroovClass.groovy
@@ -1,12 +1,32 @@
 package a
+import groovy.json.JsonBuilder
 class GroovClass{
     void hello() {
         println "hello, world"
         boolean x = true;
-        if(x){}
+        if(x){
+
+        }else {
+
+        }
+
+        JsonBuilder jb = new JsonBuilder()
+
+        int j = 0
+        int m = 0
+
         for(int i = 0; i < 10; i++){
             println "for"
+            j = i == 2? 2 : 3;
+            m = i == 2 ? 2 :3;
         }
+        println j
+        println m
+
+
+
+
+
 
     }
 }
