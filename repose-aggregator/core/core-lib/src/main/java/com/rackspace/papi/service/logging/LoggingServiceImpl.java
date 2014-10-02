ac6df0be4246b444b74f30048f1d8f217fc4f634
wdschei <Bill.Scheidegger@Rackspace.com>
2014-10-02 16:06:31 -0500

    Removed the reflection call from the LoggingServiceImpl as per team comments. Added the slf4j-log4j12 dependency to the Core-Lib POM since the reflection call was removed. Added the slf4j-log4j12 dependency to the Web-Application POM's Package scope to account for the one in Valve not being present. Updated the test runners in the Installation module for the DEB and RPM suites to be more abstracted and in turn maintainable.

--- repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/logging/LoggingServiceImpl.java
+++ repose-aggregator/core/core-lib/src/main/java/com/rackspace/papi/service/logging/LoggingServiceImpl.java
@@ -10,15 +10,6 @@ public class LoggingServiceImpl implements LoggingService {
 
     @Override
     public void updateLoggingConfiguration(Properties loggingConfigFile) {
-        //org.apache.log4j.PropertyConfigurator.configure(loggingConfigFile);
-        try {
-            Class<?> clazz = Class.forName("org.apache.log4j.PropertyConfigurator");
-            java.lang.reflect.Method method = clazz.getDeclaredMethod("configure", Properties.class);
-            method.invoke(null, loggingConfigFile);
-        } catch (ClassNotFoundException | NoSuchMethodException | java.lang.reflect.InvocationTargetException | IllegalAccessException e) {
-            org.slf4j.LoggerFactory.getLogger(LoggingServiceImpl.class).warn("Unable to configure the selected Log4J framework!", e);
-            System.err.println("Unable to configure the selected Log4J framework!");
-            e.printStackTrace();
-        }
+        org.apache.log4j.PropertyConfigurator.configure(loggingConfigFile);
     }
 }
