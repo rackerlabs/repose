c16377bd277a4110b513b4653d7187b0aea75b24
Damien Johnson <damien.johnson@rackspace.com>
2014-10-08 15:59:02 -0500

    Modified and cleaned up the existing test

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/EmptyRequestBodyTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/EmptyRequestBodyTest.groovy
@@ -4,9 +4,11 @@ import framework.ReposeValveTest
 import framework.category.Bug
 import org.junit.experimental.categories.Category
 import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
 import spock.lang.Unroll
 
 class EmptyRequestBodyTest extends ReposeValveTest {
+
     def setupSpec() {
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort)
@@ -15,30 +17,48 @@ class EmptyRequestBodyTest extends ReposeValveTest {
         repose.configurationProvider.applyConfigs("common", params)
         repose.configurationProvider.applyConfigs("features/core/proxy", params)
         repose.start()
+    }
 
-        repose.waitForNon500FromUrl(reposeEndpoint)
+    def "when GET request with body, Deproxy should forward body"() {
+        given:
+        String requestBody = "request body"
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: "http://localhost:${properties.targetPort}", requestBody: requestBody)
+
+        then:
+        mc.getSentRequest().getBody().toString() == requestBody
+        mc.getHandlings().get(0).getRequest().getBody().toString() == requestBody
     }
 
     @Unroll("#method should not have its body removed")
     def "Repose should not remove request bodies unless filters do so explicitly"() {
+        given:
+        String requestBody = "request body"
+
         when:
-        def mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: "body content")
+        def mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: requestBody)
 
         then:
-        mc.handlings[0].request.body == "body content"
+        mc.getSentRequest().getBody().toString() == requestBody
+        mc.getHandlings().get(0).getRequest().getBody().toString() == requestBody
 
         where:
         method << ["PUT", "POST", "PATCH", "DELETE"]
     }
 
     @Category(Bug)
-    @Unroll("#method should not have its body removed")
-    def "Repose should not remove request bodies unless filters do so explicitly - bug"() {
+    @Unroll("bug - #method should not have its body removed")
+    def "bug - Repose should not remove request bodies unless filters do so explicitly"() {
+        given:
+        String requestBody = "request body"
+
         when:
-        def mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: "body content")
+        def mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: requestBody)
 
         then:
-        mc.handlings[0].request.body == "body content"
+        mc.getSentRequest().getBody().toString() == requestBody
+        mc.getHandlings().get(0).getRequest().getBody().toString() == requestBody
 
         where:
         method << ["GET"]
