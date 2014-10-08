3464c3c4a1d1ef5d00ff867231a96c53e938fe39
David Kowis <david.kowis@rackspace.com>
2014-10-08 09:58:10 -0500

    Removing re-testing of rate-limiting, and reusing payloads

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/rackspaceauthuser/RackspaceAuthUserTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/rackspaceauthuser/RackspaceAuthUserTest.groovy
@@ -6,58 +6,13 @@ import org.rackspace.deproxy.Handling
 import org.rackspace.deproxy.MessageChain
 import spock.lang.Unroll
 
+import static features.filters.rackspaceauthuser.RackspaceAuthPayloads.*
+
 /*
  * rackspace auth user test include test for both versions v2.0 and v1.1
  */
 
 class RackspaceAuthUserTest extends ReposeValveTest {
-
-    static Map contentXml = ["content-type": "application/xml"]
-    static Map contentJSON = ["content-type": "application/json"]
-
-    static String xmlPasswordCred = """<?xml version="1.0" encoding="UTF-8"?>
-<auth xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
- xmlns="http://docs.openstack.org/identity/api/v2.0">
-  <passwordCredentials username="demoauthor" password="theUsersPassword" tenantId="1100111"/>
-</auth>"""
-
-
-    static String xmlPasswordCredEmptyKey = """<?xml version="1.0" encoding="UTF-8"?>
-<auth xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
- xmlns="http://docs.openstack.org/identity/api/v2.0">
-  <passwordCredentials username="demoauthor" password="" tenantId="1100111"/>
-</auth>"""
-
-    static String invalidData = "Invalid data"
-    static String xmlOverLimit = "<auth xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://docs.openstack.org/identity/api/v2.0\"><credential xsi:type=\"PasswordCredentialsRequiredUsername\" username=\"012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789\" password=\"testpwd\" /></auth>"
-
-    static String jsonPasswordCred = """
-{
-    "auth":{
-        "passwordCredentials":{
-            "username":"demoauthor",
-            "password":"theUsersPassword"
-        },
-        "tenantId": "12345678"
-    }
-}
-"""
-
-    def static String jsonApiKeyCred = """{
-    "auth": {
-        "RAX-KSKEY:apiKeyCredentials": {
-            "username": "demoauthor",
-            "apiKey": "aaaaa-bbbbb-ccccc-12345678"
-        },
-        "tenantId": "1100111"
-    }
-}
-"""
-    //v1.1
-    static String jsonKeyCred11 = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"testpwd\"}}"
-    static String jsonKeyCredEmptyKey11 = "{ \"credentials\": { \"username\": \"test-user\", \"key\": \"\"}}"
-    static String xmlKeyCred11 = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"testpwd\" />"
-    static String xmlKeyCredEmptyKey11 = "<credentials xmlns=\"http://docs.rackspacecloud.com/auth/api/v1.1\" username=\"test-user\" key=\"\" />"
 
     def setupSpec() {
         deproxy = new Deproxy()
