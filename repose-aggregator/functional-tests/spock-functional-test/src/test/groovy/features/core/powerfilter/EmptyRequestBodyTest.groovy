bbfb4f94552045ed72da0c2600ebc2b17ef7cc67
Damien Johnson <damien.johnson@rackspace.com>
2014-10-15 12:58:25 -0500

    Removed the bug tag on the test for removing the body of a get request
    Also added a comment explaining the issue and did a small amount of cleanup

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/EmptyRequestBodyTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/powerfilter/EmptyRequestBodyTest.groovy
@@ -1,8 +1,6 @@
 package features.core.powerfilter
 
 import framework.ReposeValveTest
-import framework.category.Bug
-import org.junit.experimental.categories.Category
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
 import spock.lang.Unroll
@@ -19,16 +17,42 @@ class EmptyRequestBodyTest extends ReposeValveTest {
         repose.start()
     }
 
-    def "when GET request with body, Deproxy should forward body"() {
+    def "Deproxy should not remove the body of a HTTP request"() {
         given:
         String requestBody = "request body"
+        String deproxyEndpoint = "http://localhost:${properties.targetPort}"
 
         when:
-        MessageChain mc = deproxy.makeRequest(url: "http://localhost:${properties.targetPort}", requestBody: requestBody)
+        MessageChain mc = deproxy.makeRequest(url: deproxyEndpoint, requestBody: requestBody)
 
         then:
         mc.getSentRequest().getBody().toString() == requestBody
         mc.getHandlings().get(0).getRequest().getBody().toString() == requestBody
+
+        where:
+        method << ["GET", "HEAD", "PUT", "POST", "PATCH", "DELETE"]
+    }
+
+    // NOTE: The following test has proven contentious. In accordance with RFC 2616 Section 5, all HTTP requests may
+    // contain a request body. However, in the words of Roy Fielding:
+    // "Server semantics for GET, however, are restricted such that a body, if any, has no semantic meaning to the
+    // request. The requirements on parsing are separate from the requirements on method semantics."
+    // It has been decided that Repose will remove the request body for certain request types. No one should care.
+    // Anyone who does care has either written an invalid HTTP service or is a disputatious individual.
+    @Unroll("#method should have its body removed")
+    def "Repose should remove request bodies"() {
+        given:
+        String requestBody = "request body"
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: requestBody)
+
+        then:
+        mc.getSentRequest().getBody().toString() == requestBody
+        mc.getHandlings().get(0).getRequest().getBody().toString() == ""
+
+        where:
+        method << ["GET", "HEAD"]
     }
 
     @Unroll("#method should not have its body removed")
@@ -45,22 +69,5 @@ class EmptyRequestBodyTest extends ReposeValveTest {
 
         where:
         method << ["PUT", "POST", "PATCH", "DELETE"]
-    }
-
-    @Category(Bug)
-    @Unroll("bug - #method should not have its body removed")
-    def "bug - Repose should not remove request bodies unless filters do so explicitly"() {
-        given:
-        String requestBody = "request body"
-
-        when:
-        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: method, requestBody: requestBody)
-
-        then:
-        mc.getSentRequest().getBody().toString() == requestBody
-        mc.getHandlings().get(0).getRequest().getBody().toString() == requestBody
-
-        where:
-        method << ["GET"]
     }
 }
