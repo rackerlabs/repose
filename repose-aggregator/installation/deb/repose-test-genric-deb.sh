5a6ca00d93106e6f776918cdb09c162d6c7a5969
Damien Johnson <damien.johnson@rackspace.com>
2014-10-17 12:42:47 -0500

    Pulled over Bill's scripts

--- repose-aggregator/installation/deb/repose-test-genric-deb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y wget curl patch python-pip
-
-
-
-sudo dpkg -i                          /vagrant/repose-valve-*.deb /vagrant/repose-filter-bundle-*.deb /vagrant/repose-extensions-filter-bundle-*.deb
-sudo apt-get -f install -y
