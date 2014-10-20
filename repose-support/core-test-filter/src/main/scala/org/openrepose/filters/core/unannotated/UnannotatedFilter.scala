4beb1df0c770fc77eadf841eed0133684893816e
David Kowis <david.kowis@rackspace.com>
2014-10-20 15:14:33 -0500

    Updating support project to provide more failures

---
+++ repose-support/core-test-filter/src/main/scala/org/openrepose/filters/core/unannotated/UnannotatedFilter.scala
@@ -0,0 +1,17 @@
+package org.openrepose.filters.core.unannotated
+
+import javax.servlet._
+
+class UnannotatedFilter extends Filter {
+  override def init(p1: FilterConfig): Unit = {
+
+  }
+
+  override def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain): Unit = {
+
+  }
+
+  override def destroy(): Unit = {
+
+  }
+}
