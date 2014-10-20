66490944ee9a7d5ece25f7e79e51be6e02cb0748
David Kowis <david.kowis@rackspace.com>
2014-10-20 09:33:25 -0500

    Documentation

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/spring/CoreSpringContainer.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/spring/CoreSpringContainer.java
@@ -18,10 +18,23 @@ public class CoreSpringContainer {
         coreContext.refresh();
     }
 
+    /**
+     * Just gets you the core application context
+     * @return the core application context
+     */
     public ApplicationContext getCoreContext() {
         return coreContext;
     }
 
+    /**
+     * Provides an application context for a filter, given a classloader as to where that filter is.
+     * The application context will have that classloader set to it.
+     * @param loader the classloader from where to find the filter
+     * @param className the class of the filter
+     * @param contextName the given name of the context
+     * @return the application context for that filter
+     * @throws ClassNotFoundException
+     */
     public AbstractApplicationContext getContextForFilter(ClassLoader loader, String className, String contextName) throws ClassNotFoundException {
         AnnotationConfigApplicationContext filterContext = new AnnotationConfigApplicationContext();
         filterContext.setClassLoader(loader);
