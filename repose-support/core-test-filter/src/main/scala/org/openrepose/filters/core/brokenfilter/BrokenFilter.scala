708a13fc62619b41dad906a59b9ddb234b3c358b
David Kowis <david.kowis@rackspace.com>
2014-10-20 15:14:33 -0500

    Updating support project to provide more failures

---
+++ repose-support/core-test-filter/src/main/scala/org/openrepose/filters/core/brokenfilter/BrokenFilter.scala
@@ -0,0 +1,17 @@
+package org.openrepose.filters.core.brokenfilter
+
+import javax.inject.Named
+import javax.servlet._
+
+@Named
+class BrokenFilter extends Filter{
+  override def init(p1: FilterConfig): Unit = {
+    throw new RuntimeException("WELL MY HEAD ASPLODE")
+  }
+
+  override def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain): Unit = {}
+
+  override def destroy(): Unit = {
+
+  }
+}
