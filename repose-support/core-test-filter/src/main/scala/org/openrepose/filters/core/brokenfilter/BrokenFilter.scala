8a79a617c1ced64feffbc0ff71d35d011e368e2c
David Kowis <david.kowis@rackspace.com>
2014-10-20 15:58:59 -0500

    Oops, lets throw the right kind of exception

--- repose-support/core-test-filter/src/main/scala/org/openrepose/filters/core/brokenfilter/BrokenFilter.scala
+++ repose-support/core-test-filter/src/main/scala/org/openrepose/filters/core/brokenfilter/BrokenFilter.scala
@@ -6,7 +6,7 @@ import javax.servlet._
 @Named
 class BrokenFilter extends Filter{
   override def init(p1: FilterConfig): Unit = {
-    throw new RuntimeException("WELL MY HEAD ASPLODE")
+    throw new ServletException("WELL MY HEAD ASPLODE")
   }
 
   override def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain): Unit = {}
