094960ea16bc490bef733d25b7ea9f521ab8f7d6
David Kowis <david.kowis@rackspace.com>
2014-10-20 11:26:28 -0500

    Making this not a runtime Exception any longer

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/filter/FilterInitializationException.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/filter/FilterInitializationException.java
@@ -1,10 +1,11 @@
 package org.openrepose.core.filter;
 
-/**
- * @author fran
- */
-public class FilterInitializationException extends RuntimeException {
+public class FilterInitializationException extends Exception {
     public FilterInitializationException(String message, Throwable t) {
         super(message, t);
+    }
+
+    public FilterInitializationException(String message) {
+        super(message);
     }
 }
