57dfcef2e47977e70b49cbfa667d99cdf64a87de
David Kowis <david.kowis@rackspace.com>
2014-10-20 14:59:06 -0500

    make the root deploy directory accessable

--- repose-aggregator/core/core-lib/src/test/scala/org/openrepose/core/spring/TestFilterBundlerHelper.scala
+++ repose-aggregator/core/core-lib/src/test/scala/org/openrepose/core/spring/TestFilterBundlerHelper.scala
@@ -7,10 +7,11 @@ import com.typesafe.config.ConfigFactory
 import org.apache.commons.io.filefilter.WildcardFileFilter
 
 trait TestFilterBundlerHelper {
+  lazy val testFilterBundleRoot = new File(ConfigFactory.load("test.properties").getString("coreTestFilterBundleLocation"))
+
   lazy val testFilterBundleClassLoader: ClassLoader = {
-    val directory: File = new File(ConfigFactory.load("test.properties").getString("coreTestFilterBundleLocation"))
     val fileFilter: FileFilter = new WildcardFileFilter("core-test-filter-*.jar")
-    val files: Array[File] = directory.listFiles(fileFilter)
+    val files: Array[File] = testFilterBundleRoot.listFiles(fileFilter)
     new URLClassLoader(Array(files(0).toURI.toURL), getClass.getClassLoader)
   }
 
