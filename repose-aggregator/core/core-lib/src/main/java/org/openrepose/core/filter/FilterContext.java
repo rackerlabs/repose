d318bc158d7ce4592f6ec2f558b36c15e34f8cb6
David Kowis <david.kowis@rackspace.com>
2014-10-21 09:42:09 -0500

    Formatted, and deleted useless test

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/filter/FilterContext.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/filter/FilterContext.java
@@ -1,73 +1,74 @@
 package org.openrepose.core.filter;
 
 import org.openrepose.commons.utils.Destroyable;
+
 import java.util.regex.Pattern;
 
 import javax.servlet.Filter;
 
 public class FilterContext implements Destroyable {
 
-   private final ClassLoader filterClassLoader;
-   private final Filter filter;
-   private final org.openrepose.core.systemmodel.Filter filterConfig;
-   private final String name;
-   private final String regex;
-   private final Pattern pattern;
+    private final ClassLoader filterClassLoader;
+    private final Filter filter;
+    private final org.openrepose.core.systemmodel.Filter filterConfig;
+    private final String name;
+    private final String regex;
+    private final Pattern pattern;
 
-   public FilterContext(Filter filter, ClassLoader filterClassLoader) {
-      this(filter, filterClassLoader, null);
-   }
+    public FilterContext(Filter filter, ClassLoader filterClassLoader) {
+        this(filter, filterClassLoader, null);
+    }
 
-   public FilterContext(Filter filter, ClassLoader filterClassLoader, org.openrepose.core.systemmodel.Filter filterConfig) {
-      this.filter = filter;
-      this.filterClassLoader = filterClassLoader;
-      this.filterConfig = filterConfig;
-      if (filterConfig != null && filterConfig.getUriRegex() != null) {
-         filterConfig.getName();
-         this.name = filterConfig.getName();
-         this.regex = filterConfig.getUriRegex();
-         this.pattern = Pattern.compile(regex);
-      } else {
-         this.name = "n/a";
-         this.regex = ".*";
-         this.pattern = Pattern.compile(this.regex);
-      }
+    public FilterContext(Filter filter, ClassLoader filterClassLoader, org.openrepose.core.systemmodel.Filter filterConfig) {
+        this.filter = filter;
+        this.filterClassLoader = filterClassLoader;
+        this.filterConfig = filterConfig;
+        if (filterConfig != null && filterConfig.getUriRegex() != null) {
+            filterConfig.getName();
+            this.name = filterConfig.getName();
+            this.regex = filterConfig.getUriRegex();
+            this.pattern = Pattern.compile(regex);
+        } else {
+            this.name = "n/a";
+            this.regex = ".*";
+            this.pattern = Pattern.compile(this.regex);
+        }
 
-   }
+    }
 
-   public Filter getFilter() {
-      return filter;
-   }
+    public Filter getFilter() {
+        return filter;
+    }
 
-   public ClassLoader getFilterClassLoader() {
-      return filterClassLoader;
-   }
+    public ClassLoader getFilterClassLoader() {
+        return filterClassLoader;
+    }
 
-   public org.openrepose.core.systemmodel.Filter getFilterConfig() {
-      return filterConfig;
-   }
+    public org.openrepose.core.systemmodel.Filter getFilterConfig() {
+        return filterConfig;
+    }
 
-   public Pattern getUriPattern() {
-      return pattern;
-   }
-   
-   public String getName() {
-      return name;
-   }
-   
-   public boolean isFilterAvailable() {
-      return filter != null;
-   }
+    public Pattern getUriPattern() {
+        return pattern;
+    }
 
-   public String getUriRegex() {
-      return regex;
-   }
+    public String getName() {
+        return name;
+    }
 
-   @Override
-   public void destroy() {
-       //TODO: maybe hand this guy the filter application context so it can be tanked as well
-      if (filter != null) {
-         filter.destroy();
-      }
-   }
+    public boolean isFilterAvailable() {
+        return filter != null;
+    }
+
+    public String getUriRegex() {
+        return regex;
+    }
+
+    @Override
+    public void destroy() {
+        //TODO: maybe hand this guy the filter application context so it can be tanked as well
+        if (filter != null) {
+            filter.destroy();
+        }
+    }
 }
