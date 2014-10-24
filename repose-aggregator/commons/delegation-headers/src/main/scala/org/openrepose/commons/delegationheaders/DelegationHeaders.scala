f5a100b3c7a84035fdc1f855e61d17bb2b9383f8
Damien Johnson <damien.johnson@rackspace.com>
2014-10-24 13:31:06 -0500

    Initial file structure and design

---
+++ repose-aggregator/commons/delegation-headers/src/main/scala/org/openrepose/commons/delegationheaders/DelegationHeaders.scala
@@ -0,0 +1,8 @@
+package org.openrepose.commons.delegationheaders
+
+/**
+ * An object which enumerates all of the header names used in the delegation protocol.
+ */
+object DelegationHeaders {
+
+}
---
+++ repose-aggregator/commons/delegation-headers/src/main/scala/org/openrepose/commons/delegationheaders/filter/DelegationFilter.scala
@@ -0,0 +1,8 @@
+package org.openrepose.commons.delegationheaders.filter
+
+/**
+ * A trait which a Scala filter can mixin to provide delegation functionality.
+ */
+trait DelegationFilter {
+
+}
