c019106911d9960cfbf10f67b46bc7e5d78373f0
Damien Johnson <damien.johnson@rackspace.com>
2014-10-24 13:31:06 -0500

    Initial file structure and design

---
+++ repose-aggregator/commons/delegation-headers/src/main/scala/org/openrepose/commons/delegationheaders/DelegationHeaderManager.scala
@@ -0,0 +1,8 @@
+package org.openrepose.commons.delegationheaders
+
+/**
+ * The API for the delegation headers library.
+ */
+trait DelegationHeaderManager {
+
+}
---
+++ repose-aggregator/commons/delegation-headers/src/main/scala/org/openrepose/commons/delegationheaders/impl/DelegationHeaderManagerImpl.scala
@@ -0,0 +1,10 @@
+package org.openrepose.commons.delegationheaders.impl
+
+/**
+ * An implementation of the API defined in [[org.openrepose.commons.delegationheaders.DelegationHeaderManager]].
+ *
+ * This object provides functions to manipulate delegation headers.
+ */
+object DelegationHeaderManagerImpl {
+
+}
