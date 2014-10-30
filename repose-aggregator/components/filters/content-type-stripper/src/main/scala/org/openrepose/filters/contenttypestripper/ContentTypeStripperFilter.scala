9937b3e119117bd5bb8ed24a2c44848ca11e136e
Tyler Royal <tyler.royal@rackspace.com>
2014-10-30 13:56:12 -0500

    start of filter by updating poms and initial setup

---
+++ repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/ContentTypeStripperFilter.scala
@@ -0,0 +1,11 @@
+package org.openrepose.filters.contenttypestripper
+
+import javax.servlet._
+
+class ContentTypeStripperFilter extends Filter {
+  override def init(p1: FilterConfig): Unit = ???
+
+  override def doFilter(p1: ServletRequest, p2: ServletResponse, p3: FilterChain): Unit = ???
+
+  override def destroy(): Unit = ???
+}
