a2c2034fbeea8b5f36f25af9655b4f24afdea187
Tyler Royal <tyler.royal@rackspace.com>
2014-11-03 14:30:12 -0600

    created a wrapper for the httpservletrequestwrapper that overrides inputstream with a pushback along with implementing a blacklist for headers that we dont want shown

---
+++ repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/PushBackHttpServletRequestWrapper.scala
@@ -0,0 +1,31 @@
+package org.openrepose.filters.contenttypestripper
+
+import java.io.PushbackInputStream
+import java.util
+import java.util.concurrent.CopyOnWriteArrayList
+import javax.servlet.ServletInputStream
+import javax.servlet.http.{HttpServletRequest, HttpServletRequestWrapper}
+
+import org.openrepose.commons.utils.io.stream.ServletInputStreamWrapper
+
+class PushBackHttpServletRequestWrapper(request: HttpServletRequest) extends HttpServletRequestWrapper(request) {
+  private val headerBlacklist: CopyOnWriteArrayList[String] = new CopyOnWriteArrayList[String]
+  val pushbackInputStream: PushbackInputStream = new PushbackInputStream(request.getInputStream)
+
+  override def getInputStream: ServletInputStream = new ServletInputStreamWrapper(pushbackInputStream)
+
+  override def getHeaderNames: util.Enumeration[String] = {
+    import scala.collection.JavaConversions._
+    super.getHeaderNames.collect { case name:String if !headerBlacklist.contains(name.toLowerCase) => name }
+  }
+
+  override def getHeader(headerName: String): String = {
+    if (headerBlacklist.contains(headerName.toLowerCase)) {
+      null
+    } else {
+      super.getHeader(headerName)
+    }
+  }
+
+  def addToHeaderBlacklist(headerName: String) = headerBlacklist.add(headerName.toLowerCase)
+}
---
+++ repose-aggregator/components/filters/content-type-stripper/src/test/scala/org/openrepose/filters/contenttypestripper/PushBackHttpServletRequestWrapperTest.scala
@@ -0,0 +1,49 @@
+package org.openrepose.filters.contenttypestripper
+
+import java.util
+
+import org.scalatest.{Matchers, FunSpec}
+import org.springframework.mock.web.MockHttpServletRequest
+
+class PushBackHttpServletRequestWrapperTest extends FunSpec with Matchers{
+  describe("The PushBackHttpServletRequestWrapper") {
+    val mockRequest: MockHttpServletRequest = new MockHttpServletRequest()
+    mockRequest.addHeader("Content-Type", "application/foo")
+    mockRequest.addHeader("X-Auth", "123456")
+
+    it("should get all the header names on the request") {
+      val wrapper = new PushBackHttpServletRequestWrapper(mockRequest)
+      val headerNames: util.Enumeration[String] = wrapper.getHeaderNames
+      List("Content-Type", "X-Auth").map(_ shouldBe headerNames.nextElement)
+      intercept[NoSuchElementException] {
+        headerNames.nextElement
+      }
+    }
+    it("should get all the header names but ones added to the blacklist") {
+      val wrapper = new PushBackHttpServletRequestWrapper(mockRequest)
+      wrapper.addToHeaderBlacklist("Content-Type")
+      val headerNames: util.Enumeration[String] = wrapper.getHeaderNames
+      headerNames.nextElement shouldBe "X-Auth"
+      intercept[NoSuchElementException] {
+        headerNames.nextElement
+      }
+    }
+    it("should be able to get a header by name") {
+      val wrapper = new PushBackHttpServletRequestWrapper(mockRequest)
+      wrapper.getHeader("Content-Type") shouldBe "application/foo"
+      wrapper.getHeader("X-Auth") shouldBe "123456"
+    }
+    it("should return nothing when a header is asked for that is on the blacklist") {
+      val wrapper = new PushBackHttpServletRequestWrapper(mockRequest)
+      wrapper.addToHeaderBlacklist("Content-Type")
+      wrapper.getHeader("X-Auth") shouldBe "123456"
+      wrapper.getHeader("Content-Type") shouldBe null
+    }
+    it("should be case insensitive when checking a header on the blacklist") {
+      val wrapper = new PushBackHttpServletRequestWrapper(mockRequest)
+      wrapper.addToHeaderBlacklist("content-TyPe")
+      wrapper.getHeader("X-Auth") shouldBe "123456"
+      wrapper.getHeader("Content-Type") shouldBe null
+    }
+  }
+}
