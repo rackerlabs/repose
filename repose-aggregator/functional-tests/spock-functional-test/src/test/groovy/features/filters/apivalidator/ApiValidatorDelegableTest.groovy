1a0fbe663b85257e5eef9366553b85727531fcc7
jenny.vo <jenny.vo@rackspace.com>
2014-11-03 16:50:19 -0600

    add api Validator delegable test

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegableTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegableTest.groovy
@@ -38,7 +38,7 @@ class ApiValidatorDelegableTest extends ReposeValveTest{
         MessageChain mc
 
         when:
-        mc = deproxy.makeRequest(url: reposeEndpoint + "/a", method: method, headers: headers)
+        mc = deproxy.makeRequest(url: reposeEndpoint + path, method: method, headers: headers)
 
         then:
         mc.getReceivedResponse().getCode().equals(responseCode)
@@ -51,11 +51,11 @@ class ApiValidatorDelegableTest extends ReposeValveTest{
         "PUT"   | "/a"  | ["x-roles": "raxrole-test1, a:admin"]     | "200"         | "405;component=api-checker;msg=Bad method: PUT. The Method does not match the pattern: 'DELETE|GET|POST';q=0.5"
         "POST"  | "/a"  | ["x-roles": "raxrole-test1, a:observer"]  | "200"         | "405;component=api-checker;msg=Bad method: POST. The Method does not match the pattern: 'GET';q=0.5"
         "POST"  | "/a"  | ["x-roles": "raxrole-test1, a:bar"]       | "200"         | "404;component=api-checker;msg=Resource not found: /{a};q=0.5"
-        "GET"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "404;component=api-checker;msg=Resource not found: /{a};q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "404;component=api-checker;msg=Resource not found: /{a};q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, b:observer"]  | "200"         | "404;component=api-checker;msg=Resource not found: /{a}. The URI segment does not match the pattern: 'b';q=0.2"
-        "DELETE"| "/b"  | ["x-roles": "raxrole-test2, b:bar"]       | "200"         | "404;component=api-checker;msg=Resource not found: /{a};q=0.2"
-        "POST"  | "/b"  | ["x-roles": "raxrole-test2, b:admin"]     | "200"         | "404;component=api-checker;msg=Resource not found: /{a}. The URI segment does not match the pattern: 'b';q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, a:admin"]     | "200"         | "404;component=api-checker;msg=Resource not found: /{a};q=0.2"
+        "GET"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "404;component=api-checker;msg=Resource not found: /{b};q=0.2"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "404;component=api-checker;msg=Resource not found: /{b};q=0.2"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, b:observer"]  | "200"         | "405;component=api-checker;msg=Bad method: PUT. The Method does not match the pattern: 'GET';q=0.2"
+        "DELETE"| "/b"  | ["x-roles": "raxrole-test2, b:bar"]       | "200"         | "404;component=api-checker;msg=Resource not found: /{b};q=0.2"
+        "POST"  | "/b"  | ["x-roles": "raxrole-test2, b:admin"]     | "200"         | "405;component=api-checker;msg=Bad method: POST. The Method does not match the pattern: 'DELETE|GET|PUT';q=0.2"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, a:admin"]     | "200"         | "404;component=api-checker;msg=Resource not found: /{b};q=0.2"
     }
 }
