ccd7e3744a9ea80f0659f7433080e8771ca8957e
Tyler Royal <tyler.royal@rackspace.com>
2014-11-04 14:22:59 -0600

    switched wrapper for mutablehttpservletrequestwrapper

--- repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/ContentTypeStripperFilter.scala
+++ repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/ContentTypeStripperFilter.scala
@@ -1,10 +1,13 @@
 package org.openrepose.filters.contenttypestripper
 
+import java.io.PushbackInputStream
 import javax.servlet._
 import javax.servlet.http.HttpServletRequest
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.http.HttpHeaders
+import org.openrepose.commons.utils.io.stream.ServletInputStreamWrapper
+import org.openrepose.commons.utils.servlet.http.MutableHttpServletRequest
 
 import scala.collection.JavaConversions.enumerationAsScalaIterator
 
@@ -13,14 +16,16 @@ class ContentTypeStripperFilter extends Filter {
   override def init(p1: FilterConfig): Unit = {}
 
   override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
-    val pushBackRequest = new PushBackHttpServletRequestWrapper(request.asInstanceOf[HttpServletRequest])
+    val pushBackRequest = MutableHttpServletRequest.wrap(request.asInstanceOf[HttpServletRequest])
     if(pushBackRequest.getHeaderNames exists ( _.equalsIgnoreCase(HttpHeaders.CONTENT_TYPE))) {
       val byteArray:Array[Byte] = new Array[Byte](8)
-      val bytesRead: Int = pushBackRequest.pushbackInputStream.read(byteArray, 0, 8)
-      if (bytesRead == -1 || StringUtils.isBlank(new String(byteArray).substring(0,bytesRead))) {
-        pushBackRequest.addToHeaderBlacklist(HttpHeaders.CONTENT_TYPE)
+      val pushbackStream = new PushbackInputStream(pushBackRequest.getInputStream, 8)
+      val bytesRead: Int = pushbackStream.read(byteArray, 0, 8)
+      if (bytesRead == -1 || StringUtils.isBlank(new String(byteArray.slice(0,bytesRead)))) {
+        pushBackRequest.removeHeader(HttpHeaders.CONTENT_TYPE)
       }
-      pushBackRequest.pushbackInputStream.unread(byteArray.slice(0,bytesRead))
+      pushbackStream.unread(byteArray.slice(0,bytesRead))
+      pushBackRequest.setInputStream(new ServletInputStreamWrapper(pushbackStream))
     }
     chain.doFilter(pushBackRequest, response)
   }
