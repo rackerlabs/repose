ccf36ffcaaa83174ce93fb9adee894e03e2444ab
wdschei <Bill.Scheidegger@Rackspace.com>
2014-11-05 14:43:18 -0600

    Updated the LoggingServiceImpl to the new Log4J 2.x way of changing out the configuration file location. Removed some lines in ReposeValveLauncher that were inadvertantly commited.

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingServiceImpl.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingServiceImpl.java
@@ -1,20 +1,33 @@
 package org.openrepose.core.services.logging;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 
 /**
  * @author fran
  */
 public class LoggingServiceImpl implements LoggingService {
-    public LoggingServiceImpl(){}
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(LoggingServiceImpl.class);
+
+    public LoggingServiceImpl() {
+    }
 
     @Override
     public void updateLoggingConfiguration(URI configLocation) {
-        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        Configurator.initialize(ctx.getName(), null, configLocation);
+        try {
+            URL url = configLocation.toURL();
+            ConfigurationSource configurationSource = new ConfigurationSource(url.openStream(), url);
+            XmlConfiguration xmlConfiguration = new XmlConfiguration(configurationSource);
+            ((Logger) LogManager.getRootLogger()).getContext().start(xmlConfiguration);
+        } catch (IOException e) {
+            LOG.info("Failed to load the new Logging configuration from " + configLocation, e);
+        }
     }
 }
