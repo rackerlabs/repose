7be86e6d74b8e3e887cb0a93a4f185eb1927c436
jenny.vo <jenny.vo@rackspace.com>
2014-11-11 14:21:41 -0600

    update tests to reflect the changes

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegatingTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegatingTest.groovy
@@ -6,7 +6,7 @@ import spock.lang.Unroll
 /**
  * Created by jennyvo on 11/3/14.
  */
-class ApiValidatorDelegableTest extends ReposeValveTest{
+class ApiValidatorDelegatingTest extends ReposeValveTest{
 
     def setupSpec() {
         deproxy = new Deproxy()
@@ -32,7 +32,7 @@ class ApiValidatorDelegableTest extends ReposeValveTest{
         - next filter (if exists) with failed message
         - to origin service with failed message and up to origin service handle
     */
-    @Unroll("Delegable:headers=#headers, failed message=#delegateMsg")
+    @Unroll("Delegating:headers=#headers, failed message=#delegateMsg")
     def "when delegating is true, Repose can delegate invalid request with failed reason to origin service handle"() {
         given:
         MessageChain mc
@@ -51,11 +51,11 @@ class ApiValidatorDelegableTest extends ReposeValveTest{
         "PUT"   | "/a"  | ["x-roles": "raxrole-test1, a:admin"]     | "200"         | "status_code=405`component=api-checker`message=Bad method: PUT. The Method does not match the pattern: 'DELETE|GET|POST';q=0.5"
         "POST"  | "/a"  | ["x-roles": "raxrole-test1, a:observer"]  | "200"         | "status_code=405`component=api-checker`message=Bad method: POST. The Method does not match the pattern: 'GET';q=0.5"
         "POST"  | "/a"  | ["x-roles": "raxrole-test1, a:bar"]       | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{a};q=0.5"
-        "GET"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, b:observer"]  | "200"         | "status_code=405`component=api-checker`message=Bad method: PUT. The Method does not match the pattern: 'GET';q=0.2"
-        "DELETE"| "/b"  | ["x-roles": "raxrole-test2, b:bar"]       | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.2"
-        "POST"  | "/b"  | ["x-roles": "raxrole-test2, b:admin"]     | "200"         | "status_code=405`component=api-checker`message=Bad method: POST. The Method does not match the pattern: 'DELETE|GET|PUT';q=0.2"
-        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, a:admin"]     | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.2"
+        "GET"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.5"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2"]              | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.5"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, b:observer"]  | "200"         | "status_code=405`component=api-checker`message=Bad method: PUT. The Method does not match the pattern: 'GET';q=0.5"
+        "DELETE"| "/b"  | ["x-roles": "raxrole-test2, b:bar"]       | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.5"
+        "POST"  | "/b"  | ["x-roles": "raxrole-test2, b:admin"]     | "200"         | "status_code=405`component=api-checker`message=Bad method: POST. The Method does not match the pattern: 'DELETE|GET|PUT';q=0.5"
+        "PUT"   | "/b"  | ["x-roles": "raxrole-test2, a:admin"]     | "200"         | "status_code=404`component=api-checker`message=Resource not found: /{b};q=0.5"
     }
 }
--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegatingWAuthTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegatingWAuthTest.groovy
@@ -7,12 +7,9 @@ import org.rackspace.deproxy.MessageChain
 import org.rackspace.deproxy.Response
 import spock.lang.Unroll
 /**
- * Created by jennyvo on 11/4/14.
- *  Multiple filters with delegable option:
- *  - Api Validator with delegable set to true
- *  - Client Auth with delegating set to true
+ * Created by jennyvo on 11/3/14.
  */
-class ApiValidatorDelegableWAuthDelegableTest extends ReposeValveTest {
+class ApiValidatorDelegatingWAuthTest extends ReposeValveTest {
 
     def static originEndpoint
     def static identityEndpoint
@@ -27,7 +24,7 @@ class ApiValidatorDelegableWAuthDelegableTest extends ReposeValveTest {
         repose.configurationProvider.applyConfigs("common", params)
         repose.configurationProvider.applyConfigs("features/filters/apivalidator/common", params)
         repose.configurationProvider.applyConfigs("features/filters/apivalidator/delegable", params)
-        repose.configurationProvider.applyConfigs("features/filters/apivalidator/delegable/withauthdelegable", params)
+        repose.configurationProvider.applyConfigs("features/filters/apivalidator/delegable/withauth", params)
         repose.start()
 
         originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
@@ -49,20 +46,18 @@ class ApiValidatorDelegableWAuthDelegableTest extends ReposeValveTest {
         fakeIdentityService.resetHandlers()
     }
 
-    @Unroll("Request with tenant:#reqtenant,admin-role: #adminrole, roles: #roles, method: #method")
+    @Unroll("Sending request with roles: #roles, method: #method")
     def "when using api validate with delegate and auth client"() {
 
         given:
         fakeIdentityService.with {
-            client_tenant = 12345
-            client_token = clienttoken
+            client_token = UUID.randomUUID().toString()
             tokenExpiresAt = DateTime.now().plusDays(1)
-            service_admin_role = adminrole
         }
 
         when: "User passes a request through repose"
         MessageChain mc = deproxy.makeRequest(
-                url: "$reposeEndpoint/a/$reqtenant",
+                url: "$reposeEndpoint/a",
                 method: method,
                 headers: [
                         'x-roles': roles,
@@ -76,31 +71,12 @@ class ApiValidatorDelegableWAuthDelegableTest extends ReposeValveTest {
         mc.handlings.size() == 1
         mc.handlings[0].request.headers.getFirstValue("X-Delegated") == delegateMsg
 
-        and: "If request made it to origin service"
-        if (identitystatus == "Confirmed") {
-            def request2 = mc.handlings[0].request
-            assert(mc.handlings[0].endpoint == originEndpoint)
-            assert(request2.headers.contains("x-auth-token"))
-            assert(request2.headers.contains("x-identity-status"))
-            assert(request2.headers.contains("x-authorization"))
-            assert(request2.headers.getFirstValue("x-identity-status") == identitystatus)
-            assert(request2.headers.getFirstValue("x-authorization").startsWith("Proxy"))
-        }
-
-        and: "If identityStatus was Indeterminate"
-        if (identitystatus == "Indeterminate") {
-
-            def request2 = mc.handlings[0].request
-            assert(request2.headers.getFirstValue("x-identity-status") == identitystatus)
-            assert(request2.headers.getFirstValue("x-authorization").startsWith("Proxy"))
-        }
-
         where:
-        reqtenant| adminrole | clienttoken     | roles                      | method   | responseCode | identitystatus | delegateMsg
-        ""       | "regular" |""               | "raxrole-test1"            | "GET"    | "200"        | "Indeterminate"| "status_code=404`component=api-checker`message=Resource not found: /{a};q=0.5"
-        "test"   | "admin1"  |UUID.randomUUID()| "raxrole-test1,a:observer" | "POST"   | "200"        | "Confirmed"    | "status_code=404`component=api-checker`message=Resource not found: /a/{test};q=0.5"
-        "test"   | "admin2"  |UUID.randomUUID()| "raxrole-test1,a:observer" | "DELETE" | "200"        | "Confirmed"    | "status_code=404`component=api-checker`message=Resource not found: /a/{test};q=0.5"
-        12345    | "default" |UUID.randomUUID()| "raxrole-test1,a:admin"    | "PUT"    | "200"        | "confirmed"    | "status_code=404`component=api-checker`message=Resource not found: /a/{12345};q=0.5"
+        roles                       | method     | responseCode | delegateMsg
+        "raxrole-test1"             | "GET"      | "200"        | "status_code=404`component=api-checker`message=Resource not found: /{a};q=0.5"
+        "raxrole-test1,a:observer"  | "POST"     | "200"        | "status_code=405`component=api-checker`message=Bad method: POST. The Method does not match the pattern: 'GET';q=0.5"
+        "raxrole-test1,a:observer"  | "DELETE"   | "200"        | "status_code=405`component=api-checker`message=Bad method: DELETE. The Method does not match the pattern: 'GET';q=0.5"
+        "raxrole-test1,a:admin"     | "PUT"      | "200"        | "status_code=405`component=api-checker`message=Bad method: PUT. The Method does not match the pattern: 'DELETE|GET|POST';q=0.5"
     }
 
     @Unroll("Sending request with roles: #roles and admin resp: #authresp")
@@ -110,7 +86,6 @@ class ApiValidatorDelegableWAuthDelegableTest extends ReposeValveTest {
         fakeIdentityService.with {
             client_token = UUID.randomUUID().toString()
             tokenExpiresAt = DateTime.now().plusDays(1)
-            client_tenant = 123
         }
 
         if(authresp != 200){
