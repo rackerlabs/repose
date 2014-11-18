eacc94389137fc1169804a30e59bb8b45dd44533
wdschei <Bill.Scheidegger@Rackspace.com>
2014-11-18 15:50:10 -0600

    Finished fleshing out the tests and the implementation.

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingServiceImpl.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingServiceImpl.java
@@ -20,7 +20,11 @@ public class LoggingServiceImpl implements LoggingService {
 
     @Override
     public void updateLoggingConfiguration(String configFileName) {
-        if (configFileName != null && !configFileName.equals(currentConfigFileName)) {
+        if (configFileName == null) {
+            LOG.debug("Requested to reload a NULL configuration.");
+        } else if (configFileName.equals(currentConfigFileName)) {
+            LOG.debug("Requested to reload the same configuration: {}", configFileName);
+        } else {
             URL url = null;
             try {
                 url = new URL(configFileName);
@@ -37,6 +41,7 @@ public class LoggingServiceImpl implements LoggingService {
                 file = new File(currentConfigDir, configFileName);
             }
             if (file.canRead()) {
+                currentConfigFileName = configFileName;
                 System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, file.getAbsolutePath());
                 LoggerContext context = (LoggerContext) LogManager.getContext(false);
                 context.reconfigure();
@@ -46,12 +51,17 @@ public class LoggingServiceImpl implements LoggingService {
                 LOG.debug("DEBUG LEVEL LOG STATEMENT");
                 LOG.trace("TRACE LEVEL LOG STATEMENT");
             } else if (currentConfigFileName == null) {
-                // BE LOUD - Bad Location on Initial Load
+                String fallBackResource = this.getClass().getResource("/org/openrepose/core/services/logging/log4j2-DEFAULT.xml").toString();
+                System.setProperty(ConfigurationFactory.CONFIGURATION_FILE_PROPERTY, fallBackResource);
+                LoggerContext context = (LoggerContext) LogManager.getContext(false);
+                context.reconfigure();
+                LOG.error("================================================================================");
+                LOG.error("|| ERROR FALLING BACK TO THE DEFAULT LOGGING CONFIGURATION!!!                 ||");
+                LOG.error("|| NO LOGS ARE BEING CREATED. ALL OUTPUT IS ONLY ON STANDARD OUT              ||");
+                LOG.error("================================================================================");
             } else {
-                // BE LOUD - Bad Location after running
+                LOG.warn("An attempt was made to switch to an invalid Logging Configuration file: {}", configFileName);
             }
-        } else {
-            // Location was NULL or the same as the one already loaded.
         }
     }
 }
