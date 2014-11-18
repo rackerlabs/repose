5961fe66e1fc736b3d89ffc59f4b2aec7db23110
Damien Johnson <damien.johnson@rackspace.com>
2014-11-18 10:49:25 -0600

    Setting the status code to a non-500 when delegating

--- repose-aggregator/components/filters/client-auth/src/main/java/org/openrepose/filters/clientauth/openstack/v1_0/OpenStackAuthenticationHeaderManager.java
+++ repose-aggregator/components/filters/client-auth/src/main/java/org/openrepose/filters/clientauth/openstack/v1_0/OpenStackAuthenticationHeaderManager.java
@@ -87,6 +87,7 @@ public class OpenStackAuthenticationHeaderManager {
             setDelegationHeader();
         } else if (isDelagable) {
             filterDirector.setFilterAction(FilterAction.PASS);
+            filterDirector.setResponseStatusCode(200); // Note: The response status code must be set to a non-500 so that the request will be routed appropriately.
             setExtendedAuthorization();
             setIdentityStatus();
             setDelegationHeader();
