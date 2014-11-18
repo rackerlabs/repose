30e830eed38a70e62852615ff73269778d97510a
wdschei <Bill.Scheidegger@Rackspace.com>
2014-11-18 09:54:11 -0600

    Updated the Logging Service to accept a filename and test for full URL or if it should be relatively pathed. Moved most of the functionality out of the Context and into the Impl in preparation for the latest Springification.

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/impl/LoggingServiceContext.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/impl/LoggingServiceContext.java
@@ -21,16 +21,12 @@ import java.net.URL;
 /**
  * @author fran
  */
-public class LoggingServiceContext implements ServiceContext<LoggingService>, ApplicationContextAware {
-
-   private static final Logger LOG = LoggerFactory.getLogger(LoggingServiceContext.class);
+public class LoggingServiceContext implements ServiceContext<LoggingService> {
    public static final String SERVICE_NAME = "powerapi:/services/logging";
    private final LoggingService loggingService;
    private ConfigurationService configurationManager;
    private final ContainerConfigurationListener configurationListener;
-   private File loggingConfigurationFile = null;
    private final ServiceRegistry registry;
-   private ApplicationContext applicationContext;
 
    public LoggingServiceContext(LoggingService loggingService, ServiceRegistry registry, ConfigurationService configurationManager) {
       this.loggingService = loggingService;
@@ -55,11 +51,6 @@ public class LoggingServiceContext implements ServiceContext<LoggingService>, Ap
       return loggingService;
    }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-       this.applicationContext = applicationContext;
-    }
-
     /**
     * Listens for updates to the container.cfg.xml file which holds the location of the log properties file.
     */
@@ -69,25 +60,8 @@ public class LoggingServiceContext implements ServiceContext<LoggingService>, Ap
 
       @Override
       public void configurationUpdated(ContainerConfiguration configurationObject) {
-
          if (configurationObject.getDeploymentConfig() != null) {
-            final LoggingConfiguration loggingConfig = configurationObject.getDeploymentConfig().getLoggingConfiguration();
-
-            if (loggingConfig != null && !StringUtilities.isBlank(loggingConfig.getHref())) {
-               final File newLoggingConfigFile = new File(loggingConfig.getHref());
-               if(loggingConfigurationFile == null ||
-                        !newLoggingConfigFile.getAbsolutePath().equals(loggingConfigurationFile.getAbsolutePath())) {
-                  loggingConfigurationFile = newLoggingConfigFile;
-                  LoggingServiceContext.this.loggingConfigurationFile = newLoggingConfigFile;
-                  loggingService.updateLoggingConfiguration(LoggingServiceContext.this.loggingConfigurationFile);
-
-                  LOG.error("ERROR LEVEL LOG STATEMENT");
-                  LOG.warn("WARN  LEVEL LOG STATEMENT");
-                  LOG.info("INFO  LEVEL LOG STATEMENT");
-                  LOG.debug("DEBUG LEVEL LOG STATEMENT");
-                  LOG.trace("TRACE LEVEL LOG STATEMENT");
-                }
-            }
+            loggingService.updateLoggingConfiguration(configurationObject.getDeploymentConfig().getLoggingConfiguration().getHref());
          }
          isInitialized = true;
       }
--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingService.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/logging/LoggingService.java
@@ -1,10 +1,5 @@
 package org.openrepose.core.services.logging;
 
-import java.io.File;
-
-/**
- * @author fran
- */
 public interface LoggingService {
-    void updateLoggingConfiguration(File file);
+    void updateLoggingConfiguration(String configFileName);
 }
