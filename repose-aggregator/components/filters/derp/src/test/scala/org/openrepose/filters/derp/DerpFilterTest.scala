a8ee7e0baace727db5f06585bf436189da00c05f
Damien Johnson <damien.johnson@rackspace.com>
2014-11-21 11:12:39 -0600

    Added tests

---
+++ repose-aggregator/components/filters/derp/src/test/scala/org/openrepose/filters/derp/DerpFilterTest.scala
@@ -0,0 +1,60 @@
+package org.openrepose.filters.derp
+
+import java.util
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.{FilterChain, ServletResponse}
+
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.FunSpec
+
+import scala.collection.JavaConverters.asJavaEnumerationConverter
+
+class DerpFilterTest extends FunSpec {
+
+  describe("doFilter") {
+    it("should pass the request if no delegation header is present") {
+      val derpFilter = new DerpFilter()
+      val req = mockRequest(Map())
+      val fc = mock(classOf[FilterChain])
+
+      derpFilter.doFilter(req, null, fc)
+
+      verify(fc).doFilter(same(req), any(classOf[ServletResponse]))
+    }
+
+    it("should send an error response populated with the data from the delegation header if present")(pending)
+
+    it("should send an error response corresponding to the delegation value with the highest quality")(pending)
+
+    it("should treat a delegation value without an explicit quality as having a quality of 1")(pending)
+  }
+
+  def mockRequest(headers: Map[String, Iterable[String]]) = {
+    val req = mock(classOf[HttpServletRequest])
+
+    when(req.getHeader(anyString())).thenAnswer(new Answer[String] {
+      override def answer(invocation: InvocationOnMock): String = {
+        if (headers.isEmpty) {
+          null
+        } else {
+          headers(invocation.getArguments()(0).asInstanceOf[String]).head
+        }
+      }
+    })
+    when(req.getHeaders(anyString())).thenAnswer(new Answer[util.Enumeration[String]] {
+      override def answer(invocation: InvocationOnMock): util.Enumeration[String] = {
+        if (headers.isEmpty) {
+          Iterator[String]().asJavaEnumeration
+        } else {
+          headers(invocation.getArguments()(0).asInstanceOf[String]).iterator.asJavaEnumeration
+        }
+      }
+    })
+    when(req.getHeaderNames).thenReturn(headers.keysIterator.asJavaEnumeration)
+
+    req
+  }
+}
