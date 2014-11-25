80ded8b772d9fe65f2286c8325aa0168ee30c634
David Kowis <david.kowis@rackspace.com>
2014-11-25 10:09:56 -0600

    Updating the ConnectionPoolService implementation and tests

--- repose-aggregator/services/httpclient/impl/src/test/groovy/org/openrepose/services/httpclient/impl/HttpConnectionPoolImplTest.groovy
+++ repose-aggregator/services/httpclient/impl/src/test/groovy/org/openrepose/services/httpclient/impl/HttpConnectionPoolImplTest.groovy
@@ -1,108 +1,65 @@
 package org.openrepose.services.httpclient.impl
 
-import org.openrepose.commons.config.manager.UpdateListener
+import org.junit.Before
+import org.junit.Test
 import org.openrepose.commons.config.resource.ConfigurationResource
 import org.openrepose.commons.config.resource.ConfigurationResourceResolver
-import org.openrepose.core.services.ServiceRegistry
+import org.openrepose.core.service.httpclient.config.HttpConnectionPoolConfig
 import org.openrepose.core.services.config.ConfigurationService
-import org.openrepose.core.services.context.ServiceContext
 import org.openrepose.services.healthcheck.HealthCheckService
 import org.openrepose.services.healthcheck.HealthCheckServiceProxy
 import org.openrepose.services.healthcheck.Severity
-import org.openrepose.services.httpclient.HttpClientService
-import org.openrepose.core.service.httpclient.config.HttpConnectionPoolConfig
-import org.junit.Before
-import org.junit.Test
-
-import javax.servlet.ServletContextEvent
 
 import static org.mockito.Matchers.any
 import static org.mockito.Matchers.anyString
 import static org.mockito.Mockito.*
 
-//TODO: convert this into ones on the service itself, in the services package....
 class HttpConnectionPoolImplTest {
 
-    HttpConnectionPoolServiceContext httpConnectionPoolServiceContext
+    HttpConnectionPoolServiceImpl httpConnectionPoolService
     HealthCheckService healthCheckService
     HealthCheckServiceProxy healthCheckServiceProxy
-    ServiceRegistry serviceRegistry;
     ConfigurationService configurationService;
-    HttpClientService httpClientService;
-    ServletContextEvent sce
-
 
     @Before
     void setUp() {
-
         configurationService = mock(ConfigurationService.class)
-        serviceRegistry = mock(ServiceRegistry.class)
-        httpClientService = mock(HttpClientService.class)
         healthCheckService = mock(HealthCheckService.class)
         healthCheckServiceProxy = mock(HealthCheckServiceProxy)
         when(healthCheckService.register()).thenReturn(healthCheckServiceProxy)
-        httpConnectionPoolServiceContext = new HttpConnectionPoolServiceContext(serviceRegistry, configurationService, httpClientService, healthCheckService)
-        sce = mock(ServletContextEvent.class)
 
+        httpConnectionPoolService = new HttpConnectionPoolServiceImpl(configurationService, healthCheckService)
     }
 
     @Test
     void verifyRegistrationToHealthCheckService() {
-
         verify(healthCheckService, times(1)).register()
-
     }
 
     @Test
-    void verifyInitialReportOfConfig(){
-
+    void verifyInitialReportOfConfig() {
         ConfigurationResourceResolver resourceResolver = mock(ConfigurationResourceResolver.class);
         ConfigurationResource configurationResource = mock(ConfigurationResource.class);
         when(configurationService.getResourceResolver()).thenReturn(resourceResolver);
-        when(resourceResolver.resolve(HttpConnectionPoolServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
-        when(configurationService.getResourceResolver().resolve(MetricsServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
+        when(resourceResolver.resolve(HttpConnectionPoolServiceImpl.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
+        when(configurationService.getResourceResolver().resolve(HttpConnectionPoolServiceImpl.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
 
-        httpConnectionPoolServiceContext.contextInitialized(sce);
+        httpConnectionPoolService.init()
         verify(healthCheckServiceProxy, times(1)).reportIssue(any(String), any(String), any(Severity.class));
-        verify(serviceRegistry, times(1)).addService(any(ServiceContext.class));
     }
 
     @Test
-    void verifyContextDestroyed(){
-        httpConnectionPoolServiceContext.contextDestroyed(sce)
-        verify(httpClientService, times(1)).shutdown()
-        verify(configurationService, times(1)).unsubscribeFrom(any(String),any(UpdateListener.class))
-
-    }
-
-    @Test
-    void verifyHealthCheckServiceReportingNullPointer(){
+    void verifyConfigurationUpdated() {
 
         ConfigurationResourceResolver resourceResolver = mock(ConfigurationResourceResolver.class);
         ConfigurationResource configurationResource = mock(ConfigurationResource.class);
         when(configurationService.getResourceResolver()).thenReturn(resourceResolver);
-        when(resourceResolver.resolve(HttpConnectionPoolServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
-        when(configurationService.getResourceResolver().resolve(MetricsServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
+        when(resourceResolver.resolve(HttpConnectionPoolServiceImpl.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
+        when(configurationService.getResourceResolver().resolve(HttpConnectionPoolServiceImpl.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
 
-        httpConnectionPoolServiceContext.contextInitialized(sce);
-        verify(healthCheckServiceProxy, times(1)).reportIssue(any(String), any(String), any(Severity));
-        verify(serviceRegistry, times(1)).addService(any(ServiceContext.class));
+        httpConnectionPoolService.configurationListener.configurationUpdated(mock(HttpConnectionPoolConfig))
 
-    }
-
-    @Test
-    void verifyConfigurationUpdated(){
-
-        ConfigurationResourceResolver resourceResolver = mock(ConfigurationResourceResolver.class);
-        ConfigurationResource configurationResource = mock(ConfigurationResource.class);
-        when(configurationService.getResourceResolver()).thenReturn(resourceResolver);
-        when(resourceResolver.resolve(HttpConnectionPoolServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
-        when(configurationService.getResourceResolver().resolve(MetricsServiceContext.DEFAULT_CONFIG_NAME)).thenReturn(configurationResource);
-
-        httpConnectionPoolServiceContext.configurationListener.configurationUpdated(mock(HttpConnectionPoolConfig))
-
-        assert(httpConnectionPoolServiceContext.configurationListener.initialized)
-        verify(httpClientService, times(1)).configure(any(HttpConnectionPoolConfig.class));
+        assert (httpConnectionPoolService.configurationListener.initialized)
         verify(healthCheckServiceProxy, times(1)).resolveIssue(anyString());
     }
 }
