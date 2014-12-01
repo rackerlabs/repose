83f63f08534b71225e409cc68a6bac8c6d091998
David Kowis <david.kowis@rackspace.com>
2014-12-01 14:52:27 -0600

    Moved into the LauncherService implementation
    Haven't deleted yet, in case I need to reference it.

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/impl/DistributedDatastoreServiceContext.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/impl/DistributedDatastoreServiceContext.java
@@ -22,7 +22,9 @@ import java.net.URL;
 
 /*
  * Class that will listen to system-model.cfg.xml and dist-datastore.cfg.xml file to launch the distributed-datastore servlet
+ * NOTE: I did all this already, BAM
  */
+@Deprecated
 @Component("distributedDatastoreServiceContext")
 public class DistributedDatastoreServiceContext implements ServiceContext<DistributedDatastoreLauncherService> {
     DistributedDatastoreLauncherService distDatastoreServiceLauncher;
