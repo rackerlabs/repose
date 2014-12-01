ef7319f37d84a934a70975fefefd5db658e07664
wdschei <Bill.Scheidegger@Rackspace.com>
2014-12-01 13:52:44 -0600

    Added a new test for the "crush" attribute of the Format element to ensure that all newline (\r, \n, \r\n) and whitespace ( , \t) combinations are properly collapsed.

---
+++ repose-aggregator/components/filters/slf4j-http-logging/src/test/groovy/org/openrepose/filters/slf4jlogging/ReplaceNewlineTest.groovy
@@ -0,0 +1,77 @@
+package org.openrepose.filters.slf4jlogging
+
+import com.mockrunner.mock.web.MockFilterChain
+import com.mockrunner.mock.web.MockHttpServletRequest
+import com.mockrunner.mock.web.MockHttpServletResponse
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.LoggerContext
+import org.apache.logging.log4j.test.appender.ListAppender
+import spock.lang.Shared
+import spock.lang.Specification
+
+class ReplaceNewlineTest extends Specification {
+    ListAppender app
+
+    @Shared
+    Slf4jHttpLoggingFilter filter
+
+    def setupSpec() {
+        System.setProperty("javax.xml.parsers.DocumentBuilderFactory",
+                "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
+        //NOTE This dies a horrible death if you try to stick it in an unroll. Possibly a side effect of how brittle it is
+        filter = Slf4jLoggingFilterTestUtil.configureFilter([
+                //Configure a logger with all the things so I can verify all the things we claim to support
+                Slf4jLoggingFilterTestUtil.logConfig(
+                        "Logger0",
+                        "\t\t  \r\nmore\r than \n\tone\t\r\n  line  \r \twith \t\n\t varying\t \r\n\t\twhitespace\t\t  \r\n\r\n",
+                        true,
+                        true)
+        ])
+    }
+
+    def setup() {
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false)
+        app = ((ListAppender) (ctx.getConfiguration().getAppender("List0"))).clear()
+    }
+
+    def "When using a multi-line format string, you'll get multiple lines in your log output"() {
+        given:
+        MockFilterChain chain = new MockFilterChain()
+        MockHttpServletRequest request = new MockHttpServletRequest()
+        MockHttpServletResponse response = new MockHttpServletResponse()
+
+        //Will need to set up the request and response to verify the log line
+        request.setRequestURI("http://www.example.com/derp/derp?herp=derp")
+        request.setRequestURL("http://www.example.com/derp/derp?herp=derp")
+        request.addHeader("Accept", "application/xml")
+        request.setQueryString("?herp=derp")
+        request.setMethod("GET")
+        request.setRemoteHost("10.10.220.221")
+        request.setLocalAddr("10.10.220.220")
+        request.setLocalPort(12345)
+        request.setServerPort(8080)
+        request.addHeader("X-PP-User", "leUser") //Remote user is special for Repose...
+
+
+        def responseBody = "HEY A BODY"
+        response.setContentLength(10)// size of responseBody .. but no
+        response.setStatus(200, "OK")
+        response.addHeader("X-Derp-header", "lolwut")
+        response.getWriter().print(responseBody)
+        response.getWriter().flush()
+        response.getWriter().close() //I think this should shove the body in there
+
+        when:
+        filter.doFilter(request, response, chain)
+
+        then:
+        chain.getRequestList().size() == 1
+
+        app.getEvents().size() == 1
+
+        def logMessage = app.getEvents().first().getMessage().getFormattedMessage().split("\n")
+
+        logMessage.size() == 1
+        logMessage[0].equals("more than one line with varying whitespace")
+    }
+}
