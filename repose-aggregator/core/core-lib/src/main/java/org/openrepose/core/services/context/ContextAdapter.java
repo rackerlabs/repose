5a36c8a7f54943e2d3c8c780af287f9930c51249
David Kowis <david.kowis@rackspace.com>
2014-12-02 14:48:07 -0600

    Optimized imports so it's not mad any more

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/ContextAdapter.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/context/ContextAdapter.java
@@ -4,20 +4,19 @@ import org.openrepose.commons.utils.proxy.RequestProxyService;
 import org.openrepose.core.services.classloader.ClassLoaderManagerService;
 import org.openrepose.core.services.config.ConfigurationService;
 import org.openrepose.core.services.context.container.ContainerConfigurationService;
-import org.openrepose.services.datastore.DatastoreService;
-import org.openrepose.core.services.datastore.DistributedDatastoreLauncherService;
 import org.openrepose.core.services.event.common.EventService;
 import org.openrepose.core.services.headers.request.RequestHeaderService;
 import org.openrepose.core.services.headers.response.ResponseHeaderService;
-import org.openrepose.services.healthcheck.HealthCheckService;
-import org.openrepose.services.httpclient.HttpClientService;
 import org.openrepose.core.services.logging.LoggingService;
 import org.openrepose.core.services.reporting.ReportingService;
 import org.openrepose.core.services.reporting.metrics.MetricsService;
 import org.openrepose.core.services.rms.ResponseMessageService;
 import org.openrepose.nodeservice.routing.RoutingService;
-import org.openrepose.services.serviceclient.akka.AkkaServiceClient;
 import org.openrepose.core.services.threading.ThreadingService;
+import org.openrepose.services.datastore.DatastoreService;
+import org.openrepose.services.healthcheck.HealthCheckService;
+import org.openrepose.services.httpclient.HttpClientService;
+import org.openrepose.services.serviceclient.akka.AkkaServiceClient;
 
 
 @Deprecated
