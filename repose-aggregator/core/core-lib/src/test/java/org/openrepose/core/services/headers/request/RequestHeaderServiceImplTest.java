7824f5a98af53aa7b04ee39bf1277d90b2b9d9fc
David Kowis <david.kowis@rackspace.com>
2014-12-02 15:11:41 -0600

    Fixing compilation errors

--- repose-aggregator/core/core-lib/src/test/java/org/openrepose/core/services/headers/request/RequestHeaderServiceImplTest.java
+++ repose-aggregator/core/core-lib/src/test/java/org/openrepose/core/services/headers/request/RequestHeaderServiceImplTest.java
@@ -2,11 +2,14 @@ package org.openrepose.core.services.headers.request;
 
 import org.openrepose.commons.utils.http.CommonHttpHeader;
 import org.openrepose.commons.utils.servlet.http.MutableHttpServletRequest;
+import org.openrepose.core.services.config.ConfigurationService;
 import org.openrepose.core.services.headers.common.ViaHeaderBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+import org.openrepose.services.healthcheck.HealthCheckService;
+
 import static org.mockito.Mockito.*;
 
 @RunWith(Enclosed.class)
@@ -21,24 +24,24 @@ public class RequestHeaderServiceImplTest {
         public void setup() {
             request = mock(MutableHttpServletRequest.class);
             viaBuilder = mock(ViaHeaderBuilder.class);
-            instance = new RequestHeaderServiceImpl();
+            instance = new RequestHeaderServiceImpl(mock(ConfigurationService.class), mock(HealthCheckService.class), "cluster", "node", "1.0");
         }
-        
+
         @Test
         public void shouldSetXForwardedForHeader() {
             final String remote = "1.2.3.4";
-            
+
             when(request.getRemoteAddr()).thenReturn(remote);
             instance.setXForwardedFor(request);
-            
+
             verify(request).addHeader(CommonHttpHeader.X_FORWARDED_FOR.toString(), remote);
-            
+
         }
-        
+
         @Test
         public void shouldSetViaHeader() {
             final String via = "via";
-            
+
             when(viaBuilder.buildVia(request)).thenReturn(via);
             instance.updateConfig(viaBuilder);
             instance.setVia(request);
