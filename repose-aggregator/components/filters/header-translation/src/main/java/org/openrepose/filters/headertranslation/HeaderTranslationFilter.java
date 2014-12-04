a2cef53c763ddc26b59574ae242d0d61b7a9b6f1
wdschei <Bill.Scheidegger@Rackspace.com>
2014-12-04 17:58:36 -0600

    Springified the HeaderTranslationFilter.

--- repose-aggregator/components/filters/header-translation/src/main/java/org/openrepose/filters/headertranslation/HeaderTranslationFilter.java
+++ repose-aggregator/components/filters/header-translation/src/main/java/org/openrepose/filters/headertranslation/HeaderTranslationFilter.java
@@ -5,31 +5,35 @@ import org.openrepose.filters.headertranslation.config.HeaderTranslationType;
 import org.openrepose.core.filter.FilterConfigHelper;
 import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate;
 import org.openrepose.core.services.config.ConfigurationService;
-import org.openrepose.core.services.context.ServletContextHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.*;
 import java.io.IOException;
 import java.net.URL;
 
+@Named
 public class HeaderTranslationFilter implements Filter {
     private static final Logger LOG = LoggerFactory.getLogger(HeaderTranslationFilter.class);
     private static final String DEFAULT_CONFIG = "header-translation.cfg.xml";
     private String config;
     private HeaderTranslationHandlerFactory headerTranslationHandlerFactory;
-    private ConfigurationService configurationService;
+    private final ConfigurationService configurationService;
 
+    @Inject
+    public HeaderTranslationFilter(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
-        configurationService = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
         headerTranslationHandlerFactory = new HeaderTranslationHandlerFactory();
         URL xsdURL = getClass().getResource("/META-INF/schema/config/header-translation.xsd");
         configurationService.subscribeTo(filterConfig.getFilterName(), config, xsdURL, headerTranslationHandlerFactory, HeaderTranslationType.class);
-
     }
 
     @Override
