9786215de6aa6125eacf1b90461fd47561814b3f
wdschei <Bill.Scheidegger@Rackspace.com>
2014-12-05 12:41:53 -0600

    Springified the RackspaceAuthUserFilter.

--- repose-aggregator/components/filters/rackspace-auth-user/src/main/scala/org/openrepose/filters/rackspaceauthuser/RackspaceAuthUserFilter.scala
+++ repose-aggregator/components/filters/rackspace-auth-user/src/main/scala/org/openrepose/filters/rackspaceauthuser/RackspaceAuthUserFilter.scala
@@ -1,5 +1,6 @@
 package org.openrepose.filters.rackspaceauthuser
 
+import javax.inject.{Inject, Named}
 import javax.servlet._
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
@@ -7,38 +8,29 @@ import org.openrepose.commons.config.manager.UpdateListener
 import org.openrepose.core.filter.FilterConfigHelper
 import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate
 import org.openrepose.core.services.config.ConfigurationService
-import org.openrepose.core.services.context.ServletContextHelper
 
-class RackspaceAuthUserFilter extends Filter with LazyLogging {
-
+@Named
+class RackspaceAuthUserFilter @Inject() (configurationService: ConfigurationService) extends Filter with LazyLogging {
   private final val DEFAULT_CONFIG = "rackspace-auth-user.cfg.xml"
 
   private var config: String = _
   private var handlerFactory: RackspaceAuthUserHandlerFactory = _
-  private var configurationService: ConfigurationService = _
-
 
   override def init(filterConfig: FilterConfig): Unit = {
     config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
     logger.info(s"Initializing RackspaceAuthUserFilter using config $config")
-
-    //Ew, nasty spring hax
-    val powerApiContext = ServletContextHelper.getInstance(filterConfig.getServletContext).getPowerApiContext
-    configurationService = powerApiContext.configurationService()
     handlerFactory = new RackspaceAuthUserHandlerFactory()
-
     val xsdURL = getClass.getResource("/META-INF/config/schema/rackspace-auth-user-configuration.xsd")
+    // TODO: Clean up the asInstanceOf below, if possible?
     configurationService.subscribeTo(filterConfig.getFilterName,
       config,
       xsdURL,
       handlerFactory.asInstanceOf[UpdateListener[RackspaceAuthUserConfig]],
       classOf[RackspaceAuthUserConfig])
-
   }
 
   override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
     new FilterLogicHandlerDelegate(request, response, chain).doFilter(handlerFactory.newHandler)
-
   }
 
   override def destroy(): Unit = {
