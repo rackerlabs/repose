b067d081a96100dce624f81839259e924e9b5c23
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-12-05 13:22:46 -0600

    Added filter file, test file, and POM

---
+++ repose-aggregator/components/filters/herp/src/main/scala/org.openrepose.filters.herp/HerpFilter.scala
@@ -0,0 +1,40 @@
+package org.openrepose.filters.herp
+
+import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import javax.ws.rs.core.MediaType
+
+import com.rackspace.httpdelegation._
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+import scala.collection.generic.SeqFactory
+import scala.util.Failure
+import scala.util.Success
+import scala.util.{Failure, Success}
+
+
+class HerpFilter extends Filter with HttpDelegationManager {
+
+
+
+  override def init(filterConfig: FilterConfig): Unit = {
+    LOG.trace("HeRP filter initialized")
+  }
+
+  override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain): Unit = {
+
+  }
+
+  override def destroy(): Unit = {
+    LOG.trace("HeRP filter destroyed")
+  }
+
+  def parseDelegationValues(delegationValues: Seq[String]): Seq[HttpDelegationHeader] = {
+
+  }
+
+  def sendError(httpServletResponse: HttpServletResponse, statusCode: Int, responseBody: String, responseContentType: String): Unit = {
+
+  }
+}
---
+++ repose-aggregator/components/filters/herp/src/test/scala/org.openrepose.filters.herp/HerpFilterTest.scala
@@ -0,0 +1,30 @@
+package org.openrepose.filters.herp
+
+import java.io.PrintWriter
+import java.util
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import javax.servlet.{ServletRequest, FilterChain, ServletResponse}
+
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.stubbing.Answer
+import org.scalatest.FunSpec
+
+import scala.collection.JavaConverters.asJavaEnumerationConverter
+
+class HerpFilterTest extends FunSpec {
+
+  describe("doFilter") {
+    /*it("should ...") {
+      val herpFilter = new HerpFilter()
+
+      verify...
+    }*/
+
+  }
+
+  /*def mockRequest(headers: Map[String, Iterable[String]]) = {
+
+  }*/
+}
