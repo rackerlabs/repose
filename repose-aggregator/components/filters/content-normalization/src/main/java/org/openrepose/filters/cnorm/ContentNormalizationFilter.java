1de98a26ae25df459815a8e15ede9fc6d8f587ad
wdschei <Bill.Scheidegger@Rackspace.com>
2014-12-05 09:20:56 -0600

    Springified the ContentNormalizationFilter.

--- repose-aggregator/components/filters/content-normalization/src/main/java/org/openrepose/filters/cnorm/ContentNormalizationFilter.java
+++ repose-aggregator/components/filters/content-normalization/src/main/java/org/openrepose/filters/cnorm/ContentNormalizationFilter.java
@@ -3,22 +3,29 @@ package org.openrepose.filters.cnorm;
 import org.openrepose.core.filter.FilterConfigHelper;
 import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate;
 import org.openrepose.core.services.config.ConfigurationService;
-import org.openrepose.core.services.context.ServletContextHelper;
 import org.openrepose.filters.cnorm.config.ContentNormalizationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.*;
 import java.io.IOException;
 import java.net.URL;
 
+@Named
 public class ContentNormalizationFilter implements Filter {
 
     private static final Logger LOG = LoggerFactory.getLogger(ContentNormalizationFilter.class);
     private static final String DEFAULT_CONFIG = "content-normalization.cfg.xml";
     private String config;
     private ContentNormalizationHandlerFactory handlerFactory;
-    private ConfigurationService configurationManager;
+    private ConfigurationService configurationService;
+
+    @Inject
+    public ContentNormalizationFilter(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -27,16 +34,15 @@ public class ContentNormalizationFilter implements Filter {
 
     @Override
     public void destroy() {
-        configurationManager.unsubscribeFrom(config, handlerFactory);
+        configurationService.unsubscribeFrom(config, handlerFactory);
     }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG);
         LOG.info("Initializing filter using config " + config);
-        configurationManager = ServletContextHelper.getInstance(filterConfig.getServletContext()).getPowerApiContext().configurationService();
         handlerFactory = new ContentNormalizationHandlerFactory();
         URL xsdURL = getClass().getResource("/META-INF/schema/config/normalization-configuration.xsd");
-        configurationManager.subscribeTo(filterConfig.getFilterName(),config, xsdURL,handlerFactory, ContentNormalizationConfig.class);
+        configurationService.subscribeTo(filterConfig.getFilterName(), config, xsdURL, handlerFactory, ContentNormalizationConfig.class);
     }
 }
