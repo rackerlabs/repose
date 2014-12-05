18dafbff5eb5e665304940b213a4aa55acc69ee5
Adrian <adrian.george@rackspace.com>
2014-12-05 13:30:22 -0600

    Moved most of the context stuff into the routing service, i haven't addressed any of the service ports stuff as of yet

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/routing/robin/RoundRobinRoutingService.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/routing/robin/RoundRobinRoutingService.java
@@ -1,29 +1,132 @@
 package org.openrepose.core.services.routing.robin;
 
-import org.openrepose.core.systemmodel.Node;
-import org.openrepose.core.systemmodel.SystemModel;
+import org.openrepose.commons.config.manager.UpdateListener;
+import org.openrepose.core.domain.Port;
+import org.openrepose.core.domain.ServicePorts;
+import org.openrepose.core.services.config.ConfigurationService;
 import org.openrepose.core.services.routing.RoutingService;
-import org.springframework.stereotype.Component;
+import org.openrepose.core.systemmodel.Node;
+import org.openrepose.core.systemmodel.ReposeCluster;
+import org.openrepose.core.systemmodel.SystemModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
-@Component("routingService")
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.net.URL;
+
+@Named
 public class RoundRobinRoutingService implements RoutingService {
-   private Clusters domains;
+    private static final Logger LOG = LoggerFactory.getLogger(RoundRobinRoutingService.class);
+    private ConfigurationService configurationService;
+    private PowerApiConfigListener configListener;
+    private ServicePorts servicePorts;
+    private String clusterId, nodeId;
+    private Clusters domains;
 
-   public RoundRobinRoutingService() {
-   }
-   
-   @Override
-   public void setSystemModel(SystemModel config) {
-      this.domains = new Clusters(config);
-   }
-   
-   @Override
-   public Node getRoutableNode(String domainId) {
-      ClusterWrapper domain = domains.getDomain(domainId);
-      if (domain != null) {
-         return domain.getNextNode();
-      }
-      
-      return null;
-   }
+    @Inject
+    public RoundRobinRoutingService(ConfigurationService configurationService,
+                                    @Value("node.id") String nodeId,
+                                    @Value("cluster.id") String clusterId) {
+        configListener = new PowerApiConfigListener();
+        this.configurationService = configurationService;
+        this.servicePorts = servicePorts;
+        this.clusterId = clusterId;
+        this.nodeId = nodeId;
+
+    }
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        URL xsdURL = getClass().getResource("/META-INF/schema/system-model/system-model.xsd");
+        configurationService.subscribeTo("system-model.cfg.xml", xsdURL, configListener, SystemModel.class);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (configurationService != null) {
+            configurationService.unsubscribeFrom("system-model.cfg.xml", configListener);
+        }
+    }
+
+    @Override
+    public void setSystemModel(SystemModel config) {
+        this.domains = new Clusters(config);
+    }
+
+    @Override
+    public Node getRoutableNode(String domainId) {
+        ClusterWrapper domain = domains.getDomain(domainId);
+        if (domain != null) {
+            return domain.getNextNode();
+        }
+
+        return null;
+    }
+
+    private class PowerApiConfigListener implements UpdateListener<SystemModel> {
+
+        private SystemModel config;
+        private boolean isInitialized = false;
+
+        private ServicePorts determinePorts(Node reposeNode) {
+            ServicePorts ports = new ServicePorts();
+
+            if (reposeNode != null) {
+                if (reposeNode.getHttpPort() != 0) {
+                    ports.add(new Port("http", reposeNode.getHttpPort()));
+                } else {
+                    LOG.error("Http service port not specified for Repose Node " + reposeNode.getId());
+                }
+
+                if (reposeNode.getHttpsPort() != 0) {
+                    ports.add(new Port("https", reposeNode.getHttpsPort()));
+                } else {
+                    LOG.info("Https service port not specified for Repose Node " + reposeNode.getId());
+                }
+            }
+
+            return ports;
+        }
+
+        private Node determineReposeNode(String clusterId, String nodeId) {
+
+            for (ReposeCluster cluster : config.getReposeCluster()) {
+
+                if (cluster.getId().equals(clusterId)) {
+                    for (Node node : cluster.getNodes().getNode()) {
+                        if (node.getId().equals(nodeId)) {
+                            return node;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public void configurationUpdated(SystemModel configurationObject) {
+            config = configurationObject;
+            setSystemModel(config);
+
+
+            if (!isInitialized()) {
+                LOG.info("Determining ports for repose under cluster: " + clusterId);
+                ServicePorts ports = determinePorts(determineReposeNode(clusterId, nodeId));
+                servicePorts.clear();
+                servicePorts.addAll(ports);
+            }
+            isInitialized = true;
+        }
+
+        @Override
+        public boolean isInitialized() {
+            return isInitialized;
+        }
+    }
+
 }
