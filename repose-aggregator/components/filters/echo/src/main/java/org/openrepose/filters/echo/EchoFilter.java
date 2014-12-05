7ac2427821099666d8d8287d81e6dc671cde6810
wdschei <Bill.Scheidegger@Rackspace.com>
2014-12-05 08:59:59 -0600

    Springified the EchoFilter.

--- repose-aggregator/components/filters/echo/src/main/java/org/openrepose/filters/echo/EchoFilter.java
+++ repose-aggregator/components/filters/echo/src/main/java/org/openrepose/filters/echo/EchoFilter.java
@@ -1,15 +1,17 @@
 package org.openrepose.filters.echo;
 
+import org.eclipse.jetty.http.HttpStatus;
 import org.openrepose.commons.utils.servlet.http.HttpServletHelper;
 import org.slf4j.Logger;
 
+import javax.inject.Named;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
-import org.eclipse.jetty.http.HttpStatus;
 
+@Named
 public class EchoFilter implements Filter {
 
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(EchoFilter.class);
