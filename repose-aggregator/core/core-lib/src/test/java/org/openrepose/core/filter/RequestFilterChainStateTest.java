c2ba1b1d10eeadeac08668e965b75d3c0da4527c
David Kowis <david.kowis@rackspace.com>
2014-12-08 14:50:36 -0600

    Fixing the compilation failures of this test
    I'm not sure it actually tests anything valuable, and might end up being
    thrown away when we remove all the servlet context helper stuff

--- repose-aggregator/core/core-lib/src/test/java/org/openrepose/core/filter/RequestFilterChainStateTest.java
+++ repose-aggregator/core/core-lib/src/test/java/org/openrepose/core/filter/RequestFilterChainStateTest.java
@@ -1,16 +1,15 @@
 package org.openrepose.core.filter;
 
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 import org.openrepose.core.domain.ReposeInstanceInfo;
+import org.openrepose.core.services.context.ServletContextHelper;
+import org.openrepose.core.services.context.container.ContainerConfigurationService;
 import org.openrepose.powerfilter.PowerFilterChain;
 import org.openrepose.powerfilter.PowerFilterChainException;
 import org.openrepose.powerfilter.PowerFilterRouter;
 import org.openrepose.powerfilter.filtercontext.FilterContext;
-import org.openrepose.core.services.context.ServletContextHelper;
-import org.openrepose.core.services.context.container.ContainerConfigurationService;
-import org.openrepose.core.services.context.impl.RoutingServiceContext;
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 
 import javax.naming.NamingException;
@@ -24,12 +23,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
- * @author fran
+ * TODO: I don't think this test does anything, because it's got a lot of stuff in it that shouldn't be here...
  */
 @RunWith(Enclosed.class)
 public class RequestFilterChainStateTest {
@@ -43,11 +40,9 @@ public class RequestFilterChainStateTest {
             FilterContext mockedFilterContext = mock(FilterContext.class);
             ServletContext context = mock(ServletContext.class);
             ApplicationContext appContext = mock(ApplicationContext.class);
-            RoutingServiceContext routingContext = mock(RoutingServiceContext.class);
             ContainerConfigurationService containerConfigurationService = mock(ContainerConfigurationService.class);
             when(containerConfigurationService.getVia()).thenReturn("");
             when(mockedFilterContext.getFilter()).thenReturn(mockedFilter);
-            when(appContext.getBean(anyString())).thenReturn(routingContext);
             filterContextList.add(mockedFilterContext);
             FilterChain mockedFilterChain = mock(FilterChain.class);
 
