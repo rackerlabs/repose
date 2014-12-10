c89d18bb3cf209544a5d19c041cc5cd31a11e76e
David Kowis <david.kowis@rackspace.com>
2014-12-10 14:43:48 -0600

    IntelliJ missed another
    StupidiJ

--- repose-aggregator/components/filters/rackspace-identity-basic-auth/src/test/scala/org/openrepose/filters/rackspaceidentitybasicauth/RackspaceIdentityBasicAuthHandlerTest.scala
+++ repose-aggregator/components/filters/rackspace-identity-basic-auth/src/test/scala/org/openrepose/filters/rackspaceidentitybasicauth/RackspaceIdentityBasicAuthHandlerTest.scala
@@ -1,14 +1,13 @@
 package org.openrepose.filters.rackspaceidentitybasicauth
 
 import com.mockrunner.mock.web.MockHttpServletRequest
-import org.openrepose.core.services.datastore.{DatastoreService, Datastore}
-import org.openrepose.filters.rackspaceidentitybasicauth.config.RackspaceIdentityBasicAuthConfig
 import org.junit.runner.RunWith
 import org.mockito.Matchers.anyString
 import org.mockito.Mockito.when
 import org.openrepose.commons.utils.servlet.http.ReadableHttpServletResponse
 import org.openrepose.core.filter.logic.FilterAction
-import org.openrepose.services.datastore.DatastoreService
+import org.openrepose.core.services.datastore.{Datastore, DatastoreService}
+import org.openrepose.filters.rackspaceidentitybasicauth.config.RackspaceIdentityBasicAuthConfig
 import org.openrepose.services.serviceclient.akka.AkkaServiceClient
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mock.MockitoSugar
