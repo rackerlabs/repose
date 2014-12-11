cd973b2325008cfd62d2966104fdddfb41bb2ab5
David Kowis <david.kowis@rackspace.com>
2014-12-11 15:27:40 -0600

    Have to get the file itself

--- repose-aggregator/core/core-lib/src/test/scala/org/openrepose/core/spring/TestFilterBundlerHelper.scala
+++ repose-aggregator/core/core-lib/src/test/scala/org/openrepose/core/spring/TestFilterBundlerHelper.scala
@@ -7,7 +7,11 @@ import com.typesafe.config.ConfigFactory
 import org.apache.commons.io.filefilter.WildcardFileFilter
 
 trait TestFilterBundlerHelper {
-  lazy val testFilterBundleRoot = new File(ConfigFactory.load("test.properties").getString("coreTestFilterBundleLocation"))
+  val testProps = ConfigFactory.load("test.properties")
+
+  lazy val testFilterBundleRoot = new File(testProps.getString("coreTestFilterBundleLocation"))
+
+  lazy val testFilterBundleFile = new File(testFilterBundleRoot, "core-test-filter-bundle-" + testProps.getString("coreTestFilterBundleVersion") + ".ear")
 
   lazy val testFilterBundleClassLoader: ClassLoader = {
     val fileFilter: FileFilter = new WildcardFileFilter("core-test-filter-*.jar")
