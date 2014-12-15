7096d46793e5d8a38a5d27e529caf32e2fa1eff1
jenny.vo <jenny.vo@rackspace.com>
2014-12-15 15:15:12 -0600

    add test to check region service

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthz/serviceresponse/ClientAuthZCheckRegionTest.groovy
@@ -0,0 +1,80 @@
+package features.filters.clientauthz.serviceresponse
+
+import framework.ReposeValveTest
+import framework.mocks.MockIdentityService
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+import spock.lang.Unroll
+
+/**
+ * Created by jennyvo on 12/15/14.
+ */
+class ClientAuthZCheckRegionTest extends ReposeValveTest {
+
+    def static originEndpoint
+    def static identityEndpoint
+
+    static MockIdentityService fakeIdentityService
+
+    def setupSpec() {
+        cleanLogDirectory()
+
+        deproxy = new Deproxy()
+
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/clientauthz/common", params)
+        repose.start()
+
+        originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
+        fakeIdentityService = new MockIdentityService(properties.identityPort, properties.targetPort)
+        identityEndpoint = deproxy.addEndpoint(properties.identityPort,
+                'identity service', null, fakeIdentityService.handler)
+    }
+
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+        if (repose){
+            repose.stop()
+        }
+    }
+    def "When user's service endpoint has correct region and service endpoint should receive a 200 response"(){
+
+        given: "IdentityService is configured with allowed endpoints that will differ from the user's requested endpoint"
+        def token = UUID.randomUUID().toString()
+        fakeIdentityService.client_token = token
+        fakeIdentityService.region = "ORD"
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(url:reposeEndpoint + "/v1/"+token+"/ss", method:'GET', headers:['X-Auth-Token': token])
+
+        then: "User should receive a 200 response"
+        mc.handlings.size() == 1
+        mc.receivedResponse.code == "200"
+    }
+
+    @Unroll ("User does not have right region: #serviceRegion")
+    def "When user service endpoint doesn't have right region should receive a 403 FORBIDDEN response"(){
+
+        given: "IdentityService is configured with allowed endpoints that will differ from the user's requested endpoint"
+        def token = UUID.randomUUID().toString()
+        fakeIdentityService.client_token = token
+        fakeIdentityService.region = serviceRegion
+
+        when: "User sends a request through repose"
+        MessageChain mc = deproxy.makeRequest(url:reposeEndpoint + "/v1/"+token+"/ss", method:'GET', headers:['X-Auth-Token': token])
+        def foundLogs = reposeLogSearch.searchByString("User token: " + token +
+                ": The user's service catalog does not contain an endpoint that matches the endpoint configured in openstack-authorization.cfg.xml")
+
+        then: "User should receive a 403 FORBIDDEN response"
+        foundLogs.size() == 1
+        mc.handlings.size() == 0
+        mc.receivedResponse.code == "403"
+
+        where:
+        serviceRegion << ["DFW", "RegionOne", null]
+    }
+}
