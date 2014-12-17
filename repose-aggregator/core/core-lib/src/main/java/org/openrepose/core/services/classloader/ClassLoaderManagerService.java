e2077e5fa639eb12a41d62e5c071b54aedb314e8
David Kowis <david.kowis@rackspace.com>
2014-12-17 15:18:57 -0600

    This method is never used, putting it into the trashcan

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/classloader/ClassLoaderManagerService.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/core/services/classloader/ClassLoaderManagerService.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 
 public interface ClassLoaderManagerService extends Destroyable {
 
-    EarClassLoader getApplication(String contextName);
     boolean hasFilter(String contextName);
     Collection<EarClassLoaderContext> getLoadedApplications();
 }
