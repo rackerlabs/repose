86d89e39e140bfffe139fe8d474a19ee1042e7eb
David Kowis <david.kowis@rackspace.com>
2014-12-22 12:56:13 -0600

    Have to move interfaces around to resolve dependency cycles
    Fixing transitive dependency hell has resulted in lots of cycles

--- repose-aggregator/commons/configuration/src/main/java/org/openrepose/commons/config/resource/ConfigurationResourceResolver.java
+++
@@ -1,6 +0,0 @@
-package org.openrepose.commons.config.resource;
-
-public interface ConfigurationResourceResolver {
-    
-    ConfigurationResource resolve(String resourceName) throws ResourceResolutionException;
-}
--- repose-aggregator/commons/configuration/src/main/java/org/openrepose/commons/config/resource/ResourceResolutionException.java
+++
@@ -1,8 +0,0 @@
-package org.openrepose.commons.config.resource;
-
-public class ResourceResolutionException extends RuntimeException {
-
-    public ResourceResolutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
