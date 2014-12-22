47cfa380cc1af80e236a128186c4302b2ad82c4b
David Kowis <david.kowis@rackspace.com>
2014-12-22 12:59:32 -0600

    Forgot to add the files I moved!

---
+++ repose-aggregator/core/core-service-api/src/main/java/org/openrepose/commons/config/resource/ResourceResolutionException.java
@@ -0,0 +1,8 @@
+package org.openrepose.commons.config.resource;
+
+public class ResourceResolutionException extends RuntimeException {
+
+    public ResourceResolutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
