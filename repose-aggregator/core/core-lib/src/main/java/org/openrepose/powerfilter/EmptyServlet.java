830beb3561332956279423ae71629132edbdbe07
David Kowis <dkowis@shlrm.org>
2014-12-24 14:17:05 -0600

    The default servlet implementation rejects all things

--- repose-aggregator/core/core-lib/src/main/java/org/openrepose/powerfilter/EmptyServlet.java
+++ repose-aggregator/core/core-lib/src/main/java/org/openrepose/powerfilter/EmptyServlet.java
@@ -1,7 +1,30 @@
 package org.openrepose.powerfilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public final class EmptyServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(EmptyServlet.class);
 
+    /**
+     * Have to override service so that the EmptyServlet doesn't actually do anything.
+     *
+     * We could totally probably use Jetty's ProxyServlet in here to make the request instead of doing whatever,
+     * This is the last thing called in the container config chain
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("Hit the empty servlet at the end of the chain");
+        //TODO: don't actually want to call up anything, we want it to just fall through
+    }
 }
