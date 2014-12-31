280261f3df4f09f09a6a77d023f4de7103498f8d
Eric Wehrmeister <eric.wehrmeister@rackspace.com>
2014-12-31 10:35:04 -0600

    Testing handleRequest

--- repose-aggregator/components/filters/forwarded-proto/src/test/scala/org/openrepose/filters/forwardedproto/ForwardedProtoHandlerTest.scala
+++ repose-aggregator/components/filters/forwarded-proto/src/test/scala/org/openrepose/filters/forwardedproto/ForwardedProtoHandlerTest.scala
@@ -1,36 +1,35 @@
 package org.openrepose.filters.forwardedproto
 
-import java.util
-import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
-import javax.servlet.{ServletRequest, FilterChain, ServletResponse}
-
-import org.mockito.Matchers._
-import org.mockito.Mockito._
-import org.mockito.invocation.InvocationOnMock
-import org.mockito.stubbing.Answer
-
-import org.scalatest.FunSpec
-
-import scala.collection.JavaConverters.asJavaEnumerationConverter
-
-class ForwardedProtoHandlerTest extends FunSpec {
+import java.io.InputStream
+import javax.servlet.http.{HttpServletResponse, HttpServletResponseWrapper}
 
 
 
-  describe("test") {
-    it("wat") {
+//import com.mockrunner.mock.web._
+import org.openrepose.commons.utils.http.header.HeaderName
+import org.openrepose.commons.utils.servlet.http.ReadableHttpServletResponse
+import org.openrepose.core.filter.logic.FilterDirector
+//import org.openrepose.filters.addheader.config.{AddHeadersConfig, Header, HttpMessage}
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers, PrivateMethodTester}
+import com.mockrunner.mock.web.MockHttpServletRequest
 
-      val protoFilter = new ForwardedProtoFilter();
-      val req = mockRequest(Map())
-      val fc = mock(classOf[FilterChain])
-      protoFilter.doFilter(req, null, fc)
+class ForwardedProtoHandlerTest extends FunSpec with Matchers with PrivateMethodTester with BeforeAndAfter {
+
+  var handler: ForwardedProtoHandler = _
+  var myDirector: FilterDirector = _
+
+  describe("handleRequest") {
 
 
+    it("the request should contain X-Forwarded-Proto header ") {
+      val mockRequest = new MockHttpServletRequest()
+      handler = new ForwardedProtoHandler()
 
-
-
+      myDirector = handler.handleRequest(mockRequest, null)
+      myDirector.requestHeaderManager().headersToAdd().containsKey(HeaderName.wrap("X-Forwarded-Proto"))
+      myDirector.requestHeaderManager().headersToAdd().get(HeaderName.wrap("X-Forwarded-Proto")).contains("HTTP")
+      myDirector.requestHeaderManager().headersToAdd().size() shouldEqual 1
     }
-
 
   }
 
