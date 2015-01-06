1f48dbf22fafc8f23aecff0c68ec57bb37df6d0a
David Kowis <david.kowis@rackspace.com>
2015-01-06 16:16:50 -0600

    Fixing the filter bundles, and making FilterThree scenario work

--- repose-support/filter-three/src/main/scala/org/openrepose/filters/core/filterthree/FilterThree.scala
+++ repose-support/filter-three/src/main/scala/org/openrepose/filters/core/filterthree/FilterThree.scala
@@ -6,9 +6,8 @@ import javax.servlet.http.HttpServletRequest
 
 import org.openrepose.others.SimplicityDivine
 
-/**
- * Created by dimi5963 on 1/6/15.
- */
+import scala.util.{Failure, Success}
+
 @Named
 class FilterThree extends Filter {
   override def init(p1: FilterConfig): Unit = {
@@ -16,7 +15,32 @@ class FilterThree extends Filter {
   }
 
   override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
-    new SimplicityDivine().createBar
+    //Needs to call class.forName to try to load a dependency that it doesn't have, but another ear does have.
+    //It should throw a ClassNotFound exception
+
+    val didItWork = try {
+      //This is a dependency in the FilterOne ear file
+      println("\n\n")
+      println("FILTER THREE: trying to instantiate Simplicity Divine")
+      println("\n\n")
+      Class.forName("org.openrepose.others.SimplicityDivine")
+      println("\n\n")
+      println("FILTER THREE: was able to instantiate Simplicity Divine REAL BAD")
+      println("\n\n")
+      Failure(new Exception("Should have not been able to instantiate SimplicityDivine"))
+    } catch {
+      case e: ClassNotFoundException => {
+        println("Caught the CNFE, this is the appropriate behavior")
+        Success("Yep")
+      }
+    }
+
+    if(didItWork.isFailure) {
+      println("Throwing encapsulated exception")
+      throw didItWork.failed.get
+    }
+
+    println("Finishing off the filter")
     chain.doFilter(request, response)
   }
 
