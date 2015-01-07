508ff5b33b789d0529f4db1954e7438d811414c9
David Kowis <david.kowis@rackspace.com>
2015-01-07 16:23:05 -0600

    Oops forgot to add this guy

---
+++ repose-aggregator/core/core-lib/src/test/scala/org/openrepose/core/Marshaller.scala
@@ -0,0 +1,57 @@
+package org.openrepose.core
+
+import java.net.URL
+import java.nio.charset.StandardCharsets
+
+import org.openrepose.commons.config.parser.jaxb.JaxbConfigurationParser
+import org.openrepose.commons.config.resource.impl.{ByteArrayConfigurationResource, BufferedURLConfigurationResource}
+import org.openrepose.core.container.config.ContainerConfiguration
+import org.openrepose.core.systemmodel.SystemModel
+
+import scala.reflect.ClassTag
+
+object Marshaller {
+
+  val systemModelXSD = getClass.getResource("/META-INF/schema/system-model/system-model.xsd")
+  val containerConfigXSD = getClass.getResource("/META-INF/schema/container/container-configuration.xsd")
+
+
+  def systemModel(resource: String): SystemModel = {
+    configResource[SystemModel](resource, systemModelXSD)
+  }
+
+  def containerConfig(resource: String): ContainerConfiguration = {
+    configResource[ContainerConfiguration](resource, containerConfigXSD)
+  }
+
+  def systemModelString(content: String): SystemModel = {
+    configFromString[SystemModel](content, systemModelXSD)
+  }
+
+  def configFromString[T: ClassTag](content: String, xsdURL: URL): T = {
+    import scala.reflect._
+    val ct: ClassTag[T] = classTag[T]
+    val parser = JaxbConfigurationParser.getXmlConfigurationParser(
+      ct.runtimeClass.asInstanceOf[Class[T]],
+      xsdURL,
+      this.getClass.getClassLoader)
+
+    val configResource = new ByteArrayConfigurationResource("", content.getBytes(StandardCharsets.UTF_8))
+
+    parser.read(configResource)
+  }
+
+  def configResource[T: ClassTag](resource: String, xsdURL: URL): T = {
+    import scala.reflect._
+    val ct: ClassTag[T] = classTag[T]
+    val parser = JaxbConfigurationParser.getXmlConfigurationParser(
+      ct.runtimeClass.asInstanceOf[Class[T]],
+      xsdURL,
+      this.getClass.getClassLoader)
+
+    val configResource = new BufferedURLConfigurationResource(this.getClass.getResource(resource))
+
+    parser.read(configResource)
+  }
+
+}
