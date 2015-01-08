bf1a4e247e60b37d375d2dab666ecccfa02c1dac
James Combs <james.combs@RACKSPACE.COM>
2015-01-08 14:29:45 -0600

    Adding test for forwarded-proto filter.

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/forwardedProto/AddForwardedProtoHeaderTest.groovy
@@ -0,0 +1,44 @@
+package features.filters.forwardedProto
+
+import framework.ReposeValveTest
+import org.rackspace.deproxy.Deproxy
+
+/**
+ * Created by jcombs on 1/7/15.
+ */
+class AddForwardedProtoHeaderTest extends ReposeValveTest {
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/forwardedProto", params)
+        repose.start(waitOnJmxAfterStarting: false)
+        repose.waitForNon500FromUrl(reposeEndpoint)
+    }
+
+    def cleanupSpec() {
+        deproxy.shutdown()
+        repose.stop()
+    }
+
+    def "When using forwarded-proto filter, Repose addes the x-forwarded-proto header to the request" () {
+        given:
+        def Map headers = ["x-rax-user": "test-user-a", "x-rax-groups": "reposegroup11"]
+
+        when: "Request contains value(s) of the target header"
+        def mc = deproxy.makeRequest([url: reposeEndpoint, headers: headers])
+
+        then: "The x-forwarded-proto header is additionally added to the request going to the origin service"
+        mc.getSentRequest().getHeaders().contains("x-rax-user")
+        mc.getSentRequest().getHeaders().getFirstValue("x-rax-user") == "test-user-a"
+        mc.getSentRequest().getHeaders().contains("x-forwarded-proto") == false
+        mc.handlings[0].request.headers.contains("x-rax-user")
+        mc.handlings[0].request.headers.getFirstValue("x-rax-user") == "test-user-a"
+        mc.handlings[0].request.headers.contains("x-forwarded-proto")
+        String forwardedProto = mc.handlings[0].request.headers.getFirstValue("x-forwarded-proto")
+        forwardedProto.toLowerCase().contains("http")
+    }
+}
