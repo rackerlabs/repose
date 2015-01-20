39e29ad7e5b802656d4a1ea1fe25ffd8c307096e
jenny.vo <jenny.vo@rackspace.com>
2015-01-20 16:02:27 -0600

    remove additional tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthn/akkatimeout/HttpConnTimeoutGreaterThan50SecTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthn/akkatimeout/HttpConnTimeoutGreaterThan50SecTest.groovy
@@ -1,5 +1,4 @@
 package features.filters.clientauthn.akkatimeout
-
 import framework.ReposeValveTest
 import framework.category.Slow
 import framework.mocks.MockIdentityService
@@ -7,10 +6,8 @@ import org.joda.time.DateTime
 import org.junit.experimental.categories.Category
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
-import org.rackspace.deproxy.Response
 
 import javax.servlet.http.HttpServletResponse
-
 /**
  * Created by jennyvo on 1/5/15.
  *  Previously akkatimeout was hard code to 50 second now set the same as http connection
@@ -94,39 +91,6 @@ class HttpConnTimeoutGreaterThan50SecTest extends ReposeValveTest {
         MessageChain mc = deproxy.makeRequest(
                 url: "$reposeEndpoint/servers/613/",
                 method: 'GET',
-                headers: [
-                        'content-type': 'application/json',
-                        'X-Auth-Token': fakeIdentityService.client_token
-                ]
-        )
-
-        then: "Request should not be passed from repose"
-        mc.receivedResponse.code == HttpServletResponse.SC_GATEWAY_TIMEOUT.toString()
-        mc.handlings.size() == 0
-        sleep(1000)
-        reposeLogSearch.searchByString("Error acquiring value from akka .GET. or the cache. Reason: Futures timed out after .61000 milliseconds.").size() > 0
-        reposeLogSearch.searchByString("NullPointerException").size() == 0
-    }
-
-    def "akka timeout POST test, auth response time out greater than socket connection time out" () {
-        reposeLogSearch.cleanLog()
-        fakeIdentityService.with {
-            client_token = UUID.randomUUID().toString()
-            tokenExpiresAt = DateTime.now().plusDays(1)
-            client_tenant = 613
-            service_admin_role = "not-admin"
-            client_userid = 1234
-            sleeptime = 62000
-            fakeIdentityService.generateTokenHandler = {
-                request, xml ->
-                    new Response(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null, null, "")
-            }
-        }
-
-        when: "User passes a request through repose"
-        MessageChain mc = deproxy.makeRequest(
-                url: "$reposeEndpoint/servers/613/",
-                method: 'POST',
                 headers: [
                         'content-type': 'application/json',
                         'X-Auth-Token': fakeIdentityService.client_token
