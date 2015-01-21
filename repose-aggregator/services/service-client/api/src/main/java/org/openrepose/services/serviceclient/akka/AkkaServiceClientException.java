3bd5d1eb59e90a63fb90fd85cb1decb0c4315a23
wdschei <Bill.Scheidegger@Rackspace.com>
2015-01-21 07:51:20 -0600

    Removed the constructors from the AkkaServiceClientException that are not currently being used.

--- repose-aggregator/services/service-client/api/src/main/java/org/openrepose/services/serviceclient/akka/AkkaServiceClientException.java
+++ repose-aggregator/services/service-client/api/src/main/java/org/openrepose/services/serviceclient/akka/AkkaServiceClientException.java
@@ -1,19 +1,7 @@
 package org.openrepose.services.serviceclient.akka;
 
 public class AkkaServiceClientException extends Exception {
-    public AkkaServiceClientException() {
-        super();
-    }
-
-    public AkkaServiceClientException(String message) {
-        super(message);
-    }
-
     public AkkaServiceClientException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public AkkaServiceClientException(Throwable cause) {
-        super(cause);
     }
 }
