6a8e58a705be6c07f35eaf81cb8d13e67b2e906b
wdschei <Bill.Scheidegger@Rackspace.com>
2015-01-23 13:54:34 -0600

    Removed the new AuthenticationServiceException.

--- repose-aggregator/external/service-clients/auth/src/main/java/org/openrepose/common/auth/openstack/AuthenticationServiceException.java
+++
@@ -1,7 +0,0 @@
-package org.openrepose.common.auth.openstack;
-
-public class AuthenticationServiceException extends Exception {
-    public AuthenticationServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
