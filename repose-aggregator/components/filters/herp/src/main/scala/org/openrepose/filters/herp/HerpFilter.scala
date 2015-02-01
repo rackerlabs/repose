4142c10753757a848bc79cf7a595adce2914bbbe
wdschei <Bill.Scheidegger@Rackspace.com>
2015-02-01 09:47:01 -0600

    Updated the only use of the HttpStatusCode.fromInt() method to use the more standardized org.springframework.http.HttpStatus.valueOf(). This was wrapped in a scala.util.Try() to prevent any Exceptions from escaping and provide a default return value in their occurance.

--- repose-aggregator/components/filters/herp/src/main/scala/org/openrepose/filters/herp/HerpFilter.scala
+++ repose-aggregator/components/filters/herp/src/main/scala/org/openrepose/filters/herp/HerpFilter.scala
@@ -10,14 +10,16 @@ import com.github.jknack.handlebars.{Handlebars, Template}
 import com.rackspace.httpdelegation._
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.config.manager.UpdateListener
-import org.openrepose.commons.utils.http.{CommonHttpHeader, HttpStatusCode, OpenStackServiceHeader}
+import org.openrepose.commons.utils.http.{CommonHttpHeader, OpenStackServiceHeader}
 import org.openrepose.core.filter.FilterConfigHelper
 import org.openrepose.core.services.config.ConfigurationService
 import org.openrepose.core.services.context.ServletContextHelper
 import org.openrepose.filters.herp.config.HerpConfig
 import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.http.HttpStatus
 
 import scala.collection.JavaConverters._
+import scala.util.Try
 
 class HerpFilter extends Filter with HttpDelegationManager with UpdateListener[HerpConfig] with LazyLogging {
   private final val DEFAULT_CONFIG = "highly-efficient-record-processor.cfg.xml"
@@ -93,7 +95,7 @@ class HerpFilter extends Filter with HttpDelegationManager with UpdateListener[H
       "parameters" -> translateParameters().asJava.entrySet(),
       "timestamp" -> System.currentTimeMillis(),
       "responseCode" -> httpServletResponse.getStatus,
-      "responseMessage" -> HttpStatusCode.fromInt(httpServletResponse.getStatus).name(),
+      "responseMessage" -> Try(HttpStatus.valueOf(httpServletResponse.getStatus).name()).getOrElse("UNKNOWN"),
       "guid" -> java.util.UUID.randomUUID.toString,
       "serviceCode" -> serviceCode,
       "region" -> region,
