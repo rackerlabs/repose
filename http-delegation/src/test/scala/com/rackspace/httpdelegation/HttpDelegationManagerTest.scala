b9007887b80fe75fb01501934393b841935c9bbb
wdschei <Bill.Scheidegger@Rackspace.com>
2015-03-24 19:25:16 -0500

    Converted the Start, Section Delimiter, and End tags from the defaults to a more aesthetically pleasing format.

--- http-delegation/src/test/scala/com/rackspace/httpdelegation/HttpDelegationManagerTest.scala
+++ http-delegation/src/test/scala/com/rackspace/httpdelegation/HttpDelegationManagerTest.scala
@@ -1,9 +1,9 @@
 /*
- * #%L
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
  * Repose
- * %%
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
  * Copyright (C) 2010 - 2015 Rackspace US, Inc.
- * %%
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
  */
 package com.rackspace.httpdelegation
 
