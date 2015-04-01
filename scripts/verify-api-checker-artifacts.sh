5c098bceb9621113fae37b63e0718b3b8a9e9919
Damien Johnson <damien.johnson@rackspace.com>
2015-04-01 15:02:56 -0500

    Fixing the chain of &&

--- scripts/verify-api-checker-artifacts.sh
+++ scripts/verify-api-checker-artifacts.sh
@@ -7,53 +7,53 @@ NEXUS_HOST='https://maven.research.rackspacecloud.com' #Nexus repository hostnam
 #End of constants
 
 #Start of script
-echo 'Updating and upgrading packages...'
+echo 'Updating and upgrading packages...' &&
 apt-get update &&
 apt-get upgrade &&
 
-echo 'Installing git...'
+echo 'Installing git...' &&
 apt-get install -y git &&
 
-echo 'Installing zip...'
+echo 'Installing zip...' &&
 apt-get install -y zip &&
 
-echo 'Installing java7...'
+echo 'Installing java7...' &&
 apt-get install -y openjdk-7-jdk &&
-update-alternatives --set java '/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java'
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+update-alternatives --set java '/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java' &&
+export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::") &&
 
-echo 'Installing maven...'
+echo 'Installing maven...' &&
 apt-get install -y maven &&
 
-echo 'Installing JAD...'
+echo 'Installing JAD...' &&
 curl -O "http://varaneckas.com/jad/$JAD_FILE" &&
 mkdir -p '/opt/jad/bin' &&
 unzip -f $JAD_FILE -d '/opt/jad/bin/' &&
 rm -f $JAD_FILE &&
 
-echo 'Creating working directories...'
+echo 'Creating working directories...' &&
 rm -rf workspace/api-checker/built &&
 rm -rf workspace/api-checker/published &&
 mkdir -p workspace/api-checker/built &&
 mkdir -p workspace/api-checker/published &&
 
-echo 'Cloning api-checker project...'
+echo 'Cloning api-checker project...' &&
 rm -rf api-checker &&
 git clone 'https://github.com/rackerlabs/api-checker.git' &&
 
 for i in $( seq 13 22 ); do
-	echo "Downloading api-checker-1.0.$i jar..."
+	echo "Downloading api-checker-1.0.$i jar..." &&
 	curl -o "workspace/api-checker/published/1.0.$i/checker-core-1.0.$i.jar" --create-dirs \
 		"$NEXUS_HOST/service/local/artifact/maven/content?r=releases&g=com.rackspace.papi.components.api-checker&a=checker-core&v=1.0.$i" &&
 
-	echo "Building api-checker-1.0.$1 jar..."
+	echo "Building api-checker-1.0.$1 jar..." &&
 	cd api-checker/core/ &&
 	git checkout "api-checker-1.0.$i" &&
 	mvn -DskipTests clean package &&
 	mv -u "target/checker-core-1.0.$i-SNAPSHOT.jar" "workspace/api-checker/built/1.0.$i/checker-core-1.0.$i.jar" &&
 	cd ../.. &&
 
-	echo "Extracting JAR files for version 1.0.$i..."
+	echo "Extracting JAR files for version 1.0.$i..." &&
 	mkdir -p "workspace/api-checker/built/1.0.$i/src" &&
 	mkdir -p "workspace/api-checker/published/1.0.$i/src" &&
 	cd "workspace/api-checker/built/1.0.$i/src" &&
@@ -62,18 +62,18 @@ for i in $( seq 13 22 ); do
 	jar xf "../checker-core-1.0.$i.jar" &> /dev/null &&
 	cd ../../../../.. &&
 
-	echo "Decompiling class files for version 1.0.$i..."
+	echo "Decompiling class files for version 1.0.$i..." &&
 	/opt/jad/bin/jad -o -r -sjava -d"workspace/api-checker/built/1.0.$i/src" "workspace/api-checker/built/1.0.$i/src/**.class" &> /dev/null &&
 	/opt/jad/bin/jad -o -r -sjava -d"workspace/api-checker/published/1.0.$i/src" "workspace/api-checker/published/1.0.$i/src/**.class" &> /dev/null &&
 
-	echo "Comparing files for version 1.0.$i..."
+	echo "Comparing files for version 1.0.$i..." &&
 	find "workspace/api-checker/built/1.0.$i/src" -type f ! -iname '*.class' | sort | xargs md5 > "workspace/api-checker/built/1.0.$i/built.md5" &&
 	sed -i '' 's/built\///g' "workspace/api-checker/built/1.0.$i/built.md5" &&
 	find "workspace/api-checker/published/1.0.$i/src" -type f ! -iname '*.class' | sort | xargs md5 > "workspace/api-checker/published/1.0.$i/published.md5" &&
 	sed -i '' 's/published\///g' "workspace/api-checker/published/1.0.$i/published.md5" &&
-	diff "workspace/api-checker/built/1.0.$i/built.md5" "workspace/api-checker/published/1.0.$i/published.md5"
+	diff "workspace/api-checker/built/1.0.$i/built.md5" "workspace/api-checker/published/1.0.$i/published.md5" &&
 
-	echo ''
+	echo '' &&
 done
 
 echo 'Done'
