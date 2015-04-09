945a8b757b12b280d99cde24ee0b37e90369e8eb
David Kowis <david.kowis@rackspace.com>
2015-04-09 16:16:53 -0500

    Implementation maybe?
    Also the tests are still not good, have to implement raw socket reading
    to avoid anything parsing the HTTP payload for me

--- repose-aggregator/components/filters/merge-header/src/main/scala/org/openrepose/filters/mergeheader/MergeHeaderHandler.scala
+++ repose-aggregator/components/filters/merge-header/src/main/scala/org/openrepose/filters/mergeheader/MergeHeaderHandler.scala
@@ -4,16 +4,41 @@ import javax.servlet.http.HttpServletRequest
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.utils.servlet.http.ReadableHttpServletResponse
-import org.openrepose.core.filter.logic.FilterDirector
+import org.openrepose.core.filter.logic.{FilterAction, FilterDirector}
 import org.openrepose.core.filter.logic.common.AbstractFilterLogicHandler
+import org.openrepose.core.filter.logic.impl.FilterDirectorImpl
 
 class MergeHeaderHandler(filterConfig: MergeHeaderConfig) extends AbstractFilterLogicHandler with LazyLogging {
 
+  import scala.collection.JavaConversions._
+
   override def handleRequest(request: HttpServletRequest, response: ReadableHttpServletResponse): FilterDirector = {
-    ???
+    val fd = new FilterDirectorImpl()
+    fd.setFilterAction(FilterAction.PASS)
+
+    Option(filterConfig.getRequest).foreach { requestConfig =>
+      requestConfig.getHeader.toList.foreach { name =>
+        val headerValue = request.getHeader(name)
+        logger.debug(s"REQUEST: Putting $name to $headerValue")
+        fd.requestHeaderManager().putHeader(name, headerValue)
+      }
+    }
+
+    fd
   }
 
   override def handleResponse(request: HttpServletRequest, response: ReadableHttpServletResponse): FilterDirector = {
-    ???
+    val fd = new FilterDirectorImpl()
+    fd.setFilterAction(FilterAction.PASS)
+
+    Option(filterConfig.getResponse).foreach {responseConfig =>
+      responseConfig.getHeader.toList.foreach { name =>
+        val headerValue = request.getHeader(name)
+        logger.debug(s"RESPONSE: Putting $name to $headerValue")
+        fd.responseHeaderManager().putHeader(name, headerValue)
+      }
+    }
+
+    fd
   }
 }
