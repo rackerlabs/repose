f6731e2350148b12ff8b864ec32e71708455038e
Tyler Royal <tyler.royal@rackspace.com>
2015-04-09 13:07:29 -0500

    caching added to the filter

--- repose-aggregator/components/filters/valkyrie-authorization/src/main/scala/org/openrepose/filters/valkyrieauthorization/ValkyrieAuthorizationFilter.scala
+++ repose-aggregator/components/filters/valkyrie-authorization/src/main/scala/org/openrepose/filters/valkyrieauthorization/ValkyrieAuthorizationFilter.scala
@@ -9,19 +9,20 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import com.rackspace.httpdelegation.HttpDelegationManager
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.config.manager.UpdateListener
-import org.openrepose.commons.utils.http.{ServiceClientResponse, OpenStackServiceHeader}
+import org.openrepose.commons.utils.http.{OpenStackServiceHeader, ServiceClientResponse}
 import org.openrepose.commons.utils.servlet.http.{MutableHttpServletRequest, MutableHttpServletResponse}
 import org.openrepose.core.filter.FilterConfigHelper
 import org.openrepose.core.filter.logic.common.AbstractFilterLogicHandler
 import org.openrepose.core.services.config.ConfigurationService
+import org.openrepose.core.services.datastore.DatastoreService
 import org.openrepose.core.services.serviceclient.akka.AkkaServiceClient
-import org.openrepose.filters.valkyrieauthorization.config.{ValkyrieServer, ValkyrieAuthorizationConfig}
+import org.openrepose.filters.valkyrieauthorization.config.{ValkyrieAuthorizationConfig, ValkyrieServer}
 
 import scala.io.Source
-import scala.util.{Success, Failure, Try}
+import scala.util.{Failure, Success, Try}
 
 @Named
-class ValkyrieAuthorizationFilter @Inject()(configurationService: ConfigurationService, akkaServiceClient: AkkaServiceClient)
+class ValkyrieAuthorizationFilter @Inject()(configurationService: ConfigurationService, akkaServiceClient: AkkaServiceClient, datastoreService: DatastoreService)
   extends AbstractFilterLogicHandler
   with Filter
   with UpdateListener[ValkyrieAuthorizationConfig]
@@ -29,6 +30,7 @@ class ValkyrieAuthorizationFilter @Inject()(configurationService: ConfigurationS
   with LazyLogging {
 
   private final val DEFAULT_CONFIG = "valkyrie-authorization.cfg.xml"
+  val datastore = datastoreService.getDefaultDatastore
 
   var configurationFile: String = DEFAULT_CONFIG
   var configuration: ValkyrieAuthorizationConfig = _
@@ -51,12 +53,16 @@ class ValkyrieAuthorizationFilter @Inject()(configurationService: ConfigurationS
     configurationService.unsubscribeFrom(configurationFile, this)
   }
 
+  trait ValkyrieResult
+  case class DeviceToPermission(device: Int, permission: String)
+  case class DeviceList(devices: Seq[DeviceToPermission]) extends ValkyrieResult
+  case class ResponseResult(statusCode: Int, message: String = "") extends ValkyrieResult
+
   override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain): Unit = {
     val mutableHttpRequest = MutableHttpServletRequest.wrap(servletRequest.asInstanceOf[HttpServletRequest])
     val mutableHttpResponse = MutableHttpServletResponse.wrap(mutableHttpRequest, servletResponse.asInstanceOf[HttpServletResponse])
 
-    case class ValkyrieResult(statusCode: Int, message: String = "")
-    def headerResult(headerName: String, valkyrieResult: ValkyrieResult): Either[String, ValkyrieResult] = {
+    def headerResult(headerName: String, valkyrieResult: ResponseResult): Either[String, ResponseResult] = {
       Option(mutableHttpRequest.getHeader(headerName)).map {
         Left(_)
       } getOrElse {
@@ -64,80 +70,92 @@ class ValkyrieAuthorizationFilter @Inject()(configurationService: ConfigurationS
       }
     }
 
-    val requestedTenantId = headerResult(OpenStackServiceHeader.TENANT_ID.toString, ValkyrieResult(502, "No tenant ID specified"))
-    val requestedDeviceId = headerResult("X-Device-Id", ValkyrieResult(502, "No device ID specified"))
-    val requestedContactId = headerResult("X-Contact-Id", ValkyrieResult(403, "No contact ID specified"))
-    val valkyrieServer = configuration.getValkyrieServer
-
-    val clientResponse: ValkyrieResult = (requestedTenantId, requestedContactId, requestedDeviceId) match {
+    val requestedTenantId = headerResult(OpenStackServiceHeader.TENANT_ID.toString, ResponseResult(502, "No tenant ID specified"))
+    val requestedDeviceId = headerResult("X-Device-Id", ResponseResult(502, "No device ID specified"))
+    val requestedContactId = headerResult("X-Contact-Id", ResponseResult(403, "No contact ID specified"))
+    val clientResponse: ResponseResult = (requestedTenantId, requestedContactId, requestedDeviceId) match {
       case (_, _, Right(x)) => x
       case (_, Right(x), _) => x
       case (Right(x), _, _) => x
       case (Left(tenantId), Left(contactId), Left(deviceId)) =>
-        tryValkyrieCall(valkyrieServer, tenantId, contactId) match {
-          case Success(response) => {
-            if (response.getStatus == 200) {
-              parseDevices(response.getData) match {
-                case Success(deviceList) => {
-                  deviceList.find(_.device.toString == deviceId).map { deviceToPermission =>
-                    deviceToPermission.permission match {
-                      case "view_product" if List("GET", "HEAD").contains(mutableHttpRequest.getMethod) => ValkyrieResult(200)
-                      case "edit_product" => ValkyrieResult(200)
-                      case "admin_product" => ValkyrieResult(200)
-                      case _ => ValkyrieResult(403, "Not Authorized")
-                    }
-                  } getOrElse {
-                    ValkyrieResult(403, "Not Authorized")
-                  }
-                }
-                case Failure(x) => {
-                  //JSON Parsing failure
-                  ValkyrieResult(502, x.getMessage)
-                }
-              }
-            } else {
-              //Didn't get a 200 from valkyrie
-              ValkyrieResult(502, s"Valkyrie returned a ${response.getStatus}")
-            }
-          }
-          case Failure(exception) => {
-            ValkyrieResult(502, s"Unable to communicate with Valkyrie: ${exception.getMessage}")
-          }
+        tenantId.replaceAll(".*:", "")
+        val transformedTenant = tenantId.substring(tenantId.indexOf(":") + 1, tenantId.length)
+        datastoreValue(transformedTenant, contactId, deviceId, configuration.getValkyrieServer, mutableHttpRequest.getMethod) match {
+          case x:DeviceList => authorize (deviceId, x.devices, mutableHttpRequest.getMethod)
+          case x:ResponseResult => x
         }
-
     }
 
     clientResponse match {
-      case ValkyrieResult(200, _) =>
+      case ResponseResult(200, _) =>
         filterChain.doFilter(mutableHttpRequest, mutableHttpResponse)
-      case ValkyrieResult(code, message) if Option(configuration.getDelegating).isDefined =>
+      case ResponseResult(code, message) if Option(configuration.getDelegating).isDefined =>
         buildDelegationHeaders(code, "valkyrie-authorization", message, configuration.getDelegating.getQuality).foreach { case (key, values) =>
           values.foreach { value => mutableHttpRequest.addHeader(key, value) }
         }
         filterChain.doFilter(mutableHttpRequest, mutableHttpResponse)
-      case ValkyrieResult(code, message) =>
+      case ResponseResult(code, message) =>
         mutableHttpResponse.sendError(code, message)
+    }
+  }
+
+  def cacheKey(transformedTenant: String, contactId: String): String = {
+    transformedTenant + contactId
+  }
+
+  def datastoreValue(transformedTenant: String, contactId: String, deviceId: String,  valkyrieServer: ValkyrieServer, method: String): ValkyrieResult = {
+    Option(datastore.get(cacheKey(transformedTenant, contactId))) match {
+      case Some(x: Seq[DeviceToPermission]) => DeviceList(x)
+      case None => valkyrieAuthorize(valkyrieServer, transformedTenant, contactId, deviceId, method)
+    }
+  }
+
+  def valkyrieAuthorize(valkyrieServer: ValkyrieServer, tenantId: String, contactId: String, deviceId: String, method: String): ValkyrieResult = {
+    tryValkyrieCall(valkyrieServer, tenantId, contactId) match {
+      case Success(response) => {
+        if (response.getStatus == 200) {
+          parseDevices(response.getData) match {
+            case Success(deviceList) => DeviceList(deviceList)
+            case Failure(x) => ResponseResult(502, x.getMessage) //JSON Parsing failure
+          }
+        } else {
+          ResponseResult(502, s"Valkyrie returned a ${response.getStatus}") //Didn't get a 200 from valkyrie
+        }
+      }
+      case Failure(exception) => {
+        ResponseResult(502, s"Unable to communicate with Valkyrie: ${exception.getMessage}")
+      }
+    }
+  }
+
+  def authorize(deviceId: String, deviceList: Seq[DeviceToPermission], method: String): ResponseResult = {
+    deviceList.find(_.device.toString == deviceId).map { deviceToPermission =>
+      deviceToPermission.permission match {
+        case "view_product" if List("GET", "HEAD").contains(method) => ResponseResult(200)
+        case "edit_product" => ResponseResult(200)
+        case "admin_product" => ResponseResult(200)
+        case _ => ResponseResult(403, "Not Authorized")
+      }
+    } getOrElse {
+      ResponseResult(403, "Not Authorized")
     }
   }
 
   def tryValkyrieCall(valkyrieServer: ValkyrieServer, tenantId: String, contactId: String): Try[ServiceClientResponse] = {
     import collection.JavaConversions._
 
-    val transformedTenant = tenantId.substring(tenantId.indexOf(":") + 1, tenantId.length)
-    val uri = valkyrieServer.getUri + s"/account/$transformedTenant/permissions/contacts/devices/by_contact/$contactId/effective"
-    Try(akkaServiceClient.get(transformedTenant + contactId,
+    val uri = valkyrieServer.getUri + s"/account/$tenantId/permissions/contacts/devices/by_contact/$contactId/effective"
+    Try(akkaServiceClient.get(cacheKey(tenantId, contactId),
       uri,
       Map("X-Auth-User" -> valkyrieServer.getUsername, "X-Auth-Token" -> valkyrieServer.getPassword))
     )
 
   }
 
-  case class DeviceToPermission(device: Int, permission: String)
-
   def parseDevices(is: InputStream): Try[Seq[DeviceToPermission]] = {
+    import play.api.libs.functional.syntax._
     import play.api.libs.json.Reads._
     import play.api.libs.json._
-    import play.api.libs.functional.syntax._
 
     implicit val deviceToPermissions = (
       (JsPath \ "item_id").read[Int] and
