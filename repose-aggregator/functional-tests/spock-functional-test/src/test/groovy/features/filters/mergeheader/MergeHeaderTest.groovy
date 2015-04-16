6b225f36db094ab1fe78ffda818b58c442da7df1
Adrian <adrian.george@rackspace.com>
2015-04-16 10:23:42 -0500

    Fixed the test so that it works

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/mergeheader/MergeHeaderTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/mergeheader/MergeHeaderTest.groovy
@@ -1,7 +1,27 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
 package features.filters.mergeheader
 
 import framework.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.Header
 import org.rackspace.deproxy.MessageChain
 import org.rackspace.deproxy.Response
 
@@ -10,6 +30,7 @@ import org.rackspace.deproxy.Response
  */
 class MergeHeaderTest extends ReposeValveTest {
     static Map params = [:]
+
     def setupSpec() {
         params = properties.getDefaultTemplateParams()
         repose.configurationProvider.applyConfigs("common", params)
@@ -24,6 +45,7 @@ class MergeHeaderTest extends ReposeValveTest {
         repose.stop()
         deproxy.shutdown()
     }
+
     def "Should not split request headers when configured as such with merge-header filter"() {
         given: "configurged to to split headers using the merge-header filter"
 
@@ -31,10 +53,10 @@ class MergeHeaderTest extends ReposeValveTest {
                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.65 Safari/537.36"
         def reqHeaders =
                 [
-                        "user-agent": userAgentValue,
-                        "x-pp-user" : "usertest1, usertest2, usertest3",
-                        "accept"    : "application/xml;q=1 , application/json;q=0.5",
-                        "Accept-Charset" : "UTF-8, US-ASCII, ISO-8859-1, *"
+                        "user-agent"    : userAgentValue,
+                        "x-pp-user"     : "usertest1, usertest2, usertest3",
+                        "accept"        : "application/xml;q=1 , application/json;q=0.5",
+                        "Accept-Charset": "UTF-8, US-ASCII, ISO-8859-1, *"
                 ]
 
         when: "User sends a request through repose"
@@ -53,10 +75,12 @@ class MergeHeaderTest extends ReposeValveTest {
     def "Should merge response headers when configured as such with merge-header filter"() {
         given: "Origin service returns headers "
         def respHeaders = [
-                "aurl": "http://somehost.com/blah, http://somehost.com/bloo",
-                "accept": "application/xml",
-                "accept": "application/json",
-                "Content": "none"]
+                new Header("aurl", "http://somehost.com/blah"),
+                new Header("aurl", "http://somehost.com/bloo"),
+                new Header("accept", "application/xml"),
+                new Header("accept", "application/json"),
+                new Header("Content", "none"),
+                new Header("Accept-Charset", "UTF-8, US-ASCII, ISO-8859-1")]
         def handler = { request -> return new Response(201, "Created", respHeaders, "") }
 
         when: "User sends a request through repose"
@@ -66,9 +90,12 @@ class MergeHeaderTest extends ReposeValveTest {
         mc.receivedResponse.code == "201"
         mc.handlings.size() == 1
         mc.receivedResponse.headers.findAll("aurl").size() == 1
+        mc.receivedResponse.headers['aurl'].contains("http://somehost.com/blah")
         mc.receivedResponse.headers['aurl'].contains("http://somehost.com/bloo")
         mc.receivedResponse.headers.findAll("via").size() == 1
         mc.receivedResponse.headers.findAll("accept").size() == 1
         mc.receivedResponse.headers['accept'].contains("application/json")
+        mc.receivedResponse.headers['accept'].contains("application/xml")
+        mc.receivedResponse.headers.findAll("Accept-Charset").size() == 3
     }
 }
