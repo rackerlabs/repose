919f4014250f8f996648eb73fba5fbcbab2a2aae
David Kowis <david.kowis@rackspace.com>
2015-04-16 09:49:44 -0500

    Create a very simple smoke test for listening on SSL

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/security/SSLConfigurationStartup.groovy
@@ -0,0 +1,83 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.core.security
+
+import framework.ReposeValveTest
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.conn.ssl.NoopHostnameVerifier
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.ssl.SSLContexts
+import org.rackspace.deproxy.Deproxy
+
+import java.nio.file.Files
+
+/**
+ * Make sure we can start up with SSL configuration parameters
+ */
+class SSLConfigurationStartup extends ReposeValveTest {
+
+    def setupSpec() {
+        cleanLogDirectory()
+        params = properties.getDefaultTemplateParams()
+        repose.configurationProvider.cleanConfigDirectory()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/core/security/simplessl", params)
+
+        //Have to manually copy the keystore, because the applyConfigs breaks everything :(
+        def destination = new FileOutputStream(new File(repose.configDir, "keystore.jks"))
+        def source = new File(repose.configurationProvider.configTemplatesDir, "features/core/security/simplessl/keystore.jks")
+        Files.copy(source.toPath(), destination)
+
+        repose.start()
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+    }
+
+    def cleanupSpec() {
+        deproxy.shutdown()
+        repose.stop()
+    }
+
+    static def params
+
+    def "Can execute a simple request via SSL"() {
+        //A simple request should go through
+        when:
+        def sslContext = SSLContexts.custom().loadTrustMaterial(TrustSelfSignedStrategy.INSTANCE).build()
+        def sf = new SSLConnectionSocketFactory(
+                sslContext,
+                null,
+                null,
+                NoopHostnameVerifier.INSTANCE
+        )
+
+        def client = HttpClients.custom().setSSLSocketFactory(sf).build()
+
+        def get = new HttpGet("https://localhost:$properties.reposePort")
+        def response = client.execute(get)
+
+        then:
+        response.getStatusLine().statusCode == 200
+    }
+
+
+}
