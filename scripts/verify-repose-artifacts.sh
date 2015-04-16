a36c80b8d6741cf59778958d2a91eefadff4a322
wdschei <Bill.Scheidegger@Rackspace.com>
2015-04-15 19:27:14 -0500

    Split the original verify-repose-artifacts.sh script in half and finished fleshing it out.

--- scripts/verify-repose-artifacts.sh
+++ scripts/verify-repose-artifacts.sh
@@ -1,60 +1,146 @@
 #!/bin/bash
-#This script will download and build artifacts for the Repose project, and compare the results.
+START=$(date +"%s")
+echo -en "Starting at: $(date)\n"
+SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
+#This script will determine the Repose versions in the repositories and process each one.
 
 #This script has only been tested on Ubuntu, and will likely require modification to be used
 #on any other Linux distribution. Namely, it is assumed that the deb command is available
 #by default but the rpm command requires a package to be installed.
 
 #Constants
-NEXUS_HOST='https://maven.research.rackspacecloud.com' #Nexus repository hostname and protocol
+## Nexus repository hostname and protocol.
+#MVN_HOST='https://maven.research.rackspacecloud.com'
+## Nexus search path/prefix.
+#MVN_SEARCH="${MVN_HOST}/service/local/lucene/search"
+## Nexus Groups to search.
+#PACKAGES=("com.rackspace.papi.core" "org.openrepose" )
 #End of constants
 
-#Functions
-set_java_version() {
-	if [ "$1" -ne "6" -a "$1" -ne "7" -a "$1" -ne "8" ]; then
-		echo 'Attempted to set invalid or unsupported version of Java'
-		exit 1
-	fi
-	apt-get install -y "openjdk-$1-jdk" &&
-	update-alternatives --set java "/usr/lib/jvm/java-$1-openjdk-amd64/jre/bin/java" &&
-	update-alternatives --set javac "/usr/lib/jvm/java-$1-openjdk-amd64/bin/javac" &&
-	export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
-}
-#End of functions
+##Start of script
+#echo 'Updating and upgrading packages...' &&
+#apt-get update &&
+#apt-get upgrade -y &&
+#
+#echo 'Installing git...' &&
+#apt-get install -y git &&
 
-#Start of script
-echo 'Updating and upgrading packages...' &&
-apt-get update &&
-apt-get upgrade &&
+#echo 'Adding Repose debian repository...' &&
+#wget -O - http://repo.openrepose.org/debian/pubkey.gpg | sudo apt-key add - &&
+#echo "deb http://repo.openrepose.org/debian stable main" > /etc/apt/sources.list.d/openrepose.list &&
+#apt-get update &&
 
-echo 'Installing git...' &&
-apt-get install -y git &&
+#echo 'Querying repository for Repose artifact versions...' &&
+#apt-cache show repose-valve | grep 'Version' | sed 's/Version: //g' | sort &&
 
-echo 'Using java6 first...' &&
-set_java_version 6 &&
+#echo 'Querying Nexus for Repose artifact versions...'
+## This was used to retrieve the list used below.
+#VERSIONS=( "" )
+#for package in ${PACKAGES[*]} ; do
+#	VERSIONS=(${VERSIONS[*]} `curl -s "${MVN_SEARCH}?r=releases&g=${package}&a=valve&p=jar" |
+#	xmllint --xpath '//version' - |
+#	sed 's/<version>//g' |
+#	sed $'s/<\/version>/\\\n/g' |
+#	grep -v '^$' |
+#	sort`)
+#done
+#echo "Versions:"
+#for version in ${VERSIONS[*]} ; do
+#	echo "   ${version}"
+#done
 
-echo 'Adding Repose debian repository...' &&
-wget -O - http://repo.openrepose.org/debian/pubkey.gpg | sudo apt-key add - &&
-echo "deb http://repo.openrepose.org/debian stable main" > /etc/apt/sources.list.d/openrepose.list &&
-apt-get update &&
+## This was the list used for initial testing.
+#VERSIONS=(
+#   1.0.1
+#   2.6.0
+#   6.0.0
+#   7.0.1.1
+#)
 
-echo 'Querying repository for Repose artifact versions...' &&
-apt-cache show repose-valve | grep 'Version' | sed 's/Version: //g' | sort &&
-
-
-echo 'Querying Nexus for Repose artifact versions...' &&
-curl 'https://maven.research.rackspacecloud.com/service/local/lucene/search?repositoryId=releases&g=com.rackspace.papi.core&a=valve&p=jar' |
-xmllint --xpath '//version' - | sed 's/<version>//g' | sed $'s/<\/version>/\\\n/g' | grep -v '^$' | sort &&
-for v in : do
-	
+#Loop through all versions within range (2.6.0-7.0.1.1)
+VERSIONS=(
+##   1.0.1
+##   2.3.0
+##   2.3.1
+##   2.3.2
+##   2.3.3
+##   2.3.4
+##   2.3.5
+##   2.3.6
+##   2.4.0
+##   2.4.1
+##   2.5.0
+   2.6.0
+   2.6.1
+   2.6.10
+   2.6.11
+   2.6.12
+   2.6.2
+   2.6.3
+   2.6.4
+   2.6.5
+   2.6.6
+   2.6.7
+   2.6.8
+   2.6.9
+   2.7.0
+   2.8.0
+   2.8.0.2
+   2.8.1
+   2.8.2
+   2.8.3
+   2.8.4
+   2.8.5
+   2.8.6
+   2.9.0
+   3.0.0
+   3.0.1
+   3.0.2
+   3.0.4
+   3.0.5
+   3.0.6
+   3.1.0
+   3.1.1
+   4.0.0
+   4.1.0
+   4.1.1
+   4.1.2
+   4.1.3
+   4.1.4
+   4.1.5
+   5.0.0
+   5.0.1
+   5.0.2
+   5.0.4
+   5.0.5
+   5.0.9
+   6.0.0
+   6.0.1
+   6.0.2
+   6.1.0.3
+   6.1.1.0
+   6.1.1.1
+   6.2.0.0
+   6.2.0.1
+   6.2.0.2
+   6.2.0.3
+   6.2.1.0
+   6.2.2.0
+   7.0.0.0
+   7.0.0.1
+   7.0.1.0
+   7.0.1.1
+)
+echo "Versions:"
+for version in ${VERSIONS[*]} ; do
+	echo "   ${version}"
 done
-#todo: parse artifact names and version from rpm/deb instead?
-#todo: fetch all artifacts on Nexus of a specific version
-curl 'https://maven.research.rackspacecloud.com/service/local/lucene/search?repositoryId=releases&g=com.rackspace.papi&v=$VERSION&p=jar' #todo: jar, ear, or war
-'workspace/repose/repo/rpm/$RPM_VERSION' #rpm working directory
-'workspace/repose/repo/deb/$DEB_VERSION' #deb working directory
-'workspace/repose/nexus/$NEXUS_VERSION' #nexus working directory
 
-#Loop through all versions within range (2.3-7.0)
-
+for version in ${VERSIONS[*]} ; do
+	${SCRIPT_DIR}/verify-repose-artifact.sh ${version}
+done # version in ${VERSIONS[*]}
+STOP=$(date +"%s")
+DIFF=$(($STOP-$START))
+echo -en "\nTotal time: $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds\n"
+echo -en "Finished at: $(date)\n"
 #End of script
