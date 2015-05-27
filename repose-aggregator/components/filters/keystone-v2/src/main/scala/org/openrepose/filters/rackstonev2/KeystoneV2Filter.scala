5a9530bb7d3882747758439f0d43fd2ec0e9a7a0
David Kowis <david.kowis@rackspace.com>
2015-05-27 12:02:42 -0500

    Fixing more Keystone names

--- repose-aggregator/components/filters/keystone-v2/src/main/scala/org/openrepose/filters/rackstonev2/KeystoneV2Filter.scala
+++ repose-aggregator/components/filters/keystone-v2/src/main/scala/org/openrepose/filters/rackstonev2/KeystoneV2Filter.scala
@@ -1,4 +1,4 @@
-package org.openrepose.filters.rackstonev2
+package org.openrepose.filters.Keystonev2
 
 import java.net.URL
 import javax.inject.{Inject, Named}
@@ -11,32 +11,32 @@ import org.openrepose.core.filter.FilterConfigHelper
 import org.openrepose.core.services.config.ConfigurationService
 import org.openrepose.core.services.datastore.DatastoreService
 import org.openrepose.core.services.serviceclient.akka.AkkaServiceClient
-import org.openrepose.filters.rackstonev2.config.RackstoneV2Config
+import org.openrepose.filters.keystonev2.config.KeystoneV2Config
 
 @Named
 class KeystoneV2Filter @Inject()(configurationService: ConfigurationService,
                                   akkaServiceClient: AkkaServiceClient,
                                   datastoreService: DatastoreService)
   extends Filter
-  with UpdateListener[RackstoneV2Config]
+  with UpdateListener[KeystoneV2Config]
   with HttpDelegationManager
   with LazyLogging {
 
   private val DEFAULT_CONFIG = "keystone-v2.cfg.xml"
   var configurationFile: String = DEFAULT_CONFIG
-  var configuration: RackstoneV2Config = _
+  var configuration: KeystoneV2Config = _
   var initialized = false
 
   override def init(filterConfig: FilterConfig): Unit = {
     configurationFile = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
-    logger.info(s"Initializing Keystone V2 Filter using config ${configurationFile}")
-    val xsdURL: URL = getClass.getResource("/META-INF/schema/config/rackstone-v2.xsd")
+    logger.info(s"Initializing Keystone V2 Filter using config $configurationFile")
+    val xsdURL: URL = getClass.getResource("/META-INF/schema/config/keystone-v2.xsd")
     configurationService.subscribeTo(
       filterConfig.getFilterName,
       configurationFile,
       xsdURL,
       this,
-      classOf[RackstoneV2Config]
+      classOf[KeystoneV2Config]
     )
   }
 
@@ -46,7 +46,7 @@ class KeystoneV2Filter @Inject()(configurationService: ConfigurationService,
 
   override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain): Unit = ???
 
-  override def configurationUpdated(configurationObject: RackstoneV2Config): Unit = {
+  override def configurationUpdated(configurationObject: KeystoneV2Config): Unit = {
     configuration = configurationObject
     initialized = true
   }
