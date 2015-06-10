315d1bd11218de2533488801a3186d2c6f0ea9b2
jenny.vo <jenny.vo@rackspace.com>
2015-06-10 13:29:28 -0500

    clean-up update mock identityv2

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/mocks/MockIdentityService.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/mocks/MockIdentityService.groovy
@@ -144,6 +144,8 @@ class MockIdentityService {
     def contact_id = "${random.nextInt()}"
     def contactIdJson = ""
     def contactIdXml = ""
+    def additionalRolesXml = ""
+    def additionalRolesJson = ""
     Validator validator;
 
     def templateEngine = new SimpleTemplateEngine();
@@ -568,15 +570,19 @@ class MockIdentityService {
         def template;
         def headers = [:];
 
+
         if (xml) {
             headers.put('Content-type', 'application/xml')
-            template = this.getUserGlobalRolesXmlTemplate
+            template = UserGlobalRolesXmlTemplate
         } else {
             headers.put('Content-type', 'application/json')
-            template = this.getUserGlobalRolesJsonTemplate;
+            template = UserGlobalRolesJsonTemplate;
         }
 
-        def params = [:];
+        def params = [
+                addRolesXml: additionalRolesXml,
+                addRolesJson: additionalRolesJson
+        ];
 
         def body = templateEngine.createTemplate(template).make(params);
         return new Response(200, null, headers, body);
@@ -959,5 +965,60 @@ class MockIdentityService {
         </roles>
     </user>
 </access>
+"""
+    def UserGlobalRolesXmlTemplate =
+            """<?xml version="1.0" encoding="UTF-8"?>
+  <roles
+    xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:rax-auth="http://docs.rackspace.com/identity/api/ext/RAX-AUTH/v1.0"
+    xmlns="http://docs.openstack.org/identity/api/v2.0"
+    xmlns:ns4="http://docs.rackspace.com/identity/api/ext/RAX-KSGRP/v1.0"
+    xmlns:rax-ksqa="http://docs.rackspace.com/identity/api/ext/RAX-KSQA/v1.0"
+    xmlns:os-ksadm="http://docs.openstack.org/identity/api/ext/OS-KSADM/v1.0"
+    xmlns:rax-kskey="http://docs.rackspace.com/identity/api/ext/RAX-KSKEY/v1.0"
+    xmlns:os-ksec2="http://docs.openstack.org/identity/api/ext/OS-KSEC2/v1.0">
+    <role id="9" name="Racker"
+        description="Defines a user as being a Racker"
+        serviceId="18e7a7032733486cd32f472d7bd58f709ac0d221" rax-auth:propagate="true"/>
+    <role id="5" name="object-store:default"
+        description="Role to access keystone service"
+        serviceId="18e7a7032733486cd32f472d7bd58f709ac0d221"/>
+    <role id="6" name="compute:default"
+        description="Role to access keystone service"
+        serviceId="18e7a7032733486cd32f472d7bd58f709ac0d221""/>
+    <role id="3" name="identity:user-admin"
+        description="User Admin Role"
+        serviceId="18e7a7032733486cd32f472d7bd58f709ac0d221"/>
+    \${addRolesXml}
+</roles>
+"""
+    def UserGlobalRolesJsonTemplate =
+            """{
+    "roles": [
+        {
+            "description": "Defines a user as being Racker",
+            "id": "9",
+            "name": "Racker",
+            "serviceId": "18e7a7032733486cd32f472d7bd58f709ac0d221"
+        },
+        {
+            "description": "Role to access keystone service",
+            "id": "5",
+            "name": "object-store:default",
+            "serviceId": "18e7a7032733486cd32f472d7bd58f709ac0d221"
+        },
+        {
+            "description": "Role to access keystone service",
+            "id": "6",
+            "name": "compute:default",
+            "serviceId": "18e7a7032733486cd32f472d7bd58f709ac0d221"
+        },
+        {
+            "description": "Admin role for database service",
+            "id": "3",
+            "name": "User Admin Role",
+            "serviceId": "18e7a7032733486cd32f472d7bd58f709ac0d221"
+        },
+        \${addRolesJson}
 """
 }
