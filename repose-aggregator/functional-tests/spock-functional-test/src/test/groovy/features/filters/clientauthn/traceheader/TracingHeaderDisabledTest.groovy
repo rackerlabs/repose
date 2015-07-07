ce10119d64b60d3c23af6b26e4346a6e4ae23952
jenny.vo <jenny.vo@rackspace.com>
2015-07-07 16:06:38 -0500

    add config and test for tracing header disable

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthn/traceheader/TracingHeaderDisabledTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/clientauthn/traceheader/TracingHeaderDisabledTest.groovy
@@ -1,7 +1,67 @@
 package features.filters.clientauthn.traceheader
 
+import framework.ReposeValveTest
+import framework.mocks.MockIdentityService
+import org.joda.time.DateTime
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+
+import static junit.framework.Assert.assertTrue
+
 /**
  * Created by jennyvo on 7/7/15.
+ * Repose should not pass empty x-trans-id to identity when tracing header is disabled
  */
-class TracingHeaderDisabledTest {
+class TracingHeaderDisabledTest extends ReposeValveTest {
+
+    def static originEndpoint
+    def static identityEndpoint
+
+    def static MockIdentityService fakeIdentityService
+
+    def setupSpec() {
+
+        deproxy = new Deproxy()
+
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/clientauthn/tracingheaderdisable", params)
+
+        repose.start()
+
+        originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
+        fakeIdentityService = new MockIdentityService(properties.identityPort, properties.targetPort)
+        identityEndpoint = deproxy.addEndpoint(properties.identityPort,
+                'identity service', null, fakeIdentityService.handler)
+
+    }
+
+    def cleanupSpec() {
+        deproxy.shutdown()
+
+        repose.stop()
+    }
+
+    def "x-trans-id header should not include in request to Identity when tracing-header is disable"() {
+
+        fakeIdentityService.with {
+            client_token = UUID.randomUUID().toString()
+            tokenExpiresAt = DateTime.now().plusDays(1)
+            client_tenant = "123456"
+        }
+
+
+        when: "User passes a request through repose"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/servers/123456", method: 'GET',
+                headers: ['content-type': 'application/json', 'X-Auth-Token': fakeIdentityService.client_token])
+
+        then: "Things are forward to the origin, because we're not validating existence of tenant"
+        mc.receivedResponse.code == "200"
+        mc.handlings.size() == 1
+        !mc.handlings[0].request.headers.contains("x-trans-id")
+        mc.orphanedHandlings.each {
+            e -> assertTrue(!e.request.headers.contains("x-trans-id"))
+        }
+
+    }
 }
