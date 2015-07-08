7710a1821dcf33526d59fbbeffaafcca5d5e43d8
Megan Knocke <megan.knocke@rackspace.com>
2015-07-08 11:14:59 -0500

    removed bad unit tests and mockrunner dependency

--- repose-aggregator/components/filters/client-auth/src/test/groovy/org/openrepose/filters/clientauth/common/AuthenticationHandlerTest.groovy
+++ repose-aggregator/components/filters/client-auth/src/test/groovy/org/openrepose/filters/clientauth/common/AuthenticationHandlerTest.groovy
@@ -19,28 +19,13 @@
  */
 package org.openrepose.filters.clientauth.common
 
-import com.mockrunner.mock.web.MockHttpServletRequest
 import org.openrepose.common.auth.AuthGroup
 import org.openrepose.common.auth.AuthGroups
 import org.openrepose.common.auth.AuthToken
-import org.openrepose.common.auth.ResponseUnmarshaller
-import org.openrepose.common.auth.openstack.AuthenticationServiceClient
-import org.openrepose.commons.utils.http.CommonHttpHeader
-import org.openrepose.commons.utils.http.ServiceClientResponse
 import org.openrepose.commons.utils.regex.ExtractorResult
-import org.openrepose.commons.utils.regex.KeyedRegexExtractor
 import org.openrepose.commons.utils.servlet.http.ReadableHttpServletResponse
-import org.openrepose.commons.utils.transform.jaxb.JaxbEntityToXml
 import org.openrepose.core.filter.logic.FilterDirector
-import org.openrepose.core.services.serviceclient.akka.AkkaServiceClient
-import org.openrepose.filters.clientauth.openstack.OpenStackAuthenticationHandler
 import spock.lang.Specification
-
-import javax.ws.rs.core.MediaType
-
-import static org.mockito.Matchers.*
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when;
 
 class AuthenticationHandlerTest extends Specification {
 
@@ -73,64 +58,6 @@ class AuthenticationHandlerTest extends Specification {
 
         then:
         nil == null;
-    }
-
-    def "when x-trans-id header is not defined and account is null, it should not be added"() {
-        given:
-        configurables.getEndpointsConfiguration() >> endpointsConfiguration
-        endpointsConfiguration.getCacheTimeout() >> Long.valueOf(Integer.MAX_VALUE) + 1
-        def uriMatcher = Mock(UriMatcher)
-        def akkaServiceClient = mock(AkkaServiceClient)
-        def serviceClient = new AuthenticationServiceClient("", "", "", "",
-                mock(ResponseUnmarshaller),
-                mock(ResponseUnmarshaller),
-                mock(JaxbEntityToXml),
-                akkaServiceClient)
-        authenticationHandler = new OpenStackAuthenticationHandler(configurables, serviceClient, null, null, null, null, uriMatcher)
-        MockHttpServletRequest request = new MockHttpServletRequest()
-        ReadableHttpServletResponse response = Mock(ReadableHttpServletResponse)
-        request.addHeader(CommonHttpHeader.AUTH_TOKEN.toString(), "user")
-        def serviceResponse = Mock(ServiceClientResponse)
-        when(akkaServiceClient.post(anyString(), anyString(), anyMap(), anyString(), eq(MediaType.APPLICATION_XML_TYPE)))
-                .thenReturn(serviceResponse)
-
-        when:
-        authenticationHandler.handleRequest(request, response)
-
-        then:
-        request.getHeader(CommonHttpHeader.TRACE_GUID.toString()) == null
-    }
-
-    def "when x-trans-id header is not defined and account is not null, it should not be added"() {
-        given:
-        configurables.getEndpointsConfiguration() >> endpointsConfiguration
-        endpointsConfiguration.getCacheTimeout() >> Long.valueOf(Integer.MAX_VALUE) + 1
-        def tenantedConfigurables = mock(Configurables)
-        def uriMatcher = Mock(UriMatcher)
-        def akkaServiceClient = mock(AkkaServiceClient)
-        when(tenantedConfigurables.isTenanted()).thenReturn(true)
-        def keyedRegexExtractor = mock(KeyedRegexExtractor)
-        when(tenantedConfigurables.getKeyedRegexExtractor()).thenReturn(keyedRegexExtractor)
-        when(keyedRegexExtractor.extract(anyString())).thenReturn(Mock(ExtractorResult))
-        def serviceClient = new AuthenticationServiceClient("", "", "", "",
-                mock(ResponseUnmarshaller),
-                mock(ResponseUnmarshaller),
-                mock(JaxbEntityToXml),
-                akkaServiceClient)
-        authenticationHandler = new OpenStackAuthenticationHandler(tenantedConfigurables, serviceClient, null, null, null, null, uriMatcher)
-        MockHttpServletRequest request = new MockHttpServletRequest()
-        request.setRequestURI("/repose")
-        ReadableHttpServletResponse response = Mock(ReadableHttpServletResponse)
-        request.addHeader(CommonHttpHeader.AUTH_TOKEN.toString(), "user")
-        def serviceResponse = Mock(ServiceClientResponse)
-        when(akkaServiceClient.post(anyString(), anyString(), anyMap(), anyString(), eq(MediaType.APPLICATION_XML_TYPE)))
-                .thenReturn(serviceResponse)
-
-        when:
-        authenticationHandler.handleRequest(request, response)
-
-        then:
-        request.getHeader(CommonHttpHeader.TRACE_GUID.toString()) == null
     }
 
     class TestableAuthenticationHandler extends AuthenticationHandler {
