9115f7decdd66b75d8afc5bfb0b10ec27d7f7953
Megan Knocke <megan.knocke@rackspace.com>
2015-07-21 15:38:21 -0500

    corrected trace id and created unit test

---
+++ repose-aggregator/services/datastore/impl/src/test/groovy/org/openrepose/core/services/datastore/impl/distributed/remote/command/AbstractRemoteCommandTest.groovy
@@ -0,0 +1,52 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.core.services.datastore.impl.distributed.remote.command
+
+import org.junit.Before
+import org.junit.Test
+import org.openrepose.commons.utils.http.CommonHttpHeader
+import org.openrepose.core.logging.TracingKey
+import org.openrepose.core.services.datastore.distributed.RemoteBehavior
+import org.openrepose.core.services.datastore.impl.distributed.DatastoreHeader
+import org.slf4j.MDC
+
+import static org.mockito.Mockito.*;
+
+class AbstractRemoteCommandTest {
+
+    private AbstractRemoteCommand arc;
+
+    @Before
+    public void setUp() {
+        arc = new Get(anyString(), new InetSocketAddress(anyInt()))
+        arc.setHostKey("hostKey")
+    }
+
+    @Test
+    public void shouldContainAddedHeaders() {
+        MDC.put(TracingKey.TRACING_KEY, "tracingKey")
+        RemoteBehavior rb = RemoteBehavior.ALLOW_FORWARDING
+        Map<String, String> headers = arc.getHeaders(rb)
+
+        assert(headers.get(DatastoreHeader.HOST_KEY.toString()).equals("hostKey"));
+        assert(headers.get(CommonHttpHeader.TRACE_GUID.toString()).equals("tracingKey"));
+        assert(headers.get(DatastoreHeader.REMOTE_BEHAVIOR.toString()).equals("ALLOW_FORWARDING"));
+    }
+}
