8134cc95ba1f9b13683f444cead87caedb5c3e1e
Damien Johnson <damien.johnson@rackspace.com>
2015-08-11 16:49:48 -0500

    Defining phone home service interface

--- repose-aggregator/services/phone-home/api/src/main/java/org/openrepose/core/services/phonehome/impl/PhoneHomeService.java
+++ repose-aggregator/services/phone-home/api/src/main/java/org/openrepose/core/services/phonehome/impl/PhoneHomeService.java
@@ -19,6 +19,35 @@
  */
 package org.openrepose.core.services.phonehome.impl;
 
+/**
+ * A service which sends Repose usage data to a data collection point. This data may be used to determine common usage
+ * patterns.
+ * <p/>
+ * Data that may be sent includes:
+ * - Filter chain
+ * - Services
+ * - Contact information
+ * <p/>
+ * Note that the lifecycle of this service is self-managed, and the configuration for this service is read directly
+ * from the system model configuration file. Additionally, usage data will be sent whenever an update to the system
+ * model is observed, as long as the phone home service is active.
+ */
 public interface PhoneHomeService {
 
+    /**
+     * Reports whether or not the phone home service is active.
+     *
+     * @throws IllegalStateException if the phone home service has not yet been initialized
+     */
+    boolean isActive();
+
+    /**
+     * Sends a usage update to the data collection point, as long as the phone home service is active.
+     * <p/>
+     * Calling this method may cause configurations to be read.
+     *
+     * @throws IllegalStateException if the phone home service has not yet been initialized, or if the service is not
+     *                               currently active
+     */
+    void sendUpdate();
 }
