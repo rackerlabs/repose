3c5828ecbf54fae4ffaa403f39a88a3bdd965ca1
wdschei <Bill.Scheidegger@Rackspace.com>
2015-09-24 11:32:38 -0500

    Added the quality back to the X-PP-User header in the v3 filter.

--- repose-aggregator/components/filters/openstack-identity-v3/src/main/scala/org/openrepose/filters/openstackidentityv3/OpenStackIdentityV3Handler.scala
+++ repose-aggregator/components/filters/openstack-identity-v3/src/main/scala/org/openrepose/filters/openstackidentityv3/OpenStackIdentityV3Handler.scala
@@ -183,7 +183,7 @@ class OpenStackIdentityV3Handler(identityConfig: OpenstackIdentityV3Config, iden
         requestHeaderManager.putHeader(OpenStackIdentityV3Headers.X_AUTHORIZATION.toString, OpenStackIdentityV3Headers.X_AUTH_PROXY) // TODO: Add the project ID if verified
         token.get.user.name.foreach { user =>
           requestHeaderManager.appendHeader(OpenStackIdentityV3Headers.X_USER_NAME.toString, user)
-          requestHeaderManager.appendHeader(PowerApiHeader.USER.toString, user)
+          requestHeaderManager.appendHeader(PowerApiHeader.USER.toString, user, 1.0)
         }
         token.get.roles.foreach { roles =>
           requestHeaderManager.appendHeader(OpenStackIdentityV3Headers.X_ROLES, roles.map(_.name) mkString ",")
