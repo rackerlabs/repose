96851e64658662f33494dea6a123fb25c5c4c483
Mario Lopez <Mario.Lopez@rackspace.com>
2015-10-05 15:14:42 -0500

    Added additional CORS filter functional tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/cors/CorsFilterBasicTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/cors/CorsFilterBasicTest.groovy
@@ -23,6 +23,7 @@ import framework.ReposeValveTest
 import org.openrepose.commons.utils.http.CommonHttpHeader
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
+import org.rackspace.deproxy.Response
 import spock.lang.Unroll
 
 /**
@@ -48,7 +49,7 @@ class CorsFilterBasicTest extends ReposeValveTest {
             deproxy.shutdown()
     }
 
-    @Unroll("OPTIONS request without origin header Method: #method")
+    @Unroll("Non-CORS request with method OPTIONS and a CORS header request Method: #method")
     def "OPTIONS request without origin header will bypass CORS and make to origin service"() {
         def headers = [
                 (CommonHttpHeader.ACCESS_CONTROL_REQUEST_METHOD.toString()): method]
@@ -59,13 +60,35 @@ class CorsFilterBasicTest extends ReposeValveTest {
         then:
         mc.receivedResponse.code == '200'
         mc.getHandlings().size() == 1  // OPTIONS request without origin will make to it origin service
+        !mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString())
+        !mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString())
+        !mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString())
         mc.receivedResponse.headers.contains("Vary")
+        mc.receivedResponse.headers.findAll('Vary') == ['origin', 'access-control-request-headers', 'access-control-request-method']
 
         where:
         [method, path, origin] << [['GET', 'HEAD'], ['/', '/status'], [reposeEndpoint, 'http://test.repose.site:80']].combinations()
     }
 
-    @Unroll("Sending preflight request with origin #origin and request method #method ")
+    def "Actual request with method OPTIONS"() {
+        given:
+        def origin = reposeEndpoint
+        def headers = [(CommonHttpHeader.ORIGIN.toString()): origin]
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + '/testoptions', method: 'OPTIONS', headers: headers)
+
+        then:
+        mc.receivedResponse.code == '200'
+        mc.getHandlings().size() == 1   // actual request should make it through to the origin service
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
+        !mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString())
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
+        mc.receivedResponse.headers.contains("Vary")
+        mc.receivedResponse.headers.findAll('Vary') == ['origin', 'access-control-request-headers', 'access-control-request-method']
+    }
+
+    @Unroll("Preflight request with origin #origin and request method #method ")
     def "When sending preflight request with cors filter, the specific headers should be added for requested method #method, origin #origin, and path #path"() {
         given:
         def headers = [
@@ -80,6 +103,7 @@ class CorsFilterBasicTest extends ReposeValveTest {
         mc.getHandlings().size() == 0  // preflight request doesn't make it to the origin service
         mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
         mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
         mc.receivedResponse.headers.contains("Vary")
 
         where:
@@ -101,6 +125,7 @@ class CorsFilterBasicTest extends ReposeValveTest {
         mc.getHandlings().size() == 0
         mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
         mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
         mc.receivedResponse.headers.contains("Vary")
 
         where:
@@ -108,7 +133,7 @@ class CorsFilterBasicTest extends ReposeValveTest {
                                    [reposeEndpoint, "http://test.repose.site:80"]].combinations()
     }
 
-    @Unroll("Allowed Resource with Origin request header with method #method, path #path, and origin #origin")
+    @Unroll("Preflight request with allowed Resource with Origin request header with request method #method, path #path, and origin #origin")
     def "Origin request header is allow resource in the config"() {
         given:
         def headers = [
@@ -123,6 +148,7 @@ class CorsFilterBasicTest extends ReposeValveTest {
         mc.getHandlings().size() == 0
         mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
         mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
         mc.receivedResponse.headers.contains("Vary")
 
         where:
@@ -130,7 +156,35 @@ class CorsFilterBasicTest extends ReposeValveTest {
                                    [reposeEndpoint, "http://test.repose.site:80"]].combinations()
     }
 
-    @Unroll("Not allowed Origin request header with method #method, path #path, and origin #origin")
+    @Unroll
+    def "Actual request with allowed Resource with Origin request header with method #method, path #path, and origin #origin and headers #responseHeaders"() {
+        given:
+        def headers = [(CommonHttpHeader.ORIGIN.toString()): origin]
+        // have deproxy add the response headers
+        def handler = { request -> new Response(200, 'OK', (responseHeaders as List<String>).collectEntries{[(it): 'value']})}
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + path, method: method, headers: headers, defaultHandler: handler)
+
+        then:
+
+        mc.receivedResponse.code == '200'
+        mc.getHandlings().size() == 1
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
+        !mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString())
+        !mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_HEADERS.toString())
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
+        mc.receivedResponse.headers.contains('Vary')
+
+        where:
+        [method, path, origin, responseHeaders] << [
+                ["GET", "HEAD", "PUT", "POST", "PATCH", "DELETE"],
+                ["/testupdate"],
+                [reposeEndpoint, "http://test.repose.site:80"],
+                [['x-potatoes'], ['Content-Encoding'], ['x-success', 'Content-Encoding']]].combinations()
+    }
+
+    @Unroll("Preflight request with not allowed Origin request header with request method #method, path #path, and origin #origin")
     def "Origin request header is not allow in the config response 403"() {
         given:
         def headers = [
@@ -151,8 +205,8 @@ class CorsFilterBasicTest extends ReposeValveTest {
                                    ["http://test.com", "http://test.home.site:80"]].combinations()
     }
 
-    @Unroll("Not allowed resource with origin request header with method #method, path #path, and origin #origin")
-    def "Origin request method is not allow resource in the config"() {
+    @Unroll
+    def "Preflight request with not allowed request method #method for path #path and origin #origin"() {
         given:
         def headers = [
                 (CommonHttpHeader.ORIGIN.toString())                       : origin,
@@ -162,11 +216,13 @@ class CorsFilterBasicTest extends ReposeValveTest {
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + path, method: 'OPTIONS', headers: headers)
 
         then:
-        mc.receivedResponse.code == '200'
+        mc.receivedResponse.code == '403'
         mc.getHandlings().size() == 0
         mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
+        mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()).size() == 1
         !mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
         mc.receivedResponse.headers.contains("Vary")
+        mc.receivedResponse.headers.findAll('Vary') == ['origin', 'access-control-request-headers', 'access-control-request-method']
 
         where:
         method   | path      | origin
@@ -176,6 +232,64 @@ class CorsFilterBasicTest extends ReposeValveTest {
         "DELETE" | "/"       | "http://test.repose.site:80"
         "PUT"    | "/"       | reposeEndpoint
         "POST"   | "/"       | "http://test.repose.site:80"
+    }
 
+    @Unroll
+    def "Actual request with not allowed request method #method for path #path and origin #origin"() {
+        given:
+        def headers = [(CommonHttpHeader.ORIGIN.toString()): origin]
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + path, method: method, headers: headers)
+
+        then:
+        mc.receivedResponse.code == '403'
+        mc.getHandlings().size() == 0
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
+        mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()).size() == 1
+        !mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
+        mc.receivedResponse.headers.contains('Vary')
+        mc.receivedResponse.headers.findAll('Vary') == ['origin']
+
+        where:
+        method    | path      | origin
+        "PATCH"   | "/status" | reposeEndpoint
+        "DELETE"  | "/status" | "http://test.repose.site:80"
+        "PATCH"   | "/"       | reposeEndpoint
+        "DELETE"  | "/"       | "http://test.repose.site:80"
+        "PUT"     | "/"       | reposeEndpoint
+        "POST"    | "/"       | "http://test.repose.site:80"
+        "TRACE"   | "/"       | reposeEndpoint
+    }
+
+    @Unroll
+    def "Preflight request has preflight CORS headers for request method #method and headers #requestHeaders"() {
+        given:
+        def origin = reposeEndpoint
+        def path = '/testupdate'
+        def headers = [
+                (CommonHttpHeader.ORIGIN.toString()): origin,
+                (CommonHttpHeader.ACCESS_CONTROL_REQUEST_METHOD.toString()): method,
+                (CommonHttpHeader.ACCESS_CONTROL_REQUEST_HEADERS.toString()): requestHeaders
+        ]
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: origin + path, method: 'OPTIONS', headers: headers)
+
+        then:
+        mc.receivedResponse.code == '200'
+        mc.getHandlings().size() == 0
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()) == origin
+        mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_ORIGIN.toString()).size() == 1
+        mc.receivedResponse.headers.findAll(CommonHttpHeader.ACCESS_CONTROL_ALLOW_METHODS.toString()).contains(method)
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_HEADERS.toString()) == requestHeaders
+        !mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_EXPOSE_HEADERS.toString())
+        mc.receivedResponse.headers.getFirstValue(CommonHttpHeader.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString()) == 'true'
+        mc.receivedResponse.headers.contains('Vary')
+        mc.receivedResponse.headers.findAll('Vary') == ['origin', 'access-control-request-headers', 'access-control-request-method']
+
+        where:
+        [method, requestHeaders] << [["GET", "HEAD", "PUT", "POST", "PATCH", "DELETE"],
+                                     ['x-auth-token', 'x-auth-token, x-ponies', 'cookie']].combinations()
     }
 }
