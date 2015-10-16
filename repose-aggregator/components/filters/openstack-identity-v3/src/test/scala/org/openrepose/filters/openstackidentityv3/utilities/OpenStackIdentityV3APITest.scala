74357f68783f4de6d0625179c32ecd3c0bb81dc7
David Kowis <dkowis@shlrm.org>
2015-10-15 22:16:38 -0500

    Make it optional, fix a single compile problem.
    Lets see if this just fixes it.

--- repose-aggregator/components/filters/openstack-identity-v3/src/test/scala/org/openrepose/filters/openstackidentityv3/utilities/OpenStackIdentityV3APITest.scala
+++ repose-aggregator/components/filters/openstack-identity-v3/src/test/scala/org/openrepose/filters/openstackidentityv3/utilities/OpenStackIdentityV3APITest.scala
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -477,7 +477,7 @@ class OpenStackIdentityV3APITest extends FunSpec with BeforeAndAfter with Matche
     }
 
     it("should return a Success for cached groups") {
-      when(mockDatastore.get(anyString)).thenReturn(List(Group("", "", "")).toBuffer.asInstanceOf[Serializable], Nil: _*)
+      when(mockDatastore.get(anyString)).thenReturn(List(Group("", "", Some(""))).toBuffer.asInstanceOf[Serializable], Nil: _*)
 
       identityV3API invokePrivate getGroups("test-user-id", None, false) shouldBe a[Success[_]]
       identityV3API.invokePrivate(getGroups("test-user-id", None, false)).get shouldBe a[List[_]]
