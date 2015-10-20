9652f9e3bfeaaa8a2f9e943843f2dbe619d9e089
David Kowis <dkowis@shlrm.org>
2015-10-19 20:02:06 -0500

    Created a positive test case; renamed the invalid XML
    Wanted it to be more descriptive as to why it was invalid at a glance.
    
    Hooray fixed.

--- repose-aggregator/services/rate-limiting-service/src/test/groovy/org/openrepose/core/services/ratelimit/RateLimitConfigValidationTest.groovy
+++ repose-aggregator/services/rate-limiting-service/src/test/groovy/org/openrepose/core/services/ratelimit/RateLimitConfigValidationTest.groovy
@@ -22,7 +22,7 @@ class RateLimitConfigValidationTest extends Specification {
 
     def "duplicate limit IDs across limit groups should NOT validate" () {
         given: "a rate limiting configuration with duplicate limit IDs in different groups"
-        def invalidRateLimit = "/invalid-rate-limiting-config.xml"
+        def invalidRateLimit = "/duplicate-limit-ids.xml"
 
         when: "validation is attempted"
         def loadedConfig = configResource(invalidRateLimit)
@@ -31,5 +31,18 @@ class RateLimitConfigValidationTest extends Specification {
         //It throws a ClassCastException, and logs that it wasn't able to validate.
         thrown(ClassCastException)
         loadedConfig == null
+    }
+
+    def "a configuration without duplicate limit IDs should validate"() {
+        given: "a rate limiting configuration without duplicate limit IDs in different groups"
+        def validRateLimit = "/valid-rate-limiting-config.xml"
+
+        when: "validation is attempted"
+        def loadedConfig = configResource(validRateLimit)
+
+        then: "that validation succeeds"
+        notThrown(Exception) //Explicitly saying nothing should go wrong here
+        loadedConfig != null
+        loadedConfig.class == RateLimitingConfiguration.class
     }
 }
