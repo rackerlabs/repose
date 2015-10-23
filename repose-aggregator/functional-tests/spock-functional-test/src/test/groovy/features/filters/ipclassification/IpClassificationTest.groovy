9fd917e2ac458faeeb3d7d58d71a41ab659e4c35
David Kowis <dkowis@shlrm.org>
2015-10-22 23:56:25 -0500

    Adding a functional test!
    Woo, it works!
    
    SHIP IT

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/ipclassification/IpClassificationTest.groovy
@@ -0,0 +1,74 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.filters.ipclassification
+
+import framework.ReposeValveTest
+import framework.category.Slow
+import org.junit.experimental.categories.Category
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.Handling
+import org.rackspace.deproxy.MessageChain
+import org.rackspace.deproxy.Response
+import spock.lang.Shared
+import spock.lang.Unroll
+
+class IpClassificationTest extends ReposeValveTest {
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/ipclassification", params)
+        repose.start()
+
+        //Just set up a simple endpoint
+        deproxy.addEndpoint(properties.targetPort, 'origin service')
+    }
+
+    def cleanupSpec() {
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
+    }
+
+    def "classifying a request by its IP"() {
+        when: "Request is sent through repose"
+        def mc = deproxy.makeRequest(url: reposeEndpoint, method: 'get')
+        def sentRequest = ((MessageChain) mc).handlings[0]
+
+        then: "Repose will send x-pp-group with the configured value"
+        mc.handlings.size() == 1
+
+        //TODO: do we want this?
+        //and: "Repose will send x-pp-user based on requestor ip"
+        ((Handling) sentRequest).request.getHeaders().findAll("x-pp-user").size() == 1
+        def user = sentRequest.request.headers.getFirstValue("x-pp-user")
+        //The possible IPv4 addresses
+        user == "127.0.0.1;q=0.4" || user == "0:0:0:0:0:0:0:1;q=0.4" || user == "::1;q=0.4"
+
+        and: "Repose will send x-pp-groups with the configured value"
+        ((Handling) sentRequest).request.headers.getFirstValue("x-pp-groups").equalsIgnoreCase("local-group;q=0.4")
+
+    }
+}
