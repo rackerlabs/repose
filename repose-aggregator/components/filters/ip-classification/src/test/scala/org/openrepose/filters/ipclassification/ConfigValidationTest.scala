0206f8820e480e033ef0d978d22a0786b697c399
David Kowis <dkowis@shlrm.org>
2015-10-22 21:15:54 -0500

    Wrote the XSD and built a couple tests to prove things work

---
+++ repose-aggregator/components/filters/ip-classification/src/test/scala/org/openrepose/filters/ipclassification/ConfigValidationTest.scala
@@ -0,0 +1,65 @@
+package org.openrepose.filters.ipclassification
+
+import org.openrepose.filters.ipclassification.config.IpClassificationConfig
+import org.scalatest.{FunSpec, Matchers}
+
+class ConfigValidationTest extends FunSpec with Matchers {
+
+  //Only testing that valid config is valid
+  // That the default works
+  // And that the XSD 1.1 assertions work
+
+  describe("verification of XSD validations") {
+    it("returns a config object when given a valid configuration") {
+      val validConfig =
+        """<?xml version="1.0" encoding="UTF-8"?>
+          |<ip-classification xmlns="http://docs.openrepose.org/repose/ip-classification/v1.0">
+          |    <header-name>x-pp-group</header-name>
+          |
+          |    <classifications>
+          |        <classification label="sample-group" ipv4-cidr="192.168.1.0/24 192.168.0.1/32"/>
+          |        <classification label="sample-ipv6-group" ipv6-cidr="2001:db8::/48"/>
+          |        <classification label="bolth-group" ipv4-cidr="10.10.220.0/24" ipv6-cidr="2001:1938:80:bc::1/64"/>
+          |        <classification label="ipv4-match-all" ipv4-cidr="0.0.0.0/0"/>
+          |    </classifications>
+          |</ip-classification>
+        """.stripMargin
+      val resultant = Marshaller.configFromString(validConfig)
+      resultant shouldNot be(null)
+      resultant.getClass should equal(classOf[IpClassificationConfig])
+    }
+    it("returns a default header name when one is not specified") {
+      val validConfig =
+        """<?xml version="1.0" encoding="UTF-8"?>
+          |<ip-classification xmlns="http://docs.openrepose.org/repose/ip-classification/v1.0">
+          |
+          |    <classifications>
+          |        <classification label="sample-group" ipv4-cidr="192.168.1.0/24 192.168.0.1/32"/>
+          |        <classification label="sample-ipv6-group" ipv6-cidr="2001:db8::/48"/>
+          |        <classification label="bolth-group" ipv4-cidr="10.10.220.0/24" ipv6-cidr="2001:1938:80:bc::1/64"/>
+          |        <classification label="ipv4-match-all" ipv4-cidr="0.0.0.0/0"/>
+          |    </classifications>
+          |</ip-classification>
+        """.stripMargin
+      val resultant = Marshaller.configFromString(validConfig)
+      resultant shouldNot be(null)
+
+      resultant.getHeaderName should be("x-pp-group")
+    }
+    it("validation fails when not given an ipv4-cidr nor an ipv6-cidr") {
+      val invalidConfig =
+        """<?xml version="1.0" encoding="UTF-8"?>
+          |<ip-classification xmlns="http://docs.openrepose.org/repose/ip-classification/v1.0">
+          |
+          |    <classifications>
+          |        <classification label="sample-group"/>
+          |    </classifications>
+          |</ip-classification>
+        """.stripMargin
+
+      intercept[ClassCastException] {
+        Marshaller.configFromString(invalidConfig)
+      }
+    }
+  }
+}
---
+++ repose-aggregator/components/filters/ip-classification/src/test/scala/org/openrepose/filters/ipclassification/Marshaller.scala
@@ -0,0 +1,56 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.filters.ipclassification
+
+import java.net.URL
+import java.nio.charset.StandardCharsets
+
+import org.openrepose.commons.config.parser.jaxb.JaxbConfigurationParser
+import org.openrepose.commons.config.resource.impl.{BufferedURLConfigurationResource, ByteArrayConfigurationResource}
+import org.openrepose.filters.ipclassification.config.IpClassificationConfig
+
+object Marshaller {
+
+  val ipClassificationConfig = getClass.getResource("/META-INF/schema/config/ip-classification.xsd")
+
+  def configFromResource(resource: String, xsdURL: URL = ipClassificationConfig): IpClassificationConfig = {
+
+    val parser = JaxbConfigurationParser.getXmlConfigurationParser(
+      classOf[IpClassificationConfig],
+      xsdURL,
+      this.getClass.getClassLoader)
+
+    val configResource = new BufferedURLConfigurationResource(this.getClass.getResource(resource))
+
+    parser.read(configResource)
+  }
+
+  def configFromString(content: String, xsdURL: URL = ipClassificationConfig): IpClassificationConfig = {
+    val parser = JaxbConfigurationParser.getXmlConfigurationParser(
+      classOf[IpClassificationConfig],
+      xsdURL,
+      this.getClass.getClassLoader)
+
+    val configResource = new ByteArrayConfigurationResource("", content.getBytes(StandardCharsets.UTF_8))
+
+    parser.read(configResource)
+  }
+
+}
