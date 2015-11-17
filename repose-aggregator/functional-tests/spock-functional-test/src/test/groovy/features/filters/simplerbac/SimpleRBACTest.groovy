d80bdaf77e5433b9134ec3b7e3eee03cd69bd43b
wdschei <Bill.Scheidegger@Rackspace.com>
2015-11-17 15:21:50 -0600

    Updated the Simple-RBAC filter functional tests for roles with spaces.

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/simplerbac/SimpleRBACTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/simplerbac/SimpleRBACTest.groovy
@@ -61,48 +61,55 @@ class SimpleRBACTest extends ReposeValveTest {
         mc.receivedResponse.code == respcode.toString()
 
         where:
-        path                 | method   | roles       | respcode
-        "/path/to/this"      | "GET"    | "super"     | SC_OK
-        "/path/to/this"      | "PUT"    | "super"     | SC_OK
-        "/path/to/this"      | "POST"   | "super"     | SC_OK
-        "/path/to/this"      | "DELETE" | "super"     | SC_OK
-        "/path/to/this"      | "GET"    | "useradmin" | SC_OK
-        "/path/to/this"      | "PUT"    | "useradmin" | SC_OK
-        "/path/to/this"      | "POST"   | "useradmin" | SC_OK
-        "/path/to/this"      | "DELETE" | "useradmin" | SC_FORBIDDEN
-        "/path/to/this"      | "GET"    | "admin"     | SC_OK
-        "/path/to/this"      | "PUT"    | "admin"     | SC_OK
-        "/path/to/this"      | "POST"   | "admin"     | SC_FORBIDDEN
-        "/path/to/this"      | "DELETE" | "admin"     | SC_FORBIDDEN
-        "/path/to/this"      | "GET"    | "user"      | SC_OK
-        "/path/to/this"      | "PUT"    | "user"      | SC_FORBIDDEN
-        "/path/to/this"      | "POST"   | "user"      | SC_FORBIDDEN
-        "/path/to/this"      | "DELETE" | "user"      | SC_FORBIDDEN
-        "/path/to/this"      | "GET"    | "none"      | SC_FORBIDDEN
-        "/path/to/this"      | "PUT"    | "none"      | SC_FORBIDDEN
-        "/path/to/this"      | "POST"   | "none"      | SC_FORBIDDEN
-        "/path/to/this"      | "DELETE" | "none"      | SC_FORBIDDEN
-        "/path/to/that"      | "GET"    | "super"     | SC_OK
-        "/path/to/that"      | "PUT"    | "super"     | SC_OK
-        "/path/to/that"      | "POST"   | "super"     | SC_OK
-        "/path/to/that"      | "DELETE" | "super"     | SC_OK
-        "/path/to/that"      | "GET"    | "useradmin" | SC_OK
-        "/path/to/that"      | "PUT"    | "useradmin" | SC_OK
-        "/path/to/that"      | "POST"   | "user"      | SC_FORBIDDEN
-        "/path/to/that"      | "DELETE" | "admin"     | SC_FORBIDDEN
-        "/path/to/that"      | "POST"   | "super"     | SC_OK
-        "/path/to/that"      | "DELETE" | "super"     | SC_OK
-        "/path/to/test"      | "GET"    | "user"      | SC_OK
-        "/path/to/test"      | "POST"   | "useradmin" | SC_OK
-        "/path/to/test"      | "GET"    | "admin"     | SC_FORBIDDEN
-        "/path/to/test"      | "POST"   | "super"     | SC_FORBIDDEN
-        "/path/to/test"      | "PUT"    | "user"      | SC_METHOD_NOT_ALLOWED
-        "/path/to/test"      | "DELETE" | "useradmin" | SC_METHOD_NOT_ALLOWED
-        "/path/to/something" | "GET"    | "user"      | SC_NOT_FOUND
-        "/path/to/something" | "GET"    | "super"     | SC_NOT_FOUND
-        "/path/to/something" | "GET"    | "admin"     | SC_NOT_FOUND
-        "/path/to/something" | "POST"   | "none"      | SC_NOT_FOUND
-        "/path/to/something" | "PUT"    | "useradmin" | SC_NOT_FOUND
+        path                 | method   | roles           | respcode
+        "/path/to/this"      | "GET"    | "super"         | SC_OK
+        "/path/to/this"      | "PUT"    | "super"         | SC_OK
+        "/path/to/this"      | "POST"   | "super"         | SC_OK
+        "/path/to/this"      | "DELETE" | "super"         | SC_OK
+        "/path/to/this"      | "GET"    | "useradmin"     | SC_OK
+        "/path/to/this"      | "PUT"    | "useradmin"     | SC_OK
+        "/path/to/this"      | "POST"   | "useradmin"     | SC_OK
+        "/path/to/this"      | "DELETE" | "useradmin"     | SC_FORBIDDEN
+        "/path/to/this"      | "GET"    | "admin"         | SC_OK
+        "/path/to/this"      | "PUT"    | "admin"         | SC_OK
+        "/path/to/this"      | "POST"   | "admin"         | SC_FORBIDDEN
+        "/path/to/this"      | "DELETE" | "admin"         | SC_FORBIDDEN
+        "/path/to/this"      | "GET"    | "user"          | SC_OK
+        "/path/to/this"      | "PUT"    | "user"          | SC_FORBIDDEN
+        "/path/to/this"      | "POST"   | "user"          | SC_FORBIDDEN
+        "/path/to/this"      | "DELETE" | "user"          | SC_FORBIDDEN
+        "/path/to/this"      | "GET"    | "none"          | SC_FORBIDDEN
+        "/path/to/this"      | "PUT"    | "none"          | SC_FORBIDDEN
+        "/path/to/this"      | "POST"   | "none"          | SC_FORBIDDEN
+        "/path/to/this"      | "DELETE" | "none"          | SC_FORBIDDEN
+        "/path/to/that"      | "GET"    | "super"         | SC_OK
+        "/path/to/that"      | "PUT"    | "super"         | SC_OK
+        "/path/to/that"      | "POST"   | "super"         | SC_OK
+        "/path/to/that"      | "DELETE" | "super"         | SC_OK
+        "/path/to/that"      | "GET"    | "useradmin"     | SC_OK
+        "/path/to/that"      | "PUT"    | "useradmin"     | SC_OK
+        "/path/to/that"      | "POST"   | "user"          | SC_FORBIDDEN
+        "/path/to/that"      | "DELETE" | "admin"         | SC_FORBIDDEN
+        "/path/to/that"      | "POST"   | "super"         | SC_OK
+        "/path/to/that"      | "DELETE" | "super"         | SC_OK
+        "/path/to/test"      | "GET"    | "user"          | SC_OK
+        "/path/to/test"      | "POST"   | "useradmin"     | SC_OK
+        "/path/to/test"      | "GET"    | "admin"         | SC_FORBIDDEN
+        "/path/to/test"      | "POST"   | "super"         | SC_FORBIDDEN
+        "/path/to/test"      | "PUT"    | "user"          | SC_METHOD_NOT_ALLOWED
+        "/path/to/test"      | "DELETE" | "useradmin"     | SC_METHOD_NOT_ALLOWED
+        "/path/to/something" | "GET"    | "user"          | SC_NOT_FOUND
+        "/path/to/something" | "GET"    | "super"         | SC_NOT_FOUND
+        "/path/to/something" | "GET"    | "admin"         | SC_NOT_FOUND
+        "/path/to/something" | "POST"   | "none"          | SC_NOT_FOUND
+        "/path/to/something" | "PUT"    | "useradmin"     | SC_NOT_FOUND
+        "/path/to/space"     | "GET"    | "super wsp"     | SC_OK
+        "/path/to/space"     | "GET"    | "useradmin wsp" | SC_OK
+        "/path/to/space"     | "GET"    | "admin wsp"     | SC_OK
+        "/path/to/space"     | "GET"    | "super"         | SC_FORBIDDEN
+        "/path/to/space"     | "GET"    | "useradmin"     | SC_FORBIDDEN
+        "/path/to/space"     | "GET"    | "admin"         | SC_FORBIDDEN
+        "/path/to/space"     | "GET"    | "wsp"           | SC_FORBIDDEN
     }
 
     @Unroll("Test with #path, #method")
