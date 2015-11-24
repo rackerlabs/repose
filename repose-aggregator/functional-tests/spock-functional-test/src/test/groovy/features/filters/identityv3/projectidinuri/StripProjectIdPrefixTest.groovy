a4bc03165b5994d034d3df292ebff0df72d9fac6
jenny.vo <jenny.vo@rackspace.com>
2015-11-24 16:28:47 -0600

    add strip tenant prefix for keystonev3

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/identityv3/projectidinuri/StripProjectIdPrefixTest.groovy
@@ -0,0 +1,100 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.filters.identityv3.projectidinuri
+
+import framework.ReposeValveTest
+import framework.mocks.MockIdentityV3Service
+import org.joda.time.DateTime
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+import spock.lang.Unroll
+
+/**
+ * Created by jennyvo on 11/24/15.
+ */
+class StripProjectIdPrefixTest extends ReposeValveTest {
+    def static originEndpoint
+    def static identityEndpoint
+    def static MockIdentityV3Service fakeIdentityV3Service
+
+    def setupSpec() {
+        deproxy = new Deproxy()
+        def params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/identityv3/common", params)
+        repose.configurationProvider.applyConfigs("features/filters/identityv3/projectidinuri/stripprojectprefix", params)
+        repose.start()
+        waitUntilReadyToServiceRequests('401')
+
+        originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
+        fakeIdentityV3Service = new MockIdentityV3Service(properties.identityPort, properties.targetPort)
+        identityEndpoint = deproxy.addEndpoint(properties.identityPort,
+                'identity service', null, fakeIdentityV3Service.handler)
+    }
+
+    def cleanupSpec() {
+        if (deproxy)
+            deproxy.shutdown()
+        if (repose)
+            repose.stop()
+    }
+
+    def setup() {
+        fakeIdentityV3Service.resetHandlers()
+    }
+
+    @Unroll("request project: #requestProject and response project: #responseProject - #responseCode")
+    def "Strip ProjectId Prefix enabled"() {
+        given:
+        fakeIdentityV3Service.with {
+            client_token = UUID.randomUUID().toString()
+            tokenExpiresAt = DateTime.now().plusDays(1)
+            client_projectid = responseProject
+            service_admin_role = "not-admin"
+            client_userid = requestProject
+        }
+
+        when:
+        "User passes a request through repose with request project: $requestProject, response project: $responseProject in a role that is not bypassed"
+        MessageChain mc = deproxy.makeRequest(
+                url: "$reposeEndpoint/servers/$requestProject/",
+                method: 'GET',
+                headers: [
+                        'content-type'   : 'application/json',
+                        'X-Subject-Token': fakeIdentityV3Service.client_token
+                ]
+        )
+
+        then: "Request body sent from repose to the origin service should contain"
+        mc.receivedResponse.code == responseCode
+
+        where:
+        requestProject | responseProject | responseCode
+        "1234"         | "hybrid:1234"   | "200"
+        "1234"         | "foo:1234"      | "200"
+        "1234"         | "bar:1234"      | "200"
+        "1235"         | "1235"          | "200"
+        "repose-test"  | "repose-test"   | "200"
+        "1234"         | "hybrid:1235"   | "401"
+        "hybrid:1235"  | "hybrid:1235"   | "401"
+        "hybrid:1235"  | "1235"          | "401"
+        "bar:1234"     | "bar:1234"      | "401"
+    }
+}
