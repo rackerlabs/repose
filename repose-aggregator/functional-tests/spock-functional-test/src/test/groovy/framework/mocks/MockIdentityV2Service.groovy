f447936980ad70a11b14d6d66b3f81e42f1bd497
jenny.vo <jenny.vo@rackspace.com>
2016-01-15 14:51:42 -0600

    update mockidentityV2 to work with basicauth

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/mocks/MockIdentityV2Service.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/framework/mocks/MockIdentityV2Service.groovy
@@ -163,6 +163,9 @@ class MockIdentityV2Service {
     def client_username = 'username';
     def client_userid = 12345; //TODO: this should not be an int, userIDs are UUIDs
     def client_apikey = 'this-is-the-api-key';
+    def client_password = 'this-is-the-pwd'
+    def forbidden_apikey_or_pwd = 'this-key-pwd-results-in-forbidden'
+    def not_found_apikey_or_pwd = 'this-key-pwd-results-in-not-found'
     def admin_token = 'this-is-the-admin-token';
     def admin_tenant = 'this-is-the-admin-tenant'
     def admin_username = 'admin_username';
@@ -195,6 +198,9 @@ class MockIdentityV2Service {
         client_username = 'username';
         client_userid = 12345; //TODO: this should not be an int, userIDs are UUIDs
         client_apikey = 'this-is-the-api-key';
+        client_password = 'this-is-the-pwd';
+        forbidden_apikey_or_pwd = 'this-key-pwd-results-in-forbidden'
+        not_found_apikey_or_pwd = 'this-key-pwd-results-in-not-found'
         admin_token = 'this-is-the-admin-token';
         admin_tenant = 'this-is-the-admin-tenant'
         admin_username = 'admin_username';
@@ -469,8 +475,10 @@ class MockIdentityV2Service {
             // THEN return the Admin token response.
             if (request.body.contains("username") &&
                     request.body.contains(client_username) &&
-                    request.body.contains("apiKey") &&
-                    request.body.contains(client_apikey)) {
+                    ((request.body.contains("apiKey") &&
+                            request.body.contains(client_apikey)) ||
+                            (request.body.contains("password") &&
+                                    request.body.contains(client_password)))) {
                 params = [
                         expires      : getExpires(),
                         userid       : client_userid,
@@ -548,7 +556,23 @@ class MockIdentityV2Service {
                 template = identitySuccessJsonTemplate
             }
         } else {
-            code = 401
+            //If the username or the apikey are longer than 120 characters, barf back a 400, bad request response
+            //I have to parse the XML body of the request to mimic behavior in identity
+            def auth = new XmlSlurper().parseText(request.body.toString())
+            String username = auth.apiKeyCredentials['@username']
+            String apikey = auth.apiKeyCredentials['@apiKey']
+            String password = auth.passwordCredentials['@password']
+
+            //Magic numbers are how large of a value identity will parse before giving back a 400 Bad Request
+            if (apikey.length() > 100 || password.length() > 100 || username.length() > 100) {
+                code = 400
+            } else if (request.body.toString().contains(forbidden_apikey_or_pwd)) {
+                code = 403
+            } else if (request.body.toString().contains(not_found_apikey_or_pwd)) {
+                code = 404
+            } else {
+                code = 401
+            }
             if (xml) {
                 template = identityFailureXmlTemplate
             } else {
