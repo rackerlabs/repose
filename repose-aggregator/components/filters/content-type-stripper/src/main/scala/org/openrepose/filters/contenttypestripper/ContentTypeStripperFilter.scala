31bd15f2125601ed58e872f70e1823502c4d50ea
wdschei <Bill.Scheidegger@Rackspace.com>
2016-01-18 16:11:04 -0600

    Updated the filter to replace all the old MutableHttpServletRequest with the new HttpServletRequestWrapper w/o any actual functional change.

--- repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/ContentTypeStripperFilter.scala
+++ repose-aggregator/components/filters/content-type-stripper/src/main/scala/org/openrepose/filters/contenttypestripper/ContentTypeStripperFilter.scala
@@ -20,15 +20,13 @@
 package org.openrepose.filters.contenttypestripper
 
 import java.io.PushbackInputStream
-import javax.inject.Named
 import javax.servlet._
 import javax.servlet.http.HttpServletRequest
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.http.HttpHeaders
 import org.openrepose.commons.utils.io.stream.ServletInputStreamWrapper
-import org.openrepose.commons.utils.servlet.http.MutableHttpServletRequest
-
+import org.openrepose.commons.utils.servlet.http.HttpServletRequestWrapper
 import scala.collection.JavaConversions.enumerationAsScalaIterator
 
 class ContentTypeStripperFilter extends Filter {
@@ -36,18 +34,20 @@ class ContentTypeStripperFilter extends Filter {
   override def init(p1: FilterConfig): Unit = {}
 
   override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = {
-    val pushBackRequest = MutableHttpServletRequest.wrap(request.asInstanceOf[HttpServletRequest])
-    if (pushBackRequest.getHeaderNames exists (_.equalsIgnoreCase(HttpHeaders.CONTENT_TYPE))) {
-      val byteArray: Array[Byte] = new Array[Byte](8)
-      val pushbackStream = new PushbackInputStream(pushBackRequest.getInputStream, 8)
-      val bytesRead: Int = pushbackStream.read(byteArray, 0, 8)
+    val httpServletRequest = request.asInstanceOf[HttpServletRequest]
+    if (httpServletRequest.getHeaderNames exists (_.equalsIgnoreCase(HttpHeaders.CONTENT_TYPE))) {
+      val byteArray = new Array[Byte](8)
+      val pushBackStream = new PushbackInputStream(httpServletRequest.getInputStream, 8)
+      val pushBackRequest = new HttpServletRequestWrapper(httpServletRequest, new ServletInputStreamWrapper(pushBackStream))
+      val bytesRead = pushBackStream.read(byteArray, 0, 8)
       if (bytesRead == -1 || StringUtils.isBlank(new String(byteArray.slice(0, bytesRead)))) {
         pushBackRequest.removeHeader(HttpHeaders.CONTENT_TYPE)
       }
-      pushbackStream.unread(byteArray.slice(0, bytesRead))
-      pushBackRequest.setInputStream(new ServletInputStreamWrapper(pushbackStream))
+      pushBackStream.unread(byteArray.slice(0, bytesRead))
+      chain.doFilter(pushBackRequest, response)
+    } else {
+      chain.doFilter(request, response)
     }
-    chain.doFilter(pushBackRequest, response)
   }
 
   override def destroy(): Unit = {}
