22a8a4486c7a244c9435f8c72683e34ed750159d
Damien Johnson <damien.johnson@rackspace.com>
2016-01-27 15:05:57 -0600

    Collapsing old pattern and using new wrappers

--- repose-aggregator/components/filters/merge-header/src/main/scala/org/openrepose/filters/mergeheader/MergeHeaderFilter.scala
+++ repose-aggregator/components/filters/merge-header/src/main/scala/org/openrepose/filters/mergeheader/MergeHeaderFilter.scala
@@ -1,39 +1,101 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
 package org.openrepose.filters.mergeheader
 
 import javax.inject.{Inject, Named}
 import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.config.manager.UpdateListener
+import org.openrepose.commons.utils.servlet.http.HttpServletRequestWrapper
 import org.openrepose.core.filter.FilterConfigHelper
-import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate
 import org.openrepose.core.services.config.ConfigurationService
 
+import scala.collection.JavaConversions._
+
 @Named
-class MergeHeaderFilter @Inject()(configurationService: ConfigurationService) extends Filter with LazyLogging {
+class MergeHeaderFilter @Inject()(configurationService: ConfigurationService)
+  extends Filter with UpdateListener[MergeHeaderConfig] with LazyLogging {
+
   private final val DEFAULT_CONFIG = "merge-header.cfg.xml"
 
-  private var config: String = _
-  private var handlerFactory: MergeHeaderHandlerFactory = _
+  private var initialized = false
+  private var configFilename: String = _
+  private var filterConfig: MergeHeaderConfig = _
 
   override def init(filterConfig: FilterConfig): Unit = {
-    config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
-    logger.info(s"Initializing MergeHeaderFilter using config $config")
-    handlerFactory = new MergeHeaderHandlerFactory()
+    configFilename = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
+
+    logger.info(s"Initializing MergeHeaderFilter using config $configFilename")
     val xsdURL = getClass.getResource("/META-INF/config/schema/merge-header.xsd")
-    // TODO: Clean up the asInstanceOf below, if possible?
-    configurationService.subscribeTo(filterConfig.getFilterName,
-      config,
+    configurationService.subscribeTo(
+      filterConfig.getFilterName,
+      configFilename,
       xsdURL,
-      handlerFactory.asInstanceOf[UpdateListener[MergeHeaderConfig]],
-      classOf[MergeHeaderConfig])
+      this,
+      classOf[MergeHeaderConfig]
+    )
   }
 
   override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain): Unit = {
-    new FilterLogicHandlerDelegate(servletRequest, servletResponse, filterChain).doFilter(handlerFactory.newHandler)
+    if (!isInitialized) {
+      logger.error("Filter has not yet initialized...")
+      servletResponse.asInstanceOf[HttpServletResponse].sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE)
+    } else {
+      lazy val wrappedRequest = new HttpServletRequestWrapper(servletRequest.asInstanceOf[HttpServletRequest])
+
+      Option(filterConfig.getRequest) foreach { requestConfig =>
+        requestConfig.getHeader foreach { name =>
+          val value = wrappedRequest.getHeaders(name).mkString(", ")
+          if (value.nonEmpty) {
+            logger.debug(s"REQUEST: merging header $name to $value")
+            wrappedRequest.replaceHeader(name, value)
+          }
+        }
+      }
+
+      filterChain.doFilter(wrappedRequest, servletResponse)
+
+      val httpServletResponse = servletResponse.asInstanceOf[HttpServletResponse]
+
+      Option(filterConfig.getResponse).foreach { responseConfig =>
+        responseConfig.getHeader foreach { name =>
+          val value = httpServletResponse.getHeaders(name).mkString(", ")
+          if (value.nonEmpty) {
+            logger.debug(s"RESPONSE: merging header $name to $value")
+            httpServletResponse.setHeader(name, value)
+          }
+        }
+      }
+    }
   }
 
+  override def isInitialized: Boolean = initialized
+
   override def destroy(): Unit = {
-    configurationService.unsubscribeFrom(config, handlerFactory)
+    configurationService.unsubscribeFrom(configFilename, this)
+  }
+
+  override def configurationUpdated(configurationObject: MergeHeaderConfig): Unit = {
+    filterConfig = configurationObject
+    initialized = true
   }
 }
