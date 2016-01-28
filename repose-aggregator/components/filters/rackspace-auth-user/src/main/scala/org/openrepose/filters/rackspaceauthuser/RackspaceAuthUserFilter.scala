4c99fffaa8e72b28704514ee3850fe9e1fe4a56c
Mario Lopez <Mario.Lopez@rackspace.com>
2016-01-28 11:09:22 -0600

    Rackspace Auth User filter - refactored and updated wrappers

--- repose-aggregator/components/filters/rackspace-auth-user/src/main/scala/org/openrepose/filters/rackspaceauthuser/RackspaceAuthUserFilter.scala
+++ repose-aggregator/components/filters/rackspace-auth-user/src/main/scala/org/openrepose/filters/rackspaceauthuser/RackspaceAuthUserFilter.scala
@@ -19,40 +19,74 @@
  */
 package org.openrepose.filters.rackspaceauthuser
 
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
 import javax.inject.{Inject, Named}
 import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.config.manager.UpdateListener
+import org.openrepose.commons.utils.servlet.http.HttpServletRequestWrapper
 import org.openrepose.core.filter.FilterConfigHelper
-import org.openrepose.core.filter.logic.impl.FilterLogicHandlerDelegate
 import org.openrepose.core.services.config.ConfigurationService
 
 @Named
-class RackspaceAuthUserFilter @Inject()(configurationService: ConfigurationService) extends Filter with LazyLogging {
-  private final val DEFAULT_CONFIG = "rackspace-auth-user.cfg.xml"
+class RackspaceAuthUserFilter @Inject()(configurationService: ConfigurationService)
+  extends Filter with UpdateListener[RackspaceAuthUserConfig] with LazyLogging {
 
-  private var config: String = _
-  private var handlerFactory: RackspaceAuthUserHandlerFactory = _
+  import RackspaceAuthUserFilter._
+
+  private var configFileName: String = _
+  private val initialized = new AtomicBoolean(false)
+  private val handler: AtomicReference[RackspaceAuthUserHandler] = new AtomicReference[RackspaceAuthUserHandler]()
 
   override def init(filterConfig: FilterConfig): Unit = {
-    config = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
-    logger.info(s"Initializing RackspaceAuthUserFilter using config $config")
-    handlerFactory = new RackspaceAuthUserHandlerFactory()
-    val xsdURL = getClass.getResource("/META-INF/config/schema/rackspace-auth-user-configuration.xsd")
-    // TODO: Clean up the asInstanceOf below, if possible?
-    configurationService.subscribeTo(filterConfig.getFilterName,
-      config,
-      xsdURL,
-      handlerFactory.asInstanceOf[UpdateListener[RackspaceAuthUserConfig]],
-      classOf[RackspaceAuthUserConfig])
+    logger.trace("Rackspace Auth User filter initializing...")
+    configFileName = new FilterConfigHelper(filterConfig).getFilterConfig(DEFAULT_CONFIG)
+
+    logger.info(s"Initializing Rackspace Auth User filter using config $configFileName")
+    val xsdURL = getClass.getResource(SCHEMA_FILE_NAME)
+    configurationService.subscribeTo(filterConfig.getFilterName, configFileName, xsdURL, this, classOf[RackspaceAuthUserConfig])
+
+    logger.trace("Rackspace Auth User filter initialized.")
   }
 
   override def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain): Unit = {
-    new FilterLogicHandlerDelegate(servletRequest, servletResponse, filterChain).doFilter(handlerFactory.newHandler)
+    if (!isInitialized) {
+      logger.warn("Rackspace Auth User filter has not yet initialized.")
+      servletResponse.asInstanceOf[HttpServletResponse].sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE)
+      return
+    }
+
+    val httpServletRequest = servletRequest.asInstanceOf[HttpServletRequest]
+    if (httpServletRequest.getMethod != "POST") {
+      // this filter only operates on POST requests that have a body to parse
+      filterChain.doFilter(servletRequest, servletResponse)
+    } else {
+      val wrappedRequest = new HttpServletRequestWrapper(httpServletRequest)
+      handler.get.parseUserGroupFromInputStream(wrappedRequest.getInputStream, wrappedRequest.getContentType) foreach { rackspaceAuthUserGroup =>
+        wrappedRequest.addHeader(rackspaceAuthUserGroup.user, rackspaceAuthUserGroup.group, rackspaceAuthUserGroup.quality)
+      }
+
+      filterChain.doFilter(wrappedRequest, servletResponse)
+    }
   }
 
   override def destroy(): Unit = {
-    configurationService.unsubscribeFrom(config, handlerFactory)
+    logger.trace("Rackspace Auth User filter destroying...")
+    configurationService.unsubscribeFrom(configFileName, this.asInstanceOf[UpdateListener[_]])
+    logger.trace("Rackspace Auth User filter destroyed.")
   }
+
+  override def configurationUpdated(config: RackspaceAuthUserConfig): Unit = {
+    handler.set(new RackspaceAuthUserHandler(config))
+    initialized.set(true)
+  }
+
+  override def isInitialized: Boolean = initialized.get
+}
+
+object RackspaceAuthUserFilter {
+  private final val DEFAULT_CONFIG = "rackspace-auth-user.cfg.xml"
+  private final val SCHEMA_FILE_NAME = "/META-INF/config/schema/rackspace-auth-user-configuration.xsd"
 }
