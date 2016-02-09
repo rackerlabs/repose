5f5cb8611a71df4ccb9edd96f13c35244dd0250f
Damien Johnson <damien.johnson@rackspace.com>
2016-02-09 14:13:48 -0600

    SLF4JHttpLogging filter conversion and refactor

--- repose-aggregator/components/filters/slf4j-http-logging/src/main/scala/org/openrepose/filters/slf4jlogging/Slf4jLoggerWrapper.scala
+++ repose-aggregator/components/filters/slf4j-http-logging/src/main/scala/org/openrepose/filters/slf4jlogging/Slf4jLoggerWrapper.scala
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,32 +17,11 @@
  * limitations under the License.
  * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
  */
-package org.openrepose.filters.slf4jlogging;
+package org.openrepose.filters.slf4jlogging
 
-import org.openrepose.commons.utils.logging.apache.HttpLogFormatter;
-import org.slf4j.Logger;
+import org.openrepose.commons.utils.logging.apache.HttpLogFormatter
+import org.slf4j.Logger
 
-public class Slf4jLoggerWrapper {
-
-    private Logger logger;
-    private HttpLogFormatter formatter;
-    private String formatString;
-
-    public Slf4jLoggerWrapper(Logger logger, String formatString) {
-        this.logger = logger;
-        this.formatString = formatString;
-        this.formatter = new HttpLogFormatter(formatString);
-    }
-
-    public HttpLogFormatter getFormatter() {
-        return formatter;
-    }
-
-    public String getFormatString() {
-        return formatString;
-    }
-
-    public Logger getLogger() {
-        return logger;
-    }
+case class Slf4jLoggerWrapper(logger: Logger, formatString: String) {
+  val formatter = new HttpLogFormatter(formatString)
 }
