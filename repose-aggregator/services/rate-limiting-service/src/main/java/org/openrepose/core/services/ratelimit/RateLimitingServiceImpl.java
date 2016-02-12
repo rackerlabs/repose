34ff3cde3b93f38e4f5f2fae1ced423424c1f2da
wdschei <Bill.Scheidegger@Rackspace.com>
2016-02-12 11:53:51 -0600

    Updated the Rate Limiting Service to treat the default HTTP Methods of ALL as equivalent to the JAXB returned empty List.

--- repose-aggregator/services/rate-limiting-service/src/main/java/org/openrepose/core/services/ratelimit/RateLimitingServiceImpl.java
+++ repose-aggregator/services/rate-limiting-service/src/main/java/org/openrepose/core/services/ratelimit/RateLimitingServiceImpl.java
@@ -135,7 +135,9 @@ public class RateLimitingServiceImpl implements RateLimitingService {
     }
 
     private boolean httpMethodMatches(List<HttpMethod> configMethods, String requestMethod) {
-        return configMethods.contains(HttpMethod.ALL) || configMethods.contains(HttpMethod.valueOf(requestMethod.toUpperCase()));
+        return configMethods.isEmpty() || // Indicates the current default of ALL is desired.
+                configMethods.contains(HttpMethod.ALL) ||
+                configMethods.contains(HttpMethod.valueOf(requestMethod.toUpperCase()));
     }
 
     private boolean queryParameterNameMatches(List<String> configuredQueryParams, Map<String, String[]> requestParameterMap) {
