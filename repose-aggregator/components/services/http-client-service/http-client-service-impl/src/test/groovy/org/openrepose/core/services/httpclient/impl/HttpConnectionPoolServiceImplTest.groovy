1e64f768f4552d3b2e4b57bf31f9feacbe0bbd00
Damien Johnson <damien.johnson@rackspace.com>
2016-03-08 09:28:55 -0600

    Fixing license flagged by the gradle build

--- repose-aggregator/components/services/http-client-service/http-client-service-impl/src/test/groovy/org/openrepose/core/services/httpclient/impl/HttpConnectionPoolServiceImplTest.groovy
+++ repose-aggregator/components/services/http-client-service/http-client-service-impl/src/test/groovy/org/openrepose/core/services/httpclient/impl/HttpConnectionPoolServiceImplTest.groovy
@@ -4,7 +4,7 @@
  * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
  * Copyright (C) 2010 - 2015 Rackspace US, Inc.
  * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
