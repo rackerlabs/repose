3db77f4d63c8d8e7ed7286d169146cc84b1c65c9
jenny.vo <jenny.vo@rackspace.com>
2016-03-22 14:14:24 -0500

    update add more tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/headertranslation/HeaderTranslationWQualityTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/headertranslation/HeaderTranslationWQualityTest.groovy
@@ -26,30 +26,85 @@ import spock.lang.Unroll
 
 /**
  * Created by jennyvo on 3/21/16.
+ *  Header translation with quality test
  */
 class HeaderTranslationWQualityTest extends ReposeValveTest {
+    def static Map params = [:]
+    def static originEndpoint
 
     def setupSpec() {
         deproxy = new Deproxy()
-        deproxy.addEndpoint(properties.targetPort)
+        originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
 
-        def params = properties.defaultTemplateParams
-        repose.configurationProvider.applyConfigs("common", params)
-        repose.configurationProvider.applyConfigs("features/filters/headertranslation/common", params)
-        repose.configurationProvider.applyConfigs("features/filters/headertranslation/wquality", params)
+        params = properties.defaultTemplateParams
+        repose.configurationProvider.applyConfigs("common", params);
+        repose.configurationProvider.applyConfigs("features/filters/headertranslation/common", params);
+        repose.configurationProvider.applyConfigs("features/filters/headertranslation/wquality", params);
         repose.start()
     }
 
     def cleanupSpec() {
-        deproxy.shutdown()
-        repose.stop()
+        if (deproxy) {
+            deproxy.shutdown()
+        }
+
+        if (repose) {
+            repose.stop()
+        }
     }
-    @Unroll("Request Verb: #method Headers: #reqHeaders")
-    def "when translating CSL request headers"() {
+
+    def "when translation header quality in config should override quality from header" () {
 
         when: "client passes a request through repose with headers to be translated"
-        def respFromOrigin = deproxy.makeRequest(url: (String) reposeEndpoint, method: method, headers: reqHeaders)
-        def sentRequest = ((MessageChain) respFromOrigin).getHandlings()[0]
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "GET",
+                headers: ["x-pp-user"    : "a",
+                          "x-tenant-name": "b",
+                          "x-roles"      : "c;q=0.3"]
+        )
+
+        def sentRequest = mc.getHandlings()[0]
+
+        then: "origin receives translated headers"
+        sentRequest.request.getHeaders().contains("x-rax-username")
+        sentRequest.request.getHeaders().contains("x-rax-tenants")
+        sentRequest.request.getHeaders().contains("x-rax-roles")
+        sentRequest.request.getHeaders().getFirstValue("x-rax-roles") == "c;q=0.2"
+        sentRequest.request.getHeaders().contains("x-pp-user")
+        sentRequest.request.getHeaders().contains("x-tenant-name")
+        sentRequest.request.getHeaders().contains("x-roles")
+        sentRequest.request.getHeaders().getFirstValue("x-roles") == "c;q=0.3"
+    }
+
+    def "when translation header with different quality header" () {
+
+        when: "client passes a request through repose with headers to be translated"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "GET",
+                headers: ["x-pp-user"    : "a",
+                          "x-tenant-name": "b",
+                          "x-roles"      : "test",
+                          "x-rax-roles"  : "test;q=0.5"]
+        )
+
+        def sentRequest = mc.getHandlings()[0]
+
+        then: "origin receives translated headers"
+        sentRequest.request.getHeaders().contains("x-rax-username")
+        sentRequest.request.getHeaders().contains("x-rax-tenants")
+        sentRequest.request.getHeaders().contains("x-rax-roles")
+        sentRequest.request.getHeaders().findAll("x-rax-roles").contains("test;q=0.2")
+        sentRequest.request.getHeaders().findAll("x-rax-roles").contains("test;q=0.5")
+        sentRequest.request.getHeaders().contains("x-pp-user")
+        sentRequest.request.getHeaders().contains("x-tenant-name")
+        sentRequest.request.getHeaders().contains("x-roles")
+        sentRequest.request.getHeaders().getFirstValue("x-roles") == "test"
+    }
+
+    @Unroll("Request with: #method Headers: #reqHeaders")
+    def "when translating request headers with quality"() {
+
+        when: "client passes a request through repose with headers to be translated"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: method, headers: reqHeaders)
+        def sentRequest = mc.getHandlings()[0]
 
         then: "origin receives translated headers"
         sentRequest.request.getHeaders().contains("x-rax-username")
