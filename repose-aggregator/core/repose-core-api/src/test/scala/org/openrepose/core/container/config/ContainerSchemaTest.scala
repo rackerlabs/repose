82d4e0510ae3c862d15719f6347a89e64d20fc9d
Mario Lopez <Mario.Lopez@rackspace.com>
2016-04-11 08:43:22 -0500

    Added schema test for container-configuration.xsd

---
+++ repose-aggregator/core/repose-core-api/src/test/scala/org/openrepose/core/container/config/ContainerSchemaTest.scala
@@ -0,0 +1,106 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+
+package org.openrepose.core.container.config
+
+import org.junit.runner.RunWith
+import org.openrepose.commons.test.ConfigValidator
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.junit.JUnitRunner
+import org.xml.sax.SAXParseException
+
+@RunWith(classOf[JUnitRunner])
+class ContainerSchemaTest extends FunSpec with Matchers {
+  val validator = ConfigValidator("/META-INF/schema/container/container-configuration.xsd")
+
+  describe("schema validation") {
+    it("should successfully validate the sample config") {
+      validator.validateConfigFile("/META-INF/schema/examples/container.cfg.xml")
+    }
+
+    it("should successfully validate a list of unique ciphers") {
+      val config = """<repose-container xmlns='http://docs.openrepose.org/repose/container/v2.0'>
+                     |    <deployment-config>
+                     |        <deployment-directory auto-clean="false">/var/repose</deployment-directory>
+                     |        <artifact-directory check-interval="60000">/usr/share/repose/filters</artifact-directory>
+                     |        <logging-configuration href="file:///etc/repose/log4j2.xml"/>
+                     |        <ssl-configuration>
+                     |            <keystore-filename>keystore.repose</keystore-filename>
+                     |            <keystore-password>manage</keystore-password>
+                     |            <key-password>password</key-password>
+                     |            <included-ciphers>
+                     |                <cipher>.*TLS.*</cipher>
+                     |            </included-ciphers>
+                     |            <excluded-ciphers>
+                     |                <cipher>.*SSL.*</cipher>
+                     |            </excluded-ciphers>
+                     |        </ssl-configuration>
+                     |    </deployment-config>
+                     |</repose-container>""".stripMargin
+      validator.validateConfigString(config)
+    }
+
+    it("should reject config with the same cipher configured multiple times in the included-ciphers list") {
+      val config = """<repose-container xmlns='http://docs.openrepose.org/repose/container/v2.0'>
+                     |    <deployment-config>
+                     |        <deployment-directory auto-clean="false">/var/repose</deployment-directory>
+                     |        <artifact-directory check-interval="60000">/usr/share/repose/filters</artifact-directory>
+                     |        <logging-configuration href="file:///etc/repose/log4j2.xml"/>
+                     |        <ssl-configuration>
+                     |            <keystore-filename>keystore.repose</keystore-filename>
+                     |            <keystore-password>manage</keystore-password>
+                     |            <key-password>password</key-password>
+                     |            <included-ciphers>
+                     |                <cipher>.*TLS.*</cipher>
+                     |                <cipher>.*TLS.*</cipher>
+                     |            </included-ciphers>
+                     |        </ssl-configuration>
+                     |    </deployment-config>
+                     |</repose-container>""".stripMargin
+      val exception = intercept[SAXParseException] {
+        validator.validateConfigString(config)
+      }
+      exception.getLocalizedMessage should include ("Don't duplicate ciphers")
+    }
+
+    it("should reject config with the same cipher configured multiple times in the excluded-ciphers list") {
+      val config = """<repose-container xmlns='http://docs.openrepose.org/repose/container/v2.0'>
+                     |    <deployment-config>
+                     |        <deployment-directory auto-clean="false">/var/repose</deployment-directory>
+                     |        <artifact-directory check-interval="60000">/usr/share/repose/filters</artifact-directory>
+                     |        <logging-configuration href="file:///etc/repose/log4j2.xml"/>
+                     |        <ssl-configuration>
+                     |            <keystore-filename>keystore.repose</keystore-filename>
+                     |            <keystore-password>manage</keystore-password>
+                     |            <key-password>password</key-password>
+                     |            <excluded-ciphers>
+                     |                <cipher>.*SSL.*</cipher>
+                     |                <cipher>.*SSL.*</cipher>
+                     |            </excluded-ciphers>
+                     |        </ssl-configuration>
+                     |    </deployment-config>
+                     |</repose-container>""".stripMargin
+      val exception = intercept[SAXParseException] {
+        validator.validateConfigString(config)
+      }
+      exception.getLocalizedMessage should include ("Don't duplicate ciphers")
+    }
+  }
+}
