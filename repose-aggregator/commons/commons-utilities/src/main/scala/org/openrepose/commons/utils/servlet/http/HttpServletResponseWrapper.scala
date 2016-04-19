d1462ea94c23ebe05c6b7a84bc2d927435ce84ec
Damien Johnson <damien.johnson@rackspace.com>
2016-04-19 16:03:52 -0500

    Adding back the IllegalStateException thrown by commitToResponse

--- repose-aggregator/commons/commons-utilities/src/main/scala/org/openrepose/commons/utils/servlet/http/HttpServletResponseWrapper.scala
+++ repose-aggregator/commons/commons-utilities/src/main/scala/org/openrepose/commons/utils/servlet/http/HttpServletResponseWrapper.scala
@@ -425,6 +425,9 @@ class HttpServletResponseWrapper(originalResponse: HttpServletResponse, headerMo
     }
   }
 
+  /**
+    * @throws IllegalStateException when neither headerMode nor bodyMode is ResponseMode.MUTABLE
+    */
   def commitToResponse(): Unit = {
     def writeHeaders(): Unit = {
       headerMap foreach { case (name, values) =>
@@ -448,7 +451,7 @@ class HttpServletResponseWrapper(originalResponse: HttpServletResponse, headerMo
       case (_, ResponseMode.MUTABLE) =>
         writeBody()
       case (_, _) =>
-        // do nothing
+        throw new IllegalStateException("method should not be called if the ResponseMode is not set to MUTABLE")
     }
 
     if (flushedBuffer) {
