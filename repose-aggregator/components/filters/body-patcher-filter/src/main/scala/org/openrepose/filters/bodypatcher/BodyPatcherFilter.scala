b749600e5c6cb66827e07c26ca1f249bb9b5ee87
Adrian George <adrian.george@rackspace.com>
2016-04-29 13:52:36 -0500

    The skeleton of the filter. Be amazed at the absolutely nothing it does

---
+++ repose-aggregator/components/filters/body-patcher-filter/src/main/scala/org/openrepose/filters/bodypatcher/BodyPatcherFilter.scala
@@ -0,0 +1,43 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.filters.bodypatcher
+
+import javax.servlet._
+
+import org.openrepose.core.filter.AbstractConfiguredFilter
+import org.openrepose.core.services.config.ConfigurationService
+import org.openrepose.filters.bodypatcher.config.BodyPatcherConfig
+
+/**
+  * Created by adrian on 4/29/16.
+  */
+class BodyPatcherFilter(configurationService: ConfigurationService) extends AbstractConfiguredFilter[BodyPatcherConfig](configurationService) {
+  override val DEFAULT_CONFIG: String = null
+  override val SCHEMA_LOCATION: String = null
+
+  /**
+    * Where the concrete class does it's work. This method is the equivalent doFilter in a normal filter.
+    *
+    * @param request
+    * @param response
+    * @param chain
+    */
+  override def doWork(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit = ???
+}
