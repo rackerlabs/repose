a940e961b6b0ba0fe1810b18f7e422b31540e809
jenny.vo <jenny.vo@rackspace.com>
2016-04-29 14:51:10 -0500

    update add more tests

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/bodyextractortoheader/BodyExtractorToHeaderTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/bodyextractortoheader/BodyExtractorToHeaderTest.groovy
@@ -54,25 +54,64 @@ class BodyExtractorToHeaderTest extends ReposeValveTest {
             deproxy.shutdown()
     }
 
-    @Unroll()
-    def "When request with match config jpath will add to header as config"() {
+    @Unroll
+    def "When request with match config jsonpath will add header as config and its value"() {
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "POST", requestBody: reqbody)
 
         then:
         mc.handlings.size() == 1
-        if (expectedheaders == "") {
-            assertFalse(mc.handlings[0].request.headers.contains("x-device-id"))
-            assertFalse(mc.handlings[0].request.headers.contains("x-server-id"))
+        // x-test-param will not added to req since not match request body jsonpath and default not set
+        assertFalse(mc.handlings[0].request.headers.contains("x-test-param"))
+        if (matchedheaders == "") {
+            // doesn't match still add header if default is specified
+            assertTrue(mc.handlings[0].request.headers.contains("x-device-id"))
+            assertTrue(mc.handlings[0].request.headers.contains("x-server-id"))
+            assertEquals(mc.handlings[0].request.headers.getFirstValue("x-device-id"), "test")
+            assertEquals(mc.handlings[0].request.headers.getFirstValue("x-server-id"), "test")
+
         } else {
-            assertTrue(mc.handlings[0].request.headers.contains(expectedheaders))
-            assertEquals(mc.handlings[0].request.headers.getFirstValue(expectedheaders), headervalue)
+            assertTrue(mc.handlings[0].request.headers.contains(matchedheaders))
+            assertEquals(mc.handlings[0].request.headers.getFirstValue(matchedheaders), headervalue)
+            assertEquals(mc.handlings[0].request.headers.getFirstValue(unmatchedheaders), "test")
         }
 
         where:
-        reqbody         | expectedheaders | headervalue
-        matchDevideBody | "x-device-id"   | "12345"
-        matchServerBody | "x-server-id"   | "abc123"
-        noNatchBody     | ""              | ""
+        reqbody         | matchedheaders | headervalue | unmatchedheaders
+        matchDevideBody | "x-device-id"  | "12345"     | "x-server-id"
+        matchServerBody | "x-server-id"  | "abc123"    | "x-device-id"
+        noNatchBody     | ""             | ""          | ""
+    }
+
+    def "Override exist header if override=true"() {
+        given: "request with previous set of headers"
+        Map headers = ["x-device-id": "54321", "x-server-id": "reposetest123"]
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "POST", headers: headers, requestBody: matchDevideBody)
+
+        then:
+        mc.handlings.size() == 1
+        assertTrue(mc.handlings[0].request.headers.contains("x-device-id"))
+        assertTrue(mc.handlings[0].request.headers.contains("x-server-id"))
+        assertEquals(mc.handlings[0].request.headers.getFirstValue("x-device-id"), "12345")
+        assertEquals(mc.handlings[0].request.headers.getFirstValue("x-server-id"), "reposetest123")
+    }
+
+    def "Not override exist header if override=false"() {
+        given: "request with previous set of headers"
+        Map headers = ["x-device-id": "54321", "x-server-id": "reposetest123"]
+
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "POST", headers: headers, requestBody: matchServerBody)
+
+        then:
+        mc.handlings.size() == 1
+        assertTrue(mc.handlings[0].request.headers.contains("x-device-id"))
+        assertTrue(mc.handlings[0].request.headers.contains("x-server-id"))
+        assertEquals(mc.handlings[0].request.headers.getFirstValue("x-device-id"), "54321")
+        assertTrue(mc.handlings[0].request.headers.findAll("x-server-id").contains("reposetest123"))
+        // not override but add header extracted from body
+        assertTrue(mc.handlings[0].request.headers.findAll("x-server-id").contains("abc123"))
     }
 }
