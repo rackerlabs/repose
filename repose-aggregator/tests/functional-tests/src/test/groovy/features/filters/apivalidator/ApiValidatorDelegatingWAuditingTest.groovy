b6f39524782128b92fd837db10c43efaf448752d
jenny.vo <jenny.vo@rackspace.com>
2016-05-04 16:07:04 -0500

    test

---
+++ repose-aggregator/tests/functional-tests/src/test/groovy/features/filters/apivalidator/ApiValidatorDelegatingWAuditingTest.groovy
@@ -0,0 +1,55 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.filters.apivalidator
+
+import framework.ReposeValveTest
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+
+class ApiValidatorDelegatingWAuditingTest extends ReposeValveTest {
+
+    def setupSpec() {
+        reposeLogSearch.cleanLog()
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(properties.targetPort)
+
+        def params = properties.getDefaultTemplateParams()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/apivalidator/common", params)
+        repose.configurationProvider.applyConfigs("features/filters/apivalidator/delegable/withauditing", params)
+        repose.start()
+        repose.waitForNon500FromUrl(reposeEndpoint)
+    }
+
+    def "delegating mode should not break intra-filter logging"() {
+        when:
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/audit", method: method, headers: headers, requestBody: "horrible input")
+
+        then:
+        reposeLogSearch.searchByString("Unable to populate request body").size() != 0
+        reposeLogSearch.searchByString("java.io.IOException: Stream closed").size() != 0
+        mc.receivedResponse.code == "400"
+        mc.receivedResponse.message == "Bad Request"
+
+        where:
+        method | headers
+        "POST" | ["x-roles": "default", "Content-Type": "application/atom+xml"]
+    }
+}
