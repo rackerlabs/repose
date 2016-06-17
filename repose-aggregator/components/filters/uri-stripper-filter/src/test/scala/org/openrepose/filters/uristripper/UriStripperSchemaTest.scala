51be36e86a13c6a871a41ca22417b8a87c386494
Damien Johnson <damien.johnson@rackspace.com>
2016-06-17 12:50:45 -0500

    Configuration changes

---
+++ repose-aggregator/components/filters/uri-stripper-filter/src/test/scala/org/openrepose/filters/uristripper/UriStripperSchemaTest.scala
@@ -0,0 +1,73 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.filters.uristripper
+
+import org.junit.runner.RunWith
+import org.openrepose.commons.test.ConfigValidator
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
+import org.xml.sax.SAXParseException
+
+@RunWith(classOf[JUnitRunner])
+class UriStripperSchemaTest extends FunSpec with Matchers {
+  val validator = ConfigValidator("/META-INF/schema/config/uri-stripper.xsd")
+
+  describe("schema validation") {
+    it("should successfully validate the sample config") {
+      validator.validateConfigFile("/META-INF/schema/examples/uri-stripper.cfg.xml")
+    }
+
+    val methods = Set("GET", "DELETE", "POST", "PUT", "PATCH", "HEAD", "OPTIONS", "CONNECT", "TRACE", "ALL")
+    methods.subsets.filter(_.nonEmpty).map(_.mkString(" ")).foreach { httpMethods =>
+      it(s"should successfully validate if the HTTP Methods list is not empty ($httpMethods)") {
+        val config =
+          s"""<uri-stripper xmlns="http://docs.openrepose.org/repose/uri-stripper/v1.0" rewrite-location="true" token-index="1">
+              |    <link-resources>
+              |        <resource uri-path-regex=".*" http-methods="$httpMethods">
+              |            <link-paths>
+              |                <json>$$.service.link</json>
+              |            </link-paths>
+              |        </resource>
+              |    </link-resources>
+              |</uri-stripper>""".stripMargin
+
+        validator.validateConfigString(config)
+      }
+    }
+
+    it(s"should fail to validate if the HTTP Methods list is empty") {
+      val config =
+        s"""<uri-stripper xmlns="http://docs.openrepose.org/repose/uri-stripper/v1.0" rewrite-location="true" token-index="1">
+            |    <link-resources>
+            |        <resource uri-path-regex=".*" http-methods="">
+            |            <link-paths>
+            |                <json>$$.service.link</json>
+            |            </link-paths>
+            |        </resource>
+            |    </link-resources>
+            |</uri-stripper>""".stripMargin
+
+      val exception = intercept[SAXParseException] {
+        validator.validateConfigString(config)
+      }
+      exception.getLocalizedMessage should include("If the http-methods attribute is present, then it must not be empty.")
+    }
+  }
+}
