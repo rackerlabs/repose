054590e9b2f2e895b8fc5237cdbc2b11eed3cca5
Mario Lopez <Mario.Lopez@rackspace.com>
2016-07-05 13:48:05 -0500

    Added additional functional tests for Atom Feed updates to use the Akka Service Client

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/services/atomfeed/AtomFeedServiceConnectionPoolTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/services/atomfeed/AtomFeedServiceConnectionPoolTest.groovy
@@ -29,6 +29,7 @@ import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.Endpoint
 import org.rackspace.deproxy.HeaderCollection
 import org.rackspace.deproxy.Request
+import spock.lang.Unroll
 
 import java.util.concurrent.TimeUnit
 
@@ -48,15 +49,15 @@ class AtomFeedServiceConnectionPoolTest extends ReposeValveTest {
         fakeAtomFeed = new AtomFeedResponseSimulator(atomPort)
         atomEndpoint = deproxy.addEndpoint(atomPort, 'atom service', null, fakeAtomFeed.handler)
 
+        fakeIdentityV2Service = new MockIdentityV2Service(properties.identityPort, properties.targetPort)
+        deproxy.addEndpoint(properties.identityPort, 'identity service', null, fakeIdentityV2Service.handler)
+
         Map params = properties.defaultTemplateParams + testParams
         repose.configurationProvider.applyConfigs("common", params)
         repose.configurationProvider.applyConfigs("features/services/atomfeed", params)
         repose.start()
 
         originEndpoint = deproxy.addEndpoint(properties.targetPort, 'origin service')
-
-        fakeIdentityV2Service = new MockIdentityV2Service(properties.identityPort, properties.targetPort)
-        deproxy.addEndpoint(properties.identityPort, 'identity service', null, fakeIdentityV2Service.handler)
     }
 
     def cleanup() {
@@ -64,24 +65,33 @@ class AtomFeedServiceConnectionPoolTest extends ReposeValveTest {
         repose?.stop()
     }
 
-    def "when an unauthenticated atom feed is not configured with a connection pool, it uses the default one"() {
+    @Unroll
+    def "when an #feedType atom feed is configured with connection pool #connectionPool, it uses the #expectedPoolId pool"() {
         given: "keystone is configured to use the unauth atom feed with no connection pool configured"
-        startReposeWithConfigParams("atom-feed-id": "unauth-feed-no-pool")
+        startReposeWithConfigParams("atom-feed-id": atomFeedId)
 
         and: "an atom feed entry is available for consumption"
-        String atomFeedEntry = fakeAtomFeed.createAtomEntry(id: 'urn:uuid:101')
+        String atomFeedEntry = fakeAtomFeed.createAtomEntry(id: "urn:uuid:$entryId")
         HeaderCollection requestHeaders = new HeaderCollection()
         def headerGrabber = { Request request -> requestHeaders = request.headers }
         atomEndpoint.defaultHandler = fakeAtomFeed.handlerWithEntries([atomFeedEntry], headerGrabber)
 
         when: "we wait for the Keystone V2 filter to read the feed"
         reposeLogSearch.awaitByString("</atom:entry>", 1, 11, TimeUnit.SECONDS)
-        atomEndpoint.defaultHandler = fakeAtomFeed.handler
 
         then: "the atom feed entry is read by the Keystone V2 filter"
-        reposeLogSearch.searchByString("<atom:id>urn:uuid:101</atom:id>").size() == 1
+        reposeLogSearch.searchByString("<atom:id>urn:uuid:$entryId</atom:id>").size() == 1
 
         and: "the default connection pool was used"
-        requestHeaders.getFirstValue("X-Pool-Id") == "default-pool"
+        requestHeaders.getFirstValue("X-Pool-Id") == expectedPoolId
+
+        where:
+        feedType          | connectionPool          | atomFeedId                 | expectedPoolId          | entryId
+        "unauthenticated" | "<none>"                | "unauth-feed-no-pool"      | "default-pool"          | 103
+        "unauthenticated" | "atom-feed-pool-unauth" | "unauth-feed-pool"         | "atom-feed-pool-unauth" | 105
+        "unauthenticated" | "missing-pool-unauth"   | "unauth-feed-missing-pool" | "default-pool"          | 107
+        "authenticated"   | "<none>"                | "auth-feed-no-pool"        | "default-pool"          | 109
+        "authenticated"   | "atom-feed-pool-auth"   | "auth-feed-pool"           | "atom-feed-pool-auth"   | 111
+        "authenticated"   | "missing-pool-auth"     | "auth-feed-missing-pool"   | "default-pool"          | 113
     }
 }
