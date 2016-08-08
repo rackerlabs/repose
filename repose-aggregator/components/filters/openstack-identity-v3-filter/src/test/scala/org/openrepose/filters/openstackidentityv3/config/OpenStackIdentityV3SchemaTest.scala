521b61b4418bd38d6638931f3f2eec92bc25b576
wdschei <Bill.Scheidegger@Rackspace.com>
2016-08-08 16:26:12 -0500

    Updated the OpenStack Identity v3 filter's configuration schema unit tests to the support pre-authorized roles bypass and deprecate the old roles-which-bypass-project-id-check name.

--- repose-aggregator/components/filters/openstack-identity-v3-filter/src/test/scala/org/openrepose/filters/openstackidentityv3/config/OpenStackIdentityV3SchemaTest.scala
+++ repose-aggregator/components/filters/openstack-identity-v3-filter/src/test/scala/org/openrepose/filters/openstackidentityv3/config/OpenStackIdentityV3SchemaTest.scala
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith
 import org.openrepose.commons.test.ConfigValidator
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
+import org.xml.sax.SAXParseException
 
 @RunWith(classOf[JUnitRunner])
 class OpenStackIdentityV3SchemaTest extends FunSpec with Matchers {
@@ -32,6 +33,53 @@ class OpenStackIdentityV3SchemaTest extends FunSpec with Matchers {
   describe("schema validation") {
     it("should successfully validate the sample config") {
       validator.validateConfigFile("/META-INF/schema/examples/openstack-identity-v3.cfg.xml")
+    }
+
+    it("should successfully validate with only a roles-which-bypass-project-id-check defined") {
+      val config = """<openstack-identity-v3 xmlns="http://docs.openrepose.org/repose/openstack-identity-v3/v1.0"
+                     |                       connection-pool-id="identity-v3-pool">
+                     |    <openstack-identity-service
+                     |            username="admin_username"
+                     |            password="admin_password"
+                     |            uri="http://identity.example.com"/>
+                     |    <roles-which-bypass-project-id-check>
+                     |        <role>Role</role>
+                     |    </roles-which-bypass-project-id-check>
+                     |</openstack-identity-v3>""".stripMargin
+      validator.validateConfigString(config)
+    }
+
+    it("should successfully validate with only a pre-authorized-roles defined") {
+      val config = """<openstack-identity-v3 xmlns="http://docs.openrepose.org/repose/openstack-identity-v3/v1.0"
+                     |                       connection-pool-id="identity-v3-pool">
+                     |    <openstack-identity-service
+                     |            username="admin_username"
+                     |            password="admin_password"
+                     |            uri="http://identity.example.com"/>
+                     |    <pre-authorized-roles>
+                     |        <role>Role</role>
+                     |    </pre-authorized-roles>
+                     |</openstack-identity-v3>""".stripMargin
+      validator.validateConfigString(config)
+    }
+
+    it("should reject config if both the deprecated roles-which-bypass-project-id-check element and the replacement pre-authorized-roles element are defined") {
+      val config = """<openstack-identity-v3 xmlns="http://docs.openrepose.org/repose/openstack-identity-v3/v1.0"
+                     |                       connection-pool-id="identity-v3-pool">
+                     |    <openstack-identity-service
+                     |            username="admin_username"
+                     |            password="admin_password"
+                     |            uri="http://identity.example.com"/>
+                     |    <roles-which-bypass-project-id-check>
+                     |        <role>Role</role>
+                     |    </roles-which-bypass-project-id-check>
+                     |    <pre-authorized-roles>
+                     |        <role>Role</role>
+                     |    </pre-authorized-roles>
+                     |</openstack-identity-v3>""".stripMargin
+      intercept[SAXParseException] {
+        validator.validateConfigString(config)
+      }.getLocalizedMessage should include ("Cannot define the deprecated roles-which-bypass-project-id-check and pre-authorized-roles")
     }
   }
 }
