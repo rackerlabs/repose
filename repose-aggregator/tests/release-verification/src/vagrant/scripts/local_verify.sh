51fdbe77946fadfc33a0125198ae5c2cbac9af27
wdschei <Bill.Scheidegger@Rackspace.com>
2016-10-13 16:16:54 -0500

    Updated the Vagrant artifact test's local verification script to match the behavior of the build script.

--- repose-aggregator/tests/release-verification/src/vagrant/scripts/local_verify.sh
+++ repose-aggregator/tests/release-verification/src/vagrant/scripts/local_verify.sh
@@ -4,71 +4,63 @@ START=$(date +"%s")
 SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
 REPOSE_DIR=`readlink -f ${SCRIPT_DIR}/../../../../../../`
 SOURCE_DIR=`readlink -f ${REPOSE_DIR}/repose-aggregator/tests/release-verification/src`
-BUILD_DIR=`readlink -f ${REPOSE_DIR}/repose-aggregator/tests/release-verification/build/Vagrant`
+BUILD_DIR=`readlink -f ${REPOSE_DIR}/repose-aggregator/tests/release-verification/build`
 
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-cp -R ${SOURCE_DIR}/vagrant/* \
-      ${BUILD_DIR}
 
-if [[ -n $1 && -d "${BUILD_DIR}/${1}" ]] ; then
+if [[ -n $1 && -d "${SOURCE_DIR}/vagrant/${1}" ]] ; then
+    cp -R ${SOURCE_DIR}/vagrant/${1} \
+          ${BUILD_DIR}
     VERIFY_DIR=${BUILD_DIR}/${1}
 else
+    cp -R ${SOURCE_DIR}/vagrant/deb \
+          ${BUILD_DIR}
     VERIFY_DIR=${BUILD_DIR}/deb
 fi
 cd ${VERIFY_DIR}
-cp -R ${BUILD_DIR}/scripts \
+cp -R ${SOURCE_DIR}/vagrant/scripts \
       ${VERIFY_DIR}/
-cp -R ${BUILD_DIR}/fake-services \
+cp -R ${SOURCE_DIR}/vagrant/fake-services \
       ${VERIFY_DIR}/
+mkdir -p ${VERIFY_DIR}/etc_repose
+cp ${SOURCE_DIR}/config/* \
+   ${VERIFY_DIR}/etc_repose/
 
 vagrant destroy --force
 vagrant box update
 #export REPOSE_VERSION=local
-#export REPOSE_VERSION=default
-export REPOSE_VERSION=7.3.8.0
+#export REPOSE_VERSION=current
+#export REPOSE_VERSION=7.3.8.0
+#export REPOSE_VERSION=8.1.0.0
 vagrant up
 
-# Make sure the script has executable permissions.
-vagrant ssh -c '
-cp /vagrant/scripts/vagrant_verify.sh /tmp/
-chmod a+x /tmp/vagrant_verify.sh
-'
-
-# Test the default install; should result in a Moved Permanently (301) status.
-vagrant ssh -c '
-echo "~~~TRUNCATED~~~" > /vagrant/default.log 2>&1
-/tmp/vagrant_verify.sh /var/log/repose/current.log
-if [ $? -eq 0 ]; then
-   curl -vs http://localhost:8080 > /vagrant/default.log 2>&1
-fi
-'
-grep -qs '301 Moved Permanently' ${VERIFY_DIR}/default.log
-if [ $? -ne 0 ]; then
-   echo -en "\n\n~~~~~ ERROR - Default Install FAILED ~~~~~\n\n"
-   cat ${VERIFY_DIR}/default.log
-   echo -en "\n\n"
-else
-   echo -en "\n\n~~~~~ SUCCESS - Default Install SUCCESS ~~~~~\n\n"
-fi
+## Test the default install; should result in a Moved Permanently (301) status.
+#vagrant ssh -c '
+#cp /vagrant/scripts/isReposeReady.sh /tmp/
+#chmod a+x /tmp/isReposeReady.sh
+#echo "~~~TRUNCATED~~~" > /vagrant/default.log 2>&1
+#/tmp/isReposeReady.sh /var/log/repose/current.log
+#if [ $? -eq 0 ]; then
+#   curl -vs http://localhost:8080 > /vagrant/default.log 2>&1
+#fi
+#grep -qs "301 Moved Permanently" /vagrant/default.log
+#STATUS=$?
+#if [ "$STATUS" -ne 0 ]; then
+#   cat /vagrant/default.log
+#   echo -en "\n\n"
+#fi
+#'
+#if [ $? -ne 0 ]; then
+#   echo -en "\n\n~~~~~ ERROR - Default Install FAILED ~~~~~\n\n"
+#else
+#   echo -en "\n\n~~~~~ SUCCESS - Default Install SUCCESS ~~~~~\n\n"
+#fi
 
 # Test the filter bundle install; should result in an Ok (200) status.
-mkdir -p ${VERIFY_DIR}/etc_repose
-cp ${SOURCE_DIR}/config/* \
-   ${VERIFY_DIR}/etc_repose/
-vagrant ssh -c '
-echo "~~~TRUNCATED~~~" > /vagrant/validation.log 2>&1
-/tmp/vagrant_verify.sh /vagrant/var-log-repose-current.log /vagrant/etc_repose
-if [ $? -eq 0 ]; then
-   sleep 15
-   curl -vs http://localhost:8080/resource/this-is-an-id > /vagrant/validation.log 2>&1
-fi
-'
-grep -qs '200 OK' ${VERIFY_DIR}/validation.log
+vagrant ssh -c 'sh /vagrant/scripts/vagrant_verify.sh'
 if [ $? -ne 0 ]; then
    echo -en "\n\n~~~~~ ERROR - Validation Install FAILED ~~~~~\n\n"
-   cat ${VERIFY_DIR}/validation.log
-   echo -en "\n\n"
 else
    echo -en "\n\n~~~~~ SUCCESS - Validation Install SUCCESS ~~~~~\n\n"
 fi
