d24ef3233dca714b62339757d8dcb8e21882417a
Damien Johnson <damien.johnson@rackspace.com>
2016-10-19 14:15:29 -0500

    Syncing a consistent directory

--- repose-aggregator/tests/release-verification/src/scripts/docker_verify.sh
+++ repose-aggregator/tests/release-verification/src/scripts/docker_verify.sh
@@ -3,20 +3,20 @@
 echo "-------------------------------------------------------------------------------------------------------------------"
 echo "Starting Mock Services"
 echo "-------------------------------------------------------------------------------------------------------------------"
-sh /scripts/fake_keystone_run.sh
-sh /scripts/fake_origin_run.sh
+sh /release-verification/scripts/fake_keystone_run.sh
+sh /release-verification/scripts/fake_origin_run.sh
 
-echo "~~~TRUNCATED~~~" > /validation.log 2>&1
-cp /scripts/isReposeReady.sh /tmp/
+echo "~~~TRUNCATED~~~" > /release-verification/validation.log 2>&1
+cp /release-verification/scripts/isReposeReady.sh /tmp/
 chmod a+x /tmp/isReposeReady.sh
-/tmp/isReposeReady.sh /var-log-repose-current.log
+/tmp/isReposeReady.sh /release-verification/var-log-repose-current.log
 if [ $? -eq 0 ]; then
-   curl -vs http://localhost:8080/resource/this-is-an-id > /validation.log 2>&1
+   curl -vs http://localhost:8080/resource/this-is-an-id > /release-verification/validation.log 2>&1
 fi
-grep -qs "200 OK" /validation.log
+grep -qs "200 OK" /release-verification/validation.log
 STATUS=$?
 if [ "$STATUS" -ne 0 ]; then
-   cat /validation.log
+   cat /release-verification/validation.log
    echo -en "\n\n"
 fi
 exit $STATUS
