c9ddce21754b0faa270545f7d6fbf2148d2d9a0e
Damien Johnson <damien.johnson@rackspace.com>
2016-10-24 15:13:25 -0500

    Fixing Repose startup and startup check

--- repose-aggregator/tests/release-verification/src/scripts/docker_isReposeReady.sh
+++ repose-aggregator/tests/release-verification/src/scripts/docker_isReposeReady.sh
@@ -11,7 +11,7 @@ READY=0
 COUNT=0
 TIMEOUT=60
 while [ $READY -eq 0 ]; do
-   sudo grep -qs "Repose ready" $LOG_FILE
+   grep -qs "Repose ready" $LOG_FILE
    if [ "$?" -eq 0 ]; then
       READY=1
    else
