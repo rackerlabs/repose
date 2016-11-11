3fcc04d4008b5fc5388dd5b14dc08da8f9a1bc32
wdschei <Bill.Scheidegger@Rackspace.com>
2016-11-11 13:18:52 -0600

    Fixed all of the Sonar findings for Try-with-resources should be used (squid:S2093).

--- repose-aggregator/commons/commons-utilities/src/main/java/org/openrepose/commons/utils/http/ServiceClient.java
+++ repose-aggregator/commons/commons-utilities/src/main/java/org/openrepose/commons/utils/http/ServiceClient.java
@@ -21,20 +21,13 @@ package org.openrepose.commons.utils.http;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.auth.params.AuthPNames;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.params.AuthPolicy;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.InputStreamEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.util.EntityUtils;
-import org.openrepose.commons.utils.StringUtilities;
 import org.openrepose.commons.utils.io.RawInputStreamReader;
 import org.openrepose.core.services.httpclient.HttpClientContainer;
 import org.openrepose.core.services.httpclient.HttpClientService;
@@ -57,9 +50,6 @@ import java.util.Set;
  */
 public class ServiceClient {
     private static final Logger LOG = LoggerFactory.getLogger(ServiceClient.class);
-    private String targetHostUri;
-    private String username;
-    private String password;
     private String connectionPoolId;
 
     private HttpClientService httpClientService;
@@ -69,79 +59,47 @@ public class ServiceClient {
         this.httpClientService = httpClientService;
     }
 
-    public ServiceClient(String targetHostUri, String username, String password, String connectionPoolId, HttpClientService httpClientService) {
-        this.targetHostUri = targetHostUri;
-        this.username = username;
-        this.password = password;
-        this.connectionPoolId = connectionPoolId;
-        this.httpClientService = httpClientService;
-
-    }
-
     private HttpClient getClientWithBasicAuth() {
         HttpClientContainer clientResponse = null;
-
         try {
-
             clientResponse = httpClientService.getClient(connectionPoolId);
-            final HttpClient client = clientResponse.getHttpClient();
-
-            if (!StringUtilities.isEmpty(targetHostUri) && !StringUtilities.isEmpty(username) && !StringUtilities.isEmpty(password)) {
-
-                client.getParams().setParameter(AuthPNames.PROXY_AUTH_PREF, AuthPolicy.BASIC);
-
-                CredentialsProvider credsProvider = new BasicCredentialsProvider();
-
-                credsProvider.setCredentials(
-                        new AuthScope(targetHostUri, AuthScope.ANY_PORT),
-                        new UsernamePasswordCredentials(username, password));
-                client.getParams().setParameter("http.authentication.credential-provider", credsProvider);
-
-            }
-
-            return client;
+            return clientResponse.getHttpClient();
         } finally {
             if (clientResponse != null) {
                 httpClientService.releaseClient(clientResponse);
             }
         }
-
     }
 
     private void setHeaders(HttpRequestBase base, Map<String, String> headers) {
-
         final Set<Map.Entry<String, String>> entries = headers.entrySet();
         for (Map.Entry<String, String> entry : entries) {
             base.addHeader(entry.getKey(), entry.getValue());
         }
     }
 
+    @SuppressWarnings("squid:S2093")
     private ServiceClientResponse execute(HttpRequestBase base, String... queryParameters) {
+        // I'm not exactly sure why this rule is triggering on this since there is nothing be created outside of the try.
+        // So it is safe to suppress warning squid:S2093
         try {
-
             HttpClient client = getClientWithBasicAuth();
-
             for (int index = 0; index < queryParameters.length; index = index + 2) {
                 client.getParams().setParameter(queryParameters[index], queryParameters[index + 1]);
             }
-
             HttpResponse httpResponse = client.execute(base);
             HttpEntity entity = httpResponse.getEntity();
-
             InputStream stream = new ByteArrayInputStream(new byte[0]);
             if (entity != null) {
                 stream = new ByteArrayInputStream(RawInputStreamReader.instance().readFully(entity.getContent()));
                 EntityUtils.consume(entity);
             }
-
             return new ServiceClientResponse(httpResponse.getStatusLine().getStatusCode(), httpResponse.getAllHeaders(), stream);
         } catch (IOException ex) {
             LOG.error("Error executing request", ex);
         } finally {
             base.releaseConnection();
-
         }
-
         return new ServiceClientResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null);
     }
 
@@ -151,7 +109,6 @@ public class ServiceClient {
 
     public ServiceClientResponse post(String uri, Map<String, String> headers, String body, MediaType contentMediaType) {
         HttpPost post = new HttpPost(uri);
-
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.putAll(headers);
         String localContentType = contentMediaType.getType() + "/" + contentMediaType.getSubtype();
@@ -161,9 +118,7 @@ public class ServiceClient {
         if (!requestHeaders.containsKey(CommonHttpHeader.ACCEPT.toString())) {
             requestHeaders.put(CommonHttpHeader.ACCEPT.toString(), MediaType.APPLICATION_XML);
         }
-
         setHeaders(post, requestHeaders);
-
         if (body != null && !body.isEmpty()) {
             post.setEntity(new InputStreamEntity(new ByteArrayInputStream(body.getBytes()), body.length()));
         }
@@ -171,33 +126,24 @@ public class ServiceClient {
     }
 
     public ServiceClientResponse get(String uri, Map<String, String> headers, String... queryParameters) {
-
         URI uriBuilt;
         HttpGet httpget = new HttpGet(uri);
-
         if (queryParameters != null) {
-
             if (queryParameters.length % 2 != 0) {
                 throw new IllegalArgumentException("Query parameters must be in pairs.");
             }
             try {
                 URIBuilder builder = new URIBuilder(uri);
-
                 for (int index = 0; index < queryParameters.length; index = index + 2) {
                     builder.setParameter(queryParameters[index], queryParameters[index + 1]);
                 }
-
                 uriBuilt = builder.build();
                 httpget = new HttpGet(uriBuilt);
-
             } catch (URISyntaxException e) {
                 LOG.error("Error building request URI", e);
                 return new ServiceClientResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, null);
-
             }
-
         }
-
         setHeaders(httpget, headers);
         return execute(httpget);
     }
--- repose-aggregator/core/repose-core/src/main/java/org/openrepose/nodeservice/httpcomponent/RequestProxyServiceImpl.java
+++ repose-aggregator/core/repose-core/src/main/java/org/openrepose/nodeservice/httpcomponent/RequestProxyServiceImpl.java
@@ -206,7 +206,11 @@ public class RequestProxyServiceImpl implements RequestProxyService {
         }
     }
 
+    @SuppressWarnings("squid:S2093")
     private ServiceClientResponse execute(HttpRequestBase base, String connPoolId) {
+        // I'm not exactly sure why this rule is triggering on this since HttpClientContainer does NOT implement one of the required interfaces.
+        // It is also not simply closed, but it is released back to the pool from which it was originally retrieved.
+        // So it is safe to suppress warning squid:S2093
         HttpClientContainer httpClientContainer = getClient(connPoolId);
         try {
             HttpResponse httpResponse = httpClientContainer.getHttpClient().execute(base);
