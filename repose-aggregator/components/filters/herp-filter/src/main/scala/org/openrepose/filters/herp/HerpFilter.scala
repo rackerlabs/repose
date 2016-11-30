556d0adb98c28e1ae5f1d256b3360225a435589b
wdschei <Bill.Scheidegger@Rackspace.com>
2016-11-30 17:46:09 -0600

    Updated the HERP filter to wrap the Request in order to access the OS response headers with the new wrapper's convenience methods.

--- repose-aggregator/components/filters/herp-filter/src/main/scala/org/openrepose/filters/herp/HerpFilter.scala
+++ repose-aggregator/components/filters/herp-filter/src/main/scala/org/openrepose/filters/herp/HerpFilter.scala
@@ -34,6 +34,7 @@ import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.openrepose.commons.config.manager.UpdateListener
 import org.openrepose.commons.utils.http.{CommonHttpHeader, OpenStackServiceHeader}
 import org.openrepose.commons.utils.logging.TracingHeaderHelper
+import org.openrepose.commons.utils.servlet.http.{HeaderInteractor, HttpServletRequestWrapper, HttpServletResponseWrapper, ResponseMode}
 import org.openrepose.core.filter.FilterConfigHelper
 import org.openrepose.core.services.config.ConfigurationService
 import org.openrepose.core.spring.ReposeSpringProperties
@@ -88,17 +89,18 @@ class HerpFilter @Inject()(configurationService: ConfigurationService,
       servletResponse.asInstanceOf[HttpServletResponse].sendError(HttpServletResponse.SC_SERVICE_UNAVAILABLE)
     } else {
       logger.trace("HERP filter passing request...")
-      filterChain.doFilter(servletRequest, servletResponse)
+      val httpServletResponseWrapper = new HttpServletResponseWrapper(servletResponse.asInstanceOf[HttpServletResponse], ResponseMode.READONLY, ResponseMode.READONLY)
+      filterChain.doFilter(servletRequest, httpServletResponseWrapper)
 
       logger.trace("HERP filter handling response...")
-      handleResponse(servletRequest.asInstanceOf[HttpServletRequest], servletResponse.asInstanceOf[HttpServletResponse])
+      handleResponse(servletRequest.asInstanceOf[HttpServletRequest], httpServletResponseWrapper)
 
       logger.trace("HERP filter returning response...")
     }
   }
 
   private def handleResponse(httpServletRequest: HttpServletRequest,
-                             httpServletResponse: HttpServletResponse) = {
+                             httpServletResponse: HttpServletResponseWrapper) = {
     def translateParameters(): Map[String, Array[String]] = {
       def decode(s: String) = URLDecoder.decode(s, StandardCharsets.UTF_8.name)
 
@@ -121,11 +123,16 @@ class HerpFilter @Inject()(configurationService: ConfigurationService,
       else stripHeaderParams(allProjectIds.maxBy(getQuality))
     }
 
-    val projectIds = httpServletRequest.getHeaders(OpenStackServiceHeader.TENANT_ID.toString).asScala
-      .++(httpServletRequest.getHeaders(X_PROJECT_ID).asScala).toTraversable match {
+    def getSplitHeaders(headerInteractor: HeaderInteractor, headers: List[String]): Traversable[String] = {
+      headers.foldLeft(Traversable[String]()) { (accumulator, current) =>
+        accumulator.++(headerInteractor.getSplittableHeaders(current).asScala)
+      }
+    }
+
+    val tenantProjectHeaders = List(OpenStackServiceHeader.TENANT_ID.toString, X_PROJECT_ID)
+    val projectIds = getSplitHeaders(new HttpServletRequestWrapper(httpServletRequest), tenantProjectHeaders) match {
       case x if x.nonEmpty => x
-      case _ => httpServletResponse.getHeaders(OpenStackServiceHeader.TENANT_ID.toString).asScala
-        .++(httpServletResponse.getHeaders(X_PROJECT_ID).asScala).toTraversable
+      case _ => getSplitHeaders(httpServletResponse, tenantProjectHeaders)
     }
 
     //def nullIfEmpty(it: Iterable[Any]) = if (it.isEmpty) null else it
@@ -176,7 +183,7 @@ class HerpFilter @Inject()(configurationService: ConfigurationService,
             case Some(value: Long) => pattern.findFirstIn(value.toString).isDefined
             case Some(value: String) => pattern.findFirstIn(value).isDefined
             case Some(value: Array[String]) => value.exists(pattern.findFirstIn(_).isDefined)
-            case Some(value: java.util.Set[_]) =>
+            case Some(_: java.util.Set[_]) =>
               // IF there is a sub key,
               // THEN try the map;
               // ELSE just bail.
