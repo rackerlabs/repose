1c307c1ec373c659ac8feaddfe9dfd3dc4fc995b
Mario Lopez <Mario.Lopez@rackspace.com>
2016-12-08 20:03:34 -0600

    Added more funcitonal tests for Response wrapper sendError fix

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/wrappers/response/ResponseWrapperTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/wrappers/response/ResponseWrapperTest.groovy
@@ -36,6 +36,108 @@ class ResponseWrapperTest extends ReposeValveTest {
     static final String REASON_HEADER_NAME = "X-Reason"
     static final String REASON_MESSAGE = "SOME REASON PHRASE"
 
+    static final String SCRIPT_IMPORTS = """\
+        |import org.openrepose.commons.utils.servlet.http.*
+        |import java.io.ByteArrayOutputStream
+        |import javax.servlet.ServletOutputStream
+        |import javax.servlet.WriteListener
+        |import javax.ws.rs.core.MediaType
+        |""".stripMargin()
+
+    static final String SCRIPT_CLASSES_FUNCTIONS = """\
+        |class ByteArrayServletOutputStream extends ServletOutputStream {
+        |    def baos = new ByteArrayOutputStream()
+        |
+        |    void write(int b) {
+        |        baos.write(b)
+        |    }
+        |
+        |    String toString() {
+        |        baos.toString()
+        |    }
+        |
+        |    boolean isReady() {
+        |        true
+        |    }
+        |
+        |    void setWriteListener(WriteListener wl) {
+        |        // do nothing
+        |    }
+        |}
+        |
+        |def setBody = { responseWrapper, status, body ->
+        |    responseWrapper.setStatus(status)
+        |    responseWrapper.setContentType(MediaType.TEXT_PLAIN)
+        |    responseWrapper.getOutputStream().print(body)
+        |}
+        |
+        |def responseCodeToReturn = request.getHeader("$RESPONSE_CODE_HEADER_NAME") as Integer
+        |""".stripMargin()
+
+    static final String SCRIPT_TEST_CASES = """\
+        |if (testCase == "REASON_TO_HEADER") {
+        |    filterChain.doFilter(request, response)
+        |
+        |    String reason = response.getReason()
+        |    if (reason) {
+        |        response.addHeader("$REASON_HEADER_NAME", reason)
+        |    }
+        |} else if (testCase == "THROW_EXCEPTION") {
+        |    throw new Exception("This test failed as it should not have reached this filter.")
+        |}  else if (testCase == "SEND_ERROR_WITH_MESSAGE") {
+        |    response.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "SEND_ERROR_CODE_ONLY") {
+        |    response.sendError(responseCodeToReturn)
+        |} else if (testCase == "REWRAP_RESPONSE") {
+        |    def headerMode = ResponseMode.valueOf(request.getHeader("$HEADER_MODE_HEADER_NAME"))
+        |    def bodyMode = ResponseMode.valueOf(request.getHeader("$BODY_MODE_HEADER_NAME"))
+        |
+        |    def rewrappedResponse = new HttpServletResponseWrapper(response, headerMode, bodyMode)
+        |
+        |    rewrappedResponse.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |
+        |    if (headerMode == ResponseMode.MUTABLE || bodyMode == ResponseMode.MUTABLE) {
+        |        rewrappedResponse.commitToResponse()
+        |    }
+        |} else if (testCase == "CALL_NEXT_FILTER") {
+        |    filterChain.doFilter(request, response)
+        |} else if (testCase == "SEND_ERROR_UNCOMMIT_SEND_ERROR") {
+        |    response.sendError(499, "This message should not make it to the client.")
+        |    response.uncommit()
+        |    response.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "SEND_ERROR_UNCOMMIT_SET_BODY") {
+        |    response.sendError(499, "This message should not make it to the client.")
+        |    response.uncommit()
+        |    response.resetError()
+        |    setBody(response, responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "SET_BODY_SEND_ERROR") {
+        |    setBody(response, 201, "This message should not make it to the client.")
+        |    response.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "SET_LARGE_BODY_SEND_ERROR") {
+        |    setBody(response, 201, "This message should not make it to the client." * 1_000_000)
+        |    response.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "CALL_NEXT_FILTER_THEN_SEND_ERROR") {
+        |    filterChain.doFilter(request, response)
+        |    response.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |} else if (testCase == "OUTPUT_STREAM_SEND_ERROR") {
+        |    def out = new ByteArrayServletOutputStream()
+        |    def rewrappedResponse = new HttpServletResponseWrapper(response, ResponseMode.MUTABLE, ResponseMode.MUTABLE, out)
+        |
+        |    rewrappedResponse.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |    rewrappedResponse.commitToResponse()
+        |} else if (testCase == "OUTPUT_STREAM_SET_BODY_SEND_ERROR") {
+        |    def out = new ByteArrayServletOutputStream()
+        |    def rewrappedResponse = new HttpServletResponseWrapper(response, ResponseMode.MUTABLE, ResponseMode.MUTABLE, out)
+        |
+        |    setBody(rewrappedResponse, 201, "This message should not make it to the client.")
+        |
+        |    rewrappedResponse.sendError(responseCodeToReturn, "$REASON_MESSAGE")
+        |    rewrappedResponse.commitToResponse()
+        |} else if (testCase == "SET_BODY") {
+        |    setBody(response, 201, "This message should not make it to the client.")
+        |}
+        |""".stripMargin()
+
     def setupSpec() {
         deproxy = new Deproxy()
         deproxy.addEndpoint(properties.targetPort, 'origin service')
@@ -55,7 +157,7 @@ class ResponseWrapperTest extends ReposeValveTest {
         repose.waitForNon500FromUrl(reposeEndpoint)
     }
 
-    def 'the reason phrase is passed to the next wrapper and can be added to a header'() {
+    def 'the reason message is passed to the original wrapper and can be added to a header'() {
         given: "the first filter will get the reason set by the second filter and will set the X-Reason header with the value"
         def firstFilterTestCase = "REASON_TO_HEADER"
 
@@ -198,4 +300,42 @@ class ResponseWrapperTest extends ReposeValveTest {
         "MUTABLE"         | "READONLY"
         "MUTABLE"         | "MUTABLE"
     }
+
+    @Unroll
+    def "should be able to #testDescription"() {
+        given: "the headers are set to tell the Scripting filters what to do"
+        def headers = [
+                (TEST_CASE_FIRST_FILTER_HEADER_NAME): firstFilterTestCase,
+                (TEST_CASE_SECOND_FILTER_HEADER_NAME): secondFilterTestCase,
+                (RESPONSE_CODE_HEADER_NAME): responseCode]
+
+        when: "any request is made"
+        MessageChain messageChain = deproxy.makeRequest(url: reposeEndpoint, method: "GET", headers: headers)
+
+        then: "the origin service does not receive the request"
+        messageChain.handlings.isEmpty()
+
+        and: "the response code is correctly set"
+        messageChain.receivedResponse.code as Integer == responseCode
+
+        and: "the response received by the user should contain the message in the body with the correct content type"
+        messageChain.receivedResponse.body as String == REASON_MESSAGE
+        messageChain.receivedResponse.headers.contains(CommonHttpHeader.CONTENT_TYPE.toString())
+        messageChain.receivedResponse.headers.getFirstValue(CommonHttpHeader.CONTENT_TYPE.toString()) == MediaType.TEXT_PLAIN
+
+        where:
+        firstFilterTestCase                 | secondFilterTestCase             | responseCode | testDescription
+        "SEND_ERROR_UNCOMMIT_SEND_ERROR"    | "THROW_EXCEPTION"                | 413          | "sendError, uncommit, sendError with another wrapper as the original wrapper"
+        "CALL_NEXT_FILTER"                  | "SEND_ERROR_UNCOMMIT_SEND_ERROR" | 413          | "sendError, uncommit, sendError with our wrapper as the original wrapper"
+        "SEND_ERROR_UNCOMMIT_SET_BODY"      | "THROW_EXCEPTION"                | 200          | "sendError, uncommit/resetError, set the body with another wrapper as the original wrapper"
+        "CALL_NEXT_FILTER"                  | "SEND_ERROR_UNCOMMIT_SET_BODY"   | 200          | "sendError, uncommit/resetError, set the body with our wrapper as the original wrapper"
+        "SET_BODY_SEND_ERROR"               | "THROW_EXCEPTION"                | 413          | "set the body then sendError with another wrapper as the original wrapper"
+        "CALL_NEXT_FILTER"                  | "SET_BODY_SEND_ERROR"            | 413          | "set the body then sendError with out wrapper as the original wrapper"
+        "SET_LARGE_BODY_SEND_ERROR"         | "THROW_EXCEPTION"                | 413          | "set a large body, uncommit, sendError with another wrapper as the original wrapper"
+        "CALL_NEXT_FILTER"                  | "SET_LARGE_BODY_SEND_ERROR"      | 413          | "set a large body, uncommit, sendError with out wrapper as the original wrapper"
+        "CALL_NEXT_FILTER_THEN_SEND_ERROR"  | "SET_BODY"                       | 413          | "call next filter, it sets the body, then the first filter calls sendError"
+        "OUTPUT_STREAM_SET_BODY_SEND_ERROR" | "THROW_EXCEPTION"                | 413          | "rewrap the response with a new output stream, set the body, uncommit, sendError"
+    }
+
+
 }
