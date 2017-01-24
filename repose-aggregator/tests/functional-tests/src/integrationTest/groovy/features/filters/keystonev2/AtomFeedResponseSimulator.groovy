ae1f5dbcdf440a89499b0992e0ebcf06d07b4e02
Mario Lopez <Mario.Lopez@rackspace.com>
2017-01-24 10:38:47 -0600

    Refactored AtomFeedResponseSimulator.groovy (functional tests) to use a builder instead of string literals for the test data

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/keystonev2/AtomFeedResponseSimulator.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/keystonev2/AtomFeedResponseSimulator.groovy
@@ -19,7 +19,7 @@
  */
 package features.filters.keystonev2
 
-import groovy.text.SimpleTemplateEngine
+import groovy.xml.MarkupBuilder
 import org.joda.time.DateTime
 import org.rackspace.deproxy.Request
 import org.rackspace.deproxy.Response
@@ -28,9 +28,10 @@ import org.rackspace.deproxy.Response
  * Simulates responses from an Identity Atom Feed
  */
 class AtomFeedResponseSimulator {
-    final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
-    def templateEngine = new SimpleTemplateEngine()
+    static final Map DEFAULT_FEED_PARAMS = [id: "urn:uuid:12345678-9abc-def0-1234-56789abcdef0", isLastPage: false]
+
     volatile boolean hasEntry = false
     int atomPort
 
@@ -39,18 +40,22 @@ class AtomFeedResponseSimulator {
 
     def headers = [
             'Connection'  : 'close',
-            'Content-type': 'application/xml',
-    ]
+            'Content-type': 'application/xml']
 
     AtomFeedResponseSimulator(int atomPort) {
         this.atomPort = atomPort
     }
 
+    def getDefaultParams() {
+        [atomPort: atomPort,
+         time    : new DateTime().toString(DATE_FORMAT),
+         token   : client_token,
+         tenant  : client_tenant]
+    }
+
     /**
      * This is a method that takes params, and returns a closure
      * Creates an appropriate closure for the TRR user token revocation event
-     * @param userId
-     * @return
      */
     def trrEventHandler(String userId) {
         { request ->
@@ -58,7 +63,7 @@ class AtomFeedResponseSimulator {
                 // reset for next time
                 hasEntry = false
 
-                new Response(200, 'OK', headers, populateTemplate(tokenRevocationRecordAtomEntryXml, [userId: userId]))
+                new Response(200, null, headers, atomFeed(atomEntryForTokenRevocationRecord(userId: userId)))
             }
         }
     }
@@ -69,193 +74,184 @@ class AtomFeedResponseSimulator {
                 // reset for next time
                 hasEntry = false
 
-                new Response(200, 'OK', headers, populateTemplate(userUpdateEvent, [userId: userId]))
+                new Response(200, null, headers, atomFeed(atomEntryForUserUpdate(userId: userId)))
             }
         }
     }
 
     def handler = { request ->
-        def template = hasEntry ? atomWithEntryXml() : atomEmptyXml
+        def body = hasEntry ? atomFeed(atomEntryForTokenInvalidation()) : atomFeedWithNoEntries()
 
         // reset for next time
         hasEntry = false
 
-        return new Response(200, 'OK', headers, populateTemplate(template))
+        new Response(200, null, headers, body)
     }
 
-    def handlerWithEntries(List<String> entries) {
-        { Request request -> new Response(200, 'OK', headers, populateTemplate(atomWithEntryXml(entries))) }
+    def handlerWithEntry(Closure<MarkupBuilder> entry) {
+        { Request request -> new Response(200, null, headers, atomFeed(entry)) }
     }
 
-    def populateTemplate(String template, Map params = [:]) {
-        def defaultParams = [
-                atomPort: atomPort,
-                time    : new DateTime().toString(DATE_FORMAT),
-                token   : client_token,
-                tenant  : client_tenant,
-        ]
-
-        templateEngine.createTemplate(template).make(defaultParams + params)
+    def handlerWithEntries(List<Closure<MarkupBuilder>> entries) {
+        // use closure composition and inject (fold) to turn the list of closures into one closure
+        handlerWithEntry(entries.inject({ MarkupBuilder builder -> builder }) { composedEntries, entry ->
+            composedEntries >> entry
+        })
     }
 
-    def String atomEmptyXml = """\
-        |<?xml version="1.0"?>
-        |<feed xmlns="http://www.w3.org/2005/Atom">
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="current"/>
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="self"/>
-        |    <id>urn:uuid:12345678-9abc-def0-1234-56789abcdef0</id>
-        |    <title type="text">feed</title>
-        |    <link href="http://localhost:\${atomPort}/feed/?marker=last&amp;limit=25&amp;search=&amp;direction=backward"
-        |          rel="last"/>
-        |    <updated>\${time}</updated>
-        |</feed>""".stripMargin()
+    String atomFeed(Map values = [:], Closure<MarkupBuilder> entries) {
+        Map params = getDefaultParams() + DEFAULT_FEED_PARAMS + values
 
-    def String atomWithEntryXml(List entries = [createAtomEntry()]) {
-        """\
-        |<?xml version="1.0"?>
-        |    <feed xmlns="http://www.w3.org/2005/Atom">
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="current"/>
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="self"/>
-        |    <id>urn:uuid:12345678-9abc-def0-1234-56789abcdef0</id>
-        |    <title type="text">feed</title>
-        |    <link href="http://localhost:\${atomPort}/feed/?marker=urn:uuid:1&amp;limit=25&amp;search=&amp;direction=forward"
-        |          rel="previous"/>
-        |    <updated>\${time}</updated>
-        |${entries.join("\n")}
-        |</feed>""".stripMargin()
+        def writer = new StringWriter()
+        def xmlBuilder = new MarkupBuilder(writer)
+        xmlBuilder.doubleQuotes = true
+        xmlBuilder.mkp.xmlDeclaration(version: "1.0", encoding: "UTF-8")
+
+        xmlBuilder.'feed'(xmlns: "http://www.w3.org/2005/Atom") {
+            'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "current")
+            'link'(href: "http://localhost:${params.atomPort}/feed/", rel: "self")
+            'id'(params.id)
+            'title'(type: "text", "feed")
+            if (params.isLastPage) {
+                'link'(href: "http://localhost:${params.atomPort}/feed/?marker=last&amp;limit=25&amp;search=&amp;direction=backward", rel: "last")
+            } else {
+                'link'(href: "http://localhost:${params.atomPort}/feed/?marker=urn:uuid:1&amp;limit=25&amp;search=&amp;direction=forward", rel: "previous")
+            }
+            'updated'(params.time)
+            entries(xmlBuilder)
+        }
+
+        writer.toString()
     }
 
-    def String createAtomEntry(Map<String, String> params = [:]) {
-        populateTemplate("""\
-        |    <atom:entry xmlns:atom="http://www.w3.org/2005/Atom"
-        |                xmlns="http://docs.rackspace.com/core/event"
-        |                xmlns:id="http://docs.rackspace.com/event/identity/token">
-        |        <atom:id>${params['id'] ?: 'urn:uuid:1'}</atom:id>
-        |        <atom:category term="rgn:IDK" />
-        |        <atom:category term="dc:IDK1" />
-        |        <atom:category term="rid:\${token}" />
-        |        <atom:category term="cloudidentity.token.token.delete" />
-        |        <atom:title type="text">Identity Token Event</atom:title>
-        |        <atom:author>
-        |            <atom:name>Repose Team</atom:name>
-        |        </atom:author>
-        |        <atom:content type="application/xml">
-        |            <event dataCenter="IDK"
-        |                   environment="JUNIT"
-        |                   eventTime="\${time}"
-        |                   id="12345678-9abc-def0-1234-56789abcdef0"
-        |                   region="IDK"
-        |                   resourceId="\${token}"
-        |                   type="DELETE"
-        |                   version="1">
-        |                <id:product resourceType="TOKEN"
-        |                            serviceCode="CloudIdentity"
-        |                            tenants="\${tenant}"
-        |                            version="1" />
-        |            </event>
-        |        </atom:content>
-        |        <atom:link href="http://test.feed.atomhopper.rackspace.com/some/identity/feed/entries/urn:uuid:4fa194dc-5148-a465-254d-b8ccab3766bc"
-        |                   rel="self" />
-        |        <atom:updated>\${time}</atom:updated>
-        |        <atom:published>\${time}</atom:published>
-        |    </atom:entry>""".stripMargin())
+    String atomFeedWithNoEntries() {
+        atomFeed(isLastPage: true) { MarkupBuilder builder ->
+            builder
+        }
     }
 
-    /**
-     * This is to revoke a specific set of tokens for a specific user
-     * The resourceId is a user id, not a token ID.
-     */
-    def tokenRevocationRecordAtomEntryXml = """\
-        |<?xml version="1.0"?>
-        |<feed xmlns="http://www.w3.org/2005/Atom">
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="current"/>
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="self"/>
-        |    <id>urn:uuid:12345678-9abc-def0-1234-56789abcdef0</id>
-        |    <title type="text">feed</title>
-        |    <link href="http://localhost:\${atomPort}/feed/?marker=urn:uuid:1&amp;limit=25&amp;search=&amp;direction=forward"
-        |          rel="previous"/>
-        |    <updated>\${time}</updated>
-        |    <atom:entry xmlns:atom="http://www.w3.org/2005/Atom"
-        |        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-        |        xmlns="http://www.w3.org/2001/XMLSchema">
-        |        <atom:id>urn:uuid:e53d007a-fc23-11e1-975c-cfa6b29bb814</atom:id>
-        |        <atom:category term="rgn:DFW"/>
-        |        <atom:category term="dc:DFW1"/>
-        |        <atom:category term="rid:\${userId}"/>
-        |        <atom:category term="cloudidentity.user.trr_user.delete"/>
-        |        <atom:category term="type:cloudidentity.user.trr_user.delete"/>
-        |        <atom:title>CloudIdentity</atom:title>
-        |        <atom:content type="application/xml">
-        |            <event xmlns="http://docs.rackspace.com/core/event"
-        |                xmlns:sample="http://docs.rackspace.com/event/identity/trr/user"
-        |                id="e53d007a-fc23-11e1-975c-cfa6b29bb814"
-        |                version="2"
-        |                resourceId="\${userId}"
-        |                eventTime="\${time}"
-        |                type="DELETE"
-        |                dataCenter="DFW1"
-        |                region="DFW">
-        |                <sample:product serviceCode="CloudIdentity"
-        |                    version="1"
-        |                    resourceType="TRR_USER"
-        |                    tokenCreationDate="2013-09-26T15:32:00Z">
-        |                    <sample:tokenAuthenticatedBy values="PASSWORD APIKEY"/>
-        |                </sample:product>
-        |            </event>
-        |        </atom:content>
-        |        <atom:link href="https://ord.feeds.api.rackspacecloud.com/identity/events/entries/urn:uuid:e53d007a-fc23-11e1-975c-cfa6b29bb814"
-        |            rel="self"/>
-        |        <atom:updated>\${time}</atom:updated>
-        |        <atom:published>\${time}</atom:published>
-        |    </atom:entry>
-        |</feed>""".stripMargin()
+    static Closure<MarkupBuilder> atomEntry(Map params = [:], Closure<MarkupBuilder> event) {
+        return { MarkupBuilder builder ->
+            builder.'atom:entry'(
+                    'xmlns:atom': "http://www.w3.org/2005/Atom",
+                    'xmlns': "http://docs.rackspace.com/core/event",
+                    'xmlns:id': "http://docs.rackspace.com/event/identity/token") {
+                'atom:id'(params.id)
+                params.categories.each {
+                    'atom:category'(term: it)
+                }
+                'atom:title'(type: "text", params.title)
+                'atom:author' {
+                    'atom:name'("Repose Team")
+                }
+                'atom:content'(type: "application/xml") {
+                    event(builder)
+                }
+                'atom:link'(href: params.selfLink, rel: "self")
+                'atom:updated'(params.time)
+                'atom:published'(params.time)
+            }
 
-    def userUpdateEvent = """\
-        |<?xml version="1.0"?>
-        |<feed xmlns="http://www.w3.org/2005/Atom">
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="current"/>
-        |    <link href="http://localhost:\${atomPort}/feed/" rel="self"/>
-        |    <id>urn:uuid:12345678-9abc-def0-1234-56789abcdef0</id>
-        |    <title type="text">feed</title>
-        |    <link href="http://localhost:\${atomPort}/feed/?marker=urn:uuid:1&amp;limit=25&amp;search=&amp;direction=forward"
-        |          rel="previous"/>
-        |    <updated>\${time}</updated>
-        |    <atom:entry xmlns:atom="http://www.w3.org/2005/Atom">
-        |        <atom:id>urn:uuid:e29ac1ca-fd06-11e1-a80c-bb58fc4a6929</atom:id>
-        |        <atom:category term="rgn:DFW" />
-        |        <atom:category term="dc:DFW1" />
-        |        <atom:category term="rid:\${userId}"/>
-        |        <atom:category term="tid:123456" />
-        |        <atom:category term="cloudidentity.user.user.update" />
-        |        <atom:category term="type:cloudidentity.user.user.update" />
-        |        <atom:category term="updatedAttributes:GROUPS ROLES PASSWORD" />
-        |        <atom:title type="text">Identity Event</atom:title>
-        |        <atom:content type="application/xml">
-        |            <event xmlns="http://docs.rackspace.com/core/event"
-        |                xmlns:id="http://docs.rackspace.com/event/identity/user"
-        |                dataCenter="DFW1"
-        |                environment="PROD"
-        |                eventTime="\${time}"
-        |                id="e29ac1ca-fd06-11e1-a80c-bb58fc4a6929"
-        |                region="DFW"
-        |                resourceId="\${userId}"
-        |                tenantId="123456"
-        |                resourceName="testuser"
-        |                type="UPDATE" version="1">
-        |                <id:product displayName="testUser"
-        |                    groups="group1 group2 group3"
-        |                    migrated="false"
-        |                    multiFactorEnabled="false"
-        |                    resourceType="USER"
-        |                    roles="admin RAX:admin role3"
-        |                    serviceCode="CloudIdentity"
-        |                    updatedAttributes="GROUPS ROLES PASSWORD"
-        |                    version="2" />
-        |            </event>
-        |        </atom:content>
-        |        <atom:link href="http://localhost:\${atomPort}/feed/" rel="self"/>
-        |        <atom:updated>\${time}</atom:updated>
-        |        <atom:published>\${time}</atom:published>
-        |    </atom:entry>
-        |</feed>""".stripMargin()
+            builder
+        }
+    }
+
+    Closure<MarkupBuilder> atomEntryForTokenInvalidation(Map values = [:]) {
+        Map params = getDefaultParams()
+        params += [
+                title: "Identity Token Event",
+                id: "urn:uuid:1",
+                categories: ["rgn:IDK", "dc:IDK1", "rid:${values.token ?: params.token}",
+                             "cloudidentity.token.token.delete"],
+                selfLink: "http://test.feed.atomhopper.rackspace.com/some/identity/feed/entries/urn:uuid:4fa194dc-5148-a465-254d-b8ccab3766bc"]
+        params += values
+
+        return atomEntry(params) { MarkupBuilder builder ->
+            builder.'event'(
+                    dataCenter: "IDK",
+                    environment: "JUNIT",
+                    eventTime: params.time,
+                    id: "12345678-9abc-def0-1234-56789abcdef0",
+                    region: "IDK",
+                    resourceId: params.token,
+                    type: "DELETE",
+                    version: "1") {
+                'id:product'(resourceType: "TOKEN", serviceCode: "CloudIdentity", tenants: params.tenant, version: "1")
+            }
+
+            builder
+        }
+    }
+
+    Closure<MarkupBuilder> atomEntryForTokenRevocationRecord(Map values = [:]) {
+        Map params = getDefaultParams()
+        params += [
+                title: "CloudIdentity",
+                id: "urn:uuid:e53d007a-fc23-11e1-975c-cfa6b29bb814",
+                categories: ["rgn:DFW", "dc:DFW1", "rid:${values.userId ?: params.userId}",
+                             "cloudidentity.user.trr_user.delete", "type:cloudidentity.user.trr_user.delete"],
+                selfLink: "https://ord.feeds.api.rackspacecloud.com/identity/events/entries/urn:uuid:e53d007a-fc23-11e1-975c-cfa6b29bb814"]
+        params += values
+
+        return atomEntry(params) { MarkupBuilder builder ->
+            builder.'event'(
+                    'xmlns:sample': "http://docs.rackspace.com/event/identity/trr/user",
+                    dataCenter: "DFW1",
+                    eventTime: params.time,
+                    id: "e53d007a-fc23-11e1-975c-cfa6b29bb814",
+                    region: "DFW",
+                    resourceId: params.userId,
+                    type: "DELETE",
+                    version: "2") {
+                'sample:product'(resourceType: "TRR_USER", serviceCode: "CloudIdentity", version: "1", tokenCreationDate: "2013-09-26T15:32:00Z") {
+                    'sample:tokenAuthenticatedBy'(values: "PASSWORD APIKEY")
+                }
+            }
+
+            builder
+        }
+    }
+
+    Closure<MarkupBuilder> atomEntryForUserUpdate(Map values = [:]) {
+        Map params = getDefaultParams()
+        params += [
+                title: "Identity Event",
+                id: "urn:uuid:e29ac1ca-fd06-11e1-a80c-bb58fc4a6929",
+                categories: ["rgn:DFW", "dc:DFW1", "rid:${values.userId ?: params.userId}", "tid:123456",
+                             "cloudidentity.user.user.update", "type:cloudidentity.user.user.update",
+                             "updatedAttributes:GROUPS ROLES PASSWORD"],
+                selfLink: "http://localhost:${params.atomPort}/feed/"]
+        params += values
+
+        return atomEntry(params) { MarkupBuilder builder ->
+            builder.'event'(
+                    'xmlns:id': "http://docs.rackspace.com/event/identity/user",
+                    dataCenter: "DFW1",
+                    environment: "PROD",
+                    eventTime: params.time,
+                    id: "e29ac1ca-fd06-11e1-a80c-bb58fc4a6929",
+                    region: "DFW",
+                    resourceId: params.userId,
+                    tenantId: "123456",
+                    resourceName: "testuser",
+                    type: "UPDATE",
+                    version: "1") {
+                'id:product'(resourceType: "USER", serviceCode: "CloudIdentity", version: "2", displayName: "testUser",
+                        groups: "group1 group2 group3", migrated: "false", multiFactorEnabled: "false",
+                        roles: "admin RAX:admin role3", updatedAttributes: "GROUPS ROLES PASSWORD")
+            }
+
+            builder
+        }
+    }
+
+    static String buildXmlToString(Closure<MarkupBuilder> buildFunction) {
+        def writer = new StringWriter()
+        def xmlBuilder = new MarkupBuilder(writer)
+        xmlBuilder.doubleQuotes = true
+
+        buildFunction(xmlBuilder)
+
+        writer.toString()
+    }
 }
