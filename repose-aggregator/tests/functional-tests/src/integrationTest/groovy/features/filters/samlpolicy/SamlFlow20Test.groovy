bba3ba1316564a8e047440d969c70f79d95de28d
Mario Lopez <Mario.Lopez@rackspace.com>
2017-01-28 20:42:31 -0600

    Updated SAML Flow 2.0 validation tests to add more tests for the Identity calls

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/SamlFlow20Test.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/SamlFlow20Test.groovy
@@ -26,6 +26,7 @@ import framework.mocks.MockIdentityV2Service
 import groovy.xml.MarkupBuilder
 import org.opensaml.saml.saml2.core.Response
 import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.Request
 import org.rackspace.deproxy.Response as DeproxyResponse
 import org.spockframework.runtime.ConditionNotSatisfiedError
 import spock.lang.FailsWith
@@ -33,7 +34,10 @@ import spock.lang.Unroll
 
 import static features.filters.samlpolicy.util.SamlPayloads.*
 import static features.filters.samlpolicy.util.SamlUtilities.*
+import static framework.mocks.MockIdentityV2Service.DEFAULT_MAPPING_POLICY
 import static framework.mocks.MockIdentityV2Service.IDP_NO_RESULTS
+import static framework.mocks.MockIdentityV2Service.createIdentityFaultJsonWithValues
+import static framework.mocks.MockIdentityV2Service.createIdpJsonWithValues
 import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND
@@ -89,9 +93,6 @@ class SamlFlow20Test extends ReposeValveTest {
         mc.handlings[0].request.headers.getFirstValue(CONTENT_TYPE) == CONTENT_TYPE_XML
         mc.handlings[0].request.headers.getFirstValue(IDENTITY_API_VERSION) == "2.0"
         fakeIdentityV2Service.getGenerateTokenFromSamlResponseCount() == 1
-
-        //and: "the response from the origin service is appropriately updated by the filter"
-        // TODO: what is Flow 2.0 going to do to the response?
 
         when: "the saml:response received by the origin service is unmarshalled"
         Response response = samlUtilities.unmarshallResponse(mc.handlings[0].request.body as String)
@@ -367,12 +368,12 @@ class SamlFlow20Test extends ReposeValveTest {
         "one"     | "two"     | "three"
     }
 
-    @Unroll
     @FailsWith(ConditionNotSatisfiedError)
-    def "a saml:response with an Issuer that Identity doesn't know about (returns #description) should be rejected with a 401"() {
-        given: "the IDP call will return the given response for this test"
-        // TODO: verify this is what Identity would send for this case
-        fakeIdentityV2Service.getIdpFromIssuerHandler = getIdpFromIssuerHandler
+    def "a saml:response with an Issuer that Identity doesn't know about should be rejected with a 401"() {
+        given: "the IDP call will return an empty list"
+        fakeIdentityV2Service.getIdpFromIssuerHandler = { String issuerParam, Request request ->
+            new DeproxyResponse(SC_OK, null, [(CONTENT_TYPE): CONTENT_TYPE_JSON], IDP_NO_RESULTS)
+        }
 
         and: "the Issuer is unique which will force the call to Identity (avoiding the cache)"
         def samlIssuer = generateUniqueIssuer()
@@ -387,37 +388,136 @@ class SamlFlow20Test extends ReposeValveTest {
 
         then: "the client gets back a bad response"
         mc.receivedResponse.code as Integer == SC_UNAUTHORIZED
+
+        and: "the request doesn't get to the origin service"
+        mc.handlings.isEmpty()
+    }
+
+    @FailsWith(ConditionNotSatisfiedError)
+    def "a saml:response with an Issuer that Identity has a blank mapping policy for should be rejected with a 401"() {
+        given: "the mapping policy call will return a blank mapping policy"
+        fakeIdentityV2Service.getMappingPolicyForIdpHandler = { String idpId, Request request ->
+            new DeproxyResponse(
+                    SC_OK,
+                    null,
+                    [(CONTENT_TYPE): CONTENT_TYPE_JSON],
+                    /{"policy":{"name":"name"}}/)  // TODO: verify this is an example of an empty policy
+        }
+
+        and: "the Issuer is unique which will force the call to Identity (avoiding the cache)"
+        def samlIssuer = generateUniqueIssuer()
+        def saml = samlResponse(issuer(samlIssuer) >> status() >> assertion(issuer: samlIssuer, fakeSign: true))
+
+        when:
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the client gets back a bad response"
+        mc.receivedResponse.code as Integer == SC_UNAUTHORIZED
+
+        and: "the request doesn't get to the origin service"
+        mc.handlings.isEmpty()
+    }
+
+    @Unroll
+    @FailsWith(ConditionNotSatisfiedError)
+    def "when Identity returns a #code from the mapping policy endpoint, Repose should return a 500"() {
+        given: "the mapping policy call will not be successful"
+        fakeIdentityV2Service.getMappingPolicyForIdpHandler = { String idpId, Request request ->
+            new DeproxyResponse(
+                    code,
+                    null,
+                    [(CONTENT_TYPE): CONTENT_TYPE_JSON],
+                    createIdentityFaultJsonWithValues(name: fault, code: code, message: message))
+        }
+
+        and: "the Issuer is unique which will force the call to Identity (avoiding the cache)"
+        def samlIssuer = generateUniqueIssuer()
+        def saml = samlResponse(issuer(samlIssuer) >> status() >> assertion(issuer: samlIssuer, fakeSign: true))
+
+        when:
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the client gets back a bad response"
+        mc.receivedResponse.code as Integer == SC_INTERNAL_SERVER_ERROR
 
         and: "the request doesn't get to the origin service"
         mc.handlings.isEmpty()
 
         where:
-        description     | getIdpFromIssuerHandler
-        "a 404"         | { new DeproxyResponse(SC_NOT_FOUND) }
-        "an empty list" | { new DeproxyResponse(SC_OK, null, [(CONTENT_TYPE): CONTENT_TYPE_JSON], IDP_NO_RESULTS) }
+        code                      | fault           | message
+        SC_NOT_FOUND              | "itemNotFound"  | "Identity Provider with id/name was not found."
+        SC_INTERNAL_SERVER_ERROR  | "identityFault" | "The default IDP policy is not properly configured."
     }
 
     @FailsWith(ConditionNotSatisfiedError)
-    def "a saml:response with an Issuer that Identity doesn't have a mapping policy for should be rejected with a 401"() {
-        given: "the mapping policy call will return a 404"
-        // TODO: verify this is what Identity would send for this case
-        fakeIdentityV2Service.getMappingPolicyForIdpHandler = { new DeproxyResponse(SC_NOT_FOUND) }
+    def "Identity is queried with the correct Issuer when looking for the identity provider ID"() {
+        given: "we're going to capture the requested issuer and path to Identity for the issuer to IDP ID call"
+        String requestedIssuerParam = null
+        String fullRequestPath = null
+        fakeIdentityV2Service.getIdpFromIssuerHandler = { String issuerParam, Request request ->
+            requestedIssuerParam = issuerParam
+            fullRequestPath = request.path
+
+            def body = createIdpJsonWithValues(issuer: issuerParam)
+            def headers = [(CONTENT_TYPE): CONTENT_TYPE_JSON]
+            new DeproxyResponse(SC_OK, null, headers, body)
+        }
 
         and: "the Issuer is unique which will force the call to Identity (avoiding the cache)"
         def samlIssuer = generateUniqueIssuer()
         def saml = samlResponse(issuer(samlIssuer) >> status() >> assertion(issuer: samlIssuer, fakeSign: true))
 
         when:
-        def mc = deproxy.makeRequest(
+        deproxy.makeRequest(
                 url: reposeEndpoint + SAML_AUTH_URL,
                 method: HTTP_POST,
                 headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED],
                 requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
 
-        then: "the client gets back a bad response"
-        mc.receivedResponse.code as Integer == SC_UNAUTHORIZED
+        then: "the requested issuer should match what was in the saml:response"
+        requestedIssuerParam == samlIssuer
 
-        and: "the request doesn't get to the origin service"
-        mc.handlings.isEmpty()
+        and: "the raw path should not contain the issuer since it should have been URL encoded"
+        !fullRequestPath.contains(samlIssuer)
+    }
+
+    @FailsWith(ConditionNotSatisfiedError)
+    def "Identity is queried with the correct IDP ID when looking for the identity provider mapping policy"() {
+        given: "we're going to capture the requested IDP ID to Identity for the IDP ID to mapping policy call"
+        String requestedIdpId = null
+        fakeIdentityV2Service.getMappingPolicyForIdpHandler = { String idpId, Request request ->
+            requestedIdpId = idpId
+            new DeproxyResponse(SC_OK, null, [(CONTENT_TYPE): CONTENT_TYPE_JSON], DEFAULT_MAPPING_POLICY)
+        }
+
+        and: "we're going to ensure the IDP ID returned is unique"
+        String idpId = generateUniqueIdpId()
+        fakeIdentityV2Service.getIdpFromIssuerHandler = { String issuerParam, Request request ->
+            def body = createIdpJsonWithValues(issuer: issuerParam, id: idpId)
+            def headers = [(CONTENT_TYPE): CONTENT_TYPE_JSON]
+            new DeproxyResponse(SC_OK, null, headers, body)
+        }
+
+        and: "the Issuer is unique which will force the call to Identity (avoiding the cache)"
+        def samlIssuer = generateUniqueIssuer()
+        def saml = samlResponse(issuer(samlIssuer) >> status() >> assertion(issuer: samlIssuer, fakeSign: true))
+
+        when:
+        deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the requested IDP ID should match what was returned from the first Identity call"
+        requestedIdpId == idpId
     }
 }
