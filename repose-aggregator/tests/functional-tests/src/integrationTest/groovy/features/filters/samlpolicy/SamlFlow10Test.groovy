c2ce855a8c86e6dd77ca842ddb130ea84bb70708
Mario Lopez <Mario.Lopez@rackspace.com>
2017-01-30 21:06:39 -0600

    Finished SAML Flow 1.0 validation tests

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/SamlFlow10Test.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/SamlFlow10Test.groovy
@@ -23,12 +23,15 @@ package features.filters.samlpolicy
 import features.filters.samlpolicy.util.SamlUtilities
 import framework.ReposeValveTest
 import framework.mocks.MockIdentityV2Service
+import groovy.json.JsonSlurper
+import org.custommonkey.xmlunit.Diff
 import org.opensaml.saml.saml2.core.Response
 import org.rackspace.deproxy.Deproxy
 import spock.lang.Unroll
 
 import static features.filters.samlpolicy.util.SamlPayloads.*
 import static features.filters.samlpolicy.util.SamlUtilities.*
+import static framework.mocks.MockIdentityV2Service.createMappingJsonWithValues
 import static javax.servlet.http.HttpServletResponse.SC_OK
 
 /**
@@ -36,6 +39,8 @@ import static javax.servlet.http.HttpServletResponse.SC_OK
  */
 class SamlFlow10Test extends ReposeValveTest {
     static samlUtilities = new SamlUtilities()
+    static xmlSlurper = new XmlSlurper()
+    static jsonSlurper = new JsonSlurper()
     static MockIdentityV2Service fakeIdentityV2Service
 
     def setupSpec() {
@@ -78,7 +83,7 @@ class SamlFlow10Test extends ReposeValveTest {
         then: "the client gets back a good response"
         mc.receivedResponse.code as Integer == SC_OK
 
-        and: "the origin service received the request with the correct header values"
+        and: "the origin service receives the request with the correct header values"
         mc.handlings[0]
         mc.handlings[0].request.headers.getCountByName(CONTENT_TYPE) == 1
         mc.handlings[0].request.headers.getFirstValue(CONTENT_TYPE) == CONTENT_TYPE_XML
@@ -107,7 +112,7 @@ class SamlFlow10Test extends ReposeValveTest {
         then: "the client gets back a good response"
         mc.receivedResponse.code as Integer == SC_OK
 
-        and: "the origin service received the request with the correct header values"
+        and: "the origin service receives the request with the correct header values"
         mc.handlings[0]
         mc.handlings[0].request.headers.getFirstValue(CONTENT_TYPE) == CONTENT_TYPE_XML
         mc.handlings[0].request.headers.getFirstValue(IDENTITY_API_VERSION) == "1.0"
@@ -116,9 +121,6 @@ class SamlFlow10Test extends ReposeValveTest {
         and: "Identity is not queried for a policy mapping"
         fakeIdentityV2Service.getIdpFromIssuerCount == 0
         fakeIdentityV2Service.getMappingPolicyForIdpCount == 0
-
-        //and: "the response from the origin service is not updated by the filter"
-        // TODO: what is Flow 2.0 going to do to the response?
 
         where:
         saml                                     | flow20ValidationIssue
@@ -128,7 +130,16 @@ class SamlFlow10Test extends ReposeValveTest {
 
     @Unroll
     def "a saml:response that #signedState will not be altered and will maintain signature validity through Repose"() {
-        when:
+        given: "the Identity mock will return a mapping policy with extended attributes should one be requested (it shouldn't be)"
+        def mappingPolicy = createMappingJsonWithValues(
+                userExtAttribs: [banana: "phone", potato: "{0}"],
+                remote: [[path: $/\/saml2p:Response\/saml2:Assertion\/saml2:Subject\/saml2:NameID\/@SPProvidedID/$]])
+        def idpId = generateUniqueIdpId()
+        fakeIdentityV2Service.getIdpFromIssuerHandler = fakeIdentityV2Service.createGetIdpFromIssuerHandler(id: idpId)
+        fakeIdentityV2Service.getMappingPolicyForIdpHandler = fakeIdentityV2Service
+                .createGetMappingPolicyForIdp(mappings: [(idpId): mappingPolicy])
+
+        when: "a request containing a saml:response with a legacy Issuer is sent to Repose"
         def mc = deproxy.makeRequest(
                 url: reposeEndpoint + SAML_AUTH_URL,
                 method: HTTP_POST,
@@ -138,7 +149,7 @@ class SamlFlow10Test extends ReposeValveTest {
         then: "the client gets back a good response"
         mc.receivedResponse.code as Integer == SC_OK
 
-        and: "the origin service received the request with the correct header values"
+        and: "the origin service receives the request with the correct header values"
         mc.handlings[0]
         mc.handlings[0].request.headers.getFirstValue(CONTENT_TYPE) == CONTENT_TYPE_XML
         mc.handlings[0].request.headers.getFirstValue(IDENTITY_API_VERSION) == "1.0"
@@ -147,9 +158,6 @@ class SamlFlow10Test extends ReposeValveTest {
         and: "Identity is not queried for a policy mapping"
         fakeIdentityV2Service.getIdpFromIssuerCount == 0
         fakeIdentityV2Service.getMappingPolicyForIdpCount == 0
-
-        //and: "the response from the origin service is not updated by the filter"
-        // TODO: what is Flow 2.0 going to do to the response?
 
         when: "the saml:response received by the origin service is unmarshalled"
         Response response = samlUtilities.unmarshallResponse(mc.handlings[0].request.body as String)
@@ -164,5 +172,102 @@ class SamlFlow10Test extends ReposeValveTest {
         SAML_LEGACY_ISSUER_SIGNED_ASSERTION             | "has a signed assertion"               | false            | true
         SAML_LEGACY_ISSUER_SIGNED_MESSAGE               | "is signed"                            | true             | false
         SAML_LEGACY_ISSUER_SIGNED_MESSAGE_AND_ASSERTION | "has a signed assertion and is signed" | true             | true
+    }
+
+    @FailsWith(ConditionNotSatisfiedError)
+    def "the response to the client should not be translated by Repose when the saml:response has a Flow 1.0 Issuer"() {
+        given: "a saml:response with a legacy issuer"
+        def saml = samlResponse(issuer(SAML_LEGACY_ISSUER) >> status() >> assertion(issuer: SAML_LEGACY_ISSUER))
+
+        and: "the Identity mock will return a mapping policy with extended attributes should one be requested (it shouldn't be)"
+        def mappingPolicy = createMappingJsonWithValues(
+                userExtAttribs: [banana: "phone", potato: "{0}"],
+                remote: [[path: $/\/saml2p:Response\/saml2:Assertion\/saml2:Subject\/saml2:NameID\/@SPProvidedID/$]])
+        def idpId = generateUniqueIdpId()
+        fakeIdentityV2Service.getIdpFromIssuerHandler = fakeIdentityV2Service.createGetIdpFromIssuerHandler(id: idpId)
+        fakeIdentityV2Service.getMappingPolicyForIdpHandler = fakeIdentityV2Service
+                .createGetMappingPolicyForIdp(mappings: [(idpId): mappingPolicy])
+
+        when: "the request is sent to Repose asking for a JSON response"
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED, (ACCEPT): CONTENT_TYPE_JSON],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the origin service receives the request and the client receives the response"
+        mc.handlings[0]
+        mc.receivedResponse.code as Integer == SC_OK
+
+        when: "the 'access' response sent by the origin service is parsed as JSON"
+        def sentResponseJson = jsonSlurper.parseText(mc.handlings[0].response.body as String)
+
+        and: "the 'access' response received by the client is parsed as JSON"
+        def receivedResponseJson = jsonSlurper.parseText(mc.receivedResponse.body as String)
+
+        then: "there should not be any extended attributes in the JSON received by the client"
+        !receivedResponseJson.access.'RAX-AUTH:extendedAttributes'
+
+        and: "the JSON response was not altered by Repose"
+        receivedResponseJson == sentResponseJson
+    }
+
+    @FailsWith(ConditionNotSatisfiedError)
+    def "the JSON response to the client should not be translated by Repose even if the saml:response contained extended attributes when it has a Flow 1.0 Issuer"() {
+        given: "a saml:response with a legacy issuer and the extended attribute 'user/foo'"
+        def saml = samlResponse(issuer(SAML_LEGACY_ISSUER) >> status() >> assertion(
+                issuer: SAML_LEGACY_ISSUER,
+                attributes: [roles: ["nova:admin"], domain: ["827319"], email: ["popeye@sailor.man"], "user/foo": ["bar"]]))
+
+        when: "the request is sent to Repose asking for a JSON response"
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED, (ACCEPT): CONTENT_TYPE_JSON],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the origin service receives the request and the client receives the response"
+        mc.handlings[0]
+        mc.receivedResponse.code as Integer == SC_OK
+
+        when: "the 'access' response sent by the origin service is parsed as JSON"
+        def sentResponseJson = jsonSlurper.parseText(mc.handlings[0].response.body as String)
+
+        and: "the 'access' response received by the client is parsed as JSON"
+        def receivedResponseJson = jsonSlurper.parseText(mc.receivedResponse.body as String)
+
+        then: "there should not be any extended attributes in the JSON received by the client"
+        !receivedResponseJson.access.'RAX-AUTH:extendedAttributes'
+
+        and: "the JSON response was not altered by Repose"
+        receivedResponseJson == sentResponseJson
+    }
+
+    @FailsWith(ConditionNotSatisfiedError)
+    def "the XML response to the client should not be translated by Repose even if the saml:response contained extended attributes when it has a Flow 1.0 Issuer"() {
+        given: "a saml:response with a legacy issuer and the extended attribute 'user/foo'"
+        def saml = samlResponse(issuer(SAML_LEGACY_ISSUER) >> status() >> assertion(
+                issuer: SAML_LEGACY_ISSUER,
+                attributes: [roles: ["nova:admin"], domain: ["827319"], email: ["popeye@sailor.man"], "user/foo": ["baz"]]))
+
+        when: "the request is sent to Repose asking for an XML response"
+        def mc = deproxy.makeRequest(
+                url: reposeEndpoint + SAML_AUTH_URL,
+                method: HTTP_POST,
+                headers: [(CONTENT_TYPE): CONTENT_TYPE_FORM_URLENCODED, (ACCEPT): CONTENT_TYPE_XML],
+                requestBody: asUrlEncodedForm((PARAM_SAML_RESPONSE): encodeBase64(saml)))
+
+        then: "the origin service receives the request and the client receives the response"
+        mc.handlings[0]
+        mc.receivedResponse.code as Integer == SC_OK
+
+        when: "the response received by the client is parsed as XML"
+        def access = xmlSlurper.parseText(mc.receivedResponse.body as String)
+
+        then: "the response does not contain any extended attributes"
+        !access.'RAX-AUTH:extendedAttributes'
+
+        and: "the XML sent by the origin service and received by the client are similar enough"
+        new Diff(mc.handlings[0].response.body as String, mc.receivedResponse.body as String).similar()
     }
 }
