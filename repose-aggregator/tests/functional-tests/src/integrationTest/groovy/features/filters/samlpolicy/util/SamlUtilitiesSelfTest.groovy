645393b92efb2c6dd4e401ad1b6b51859c155105
Mario Lopez <Mario.Lopez@rackspace.com>
2017-02-03 11:04:34 -0600

    Added more SAML functional tests to verify the test SAML payloads are legit

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/util/SamlUtilitiesSelfTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/samlpolicy/util/SamlUtilitiesSelfTest.groovy
@@ -67,6 +67,34 @@ class SamlUtilitiesSelfTest extends Specification {
     }
 
     @Unroll
+    def "the signature of the SAML payload '#payloadDescription' is deemed valid '#shouldBeValid' when checked against the cert entity id '#certificateEntityId'"() {
+        when: "we unmarshall the SAML string and try to validate the first Assertion's signature"
+        Response response = samlUtilities.unmarshallResponse(saml)
+        def isValidSignature = samlUtilities.validateSignature(response.assertions[0].signature, certificateEntityId)
+
+        then: "no exceptions were thrown (SAML was successfully unmarshalled)"
+        notThrown(Exception)
+
+        and: "the signature's validity matches expectations"
+        isValidSignature == shouldBeValid
+
+        where:
+        saml                                                        | certificateEntityId       | shouldBeValid | payloadDescription
+        SAML_ONE_ASSERTION_SIGNED                                   | "idp.external.com"        | true          | "known good payload"
+        SAML_ASSERTION_AND_MESSAGE_SIGNED                           | "idp.external.com"        | true          | "known good payload with message signed too"
+        SAML_LEGACY_ISSUER_SIGNED_ASSERTION                         | "legacy.idp.external.com" | true          | "known good payload with legacy issuer"
+        SAML_LEGACY_ISSUER_SIGNED_MESSAGE_AND_ASSERTION             | "legacy.idp.external.com" | true          | "known good payload with message signed too and a legacy issuer"
+        SAML_ONE_ASSERTION_SIGNED                                   | "not.the.idp.com"         | false         | "known good payload"
+        SAML_LEGACY_ISSUER_SIGNED_ASSERTION                         | "not.the.idp.com"         | false         | "known good payload with legacy issuer"
+        SAML_ONE_ASSERTION_SIGNED.replace("    ", "")               | "idp.external.com"        | false         | "altered known good payload"
+        SAML_LEGACY_ISSUER_SIGNED_ASSERTION.replace("    ", "")     | "legacy.idp.external.com" | false         | "altered known good payload with legacy issuer"
+        SAML_ASSERTION_INVALID_SIGNATURE                            | "idp.external.com"        | false         | "known bad payload"
+        samlResponse(issuer() >> assertion(ASSERTION_SIGNED))       | "idp.external.com"        | true          | "generated saml with signature payload 1"
+        samlResponse(issuer() >> assertion(ASSERTION_SIGNED_TWO))   | "idp.external.com"        | true          | "generated saml with signature payload 2"
+        samlResponse(issuer() >> assertion(ASSERTION_SIGNED_THREE)) | "idp.external.com"        | true          | "generated saml with signature payload 3"
+    }
+
+    @Unroll
     def "SAML Utility validator will reject a SAML response with an invalid signature #testDescription"() {
         when: "we unmarshall the SAML string and try to validate the Assertion's signature"
         Response response = samlUtilities.unmarshallResponse(saml)
@@ -79,13 +107,14 @@ class SamlUtilitiesSelfTest extends Specification {
         !isValidSignature
 
         and: "the Issuer was set correctly"
-        response.issuer.value == SAML_EXTERNAL_ISSUER
+        response.issuer.value == "http://${certificateEntityId}" as String
 
         where:
-        saml                                                | testDescription
-        SAML_ONE_ASSERTION_SIGNED.replace("    ", "")       | "by tampering with the whitespace of a valid payload"
-        SAML_ASSERTION_INVALID_SIGNATURE                    | "using a known bad payload"
-        samlResponse(issuer() >> assertion(fakeSign: true)) | "generating a saml:response with an invalid signature in the Assertion"
+        saml                                                    | certificateEntityId       | testDescription
+        SAML_ONE_ASSERTION_SIGNED.replace("    ", "")           | "idp.external.com"        | "by tampering with the whitespace of a valid payload"
+        SAML_LEGACY_ISSUER_SIGNED_ASSERTION.replace("    ", "") | "legacy.idp.external.com" | "by tampering with the whitespace of a valid payload for a legacy issuer"
+        SAML_ASSERTION_INVALID_SIGNATURE                        | "idp.external.com"        | "using a known bad payload"
+        samlResponse(issuer() >> assertion(fakeSign: true))     | "idp.external.com"        | "generating a saml:response with an invalid signature in the Assertion"
     }
 
     @Unroll
