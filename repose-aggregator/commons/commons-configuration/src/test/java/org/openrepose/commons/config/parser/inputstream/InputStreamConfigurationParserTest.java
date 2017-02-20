e8d089ea8aef9e00f91851e60c9e6eae681d2247
Adrian George <adrian.george@rackspace.com>
2017-02-20 08:53:31 -0600

    Fixed the assertions, aand removed pointless code in InputStreamConfigurationParserTest

--- repose-aggregator/commons/commons-configuration/src/test/java/org/openrepose/commons/config/parser/inputstream/InputStreamConfigurationParserTest.java
+++ repose-aggregator/commons/commons-configuration/src/test/java/org/openrepose/commons/config/parser/inputstream/InputStreamConfigurationParserTest.java
@@ -19,79 +19,60 @@
  */
 package org.openrepose.commons.config.parser.inputstream;
 
-import org.junit.*;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.Test;
 import org.openrepose.commons.config.resource.ConfigurationResource;
 import org.openrepose.commons.config.resource.ResourceResolutionException;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
-@RunWith(Enclosed.class)
 public class InputStreamConfigurationParserTest {
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
+    private InputStreamConfigurationParser instance;
+    private ConfigurationResource cr;
+    private InputStream stream;
+    private ConfigurationResource badCr;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws IOException {
+        instance = new InputStreamConfigurationParser();
+
+        // good case
+        cr = mock(ConfigurationResource.class);
+        stream = mock(InputStream.class);
+        when(cr.newInputStream()).thenReturn(stream);
+
+        // bad case
+        badCr = mock(ConfigurationResource.class);
+        when(badCr.newInputStream()).thenThrow(new IOException());
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void shouldGetInputStream() throws IOException {
+        InputStream actual = instance.read(cr);
+        verify(cr, times(1)).newInputStream();
+        assertThat("Should get input stream", actual, sameInstance(stream));
     }
 
-    public static class WhenReadingResource {
-        private InputStreamConfigurationParser instance;
-        private ConfigurationResource cr;
-        private InputStream stream;
-        private ConfigurationResource badCr;
+    @Test(expected = ResourceResolutionException.class)
+    public void shouldThrowResourceResolutionException() {
+        instance.read(badCr);
+    }
 
-        @Before
-        public void setUp() throws IOException {
-            instance = new InputStreamConfigurationParser();
+    /**
+     * Test of read method, of class InputStreamConfigurationParser.
+     */
+    @Test
+    public void testRead() {
+        InputStream result = instance.read(cr);
+        assertNotNull(result);
+        // TODO review the generated test code and remove the default call to fail.
 
-            // good case
-            cr = mock(ConfigurationResource.class);
-            stream = mock(InputStream.class);
-            when(cr.newInputStream()).thenReturn(stream);
-
-            // bad case
-            badCr = mock(ConfigurationResource.class);
-            when(badCr.newInputStream()).thenThrow(new IOException());
-        }
-
-        @Test
-        public void shouldGetInputStream() throws IOException {
-            InputStream actual = instance.read(cr);
-            verify(cr, times(1)).newInputStream();
-            assertTrue("Should get input stream", actual == stream);
-        }
-
-        @Test(expected = ResourceResolutionException.class)
-        public void shouldThrowResourceResolutionException() {
-            instance.read(badCr);
-        }
-
-        /**
-         * Test of read method, of class InputStreamConfigurationParser.
-         */
-        @Test
-        public void testRead() {
-            InputStream result = instance.read(cr);
-            assertNotNull(result);
-            // TODO review the generated test code and remove the default call to fail.
-
-        }
     }
 }
