03902f564eb1448e74326662ad8179e6a5b22069
Adrian George <adrian.george@rackspace.com>
2017-02-23 10:33:50 -0600

    Made doubles in the more standard way

--- repose-aggregator/core/repose-core/src/test/java/org/openrepose/core/services/reporting/metrics/MetricsServiceImplTest.java
+++ repose-aggregator/core/repose-core/src/test/java/org/openrepose/core/services/reporting/metrics/MetricsServiceImplTest.java
@@ -138,12 +138,12 @@ public class MetricsServiceImplTest {
             tc.stop();
 
             assertEquals(1L, ((Long) getAttribute(this.getClass(), "name1", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "name1", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "name1", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
 
             t.update(1000L, TimeUnit.MILLISECONDS);
 
             assertEquals(2L, ((Long) getAttribute(this.getClass(), "name1", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "name1", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "name1", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
         }
 
         @Test
@@ -209,7 +209,7 @@ public class MetricsServiceImplTest {
             tc.stop();
 
             assertEquals(1L, ((Long) getAttribute(this.getClass(), "key1", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "key1", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "key1", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
 
             tc = t.time("key2");
             try {
@@ -219,17 +219,17 @@ public class MetricsServiceImplTest {
             tc.stop();
 
             assertEquals(1L, ((Long) getAttribute(this.getClass(), "key2", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "key2", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "key2", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
 
             t.update("key1", 1000L, TimeUnit.MILLISECONDS);
 
             assertEquals(2L, ((Long) getAttribute(this.getClass(), "key1", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "key1", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "key1", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
 
             t.update("key2", 1000L, TimeUnit.MILLISECONDS);
 
             assertEquals(2L, ((Long) getAttribute(this.getClass(), "key2", "scope1", "Count")).longValue());
-            assertThat(((Double) getAttribute(this.getClass(), "key2", "scope1", "Mean")).doubleValue(), greaterThan(0d));
+            assertThat(((Double) getAttribute(this.getClass(), "key2", "scope1", "Mean")).doubleValue(), greaterThan(0.0));
         }
 
         @Test(expected = IllegalArgumentException.class)
