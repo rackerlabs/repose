f4285bd9fdcbc12946c4a0490800da7a64c5cca1
Damien Johnson <damien.johnson@rackspace.com>
2017-02-27 13:04:52 -0600

    Fixing immediate npm issue

--- repose-aggregator/tests/release-verification/src/scripts/fake_keystone_install_docker.sh
+++ repose-aggregator/tests/release-verification/src/scripts/fake_keystone_install_docker.sh
@@ -6,6 +6,14 @@ cd /opt/fake-keystone
 echo `pwd`
 
 # download and install the Fake Keystone app dependencies
-npm install
+#
+# The --unsafe-perm flag is used to support building the libxmljs library.
+# Since there is no guarantee that libxmljs has been pre-compiled for the Docker image OS,
+# node-gyp falls back to building the library. Since the Docker user is root, gyp
+# creates a temporary directory for the build which breaks the Makefile. The flag
+# makes things work again.
+#
+# TODO: If this fails, it does so silently. WRONG!
+npm install --unsafe-perm
 
 chmod 755 /opt/fake-keystone
