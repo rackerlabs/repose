11de8f01241ce736fe52ba92c89f7fefdae8547b
Damien Johnson <damien.johnson@rackspace.com>
2017-03-20 13:14:17 -0500

    Removing unnecessary stuff from ReposeJmxNamingStrategy

--- repose-aggregator/core/repose-core/src/main/java/org/openrepose/core/spring/ReposeJmxNamingStrategy.java
+++ repose-aggregator/core/repose-core/src/main/java/org/openrepose/core/spring/ReposeJmxNamingStrategy.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,6 @@ package org.openrepose.core.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jmx.export.annotation.AnnotationJmxAttributeSource;
 import org.springframework.jmx.export.naming.MetadataNamingStrategy;
@@ -36,8 +35,8 @@ import java.net.UnknownHostException;
 import java.util.UUID;
 
 @Named
-@Lazy(true)
-public class ReposeJmxNamingStrategy extends MetadataNamingStrategy implements ObjectNamingStrategy, InitializingBean {
+@Lazy
+public class ReposeJmxNamingStrategy extends MetadataNamingStrategy implements ObjectNamingStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(ReposeJmxNamingStrategy.class);
     private static final String SEPARATOR = "-";
