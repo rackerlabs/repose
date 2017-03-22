dff083b3a40e566b5a1e73def2a353ef926c3623
wdschei <Bill.Scheidegger@Rackspace.com>
2017-03-22 10:23:48 -0500

    Added a new unit test for the Remote Datastore Configuration schema/example.

---
+++ repose-aggregator/components/services/datastore-service/datastore-service-impl/src/test/scala/org/openrepose/core/services/datastore/remote/config/RemoteDatastoreSchemaTest.scala
@@ -0,0 +1,38 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+
+package org.openrepose.core.services.datastore.remote.config
+
+import org.junit.runner.RunWith
+import org.openrepose.commons.test.ConfigValidator
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
+import org.xml.sax.SAXParseException
+
+@RunWith(classOf[JUnitRunner])
+class RemoteDatastoreSchemaTest extends FunSpec with Matchers {
+  val validator = ConfigValidator("/META-INF/schema/remote-datastore/remote-datastore.xsd")
+
+  describe("schema validation") {
+    it("should successfully validate the sample config") {
+      validator.validateConfigFile("/META-INF/schema/examples/remote-datastore.cfg.xml")
+    }
+  }
+}
