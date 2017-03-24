bef6b273622a316d08a1226fb65318981c229e2d
Damien Johnson <damien.johnson@rackspace.com>
2017-03-24 15:09:48 -0500

    Fixing DestinationRouterJMXTest

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/powerfilter/DestinationRouterJMXTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/powerfilter/DestinationRouterJMXTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,12 +27,9 @@ import org.rackspace.deproxy.Deproxy
 @Category(Slow.class)
 class DestinationRouterJMXTest extends ReposeValveTest {
 
-    String PREFIX = "\"${jmxHostname}-org.openrepose.core.filters\":type=\"DestinationRouter\",scope=\""
-    String NAME_TARGET = "\",name=\"endpoint\""
-    String NAME_TARGET_ALL = "\",name=\"ACROSS ALL\""
+    String PREFIX = "${jmxHostname}:001=\"org\",002=\"openrepose\",003=\"filters\",004=\"destinationrouter\",005=\"DestinationRouterFilter\",006=\"Routed Response\""
 
-    String DESTINATION_ROUTER_TARGET = PREFIX + "destination-router" + NAME_TARGET
-    String DESTINATION_ROUTER_ALL = PREFIX + "destination-router" + NAME_TARGET_ALL
+    String DESTINATION_ROUTER_TARGET = PREFIX + ",007=\"endpoint\""
 
     def setup() {
         def params = properties.getDefaultTemplateParams()
@@ -61,33 +58,5 @@ class DestinationRouterJMXTest extends ReposeValveTest {
 
         then:
         repose.jmx.getMBeanAttribute(DESTINATION_ROUTER_TARGET, "Count") == (target + 1)
-    }
-
-
-    def "when requests match destination router target URI, should increment DestinationRouter mbeans for all endpoints"() {
-        given:
-        def target = repose.jmx.quickMBeanAttribute(DESTINATION_ROUTER_ALL, "Count")
-        target = (target == null) ? 0 : target
-
-        when:
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint2/2"])
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint/2"])
-
-        then:
-        repose.jmx.getMBeanAttribute(DESTINATION_ROUTER_ALL, "Count") == (target + 2)
-    }
-
-    def "when requests DO NOT match destination router target URI, should NOT increment DestinationRouter mbeans for all endpoints"() {
-        given:
-        def target = repose.jmx.quickMBeanAttribute(DESTINATION_ROUTER_ALL, "Count")
-        target = (target == null) ? 0 : target
-
-
-        when:
-        deproxy.makeRequest([url: reposeEndpoint + "/non-existing"])
-        deproxy.makeRequest([url: reposeEndpoint + "/non-existing"])
-
-        then:
-        repose.jmx.quickMBeanAttribute(DESTINATION_ROUTER_ALL, "Count") == target
     }
 }
