69cd3d6d31b954e3a83d8207d61fba3244772ec7
Damien Johnson <damien.johnson@rackspace.com>
2017-03-24 16:46:59 -0500

    Fixing RequestTimeoutJMXTest

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/powerfilter/RequestTimeoutJMXTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/powerfilter/RequestTimeoutJMXTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,13 +28,7 @@ import org.rackspace.deproxy.Response
 @Category(Slow.class)
 class RequestTimeoutJMXTest extends ReposeValveTest {
 
-    String PREFIX = "\"${jmxHostname}-org.openrepose.core\":type=\"RequestTimeout\",scope=\""
-
-    String NAME_OPENREPOSE_ENDPOINT = "\",name=\"localhost:${properties.targetPort}/root_path\""
-    String ALL_ENDPOINTS = "\",name=\"All Endpoints\""
-
-    String TIMEOUT_TO_ORIGIN = PREFIX + "TimeoutToOrigin" + NAME_OPENREPOSE_ENDPOINT
-    String ALL_TIMEOUT_TO_ORIGIN = PREFIX + "TimeoutToOrigin" + ALL_ENDPOINTS
+    String MBEAN_NAME = "${jmxHostname}:001=\"org\",002=\"openrepose\",003=\"core\",004=\"RequestTimeout\",005=\"TimeoutToOrigin\",006=\"localhost:${properties.targetPort}/root_path\""
 
     def handlerTimeout = { request -> return new Response(408, 'WIZARD FAIL') }
 
@@ -50,7 +44,7 @@ class RequestTimeoutJMXTest extends ReposeValveTest {
 
     def "when responses have timed out, should increment RequestTimeout mbeans for specific endpoint"() {
         given:
-        def target = repose.jmx.quickMBeanAttribute(TIMEOUT_TO_ORIGIN, "Count")
+        def target = repose.jmx.quickMBeanAttribute(MBEAN_NAME, "Count")
         target = (target == null) ? 0 : target
 
         when:
@@ -58,35 +52,6 @@ class RequestTimeoutJMXTest extends ReposeValveTest {
         deproxy.makeRequest([url: reposeEndpoint + "/endpoint", defaultHandler: handlerTimeout])
 
         then:
-        repose.jmx.getMBeanAttribute(TIMEOUT_TO_ORIGIN, "Count") == (target + 2)
-    }
-
-
-    def "when responses have timed out, should increment RequestTimeout mbeans for all endpoint"() {
-        given:
-        def target = repose.jmx.quickMBeanAttribute(ALL_TIMEOUT_TO_ORIGIN, "Count")
-        target = (target == null) ? 0 : target
-
-        when:
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint", defaultHandler: handlerTimeout])
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint", defaultHandler: handlerTimeout])
-
-        then:
-        repose.jmx.getMBeanAttribute(ALL_TIMEOUT_TO_ORIGIN, "Count") == (target + 2)
-    }
-
-    def "when SOME responses have timed out, should increment RequestTimeout mbeans for specific endpoint only for timeouts"() {
-        given:
-        def target = repose.jmx.quickMBeanAttribute(ALL_TIMEOUT_TO_ORIGIN, "Count")
-        target = (target == null) ? 0 : target
-
-        when:
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint", defaultHandler: handlerTimeout])
-        deproxy.makeRequest([url: reposeEndpoint + "/endpoint", defaultHandler: handlerTimeout])
-        deproxy.makeRequest(url: reposeEndpoint + "/endpoint")
-        deproxy.makeRequest(url: reposeEndpoint + "/endpoint")
-
-        then:
-        repose.jmx.getMBeanAttribute(ALL_TIMEOUT_TO_ORIGIN, "Count") == (target + 2)
+        repose.jmx.getMBeanAttribute(MBEAN_NAME, "Count") == (target + 2)
     }
 }
