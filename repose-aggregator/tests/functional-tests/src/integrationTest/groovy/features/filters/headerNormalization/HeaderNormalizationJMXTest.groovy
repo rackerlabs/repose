1e9bf0f05a295f4e0d24932aa443f56adf7120b8
Mario Lopez <Mario.Lopez@rackspace.com>
2017-04-04 16:05:26 -0500

    Updated HeaderNormalizationJMXTest to use the new methods in JmxClient

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/headerNormalization/HeaderNormalizationJMXTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/headerNormalization/HeaderNormalizationJMXTest.groovy
@@ -28,7 +28,7 @@ import spock.lang.Unroll
 @Category(Slow.class)
 class HeaderNormalizationJMXTest extends ReposeValveTest {
 
-    private static final KEY_PROPERTIES_PREFIX =
+    private static final String KEY_PROPERTIES_PREFIX =
         /001="org",002="openrepose",003="filters",004="headernormalization",005="HeaderNormalizationFilter",006="Normalization"/
 
     private static Map params
@@ -72,57 +72,57 @@ class HeaderNormalizationJMXTest extends ReposeValveTest {
         deproxy.makeRequest(url: reposeEndpoint, method: "GET")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootGet, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootGet) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 1
 
         when:
         deproxy.makeRequest(url: reposeEndpoint, method: "POST")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootPost, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 2
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootPost) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 2
 
         when:
         deproxy.makeRequest(url: reposeEndpoint, method: "PUT")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootPut, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 3
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootPut) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 3
 
         when:
         deproxy.makeRequest(url: "$reposeEndpoint/resource/1243/", method: "POST")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormResourcePost, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 4
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormResourcePost) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 4
 
         when:
         deproxy.makeRequest(url: "$reposeEndpoint/resource/1243/", method: "PUT")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormResourcePut, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 5
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormResourcePut) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 5
 
         when:
         deproxy.makeRequest(url: "$reposeEndpoint/servers/1243/", method: "GET")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormServersGet, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 6
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormServersGet) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 6
 
         when:
         deproxy.makeRequest(url: "$reposeEndpoint/servers/1243/", method: "POST")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormServersPost, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 7
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormServersPost) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 7
 
         when:
         deproxy.makeRequest(url: "$reposeEndpoint/servers/1243/", method: "PUT")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormServersPut, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 8
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormServersPut) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 8
 
         where:
         reqres << ["request", "response"]
@@ -143,27 +143,27 @@ class HeaderNormalizationJMXTest extends ReposeValveTest {
         deproxy.makeRequest(url: reposeEndpoint)
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootGet, "Count") == 1
-        (repose.jmx.quickMBeanAttribute(headerNormSecondaryPathGet, "Count") ?: 0) == 0
-        (repose.jmx.quickMBeanAttribute(headerNormTertiaryPathGet, "Count") ?: 0) == 0
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootGet) == 1
+        repose.jmx.getMBeanCountAttribute(headerNormSecondaryPathGet) == 0
+        repose.jmx.getMBeanCountAttribute(headerNormTertiaryPathGet) == 0
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 1
 
         when: "client makes a request that matches filters one and two's uri-regex attributes"
         deproxy.makeRequest(url: "$reposeEndpoint/secondary/path/asdf")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootGet, "Count") == 2
-        repose.jmx.getMBeanAttribute(headerNormSecondaryPathGet, "Count") == 1
-        (repose.jmx.quickMBeanAttribute(headerNormTertiaryPathGet, "Count") ?: 0) == 0
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 3
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootGet) == 2
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormSecondaryPathGet) == 1
+        repose.jmx.getMBeanCountAttribute(headerNormTertiaryPathGet) == 0
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 3
 
         when: "client makes a request that matches filters one and three's uri-regex attributes"
         deproxy.makeRequest(url: "$reposeEndpoint/tertiary/path/asdf")
 
         then:
-        repose.jmx.getMBeanAttribute(headerNormRootGet, "Count") == 2
-        repose.jmx.getMBeanAttribute(headerNormSecondaryPathGet, "Count") == 1
-        repose.jmx.getMBeanAttribute(headerNormTertiaryPathGet, "Count") == 2
-        repose.jmx.getMBeanAttribute(headerNormAll, "Count") == 5
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormRootGet) == 2
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormSecondaryPathGet) == 1
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormTertiaryPathGet) == 2
+        repose.jmx.getMBeanCountAttributeWithWaitForNonZero(headerNormAll) == 5
     }
 }
