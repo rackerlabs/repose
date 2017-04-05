b3d746f71c1ed4694e304eb2826df6cc2cf84618
Damien Johnson <damien.johnson@rackspace.com>
2017-04-05 13:54:32 -0500

    Small cleanup of ServerHeaderTest

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/headers/ServerHeaderTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/headers/ServerHeaderTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,8 +19,6 @@
  */
 package features.core.headers
 
-import framework.ReposeConfigurationProvider
-import framework.ReposeValveLauncher
 import framework.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.MessageChain
@@ -31,55 +29,36 @@ import spock.lang.Unroll
 
 class ServerHeaderTest extends ReposeValveTest {
 
-    static int originServicePort
-    static int reposePort
-    static String url
-    static ReposeConfigurationProvider reposeConfigProvider
-
     def setupSpec() {
         deproxy = new Deproxy()
-        originServicePort = properties.targetPort
-        deproxy.addEndpoint(originServicePort)
-
-        reposePort = properties.reposePort
-        url = "http://localhost:${reposePort}"
-
-        reposeConfigProvider = new ReposeConfigurationProvider(configDirectory, configTemplates)
-        repose = new ReposeValveLauncher(
-                reposeConfigProvider,
-                properties.getReposeJar(),
-                url,
-                properties.getConfigDirectory(),
-                reposePort)
-        repose.enableDebug()
+        deproxy.addEndpoint(properties.targetPort)
 
         def params = properties.getDefaultTemplateParams()
-
-        reposeConfigProvider.cleanConfigDirectory()
-        reposeConfigProvider.applyConfigs("common", params)
-        reposeConfigProvider.applyConfigs("features/core/headers", params)
+        repose.configurationProvider.cleanConfigDirectory()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/core/headers", params)
 
         repose.start(
-                killOthersBeforeStarting: false,
-                waitOnJmxAfterStarting: false)
+            killOthersBeforeStarting: false,
+            waitOnJmxAfterStarting: false)
 
-        repose.waitForNon500FromUrl(url)
+        repose.waitForNon500FromUrl(reposeEndpoint)
     }
 
     @Unroll("The received response should have the Server header value of \"#headerValue\"")
     def "The received response should have the Server header value of \"#headerValue\""() {
-
         given: "a set of Origin Service headers"
         def headers = [
-                'Content-Length': '0'
+            'Content-Length': '0'
         ]
         Optional.ofNullable(headerValue).ifPresent {
-            value -> headers[HttpHeaders.SERVER] = value }
+            value -> headers[HttpHeaders.SERVER] = value
+        }
 
         when: "a request is made"
         MessageChain mc = deproxy.makeRequest(
-                url: url,
-                defaultHandler: { new Response(HttpStatus.OK, null, headers) })
+            url: reposeEndpoint,
+            defaultHandler: { new Response(HttpStatus.OK, null, headers) })
 
         then: "it should make it to the origin service"
         mc.handlings.size() == 1
