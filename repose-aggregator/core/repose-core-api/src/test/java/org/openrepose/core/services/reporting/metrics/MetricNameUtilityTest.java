9ee79b98cfc6b0bde471390fac43564f19d34e47
Damien Johnson <damien.johnson@rackspace.com>
2017-04-05 16:28:25 -0500

    Adding a MetricNameUtility

---
+++ repose-aggregator/core/repose-core-api/src/test/java/org/openrepose/core/services/reporting/metrics/MetricNameUtilityTest.java
@@ -0,0 +1,46 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.core.services.reporting.metrics;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+public class MetricNameUtilityTest {
+
+    @Test
+    public void safeReportingNameShouldNotModifyASafeName() throws Exception {
+        String name = "myName";
+
+        String safeName = MetricNameUtility.safeReportingName(name);
+
+        assertThat(safeName, equalTo(name));
+    }
+
+    @Test
+    public void safeReportingNameShouldReplaceUnsafeCharacters() throws Exception {
+        String name = "/a/.+/.*";
+
+        String safeName = MetricNameUtility.safeReportingName(name);
+
+        assertThat(safeName, equalTo(name.replace('.', '_')));
+    }
+}
