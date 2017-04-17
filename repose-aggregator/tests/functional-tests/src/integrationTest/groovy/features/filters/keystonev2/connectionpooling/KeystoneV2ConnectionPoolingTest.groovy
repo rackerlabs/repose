d9b552ce37857e899cc57ba523cad52b925443f8
Adrian George <adrian.george@rackspace.com>
2017-04-17 13:35:56 -0500

    Updated Keystone v2 connection pool test to repose valve test

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/keystonev2/connectionpooling/KeystoneV2ConnectionPoolingTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/keystonev2/connectionpooling/KeystoneV2ConnectionPoolingTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,26 +19,18 @@
  */
 package features.filters.keystonev2.connectionpooling
 
-import framework.ReposeConfigurationProvider
-import framework.ReposeValveLauncher
-import framework.TestProperties
+import framework.ReposeValveTest
 import framework.mocks.MockIdentityV2Service
 import org.rackspace.deproxy.Deproxy
 import org.rackspace.deproxy.Endpoint
 import org.rackspace.deproxy.Handling
-import spock.lang.Specification
 
 /**
  * Created with IntelliJ IDEA.
  * User: izrik
  *
  */
-class KeystoneV2ConnectionPoolingTest extends Specification {
-
-    int reposePort
-    int originServicePort
-    int identityServicePort
-    String urlBase
+class KeystoneV2ConnectionPoolingTest extends ReposeValveTest {
 
     MockIdentityV2Service fakeIdentityV2Service
 
@@ -46,57 +38,28 @@ class KeystoneV2ConnectionPoolingTest extends Specification {
     Endpoint originEndpoint
     Endpoint identityEndpoint
 
-    TestProperties properties
-    def logFile
-    ReposeConfigurationProvider reposeConfigProvider
-    ReposeValveLauncher repose
-
     def setup() {
 
-        // get ports
-        properties = new TestProperties()
-        reposePort = properties.reposePort
-        originServicePort = properties.targetPort
-        identityServicePort = properties.identityPort
-
-        fakeIdentityV2Service = new MockIdentityV2Service(identityServicePort, originServicePort)
+        fakeIdentityV2Service = new MockIdentityV2Service(properties.identityPort, properties.targetPort)
 
         // start deproxy
         deproxy = new Deproxy()
-        originEndpoint = deproxy.addEndpoint(originServicePort)
-        identityEndpoint = deproxy.addEndpoint(identityServicePort,
+        originEndpoint = deproxy.addEndpoint(properties.targetPort)
+        identityEndpoint = deproxy.addEndpoint(properties.identityPort,
                 "identity", "localhost", fakeIdentityV2Service.handler)
 
         // configure and start repose
-
-        def targetHostname = properties.targetHostname
-        urlBase = "http://${targetHostname}:${reposePort}"
-        logFile = properties.logFile
-
-        def configDirectory = properties.configDirectory
-        def configTemplates = properties.configTemplates
-        reposeConfigProvider = new ReposeConfigurationProvider(configDirectory, configTemplates)
-
-        repose = new ReposeValveLauncher(
-                reposeConfigProvider,
-                properties.reposeJar,
-                urlBase,
-                configDirectory,
-                reposePort
-        )
-        repose.enableDebug()
-
         def params = properties.getDefaultTemplateParams()
-        reposeConfigProvider.applyConfigs("common", params)
-        reposeConfigProvider.applyConfigs("features/filters/keystonev2/connectionpooling", params)
-        reposeConfigProvider.applyConfigs("features/filters/keystonev2/connectionpooling2", params)
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/keystonev2/connectionpooling", params)
+        repose.configurationProvider.applyConfigs("features/filters/keystonev2/connectionpooling2", params)
         repose.start()
     }
 
     def "when a client makes requests, Repose should re-use the connection to the Identity service"() {
 
         setup: "craft an url to a resource that requires authentication"
-        def url = "${urlBase}/servers/tenantid/resource"
+        def url = "${reposeEndpoint}/servers/tenantid/resource"
 
 
         when: "making two authenticated requests to Repose"
@@ -120,16 +83,5 @@ class KeystoneV2ConnectionPoolingTest extends Specification {
         // REP-3577 - add header using connection pool
         mc1.orphanedHandlings.get(0).request.headers.contains("auth-proxy")
         mc1.orphanedHandlings.get(0).request.headers.getFirstValue("auth-proxy") == "testing"
-    }
-
-    def cleanup() {
-
-        if (repose && repose.isUp()) {
-            repose.stop()
-        }
-
-        if (deproxy) {
-            deproxy.shutdown()
-        }
     }
 }
