8fd8f382ccaa1af9711c669ccd0db76b319b4262
Adrian George <adrian.george@rackspace.com>
2017-04-19 08:42:48 -0500

    Updated TomcatProxyTest to use new config directories

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/proxy/TomcatProxyTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/proxy/TomcatProxyTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ class TomcatProxyTest extends Specification {
 
     def setupSpec() {
 
-        def TestProperties properties = new TestProperties()
+        TestProperties properties = new TestProperties(this.getClass().canonicalName.replace('.', '/'))
 
         int originServicePort = properties.targetPort
         deproxy = new Deproxy()
@@ -47,7 +47,6 @@ class TomcatProxyTest extends Specification {
         def configDirectory = properties.getConfigDirectory()
         def configTemplates = properties.getConfigTemplates()
         def rootWar = properties.getReposeRootWar()
-        def buildDirectory = properties.getReposeHome() + "/.."
         ReposeConfigurationProvider config = new ReposeConfigurationProvider(configDirectory, configTemplates)
 
         def params = properties.getDefaultTemplateParams()
