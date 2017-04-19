785b070d19eaec2086ec793937644c8cd6466279
Adrian George <adrian.george@rackspace.com>
2017-04-19 09:29:54 -0500

    Updated RuntimeSysmodChangesTest to repose valve test

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/valveSelfConfigure/RuntimeSysmodChangesTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/core/valveSelfConfigure/RuntimeSysmodChangesTest.groovy
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,29 +19,18 @@
  */
 package features.core.valveSelfConfigure
 
-import framework.ReposeConfigurationProvider
-import framework.ReposeValveLauncher
-import framework.TestProperties
+import framework.PortFinder
+import framework.ReposeValveTest
 import framework.category.Slow
 import org.junit.experimental.categories.Category
 import org.rackspace.deproxy.Deproxy
-import org.rackspace.deproxy.Endpoint
-import framework.PortFinder
-import spock.lang.Specification
 
 @Category(Slow.class)
-class RuntimeSysmodChangesTest extends Specification {
-
-    int targetPort
-    Deproxy deproxy
-    Endpoint endpoint
+class RuntimeSysmodChangesTest extends ReposeValveTest {
 
     int port1
     int port2
     int port3
-    TestProperties properties
-    ReposeConfigurationProvider reposeConfigProvider
-    ReposeValveLauncher repose
 
     //This must be longer than the 15 second hard coded Poller timeout
     //TODO: eventually replace this with a JMX trigger to force a configuration update.
@@ -49,17 +38,12 @@ class RuntimeSysmodChangesTest extends Specification {
 
     def setup() {
 
-        properties = new TestProperties()
-
-        targetPort = properties.targetPort
         deproxy = new Deproxy()
-        endpoint = deproxy.addEndpoint(targetPort)
+        deproxy.addEndpoint(properties.targetPort)
 
         port1 = properties.reposePort
         port2 = PortFinder.instance.getNextOpenPort()
         port3 = PortFinder.instance.getNextOpenPort()
-
-        reposeConfigProvider = new ReposeConfigurationProvider(properties.getConfigDirectory(), properties.getConfigTemplates())
 
         def params = properties.defaultTemplateParams
         params += [
@@ -68,24 +52,15 @@ class RuntimeSysmodChangesTest extends Specification {
                 'port3'     : port3,
 
                 'proto'     : 'http',
-                'targetPort': targetPort,
                 'sysmodPort': port1,
 
         ]
-        reposeConfigProvider.cleanConfigDirectory()
+        repose.configurationProvider.cleanConfigDirectory()
 
-        reposeConfigProvider.applyConfigs("common", params)
-        reposeConfigProvider.applyConfigs("features/core/valveSelfConfigure/common", params)
-        reposeConfigProvider.applyConfigs("features/core/valveSelfConfigure/container-no-port", params)
-        reposeConfigProvider.applyConfigs("features/core/valveSelfConfigure/single-node-with-proto", params)
-        repose = new ReposeValveLauncher(
-                reposeConfigProvider,
-                properties.getReposeJar(),
-                "http://localhost:${port1}",
-                properties.getConfigDirectory(),
-                port1
-        )
-        repose.enableDebug()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/core/valveSelfConfigure/common", params)
+        repose.configurationProvider.applyConfigs("features/core/valveSelfConfigure/container-no-port", params)
+        repose.configurationProvider.applyConfigs("features/core/valveSelfConfigure/single-node-with-proto", params)
         repose.start(killOthersBeforeStarting: false,
                 waitOnJmxAfterStarting: false)
         repose.waitForNon500FromUrl("http://localhost:${port1}")
@@ -116,13 +91,12 @@ class RuntimeSysmodChangesTest extends Specification {
         when: "change the configs while it's running - two nodes"
         def params = properties.getDefaultTemplateParams()
         params += [
-                'targetPort': targetPort,
                 'node1host' : 'localhost',
                 'node2host' : 'localhost',
                 'node1port' : port1,
                 'node2port' : port2,
         ]
-        reposeConfigProvider.applyConfigs('features/core/valveSelfConfigure/two-nodes', params)
+        repose.configurationProvider.applyConfigs('features/core/valveSelfConfigure/two-nodes', params)
         println("Change config to two-nodes")
         sleep(sleep_duration)
         repose.waitForNon500FromUrl("http://localhost:${port1}")
@@ -154,10 +128,9 @@ class RuntimeSysmodChangesTest extends Specification {
         params = properties.getDefaultTemplateParams()
         params += [
                 'proto'     : 'http',
-                'targetPort': targetPort,
                 'sysmodPort': port2,
         ]
-        reposeConfigProvider.applyConfigs('features/core/valveSelfConfigure/single-node-with-proto', params)
+        repose.configurationProvider.applyConfigs('features/core/valveSelfConfigure/single-node-with-proto', params)
         println("changed configs to single-node-with-proto")
         sleep(sleep_duration)
         repose.waitForNon500FromUrl("http://localhost:${port2}")
@@ -187,7 +160,6 @@ class RuntimeSysmodChangesTest extends Specification {
         when: "change the configs while it's running - two of three nodes"
         params = properties.getDefaultTemplateParams()
         params += [
-                'targetPort': targetPort,
                 'node1host' : 'localhost',
                 'node2host' : 'localhost',
                 'node3host' : 'example.com',
@@ -195,7 +167,7 @@ class RuntimeSysmodChangesTest extends Specification {
                 'node2port' : port2,
                 'node3port' : port3,
         ]
-        reposeConfigProvider.applyConfigs('features/core/valveSelfConfigure/three-nodes', params)
+        repose.configurationProvider.applyConfigs('features/core/valveSelfConfigure/three-nodes', params)
         println("changed to three-nodes config")
         sleep(sleep_duration)
         repose.waitForNon500FromUrl("http://localhost:${port1}")
@@ -226,7 +198,6 @@ class RuntimeSysmodChangesTest extends Specification {
         when: "change the configs while it's running - two of three nodes again, but different hostnames"
         params = properties.getDefaultTemplateParams()
         params += [
-                'targetPort': targetPort,
                 'node1host' : 'example.com',
                 'node2host' : 'localhost',
                 'node3host' : 'localhost',
@@ -234,7 +205,7 @@ class RuntimeSysmodChangesTest extends Specification {
                 'node2port' : port2,
                 'node3port' : port3,
         ]
-        reposeConfigProvider.applyConfigs('features/core/valveSelfConfigure/three-nodes', params)
+        repose.configurationProvider.applyConfigs('features/core/valveSelfConfigure/three-nodes', params)
         println("changed to three-nodes config again, but a different hostname")
         sleep(sleep_duration)
         repose.waitForNon500FromUrl("http://localhost:${port2}")
@@ -260,15 +231,5 @@ class RuntimeSysmodChangesTest extends Specification {
         then: "node 3 should be available"
         mc.receivedResponse.code == "200"
         mc.handlings.size() == 1
-    }
-
-    def cleanup() {
-
-        if (repose) {
-            repose.stop()
-        }
-        if (deproxy) {
-            deproxy.shutdown()
-        }
     }
 }
