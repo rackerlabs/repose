503563dfce4affba12230123d8d30272244290b5
Mario Lopez <Mario.Lopez@rackspace.com>
2017-05-03 18:53:27 -0500

    Updated Jenkins test script failure rate to be based off of the build number

--- test.sh
+++ test.sh
@@ -2,9 +2,8 @@
 
 echo "This is the perf_test param: $perf_test"
 echo "This is the extra_vars param: $extra_vars"
-for i in $(seq 5); do echo "$i "; sleep 1; done
 
-if [ "$(( RANDOM % 3 ))" -eq "0" ]; then
+if [ "$(( BUILD_NUMBER % 2 ))" -eq "0" ]; then
    echo "I feel like failing.";
    exit 1;
 fi
