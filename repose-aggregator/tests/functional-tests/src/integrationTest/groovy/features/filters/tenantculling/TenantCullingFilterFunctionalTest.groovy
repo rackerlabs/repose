af2b07f99583c20b2b09db679a71250f133a2ead
wdschei <Bill.Scheidegger@Rackspace.com>
2017-08-31 15:04:37 -0500

    Updated to reference the main Repose project directly and to have the correct package names and headers.

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/tenantculling/TenantCullingFilterFunctionalTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/tenantculling/TenantCullingFilterFunctionalTest.groovy
@@ -1,4 +1,23 @@
-package com.rackspace.lefty.tenant
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.filters.tenantculling
 
 import groovy.json.JsonBuilder
 import org.apache.commons.lang3.RandomStringUtils
@@ -45,7 +64,7 @@ class TenantCullingFilterFunctionalTest extends ReposeValveTest {
         deproxy.addEndpoint(properties.targetPort as Integer, 'origin service')
         def params = properties.defaultTemplateParams
         repose.configurationProvider.applyConfigs('common', params)
-        repose.configurationProvider.applyConfigs('withKeystone', params)
+        repose.configurationProvider.applyConfigs('features/filters/tenantculling/withKeystone', params)
         repose.start()
 
         fakeIdentityService = new MockIdentityV2Service(properties.identityPort as Integer, properties.targetPort as Integer)
