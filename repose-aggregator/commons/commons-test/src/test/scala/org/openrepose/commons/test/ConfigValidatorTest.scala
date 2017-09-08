cffae42ebbb740618d5b9ea0d65d404cd79637b9
Damien Johnson <damien.johnson@rackspace.com>
2017-09-08 09:42:48 -0500

    Updating the ConfigValidator test utility to support coming changes

--- repose-aggregator/commons/commons-test/src/test/scala/org/openrepose/commons/test/ConfigValidatorTest.scala
+++ repose-aggregator/commons/commons-test/src/test/scala/org/openrepose/commons/test/ConfigValidatorTest.scala
@@ -19,13 +19,17 @@
  */
 package org.openrepose.commons.test
 
+import java.io.InputStream
+import java.net.URL
+
 import org.junit.runner.RunWith
-import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
 import org.xml.sax.SAXParseException
 
 @RunWith(classOf[JUnitRunner])
 class ConfigValidatorTest extends FunSpec with Matchers {
+  import ConfigValidatorTest._
 
   describe("validating a config string") {
     it("should return normally if the config string is valid against the schema") {
@@ -36,7 +40,18 @@ class ConfigValidatorTest extends FunSpec with Matchers {
                      |    <header original-name="Content-Type" new-name="rax-content-type" quality="0.9" splittable="true"/>
                      |    <header original-name="Content-Length" new-name="rax-content-length"/>
                      |</header-translation>""".stripMargin
-      ConfigValidator("/schema-good.xsd").validateConfigString(config)
+      ConfigValidator(GoodSchemaResourceName).validateConfigString(config)
+    }
+
+    it("should return normally if the config string is valid against the schema URL") {
+      val config = """<?xml version="1.0" encoding="UTF-8"?>
+                     |<header-translation xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     |                    xsi:schemaLocation="http://docs.openrepose.org/repose/header-translation/v1.0"
+                     |                    xmlns="http://docs.openrepose.org/repose/header-translation/v1.0">
+                     |    <header original-name="Content-Type" new-name="rax-content-type" quality="0.9" splittable="true"/>
+                     |    <header original-name="Content-Length" new-name="rax-content-length"/>
+                     |</header-translation>""".stripMargin
+      ConfigValidator(GoodSchemaUrl).validateConfigString(config)
     }
 
     it("should throw an exception if the config string is NOT valid against the schema") {
@@ -48,7 +63,21 @@ class ConfigValidatorTest extends FunSpec with Matchers {
                      |    <header original-name="content-Length" new-name="rax-content-length"/>
                      |</header-translation>""".stripMargin
       val exception = intercept[SAXParseException] {
-        ConfigValidator("/schema-good.xsd").validateConfigString(config)
+        ConfigValidator(GoodSchemaResourceName).validateConfigString(config)
+      }
+      exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
+    }
+
+    it("should throw an exception if the config string is NOT valid against the schema URL") {
+      val config = """<?xml version="1.0" encoding="UTF-8"?>
+                     |<header-translation xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     |                    xsi:schemaLocation="http://docs.openrepose.org/repose/header-translation/v1.0"
+                     |                    xmlns="http://docs.openrepose.org/repose/header-translation/v1.0">
+                     |    <header original-name="Content-Length" new-name="rax-content-type" quality="0.9" splittable="true"/>
+                     |    <header original-name="content-Length" new-name="rax-content-length"/>
+                     |</header-translation>""".stripMargin
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaUrl).validateConfigString(config)
       }
       exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
     }
@@ -56,19 +85,75 @@ class ConfigValidatorTest extends FunSpec with Matchers {
 
   describe("validating a config file") {
     it("should return normally if the config file is valid against the schema") {
-      ConfigValidator("/schema-good.xsd").validateConfigFile("/config-good.xml")
+      ConfigValidator(GoodSchemaResourceName).validateConfigFile("/config-good.xml")
+    }
+
+    it("should return normally if the config file is valid against the schema URL") {
+      ConfigValidator(GoodSchemaUrl).validateConfigFile("/config-good.xml")
     }
 
     it("should throw an exception if the config file is NOT valid against the schema due to an assertion") {
       val exception = intercept[SAXParseException] {
-        ConfigValidator("/schema-good.xsd").validateConfigFile("/config-bad-assertion.xml")
+        ConfigValidator(GoodSchemaResourceName).validateConfigFile("/config-bad-assertion.xml")
+      }
+      exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema URL due to an assertion") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaUrl).validateConfigFile("/config-bad-assertion.xml")
       }
       exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
     }
 
     it("should throw an exception if the config file is NOT valid against the schema due to a type restriction") {
       val exception = intercept[SAXParseException] {
-        ConfigValidator("/schema-good.xsd").validateConfigFile("/config-bad-type-restriction.xml")
+        ConfigValidator(GoodSchemaResourceName).validateConfigFile("/config-bad-type-restriction.xml")
+      }
+      exception.getLocalizedMessage should include ("Value '1.1' is not facet-valid with respect to maxInclusive '1.0E0' for type 'doubleBetweenZeroAndOne'")
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema URL due to a type restriction") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaUrl).validateConfigFile("/config-bad-type-restriction.xml")
+      }
+      exception.getLocalizedMessage should include ("Value '1.1' is not facet-valid with respect to maxInclusive '1.0E0' for type 'doubleBetweenZeroAndOne'")
+    }
+  }
+
+  describe("validating a config source") {
+    it("should return normally if the config file is valid against the schema") {
+      ConfigValidator(GoodSchemaResourceName).validateConfig(stringResourceToInputStream("/config-good.xml"))
+    }
+
+    it("should return normally if the config file is valid against the schema URL") {
+      ConfigValidator(GoodSchemaUrl).validateConfig(stringResourceToInputStream("/config-good.xml"))
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema due to an assertion") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaResourceName).validateConfig(stringResourceToInputStream("/config-bad-assertion.xml"))
+      }
+      exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema URL due to an assertion") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaUrl).validateConfig(stringResourceToInputStream("/config-bad-assertion.xml"))
+      }
+      exception.getLocalizedMessage should include ("Original names must be unique. Evaluation is case insensitive.")
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema due to a type restriction") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaResourceName).validateConfig(stringResourceToInputStream("/config-bad-type-restriction.xml"))
+      }
+      exception.getLocalizedMessage should include ("Value '1.1' is not facet-valid with respect to maxInclusive '1.0E0' for type 'doubleBetweenZeroAndOne'")
+    }
+
+    it("should throw an exception if the config file is NOT valid against the schema URL due to a type restriction") {
+      val exception = intercept[SAXParseException] {
+        ConfigValidator(GoodSchemaUrl).validateConfig(stringResourceToInputStream("/config-bad-type-restriction.xml"))
       }
       exception.getLocalizedMessage should include ("Value '1.1' is not facet-valid with respect to maxInclusive '1.0E0' for type 'doubleBetweenZeroAndOne'")
     }
@@ -78,5 +163,16 @@ class ConfigValidatorTest extends FunSpec with Matchers {
     it("will not throw an exception if the schema has an invalid assertion") {
       ConfigValidator("/schema-bad.xsd").validateConfigFile("/config-bad-assertion.xml")
     }
+
+    it("will not throw an exception if the schema source has an invalid assertion") {
+      ConfigValidator(classOf[ConfigValidatorTest].getResource("/schema-bad.xsd")).validateConfigFile("/config-bad-assertion.xml")
+    }
   }
+}
+
+object ConfigValidatorTest {
+  final val GoodSchemaResourceName: String = "/schema-good.xsd"
+  final val GoodSchemaUrl: URL = classOf[ConfigValidatorTest].getResource(GoodSchemaResourceName)
+
+  def stringResourceToInputStream(path: String): InputStream = classOf[ConfigValidatorTest].getResourceAsStream(path)
 }
