d154fba934552c4573d6c164e470dc5703bc4d4b
Damien Johnson <damien.johnson@rackspace.com>
2017-09-20 12:24:19 -0500

    Using any mapped value in createAccessJsonWithValues
    This is a semantic change to that creation method. This change allows a user to provide Groovy false-ish values to be used during creation.

--- repose-aggregator/tests/functional-test-framework/src/main/groovy/org/openrepose/framework/test/mocks/MockIdentityV2Service.groovy
+++ repose-aggregator/tests/functional-test-framework/src/main/groovy/org/openrepose/framework/test/mocks/MockIdentityV2Service.groovy
@@ -793,12 +793,12 @@ class MockIdentityV2Service {
     }
 
     String createAccessJsonWithValues(Map values = [:]) {
-        def token = values.token ?: client_token
-        def expires = values.expires ?: getExpires()
-        def tenantId = values.tenantId ?: client_tenantid
-        def userId = values.userId ?: client_userid
-        def username = values.username ?: client_username
-        def roles = (values.roles ?: [[name: "identity:admin"]])
+        def token = values.getOrDefault('token', client_token)
+        def expires = values.getOrDefault('expires', getExpires())
+        def tenantId = values.getOrDefault('tenantId', client_tenantid)
+        def userId = values.getOrDefault('userId', client_userid)
+        def username = values.getOrDefault('username', client_username)
+        def roles = values.getOrDefault('roles', [[name: "identity:admin"]])
 
         def json = new JsonBuilder()
 
