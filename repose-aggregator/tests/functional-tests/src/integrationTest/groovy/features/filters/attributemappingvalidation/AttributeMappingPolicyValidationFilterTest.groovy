b78ed2bacb3eba3e969a0a1970e5e8f18ef4083d
wdschei <Bill.Scheidegger@Rackspace.com>
2017-10-18 15:35:39 -0500

    Cleaned up some of the formatting on the Attribute Mapping Policy Validation Filter functional tests.

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/attributemappingvalidation/AttributeMappingPolicyValidationFilterTest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/attributemappingvalidation/AttributeMappingPolicyValidationFilterTest.groovy
@@ -26,7 +26,7 @@ import org.rackspace.deproxy.MessageChain
 import org.yaml.snakeyaml.Yaml
 import spock.lang.Unroll
 
-import static javax.servlet.http.HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE
+import static javax.servlet.http.HttpServletResponse.*
 import static javax.ws.rs.core.MediaType.*
 
 /**
@@ -90,32 +90,32 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
         given:
         String body =
             """<?xml version="1.0" encoding="UTF-8"?>
-                <mapping xmlns="http://docs.rackspace.com/identity/api/ext/MappingRules"
-                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                         xmlns:xs="http://www.w3.org/2001/XMLSchema"
-                         xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion"
-                         version="RAX-1">
-                   <rules>
-                      <rule>
-                        <local>
-                            <user>
-                               <name value="{D}"/>
-                               <email value="{D}"/>
-                               <expire value="{D}"/>
-                               <domain value="{D}"/>
-                               <roles value="{D}"/>
-                            </user>
-                         </local>
-                      </rule>
-                   </rules>
-                </mapping>
-            """
+            |<mapping xmlns="http://docs.rackspace.com/identity/api/ext/MappingRules"
+            |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            |         xmlns:xs="http://www.w3.org/2001/XMLSchema"
+            |         xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion"
+            |         version="RAX-1">
+            |   <rules>
+            |      <rule>
+            |        <local>
+            |            <user>
+            |               <name value="{D}"/>
+            |               <email value="{D}"/>
+            |               <expire value="{D}"/>
+            |               <domain value="{D}"/>
+            |               <roles value="{D}"/>
+            |            </user>
+            |         </local>
+            |      </rule>
+            |   </rules>
+            |</mapping>
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": APPLICATION_XML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         xmlSlurper.parseText(body) == xmlSlurper.parseText(mc.handlings[0].request.body as String)
     }
@@ -123,31 +123,30 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
     def "should validate correct JSON"() {
         given:
         String body =
-            """
-            {
-              "mapping": {
-                "rules": [
-                  {
-                    "local": {
-                      "user": {
-                        "domain": "{D}",
-                        "name": "{D}",
-                        "email": "{D}",
-                        "expire": "{D}"
-                      }
-                    }
-                  }
-                ],
-                "version":"RAX-1"
-              }
-            }
-            """
+            """{
+            |  "mapping": {
+            |    "rules": [
+            |      {
+            |        "local": {
+            |          "user": {
+            |            "domain": "{D}",
+            |            "name": "{D}",
+            |            "email": "{D}",
+            |            "expire": "{D}"
+            |          }
+            |        }
+            |      }
+            |    ],
+            |    "version":"RAX-1"
+            |  }
+            |}
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": APPLICATION_JSON], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         jsonSlurper.parseText(body) == jsonSlurper.parseText(mc.handlings[0].request.body as String)
     }
@@ -155,8 +154,7 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
     def "should validate correct YAML"() {
         given:
         String body =
-            """
-            |---
+            """---
             |mapping:
             |  rules:
             |  - local:
@@ -167,13 +165,13 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
             |        roles: "{D}"
             |        expire: "{D}"
             |  version: RAX-1
-            """.stripMargin()
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": TEXT_YAML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         yaml.load(body) == yaml.load(mc.handlings[0].request.body as String)
     }
@@ -181,35 +179,34 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
     def "should not remove the name attribute from a remote in a JSON policy"() {
         given:
         String body =
-            """
-            {
-              "mapping": {
-                "rules": [
-                  {
-                    "local": {
-                      "user": {
-                        "name": "{D}"
-                      }
-                    },
-                    "remote": [
-                      {
-                        "multiValue": false,
-                        "name": "Username",
-                        "regex": false
-                      }
-                    ]
-                  }
-                ],
-                "version":"RAX-1"
-              }
-            }
-            """
+            """{
+            |  "mapping": {
+            |    "rules": [
+            |      {
+            |        "local": {
+            |          "user": {
+            |            "name": "{D}"
+            |          }
+            |        },
+            |        "remote": [
+            |          {
+            |            "multiValue": false,
+            |            "name": "Username",
+            |            "regex": false
+            |          }
+            |        ]
+            |      }
+            |    ],
+            |    "version":"RAX-1"
+            |  }
+            |}
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": APPLICATION_JSON], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         jsonSlurper.parseText(body) == jsonSlurper.parseText(mc.handlings[0].request.body as String)
     }
@@ -217,8 +214,7 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
     def "should not remove the name attribute from a remote in a YAML policy"() {
         given:
         String body =
-            """
-            |---
+            """---
             |mapping:
             |  rules:
             |  - local:
@@ -229,13 +225,13 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
             |      name: Username
             |      regex: false
             |  version: RAX-1
-            """.stripMargin()
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": TEXT_YAML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         yaml.load(body) == yaml.load(mc.handlings[0].request.body as String)
     }
@@ -244,70 +240,68 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
         given:
         String body =
             """<?xml version="1.0" encoding="UTF-8"?>
-                <mapping xmlns="http://docs.rackspace.com/identity/api/ext/MappingRules"
-                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                         xmlns:xs="http://www.w3.org/2001/XMLSchema"
-                         xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion"
-                         version="RAX-1">
-                   <rules>
-                      <rule>
-                        <local>
-                            <user>
-                               <name value="{D}"/>
-                               <email value="{D}"/>
-                               <expire value="{D}"/>
-                               <domain value="{D}"/>
-                               <roles value="{D}"/>
-                            </user>
-                         </local>
-                      </rule>
-                   </rules>
-            """
+            |<mapping xmlns="http://docs.rackspace.com/identity/api/ext/MappingRules"
+            |         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            |         xmlns:xs="http://www.w3.org/2001/XMLSchema"
+            |         xmlns:saml2="urn:oasis:names:tc:SAML:2.0:assertion"
+            |         version="RAX-1">
+            |   <rules>
+            |      <rule>
+            |         <local>
+            |            <user>
+            |               <name value="{D}"/>
+            |               <email value="{D}"/>
+            |               <expire value="{D}"/>
+            |               <domain value="{D}"/>
+            |               <roles value="{D}"/>
+            |            </user>
+            |         </local>
+            |      </rule>
+            |   </rules>
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": APPLICATION_XML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "400"
+        mc.receivedResponse.code as Integer == SC_BAD_REQUEST
         mc.handlings.size() == 0
     }
 
     def "should not validate bad JSON"() {
         given:
         String body =
-            """
-            {
-              "mapping": {
-                "rules": [
-                   {
-                    "local": {
-                      "user": {
-                        "domain":"{D}",
-                        "name":"{D}",
-                        "email":"{D}",
-                        "roles":"{D}",
-                        "expire":"{D}"
-                       }
-                     }
-                   }
-                 ],
-                "version":"RAX-1"
-              }
-            """
+            """{
+            |  "mapping": {
+            |    "rules": [
+            |       {
+            |        "local": {
+            |          "user": {
+            |            "domain":"{D}",
+            |            "name":"{D}",
+            |            "email":"{D}",
+            |            "roles":"{D}",
+            |            "expire":"{D}"
+            |           }
+            |         }
+            |       }
+            |     ],
+            |    "version":"RAX-1"
+            |  }
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": APPLICATION_JSON], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "400"
+        mc.receivedResponse.code as Integer == SC_BAD_REQUEST
         mc.handlings.size() == 0
     }
 
     def "should fail to validate bad YAML"() {
         given:
         String body =
-            """
-            |---
+            """---
             |mapping:
             |  rules:
             |    local:
@@ -318,32 +312,31 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
             |        roles: "{D}"
             |        expire: "{D}"
             |  version: RAX-1
-            """.stripMargin()
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": TEXT_YAML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "400"
+        mc.receivedResponse.code as Integer == SC_BAD_REQUEST
         mc.handlings.size() == 0
     }
 
     def "should fail to validate bad YAML (dimitry style)"() {
         given:
         String body =
-            """
-            |- just: write some
+            """- just: write some
             |- yaml:
             |  - [here, and]
             |  - {it: updates, in: real-time}
             |sdfsdds
-            """.stripMargin()
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": TEXT_YAML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "400"
+        mc.receivedResponse.code as Integer == SC_BAD_REQUEST
         mc.handlings.size() == 0
     }
 
@@ -351,8 +344,7 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
         given:
         String comment = "Find me"
         String body =
-            """
-            |---
+            """---
             |mapping: # ${comment}
             |  rules:
             |  - local:
@@ -363,13 +355,13 @@ class AttributeMappingPolicyValidationFilterTest extends ReposeValveTest {
             |      name: Username
             |      regex: false
             |  version: RAX-1
-            """.stripMargin()
+            |""".stripMargin()
 
         when:
         MessageChain mc = deproxy.makeRequest(url: reposeEndpoint, method: "PUT", headers: ["content-type": TEXT_YAML], requestBody: body)
 
         then:
-        mc.receivedResponse.code == "200"
+        mc.receivedResponse.code as Integer == SC_OK
         mc.handlings.size() == 1
         (mc.handlings[0].request.body as String).contains(comment)
     }
