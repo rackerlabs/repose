61aded7173e7e7d879ea96fc660fa2d476f88032
wdschei <Bill.Scheidegger@Rackspace.com>
2017-11-08 10:08:27 -0600

    Updated the RegEx RBAC filter unit tests to use the Configuration Test parent.

--- repose-aggregator/components/filters/regex-rbac-filter/src/test/scala/org/openrepose/filters/regexrbac/RegexRbacSchemaTest.scala
+++ repose-aggregator/components/filters/regex-rbac-filter/src/test/scala/org/openrepose/filters/regexrbac/RegexRbacSchemaTest.scala
@@ -19,61 +19,64 @@
  */
 package org.openrepose.filters.regexrbac
 
+import java.net.URL
+
 import org.junit.runner.RunWith
-import org.openrepose.commons.test.ConfigValidator
-import org.scalatest.{FunSpec, Matchers}
+import org.openrepose.commons.test.ConfigurationTest
+import org.openrepose.filters.regexrbac.config.ObjectFactory
 import org.scalatest.junit.JUnitRunner
 import org.xml.sax.SAXParseException
 
 @RunWith(classOf[JUnitRunner])
-class RegexRbacSchemaTest extends FunSpec with Matchers {
-  val validator = ConfigValidator("/META-INF/schema/config/regex-rbac.xsd")
+class RegexRbacSchemaTest extends ConfigurationTest {
+  override val schema: URL = getClass.getResource("/META-INF/schema/config/regex-rbac.xsd")
+  override val exampleConfig: URL = getClass.getResource("/META-INF/schema/examples/regex-rbac.cfg.xml")
+  override val jaxbContextPath: String = classOf[ObjectFactory].getPackage.getName
 
   describe("schema validation") {
-    it("should successfully validate the sample config") {
-      validator.validateConfigFile("/META-INF/schema/examples/regex-rbac.cfg.xml")
-    }
-
     it("should successfully validate the config when resources are specified inline") {
-      val config = """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
-                     |             mask-rax-roles-403="false">
-                     |    <resources>
-                     |/path/to/this   GET     role1,role2,role3,role4
-                     |/path/to/this   PUT     role1,role2,role3
-                     |/path/to/this   POST    role1,role2
-                     |/path/to/this   DELETE  role1
-                     |/path/to/that   GET,PUT ALL
-                     |/path/to/that   ALL     role1
-                     |/path/[^/]+/.*  GET     role1
-                     |    </resources>
-                     |</regex-rbac>""".stripMargin
+      val config =
+        """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
+          |             mask-rax-roles-403="false">
+          |    <resources>
+          |/path/to/this   GET     role1,role2,role3,role4
+          |/path/to/this   PUT     role1,role2,role3
+          |/path/to/this   POST    role1,role2
+          |/path/to/this   DELETE  role1
+          |/path/to/that   GET,PUT ALL
+          |/path/to/that   ALL     role1
+          |/path/[^/]+/.*  GET     role1
+          |    </resources>
+          |</regex-rbac>""".stripMargin
       validator.validateConfigString(config)
     }
 
     it("should successfully validate the config when resources are specified using an href link") {
-      val config = """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
-                     |             mask-rax-roles-403="false">
-                     |    <resources href="some_file"/>
-                     |</regex-rbac>""".stripMargin
+      val config =
+        """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
+          |             mask-rax-roles-403="false">
+          |    <resources href="some_file"/>
+          |</regex-rbac>""".stripMargin
       validator.validateConfigString(config)
     }
 
     it("should reject the config when resources are specified inline and using an href link") {
-      val config = """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
-                     |             mask-rax-roles-403="false">
-                     |    <resources href="some_file">
-                     |/path/to/this   GET     role1,role2,role3,role4
-                     |/path/to/this   PUT     role1,role2,role3
-                     |/path/to/this   POST    role1,role2
-                     |/path/to/this   DELETE  role1
-                     |/path/to/that   GET,PUT ALL
-                     |/path/to/that   ALL     role1
-                     |/path/[^/]+/.*  GET     role1
-                     |    </resources>
-                     |</regex-rbac>""".stripMargin
+      val config =
+        """<regex-rbac xmlns="http://docs.openrepose.org/repose/regex-rbac/v1.0"
+          |             mask-rax-roles-403="false">
+          |    <resources href="some_file">
+          |/path/to/this   GET     role1,role2,role3,role4
+          |/path/to/this   PUT     role1,role2,role3
+          |/path/to/this   POST    role1,role2
+          |/path/to/this   DELETE  role1
+          |/path/to/that   GET,PUT ALL
+          |/path/to/that   ALL     role1
+          |/path/[^/]+/.*  GET     role1
+          |    </resources>
+          |</regex-rbac>""".stripMargin
       intercept[SAXParseException] {
         validator.validateConfigString(config)
-      }.getLocalizedMessage should include ("Cannot define message inline and reference to external message file")
+      }.getLocalizedMessage should include("Cannot define message inline and reference to external message file")
     }
   }
 }
