6c191b772b42675149a216a0243c5320a74ac762
Dimitry Ushakov <dimitry.ushakov@rackspace.com>
2018-02-06 08:15:12 -0600

    Moved java implementation to scala

---
+++ repose-aggregator/components/services/opentracing-service/opentracing-service-impl/src/main/scala/org/openrepose/core/services/opentracing/TracerExtractor.scala
@@ -0,0 +1,105 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.core.services.opentracing
+
+import java.util
+import java.util.AbstractMap.SimpleImmutableEntry
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import io.opentracing.propagation.TextMap
+import org.openrepose.commons.utils.servlet.http.HttpServletRequestWrapper
+
+import scala.collection.mutable.ListBuffer
+
+class TracerExtractor() extends TextMap with LazyLogging {
+  var headers: Map[String, List[String]] = _
+
+  def this(httpServletRequestWrapper: HttpServletRequestWrapper) = {
+    this()
+
+    headers = servletHeadersToMultiMap(httpServletRequestWrapper)
+
+    def servletHeadersToMultiMap(httpServletRequest: HttpServletRequestWrapper): Map[String, List[String]] = {
+      val headersResult = scala.collection.mutable.HashMap.empty[String, List[String]]
+      if (httpServletRequest != null) {
+        val headerNamesIterator = httpServletRequest.getHeaderNames
+        while ( {
+          headerNamesIterator.hasMoreElements
+        }) {
+          val headerName: String = headerNamesIterator.nextElement
+          val valuesIterator = httpServletRequest.getHeaders(headerName)
+          var valuesList = ListBuffer[String]()
+          while ( {
+            valuesIterator.hasMoreElements
+          }) {
+            valuesList += valuesIterator.nextElement
+          }
+          headersResult += (headerName -> valuesList.toList)
+        }
+      }
+
+      headersResult.toMap
+
+    }
+  }
+
+  override def put(key: String, value: String): Unit = ???
+
+  override def iterator() = new MultivaluedMapFlatIterator[String, String](this.headers.toSet)
+
+  /**
+    * Iterate through a map with lists
+    * @tparam K String key
+    * @tparam V List[String] value
+    */
+  class MultivaluedMapFlatIterator[K <: AnyRef, V >: Null <: AnyRef]() extends util.Iterator[util.Map.Entry[K, V]] {
+    var mapIterator: Iterator[(K, List[V])] = _
+    var mapEntry: (K, List[V]) = _
+    var listIterator: Iterator[V] = _
+
+    def this(multiValuesEntrySet: Set[(K, List[V])]) = {
+      this()
+
+      mapIterator = multiValuesEntrySet.iterator
+      println(s"ctor $mapIterator")
+    }
+
+    override def hasNext: Boolean = {
+      println(s"hasNext $listIterator")
+      if (listIterator != null && listIterator.hasNext) return true
+      println(s"hasNext ${mapIterator.hasNext}")
+      mapIterator.hasNext
+    }
+
+    override def next: util.Map.Entry[K, V] = {
+      println(s"next $mapEntry")
+      if (mapEntry == null || (!listIterator.hasNext && mapIterator.hasNext)) {
+        mapEntry = mapIterator.next
+        println(s"next 2 $mapEntry")
+        listIterator = mapEntry._2.iterator
+        println(s"listIterator $listIterator")
+      }
+      println(s"listIterator 2 ${listIterator.hasNext}")
+      if (listIterator.hasNext) new SimpleImmutableEntry[K, V](mapEntry._1, listIterator.next)
+      else new SimpleImmutableEntry[K, V](mapEntry._1, null)
+    }
+
+  }
+}
---
+++ repose-aggregator/components/services/opentracing-service/opentracing-service-impl/src/test/scala/org/openrepose/core/services/opentracing/interceptors/JaegerRequestInterceptorTest.scala
@@ -0,0 +1,80 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.core.services.opentracing.interceptors
+
+import com.uber.jaeger.Tracer
+import io.opentracing.{Scope, ScopeManager, Span}
+import org.apache.http.{HttpRequest}
+import org.apache.http.message.BasicHeader
+import org.apache.http.protocol.HttpContext
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito.{never, times, verify, when}
+import org.openrepose.commons.utils.http.CommonHttpHeader
+import org.mockito.Matchers.any
+
+
+@RunWith(classOf[JUnitRunner])
+class JaegerRequestInterceptorTest extends FunSpec with Matchers with MockitoSugar with BeforeAndAfter {
+
+  describe("testOnSpanStarted") {
+    it("no headers") {
+      val httpRequest = mock[HttpRequest]
+      val httpContext = mock[HttpContext]
+      val tracer = mock[Tracer]
+      val scopeManager = mock[ScopeManager]
+      val scope = mock[Scope]
+      val span = mock[Span]
+
+      when(scope.span()).thenReturn(span)
+      when(scopeManager.active()).thenReturn(scope)
+      when(tracer.scopeManager()).thenReturn(scopeManager)
+
+      val jaegerRequestInterceptor = new JaegerRequestInterceptor(tracer)
+
+      jaegerRequestInterceptor.process(httpRequest, httpContext)
+
+      verify(span, never()).setTag(any[String](), any[String]())
+    }
+
+    it("with request header") {
+      val httpRequest = mock[HttpRequest]
+      val httpContext = mock[HttpContext]
+      val tracer = mock[Tracer]
+      val scopeManager = mock[ScopeManager]
+      val scope = mock[Scope]
+      val span = mock[Span]
+
+      when(httpRequest.getFirstHeader(any())).thenReturn(new BasicHeader(CommonHttpHeader.REQUEST_ID, "1234"))
+      when(scope.span()).thenReturn(span)
+      when(scopeManager.active()).thenReturn(scope)
+      when(tracer.scopeManager()).thenReturn(scopeManager)
+
+      val jaegerRequestInterceptor = new JaegerRequestInterceptor(tracer)
+
+      jaegerRequestInterceptor.process(httpRequest, httpContext)
+
+      verify(span, times(1)).setTag(any[String](), any[String]())
+    }
+  }
+
+}
