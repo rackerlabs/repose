887627f54f72e20042c27b2f6736dce9874712a5
wdschei <Bill.Scheidegger@Rackspace.com>
2018-03-01 12:50:23 -0600

    Added FIT's for Valkyrie with header qualities.

---
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/valkyrie/ValkyrieAuthorizationQualityTest.groovy
@@ -0,0 +1,176 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package features.filters.valkyrie
+
+import org.openrepose.framework.test.ReposeValveTest
+import org.openrepose.framework.test.mocks.MockValkyrie
+import org.rackspace.deproxy.Deproxy
+import org.rackspace.deproxy.MessageChain
+import spock.lang.Shared
+import spock.lang.Unroll
+
+import static javax.servlet.http.HttpServletResponse.SC_FORBIDDEN
+import static javax.servlet.http.HttpServletResponse.SC_OK
+import static org.openrepose.commons.utils.http.CommonHttpHeader.AUTH_TOKEN
+import static org.openrepose.commons.utils.http.OpenStackServiceHeader.CONTACT_ID
+import static org.openrepose.commons.utils.http.OpenStackServiceHeader.TENANT_ID
+
+class ValkyrieAuthorizationQualityTest extends ReposeValveTest {
+    static def HDR_DEVICE_ID = "X-Device-Id"
+
+    @Shared
+    MockValkyrie mockValkyrie
+
+    def setupSpec() {
+        Map params = properties.getDefaultTemplateParams()
+        repose.configurationProvider.applyConfigs("common", params)
+        repose.configurationProvider.applyConfigs("features/filters/valkyrie/quality", params)
+
+        repose.start()
+
+        mockValkyrie = new MockValkyrie(properties.valkyriePort)
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(port: properties.targetPort)
+        deproxy.addEndpoint(port: properties.valkyriePort, name: "valkyrie service", defaultHandler: mockValkyrie.getHandler())
+    }
+
+    def setup() {
+        mockValkyrie.resetCounts()
+        mockValkyrie.resetHandlers()
+        mockValkyrie.resetParameters()
+    }
+
+    @Unroll
+    def "should allow #method calls with permission '#devicePermission' and quality '#quality'"() {
+        given:
+        def authToken = UUID.randomUUID().toString()
+        mockValkyrie.with {
+            valid_auth = authToken
+            contact_id = "$contactId"
+            device_perm = devicePermission
+        }
+
+
+        when:
+        MessageChain mc = deproxy.makeRequest(
+            url: "$reposeEndpoint/",
+            method: method,
+            headers: [
+                (AUTH_TOKEN)   : authToken,
+                (TENANT_ID)    : "someOtherTenant;q=0.5,hybrid:$contactId$quality",
+                (CONTACT_ID)   : mockValkyrie.contact_id,
+                (HDR_DEVICE_ID): mockValkyrie.device_id])
+
+        then:
+        mc.handlings.size() == 1
+        mc.handlings[0].request.path == "/"
+        List<String> tenantIdValues = mc.handlings[0].request.headers.findAll(TENANT_ID)
+        tenantIdValues.contains("hybrid:$contactId$quality" as String)
+        mc.receivedResponse.code as Integer == SC_OK
+
+        where:
+        [a, b, c] << [["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
+                      [["edit_product", 1234],
+                       ["admin_product", 2345],
+                       ["account_admin", 3456]],
+                      [";q=0.98", ""]
+        ].combinations()
+
+        method = a
+        devicePermission = b[0]
+        contactId = b[1]
+        quality = c
+
+    }
+
+    @Unroll
+    def "should allow #method calls with permission 'view_product' and quality '#quality'"() {
+        given:
+        def authToken = UUID.randomUUID().toString()
+        mockValkyrie.with {
+            valid_auth = authToken
+            contact_id = "4567"
+            device_perm = "view_product"
+        }
+
+
+        when:
+        MessageChain mc = deproxy.makeRequest(
+            url: "$reposeEndpoint/",
+            method: method,
+            headers: [
+                (AUTH_TOKEN)   : authToken,
+                (TENANT_ID)    : "someOtherTenant;q=0.5,hybrid:4567$quality",
+                (CONTACT_ID)   : mockValkyrie.contact_id,
+                (HDR_DEVICE_ID): mockValkyrie.device_id])
+
+        then:
+        mc.handlings.size() == 1
+        mc.handlings[0].request.path == "/"
+        List<String> tenantIdValues = mc.handlings[0].request.headers.findAll(TENANT_ID)
+        tenantIdValues.contains("hybrid:4567$quality" as String)
+        mc.receivedResponse.code as Integer == SC_OK
+
+        where:
+        [a, b] << [["GET", "HEAD"],
+                   [";q=0.98", ""]
+        ].combinations()
+
+        method = a
+        quality = b
+
+    }
+
+    @Unroll
+    def "should not allow #method calls with permission 'view_product' and quality '#quality'"() {
+        given:
+        def authToken = UUID.randomUUID().toString()
+        mockValkyrie.with {
+            valid_auth = authToken
+            contact_id = "4567"
+            device_perm = "view_product"
+        }
+
+
+        when:
+        MessageChain mc = deproxy.makeRequest(
+            url: "$reposeEndpoint/",
+            method: method,
+            headers: [
+                (AUTH_TOKEN)   : authToken,
+                (TENANT_ID)    : "someOtherTenant;q=0.5,hybrid:4567$quality",
+                (CONTACT_ID)   : mockValkyrie.contact_id,
+                (HDR_DEVICE_ID): mockValkyrie.device_id])
+
+        then:
+        mc.handlings.size() == 0
+        mc.receivedResponse.code as Integer == SC_FORBIDDEN
+
+        where:
+        [a, b] << [["PUT", "POST", "DELETE", "PATCH"],
+                   [";q=0.98", ""]
+        ].combinations()
+
+        method = a
+        quality = b
+
+    }
+}
