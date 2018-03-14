4f08786b8ac2e3c0bbb408dd52213997c5a58add
Damien Johnson <damien.johnson@rackspace.com>
2018-03-13 22:54:22 -0500

    Refactoring the Open Tracing Service to only configure the Tracer

--- repose-aggregator/components/services/open-tracing-service/open-tracing-service-impl/src/main/scala/org/openrepose/core/services/opentracing/OpenTracingServiceImpl.scala
+++ repose-aggregator/components/services/open-tracing-service/open-tracing-service-impl/src/main/scala/org/openrepose/core/services/opentracing/OpenTracingServiceImpl.scala
@@ -25,168 +25,155 @@ import javax.inject.{Inject, Named}
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import com.uber.jaeger.Configuration
 import com.uber.jaeger.Configuration.{SamplerConfiguration, SenderConfiguration}
+import com.uber.jaeger.propagation.TextMapCodec
 import com.uber.jaeger.samplers.{ConstSampler, ProbabilisticSampler, RateLimitingSampler}
-import io.opentracing.Tracer
-import io.opentracing.util.GlobalTracer
+import io.opentracing.propagation.Format
 import org.openrepose.commons.config.manager.UpdateListener
+import org.openrepose.core.opentracing.DelegatingTracer
 import org.openrepose.core.service.opentracing.config._
 import org.openrepose.core.services.config.ConfigurationService
 import org.openrepose.core.systemmodel.config.SystemModel
 
 @Named
-class OpenTracingServiceImpl @Inject()(configurationService: ConfigurationService)
+class OpenTracingServiceImpl @Inject()(configurationService: ConfigurationService, reposeTracer: DelegatingTracer)
   extends OpenTracingService with LazyLogging {
 
   import OpenTracingServiceImpl._
 
-  private var isServiceEnabled: Boolean = false
-  private var serviceName: String = _
+  @volatile private var currentSystemModelConfig: SystemModel = _
+  @volatile private var currentOpenTracingConfig: OpenTracingConfig = _
 
   @PostConstruct
   def init(): Unit = {
-    logger.info("Initializing OpenTracingService")
+    logger.info("Initializing Open Tracing Service")
     val xsdURL = getClass.getResource("/META-INF/schema/config/opentracing.xsd")
 
+    configurationService.subscribeTo(
+      SystemModelConfig,
+      SystemModelConfigurationListener,
+      classOf[SystemModel])
     configurationService.subscribeTo(
       DefaultConfig,
       xsdURL,
       OpenTracingConfigurationListener,
       classOf[OpenTracingConfig])
-    configurationService.subscribeTo(
-      SystemModelConfig,
-      SystemModelConfigurationListener,
-      classOf[SystemModel])
   }
-
 
   @PreDestroy
   def destroy(): Unit = {
-    logger.info("Unregistering configuration listeners and shutting down service")
+    logger.info("Unsubscribing configuration listeners and shutting down service")
     configurationService.unsubscribeFrom(DefaultConfig, OpenTracingConfigurationListener)
     configurationService.unsubscribeFrom(SystemModelConfig, SystemModelConfigurationListener)
   }
 
-  /**
-    * Check that the service is enabled.  The tracing will only happen if this is enabled.
-    * While configuration might set the service as enabled, there are a couple instances when this might
-    * get turned back to disabled:
-    *
-    * * Invalid tracer specified
-    * * Unable to connect to tracer
-    *
-    * @return a Boolean which corresponds to the clientId parameter
-    */
-  override def isEnabled: Boolean = this.isServiceEnabled
+  private def configurationHeartbeat(): Unit = {
+    synchronized {
+      Option(currentSystemModelConfig) foreach { systemModelConfig =>
+        Option(currentOpenTracingConfig) foreach { openTracingConfig =>
+          // todo: add enabling/disabling of the service to the configuration
+          openTracingConfig.getJaeger match {
+            case jaegerConfig: JaegerTracerConfiguration =>
+              logger.debug("Jaeger tracer configured")
 
-  /**
-    * Retrieves the global tracer singleton.  This is configured at startup via opentracing.cfg.xml tracer
-    * specific configuration.  If an invalid tracer is provided, the tracer will not be available.
-    *
-    * @return io.opentracing.Tracer object that contains Tracer implementation information
-    */
-  override def getGlobalTracer: Tracer = {
-    logger.debug("Retrieve global tracer.")
-    try {
-      if (!OpenTracingConfigurationListener.isInitialized) throw new IllegalStateException("The OpenTracingService has not yet been initialized")
-      if (!GlobalTracer.isRegistered) {
-        logger.error("Opentracing configuration is missing.  " +
-          "Check that you have opentracing.cfg.xml properly configured with one of the tracers registered")
-        logger.trace("If we don't disable it, we would through an NPE wherever the tracer is called.  Don't do that.")
-        this.isServiceEnabled = false
+              val samplerConfiguration = getJaegerSamplerConfiguration(jaegerConfig)
+              val senderConfiguration = getJaegerSenderConfiguration(jaegerConfig)
+              val reporterConfiguration = new Configuration.ReporterConfiguration(
+                jaegerConfig.isLogSpans,
+                jaegerConfig.getFlushIntervalMs,
+                jaegerConfig.getMaxBufferSize,
+                senderConfiguration)
+              val configuration = new Configuration(
+                openTracingConfig.getServiceName,
+                samplerConfiguration,
+                reporterConfiguration)
+
+              val tracerBuilder = configuration.getTracerBuilder
+
+              // todo: add support for baggage prefix customization
+              logger.debug("Registering Repose-specific injectors and extractors")
+              val textMapCodecBuilder = new TextMapCodec.Builder()
+                .withSpanContextKey(systemModelConfig.getOpenTracingHeader)
+              val textMapCodec = textMapCodecBuilder.withUrlEncoding(false).build()
+              tracerBuilder.registerInjector(Format.Builtin.TEXT_MAP, textMapCodec)
+              tracerBuilder.registerExtractor(Format.Builtin.TEXT_MAP, textMapCodec)
+              val httpTextMapCodec = textMapCodecBuilder.withUrlEncoding(true).build()
+              tracerBuilder.registerInjector(Format.Builtin.HTTP_HEADERS, httpTextMapCodec)
+              tracerBuilder.registerExtractor(Format.Builtin.HTTP_HEADERS, httpTextMapCodec)
+
+              logger.debug("Registering the tracer with global tracer")
+              reposeTracer.register(tracerBuilder.build())
+            case _ =>
+              logger.error("Unsupported tracer specified")
+          }
+        }
       }
-    } catch {
-      case ise: IllegalStateException =>
-        logger.error("Opentracing was not initialized.  We will turn this off.  Check the logs for the issue. " +
-          "For example, an invalid tracer host/port", ise)
-        this.isServiceEnabled = false
     }
 
-    GlobalTracer.get
+    def getJaegerSamplerConfiguration(jaegerConfig: JaegerTracerConfiguration): SamplerConfiguration = {
+      jaegerConfig.getSampling match {
+        case constant: JaegerSamplingConstant =>
+          logger.debug("Constant sampling configured with value set to {}", constant.getToggle)
+          new Configuration.SamplerConfiguration(ConstSampler.TYPE, if (Toggle.ON.equals(constant.getToggle)) 1 else 0)
+        case rateLimiting: JaegerSamplingRateLimiting =>
+          logger.debug("Rate limiting sampling configured with value set to {} samples per second", rateLimiting.getMaxTracesPerSecond.asInstanceOf[AnyRef])
+          new Configuration.SamplerConfiguration(RateLimitingSampler.TYPE, rateLimiting.getMaxTracesPerSecond)
+        case probabilistic: JaegerSamplingProbabilistic =>
+          logger.debug("Probabilistic sampling configured with value set to {}", probabilistic.getProbability.asInstanceOf[AnyRef])
+          new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, probabilistic.getProbability)
+      }
+    }
+
+    def getJaegerSenderConfiguration(jaegerConfig: JaegerTracerConfiguration): SenderConfiguration = {
+      jaegerConfig.getConnection match {
+        case udp: JaegerConnectionUdp =>
+          logger.debug("UDP sender configured")
+          new SenderConfiguration.Builder().agentHost(udp.getHost).agentPort(udp.getPort).build
+        case http: JaegerConnectionHttp =>
+          if (Option(http.getToken).isDefined) {
+            logger.debug("HTTP sender configured with bearer token")
+            new SenderConfiguration.Builder().authToken(http.getToken).endpoint(s"${http.getHost}:${http.getPort}").build
+          } else if (Option(http.getUsername).isDefined) {
+            logger.debug("HTTP sender configured with basic auth")
+            new SenderConfiguration.Builder().authUsername(http.getUsername).authPassword(http.getPassword).endpoint(s"${http.getHost}:${http.getPort}").build
+          } else {
+            logger.debug("HTTP sender configured without authentication")
+            new SenderConfiguration.Builder().endpoint(s"${http.getHost}:${http.getPort}").build
+          }
+      }
+    }
   }
 
-  /**
-    * Retrieves service name.  This is specific for every repose implementation and defines the namespace
-    * for your service.  It should be unique in your company/flow.
-    *
-    * @return String object that contains your service name
-    */
-  override def getServiceName: String = this.serviceName
-
-  private def isInitialized: Boolean =
-    SystemModelConfigurationListener.isInitialized && OpenTracingConfigurationListener.isInitialized
-
-  private object OpenTracingConfigurationListener extends UpdateListener[OpenTracingConfig] {
+  object OpenTracingConfigurationListener extends UpdateListener[OpenTracingConfig] {
     private var initialized = false
 
     override def configurationUpdated(openTracingConfig: OpenTracingConfig): Unit = {
-      synchronized {
-        logger.trace("OpenTracingService configuration updated")
-        initialized = true
+      logger.debug("Open Tracing Service configuration updated")
 
-        logger.trace("get the tracer from configuration")
-
-        serviceName = openTracingConfig.getServiceName
-
-        val tracerConfig = Option(openTracingConfig.getJaeger)
-        tracerConfig match {
-          case Some(jaegerConfig) =>
-            logger.debug("register Jaeger tracer")
-            logger.debug("figure out which sampler we want!")
-            val samplerConfiguration: SamplerConfiguration = getJaegerSamplerConfiguration(jaegerConfig)
-            val senderConfiguration: SenderConfiguration = getJaegerSenderConfiguration(jaegerConfig)
-            val configuration = new Configuration(openTracingConfig.getServiceName, samplerConfiguration, new Configuration.ReporterConfiguration(jaegerConfig.isLogSpans, jaegerConfig.getFlushIntervalMs, jaegerConfig.getMaxBufferSize, senderConfiguration))
-            logger.debug("register the tracer with global tracer")
-            GlobalTracer.register(configuration.getTracer)
-          case _ =>
-            logger.error("Invalid tracer specified.  Problem with opentracing.xsd enumeration")
-            isServiceEnabled = false
-        }
-
-        def getJaegerSamplerConfiguration(jaegerConfig: JaegerTracerConfiguration): SamplerConfiguration = {
-          jaegerConfig.getSampling match {
-            case constant: JaegerSamplingConstant =>
-              logger.trace("Constant sampling configured with value set to {}", constant.getToggle)
-              new Configuration.SamplerConfiguration(ConstSampler.TYPE, if (Toggle.ON.equals(constant.getToggle)) 1 else 0)
-            case rateLimiting: JaegerSamplingRateLimiting =>
-              logger.trace("Rate limiting sampling configured with value set to {} samples per second", rateLimiting.getMaxTracesPerSecond)
-              new Configuration.SamplerConfiguration(RateLimitingSampler.TYPE, rateLimiting.getMaxTracesPerSecond)
-            case probabilistic: JaegerSamplingProbabilistic =>
-              logger.trace("Probabilistic sampling configured with value set to {}", probabilistic.getProbability)
-              new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, probabilistic.getProbability)
-          }
-        }
-
-        def getJaegerSenderConfiguration(jaegerConfig: JaegerTracerConfiguration): SenderConfiguration = {
-          jaegerConfig.getConnection match {
-            case udp: JaegerConnectionUdp =>
-              logger.trace("UDP sender configured")
-              new SenderConfiguration.Builder().agentHost(udp.getHost).agentPort(udp.getPort).build
-            case http: JaegerConnectionHttp =>
-              logger.trace("Checking if username and password are provided")
-              (Option(http.getToken), Option(http.getUsername)) match {
-                case (Some(_), _) =>
-                  logger.trace("HTTP sender configured with bearer token")
-                  new SenderConfiguration.Builder().authToken(http.getToken).endpoint(s"${http.getHost}:${http.getPort}").build
-                case (_, Some(_)) =>
-                  logger.trace("HTTP sender configured with basic auth ")
-                  new SenderConfiguration.Builder().authUsername(http.getUsername).authPassword(http.getPassword).endpoint(s"${http.getHost}:${http.getPort}").build
-                case (_, _) =>
-                  logger.trace("HTTP sender configured without authentication")
-                  new SenderConfiguration.Builder().endpoint(s"${http.getHost}:${http.getPort}").build
-              }
-          }
-        }
-
-      }
+      currentOpenTracingConfig = openTracingConfig
+      configurationHeartbeat()
+      initialized = true
     }
 
     override def isInitialized: Boolean = initialized
   }
+
+  object SystemModelConfigurationListener extends UpdateListener[SystemModel] {
+    private var initialized = false
+
+    override def configurationUpdated(systemModel: SystemModel): Unit = {
+      logger.debug("System Model configuration updated")
+
+      currentSystemModelConfig = systemModel
+      configurationHeartbeat()
+      initialized = true
+    }
+
+    override def isInitialized: Boolean = initialized
+  }
+
 }
 
-
 object OpenTracingServiceImpl {
-
   private final val DefaultConfig = "open-tracing.cfg.xml"
   private final val SystemModelConfig = "system-model.cfg.xml"
 }
