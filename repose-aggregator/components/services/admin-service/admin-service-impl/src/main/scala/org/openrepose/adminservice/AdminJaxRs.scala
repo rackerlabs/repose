893c053935e27f3a83de2b6514533dd9f19ac4f3
Adrian George <adrian.george@rackspace.com>
2018-04-27 13:05:49 -0500

    Rest interface stands up

---
+++ repose-aggregator/components/services/admin-service/admin-service-impl/src/main/scala/org/openrepose/adminservice/AdminJaxRs.scala
@@ -0,0 +1,37 @@
+/*
+ * _=_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=
+ * Repose
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Copyright (C) 2010 - 2015 Rackspace US, Inc.
+ * _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_=_
+ */
+package org.openrepose.adminservice
+
+import java.util
+
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.{GET, Path, Produces}
+import org.springframework.stereotype.Component
+
+import scala.collection.JavaConverters._
+
+@Component
+@Path("/banana")
+class AdminJaxRs extends AdminWebInterface {
+  @Produces( Array(MediaType.APPLICATION_JSON) )
+  @GET
+  @Path("/phone")
+  override def doTheThing(): util.Map[String, String] = Map("greeting" -> "Hello from this node").asJava
+}
