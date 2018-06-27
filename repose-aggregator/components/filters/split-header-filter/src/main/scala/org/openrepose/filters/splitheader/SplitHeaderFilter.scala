e633e44d7957882a22bcdbb2759fcf8b2a510fae
Damien Johnson <damien.johnson@rackspace.com>
2018-06-27 10:44:49 -0500

    Implementing the split-header filter

--- repose-aggregator/components/filters/split-header-filter/src/main/scala/org/openrepose/filters/splitheader/SplitHeaderFilter.scala
+++ repose-aggregator/components/filters/split-header-filter/src/main/scala/org/openrepose/filters/splitheader/SplitHeaderFilter.scala
@@ -23,18 +23,59 @@ import com.typesafe.scalalogging.slf4j.StrictLogging
 import javax.inject.{Inject, Named}
 import javax.servlet.FilterChain
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import org.openrepose.commons.utils.servlet.http.{HeaderInteractor, HttpServletRequestWrapper, HttpServletResponseWrapper, ResponseMode}
 import org.openrepose.core.filter.AbstractConfiguredFilter
 import org.openrepose.core.services.config.ConfigurationService
+import org.openrepose.filters.splitheader.config.{HeaderList, SplitHeaderConfig}
 
+import scala.collection.JavaConverters._
+
+/**
+  * This filter will "split" configured HTTP message headers (defined by header name)
+  * across multiple lines, each with a single value.
+  *
+  * Splitting is performed using the comma character (i.e., ',') as a delimiter.
+  *
+  * Note: A side effect of this filter is that the resultant header(s) will have
+  * casing matching the configured header name rather than the corresponding
+  * header name from the HTTP message.
+  * Although it is possible to keep the casing of the header name from the HTTP
+  * message, we opted not to do so since:
+  * 1. By specification, header names are case-insensitive.
+  * 2. The added complexity would make the code less clear and succinct,
+  * and thus, more difficult to maintain.
+  */
 @Named
 class SplitHeaderFilter @Inject()(configurationService: ConfigurationService)
-  extends AbstractConfiguredFilter(configurationService)
+  extends AbstractConfiguredFilter[SplitHeaderConfig](configurationService)
     with StrictLogging {
 
   override val DEFAULT_CONFIG = "split-header.cfg.xml"
   override val SCHEMA_LOCATION = "/META-INF/schema/config/split-header.xsd"
 
   override def doWork(httpRequest: HttpServletRequest, httpResponse: HttpServletResponse, chain: FilterChain): Unit = {
-    ???
+    val wrappedHttpRequest = new HttpServletRequestWrapper(httpRequest)
+    val wrappedHttpResponse = new HttpServletResponseWrapper(
+      httpResponse,
+      ResponseMode.MUTABLE,
+      ResponseMode.PASSTHROUGH)
+
+    splitHeaders(wrappedHttpRequest, configuration.getRequest)
+
+    chain.doFilter(wrappedHttpRequest, wrappedHttpResponse)
+
+    wrappedHttpResponse.uncommit()
+
+    splitHeaders(wrappedHttpResponse, configuration.getResponse)
+
+    wrappedHttpResponse.commitToResponse()
+  }
+
+  private def splitHeaders(wrappedHttpMessage: HeaderInteractor, configuredHeaders: HeaderList): Unit = {
+    Option(configuredHeaders).map(_.getHeader.asScala).getOrElse(Seq.empty) foreach { headerToSplit =>
+      val headerValues = wrappedHttpMessage.getSplittableHeaders(headerToSplit).asScala
+      wrappedHttpMessage.removeHeader(headerToSplit)
+      headerValues.foreach(wrappedHttpMessage.addHeader(headerToSplit, _))
+    }
   }
 }
--- repose-aggregator/components/filters/split-header-filter/src/test/scala/org/openrepose/filters/splitheader/SplitHeaderFilterTest.scala
+++ repose-aggregator/components/filters/split-header-filter/src/test/scala/org/openrepose/filters/splitheader/SplitHeaderFilterTest.scala
@@ -19,11 +19,287 @@
  */
 package org.openrepose.filters.splitheader
 
+import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.junit.runner.RunWith
-import org.scalatest.FunSpec
+import org.openrepose.core.services.config.ConfigurationService
+import org.openrepose.filters.splitheader.config.{HeaderList, SplitHeaderConfig}
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfterEach, FunSpec, Matchers}
+import org.springframework.mock.web.{MockFilterChain, MockHttpServletRequest, MockHttpServletResponse}
+
+import scala.collection.JavaConverters._
 
 @RunWith(classOf[JUnitRunner])
-class SplitHeaderFilterTest extends FunSpec {
+class SplitHeaderFilterTest extends FunSpec with BeforeAndAfterEach with MockitoSugar with Matchers {
 
+  final val TestHeaderName: String = "test"
+
+  var configurationService: ConfigurationService = _
+  var splitHeaderFilter: SplitHeaderFilter = _
+
+  var request: MockHttpServletRequest = _
+  var response: MockHttpServletResponse = _
+  var filterChain: MockFilterChain = _
+
+  override protected def beforeEach(): Unit = {
+    super.beforeEach()
+
+    configurationService = mock[ConfigurationService]
+    splitHeaderFilter = new SplitHeaderFilter(configurationService)
+
+    request = new MockHttpServletRequest()
+    response = new MockHttpServletResponse()
+
+    // A mock filter chain with a Servlet that copies headers from the shared response object
+    // into the response object passed to the Servlet.
+    // This is necessary because the response wrapper is used in the filter, but does not copy
+    // in any headers when it wraps a response.
+    // Unfortunately, this couples testing of the Split Header filter with testing of the response
+    // wrapper to some degree.
+    // This could be avoided by either copying headers in the response wrapper, or not using the
+    // response wrapped in the Split Header filter.
+    filterChain = new MockFilterChain(new Servlet() {
+      override def init(config: ServletConfig): Unit = ???
+
+      override def getServletConfig: ServletConfig = ???
+
+      override def service(req: ServletRequest, res: ServletResponse): Unit = {
+        response.getHeaderNames.asScala foreach { headerName =>
+          response.getHeaders(headerName).asScala foreach { headerValue =>
+            res.asInstanceOf[HttpServletResponse].addHeader(headerName, headerValue)
+          }
+        }
+      }
+
+      override def getServletInfo: String = ???
+
+      override def destroy(): Unit = ???
+    })
+  }
+
+  describe("doWork") {
+    it("should not modify headers if no headers are configured to be split") {
+      splitHeaderFilter.configuration = new SplitHeaderConfig()
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaderNames.asScala shouldBe empty
+      response.getHeaderNames.asScala shouldBe empty
+    }
+
+    it("should split a request header with multiple values on the one line") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one,two,three")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three")
+    }
+
+    it("should split a response header with multiple values on the one line") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one,two,three")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three")
+    }
+
+    it("should split a request header with multiple lines each with one value") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one")
+      request.addHeader(TestHeaderName, "two")
+      request.addHeader(TestHeaderName, "three")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three")
+    }
+
+    it("should split a response header with multiple lines each with one value") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one")
+      response.addHeader(TestHeaderName, "two")
+      response.addHeader(TestHeaderName, "three")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three")
+    }
+
+    it("should split a request header with multiple values on multiple lines") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one,two")
+      request.addHeader(TestHeaderName, "three,four")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three", "four")
+    }
+
+    it("should split a response header with multiple values on multiple lines") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one,two")
+      response.addHeader(TestHeaderName, "two,four")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three", "four")
+    }
+
+    it("should split a request header regardless of casing") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq("test-header"))
+
+      request.addHeader("TEST-HeAdEr", "one,two")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders("test-header").asScala.toSeq should contain only("one", "two")
+    }
+
+    it("should split a response header regardless of casing") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq("test-header"))
+
+      response.addHeader("TEST-HeAdEr", "one,two")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders("test-header").asScala.toSeq should contain only("one", "two")
+    }
+
+    it("should not change the order of request header values when splitting") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one,two")
+      request.addHeader(TestHeaderName, "three")
+      request.addHeader(TestHeaderName, "four")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain inOrderOnly("one", "two", "three", "four")
+    }
+
+    it("should not change the order of response header values when splitting") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one,two")
+      response.addHeader(TestHeaderName, "three")
+      response.addHeader(TestHeaderName, "four")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain inOrderOnly("one", "two", "three", "four")
+    }
+
+    it("should trim the values of a request header when splitting") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one, two")
+      request.addHeader(TestHeaderName, "three  ")
+      request.addHeader(TestHeaderName, "  four")
+      request.addHeader(TestHeaderName, "fi  ve")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three", "four", "fi  ve")
+    }
+
+    it("should trim the values of a response header when splitting") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one, two")
+      response.addHeader(TestHeaderName, "three")
+      response.addHeader(TestHeaderName, "four")
+      response.addHeader(TestHeaderName, "fi  ve")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain only("one", "two", "three", "four", "fi  ve")
+    }
+
+    it("should retain parameters with their associated value on request headers") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq(TestHeaderName))
+
+      request.addHeader(TestHeaderName, "one;q=0.1;foo=bar,two;baz=zaz;q=0.2")
+      request.addHeader(TestHeaderName, "three;q=0.3;too=too")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaders(TestHeaderName).asScala.toSeq should contain only("one;q=0.1;foo=bar", "two;baz=zaz;q=0.2", "three;q=0.3;too=too")
+    }
+
+    it("should retain parameters with their associated value on response headers") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq(TestHeaderName))
+
+      response.addHeader(TestHeaderName, "one;q=0.1;foo=bar,two;baz=zaz;q=0.2")
+      response.addHeader(TestHeaderName, "three;q=0.3;too=too")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaders(TestHeaderName).asScala.toSeq should contain only("one;q=0.1;foo=bar", "two;baz=zaz;q=0.2", "three;q=0.3;too=too")
+    }
+
+    it("should adopt the configured request header name casing") {
+      splitHeaderFilter.configuration = createConfig(requestHeaders = Seq("TEST"))
+
+      request.addHeader("test", "one")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedRequest = filterChain.getRequest.asInstanceOf[HttpServletRequest]
+      passedRequest.getHeaderNames.asScala.toSeq should contain("TEST")
+    }
+
+    it("should adopt the configured response header name casing") {
+      splitHeaderFilter.configuration = createConfig(responseHeaders = Seq("TEST"))
+
+      response.addHeader("test", "one")
+
+      splitHeaderFilter.doWork(request, response, filterChain)
+
+      val passedResponse = filterChain.getResponse.asInstanceOf[HttpServletResponse]
+      passedResponse.getHeaderNames.asScala.toSeq should contain("TEST")
+    }
+  }
+
+  def createConfig(requestHeaders: Seq[String] = Seq.empty, responseHeaders: Seq[String] = Seq.empty): SplitHeaderConfig = {
+    val config = new SplitHeaderConfig()
+
+    if (requestHeaders.nonEmpty) {
+      val requestHeaderList = new HeaderList()
+      requestHeaderList.getHeader.addAll(requestHeaders.asJavaCollection)
+      config.setRequest(requestHeaderList)
+    }
+
+    if (responseHeaders.nonEmpty) {
+      val responseHeaderList = new HeaderList()
+      responseHeaderList.getHeader.addAll(responseHeaders.asJavaCollection)
+      config.setResponse(responseHeaderList)
+    }
+
+    config
+  }
 }
