e9b0b2b08c194c96f3614a4c6df045982cd11f87
Adrian George <adrian.george@rackspace.com>
2018-07-06 11:03:07 -0500

    this fixes the jaxb marshalling problems

---
+++ repose-aggregator/core/repose-core-api/src/main/java/org/openrepose/core/systemmodel/config/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://docs.openrepose.org/repose/system-model/v2.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.openrepose.core.systemmodel.config;
