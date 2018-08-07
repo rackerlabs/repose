cbca98551a16cd59211f970e89a3a28340da51df
wdschei <Bill.Scheidegger@Rackspace.com>
2018-08-06 19:26:08 -0500

    Removed some test cases that can not happen in the wild due to XML Schema Validation.

--- repose-aggregator/core/repose-core/src/test/groovy/org/openrepose/powerfilter/PowerFilterRouterImplTest.groovy
+++ repose-aggregator/core/repose-core/src/test/groovy/org/openrepose/powerfilter/PowerFilterRouterImplTest.groovy
@@ -19,7 +19,6 @@
  */
 package org.openrepose.powerfilter
 
-import akka.actor.dsl.Creators
 import org.openrepose.commons.utils.io.stream.ReadLimitReachedException
 import org.openrepose.commons.utils.servlet.http.HttpServletRequestWrapper
 import org.openrepose.core.filter.routing.DestinationLocation
@@ -37,20 +36,14 @@ import spock.lang.Unroll
 
 import javax.servlet.RequestDispatcher
 import javax.servlet.ServletContext
-import javax.servlet.ServletException
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import java.rmi.server.UnicastRemoteObject
 
 import static org.mockito.Matchers.any
 import static org.mockito.Matchers.anyString
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.never
-import static org.mockito.Mockito.verify
-import static org.mockito.Mockito.when
-import static org.mockito.Mockito.times
+import static org.mockito.Mockito.*
 
 class PowerFilterRouterImplTest extends Specification {
 
@@ -104,19 +97,23 @@ class PowerFilterRouterImplTest extends Specification {
     }
 
     @Unroll
-    def "route default destination: #defaultDestination, destination id: #destinationId, destination default flag: #destinationDefault"() {
+    def "route default destination: #defaultDestinationId, dispatch error: #dispatchError"() {
         given:
         Map<String, Destination> destinationMap = new HashMap<>()
-        Destination destination = new DestinationEndpoint()
-        destination.setDefault(destinationDefault)
-        destination.setId(destinationId)
-        destination.setProtocol("http")
-        destination.setRootPath("/")
-        destinationMap.put("/", destination)
+        Destination destinationOne = new DestinationEndpoint()
+        destinationOne.setDefault("one" == defaultDestinationId)
+        destinationOne.setId("one")
+        destinationOne.setProtocol("http")
+        destinationMap.put("one", destinationOne)
+        Destination destinationTwo = new DestinationEndpoint()
+        destinationTwo.setDefault("two" == defaultDestinationId)
+        destinationTwo.setId("two")
+        destinationTwo.setProtocol("http")
+        destinationMap.put("two", destinationTwo)
         powerFilterRouter = new PowerFilterRouterImpl(destinationLocationBuilder,
             destinationMap,
             reposeCluster,
-            defaultDestination,
+            defaultDestinationId,
             servletContext,
             requestHeaderService,
             responseHeaderService,
@@ -139,13 +136,12 @@ class PowerFilterRouterImplTest extends Specification {
         // populate that location
         when(destinationLocationBuilder.build(any(), anyString(), any(HttpServletRequest.class))).thenReturn(
             new DestinationLocation(
-                new URL(destination.getProtocol(), "localhost", 8080, destination.getRootPath()),
-                new URI("http", null, "localhost", 8080, destination.getRootPath(),
-                    "", null)
+                new URL(destinationOne.getProtocol(), "localhost", 8080, destinationOne.getRootPath()),
+                new URI("http", null, "localhost", 8080, destinationOne.getRootPath(),"", null)
             )
         )
 
-        if (dispatchError != null ) {
+        if (dispatchError != null) {
             when(requestDispatcher.forward(any(ServletRequest.class), any(ServletResponse.class))).thenThrow(dispatchError)
         }
 
@@ -155,7 +151,7 @@ class PowerFilterRouterImplTest extends Specification {
         then:
         assert httpServletResponse.getStatus() == responseCode
 
-        if (defaultDestination == null) {
+        if (defaultDestinationId == null) {
             verify(requestDispatcher, never()).forward(
                 any(ServletRequest.class), any(ServletResponse.class))
             verify(reportingService, never()).incrementRequestCount(anyString())
@@ -167,31 +163,15 @@ class PowerFilterRouterImplTest extends Specification {
 
         where:
 
-        defaultDestination | destinationId | destinationDefault | responseCode                                      | dispatchError
-        "/"                | "test"        | true               | HttpServletResponse.SC_OK                         | null
-        null               | "test"        | true               | HttpServletResponse.SC_OK                         | null
-        "/"                | "test"        | false              | HttpServletResponse.SC_OK                         | null
-        null               | "test"        | false              | HttpServletResponse.SC_OK                         | null
-        "/"                | null          | true               | HttpServletResponse.SC_OK                         | null
-        null               | null          | true               | HttpServletResponse.SC_OK                         | null
-        "/"                | null          | false              | HttpServletResponse.SC_OK                         | null
-        null               | null          | false              | HttpServletResponse.SC_OK                         | null
-        "/"                | "test"        | true               | HttpServletResponse.SC_SERVICE_UNAVAILABLE        | new IOException()
-        null               | "test"        | true               | HttpServletResponse.SC_OK                         | new IOException()
-        "/"                | "test"        | false              | HttpServletResponse.SC_SERVICE_UNAVAILABLE        | new IOException()
-        null               | "test"        | false              | HttpServletResponse.SC_OK                         | new IOException()
-        "/"                | null          | true               | HttpServletResponse.SC_SERVICE_UNAVAILABLE        | new IOException()
-        null               | null          | true               | HttpServletResponse.SC_OK                         | new IOException()
-        "/"                | null          | false              | HttpServletResponse.SC_SERVICE_UNAVAILABLE        | new IOException()
-        null               | null          | false              | HttpServletResponse.SC_OK                         | new IOException()
-        "/"                | "test"        | true               | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE   | new IOException().initCause(new ReadLimitReachedException("too much"))
-        null               | "test"        | true               | HttpServletResponse.SC_OK                         | new IOException().initCause(new ReadLimitReachedException("too much"))
-        "/"                | "test"        | false              | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE   | new IOException().initCause(new ReadLimitReachedException("too much"))
-        null               | "test"        | false              | HttpServletResponse.SC_OK                         | new IOException().initCause(new ReadLimitReachedException("too much"))
-        "/"                | null          | true               | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE   | new IOException().initCause(new ReadLimitReachedException("too much"))
-        null               | null          | true               | HttpServletResponse.SC_OK                         | new IOException().initCause(new ReadLimitReachedException("too much"))
-        "/"                | null          | false              | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE   | new IOException().initCause(new ReadLimitReachedException("too much"))
-        null               | null          | false              | HttpServletResponse.SC_OK                         | new IOException().initCause(new ReadLimitReachedException("too much"))
-
+        defaultDestinationId | responseCode                                    | dispatchError
+        null                 | HttpServletResponse.SC_OK                       | null
+        null                 | HttpServletResponse.SC_OK                       | new IOException()
+        null                 | HttpServletResponse.SC_OK                       | new IOException().initCause(new ReadLimitReachedException("too much"))
+        "one"                | HttpServletResponse.SC_OK                       | null
+        "one"                | HttpServletResponse.SC_SERVICE_UNAVAILABLE      | new IOException()
+        "one"                | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE | new IOException().initCause(new ReadLimitReachedException("too much"))
+        "two"                | HttpServletResponse.SC_OK                       | null
+        "two"                | HttpServletResponse.SC_SERVICE_UNAVAILABLE      | new IOException()
+        "two"                | HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE | new IOException().initCause(new ReadLimitReachedException("too much"))
     }
 }
