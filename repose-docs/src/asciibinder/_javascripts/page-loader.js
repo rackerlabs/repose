fbb9c616215f09d9a3d44f84d7307de33332269a
wdschei <Bill.Scheidegger@Rackspace.com>
2018-09-12 09:01:16 -0500

    Updated formatting based on feedback.

---
+++ repose-docs/src/asciibinder/_javascripts/page-loader.js
@@ -0,0 +1,26 @@
+function versionSelector(list) {
+  // the version we want
+  newVersion = list[list.selectedIndex].value;
+
+  // spilt the current path
+  var pathArray = window.location.pathname.split( '/' );
+
+  // so we can get the current version
+  currentVersion = pathArray[1];
+
+  // the file path is just the version number + the end of the path
+  var fileRequested =
+    window.location.pathname.substring(
+      window.location.pathname.lastIndexOf(currentVersion) +
+      currentVersion.length);
+  //alert(fileRequested);
+
+  // without doing async loads, there is no way to know if the path actually
+  // exists - so we will just have to load
+  window.location = "/" + newVersion + fileRequested;
+}
+
+function selectVersion(currentVersion) {
+  document.getElementById("version-selector").value = currentVersion;
+  //alert(currentVersion);
+}
