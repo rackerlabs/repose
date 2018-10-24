5170a2244abd901c9711451e319f0da90a4cab9f
wdschei <Bill.Scheidegger@Rackspace.com>
2018-10-24 08:50:51 -0500

    Made a private constant public so it could be reused.

--- repose-aggregator/commons/commons-utilities/src/main/java/org/openrepose/commons/utils/logging/apache/format/stock/ResponseTimeHandler.java
+++ repose-aggregator/commons/commons-utilities/src/main/java/org/openrepose/commons/utils/logging/apache/format/stock/ResponseTimeHandler.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,10 +28,9 @@ import java.text.DecimalFormat;
 
 public class ResponseTimeHandler implements FormatterLogic {
 
-    private static final String START_TIME_ATTRIBUTE = "org.openrepose.repose.logging.start.time";
-    private static final double DEFAULT_MULTIPLIER = 1000;
+    public static final String START_TIME_ATTRIBUTE = "org.openrepose.repose.logging.start.time";
     private final DecimalFormat decimalFormat = new DecimalFormat("#.##");
-    private double multiplier = DEFAULT_MULTIPLIER;
+    private double multiplier;
 
     public ResponseTimeHandler(double multiplier) {
         this.multiplier = multiplier;
