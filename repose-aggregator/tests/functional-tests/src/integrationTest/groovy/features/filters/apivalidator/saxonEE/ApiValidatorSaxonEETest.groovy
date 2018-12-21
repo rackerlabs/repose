dabb4daf9133a069a1a42f71fe070cb68436f115
wdschei <Bill.Scheidegger@Rackspace.com>
2018-12-21 08:50:43 -0600

    General test formatting and cleanup.

--- repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/apivalidator/saxonEE/ApiValidatorSaxonEETest.groovy
+++ repose-aggregator/tests/functional-tests/src/integrationTest/groovy/features/filters/apivalidator/saxonEE/ApiValidatorSaxonEETest.groovy
@@ -21,8 +21,9 @@ package features.filters.apivalidator.saxonEE
 
 import org.openrepose.framework.test.ReposeValveTest
 import org.rackspace.deproxy.Deproxy
-import org.rackspace.deproxy.MessageChain
 import org.rackspace.deproxy.Response
+
+import static javax.servlet.http.HttpServletResponse.*
 
 /*
  * Api validator tests ported over from and JMeter
@@ -34,10 +35,10 @@ class ApiValidatorSaxonEETest extends ReposeValveTest {
     private final String baseDefaultPath = "/wadl/default"
 
     private final Map<String, String> defaultHeaders = [
-            "Accept"         : "application/xml",
-            "Host"           : "localhost",
-            "Accept-Encoding": "identity",
-            "User-Agent"     : "gdeproxy"
+        "Accept"         : "application/xml",
+        "Host"           : "localhost",
+        "Accept-Encoding": "identity",
+        "User-Agent"     : "gdeproxy"
     ]
 
     def setupSpec() {
@@ -64,109 +65,119 @@ class ApiValidatorSaxonEETest extends ReposeValveTest {
     }
 
     def "Happy path: when no role passed, should get default wadl"() {
-        setup: "declare messageChain to be of type MessageChain"
-        MessageChain messageChain
-        def customHandler = { return new Response(200, "OK", [], reqBody) }
+        setup: "declare custom handler"
+        def customHandler = { return new Response(SC_OK, "OK", [], reqBody) }
 
-        when:
-        "When Requesting " + method + " " + request
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseDefaultPath +
-                request, method: method, headers: defaultHeaders,
-                requestBody: reqBody, defaultHandler: customHandler,
-                addDefaultHeaders: false
+        when: "When Requesting method request"
+        def messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseDefaultPath$request",
+            method: method,
+            headers: defaultHeaders,
+            requestBody: reqBody,
+            defaultHandler: customHandler,
+            addDefaultHeaders: false
         )
 
-        then:
-        "result should be " + responseCode
-        messageChain.receivedResponse.code.equals(responseCode)
-
-//        messageChain.receivedResponse.body.contains("XML Not Authorized... Syntax highlighting is magical.")
+        then: "result should be responseCode"
+        messageChain.receivedResponse.code as Integer == responseCode
 
         where:
-        responseCode | request                                               | method | reqBody
-        "200"        | "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"  | "GET"  | ""
-        "404"        | "/resource1x/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa" | "GET"  | ""
-        "405"        | "/resource1/id"                                       | "POST" | ""
-        "415"        | "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"  | "PUT"  | "some data"
-
+        responseCode              | request                                               | method | reqBody
+        SC_OK                     | "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"  | "GET"  | ""
+        SC_NOT_FOUND              | "/resource1x/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa" | "GET"  | ""
+        SC_METHOD_NOT_ALLOWED     | "/resource1/id"                                       | "POST" | ""
+        SC_UNSUPPORTED_MEDIA_TYPE | "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"  | "PUT"  | "some data"
     }
 
 
     def "Happy path: when Group Passed, Should Get Role Specific WADL"() {
-        setup: "declare messageChain to be of type MessageChain, additional headers"
-        MessageChain messageChain
+        setup: "declare additional headers"
         Map<String, String> headers = ["X-Roles": "group1", "Content-Type": "application/xml"]
 
         when: "When Requesting resource with x-roles"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseGroupPath +
-                "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", method: "GET", headers: defaultHeaders + headers)
+        def messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseGroupPath/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+            method: "GET",
+            headers: defaultHeaders + headers
+        )
 
         then: "should return resource"
-        messageChain.receivedResponse.code.equals("200")
+        messageChain.receivedResponse.code as Integer == SC_OK
 
         when: "When Requesting invalid resource with x-roles"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseGroupPath +
-                "/resource1x/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", method: "GET", headers: defaultHeaders + headers)
+        messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseGroupPath/resource1x/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+            method: "GET",
+            headers: defaultHeaders + headers
+        )
 
         then: "should return not found"
-        messageChain.receivedResponse.code.equals("404")
+        messageChain.receivedResponse.code as Integer == SC_NOT_FOUND
         messageChain.receivedResponse.body.contains("XML Not Authorized... Syntax highlighting is magical.")
 
         when: "When using invalid method with x-roles"
         messageChain = deproxy.makeRequest(url: reposeEndpoint + baseGroupPath +
-                "/resource1/id", method: "POST", headers: defaultHeaders + headers)
+            "/resource1/id", method: "POST", headers: defaultHeaders + headers)
 
         then: "should return not found"
-        messageChain.receivedResponse.code.equals("405")
+        messageChain.receivedResponse.code as Integer == SC_METHOD_NOT_ALLOWED
         messageChain.receivedResponse.body.contains("XML Not Authorized... Syntax highlighting is magical.")
 
         when: "When using valid media type with x-roles"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseGroupPath +
-                "/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa", method: "POST", headers: defaultHeaders + headers,
-                requestBody: "<c xmlns='http://test.openrespose/test/v1.1'><test>some data</test></c>")
+        messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseGroupPath/resource1/id/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+            method: "POST",
+            headers: defaultHeaders + headers,
+            requestBody: "<c xmlns='http://test.openrespose/test/v1.1'><test>some data</test></c>"
+        )
 
         then: "should return OK"
-        messageChain.receivedResponse.code.equals("200")
+        messageChain.receivedResponse.code as Integer == SC_OK
         // Origin service handler does not return a body
         // messageChain.receivedResponse.body.contains("some data")
     }
 
     def "Happy path: when Ignore XSD Extension enabled"() {
-        setup: "declare messageChain to be of type MessageChain, additional headers"
-        MessageChain messageChain
+        setup: "declare additional headers"
         Map<String, String> headers = ["X-Roles": "default", "Content-Type": "application/xml"]
 
         when: "When Requesting with valid content"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseDefaultPath +
-                "/resource2/unvalidated/echobody", method: "PUT", headers: defaultHeaders + headers,
-                requestBody: "<c xmlns=\"http://test.openrespose/test/v1.1\"><nodeList><node id=\"proxy-n01\" hostname=\"localhost\" http-port=\"8088\"  /></nodeList></c>")
+        def messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseDefaultPath/resource2/unvalidated/echobody",
+            method: "PUT",
+            headers: defaultHeaders + headers,
+            requestBody: "<c xmlns=\"http://test.openrespose/test/v1.1\"><nodeList><node id=\"proxy-n01\" hostname=\"localhost\" http-port=\"8088\"  /></nodeList></c>"
+        )
 
         then: "should return resource"
-        messageChain.receivedResponse.code.equals("200")
+        messageChain.receivedResponse.code as Integer == SC_OK
         // Origin service handler does not return a body
         // messageChain.receivedResponse.body.contains("<node hostname=\"localhost\" http-port=\"8088\" id=\"proxy-n01\"/>")
 
         when: "When Requesting with invalid content and Ignore XSD enabled"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseDefaultPath +
-                "/resource2/unvalidated/echobody", method: "PUT", headers: defaultHeaders + headers,
-                requestBody: "<c xmlns=\"http://test.openrespose/test/v1.1\"><nodeList><node2 id=\"proxy-n01\" hostname=\"localhost\" http-port=\"8088\"  /></nodeList></c>")
+        messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseDefaultPath/resource2/unvalidated/echobody",
+            method: "PUT",
+            headers: defaultHeaders + headers,
+            requestBody: "<c xmlns=\"http://test.openrespose/test/v1.1\"><nodeList><node2 id=\"proxy-n01\" hostname=\"localhost\" http-port=\"8088\"  /></nodeList></c>"
+        )
 
         then: "should return resource"
-        messageChain.receivedResponse.code.equals("200")
+        messageChain.receivedResponse.code as Integer == SC_OK
         // Origin service handler does not return a body
         // messageChain.receivedResponse.body.contains("<node2 hostname=\"localhost\" http-port=\"8088\" id=\"proxy-n01\"/>")
     }
 
     def "Unhappy path: When Passing to resource without required header"() {
-        setup: "declare messageChain to be of type MessageChain"
-        MessageChain messageChain
-
         when: "When Requesting default resource with no roles without required header"
-        messageChain = deproxy.makeRequest(url: reposeEndpoint + baseDefaultPath +
-                "/resource1/id/reqheader", method: "GET", headers: defaultHeaders)
+        def messageChain = deproxy.makeRequest(
+            url: "$reposeEndpoint$baseDefaultPath/resource1/id/reqheader",
+            method: "GET",
+            headers: defaultHeaders
+        )
 
         then: "should return resource"
-        messageChain.receivedResponse.code.equals("400")
+        messageChain.receivedResponse.code as Integer == SC_BAD_REQUEST
         messageChain.receivedResponse.body.contains("Expecting an HTTP header x-required-header")
     }
 }
