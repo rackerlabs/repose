f5e422f5f9fca7780d7be8dfb4dd6b26a7515c5d
Damien Johnson <damien.johnson@rackspace.com>
2019-01-09 16:26:54 -0600

    Dropping cluster support from the Remote Datastore

--- repose-aggregator/components/services/datastore-service/datastore-service-impl/src/main/java/org/openrepose/nodeservice/remote/RemoteDatastoreLauncherService.java
+++ repose-aggregator/components/services/datastore-service/datastore-service-impl/src/main/java/org/openrepose/nodeservice/remote/RemoteDatastoreLauncherService.java
@@ -25,13 +25,11 @@ import org.openrepose.core.filter.SystemModelInterrogator;
 import org.openrepose.core.services.RequestProxyService;
 import org.openrepose.core.services.config.ConfigurationService;
 import org.openrepose.core.services.datastore.DatastoreService;
-import org.openrepose.core.services.datastore.remote.config.RemoteClusterConfiguration;
 import org.openrepose.core.services.datastore.remote.config.RemoteDatastoreConfiguration;
 import org.openrepose.core.services.healthcheck.HealthCheckService;
 import org.openrepose.core.services.healthcheck.HealthCheckServiceProxy;
 import org.openrepose.core.services.healthcheck.Severity;
 import org.openrepose.core.spring.ReposeSpringProperties;
-import org.openrepose.core.systemmodel.config.ReposeCluster;
 import org.openrepose.core.systemmodel.config.SystemModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,8 +41,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import java.net.InetSocketAddress;
 import java.net.URL;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Named
 public class RemoteDatastoreLauncherService {
@@ -57,7 +53,6 @@ public class RemoteDatastoreLauncherService {
     private static final Logger LOG = LoggerFactory.getLogger(RemoteDatastoreLauncherService.class);
     private static final String REMOTE_DATASTORE_SERVICE_CONFIG_ISSUE = "remote-datastore-config-issue";
 
-    private final String clusterId;
     private final String nodeId;
     private final DatastoreService datastoreService;
     private final HealthCheckServiceProxy healthCheckServiceProxy;
@@ -65,19 +60,16 @@ public class RemoteDatastoreLauncherService {
     private final ConfigurationService configurationService;
     private final SystemModelListener systemModelListener;
     private final RemoteDatastoreConfigurationListener configurationListener;
-    private final AtomicReference<RemoteDatastoreConfiguration> currentConfiguration = new AtomicReference<>();
     private volatile boolean isRunning = false;
 
     @Inject
     public RemoteDatastoreLauncherService(
-            @Value(ReposeSpringProperties.NODE.CLUSTER_ID) String clusterId,
             @Value(ReposeSpringProperties.NODE.NODE_ID) String nodeId,
             DatastoreService datastoreService,
             ConfigurationService configurationService,
             HealthCheckService healthCheckService,
             RequestProxyService requestProxyService) {
 
-        this.clusterId = clusterId;
         this.nodeId = nodeId;
         this.datastoreService = datastoreService;
         this.healthCheckServiceProxy = healthCheckService.register();
@@ -119,22 +111,19 @@ public class RemoteDatastoreLauncherService {
 
         @Override
         public void configurationUpdated(SystemModel configurationObject) {
-            SystemModelInterrogator smi = new SystemModelInterrogator(clusterId, nodeId);
-            Optional<ReposeCluster> clusterOption = smi.getLocalCluster(configurationObject);
-            if (clusterOption.isPresent()) {
-                boolean listed = smi.getServiceForCluster(configurationObject, REMOTE_DATASTORE_NAME).isPresent();
+            SystemModelInterrogator smi = new SystemModelInterrogator(nodeId);
+            boolean listed = smi.getService(configurationObject, REMOTE_DATASTORE_NAME).isPresent();
 
-                if (listed && !isRunning) {
-                    //Note it as being broke, until it's properly configured.
-                    healthCheckServiceProxy.reportIssue(
-                            REMOTE_DATASTORE_SERVICE_CONFIG_ISSUE,
-                            "Remote Datastore Configuration Issue: Remote Datastore Specified in system model, but not yet configured",
-                            Severity.BROKEN);
-                    initRemoteDatastore();
-                } else if (!listed && isRunning) {
-                    //any health check problems are resolved when we stop it
-                    destroyRemoteDatastore();
-                }
+            if (listed && !isRunning) {
+                //Note it as being broke, until it's properly configured.
+                healthCheckServiceProxy.reportIssue(
+                        REMOTE_DATASTORE_SERVICE_CONFIG_ISSUE,
+                        "Remote Datastore Configuration Issue: Remote Datastore Specified in system model, but not yet configured",
+                        Severity.BROKEN);
+                initRemoteDatastore();
+            } else if (!listed && isRunning) {
+                //any health check problems are resolved when we stop it
+                destroyRemoteDatastore();
             }
         }
 
@@ -149,56 +138,14 @@ public class RemoteDatastoreLauncherService {
 
         @Override
         public void configurationUpdated(RemoteDatastoreConfiguration configurationObject) {
-            RemoteDatastoreConfiguration currentRemoteDatastoreConfiguration = currentConfiguration.get();
-            Optional<RemoteClusterConfiguration> oldClusterConfigOpt = Optional.ofNullable(currentRemoteDatastoreConfiguration)
-                    .flatMap(s -> s.getCluster().stream()
-                            .filter(cluster -> cluster.getId().equals(clusterId))
-                            .findFirst());
-            Optional<RemoteClusterConfiguration> newClusterConfigOpt = configurationObject
-                    .getCluster()
-                    .stream()
-                    .filter(cluster -> cluster.getId().equals(clusterId))
-                    .findFirst();
-
-            // IF only the old config had a Cluster for this ID,
-            // THEN destroy it;
-            // ELSE IF only the new config has a Cluster for this ID,
-            // THEN simple initialize it;
-            // ELSE IF both the old and new configs had a Cluster for this ID,
-            // AND something has changed,
-            // THEN destroy the old one
-            // AND initialize the new one;
-            // ELSE nothing has changed for this Cluster ID,
-            // SO do nothing.
-            if (oldClusterConfigOpt.isPresent() && !newClusterConfigOpt.isPresent()) {
-                destroyRemoteDatastore();
-            } else if (!oldClusterConfigOpt.isPresent() && newClusterConfigOpt.isPresent()) {
-                RemoteClusterConfiguration newClusterConfig = newClusterConfigOpt.get();
-                datastoreService.createRemoteDatastore(
-                        REMOTE_DATASTORE_NAME,
-                        requestProxyService,
-                        UUIDEncodingProvider.getInstance(),
-                        new InetSocketAddress(newClusterConfig.getHost(), newClusterConfig.getPort()),
-                        newClusterConfig.getConnectionPoolId(),
-                        newClusterConfig.isUseHTTPS());
-            } else if (oldClusterConfigOpt.isPresent() /*&& newClusterConfigOpt.isPresent()*/) {
-                RemoteClusterConfiguration oldClusterConfig = oldClusterConfigOpt.get();
-                RemoteClusterConfiguration newClusterConfig = newClusterConfigOpt.get();
-                if (!oldClusterConfig.getHost().equalsIgnoreCase(newClusterConfig.getHost()) ||
-                        !oldClusterConfig.getConnectionPoolId().equals(newClusterConfig.getConnectionPoolId()) ||
-                        oldClusterConfig.getPort() != newClusterConfig.getPort() ||
-                        oldClusterConfig.isUseHTTPS() != newClusterConfig.isUseHTTPS()) {
-                    destroyRemoteDatastore();
-                    datastoreService.createRemoteDatastore(
-                            REMOTE_DATASTORE_NAME,
-                            requestProxyService,
-                            UUIDEncodingProvider.getInstance(),
-                            new InetSocketAddress(newClusterConfig.getHost(), newClusterConfig.getPort()),
-                            newClusterConfig.getConnectionPoolId(),
-                            newClusterConfig.isUseHTTPS());
-                }
-            } //else { /* DO NOTHING */ }
-            currentConfiguration.set(configurationObject);
+            datastoreService.destroyDatastore(REMOTE_DATASTORE_NAME);
+            datastoreService.createRemoteDatastore(
+                    REMOTE_DATASTORE_NAME,
+                    requestProxyService,
+                    UUIDEncodingProvider.getInstance(),
+                    new InetSocketAddress(configurationObject.getHost(), configurationObject.getPort()),
+                    configurationObject.getConnectionPoolId(),
+                    configurationObject.isUseHTTPS());
             initialized = true;
             healthCheckServiceProxy.resolveIssue(REMOTE_DATASTORE_SERVICE_CONFIG_ISSUE);
         }
