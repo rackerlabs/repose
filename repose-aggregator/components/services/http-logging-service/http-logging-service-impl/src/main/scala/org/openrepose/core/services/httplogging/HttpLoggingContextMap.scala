13f93e199553b60a5146ccc8db606a86c9b66b27
Damien Johnson <damien.johnson@rackspace.com>
2019-05-13 14:41:15 -0500

    Initial implementation of HttpLoggingContextMap

--- repose-aggregator/components/services/http-logging-service/http-logging-service-impl/src/main/scala/org/openrepose/core/services/httplogging/HttpLoggingContextMap.scala
+++ repose-aggregator/components/services/http-logging-service/http-logging-service-impl/src/main/scala/org/openrepose/core/services/httplogging/HttpLoggingContextMap.scala
@@ -19,18 +19,86 @@
  */
 package org.openrepose.core.services.httplogging
 
+import java.time.Duration
+import java.util
+
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+import org.openrepose.commons.utils.http.CommonHttpHeader
+import org.openrepose.commons.utils.logging.TracingHeaderHelper
+
+import scala.collection.JavaConverters._
+import scala.language.implicitConversions
+import scala.util.Try
+
 /**
   * Converts an [[HttpLoggingContext]] into a [[Map]] for use by the templating
   * engine.
   */
 object HttpLoggingContextMap {
+
+  private final type Mapping = (String, Any)
+
+  // Handles exceptions thrown by mappings.
+  // Doing so enables a map to be created with values generated in a predetermined, but unreliable, manner.
+  private final implicit def optionalMapping(mapping: => Mapping): Option[Mapping] = {
+    Try(mapping)
+      .toOption
+      .filter(Function.tupled((_, value) => Option(value).isDefined))
+  }
+
   def from(httpLoggingContext: HttpLoggingContext): Map[String, AnyRef] = {
-    Map(
+    val mappings: Seq[Option[Mapping]] = Seq(
+      "inboundRequestProtocol" -> httpLoggingContext.getInboundRequest.getProtocol,
+      "outboundRequestProtocol" -> httpLoggingContext.getOutboundRequest.getProtocol,
       "inboundRequestMethod" -> httpLoggingContext.getInboundRequest.getMethod,
-      "outboundRequestMethod" -> httpLoggingContext.getInboundRequest.getMethod,
+      "outboundRequestMethod" -> httpLoggingContext.getOutboundRequest.getMethod,
+      "inboundRequestPath" -> httpLoggingContext.getInboundRequest.getRequestURI,
+      "outboundRequestPath" -> httpLoggingContext.getOutboundRequest.getRequestURI,
+      "inboundRequestQueryString" -> httpLoggingContext.getInboundRequest.getQueryString,
+      "outboundRequestQueryString" -> httpLoggingContext.getOutboundRequest.getQueryString,
+      "inboundRequestUrl" -> httpLoggingContext.getInboundRequest.getRequestURL,
+      "outboundRequestUrl" -> httpLoggingContext.getOutboundRequest.getRequestURL,
+      "inboundRequestHeaders" -> headerMap(httpLoggingContext.getInboundRequest),
+      "outboundRequestHeaders" -> headerMap(httpLoggingContext.getOutboundRequest),
+      "outboundResponseHeaders" -> headerMap(httpLoggingContext.getOutboundResponse),
+      "outboundResponseStatusCode" -> httpLoggingContext.getOutboundResponse.getStatus,
+      "outboundResponseReasonPhrase" -> httpLoggingContext.getOutboundResponseReasonPhrase,
+      "timeRequestReceived" -> httpLoggingContext.getTimeRequestReceived,
+      "timeToHandleRequest" -> Duration.between(httpLoggingContext.getTimeRequestReceived, httpLoggingContext.getTimeRequestCompleted),
+      "timeInOriginService" -> httpLoggingContext.getTimeInOriginService,
       "localIpAddress" -> httpLoggingContext.getInboundRequest.getLocalAddr,
       "remoteIpAddress" -> httpLoggingContext.getInboundRequest.getRemoteAddr,
+      "remoteHost" -> httpLoggingContext.getInboundRequest.getRemoteHost,
+      "traceId" -> TracingHeaderHelper.getTraceGuid(httpLoggingContext.getOutboundRequest.getHeader(CommonHttpHeader.TRACE_GUID)),
       "extensions" -> httpLoggingContext.getExtensions
     )
+
+    // Removes mappings where a value could not be generated, then restructures the data.
+    mappings
+      .flatten
+      .toMap
+      .mapValues(_.asInstanceOf[AnyRef])
+  }
+
+  private def headerMap(request: HttpServletRequest): util.Map[String, _ <: util.List[String]] = {
+    // Note that header names are lower-cased so that lookups are consistent.
+    // Unfortunately, even if we pass JTwig a case-insensitive map, we see that
+    // casing matters when rendering a template (implying that JTwig converts or
+    // wraps our map with their own which is case-sensitive).
+    request.getHeaderNames.asScala
+      .map(headerName => headerName.toLowerCase -> util.Collections.list(request.getHeaders(headerName)))
+      .toMap
+      .asJava
+  }
+
+  private def headerMap(response: HttpServletResponse): util.Map[String, _ <: util.List[String]] = {
+    // Note that header names are lower-cased so that lookups are consistent.
+    // Unfortunately, even if we pass JTwig a case-insensitive map, we see that
+    // casing matters when rendering a template (implying that JTwig converts or
+    // wraps our map with their own which is case-sensitive).
+    response.getHeaderNames.asScala
+      .map(headerName => headerName.toLowerCase -> new util.ArrayList[String](response.getHeaders(headerName)))
+      .toMap
+      .asJava
   }
 }
