4a7f58c0dea11004383e3b4acc510967d9eab69f
Damien Johnson <damien.johnson@rackspace.com>
2019-05-24 10:22:56 -0500

    Fixing the HttpLoggingServiceRequestLogTest after updates to the CUT

--- repose-aggregator/artifacts/valve/src/test/scala/org/openrepose/valve/jetty/HttpLoggingServiceRequestLogTest.scala
+++ repose-aggregator/artifacts/valve/src/test/scala/org/openrepose/valve/jetty/HttpLoggingServiceRequestLogTest.scala
@@ -21,9 +21,10 @@ package org.openrepose.valve.jetty
 
 import java.time.Instant
 
-import org.eclipse.jetty.server.{Request, Response}
+import org.eclipse.jetty.http.MetaData
+import org.eclipse.jetty.server.{HttpChannel, Request, Response}
 import org.junit.runner.RunWith
-import org.mockito.Matchers.any
+import org.mockito.Matchers.{any, anyLong}
 import org.mockito.Mockito.{never, verify, when}
 import org.openrepose.commons.utils.http.CommonRequestAttributes
 import org.openrepose.core.services.httplogging.{HttpLoggingContext, HttpLoggingService}
@@ -37,71 +38,105 @@ class HttpLoggingServiceRequestLogTest extends FunSpec with BeforeAndAfterEach w
   var httpLoggingService: HttpLoggingService = _
   var httpLoggingServiceRequestLog: HttpLoggingServiceRequestLog = _
 
+  var request: Request = _
+  var response: Response = _
+  var responseMetadata: MetaData.Response = _
+  var httpChannel: HttpChannel = _
+  var loggingContext: HttpLoggingContext = _
+
   override def beforeEach(): Unit = {
     httpLoggingService = mock[HttpLoggingService]
     httpLoggingServiceRequestLog = new HttpLoggingServiceRequestLog(httpLoggingService)
+
+    request = mock[Request]
+    response = mock[Response]
+    responseMetadata = mock[MetaData.Response]
+    httpChannel = mock[HttpChannel]
+    loggingContext = mock[HttpLoggingContext]
+
+    when(response.getCommittedMetaData)
+      .thenReturn(responseMetadata)
+    when(response.getHttpChannel)
+      .thenReturn(httpChannel)
+    when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
+      .thenReturn(loggingContext, Nil: _*)
   }
 
   describe("log") {
     it("should add the response to the context from the request") {
-      val request = mock[Request]
-      val response = mock[Response]
-      val loggingContext = mock[HttpLoggingContext]
-
-      when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
-        .thenReturn(loggingContext, Nil: _*)
-
       httpLoggingServiceRequestLog.log(request, response)
 
       verify(loggingContext).setOutboundResponse(response)
     }
 
+    it("should add the status code to the context from the request") {
+      val statusCode = 418
+
+      when(responseMetadata.getStatus)
+        .thenReturn(statusCode)
+
+      httpLoggingServiceRequestLog.log(request, response)
+
+      verify(loggingContext).setOutboundResponseStatusCode(statusCode)
+    }
+
     it("should add the reason phrase to the context from the request") {
-      val request = mock[Request]
-      val response = mock[Response]
-      val loggingContext = mock[HttpLoggingContext]
       val reasonPhrase = "Test reason"
 
-      when(response.getReason)
+      when(responseMetadata.getReason)
         .thenReturn(reasonPhrase)
-      when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
-        .thenReturn(loggingContext, Nil: _*)
 
       httpLoggingServiceRequestLog.log(request, response)
 
       verify(loggingContext).setOutboundResponseReasonPhrase(reasonPhrase)
     }
 
+    it("should add the bytes written to the context from the request") {
+      val bytesWritten = 1234L
+
+      when(httpChannel.getBytesWritten)
+        .thenReturn(bytesWritten)
+
+      httpLoggingServiceRequestLog.log(request, response)
+
+      verify(loggingContext).setOutboundResponseBytesWritten(bytesWritten)
+    }
+
+    it("should add the content length to the context from the request") {
+      val contentLength = 9876L
+
+      when(responseMetadata.getContentLength)
+        .thenReturn(contentLength)
+
+      httpLoggingServiceRequestLog.log(request, response)
+
+      verify(loggingContext).setOutboundResponseContentLength(contentLength)
+    }
+
+    Set(Long.MinValue, -1L).foreach { contentLength =>
+      it(s"should not add the content length to the context from the request if it is $contentLength") {
+        when(responseMetadata.getContentLength)
+          .thenReturn(contentLength)
+
+        httpLoggingServiceRequestLog.log(request, response)
+
+        verify(loggingContext, never).setOutboundResponseContentLength(anyLong)
+      }
+    }
+
     it("should add the time request completed to the context from the request") {
-      val request = mock[Request]
-      val response = mock[Response]
-      val loggingContext = mock[HttpLoggingContext]
-
-      when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
-        .thenReturn(loggingContext, Nil: _*)
-
       httpLoggingServiceRequestLog.log(request, response)
 
       verify(loggingContext).setTimeRequestCompleted(any[Instant])
     }
 
     it("should close the context from the request") {
-      val request = mock[Request]
-      val response = mock[Response]
-      val loggingContext = mock[HttpLoggingContext]
-
-      when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
-        .thenReturn(loggingContext, Nil: _*)
-
       httpLoggingServiceRequestLog.log(request, response)
 
       verify(httpLoggingService).close(loggingContext)
     }
 
     it("should fail to close the context gracefully if the provided from the request is invalid") {
-      val request = mock[Request]
-      val response = mock[Response]
-
       when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
         .thenReturn("some random string object", Nil: _*)
 
@@ -111,9 +146,6 @@ class HttpLoggingServiceRequestLogTest extends FunSpec with BeforeAndAfterEach w
     }
 
     it("should fail to close the context gracefully if the provided from the request is missing") {
-      val request = mock[Request]
-      val response = mock[Response]
-
       when(request.getAttribute(CommonRequestAttributes.HTTP_LOGGING_CONTEXT))
         .thenReturn(null, Nil: _*)
 
