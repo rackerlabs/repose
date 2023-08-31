ae4097bc7def867fc2d2f39b6c54b01e2326dfb9
Karteek Kanneganti <karteek.kanneganti@rackspace.com>
2023-08-30 22:38:11 -0500

    update modules, fix jena iri log4j implementation and tests

--- repose-aggregator/components/filters/translation-filter/src/main/java/org/openrepose/filters/translation/resolvers/InputStreamUriParameterResolver.java
+++ repose-aggregator/components/filters/translation-filter/src/main/java/org/openrepose/filters/translation/resolvers/InputStreamUriParameterResolver.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -80,7 +80,7 @@ public class InputStreamUriParameterResolver extends SourceUriResolver {
     public String getHref(InputStream inputStreamReference) {
         try {
             return PREFIX + UriUtils.encodePathSegment(inputStreamReference.toString(), "utf-8");
-        } catch (UnsupportedEncodingException ex) {
+        } catch (Exception ex) {
             LOG.trace("Unable to encode the path segment to utf-8", ex);
             return PREFIX + inputStreamReference.toString();
         }
